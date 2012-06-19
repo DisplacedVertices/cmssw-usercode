84e7296c003aaa0ae31e5bccf1d58b6dd01c845b
tucker <>
2012-06-19 15:27:39 +0000

    Get original parton (gluon) vertex and use it as origin.

--- MFVNeutralino/plugins/GenHistos.cc
+++ MFVNeutralino/plugins/GenHistos.cc
@@ -3,6 +3,7 @@
 #include "TH2F.h"
 #include "TLorentzVector.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
 #include "DataFormats/JetReco/interface/GenJet.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/METReco/interface/GenMET.h"
@@ -27,6 +28,8 @@ class MFVNeutralinoGenHistos : public edm::EDAnalyzer {
   const edm::InputTag gen_met_src;
   
   TH2F* h_vtx_2d;
+
+  edm::ESHandle<ParticleDataTable> pdt;
 };
 
 MFVNeutralinoGenHistos::MFVNeutralinoGenHistos(const edm::ParameterSet& cfg)
@@ -48,11 +51,30 @@ void MFVNeutralinoGenHistos::analyze(const edm::Event& event, const edm::EventSe
   event.getByLabel(gen_jet_src, gen_jets);
   edm::Handle<reco::GenMETCollection> gen_mets;
   event.getByLabel(gen_met_src, gen_mets);
-  const reco::GenMET& gen_met = gen_mets->at(0);
+  //const reco::GenMET& gen_met = gen_mets->at(0);
 
+  edm::Handle<reco::BeamSpot> bs;
+  event.getByLabel("offlineBeamSpot", bs);
+  printf("beamspot x,y: %f, %f\n", bs->x0(), bs->y0());
+
+  // Assume pythia line 2 (probably a gluon) has the "right"
+  // production vertex. (The protons are just at 0,0,0.)
+  const reco::GenParticle& for_vtx = gens->at(2);
+  float vx = for_vtx.vx(), vy = for_vtx.vy(), vz = for_vtx.vz();
+  printf("gluon x,y: %f, %f\n", vx, vy);
+  
   BOOST_FOREACH(const reco::GenParticle& gen, *gens) {
-    if (gen.pdgId() == 1000022 && gen.status() == 52) {
-      h_vtx_2d->Fill(gen.vx(), gen.vy());
+    if (gen.pdgId() == 1000022 && gen.status() == 52 && gen.numberOfDaughters() == 3) {
+      printf("neutralino vertex: %f, %f, %f\n", gen.vx() - vx, gen.vy() - vy, gen.vz() - vz);
+      float dx, dy, dz;
+      dx = dy = dz = 1e99;
+      for (int i = 0, ie = int(gen.numberOfDaughters()); i < ie; ++i) {
+	dx =  gen.daughter(i)->vx() - vx;
+	dy =  gen.daughter(i)->vy() - vy;
+	dz =  gen.daughter(i)->vz() - vz;
+	printf("neutralino daughter %i: id %i pt %f eta %f phi %f vertex: %f, %f, %f (rho: %f)\n", i, gen.daughter(i)->pdgId(), gen.daughter(i)->pt(), gen.daughter(i)->eta(), gen.daughter(i)->phi(), dx, dy, dz, sqrt(dx*dx + dy*dy));
+      }
+      h_vtx_2d->Fill(dx, dy);
     }
   }
 }
