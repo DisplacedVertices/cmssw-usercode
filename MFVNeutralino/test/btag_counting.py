d1e88ef8c374633aa172e388e026e887c8dba4fc
tucker <>
2012-07-02 18:49:46 +0000

    PAT version

--- MFVNeutralino/test/btag_counting.py
+++ MFVNeutralino/test/btag_counting.py
@@ -2,11 +2,11 @@ import os, sys, FWCore.ParameterSet.Config as cms
 
 process = cms.Process('MFVNeutralino')
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('/store/user/tucker/mfvneutralino_genfsimreco_tau1mm/mfvneutralino_genfsimreco_tau1mm//f0b5b0c98c357fc0015e0194f7aef803/fastsim_9_1_JND.root'))
+process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('/store/user/tucker/mfvneutralino_genfsimreco_tau0/sstoptuple_mfvN3jtau0//8f6e1718f92d8def674221dd8677caf1/pat_1_1_gKm.root'))
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 process.TFileService = cms.Service('TFileService', fileName = cms.string('btag_counting.root'))
 process.load('FWCore.MessageLogger.MessageLogger_cfi')
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
 btag_srcs = [
     ('CSV',   'combinedSecondaryVertexBJetTags',      (0.244, 0.679, 0.898)),
@@ -18,18 +18,22 @@ btag_srcs = [
     ('TCHP',  'trackCountingHighPurBJetTags',         (1.19, 1.93, 3.41)),
     ]
 
+debug = 'debug' in sys.argv
+
 process.p = cms.Path()
 for btag_name, src, bdisc_mins in btag_srcs:
     for bdisc_min in bdisc_mins:
-        obj = cms.EDAnalyzer('MFVNeutralinoBTagCounting',
-                             btag_src = cms.InputTag(src),
+        obj = cms.EDAnalyzer('MFVNeutralinoPATBTagCounting',
+                             jet_src = cms.InputTag('selectedPatJetsPF'),
+                             b_discriminator_name = cms.string(src),
                              jet_pt_min = cms.double(50),
-                             bdisc_min = cms.double(bdisc_min)
+                             bdisc_min = cms.double(bdisc_min),
+                             verbose = cms.bool(debug),
                              )
         name = btag_name + 'pt50bd' + str(bdisc_min).replace('.','p')
         setattr(process, name, obj)
         process.p *= obj
-                         
+    
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     if 'debug' in sys.argv:
         raise RuntimeError('refusing to submit jobs in debug (verbose print out) mode')
@@ -54,11 +58,11 @@ return_data = 1
     testing = 'testing' in sys.argv
 
     jobs = [
-        ('tau0',     '/mfvneutralino_genfsimreco_tau0/tucker-mfvneutralino_genfsimreco_tau0-aa8b56a9a9cba6aa847bda9acf329ad0/USER'),
-        ('tau100um', '/mfvneutralino_genfsimreco_tau100um/tucker-mfvneutralino_genfsimreco_tau100um-465709e5340ac2cc11e2751b48bbef3e/USER'),
-        ('tau10um',  '/mfvneutralino_genfsimreco_tau10um/tucker-mfvneutralino_genfsimreco_tau10um-719b1b049e9de8135afa1f308d0994e6/USER'),
-        ('tau1mm',   '/mfvneutralino_genfsimreco_tau1mm/tucker-mfvneutralino_genfsimreco_tau1mm-f0b5b0c98c357fc0015e0194f7aef803/USER'),
-        ('tau9p9mm', '/mfvneutralino_genfsimreco_tau9p9mm/tucker-mfvneutralino_genfsimreco_tau9p9mm-891f0c49f79ad2222cb205736c37de4f/USER'),
+        ('tau0',     '/mfvneutralino_genfsimreco_tau0/tucker-sstoptuple_mfvN3jtau0-8f6e1718f92d8def674221dd8677caf1/USER'),	  
+        ('tau100um', '/mfvneutralino_genfsimreco_tau100um/tucker-sstoptuple_mfvN3jtau100um-8f6e1718f92d8def674221dd8677caf1/USER'),
+        ('tau10um',  '/mfvneutralino_genfsimreco_tau10um/tucker-sstoptuple_mfvN3jtau10um-8f6e1718f92d8def674221dd8677caf1/USER'),  
+        ('tau1mm',   '/mfvneutralino_genfsimreco_tau1mm/tucker-sstoptuple_mfvN3jtau1mm-8f6e1718f92d8def674221dd8677caf1/USER'),	  
+        ('tau9p9mm', '/mfvneutralino_genfsimreco_tau9p9mm/tucker-sstoptuple_mfvN3jtau9p9mm-8f6e1718f92d8def674221dd8677caf1/USER'),
         ]
 
     for name, dataset in jobs:
