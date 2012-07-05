b5d317c4abcb1a3c80ecde28477fa0c9b65fe923
tucker <>
2012-07-05 23:10:37 +0000

    Get run/lumi/event from files for various debugging purposes.

---
+++ Tools/plugins/EventIdRecorder.cc
@@ -0,0 +1,38 @@
+#include "TTree.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+class EventIdRecorder : public edm::EDAnalyzer {
+public:
+  explicit EventIdRecorder(const edm::ParameterSet&);
+
+private:
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+
+  unsigned run;
+  unsigned lumi;
+  unsigned evt;
+  TTree* tree;
+};
+
+EventIdRecorder::EventIdRecorder(const edm::ParameterSet& cfg) {
+  edm::Service<TFileService> fs;
+
+  tree = fs->make<TTree>("event_ids", "");
+  tree->Branch("run",   &run,   "run/i");
+  tree->Branch("lumi",  &lumi,  "lumi/i");
+  tree->Branch("event", &evt,   "event/i");
+}
+
+void EventIdRecorder::analyze(const edm::Event& event, const edm::EventSetup&) {
+  run  = event.id().run();
+  lumi = event.luminosityBlock();
+  evt  = event.id().event();
+  tree->Fill();
+}
+
+DEFINE_FWK_MODULE(EventIdRecorder);
