b4a04a8d3432a5b0a6dba1c42db9e4890953901d
tucker <>
2012-08-03 12:45:54 +0000

    simple reorg

--- Tools/test/event_ids.py
+++ Tools/test/event_ids.py
@@ -6,10 +6,9 @@ process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring(''))
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 process.load('FWCore.MessageLogger.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000000
-
 process.TFileService = cms.Service('TFileService', fileName = cms.string('eid.root'))
-process.eid = cms.EDAnalyzer('EventIdRecorder')
-process.p = cms.Path(process.eid)
 
 process.source.fileNames = files = [x.strip() for x in open('infiles.txt') if x.strip()]
-process.eid.notes = cms.string('\n'.join(files))
+
+process.eid = cms.EDAnalyzer('EventIdRecorder', notes = cms.string('\n'.join(files))
+process.p = cms.Path(process.eid)
