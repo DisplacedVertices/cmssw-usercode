f4b080935d90d04f5064d0f31395345658feb277
tucker <>
2012-08-03 11:51:18 +0000

    check_gen_particles flag to record the first parton's pz in the tree (useful for checking that generated samples are not duplicates).

--- Tools/plugins/EventIdRecorder.cc
+++ Tools/plugins/EventIdRecorder.cc
@@ -1,3 +1,4 @@
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "TTree.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
@@ -13,19 +14,26 @@ public:
 private:
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
 
+  bool check_gen_particles;
   unsigned run;
   unsigned lumi;
   unsigned evt;
+  float first_parton_pz;
   TTree* tree;
 };
 
-EventIdRecorder::EventIdRecorder(const edm::ParameterSet& cfg) {
+EventIdRecorder::EventIdRecorder(const edm::ParameterSet& cfg)
+  : check_gen_particles(cfg.getParameter<bool>("check_gen_particles"))
+{
   edm::Service<TFileService> fs;
 
   tree = fs->make<TTree>("event_ids", "");
   tree->Branch("run",   &run,   "run/i");
   tree->Branch("lumi",  &lumi,  "lumi/i");
   tree->Branch("event", &evt,   "event/i");
+
+  if (check_gen_particles)
+    tree->Branch("first_parton_pz", &first_parton_pz, "first_parton_pz/F");
 
   if (cfg.existsAs<std::string>("notes"))
     tree->SetAlias("notes", cfg.getParameter<std::string>("notes").c_str());
@@ -35,6 +43,13 @@ void EventIdRecorder::analyze(const edm::Event& event, const edm::EventSetup&) {
   run  = event.id().run();
   lumi = event.luminosityBlock();
   evt  = event.id().event();
+
+  if (check_gen_particles) {
+    edm::Handle<reco::GenParticleCollection> gens;
+    event.getByLabel("genParticles", gens);
+    first_parton_pz = gens->at(2).pz();
+  }
+
   tree->Fill();
 }
 
