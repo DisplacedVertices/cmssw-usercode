7e4fc7ace2e88a6d1e07902479f9e1e386db68d5
tucker <>
2012-09-11 19:41:44 +0000

    crab script

--- Tools/test/particle_list.py
+++ Tools/test/particle_list.py
@@ -17,3 +17,27 @@ process.ParticleListDrawer = cms.EDAnalyzer('ParticleListDrawer',
 process.p = cms.Path(process.ParticleListDrawer)
 
 process.source.fileNames = [x.strip() for x in open('infiles.txt') if x.strip()]
+
+if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
+    crab_cfg = '''
+[CRAB]
+jobtype = cmssw
+scheduler = condor
+
+[CMSSW]
+dbs_url = https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_02_writer/servlet/DBSServlet
+datasetpath = /QCD_Pt-15to30_TuneZ2star_8TeV_pythia6/tucker-sstoptuple_v1_qcd15-3312fbeda721580c3cdebaec6739016e/USER
+pset = particle_list.py
+total_number_of_events = -1
+events_per_job = 1000
+
+[USER]
+ui_working_dir = crab_particle_list
+return_data = 1
+'''
+    
+    just_testing = 'testing' in sys.argv
+    open('crab.cfg', 'wt').write(crab_cfg)
+    if not just_testing:
+        os.system('crab -create -submit')
+        os.system('rm crab.cfg')
