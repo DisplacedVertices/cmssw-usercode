42fbe7943e77c47d4eced692b274fb9a5ca14fab
tucker <>
2012-09-20 22:59:00 +0000

    Reorg debug bit.

--- Tools/interface/TriggerHelper.h
+++ Tools/interface/TriggerHelper.h
@@ -10,16 +10,19 @@ struct TriggerHelper {
 
   TriggerHelper(const edm::TriggerResults& trigger_results_, const edm::TriggerNames& trigger_names_) : trigger_results(trigger_results_), trigger_names(trigger_names_) {}
 
-  bool pass(const char* name, bool& found) const {
-#if 0
-    printf("TriggerHelper debug\nname: %s\n", name);
-    printf("names size: %lu\n", trigger_names.size());
-    for (size_t i = 0, ie= trigger_names.size(); i < ie; ++i)
-      printf("name %lu: %s\n", i, trigger_names.triggerName(i).c_str());
-    printf("done.\n\n");
-#endif
+  bool pass(const char* name, bool& found, const bool debug=false) const {
     const unsigned ndx = trigger_names.triggerIndex(name);
     found = ndx < trigger_results.size();
+    if (debug) {
+      printf("TriggerHelper debug\nname: %s\n", name);
+      printf("names size: %lu\n", trigger_names.size());
+      for (size_t i = 0, ie= trigger_names.size(); i < ie; ++i)
+	printf("name %lu: %s\n", i, trigger_names.triggerName(i).c_str());
+      printf("ndx: %u => name %s found? %i\n", ndx, name, found);
+      if (found)
+	printf("trigger fired? %i\n", trigger_results.accept(ndx));
+      printf("done.\n");
+    }
     return found ? trigger_results.accept(ndx) : false;
   }
 
