23c8d10988466f4d5df31d75f8d14a7868b3958d
tucker <>
2012-10-24 21:51:40 +0000

    process.options.wantSummary line

--- Tools/python/BasicAnalyzer_cfg.py
+++ Tools/python/BasicAnalyzer_cfg.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('BasicAnalyzer')
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('file:pat.root'))
 process.load('FWCore.MessageLogger.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000000
