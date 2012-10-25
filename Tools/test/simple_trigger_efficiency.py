42285c7a35c091edf31879fa4fc1776f6054b2e7
tucker <>
2012-10-25 18:18:53 +0000

    Add example of changing denominator to only use events with muons from W decay (or to same in acceptance); "analyze" -> "table"; apply prescales (requires a dummy python module to export a function get(hlt_path) that returns (l1_prescale, hlt_prescale, overall_prescale)); example comparison plot between eff of all events and eff of events with muons.

--- Tools/test/simple_trigger_efficiency.py
+++ Tools/test/simple_trigger_efficiency.py
@@ -5,12 +5,23 @@ from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
 process.source.fileNames = ['/store/user/tucker/mfvneutralino_genfsimreco_tau1mm/mfvneutralino_genfsimreco_tau1mm/f0b5b0c98c357fc0015e0194f7aef803/fastsim_47_1_0c1.root']
 process.TFileService.fileName = 'simple_trigger_efficiency.root'
 
+process.genMuons = cms.EDFilter('CandViewSelector', src = cms.InputTag('genParticles'), cut = cms.string('abs(pdgId) == 13 && abs(mother.pdgId) == 24'))
+process.genMuonCount = cms.EDFilter('CandViewCountFilter', src = cms.InputTag('genMuons'), minNumber = cms.uint32(1))
+                                
+process.genMuonsInAcc = cms.EDFilter('CandViewSelector', src = cms.InputTag('genParticles'), cut = cms.string('abs(pdgId) == 13 && abs(mother.pdgId) == 24 && pt > 17 && abs(eta) < 2.1'))
+process.genMuonInAccCount = cms.EDFilter('CandViewCountFilter', src = cms.InputTag('genMuonsInAcc'), minNumber = cms.uint32(1))
+
 process.SimpleTriggerEfficiency = cms.EDAnalyzer('SimpleTriggerEfficiency', trigger_results_src = cms.InputTag('TriggerResults', '', 'HLT'))
-process.p = cms.Path(process.SimpleTriggerEfficiency)
+process.SimpleTriggerEfficiencyMu = process.SimpleTriggerEfficiency.clone()
+process.SimpleTriggerEfficiencyMuInAcc = process.SimpleTriggerEfficiency.clone()
+
+process.p1 = cms.Path(process.SimpleTriggerEfficiency)
+process.p2 = cms.Path(process.genMuons      * process.genMuonCount      * process.SimpleTriggerEfficiencyMu)
+process.p3 = cms.Path(process.genMuonsInAcc * process.genMuonInAccCount * process.SimpleTriggerEfficiencyMuInAcc)
 
 ################################################################################
 
-if running_script and 'analyze' in sys.argv:
+def get_f():
     try:
         fn = [x for x in sys.argv[1:] if x.endswith('.root')][0]
     except IndexError:
@@ -19,13 +30,24 @@ if running_script and 'analyze' in sys.argv:
         print 'need a input file! (%s not found)' % fn
         sys.exit(1)
 
-    from JMTucker.Tools.ROOTTools import ROOT, clopper_pearson
     f = ROOT.TFile(fn)
-    hnum = f.Get('SimpleTriggerEfficiency/triggers_pass_num')
-    hden = f.Get('SimpleTriggerEfficiency/triggers_pass_den')
+    return f
 
+def get_hists(f, dn):
+    hnum = f.Get(dn).Get('triggers_pass_num')
+    hden = f.Get(dn).Get('triggers_pass_den')
+    return hnum, hden
+
+if running_script and 'table' in sys.argv:
+    from JMTucker.Tools.ROOTTools import ROOT
+    f = get_f()
+    hnum, hden = get_hists(f, 'SimpleTriggerEfficiencyMuInAcc')
     print 'number of events:', hden.GetBinContent(1)
+
+    apply_prescales = True
     
+    from JMTucker.Tools.ROOTTools import clopper_pearson
+    import prescales
     width = 0
     content = []
     for i in xrange(1, hden.GetNbinsX() + 1):
@@ -36,9 +58,19 @@ if running_script and 'analyze' in sys.argv:
         den = hden.GetBinContent(i)
         eff, lo, hi = clopper_pearson(num, den)
 
-        content.append((i-1, path, eff, lo, hi))
+        l1, hlt, overall = prescales.get(path)
+            
+        if apply_prescales:
+            if overall > 0:
+                eff /= overall
+                lo /= overall
+                hi /= overall
+            else:
+                eff = lo = hi = 0.
 
-    fmt = '(%3i) %' + str(width + 2) + 's %.4f  [%.4f, %.4f]'
+        content.append((i-1, path, eff, lo, hi, l1, hlt, overall))
+
+    fmt = '(%3i) %' + str(width + 2) + 's %.4f  68%% CL: [%.4f, %.4f]  (prescales: %10i * %10i = %10i )'
 
     print 'sorted by trigger bit:'
     for c in content:
@@ -48,6 +80,26 @@ if running_script and 'analyze' in sys.argv:
     content.sort(key=lambda x: x[2], reverse=True)
     for c in content:
         print fmt % c
+
+################################################################################
+            
+if running_script and 'compare' in sys.argv:
+    from JMTucker.Tools.ROOTTools import ROOT, set_style, plot_saver, histogram_divide
+    set_style()
+    ps = plot_saver('plots/mfv_simple_eff')
+    
+    f = get_f()
+    hnum,    hden    = get_hists(f, 'SimpleTriggerEfficiency')
+    hnum_mu, hden_mu = get_hists(f, 'SimpleTriggerEfficiencyMu')
+
+    eff_all = histogram_divide(hnum,    hden)
+    eff_mu  = histogram_divide(hnum_mu, hden_mu)
+
+    eff_all.SetLineColor(ROOT.kRed)
+    eff_all.Draw('APL')
+    eff_mu.SetLineColor(ROOT.kBlue)
+    eff_mu.Draw('P same')
+    ps.save('compare')
 
 ################################################################################
             
