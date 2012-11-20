9e8233e914d72af6003f90b509172f9d76f84570
tucker <>
2012-11-20 22:34:30 +0000

    MCInteraction for MFV LSP -> 3 jet (t,s,b).

---
+++ Tools/interface/MCInteractionMFV3j.h
@@ -0,0 +1,41 @@
+#ifndef JMTucker_Tools_MCInteractionMFV3j_h
+#define JMTucker_Tools_MCInteractionMFV3j_h
+
+#include "JMTucker/Tools/interface/MCInteraction.h"
+
+struct MCInteractionMFV3j : public MCInteraction { 
+  const reco::GenParticle* lsps[2];
+  const reco::GenParticle* stranges[2];
+  const reco::GenParticle* bottoms[2];
+  const reco::GenParticle* tops[2];
+  const reco::GenParticle* Ws[2];
+  const reco::GenParticle* bottoms_from_tops[2];
+  const reco::GenParticle* W_daughters[2][2]; // first index is same as above, second is W daughters in order (d-type, u-type quark) or (charged lepton, neutrino)
+
+  int num_leptonic;
+  int decay_type[2]; // for Wplus and Wminus decays (the array index) into e, mu, tau, hadronic (values 0-3)
+
+  // "Plain" momentum four-vectors.
+  TLorentzVector p4_lsps[2];
+  TLorentzVector p4_stranges[2];
+  TLorentzVector p4_bottoms[2];
+  TLorentzVector p4_tops[2];
+  TLorentzVector p4_Ws[2];
+  TLorentzVector p4_bottoms_from_tops[2];
+  TLorentzVector p4_W_daughters[2][2];
+  TLorentzVector p4_W_neutrinosum;
+
+  MCInteractionMFV3j()
+    : MCInteraction(MCInteraction::pythia8)
+  {
+    Clear();
+  }
+
+  virtual void Clear();
+  virtual bool Valid();
+  virtual void Fill();
+  virtual void SetFourVectors();
+  virtual void Print(std::ostream&);
+};
+
+#endif
---
+++ Tools/src/MCInteractionMFV3j.cc
@@ -0,0 +1,183 @@
+#include "DataFormats/Math/interface/deltaR.h"
+#include "JMTucker/Tools/interface/MCInteractionMFV3j.h"
+#include "JMTucker/Tools/interface/GenUtilities.h"
+#include "JMTucker/Tools/interface/Utilities.h"
+
+void MCInteractionMFV3j::Clear() {
+  MCInteraction::Clear();
+  for (int i = 0; i < 2; ++i) {
+    lsps[i] = stranges[i] = bottoms[i] = tops[i] = Ws[i] = bottoms_from_tops[i] = 0;
+    for (int j = 0; j < 2; ++j)
+      W_daughters[i][j] = 0;
+    decay_type[i] = -1;
+  }
+  num_leptonic = -1;
+}
+
+bool MCInteractionMFV3j::Valid() {
+  // bottoms_from_tops can be null if Vtb != 1 in gen, but everything else has to be there
+  return 
+    lsps[0] && stranges[0] && bottoms[0] && tops[0] && Ws[0] && W_daughters[0][0] && W_daughters[0][1] &&
+    lsps[1] && stranges[1] && bottoms[1] && tops[1] && Ws[1] && W_daughters[1][0] && W_daughters[1][1];
+}
+
+int sgn(int x) {
+  return x >= 0 ? 1 : -1;
+}
+
+void MCInteractionMFV3j::Fill() {
+
+  // JMTBAD split class into TopsPythia8 and the rest
+
+  const int lsp_id = 1000021;
+  const int ndau = 3;
+  const int dau_id_order[ndau] = { 3, 5, 6 };
+
+  // Find the LSPs (e.g. gluinos or neutralinos). Since this is
+  // PYTHIA8 there are lots of copies -- try to get the ones that
+  // decay to the three quarks.
+  for (int i = 0, ie = int(gen_particles->size()); i < ie; ++i) {
+    const reco::GenParticle& gen = gen_particles->at(i);
+    if (gen.pdgId() != lsp_id || gen.numberOfDaughters() != 3)
+      continue;
+
+    const reco::GenParticle& lsp = gen;
+
+    size_t which = 0;
+    if (lsps[0] == 0)
+      lsps[0] = &lsp;
+    else {
+      die_if_not(reco::deltaR(*lsps[0], gen) > 0.01, "found same LSP twice");
+      which = 1;
+      lsps[1] = &lsp;
+    }
+
+    // Get the immediate daughters, in order by the ids specified in
+    // dau_id_order.
+    const reco::Candidate* daughters[ndau] = {0};
+    for (int i = 0; i < ndau; ++i) {
+      int id = lsp.daughter(i)->pdgId();
+      for (int j = 0; j < ndau; ++j) {
+	if (abs(id) == dau_id_order[j])
+	  daughters[j] = lsp.daughter(i);
+      }
+    }
+      
+    // Make sure we found all three daughters, and then get the
+    // "final" daughters (PYTHIA8 likes to copy things a lot while it
+    // messes with e.g. gluon radiation).
+    for (int i = 0; i < ndau; ++i)
+      daughters[i] = final_candidate(daughters[i], 21);
+
+    // The asserts protect against dau_id_order changing above.
+    assert(abs(daughters[0]->pdgId()) == 3); stranges[which] = dynamic_cast<const reco::GenParticle*>(daughters[0]);
+    assert(abs(daughters[1]->pdgId()) == 5); bottoms [which] = dynamic_cast<const reco::GenParticle*>(daughters[1]);
+    assert(abs(daughters[2]->pdgId()) == 6); tops    [which] = dynamic_cast<const reco::GenParticle*>(daughters[2]);
+
+    die_if_not(tops[which]->numberOfDaughters() >= 2,
+	       "at least one top doesn't have at least two daughters: tops[%i] %i", which, tops[which]->numberOfDaughters());
+
+    // Find the Ws and bs from top decay. Bottom or bottombar might
+    // not be there, since |Vtb| isn't exactly 1. The W may have a
+    // lot of copies, but the copies should always have just one
+    // daughter until we reach the actual W decay (qq' or lnu). The
+    // bottom also can have a lot of copies, with the daughter being
+    // just a new bottom, or along with some gluons.
+    Ws[which]                = dynamic_cast<const reco::GenParticle*>(final_candidate(daughter_with_id(tops[which], sgn(tops[which]->pdgId()) * 24),  0));
+    bottoms_from_tops[which] = dynamic_cast<const reco::GenParticle*>(final_candidate(daughter_with_id(tops[which], sgn(tops[which]->pdgId()) *  5), 21));
+
+    die_if_not(Ws[which],
+	       "at least one W not found: Ws[%i] == %p",
+	       which, Ws[which]);
+    die_if_not(Ws[which]->numberOfDaughters() >= 2,
+	       "one W did not have at least two daughters: Ws[%i] id %i numDau %i",
+	       which, Ws[which]->pdgId(), Ws[which]->numberOfDaughters());
+
+    // Find the W daughters, and store them in the order (down-type
+    // quark, up-type quark) or (charged lepton, neutrino).
+    std::vector<const reco::GenParticle*> daus;
+    for (int j = 0, je = Ws[which]->numberOfDaughters(); j < je; ++j) {
+      const reco::Candidate* d = Ws[which]->daughter(j);
+      if (d->pdgId() != Ws[which]->pdgId()) {
+	int allowed_other = is_quark(d) ? 21 : 0; // JMTBAD photons?
+	daus.push_back(dynamic_cast<const reco::GenParticle*>(final_candidate(d, allowed_other)));
+      }
+    }
+    die_if_not(daus.size() == 2, "a W did not have exactly two non-W daughters: which=%i", which);
+    const int order = abs(daus[0]->pdgId()) % 2 == 0;
+    W_daughters[which][0] = daus[ order];
+    W_daughters[which][1] = daus[!order];
+  }
+
+  if (!Valid())
+    return;
+
+  // Count and store decay types: e, mu, tau, hadronic (0-3).
+  num_leptonic = 0;
+  for (int i = 0; i < 2; ++i) {
+    if (is_quark(W_daughters[i][0]))
+      die_if_not(is_quark(W_daughters[i][1]), "one W daughter is quark but other is not");
+    else {
+      die_if_not(is_lepton(W_daughters[i][0]) && is_lepton(W_daughters[i][1]), "one W daughter is lepton and other is not");
+      ++num_leptonic;
+    }
+
+    decay_type[i] = lepton_code(W_daughters[i][0]);
+  }
+
+  for (int j = 0; j < 2; ++j)
+    for (int k = 0; k < 2; ++k)
+      immediate_nus.push_back(dynamic_cast<const reco::Candidate*>(W_daughters[j][k]));
+}
+
+void MCInteractionMFV3j::SetFourVectors() {
+  for (int i = 0; i < 2; ++i) {
+    p4_lsps[i] = make_tlv(lsps[i]);
+    p4_stranges[i] = make_tlv(stranges[i]);
+    p4_bottoms[i] = make_tlv(bottoms[i]);
+    p4_tops[i] = make_tlv(tops[i]);
+    p4_Ws  [i] = make_tlv(Ws[i]);
+    p4_bottoms_from_tops[i] = make_tlv(bottoms_from_tops[i]);
+    for (int j = 0; j < 2; ++j)
+      p4_W_daughters[i][j] = make_tlv(W_daughters[i][j]);
+  }
+
+  p4_W_neutrinosum.SetXYZT(0,0,0,0);
+  for (int j = 0; j < 2; ++j)
+    if (decay_type[j] < 3)
+      p4_W_neutrinosum += p4_W_daughters[j][1];
+
+  MCInteraction::SetFourVectors(); // sets p4_dif_neutrinosum
+
+  p4_neutrinosum += p4_W_neutrinosum;
+  p4_missingsum += p4_W_neutrinosum;
+}
+
+void MCInteractionMFV3j::Print(std::ostream& out) {
+  if (!Valid()) {
+    out << "not valid\n";
+    return;
+  }
+  printf("num_leptons: %i\n", num_leptonic);
+  const char* decay_types[4] = {"e", "mu", "tau", "h"};
+  printf("decay type: Ws[0] -> %s,  Ws[1] -> %s\n", decay_types[decay_type[0]], decay_types[decay_type[1]]);
+  print_gen_and_daus(0,                      "header",                  *gen_particles);
+  print_gen_and_daus(lsps[0],                "lsps[0]",                 *gen_particles);
+  print_gen_and_daus(lsps[1],                "lsps[1]",                 *gen_particles);
+  print_gen_and_daus(stranges[0],            "stranges[0]",             *gen_particles);
+  print_gen_and_daus(stranges[1],            "stranges[1]",             *gen_particles);
+  print_gen_and_daus(bottoms[0],             "bottoms[0]",              *gen_particles);
+  print_gen_and_daus(bottoms[1],             "bottoms[1]",              *gen_particles);
+  print_gen_and_daus(tops[0],                "tops[0]",                 *gen_particles);
+  print_gen_and_daus(tops[1],                "tops[1]",                 *gen_particles);
+  print_gen_and_daus(Ws[0],                  "Ws[0]",                   *gen_particles);
+  print_gen_and_daus(Ws[1],                  "Ws[1]",                   *gen_particles);
+  print_gen_and_daus(bottoms_from_tops[0],   "bottoms_from_tops[0]",    *gen_particles);
+  print_gen_and_daus(bottoms_from_tops[1],   "bottoms_from_tops[1]",    *gen_particles);
+  print_gen_and_daus(W_daughters[0][0],      "Wplus daughter 0",        *gen_particles);
+  print_gen_and_daus(W_daughters[0][1],      "Wplus daughter 1",        *gen_particles);
+  print_gen_and_daus(W_daughters[1][0],      "Wminus daughter 0",       *gen_particles);
+  print_gen_and_daus(W_daughters[1][1],      "Wminus daughter 1",       *gen_particles);
+  MCInteraction::Print(out);
+}
+
