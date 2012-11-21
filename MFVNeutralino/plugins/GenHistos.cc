4127942228b052f8bb4be6eb4ad60d9874ab86a8
tucker <>
2012-11-21 22:05:29 +0000

    More histos: use MCInteractionMFV3j to get all the particles' properties.

--- MFVNeutralino/plugins/GenHistos.cc
+++ MFVNeutralino/plugins/GenHistos.cc
@@ -16,6 +16,9 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
+#include "JMTucker/Tools/interface/BasicKinematicHists.h"
+#include "JMTucker/Tools/interface/MCInteractionMFV3j.h"
+#include "JMTucker/Tools/interface/Utilities.h"
 
 class MFVNeutralinoGenHistos : public edm::EDAnalyzer {
  public:
@@ -26,110 +29,241 @@ class MFVNeutralinoGenHistos : public edm::EDAnalyzer {
   const edm::InputTag gen_src;
   const edm::InputTag gen_jet_src;
   const edm::InputTag gen_met_src;
-  
-  TH1F* h_num_ninos;
-  TH2F* h_vtx_2d;
-  TH1F* h_rho;
-  TH1F* h_ninobeta;
-  TH1F* h_ninobetagamma;
-  TH1F* h_max_dR;
-  TH1F* h_min_dR;
-  TH2F* h_max_dR_vs_ninobeta;
-  TH2F* h_min_dR_vs_ninobeta;
-  TH2F* h_max_dR_vs_ninobetagamma;
-  TH2F* h_min_dR_vs_ninobetagamma;
+  const int required_num_leptonic;
+  const std::vector<int> allowed_decay_types;
+  const bool print_info;
 
   edm::ESHandle<ParticleDataTable> pdt;
+
+  TH1F* NumLeptons;
+  TH2F* DecayType;
+  TH1F* TopDaughterIds[2];
+  TH2F* WDaughterIds[2];
+
+  BasicKinematicHistsFactory* bkh_factory;
+
+  BasicKinematicHists* Lsps[2];
+  BasicKinematicHists* Stranges[2];
+  BasicKinematicHists* Bottoms[2];
+  BasicKinematicHists* Tops[2];
+  BasicKinematicHists* Ws[2];
+  BasicKinematicHists* BottomsFromTops[2];
+  BasicKinematicHists* WDaughters[2][2];
+
+  TH2F* h_vtx_2d;
+  TH1F* h_rho;
+  TH1F* h_lspbeta;
+  TH1F* h_lspbetagamma;
+  TH1F* h_max_dR;
+  TH1F* h_min_dR;
+  TH2F* h_max_dR_vs_lspbeta;
+  TH2F* h_min_dR_vs_lspbeta;
+  TH2F* h_max_dR_vs_lspbetagamma;
+  TH2F* h_min_dR_vs_lspbetagamma;
 };
 
 MFVNeutralinoGenHistos::MFVNeutralinoGenHistos(const edm::ParameterSet& cfg)
   : gen_src(cfg.getParameter<edm::InputTag>("gen_src")),
     gen_jet_src(cfg.getParameter<edm::InputTag>("gen_jet_src")),
-    gen_met_src(cfg.getParameter<edm::InputTag>("gen_met_src"))
+    gen_met_src(cfg.getParameter<edm::InputTag>("gen_met_src")),
+    required_num_leptonic(cfg.getParameter<int>("required_num_leptonic")),
+    allowed_decay_types(cfg.getParameter<std::vector<int> >("allowed_decay_types")),
+    print_info(cfg.getParameter<bool>("print_info"))
 {
   edm::Service<TFileService> fs;
 
-  h_num_ninos = fs->make<TH1F>("h_num_ninos", "", 5, 0, 5);
+  NumLeptons = fs->make<TH1F>("NumLeptons", "", 3, 0, 3);
+  NumLeptons->SetTitle(";number of leptons from top decays;events");
+  DecayType = fs->make<TH2F>("DecayType", "", 4, 0, 4, 4, 0, 4);
+  DecayType->SetTitle(";W+ decay mode;W- decay mode");
+
+  TopDaughterIds[0] = fs->make<TH1F>("TopDaughterIds",    "top daughter ids",    15, 0, 15);
+  TopDaughterIds[1] = fs->make<TH1F>("TopbarDaughterIds", "topbar daughter ids", 15, 0, 15);
+  const char* TopDaughterIdLabels[] = { "W+", "W-", "b", "bbar", "g", "u", "ubar", "d", "dbar", "s", "sbar" , "c", "cbar", "gamma", "N/A" };
+  for (int i = 0; i < 2; ++i)
+    set_bin_labels(TopDaughterIds[i]->GetXaxis(), TopDaughterIdLabels);
+
+  WDaughterIds[0] = fs->make<TH2F>("WplusDaughterIds",  "W+ daughter ids", 25, 0, 25, 25, 0, 25);
+  WDaughterIds[1] = fs->make<TH2F>("WminusDaughterIds", "W- daughter ids", 25, 0, 25, 25, 0, 25);
+  const char* WDaughterIdLabels[] = { "d", "u", "s", "c", "b", "dbar", "ubar", "sbar", "cbar", "bbar", "e-", "nu_e", "mu-", "nu_mu", "tau-", "nu_tau", "e+", "nu_ebar", "mu+", "nu_mubar", "tau+", "nu_taubar", "W+", "W-", "N/A" };
+  for (int i = 0; i < 2; ++i) {
+    set_bin_labels(WDaughterIds[i]->GetXaxis(), WDaughterIdLabels);
+    set_bin_labels(WDaughterIds[i]->GetYaxis(), WDaughterIdLabels);
+  }
+
+  bkh_factory = new BasicKinematicHistsFactory(fs);
+
+  for (int i = 0; i < 2; ++i) {
+    Lsps[i] = bkh_factory->make(TString::Format("Lsps#%i", i), TString::Format("lsp #%i", i));
+    Lsps[i]->BookE (200, 0, 2000, "10");
+    Lsps[i]->BookP (200, 0, 2000, "10");
+    Lsps[i]->BookPz(200, 0, 2000, "10");
+    Lsps[i]->BookM (200, 0, 2000, "10");
+    Lsps[i]->BookRapEta(200, "0.1");
+    Lsps[i]->BookPhi(50, "0.125");
+
+    Stranges[i] = bkh_factory->make(TString::Format("Stranges#%i", i), TString::Format("strange #%i", i));
+    Stranges[i]->BookE (200, 0, 2000, "10");
+    Stranges[i]->BookP (200, 0, 2000, "10");
+    Stranges[i]->BookPz(200, 0, 2000, "10");
+    Stranges[i]->BookM (200, 0, 2000, "10");
+    Stranges[i]->BookRapEta(200, "0.1");
+    Stranges[i]->BookPhi(50, "0.125");
+    Stranges[i]->BookDxy(200, -50, 50, "0.5");
+    Stranges[i]->BookDz (200, -50, 50, "0.5");
+    Stranges[i]->BookQ();
+
+    Bottoms[i] = bkh_factory->make(TString::Format("Bottoms#%i", i), TString::Format("bottom #%i", i));
+    Bottoms[i]->BookE (200, 0, 2000, "10");
+    Bottoms[i]->BookP (200, 0, 2000, "10");
+    Bottoms[i]->BookPz(200, 0, 2000, "10");
+    Bottoms[i]->BookM (200, 0, 2000, "10");
+    Bottoms[i]->BookRapEta(200, "0.1");
+    Bottoms[i]->BookPhi(50, "0.125");
+    Bottoms[i]->BookDxy(200, -50, 50, "0.5");
+    Bottoms[i]->BookDz (200, -50, 50, "0.5");
+    Bottoms[i]->BookQ();
+    
+    Tops[i] = bkh_factory->make(TString::Format("Tops#%i", i), TString::Format("top #%i", i));
+    Tops[i]->BookE (200, 0, 2000, "10");
+    Tops[i]->BookP (200, 0, 2000, "10");
+    Tops[i]->BookPz(200, 0, 2000, "10");
+    Tops[i]->BookM (200, 0, 2000, "10");
+    Tops[i]->BookRapEta(200, "0.1");
+    Tops[i]->BookPhi(50, "0.125");
+    Tops[i]->BookDxy(200, -50, 50, "0.5");
+    Tops[i]->BookDz (200, -50, 50, "0.5");
+    Tops[i]->BookQ();
+    
+    Ws[i] = bkh_factory->make(TString::Format("Ws#%i", i), TString::Format("w #%i", i));
+    Ws[i]->BookE (200, 0, 2000, "10");
+    Ws[i]->BookP (200, 0, 2000, "10");
+    Ws[i]->BookPz(200, 0, 2000, "10");
+    Ws[i]->BookM (200, 0, 2000, "10");
+    Ws[i]->BookRapEta(200, "0.1");
+    Ws[i]->BookPhi(50, "0.125");
+    Ws[i]->BookDxy(200, -50, 50, "0.5");
+    Ws[i]->BookDz (200, -50, 50, "0.5");
+    Ws[i]->BookQ();
+
+    BottomsFromTops[i] = bkh_factory->make(TString::Format("BottomsFromTops#%i", i), TString::Format("bottom from top #%i", i));
+    BottomsFromTops[i]->BookE (200, 0, 2000, "10");
+    BottomsFromTops[i]->BookP (200, 0, 2000, "10");
+    BottomsFromTops[i]->BookPz(200, 0, 2000, "10");
+    BottomsFromTops[i]->BookM (200, 0, 2000, "10");
+    BottomsFromTops[i]->BookRapEta(200, "0.1");
+    BottomsFromTops[i]->BookPhi(50, "0.125");
+    BottomsFromTops[i]->BookDxy(200, -50, 50, "0.5");
+    BottomsFromTops[i]->BookDz (200, -50, 50, "0.5");
+    BottomsFromTops[i]->BookQ();
+
+    for (int j = 0; j < 2; ++j) {
+      WDaughters[i][j] = bkh_factory->make(TString::Format("WDaughters#%i#%i", i,j), TString::Format("W#%i daughter #%i", i, j));
+      WDaughters[i][j]->BookE (200, 0, 2000, "10");
+      WDaughters[i][j]->BookP (200, 0, 2000, "10");
+      WDaughters[i][j]->BookPz(200, 0, 2000, "10");
+      WDaughters[i][j]->BookM (200, 0, 2000, "10");
+      WDaughters[i][j]->BookRapEta(200, "0.1");
+      WDaughters[i][j]->BookPhi(50, "0.125");
+      WDaughters[i][j]->BookDxy(200, -50, 50, "0.5");
+      WDaughters[i][j]->BookDz (200, -50, 50, "0.5");
+      WDaughters[i][j]->BookQ();
+    }
+  }
+
   h_vtx_2d = fs->make<TH2F>("h_vtx_2d", "", 500, -1, 1, 500, -1, 1);
   h_rho = fs->make<TH1F>("h_rho", "", 100, 0, 2);
-  h_ninobeta = fs->make<TH1F>("h_ninobeta", "", 100, 0, 1);
-  h_ninobetagamma = fs->make<TH1F>("h_ninobetagamma", "", 100, 0, 10);
+  h_lspbeta = fs->make<TH1F>("h_lspbeta", "", 100, 0, 1);
+  h_lspbetagamma = fs->make<TH1F>("h_lspbetagamma", "", 100, 0, 10);
   h_max_dR = fs->make<TH1F>("h_max_dR", "", 100, 0, 5);
   h_min_dR = fs->make<TH1F>("h_min_dR", "", 100, 0, 5);
-  h_max_dR_vs_ninobeta = fs->make<TH2F>("h_max_dR_vs_ninobeta", "", 100, 0, 1, 100, 0, 5);
-  h_min_dR_vs_ninobeta = fs->make<TH2F>("h_min_dR_vs_ninobeta", "", 100, 0, 1, 100, 0, 5);
-  h_max_dR_vs_ninobetagamma = fs->make<TH2F>("h_max_dR_vs_ninobetagamma", "", 100, 0, 10, 100, 0, 5);
-  h_min_dR_vs_ninobetagamma = fs->make<TH2F>("h_min_dR_vs_ninobetagamma", "", 100, 0, 10, 100, 0, 5);
+  h_max_dR_vs_lspbeta = fs->make<TH2F>("h_max_dR_vs_lspbeta", "", 100, 0, 1, 100, 0, 5);
+  h_min_dR_vs_lspbeta = fs->make<TH2F>("h_min_dR_vs_lspbeta", "", 100, 0, 1, 100, 0, 5);
+  h_max_dR_vs_lspbetagamma = fs->make<TH2F>("h_max_dR_vs_lspbetagamma", "", 100, 0, 10, 100, 0, 5);
+  h_min_dR_vs_lspbetagamma = fs->make<TH2F>("h_min_dR_vs_lspbetagamma", "", 100, 0, 10, 100, 0, 5);
+}
+
+float mag(float x, float y) {
+  return sqrt(x*x + y*y);
 }
 
 void MFVNeutralinoGenHistos::analyze(const edm::Event& event, const edm::EventSetup& setup) {
   setup.getData(pdt);
 
-  edm::Handle<reco::GenParticleCollection> gens;
-  event.getByLabel(gen_src, gens);
+  edm::Handle<reco::GenParticleCollection> gen_particles;
+  event.getByLabel(gen_src, gen_particles);
   edm::Handle<reco::GenJetCollection> gen_jets;
   event.getByLabel(gen_jet_src, gen_jets);
   edm::Handle<reco::GenMETCollection> gen_mets;
   event.getByLabel(gen_met_src, gen_mets);
-  //const reco::GenMET& gen_met = gen_mets->at(0);
+  const reco::GenMET& gen_met = gen_mets->at(0);
+
+  MCInteractionMFV3j mci;
+  mci.Init(*gen_particles, *gen_jets, gen_met);
+  if (!mci.Valid()) {
+    edm::LogWarning("GenHistos") << "MCInteractionMFV3j invalid!";
+    return;
+  }
+
+  if (print_info)
+    mci.Print(std::cout);
+
+  if (required_num_leptonic >= 0 && mci.num_leptonic != required_num_leptonic) 
+    return;
+
+  if (allowed_decay_types.size())
+    for (int i = 0; i < 2; ++i)
+      if (std::find(allowed_decay_types.begin(), allowed_decay_types.end(), mci.decay_type[i]) == allowed_decay_types.end())
+	return;
+
+  NumLeptons->Fill(mci.num_leptonic);
+  DecayType->Fill(mci.decay_type[0], mci.decay_type[1]);
 
   edm::Handle<reco::BeamSpot> bs;
   event.getByLabel("offlineBeamSpot", bs);
-  printf("beamspot x,y: %f, %f\n", bs->x0(), bs->y0());
+  if (print_info) printf("beamspot x,y: %f, %f\n", bs->x0(), bs->y0());
 
   // Assume pythia line 2 (probably a gluon) has the "right"
   // production vertex. (The protons are just at 0,0,0.)
-  const reco::GenParticle& for_vtx = gens->at(2);
+  const reco::GenParticle& for_vtx = gen_particles->at(2);
   const float vx = for_vtx.vx();
   const float vy = for_vtx.vy();
-  printf("gluon x,y: %f, %f\n", vx, vy);
+  if (print_info) printf("gluon x,y: %f, %f\n", vx, vy);
 
-  int num_ninos = 0;
-  const int ndau = 3;
-  const int dau_id_order[ndau] = { 3, 5, 6 };
+  for (int i = 0; i < 2; ++i) {
+    // For debugging, record all the daughter ids for tops, stops, Ws.
+    for (size_t j = 0; j < mci.tops[i]->numberOfDaughters(); ++j)
+      fill_by_label(TopDaughterIds[i], pdt->particle(mci.tops[i]->daughter(j)->pdgId())->name());
+    fill_by_label(WDaughterIds[i], pdt->particle(mci.W_daughters[i][0]->pdgId())->name(), pdt->particle(mci.W_daughters[i][1]->pdgId())->name());
 
-  BOOST_FOREACH(const reco::GenParticle& gen, *gens) {
-    if (!(gen.pdgId() == 1000022 && gen.status() == 52 && gen.numberOfDaughters() == 3))
-      continue;
-    const reco::GenParticle& nino = gen;
+    Lsps[i]->Fill(mci.lsps[i]);
+    Stranges[i]->Fill(mci.stranges[i]);
+    Bottoms[i]->Fill(mci.bottoms[i]);
+    Tops[i]->Fill(mci.tops[i]);
 
-    ++num_ninos;
-    const double ninobeta  = nino.p()/nino.energy();
-    const double ninobetagamma = ninobeta/sqrt(1-ninobeta*ninobeta);
-    h_ninobeta->Fill(ninobeta);
-    h_ninobetagamma->Fill(ninobetagamma);
+    Stranges[i]->FillEx(mag(mci.stranges[i]->vx(), mci.stranges[i]->vy()), mci.stranges[i]->vz(), mci.stranges[i]->charge());
+    Bottoms [i]->FillEx(mag(mci.bottoms [i]->vx(), mci.bottoms [i]->vy()), mci.bottoms [i]->vz(), mci.bottoms [i]->charge());
+    Tops    [i]->FillEx(mag(mci.tops    [i]->vx(), mci.tops    [i]->vy()), mci.tops    [i]->vz(), mci.tops    [i]->charge());
+
+    Ws[i]->Fill(mci.Ws[i]);
+    if (mci.bottoms_from_tops[i])
+      BottomsFromTops[i]->Fill(mci.bottoms_from_tops[i]);
+    for (int j = 0; j < 2; ++j)
+      WDaughters[i][j]->Fill(mci.W_daughters[i][j]);
+
+
+    const reco::GenParticle& lsp = *mci.lsps[i];
+    const int ndau = 3;
+    const reco::GenParticle* daughters[ndau] = { mci.stranges[i], mci.bottoms[i], mci.tops[i] };
+
+    const double lspbeta  = lsp.p()/lsp.energy();
+    const double lspbetagamma = lspbeta/sqrt(1-lspbeta*lspbeta);
+    h_lspbeta->Fill(lspbeta);
+    h_lspbetagamma->Fill(lspbetagamma);
     
-    // Get the immediate daughters, in order by the ids specified in
-    // dau_id_order.
-    const reco::Candidate* daughters[ndau] = {0};
-    for (int i = 0; i < ndau; ++i) {
-      int id = nino.daughter(i)->pdgId();
-      for (int j = 0; j < ndau; ++j) {
-	if (abs(id) == dau_id_order[j])
-	  daughters[j] = nino.daughter(i);
-      }
-    }
-
-    // Make sure we found all three daughters, and then get the
-    // "final" state daughters (having status 22 or 23 depending on
-    // quark type).
-    for (int i = 0; i < ndau; ++i) {
-      const reco::Candidate* dau = daughters[i];
-      if (dau == 0)
-	throw cms::Exception("DaughterNotFound") << "did not find daughter for neutralino with id " << dau_id_order[i];
-      
-      while (daughters[i]->status() != 22 && daughters[i]->status() != 23) {
-	for (int j = 0, je = int(dau->numberOfDaughters()); j < je; ++j)
-	  if (dau->pdgId() == dau->daughter(j)->pdgId())
-	    daughters[i] = dau->daughter(j);
-	if (daughters[i] == dau)
-	  throw cms::Exception("FinalDaughterNotFound") << "for id " << dau_id_order[i] << ", final state (22 or 23) daughter descendant not found";
-      }
-    }
-
     // Fill some simple histos: 2D vertex location, and distance to
     // origin, and the min/max deltaR of the daughters (also versus
-    // nino boost).
+    // lsp boost).
     float dx = daughters[0]->vx() - vx;
     float dy = daughters[0]->vy() - vy;
     h_vtx_2d->Fill(dx, dy);
@@ -149,13 +283,11 @@ void MFVNeutralinoGenHistos::analyze(const edm::Event& event, const edm::EventSe
 
     h_min_dR->Fill(min_dR);
     h_max_dR->Fill(max_dR);
-    h_min_dR_vs_ninobeta->Fill(ninobeta, min_dR);
-    h_max_dR_vs_ninobeta->Fill(ninobeta, max_dR);
-    h_min_dR_vs_ninobetagamma->Fill(ninobetagamma, min_dR);
-    h_max_dR_vs_ninobetagamma->Fill(ninobetagamma, max_dR);
+    h_min_dR_vs_lspbeta->Fill(lspbeta, min_dR);
+    h_max_dR_vs_lspbeta->Fill(lspbeta, max_dR);
+    h_min_dR_vs_lspbetagamma->Fill(lspbetagamma, min_dR);
+    h_max_dR_vs_lspbetagamma->Fill(lspbetagamma, max_dR);
   }
-
-  h_num_ninos->Fill(num_ninos);
 }
 
 DEFINE_FWK_MODULE(MFVNeutralinoGenHistos);
--- MFVNeutralino/test/gen_histos.py
+++ MFVNeutralino/test/gen_histos.py
@@ -2,37 +2,36 @@ import os, sys, FWCore.ParameterSet.Config as cms
 
 process = cms.Process('MFVNeutralino')
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('/store/user/tucker/mfvneutralino_genfsimreco_tau1mm/mfvneutralino_genfsimreco_tau1mm//f0b5b0c98c357fc0015e0194f7aef803/fastsim_9_1_JND.root'))
+process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('file:/uscms/home/tucker/private/mfv_genfsimreco_535/src/JMTucker/MFVNeutralino/test/fastsim.root'))
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 process.TFileService = cms.Service('TFileService', fileName = cms.string('gen_histos.root'))
 process.load('FWCore.MessageLogger.MessageLogger_cfi')
-process.MessageLogger.cerr.FwkReport.reportEvery = 1
+process.MessageLogger.cerr.FwkReport.reportEvery = 100000
 
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-process.printList = cms.EDAnalyzer('ParticleListDrawer',
-                                   maxEventsToPrint = cms.untracked.int32(100),
-                                   src = cms.InputTag('genParticles'),
-                                   printOnlyHardInteraction = cms.untracked.bool(False),
-                                   useMessageLogger = cms.untracked.bool(False),
-                                   printVertex = cms.untracked.bool(True),
-                                   )
-
-process.printTree = cms.EDAnalyzer('ParticleTreeDrawer',
-                                   src = cms.InputTag('genParticles'),
-                                   printP4 = cms.untracked.bool(False),
-                                   printPtEtaPhi = cms.untracked.bool(True),
-                                   printVertex = cms.untracked.bool(False),
-                                   printStatus = cms.untracked.bool(True),
-                                   printIndex = cms.untracked.bool(True),
-                                   )
 
 process.GenHistos = cms.EDAnalyzer('MFVNeutralinoGenHistos',
                                    gen_src = cms.InputTag('genParticles'),
                                    gen_jet_src = cms.InputTag('ak5GenJets'),
                                    gen_met_src = cms.InputTag('genMetTrue'),
+                                   required_num_leptonic = cms.int32(-1),
+                                   allowed_decay_types = cms.vint32(),
+                                   print_info = cms.bool(False),
                                    )
 
-process.p = cms.Path(process.printList * process.GenHistos)
+process.p = cms.Path(process.GenHistos)
+
+if 'debug' in sys.argv:
+    process.printList = cms.EDAnalyzer('ParticleListDrawer',
+                                       maxEventsToPrint = cms.untracked.int32(100),
+                                       src = cms.InputTag('genParticles'),
+                                       printOnlyHardInteraction = cms.untracked.bool(False),
+                                       useMessageLogger = cms.untracked.bool(False),
+                                       printVertex = cms.untracked.bool(True),
+                                       )
+    process.GenHistos.print_info = True
+    process.p *= process.printList
+
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     if 'debug' in sys.argv:
