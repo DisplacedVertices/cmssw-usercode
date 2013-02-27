3700b49b1de020d3c35b8c2e3733b93ae57f8ed5
wsun <>
2013-02-27 19:01:31 +0000

    Added initial partons.

--- Tools/interface/MCInteractionMFV3j.h
+++ Tools/interface/MCInteractionMFV3j.h
@@ -12,6 +12,13 @@ struct MCInteractionMFV3j : public MCInteraction {
   const reco::GenParticle* bottoms_from_tops[2];
   const reco::GenParticle* W_daughters[2][2]; // first index is same as above, second is W daughters in order (d-type, u-type quark) or (charged lepton, neutrino)
 
+  const reco::GenParticle* stranges_init[2];
+  const reco::GenParticle* bottoms_init[2];
+  const reco::GenParticle* tops_init[2];
+  const reco::GenParticle* Ws_init[2];
+  const reco::GenParticle* bottoms_from_tops_init[2];
+  const reco::GenParticle* W_daughters_init[2][2]; // first index is same as above, second is W daughters in order (d-type, u-type quark) or (charged lepton, neutrino)
+
   int num_leptonic;
   int decay_type[2]; // for Wplus and Wminus decays (the array index) into e, mu, tau, hadronic (values 0-3)
 
