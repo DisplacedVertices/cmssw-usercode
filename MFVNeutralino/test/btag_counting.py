4f2d9a17e2e4ff2727863ae4ebc45b1e74cb0031
tucker <>
2013-04-03 14:25:25 +0000

    Trivial change: check new samples.

--- MFVNeutralino/test/btag_counting.py
+++ MFVNeutralino/test/btag_counting.py
@@ -2,7 +2,7 @@ import os, sys, FWCore.ParameterSet.Config as cms
 
 process = cms.Process('MFVNeutralino')
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('/store/user/tucker/mfvneutralino_genfsimreco_tau10um/sstoptuple_v3_mfvN3jtau10um/ffbc82b68f588f5f183a150670744b16/pat_1_1_6Sq.root'))
+process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('/store/user/tucker/mfv_gensimhlt_gluino_tau1000um_M0400/jtuple_v6_mfv_gluino_tau1000um_M0400/bd05e25367130164ec2dd1b6bce4cd3c/pat_1_1_cQg.root'))
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 process.TFileService = cms.Service('TFileService', fileName = cms.string('btag_counting.root'))
 process.load('FWCore.MessageLogger.MessageLogger_cfi')
@@ -40,14 +40,16 @@ total_number_of_events = -1
 events_per_job = 100000
 
 [USER]
-ui_working_dir = crab/crab_mfvnu_btag_counting_%(name)s
+ui_working_dir = crab/btag_counting/crab_mfv_btag_counting_%(name)s
 return_data = 1
 '''
 
+    os.system('mkdir -p crab/btag_counting')
+    
     testing = 'testing' in sys.argv
 
-    from JMTucker.Tools.Samples import background_samples, mfv_signal_samples
-    for sample in background_samples + mfv_signal_samples:
+    from JMTucker.Tools.Samples import mfv_signal_samples
+    for sample in mfv_signal_samples:
         open('crab.cfg', 'wt').write(crab_cfg % sample)
         if not testing:
             os.system('crab -create -submit')
