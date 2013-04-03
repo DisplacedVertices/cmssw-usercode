23e96f08b124c566742337ee3961f8c9571288c7
tucker <>
2013-04-03 14:28:42 +0000

    Loop over different discriminators/working points.

--- MFVNeutralino/test/reco_counting.py
+++ MFVNeutralino/test/reco_counting.py
@@ -2,14 +2,14 @@ import os, sys, FWCore.ParameterSet.Config as cms
 
 process = cms.Process('MFVNeutralino')
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('/store/user/tucker/mfvneutralino_genfsimreco_tau1mm/mfvneutralino_genfsimreco_tau1mm//f0b5b0c98c357fc0015e0194f7aef803/fastsim_14_1_cnp.root'))
+process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('/store/user/tucker/mfv_gensimhlt_gluino_tau9900um_M400/mfv_reco_gluino_tau9900um_M400/4b815091ea4b0e75a52a1ca758900a17/reco_1_1_cNq.root'))
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 process.TFileService = cms.Service('TFileService', fileName = cms.string('reco_counting.root'))
 process.load('FWCore.MessageLogger.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
-process.load("Configuration.StandardSequences.Services_cff")
-process.load('Configuration.StandardSequences.Geometry_cff')
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.Geometry.GeometryIdeal_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load("Configuration.StandardSequences.Reconstruction_cff")
@@ -17,15 +17,16 @@ process.GlobalTag.globaltag = 'START53_V13::All'
 
 debug = 'debug' in sys.argv
 
-process.btagCounting = cms.EDAnalyzer('MFVNeutralinoBTagCounting',
-                                      vertex_src = cms.InputTag('offlinePrimaryVertices'),
-                                      muon_src = cms.InputTag('muons'),
-                                      electron_src = cms.InputTag('gsfElectrons'),
-                                      btag_src = cms.InputTag('jetBProbabilityBJetTags'),
-                                      jet_pt_min = cms.double(30),
-                                      bdisc_min = cms.double(2.55),
-                                      verbose = cms.bool(debug),
-                                      )
+
+btag_srcs = [
+        ('CSV',   'combinedSecondaryVertexBJetTags',      (0.244, 0.679, 0.898)),
+        ('JP',    'jetProbabilityBJetTags',               (0.275, 0.545, 0.790)),
+        ('JBP',   'jetBProbabilityBJetTags',              (1.33, 2.55, 3.74)),
+        ('SSVHE', 'simpleSecondaryVertexHighEffBJetTags', (1.74, 3.05)),
+        ('SSVHP', 'simpleSecondaryVertexHighPurBJetTags', (2.,)),
+        ('TCHE',  'trackCountingHighEffBJetTags',         (1.7, 3.3, 10.2)),
+        ('TCHP',  'trackCountingHighPurBJetTags',         (1.19, 1.93, 3.41)),
+        ]
 
 from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets
 assert not hasattr(process, 'kt6PFJetsForIsolation')
@@ -37,8 +38,24 @@ for name in ['elPFIsoValuePU04PFIdPFIso', 'elPFIsoValuePU03PFIdPFIso', 'elPFIsoV
 from CommonTools.ParticleFlow.Tools.pfIsolation import setupPFElectronIso
 process.gsfEleIsoSequence = setupPFElectronIso(process, 'gsfElectrons')
 
-process.p = cms.Path((process.pfParticleSelectionSequence + process.gsfEleIsoSequence + process.kt6PFJetsForIsolation) * process.btagCounting)
-    
+pobj = (process.pfParticleSelectionSequence + process.gsfEleIsoSequence + process.kt6PFJetsForIsolation)
+
+for btag_name, bdisc_src, bdisc_mins in btag_srcs:
+    for bdisc_min in bdisc_mins:
+        obj = cms.EDAnalyzer('MFVNeutralinoBTagCounting',
+                             btag_src = cms.InputTag(bdisc_src),
+                             bdisc_min = cms.double(bdisc_min),
+                             jet_pt_min = cms.double(30),
+                             vertex_src = cms.InputTag('offlinePrimaryVertices'),
+                             muon_src = cms.InputTag('muons'),
+                             electron_src = cms.InputTag('gsfElectrons'),
+                             verbose = cms.bool(debug),
+                             )
+        name = btag_name + 'pt30bd' + str(bdisc_min).replace('.','p')
+        setattr(process, name, obj)
+        pobj *= obj
+process.p = cms.Path(pobj)
+
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     if 'debug' in sys.argv:
         raise RuntimeError('refusing to submit jobs in debug (verbose print out) mode')
@@ -62,9 +79,12 @@ return_data = 1
 
     testing = 'testing' in sys.argv
 
-    from JMTucker.Tools.Samples import mfv_signal_samples
-    for sample in mfv_signal_samples:
+    def submit(sample):
         open('crab.cfg', 'wt').write(crab_cfg % sample)
         if not testing:
             os.system('crab -create -submit')
             os.system('rm crab.cfg')
+
+    from JMTucker.Tools.Samples import mfv_signal_samples
+    for sample in mfv_signal_samples:
+        submit(sample)
