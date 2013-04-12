15c1f759e0856d1c085134929a205ba0a9fb6445
tucker <>
2013-04-12 17:18:45 +0000

    Make min_njets a parameter; drop pair-wise combination of vertices; make h_dist(2d) the distance to the beamspot, and h_gendist(2d) the distance to the closest gen vertex; center h_gen_vtx and h_rec_vtx plots on beamspot.

--- MFVNeutralino/plugins/VtxRecoPlay.cc
+++ MFVNeutralino/plugins/VtxRecoPlay.cc
@@ -34,6 +34,7 @@ class VtxRecoPlay : public edm::EDAnalyzer {
   const edm::InputTag vertex_src;
   const bool print_info;
   const bool is_mfv;
+  const int min_njets;
   TH1F* h_njets;
   TH2F* h_gen_vtx;
   TH1F* h_nrecvtx;
@@ -42,12 +43,9 @@ class VtxRecoPlay : public edm::EDAnalyzer {
   TH1F* h_recvtxchi2prob;
   TH1F* h_dist;
   TH1F* h_dist2d;
-  TH1F* h_pairvtxsuccess;
-  TH1F* h_pairvtxchi2;
-  TH1F* h_pairvtxchi2prob;
-  TH2F* h_pairvtx;
-  TH1F* h_pairvtxdist2d;
-  TH1F* h_pairvtxdist;
+  TH1F* h_dist2dfull;
+  TH1F* h_gendist;
+  TH1F* h_gendist2d;
 };
 
 VtxRecoPlay::VtxRecoPlay(const edm::ParameterSet& cfg)
@@ -55,7 +53,8 @@ VtxRecoPlay::VtxRecoPlay(const edm::ParameterSet& cfg)
     gen_src(cfg.getParameter<edm::InputTag>("gen_src")),
     vertex_src(cfg.getParameter<edm::InputTag>("vertex_src")),
     print_info(cfg.getParameter<bool>("print_info")),
-    is_mfv(cfg.getParameter<bool>("is_mfv"))
+    is_mfv(cfg.getParameter<bool>("is_mfv")),
+    min_njets(cfg.getParameter<int>("min_njets"))
 {
   edm::Service<TFileService> fs;
   
@@ -66,13 +65,10 @@ VtxRecoPlay::VtxRecoPlay(const edm::ParameterSet& cfg)
   h_recvtxchi2 = fs->make<TH1F>("h_recvtxchi2", "", 50,0,50);
   h_recvtxchi2prob = fs->make<TH1F>("h_recvtxchi2prob", "", 50,0,1);
   h_dist2d = fs->make<TH1F>("h_dist2d", "", 1000, 0, 5);
+  h_dist2dfull = fs->make<TH1F>("h_dist2dfull", "", 1000, 0, 5);
   h_dist = fs->make<TH1F>("h_dist", "", 1000, 0, 5);
-  h_pairvtxsuccess = fs->make<TH1F>("h_pairvtxsuccess", "", 2,0,2);
-  h_pairvtxchi2 = fs->make<TH1F>("h_pairvtxchi2", "", 50,0,50);
-  h_pairvtxchi2prob = fs->make<TH1F>("h_pairvtxchi2prob", "", 50,0,1);
-  h_pairvtx = fs->make<TH2F>("h_pairvtx", "", 500, -1, 1, 500, -1, 1);
-  h_pairvtxdist2d = fs->make<TH1F>("h_pairvtxdist2d", "", 1000, 0, 5);
-  h_pairvtxdist = fs->make<TH1F>("h_pairvtxdist", "", 1000, 0, 5);
+  h_gendist2d = fs->make<TH1F>("h_gendist2d", "", 1000, 0, 5);
+  h_gendist = fs->make<TH1F>("h_gendist", "", 1000, 0, 5);
 }
 
 namespace {
@@ -86,25 +82,30 @@ namespace {
 }
 
 void VtxRecoPlay::analyze(const edm::Event& event, const edm::EventSetup& setup) {
+  if (min_njets > 0) {
+    edm::Handle<reco::PFJetCollection> jets;
+    event.getByLabel("ak5PFJets", jets);
+
+    int njets = 0;
+    for (const reco::PFJet& jet : *jets) {
+      if (jet.pt() > 20 && 
+	  fabs(jet.eta()) < 2.5 && 
+	  jet.numberOfDaughters() > 1 &&
+	  jet.neutralHadronEnergyFraction() < 0.99 && 
+	  jet.neutralEmEnergyFraction() < 0.99 && 
+	  (fabs(jet.eta()) >= 2.4 || (jet.chargedEmEnergyFraction() < 0.99 && jet.chargedHadronEnergyFraction() > 0. && jet.chargedMultiplicity() > 0)))
+	njets += 1;
+    }
+    h_njets->Fill(njets);
+    if (njets < 6)
+      return;
+  }
+
+  edm::Handle<reco::BeamSpot> beamspot;
+  event.getByLabel("offlineBeamSpot", beamspot);
+
   edm::Handle<reco::VertexCollection> primary_vertices;
   event.getByLabel(primary_vertex_src, primary_vertices);
-  
-  edm::Handle<reco::PFJetCollection> jets;
-  event.getByLabel("ak5PFJets", jets);
-
-  int njets = 0;
-  for (const reco::PFJet& jet : *jets) {
-    if (jet.pt() > 20 && 
-	fabs(jet.eta()) < 2.5 && 
-	jet.numberOfDaughters() > 1 &&
-	jet.neutralHadronEnergyFraction() < 0.99 && 
-	jet.neutralEmEnergyFraction() < 0.99 && 
-	(fabs(jet.eta()) >= 2.4 || (jet.chargedEmEnergyFraction() < 0.99 && jet.chargedHadronEnergyFraction() > 0. && jet.chargedMultiplicity() > 0)))
-      njets += 1;
-  }
-  h_njets->Fill(njets);
-  if (njets < 6)
-    return;
 
   edm::Handle<reco::GenParticleCollection> gen_particles;
   event.getByLabel(gen_src, gen_particles);
@@ -120,7 +121,7 @@ void VtxRecoPlay::analyze(const edm::Event& event, const edm::EventSetup& setup)
 	mci.Print(std::cout);
       for (int i = 0; i < 2; ++i) {
 	const reco::GenParticle* daughter = mci.stranges[i];
-	h_gen_vtx->Fill(daughter->vx(), daughter->vy());
+	h_gen_vtx->Fill(daughter->vx() - beamspot->x0(), daughter->vy() - beamspot->y0());
 	gen_verts[i][0] = daughter->vx();
 	gen_verts[i][1] = daughter->vy();
 	gen_verts[i][2] = daughter->vz();
@@ -133,7 +134,7 @@ void VtxRecoPlay::analyze(const edm::Event& event, const edm::EventSetup& setup)
     if ((mci_valid = mci.Valid())) {
       for (int i = 0; i < 2; ++i) {
 	const reco::GenParticle* daughter = mci.tops[i];
-	h_gen_vtx->Fill(daughter->vx(), daughter->vy());
+	h_gen_vtx->Fill(daughter->vx() - beamspot->x0(), daughter->vy() - beamspot->y0());
 	gen_verts[i][0] = daughter->vx();
 	gen_verts[i][1] = daughter->vy();
 	gen_verts[i][2] = daughter->vz();
@@ -150,7 +151,7 @@ void VtxRecoPlay::analyze(const edm::Event& event, const edm::EventSetup& setup)
   const int nvtx = rec_vertices->size();
   h_nrecvtx->Fill(nvtx);
   for (const reco::Vertex& vtx : *rec_vertices) {
-    h_rec_vtx->Fill(vtx.x(), vtx.y());
+    h_rec_vtx->Fill(vtx.x() - beamspot->x0(), vtx.y() - beamspot->y0());
     h_recvtxchi2->Fill(vtx.normalizedChi2());
     h_recvtxchi2prob->Fill(TMath::Prob(vtx.chi2(), vtx.ndof()));
     float closest2d = 1e99;
@@ -163,48 +164,12 @@ void VtxRecoPlay::analyze(const edm::Event& event, const edm::EventSetup& setup)
     }
     assert(closest2d < 1e99); // obviously
     assert(closest   < 1e99);
-    h_dist2d->Fill(closest2d);
-    h_dist  ->Fill(closest);
+    h_gendist2d->Fill(closest2d);
+    h_gendist  ->Fill(closest);
+
+    h_dist2d->Fill(mag(vtx.x() - beamspot->x(vtx.z()), vtx.y() - beamspot->y(vtx.z())));
+    h_dist2dfull->Fill(mag(vtx.x() - beamspot->x0(), vtx.y() - beamspot->y0()));
   }
-
-  edm::ESHandle<TransientTrackBuilder> trackBuilder;
-  setup.get<TransientTrackRecord>().get("TransientTrackBuilder", trackBuilder);
-
-  edm::Handle<reco::TrackCollection> tracks;
-  event.getByLabel("generalTracks", tracks);
-
-  for (int ivtx = 0; ivtx < nvtx; ++ivtx) {
-    const reco::Vertex& vtxi = rec_vertices->at(ivtx);
-
-    std::vector<reco::TransientTrack> ttvi;
-    for (reco::Vertex::trackRef_iterator it = vtxi.tracks_begin(), ite = vtxi.tracks_end(); it != ite; ++it)
-      ttvi.push_back(trackBuilder->build(**it));
-
-    for (int jvtx = ivtx+1; jvtx < nvtx; ++jvtx) {
-      const reco::Vertex& vtxj = rec_vertices->at(jvtx);
-      
-      std::vector<reco::TransientTrack> ttvj(ttvi);
-      for (reco::Vertex::trackRef_iterator it = vtxj.tracks_begin(), ite = vtxj.tracks_end(); it != ite; ++it)
-	ttvj.push_back(trackBuilder->build(**it));
-
-      KalmanVertexFitter kvf(true);
-      CachingVertex<5> cv = kvf.vertex(ttvj);
-      TransientVertex tv = cv;
-      reco::Vertex vtx = tv;
-      
-      h_pairvtxsuccess->Fill(tv.isValid());
-      if (tv.isValid()) {
-	h_pairvtxchi2->Fill(vtx.normalizedChi2());
-	h_pairvtxchi2prob->Fill(TMath::Prob(vtx.chi2(), vtx.ndof()));
-	h_pairvtx->Fill(vtx.x(), vtx.y());
-	for (int igen = 0; igen < 2; ++igen) {
-	  h_pairvtxdist2d->Fill(mag(vtx.x() - gen_verts[igen][0], vtx.y() - gen_verts[igen][1]));
-	  h_pairvtxdist  ->Fill(mag(vtx.x() - gen_verts[igen][0], vtx.y() - gen_verts[igen][1], vtx.z() - gen_verts[igen][2]));
-	}
-      }
-    }
-  }
-
 }
 
 DEFINE_FWK_MODULE(VtxRecoPlay);
