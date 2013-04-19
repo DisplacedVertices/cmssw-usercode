e38a1e9c1ed8d4f194e072685b728f0f9b561940
tucker <>
2013-04-19 13:28:36 +0000

    Consistent 3d names; pair-wise vertex distance/'compatibility' plots.

--- MFVNeutralino/plugins/VtxRecoPlay.cc
+++ MFVNeutralino/plugins/VtxRecoPlay.cc
@@ -16,6 +16,8 @@
 #include "RecoVertex/KalmanVertexFit/interface/KalmanVertexFitter.h"
 #include "RecoVertex/VertexPrimitives/interface/CachingVertex.h"
 #include "RecoVertex/VertexPrimitives/interface/TransientVertex.h"
+#include "RecoVertex/VertexTools/interface/VertexDistanceXY.h"
+#include "RecoVertex/VertexTools/interface/VertexDistance3D.h"
 #include "TrackingTools/Records/interface/TransientTrackRecord.h"
 #include "TrackingTools/TransientTrack/interface/TransientTrack.h"
 #include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
@@ -41,11 +43,23 @@ class VtxRecoPlay : public edm::EDAnalyzer {
   TH2F* h_rec_vtx;
   TH1F* h_recvtxchi2;
   TH1F* h_recvtxchi2prob;
-  TH1F* h_dist;
   TH1F* h_dist2d;
   TH1F* h_dist2dfull;
-  TH1F* h_gendist;
+  TH1F* h_dist3d;
   TH1F* h_gendist2d;
+  TH1F* h_gendist3d;
+  TH1F* h_paircompat2d;
+  TH1F* h_paircompat3d;
+  TH1F* h_pairdistcompat2dfail;
+  TH1F* h_pairdistcompat3dfail;
+  TH1F* h_pairerrcompat2dfail;
+  TH1F* h_pairerrcompat3dfail;
+  TH1F* h_pairdist2d;
+  TH1F* h_pairdist3d;
+  TH1F* h_pairerr2d;
+  TH1F* h_pairerr3d;
+  TH1F* h_pairsig2d;
+  TH1F* h_pairsig3d;
 };
 
 VtxRecoPlay::VtxRecoPlay(const edm::ParameterSet& cfg)
@@ -58,17 +72,29 @@ VtxRecoPlay::VtxRecoPlay(const edm::ParameterSet& cfg)
 {
   edm::Service<TFileService> fs;
   
-  h_njets = fs->make<TH1F>("h_njets", "", 50, 0, 50);
-  h_gen_vtx = fs->make<TH2F>("h_gen_vtx", "", 500, -1, 1, 500, -1, 1);
-  h_nrecvtx = fs->make<TH1F>("h_nrecvtx", "", 100, 0, 100);
-  h_rec_vtx = fs->make<TH2F>("h_rec_vtx", "", 500, -1, 1, 500, -1, 1);
-  h_recvtxchi2 = fs->make<TH1F>("h_recvtxchi2", "", 50,0,50);
-  h_recvtxchi2prob = fs->make<TH1F>("h_recvtxchi2prob", "", 50,0,1);
-  h_dist2d = fs->make<TH1F>("h_dist2d", "", 1000, 0, 5);
-  h_dist2dfull = fs->make<TH1F>("h_dist2dfull", "", 1000, 0, 5);
-  h_dist = fs->make<TH1F>("h_dist", "", 1000, 0, 5);
-  h_gendist2d = fs->make<TH1F>("h_gendist2d", "", 1000, 0, 5);
-  h_gendist = fs->make<TH1F>("h_gendist", "", 1000, 0, 5);
+  h_njets                  = fs->make<TH1F>("h_njets",                  "",      50,    0,   50);
+  h_gen_vtx                = fs->make<TH2F>("h_gen_vtx",                "",     500,   -1,    1,    500,  -1,   1);
+  h_nrecvtx                = fs->make<TH1F>("h_nrecvtx",                "",     100,    0,  100);
+  h_rec_vtx                = fs->make<TH2F>("h_rec_vtx",                "",     500,   -1,    1,    500,  -1,   1);
+  h_recvtxchi2             = fs->make<TH1F>("h_recvtxchi2",             "",      50,    0,   50);
+  h_recvtxchi2prob         = fs->make<TH1F>("h_recvtxchi2prob",         "",      50,    0,    1);
+  h_dist2d                 = fs->make<TH1F>("h_dist2d",                 "",    1000,    0,    5);
+  h_dist2dfull             = fs->make<TH1F>("h_dist2dfull",             "",    1000,    0,    5);
+  h_dist3d                 = fs->make<TH1F>("h_dist3d",                 "",    1000,    0,    5);
+  h_gendist2d              = fs->make<TH1F>("h_gendist2d",              "",    1000,    0,    5);
+  h_gendist3d              = fs->make<TH1F>("h_gendist3d",              "",    1000,    0,    5);
+  h_paircompat2d           = fs->make<TH1F>("h_paircompat2d",           "",    1000,    0,    5);
+  h_paircompat3d           = fs->make<TH1F>("h_paircompat3d",           "",    1000,    0,    5);
+  h_pairdistcompat2dfail   = fs->make<TH1F>("h_pairdistcompat2dfail",   "",    1000,    0,    5);
+  h_pairdistcompat3dfail   = fs->make<TH1F>("h_pairdistcompat3dfail",   "",    1000,    0,    5);
+  h_pairerrcompat2dfail    = fs->make<TH1F>("h_pairerrcompat2dfail",    "",    1000,    0,    5);
+  h_pairerrcompat3dfail    = fs->make<TH1F>("h_pairerrcompat3dfail",    "",    1000,    0,    5);
+  h_pairdist2d             = fs->make<TH1F>("h_pairdist2d",             "",    1000,    0,    5);
+  h_pairdist3d             = fs->make<TH1F>("h_pairdist3d",             "",    1000,    0,    5);
+  h_pairerr2d              = fs->make<TH1F>("h_pairerr2d",              "",    1000,    0,    5);
+  h_pairerr3d              = fs->make<TH1F>("h_pairerr3d",              "",    1000,    0,    5);
+  h_pairsig2d              = fs->make<TH1F>("h_pairsig2d",              "",    1000,    0,    5);
+  h_pairsig3d              = fs->make<TH1F>("h_pairsig3d",              "",    1000,    0,    5);
 }
 
 namespace {
@@ -97,7 +123,7 @@ void VtxRecoPlay::analyze(const edm::Event& event, const edm::EventSetup& setup)
 	njets += 1;
     }
     h_njets->Fill(njets);
-    if (njets < 6)
+    if (njets < min_njets)
       return;
   }
 
@@ -155,20 +181,59 @@ void VtxRecoPlay::analyze(const edm::Event& event, const edm::EventSetup& setup)
     h_recvtxchi2->Fill(vtx.normalizedChi2());
     h_recvtxchi2prob->Fill(TMath::Prob(vtx.chi2(), vtx.ndof()));
     float closest2d = 1e99;
-    float closest   = 1e99;
+    float closest3d = 1e99;
     for (int i = 0; i < 2; ++i) {
       float dist2d = mag(vtx.x() - gen_verts[i][0], vtx.y() - gen_verts[i][1]);
-      float dist   = mag(vtx.x() - gen_verts[i][0], vtx.y() - gen_verts[i][1], vtx.z() - gen_verts[i][2]);
+      float dist3d = mag(vtx.x() - gen_verts[i][0], vtx.y() - gen_verts[i][1], vtx.z() - gen_verts[i][2]);
       if (dist2d < closest2d) closest2d = dist2d;
-      if (dist   < closest)   closest   = dist;
+      if (dist3d < closest3d) closest3d = dist3d;
     }
     assert(closest2d < 1e99); // obviously
-    assert(closest   < 1e99);
+    assert(closest3d < 1e99);
     h_gendist2d->Fill(closest2d);
-    h_gendist  ->Fill(closest);
+    h_gendist3d->Fill(closest3d);
 
-    h_dist2d->Fill(mag(vtx.x() - beamspot->x(vtx.z()), vtx.y() - beamspot->y(vtx.z())));
-    h_dist2dfull->Fill(mag(vtx.x() - beamspot->x0(), vtx.y() - beamspot->y0()));
+    h_dist2d    ->Fill(mag(vtx.x() - beamspot->x(vtx.z()), vtx.y() - beamspot->y(vtx.z())));
+    h_dist2dfull->Fill(mag(vtx.x() - beamspot->x0(),       vtx.y() - beamspot->y0()));
+  }
+
+  VertexDistance3D distcalc_3d;
+  VertexDistanceXY distcalc_2d;
+  for (int ivtx = 0; ivtx < nvtx - 1; ++ivtx) {
+    const reco::Vertex& vtx_i = rec_vertices->at(ivtx);
+    for (int jvtx = ivtx + 1; jvtx < nvtx; ++jvtx) {
+      const reco::Vertex& vtx_j = rec_vertices->at(jvtx);
+
+      Measurement1D dist_2d = distcalc_2d.distance(vtx_i, vtx_j);
+      Measurement1D dist_3d = distcalc_3d.distance(vtx_i, vtx_j);
+
+      h_pairdist2d->Fill(dist_2d.value());
+      h_pairerr2d ->Fill(dist_2d.error());
+      h_pairsig2d ->Fill(dist_2d.significance());
+
+      h_pairdist3d->Fill(dist_3d.value());
+      h_pairerr3d ->Fill(dist_3d.error());
+      h_pairsig3d ->Fill(dist_3d.significance());
+
+      try {
+	h_paircompat2d->Fill(distcalc_2d.compatibility(vtx_i, vtx_j));
+      }
+      catch (cms::Exception& e) {
+	if (e.category() != "VertexDistanceXY::matrix inversion problem")
+	  throw;
+	h_pairdistcompat2dfail->Fill(dist_2d.value());
+	h_pairerrcompat2dfail ->Fill(dist_2d.error());
+      }
+      try {
+	h_paircompat3d->Fill(distcalc_3d.compatibility(vtx_i, vtx_j));
+      }
+      catch (cms::Exception& e) {
+	if (e.category() != "VertexDistance3D::matrix inversion problem")
+	  throw;
+	h_pairdistcompat3dfail->Fill(dist_3d.value());
+	h_pairerrcompat3dfail ->Fill(dist_3d.error());
+      }
+    }
   }
 }
 
