5ed1ca141f3c57ced05f9904c0a7ccb4ac07a9fb
tucker <>
2013-05-09 18:37:45 +0000

    input_from_argv(): take filenames and number of events from argv.

--- Tools/python/BasicAnalyzer_cfg.py
+++ Tools/python/BasicAnalyzer_cfg.py
@@ -1,4 +1,4 @@
-import FWCore.ParameterSet.Config as cms
+import sys, FWCore.ParameterSet.Config as cms
 
 process = cms.Process('BasicAnalyzer')
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
@@ -12,3 +12,19 @@ def add_analyzer(name, **kwargs):
     obj = cms.EDAnalyzer(name, **kwargs)
     setattr(process, name, obj)
     setattr(process, 'p' + name, cms.Path(obj))
+
+def input_from_argv():
+    fns = []
+    nev = -1
+    for x in sys.argv:
+        if x.endswith('.root'):
+            fns.append(x)
+        else:
+            try:
+                x = int(x)
+                nev = x
+            except ValueError:
+                pass
+
+    process.maxEvents.input = nev
+    process.source.fileNames = fns
