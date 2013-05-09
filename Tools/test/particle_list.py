5011436210e14d852934d325d14f2a191c809036
tucker <>
2013-05-09 18:33:09 +0000

    Use BasicAnalyzer_cfg; use JMT version of ParticleListDrawer.

--- Tools/test/particle_list.py
+++ Tools/test/particle_list.py
@@ -1,20 +1,14 @@
-import os, sys, FWCore.ParameterSet.Config as cms
+import os, sys
+from JMTucker.Tools.BasicAnalyzer_cfg import cms, process, add_analyzer
 
-process = cms.Process('ParticleListDrawer')
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring(''))
-process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
-process.load('FWCore.MessageLogger.MessageLogger_cfi')
+del process.TFileService
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
-
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-process.ParticleListDrawer = cms.EDAnalyzer('ParticleListDrawer',
-                                            maxEventsToPrint = cms.untracked.int32(1000000),
-                                            src = cms.InputTag('genParticles'),
-                                            printOnlyHardInteraction = cms.untracked.bool(True),
-                                            useMessageLogger = cms.untracked.bool(False)
-                                            )
-process.p = cms.Path(process.ParticleListDrawer)
+
+add_analyzer('JMTParticleListDrawer',
+             src = cms.InputTag('genParticles'),
+             printVertex = cms.untracked.bool(True),
+             )
 
 fns = []
 nev = -1
