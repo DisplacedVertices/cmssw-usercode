6473ee69e155fccc05f4e965f61918cb4a067008
tucker <>
2013-05-23 20:09:19 +0000

    Merge in TrackVertexArbitrat(rat)ion helper class.

--- MFVNeutralino/plugins/TrackVertexArbitrator.cc
+++ MFVNeutralino/plugins/TrackVertexArbitrator.cc
@@ -18,37 +18,45 @@
 #include "TrackingTools/TransientTrack/interface/TransientTrack.h"
 #include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
 
-#include "JMTucker/MFVNeutralino/plugins/TrackVertexArbitratration.h"
-
 class MFVTrackVertexArbitrator : public edm::EDProducer {
 public:
   MFVTrackVertexArbitrator(const edm::ParameterSet&);
-  ~MFVTrackVertexArbitrator();
 
   virtual void produce(edm::Event&, const edm::EventSetup&);
 
 private:
   bool trackFilter(const reco::TrackRef&) const;
 
-  edm::InputTag primaryVertexCollection;
-  edm::InputTag secondaryVertexCollection;
-  edm::InputTag trackCollection;
-  edm::InputTag beamSpotCollection;
-  MFVTrackVertexArbitration* theArbitrator;
+  const edm::InputTag primaryVertexCollection;
+  const edm::InputTag secondaryVertexCollection;
+  const edm::InputTag trackCollection;
+  const edm::InputTag beamSpotCollection;
+  const double dRCut;
+  const double distCut;
+  const double sigCut;
+  const double dLenFraction;
+
+  VertexDistance3D vdist;
 };
 
 MFVTrackVertexArbitrator::MFVTrackVertexArbitrator(const edm::ParameterSet& cfg)
   : primaryVertexCollection  (cfg.getParameter<edm::InputTag>("primaryVertices")),
     secondaryVertexCollection(cfg.getParameter<edm::InputTag>("secondaryVertices")),
     trackCollection          (cfg.getParameter<edm::InputTag>("tracks")),
-    beamSpotCollection       (cfg.getParameter<edm::InputTag>("beamSpot"))
+    beamSpotCollection       (cfg.getParameter<edm::InputTag>("beamSpot")),
+    dRCut       (cfg.getParameter<double>("dRCut")),
+    distCut     (cfg.getParameter<double>("distCut")),
+    sigCut      (cfg.getParameter<double>("sigCut")),
+    dLenFraction(cfg.getParameter<double>("dLenFraction"))
 {
   produces<reco::VertexCollection>();
-  theArbitrator = new MFVTrackVertexArbitration(cfg);
 }
 
-MFVTrackVertexArbitrator::~MFVTrackVertexArbitrator() {
-  delete theArbitrator;
+bool MFVTrackVertexArbitrator::trackFilter(const reco::TrackRef& track) const {
+  return
+    track->hitPattern().trackerLayersWithMeasurement() >= 4 &&
+    track->hitPattern().numberOfValidPixelHits() >= 1 &&
+    track->pt() > 0.4;
 }
 
 void MFVTrackVertexArbitrator::produce(edm::Event& event, const edm::EventSetup& setup) {
@@ -58,7 +66,7 @@ void MFVTrackVertexArbitrator::produce(edm::Event& event, const edm::EventSetup&
   std::auto_ptr<reco::VertexCollection> recoVertices(new reco::VertexCollection);
 
   if (primaryVertices->size() > 0) {
-    const reco::Vertex &pv = primaryVertices->at(0);
+    const reco::Vertex& pv = primaryVertices->at(0);
 
     edm::Handle<reco::BeamSpot> beamSpot;
     event.getByLabel(beamSpotCollection, beamSpot);
@@ -72,13 +80,56 @@ void MFVTrackVertexArbitrator::produce(edm::Event& event, const edm::EventSetup&
     edm::ESHandle<TransientTrackBuilder> trackBuilder;
     setup.get<TransientTrackRecord>().get("TransientTrackBuilder", trackBuilder);
 
+    const double annealing_sigmacut = 3.0;
+    const double annealing_Tini     = 256.;
+    const double annealing_ratio    = 0.25;
+    AdaptiveVertexFitter theAdaptiveFitter(GeometricAnnealing(annealing_sigmacut, annealing_Tini, annealing_ratio),
+                                           DefaultLinearizationPointFinder(),
+                                           KalmanVertexUpdator<5>(),
+                                           KalmanVertexTrackCompatibilityEstimator<5>(),
+                                           KalmanVertexSmoother());
+
     reco::TrackRefVector selectedTracks;
-    for (size_t i = 0, ie = tracks->size(); i < ie; ++i)
-      selectedTracks.push_back(reco::TrackRef(tracks, i));
+    for (size_t i = 0, ie = tracks->size(); i < ie; ++i) {
+      reco::TrackRef tk(tracks, i);
+      if (trackFilter(tk))
+        selectedTracks.push_back(tk);
+    }
     
-    reco::VertexCollection theRecoVertices = theArbitrator->trackVertexArbitrator(beamSpot, pv, trackBuilder, selectedTracks, *secondaryVertices);
-    for (const reco::Vertex& v : theRecoVertices)
-      recoVertices->push_back(v);
+    for (std::vector<reco::Vertex>::const_iterator sv = secondaryVertices->begin(); sv != secondaryVertices->end(); ++sv) {
+      GlobalPoint ppv(pv .position().x(), pv .position().y(), pv .position().z());
+      GlobalPoint ssv(sv->position().x(), sv->position().y(), sv->position().z());
+      GlobalVector flightDir = ssv - ppv;
+
+      Measurement1D dlen = vdist.distance(pv, *sv);
+      std::vector<reco::TransientTrack> selTracks;
+
+      for (unsigned int itrack = 0; itrack < selectedTracks.size(); itrack++) {
+        reco::TrackRef tkref = selectedTracks[itrack];
+
+        reco::TransientTrack tt = trackBuilder->build(tkref);
+        tt.setBeamSpot(*beamSpot);
+        float w = sv->trackWeight(tkref);
+        std::pair<bool,Measurement1D> ipv  = IPTools::absoluteImpactParameter3D(tt, pv);
+        std::pair<bool,Measurement1D> isv  = IPTools::absoluteImpactParameter3D(tt, *sv);
+        //        std::pair<bool,Measurement1D> itpv = IPTools::absoluteTransverseImpactParameter(tt, pv);
+        float dR = reco::deltaR(flightDir, tt.track());
+
+        if (w > 0 || (isv.second.significance() < sigCut && isv.second.value() < distCut && isv.second.value() < dlen.value() * dLenFraction)) {
+          if (isv.second.value() < ipv.second.value() && isv.second.value() < distCut && isv.second.value() < dlen.value()*dLenFraction && dR < dRCut)
+            selTracks.push_back(tt);
+          // add also the tracks used in previous fitting that are still closer to Sv than Pv 
+          else if (w > 0.5 && isv.second.value() <= ipv.second.value() && dR < dRCut)
+            selTracks.push_back(tt);
+        }
+      }
+
+      if (selTracks.size() >= 2) { 
+        TransientVertex singleFitVertex = theAdaptiveFitter.vertex(selTracks,ssv);
+        if (singleFitVertex.isValid())
+          recoVertices->push_back(reco::Vertex(singleFitVertex));
+      } 
+    }
   }	
 
   event.put(recoVertices);
