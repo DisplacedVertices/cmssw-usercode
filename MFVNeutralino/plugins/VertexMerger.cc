16c8f7a468f46d8e02565bf67c92a161a4aac194
tucker <>
2013-05-23 20:45:52 +0000

    Style.

--- MFVNeutralino/plugins/VertexMerger.cc
+++ MFVNeutralino/plugins/VertexMerger.cc
@@ -1,105 +1,80 @@
-#include <memory>
-#include <set>
-
-#include "FWCore/Framework/interface/EDProducer.h"
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Utilities/interface/InputTag.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include "DataFormats/Common/interface/Handle.h"
 #include "DataFormats/TrackReco/interface/Track.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoVertex/VertexTools/interface/VertexDistance3D.h"
 
-class VertexMerger : public edm::EDProducer {
-    public:
-	VertexMerger(const edm::ParameterSet &params);
+class MFVVertexMerger : public edm::EDProducer {
+public:
+  MFVVertexMerger(const edm::ParameterSet&);
 
-	virtual void produce(edm::Event &event, const edm::EventSetup &es);
+  virtual void produce(edm::Event&, const edm::EventSetup&);
 
-    private:
-	bool trackFilter(const reco::TrackRef &track) const;
+private:
+  double computeSharedTracks(const reco::Vertex&, const reco::Vertex&) const;
 
-//	edm::InputTag				primaryVertexCollection;
-	edm::InputTag				secondaryVertexCollection;
-	double					maxFraction;
-	double					minSignificance;
+  const edm::InputTag vertex_src;
+  const double max_frac;
+  const double min_sig;
+
+  VertexDistance3D dist;
 };
 
-VertexMerger::VertexMerger(const edm::ParameterSet &params) :
-//	primaryVertexCollection(params.getParameter<edm::InputTag>("primaryVertices")),
-	secondaryVertexCollection(params.getParameter<edm::InputTag>("secondaryVertices")),
-	maxFraction(params.getParameter<double>("maxFraction")),
-	minSignificance(params.getParameter<double>("minSignificance"))
+MFVVertexMerger::MFVVertexMerger(const edm::ParameterSet& params)
+  : vertex_src(params.getParameter<edm::InputTag>("vertex_src")),
+    max_frac(params.getParameter<double>("max_frac")),
+    min_sig(params.getParameter<double>("min_sig"))
 {
-	produces<reco::VertexCollection>();
+  produces<reco::VertexCollection>();
 }
 
-static double computeSharedTracks(const reco::Vertex &pv,
-                                        const reco::Vertex &sv)
-{
-	std::set<reco::TrackRef> pvTracks;
-	for(std::vector<reco::TrackBaseRef>::const_iterator iter = pv.tracks_begin();
-	    iter != pv.tracks_end(); iter++) {
-		if (pv.trackWeight(*iter) >= 0.5)
-			pvTracks.insert(iter->castTo<reco::TrackRef>());
-	}
+double MFVVertexMerger::computeSharedTracks(const reco::Vertex& sv, const reco::Vertex& sv2) const {
+  std::set<reco::TrackRef> sv_tracks;
 
-	unsigned int count = 0, total = 0;
-	for(std::vector<reco::TrackBaseRef>::const_iterator iter = sv.tracks_begin();
-	    iter != sv.tracks_end(); iter++) {
-		if (sv.trackWeight(*iter) >= 0.5) {
-			total++;
-			count += pvTracks.count(iter->castTo<reco::TrackRef>());
-		}
-	}
+  for (auto it = sv.tracks_begin(); it != sv.tracks_end(); ++it)
+    if (sv.trackWeight(*it) >= 0.5)
+      sv_tracks.insert(it->castTo<reco::TrackRef>());
 
-	return (double)count / (double)total;
+  unsigned count = 0, total = 0;
+  for (auto it = sv2.tracks_begin(), ite = sv2.tracks_end(); it != ite; ++it) {
+    if (sv2.trackWeight(*it) >= 0.5) {
+      ++total;
+      count += sv_tracks.count(it->castTo<reco::TrackRef>());
+    }
+  }
+
+  return double(count)/total;
 }
 
-void VertexMerger::produce(edm::Event &event, const edm::EventSetup &es)
-{
-	using namespace reco;
+void MFVVertexMerger::produce(edm::Event& event, const edm::EventSetup& setup) {
+  edm::Handle<reco::VertexCollection> vertices;
+  event.getByLabel(vertex_src, vertices);
 
-	edm::Handle<VertexCollection> secondaryVertices;
-	event.getByLabel(secondaryVertexCollection, secondaryVertices);
+  std::auto_ptr<reco::VertexCollection> new_vertices(new reco::VertexCollection);
+  for (const reco::Vertex& sv : *vertices)
+    new_vertices->push_back(sv);
 
-        VertexDistance3D dist;
-	std::auto_ptr<VertexCollection> recoVertices(new VertexCollection);
-	for(std::vector<reco::Vertex>::const_iterator sv = secondaryVertices->begin();
-	    sv != secondaryVertices->end(); ++sv) {
-          recoVertices->push_back(*sv);
-        }
-       for(std::vector<reco::Vertex>::iterator sv = recoVertices->begin();
-	    sv != recoVertices->end(); ++sv) {
+  for (std::vector<reco::Vertex>::iterator sv = new_vertices->begin(); sv != new_vertices->end(); ++sv) {
+    bool shared = false;
+    for (std::vector<reco::Vertex>::iterator sv2 = new_vertices->begin(); sv2 != new_vertices->end(); ++sv2) {
+      if (sv == sv2 || dist.distance(*sv, *sv2).significance() > min_sig)
+        continue;
 
-        bool shared=false;
-       for(std::vector<reco::Vertex>::iterator sv2 = recoVertices->begin();
-	    sv2 != recoVertices->end(); ++sv2) {
-                  double fr=computeSharedTracks(*sv2, *sv);
-        //        std::cout << sv2-recoVertices->begin() << " vs " << sv-recoVertices->begin() << " : " << fr << " "  <<  computeSharedTracks(*sv, *sv2) << " sig " << dist.distance(*sv,*sv2).significance() << std::endl;
-          //      std::cout << (fr > maxFraction) << " && " << (dist.distance(*sv,*sv2).significance() < 2)  <<  " && " <<  (sv-sv2!=0)  << " && " <<  (fr >= computeSharedTracks(*sv2, *sv))  << std::endl;
-		if (fr > maxFraction && dist.distance(*sv,*sv2).significance() < minSignificance && sv-sv2!=0 
-                    && fr >= computeSharedTracks(*sv, *sv2) )
-		  {
-                      shared=true; 
-                     // std::cout << "shared " << sv-recoVertices->begin() << " and "  << sv2-recoVertices->begin() << " fractions: " << fr << " , "  << computeSharedTracks(*sv2, *sv) << " sig: " <<  dist.distance(*sv,*sv2).significance() <<  std::endl;
-         
-                  }
-                 
+      double fr = computeSharedTracks(*sv2, *sv);
+      if (fr > max_frac && fr >= computeSharedTracks(*sv, *sv2)) {
+        shared = true;
+        break;
+      }
+    }
 
-	}
-        if(shared) { sv=recoVertices->erase(sv)-1; }
-     //    std::cout << "it = " <<  sv-recoVertices->begin() << " new size is: " << recoVertices->size() <<   std::endl;
-       }
+    if (shared)
+      sv = new_vertices->erase(sv) - 1;
+  }
 
-	event.put(recoVertices);
-
-
-
+  event.put(new_vertices);
 }
 
-DEFINE_FWK_MODULE(VertexMerger);
+DEFINE_FWK_MODULE(MFVVertexMerger);
