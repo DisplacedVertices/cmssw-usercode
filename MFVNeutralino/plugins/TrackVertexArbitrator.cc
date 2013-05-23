91cf1da714c9980186ac797e83232972dc47d262
tucker <>
2013-05-23 00:40:51 +0000

    Style.

--- MFVNeutralino/plugins/TrackVertexArbitrator.cc
+++ MFVNeutralino/plugins/TrackVertexArbitrator.cc
@@ -1,123 +1,87 @@
-#include <memory>
-#include <set>
-
-
-#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
-#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
-#include "TrackingTools/Records/interface/TransientTrackRecord.h"
-#include "TrackingTools/IPTools/interface/IPTools.h"
-
-
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+#include "DataFormats/Math/interface/deltaR.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/Utilities/interface/InputTag.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include "DataFormats/Common/interface/Handle.h"
-#include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/VertexReco/interface/Vertex.h"
-#include "DataFormats/VertexReco/interface/VertexFwd.h"
-#include "RecoVertex/VertexTools/interface/VertexDistance3D.h"
-
-
-#include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/VertexReco/interface/Vertex.h"
-#include "DataFormats/VertexReco/interface/VertexFwd.h"
-#include "DataFormats/BeamSpot/interface/BeamSpot.h"
-
 #include "RecoVertex/AdaptiveVertexFit/interface/AdaptiveVertexFitter.h"
+#include "RecoVertex/ConfigurableVertexReco/interface/ConfigurableVertexReconstructor.h"
 #include "RecoVertex/KalmanVertexFit/interface/KalmanVertexUpdator.h"
 #include "RecoVertex/KalmanVertexFit/interface/KalmanVertexTrackCompatibilityEstimator.h"
 #include "RecoVertex/KalmanVertexFit/interface/KalmanVertexSmoother.h"
-#include "DataFormats/Math/interface/deltaR.h"
+#include "RecoVertex/VertexTools/interface/VertexDistance3D.h"
+#include "TrackingTools/IPTools/interface/IPTools.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
 
-
-#include "RecoVertex/ConfigurableVertexReco/interface/ConfigurableVertexReconstructor.h"
-#include "RecoVertex/AdaptiveVertexFinder/interface/TrackVertexArbitratration.h"
-
-
-
-//#define VTXDEBUG
+#include "JMTucker/MFVNeutralino/plugins/TrackVertexArbitratration.h"
 
 class MFVTrackVertexArbitrator : public edm::EDProducer {
-    public:
-	MFVTrackVertexArbitrator(const edm::ParameterSet &params);
+public:
+  MFVTrackVertexArbitrator(const edm::ParameterSet&);
+  ~MFVTrackVertexArbitrator();
 
+  virtual void produce(edm::Event&, const edm::EventSetup&);
 
-	virtual void produce(edm::Event &event, const edm::EventSetup &es);
+private:
+  bool trackFilter(const reco::TrackRef&) const;
 
-    private:
-	bool trackFilter(const reco::TrackRef &track) const;
-
-	edm::InputTag				primaryVertexCollection;
-	edm::InputTag				secondaryVertexCollection;
-	edm::InputTag				trackCollection;
-        edm::InputTag                           beamSpotCollection;
-	TrackVertexArbitration * theArbitrator;
+  edm::InputTag primaryVertexCollection;
+  edm::InputTag secondaryVertexCollection;
+  edm::InputTag trackCollection;
+  edm::InputTag beamSpotCollection;
+  MFVTrackVertexArbitration* theArbitrator;
 };
 
-MFVTrackVertexArbitrator::MFVTrackVertexArbitrator(const edm::ParameterSet &params) :
-	primaryVertexCollection      (params.getParameter<edm::InputTag>("primaryVertices")),
-	secondaryVertexCollection    (params.getParameter<edm::InputTag>("secondaryVertices")),
-	trackCollection              (params.getParameter<edm::InputTag>("tracks")),
-        beamSpotCollection           (params.getParameter<edm::InputTag>("beamSpot"))
+MFVTrackVertexArbitrator::MFVTrackVertexArbitrator(const edm::ParameterSet& cfg)
+  : primaryVertexCollection  (cfg.getParameter<edm::InputTag>("primaryVertices")),
+    secondaryVertexCollection(cfg.getParameter<edm::InputTag>("secondaryVertices")),
+    trackCollection          (cfg.getParameter<edm::InputTag>("tracks")),
+    beamSpotCollection       (cfg.getParameter<edm::InputTag>("beamSpot"))
 {
-	produces<reco::VertexCollection>();
-	theArbitrator = new TrackVertexArbitration(params);
+  produces<reco::VertexCollection>();
+  theArbitrator = new MFVTrackVertexArbitration(cfg);
 }
 
+MFVTrackVertexArbitrator::~MFVTrackVertexArbitrator() {
+  delete theArbitrator;
+}
 
-void MFVTrackVertexArbitrator::produce(edm::Event &event, const edm::EventSetup &es)
-{
-	using namespace reco;
+void MFVTrackVertexArbitrator::produce(edm::Event& event, const edm::EventSetup& setup) {
+  edm::Handle<reco::VertexCollection> primaryVertices;
+  event.getByLabel(primaryVertexCollection, primaryVertices);
 
-	edm::Handle<VertexCollection> secondaryVertices;
-	event.getByLabel(secondaryVertexCollection, secondaryVertices);
-        VertexCollection theSecVertexColl = *(secondaryVertices.product());
+  std::auto_ptr<reco::VertexCollection> recoVertices(new reco::VertexCollection);
 
-        edm::Handle<VertexCollection> primaryVertices;
-        event.getByLabel(primaryVertexCollection, primaryVertices);
+  if (primaryVertices->size() > 0) {
+    const reco::Vertex &pv = primaryVertices->at(0);
 
-	std::auto_ptr<VertexCollection> recoVertices(new VertexCollection);
-        if(primaryVertices->size()!=0){ 
-        const reco::Vertex &pv = (*primaryVertices)[0];
+    edm::Handle<reco::BeamSpot> beamSpot;
+    event.getByLabel(beamSpotCollection, beamSpot);
+
+    edm::Handle<reco::TrackCollection> tracks;
+    event.getByLabel(trackCollection, tracks);
+
+    edm::Handle<reco::VertexCollection> secondaryVertices;
+    event.getByLabel(secondaryVertexCollection, secondaryVertices);
     
-        edm::Handle<TrackCollection> tracks;
-        event.getByLabel(trackCollection, tracks);
+    edm::ESHandle<TransientTrackBuilder> trackBuilder;
+    setup.get<TransientTrackRecord>().get("TransientTrackBuilder", trackBuilder);
 
-        edm::ESHandle<TransientTrackBuilder> trackBuilder;
-        es.get<TransientTrackRecord>().get("TransientTrackBuilder",
-                                           trackBuilder);
+    reco::TrackRefVector selectedTracks;
+    for (size_t i = 0, ie = tracks->size(); i < ie; ++i)
+      selectedTracks.push_back(reco::TrackRef(tracks, i));
+    
+    reco::VertexCollection theRecoVertices = theArbitrator->trackVertexArbitrator(beamSpot, pv, trackBuilder, selectedTracks, *secondaryVertices);
+    for (const reco::Vertex& v : theRecoVertices)
+      recoVertices->push_back(v);
+  }	
 
-        edm::Handle<BeamSpot> beamSpot;
-        event.getByLabel(beamSpotCollection, beamSpot);
-
-        
-	edm::RefVector< TrackCollection >  selectedTracks;
-	for(TrackCollection::const_iterator track = tracks->begin();
-	    track != tracks->end(); ++track) {
-		TrackRef ref(tracks, track - tracks->begin());
-		selectedTracks.push_back(ref);
-	   
-	}
-	
-	
-        const edm::RefVector< TrackCollection > tracksForArbitration= selectedTracks;
-	reco::VertexCollection  theRecoVertices = theArbitrator->trackVertexArbitrator(beamSpot, pv, trackBuilder, tracksForArbitration,
-	theSecVertexColl);
-	
-        for(unsigned int ivtx=0; ivtx < theRecoVertices.size(); ivtx++){
-         recoVertices->push_back(theRecoVertices[ivtx]);
-        }
-
-        }	
-	event.put(recoVertices);
-	
-	
-	
+  event.put(recoVertices);
 }
 
 DEFINE_FWK_MODULE(MFVTrackVertexArbitrator);
