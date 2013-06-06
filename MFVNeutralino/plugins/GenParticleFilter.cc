d40ce3737c12338c1f069826d571025b449401e9
tucker <>
2013-06-06 16:03:42 +0000

    Add print_info, required_num_leptonic, allowed_decay_types, min_lepton_pt, max_lepton_eta.

--- MFVNeutralino/plugins/GenParticleFilter.cc
+++ MFVNeutralino/plugins/GenParticleFilter.cc
@@ -14,16 +14,30 @@ private:
   virtual bool filter(edm::Event&, const edm::EventSetup&);
 
   const edm::InputTag gen_src;
+  const bool print_info;
   const bool cut_invalid;
+  const int required_num_leptonic;
+  const std::vector<int> allowed_decay_types;
+  const double min_lepton_pt;
+  const double max_lepton_eta;
   const double min_rho0;
   const double max_rho0;
   const double min_rho1;
   const double max_rho1;
+
+  bool cut_lepton(const reco::Candidate* lep) const {
+    return lep->pt() < min_lepton_pt || fabs(lep->eta()) > max_lepton_eta;
+  }
 };
 
 MFVGenParticleFilter::MFVGenParticleFilter(const edm::ParameterSet& cfg) 
   : gen_src(cfg.getParameter<edm::InputTag>("gen_src")),
+    print_info(cfg.getParameter<bool>("print_info")),
     cut_invalid(cfg.getParameter<bool>("cut_invalid")),
+    required_num_leptonic(cfg.getParameter<int>("required_num_leptonic")),
+    allowed_decay_types(cfg.getParameter<std::vector<int> >("allowed_decay_types")),
+    min_lepton_pt(cfg.getParameter<double>("min_lepton_pt")),
+    max_lepton_eta(cfg.getParameter<double>("max_lepton_eta")),
     min_rho0(cfg.getParameter<double>("min_rho0")),
     max_rho0(cfg.getParameter<double>("max_rho0")),
     min_rho1(cfg.getParameter<double>("min_rho1")),
@@ -45,8 +59,31 @@ bool MFVGenParticleFilter::filter(edm::Event& event, const edm::EventSetup&) {
   MCInteractionMFV3j mci;
   mci.Init(*gen_particles);
 
-  if (!mci.Valid())
+  if (print_info)
+    mci.Print(std::cout);
+
+  if (!mci.Valid()) {
+    edm::LogWarning("GenHistos") << "MCInteractionMFV3j invalid!";
     return !cut_invalid;
+  }
+
+  if (required_num_leptonic >= 0 && mci.num_leptonic != required_num_leptonic) 
+    return false;
+
+  if (mci.num_leptonic == 1) {
+    if (cut_lepton(mci.W_daughters[mci.which_is_lepton][0]))
+      return false;
+  }
+  else if (mci.num_leptonic == 2) {
+    for (int i = 0; i < 2; ++i)
+      if (cut_lepton(mci.W_daughters[i][0]))
+        return false;
+  }
+
+  if (allowed_decay_types.size())
+    for (int i = 0; i < 2; ++i)
+      if (std::find(allowed_decay_types.begin(), allowed_decay_types.end(), mci.decay_type[i]) == allowed_decay_types.end())
+        return false;
 
   const double rho0 = mag(mci.stranges[0]->vx() - mci.lsps[0]->vx(), mci.stranges[0]->vy() - mci.lsps[0]->vy());
   const double rho1 = mag(mci.stranges[1]->vx() - mci.lsps[1]->vx(), mci.stranges[1]->vy() - mci.lsps[1]->vy());
--- MFVNeutralino/python/GenParticleFilter_cfi.py
+++ MFVNeutralino/python/GenParticleFilter_cfi.py
@@ -2,11 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 mfvGenParticleFilter = cms.EDFilter('MFVGenParticleFilter',
                                     gen_src = cms.InputTag('genParticles'),
+                                    print_info = cms.bool(False),
                                     cut_invalid = cms.bool(True),
+                                    required_num_leptonic = cms.int32(-1),
+                                    allowed_decay_types = cms.vint32(),
+                                    min_lepton_pt = cms.double(0),
+                                    max_lepton_eta = cms.double(1e99),
                                     min_rho0 = cms.double(-1),
                                     max_rho0 = cms.double(-1),
                                     min_rho1 = cms.double(-1),
                                     max_rho1 = cms.double(-1),
                                     )
-
-                                    
