3b9ad269b10f9d251877127dce3aaae0648b2bbd
tucker <>
2013-06-10 17:00:14 +0000

    Move import of ROOT module to after optparse so that --help doesn't get clobbered; this requires --color defaults to be numeric instead of the nice e.g. 'ROOT.kRed'; add --skip option.

--- Tools/scripts/comparehists.py
+++ Tools/scripts/comparehists.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import optparse, sys
-from JMTucker.Tools.ROOTTools import *
 
 parser = optparse.OptionParser(usage='%prog [options] file1.root file2.root dir_path plot_path')
 parser.add_option('--per-page', type=int, default=-1,
@@ -12,9 +11,9 @@ parser.add_option('--nice1', default='file1',
                   help='Nice name for file #1 (default is %default).')
 parser.add_option('--nice2', default='file2',
                   help='Nice name for file #2 (default is %default).')
-parser.add_option('--color1', default='ROOT.kRed',
+parser.add_option('--color1', default='632',
                   help='Color #1: may be a python snippet, e.g. the default %default.')
-parser.add_option('--color2', default='ROOT.kBlue',
+parser.add_option('--color2', default='600',
                   help='Color #1: may be a python snippet, e.g. the default %default.')
 parser.add_option('--no-stats', default='False',
                   help='Snippet for no_stats lambda, which takes name, hist1, hist2 as args (default is %default).')
@@ -22,13 +21,16 @@ parser.add_option('--apply-commands', default='None',
                   help='Snippet for apply_commands lambda, which takes name, hist1, hist2 as args (default is %default).')
 parser.add_option('--separate-plots', default='None',
                   help='Snippet for separate_plots lambda, which takes name, hist1, hist2 as args (default is %default).')
+parser.add_option('--skip', default='None',
+                  help='Snippet for skip lambda, which takes name, hist1, hist2 as args (default is %default).')
 options, args = parser.parse_args()
 
 if len(args) < 4:
     print 'Required args missing:\n'
     parser.print_help()
     sys.exit(1)
-    
+
+print options
 options.file1, options.file2, options.dir_path, options.plot_path = args
 options.color1 = eval(options.color1)
 options.color2 = eval(options.color2)
@@ -36,10 +38,13 @@ _lambda = 'lambda name, hist1, hist2: '
 options.no_stats       = eval(_lambda + options.no_stats)
 options.apply_commands = eval(_lambda + options.apply_commands)
 options.separate_plots = eval(_lambda + options.separate_plots)
+options.skip           = eval(_lambda + options.skip)
 
 #print options ; print args ; import sys ; print sys.argv ; raise 1
 
 ########################################################################
+
+from JMTucker.Tools.ROOTTools import *
 
 set_style()
 ps = plot_saver(options.plot_path, size=options.size, per_page=options.per_page)
@@ -53,4 +58,5 @@ compare_all_hists(ps,
                   no_stats = options.no_stats,
                   apply_commands = options.apply_commands,
                   separate_plots = options.separate_plots,
+                  skip = options.skip,
                   )
