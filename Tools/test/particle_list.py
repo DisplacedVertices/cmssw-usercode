51a7c9d3a9aea860688708135c6c8030742a8f2a
tucker <>
2013-06-24 15:59:23 +0000

    Use CRABSubmitter.

--- Tools/test/particle_list.py
+++ Tools/test/particle_list.py
@@ -13,25 +13,11 @@ add_analyzer('JMTParticleListDrawer',
 input_from_argv()
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
-    crab_cfg = '''
-[CRAB]
-jobtype = cmssw
-scheduler = condor
-
-[CMSSW]
-dbs_url = https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_02_writer/servlet/DBSServlet
-datasetpath = /QCD_Pt-15to30_TuneZ2star_8TeV_pythia6/tucker-sstoptuple_v1_qcd15-3312fbeda721580c3cdebaec6739016e/USER
-pset = particle_list.py
-total_number_of_events = -1
-events_per_job = 1000
-
-[USER]
-ui_working_dir = crab_particle_list
-return_data = 1
-'''
-    
-    just_testing = 'testing' in sys.argv
-    open('crab.cfg', 'wt').write(crab_cfg)
-    if not just_testing:
-        os.system('crab -create -submit')
-        os.system('rm crab.cfg')
+    from JMTucker.Tools.Samples import background_samples
+    from JMTucker.Tools.CRABSubmitter import CRABSubmitter
+    cs = CRABSubmitter('BkgsParticleList',
+                       total_number_of_events = 3,
+                       number_of_jobs = 1,
+                       USER_jmt_skip_input_files = 'src/EGamma/EGammaAnalysisTools/data/*',
+                       )
+    cs.submit_all([s for s in background_samples if s.name != 'ttbarhadronic'])
