56e79c271f5e75ac927d6563de4e1b4ba2c3e186
tucker <>
2013-06-29 00:42:17 +0000

    Switch to argparse; support more than two input files.

--- Tools/scripts/comparehists.py
+++ Tools/scripts/comparehists.py
@@ -1,62 +1,83 @@
 #!/usr/bin/env python
 
-import optparse, sys
+import sys
 from pprint import pprint
+import JMTucker.Tools.argparse as argparse
 
-parser = optparse.OptionParser(usage='%prog [options] file1.root file2.root dir_path plot_path')
-parser.add_option('--per-page', type=int, default=-1,
-                  help='Put PER_PAGE plots per html page (default all on one page).')
-parser.add_option('--size', nargs=2, type=int, default=(600,600), metavar='SIZEX,SIZEY',
-                  help='Set the plot size to SIZEX x SIZEY (default %default.')
-parser.add_option('--nice1', default='file1',
-                  help='Nice name for file #1 (default is %default).')
-parser.add_option('--nice2', default='file2',
-                  help='Nice name for file #2 (default is %default).')
-parser.add_option('--color1', default='ROOT.kRed',
-                  help='Color #1: may be a python snippet, e.g. the default %default.')
-parser.add_option('--color2', default='ROOT.kBlue',
-                  help='Color #2: may be a python snippet, e.g. the default %default.')
-parser.add_option('--no-stats', default='False',
-                  help='Snippet for no_stats lambda, which takes name, hist1, hist2 as args (default is %default).')
-parser.add_option('--apply-commands', default='None',
-                  help='Snippet for apply_commands lambda, which takes name, hist1, hist2 as args (default is %default).')
-parser.add_option('--separate-plots', default='None',
-                  help='Snippet for separate_plots lambda, which takes name, hist1, hist2 as args (default is %default).')
-parser.add_option('--skip', default='None',
-                  help='Snippet for skip lambda, which takes name, hist1, hist2 as args (default is %default).')
-options, args = parser.parse_args()
+parser = argparse.ArgumentParser(description = 'comparehists: compare all histograms in multiple files',
+                                 usage = '%(prog)s [options] file1.root file2.root [... fileN.root] dir_path plot_path')
 
-if len(args) < 4:
-    print 'Required args missing:\n'
+parser.add_argument('positional', nargs='*', help='The .root files.')
+
+parser.add_argument('--per-page', type=int, default=-1,
+                    help='Put PER_PAGE plots per html page (default all on one page).')
+parser.add_argument('--size', nargs=2, type=int, default=(600,600), metavar='SIZE',
+                    help='Set the plot size to SIZEX x SIZEY (default %(default)s.')
+parser.add_argument('--nice', nargs='+', default=[],
+                    help='Nice names for the files (default is file1, file2, ...).')
+parser.add_argument('--colors', default=['ROOT.kRed', 'ROOT.kBlue', 'ROOT.kGreen+2', 'ROOT.kOrange+7'],
+                    help='Colors for the files: may be a python snippet, e.g. the default %(default)s.')
+
+group = parser.add_argument_group('Callback function snippets: will be of the form lambda name, hists: <snippet here>')
+group.add_argument('--no-stats', default='False',
+                    help='Snippet for no_stats lambda (default is %(default)s).')
+group.add_argument('--apply-commands', default='None',
+                  help='Snippet for apply_commands lambda, which takes name, hist1, hist2 as args (default is %(default)s).')
+group.add_argument('--separate-plots', default='None',
+                  help='Snippet for separate_plots lambda, which takes name, hist1, hist2 as args (default is %(default)s).')
+group.add_argument('--skip', default='None',
+                  help='Snippet for skip lambda, which takes name, hist1, hist2 as args (default is %(default)s).')
+
+options = parser.parse_args()
+
+if len(options.positional) < 4:
+    print 'Required args missing, including at least two filenames\n'
     parser.print_help()
     sys.exit(1)
 
 from JMTucker.Tools.ROOTTools import *
 
-options.file1, options.file2, options.dir_path, options.plot_path = args
-options.color1 = eval(options.color1)
-options.color2 = eval(options.color2)
-_lambda = 'lambda name, hist1, hist2: '
-options.no_stats       = eval(_lambda + options.no_stats)
-options.apply_commands = eval(_lambda + options.apply_commands)
-options.separate_plots = eval(_lambda + options.separate_plots)
-options.skip           = eval(_lambda + options.skip)
+options.files = options.positional[:-2]
+options.dir_path, options.plot_path = options.positional[-2:]
+nfiles = len(options.files)
 
-#print args ; import sys ; print sys.argv ; raise 1
+options.nice = options.nice[:nfiles]
+while len(options.nice) < nfiles:
+    options.nice.append('file%i' % (len(options.nice) + 1))
+
+options.colors = options.colors[:nfiles]
+options.colors = [eval(c) for c in options.colors]
+ncolors = len(options.colors)
+while len(options.colors) < nfiles:
+    options.colors.append(ROOT.kMagenta + len(options.colors) - ncolors)
+
+_lambda = 'lambda name, hists: '
+options.no_stats       = _lambda + options.no_stats
+options.apply_commands = _lambda + options.apply_commands
+options.separate_plots = _lambda + options.separate_plots
+options.skip           = _lambda + options.skip
+options.lambda_no_stats       = eval(options.no_stats)
+options.lambda_apply_commands = eval(options.apply_commands)
+options.lambda_separate_plots = eval(options.separate_plots)
+options.lambda_skip           = eval(options.skip)
+
+print 'comparehists running with these options:'
+pprint(vars(options))
+
+#import sys ; print 'argv:', sys.argv ;  sys.exit(1)
 
 ########################################################################
 
 set_style()
 ps = plot_saver(options.plot_path, size=options.size, per_page=options.per_page)
 
-f_1 = ROOT.TFile(options.file1)
-f_2 = ROOT.TFile(options.file2)
+files = [ROOT.TFile(file) for file in options.files]
+dirs = [file.Get(options.dir_path) for file in files]
 
 compare_all_hists(ps,
-                  options.nice1, f_1.Get(options.dir_path), options.color1,
-                  options.nice2, f_2.Get(options.dir_path), options.color2,
-                  no_stats = options.no_stats,
-                  apply_commands = options.apply_commands,
-                  separate_plots = options.separate_plots,
-                  skip = options.skip,
+                  samples = zip(options.nice, dirs, options.colors),
+                  no_stats = options.lambda_no_stats,
+                  apply_commands = options.lambda_apply_commands,
+                  separate_plots = options.lambda_separate_plots,
+                  skip = options.lambda_skip,
                   )
