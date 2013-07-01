056992666e2951fb9f83286939859a8daf86d010
tucker <>
2013-07-01 16:33:34 +0000

    Preserve config of original file (in ..), except use CRABSubmitter (untested).

--- MFVNeutralino/test/MakeSamples/reco.py
+++ MFVNeutralino/test/MakeSamples/reco.py
@@ -1,4 +1,4 @@
-import FWCore.ParameterSet.Config as cms
+import sys, os, FWCore.ParameterSet.Config as cms
 
 process = cms.Process('RECO')
 
@@ -17,12 +17,14 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('file:gensimhlt.root'))
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000000
-for category in ['TwoTrackMinimumDistance']:
-    process.MessageLogger.categories.append(category)
-    setattr(process.MessageLogger.cerr, category, cms.untracked.PSet(limit=cms.untracked.int32(0)))
+if not 'debug' in sys.argv:
+    process.options.wantSummary = False
+    process.MessageLogger.cerr.FwkReport.reportEvery = 1000000
+    for category in ['TwoTrackMinimumDistance']:
+        process.MessageLogger.categories.append(category)
+        setattr(process.MessageLogger.cerr, category, cms.untracked.PSet(limit=cms.untracked.int32(0)))
 
 process.output = cms.OutputModule('PoolOutputModule',
                                   fileName = cms.untracked.string('reco.root'),
@@ -50,3 +52,17 @@ process.reconstruction_step *= process.mfvTrackMatches
 process.output.outputCommands += ['keep *_mfvTrackMatches_*_*']
 
 process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.output_step)
+
+if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
+    from JMTucker.Tools.CRABSubmitter import CRABSubmitter
+    from JMTucker.Tools.Samples import mfv_signal_samples as samples
+    cs = CRABSubmitter('MFVReco',
+                       use_parent_dataset = True,
+                       get_edm_output = True,
+                       data_retrieval = 'fnal',
+                       total_number_of_events = -1,
+                       events_per_job = 200,
+                       publish_data_name = 'reco',
+                       USER_jmt_skip_input_files = 'src/EGamma/EGammaAnalysisTools/data/*',
+                       )
+    cs.submit_all(samples)
