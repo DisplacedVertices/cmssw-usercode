4d4a6f3a157fc6222fa9096ae9212f5823878936
tucker <>
2013-07-01 23:50:49 +0000

    twostep.csh -> .sh: crab runs scripts with sh and not whatever is in the hashbang.

--- MFVNeutralino/test/MakeSamples/twostep.sh
+++ MFVNeutralino/test/MakeSamples/twostep.sh
@@ -1,7 +1,7 @@
-#!/bin/tcsh
+#!/bin/bash
 
 echo TWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEP
-echo twostep.csh called with args:
+echo twostep.sh called with args:
 echo $*
 echo NJob is $NJob
 echo MaxEvents is $MaxEvents
@@ -15,35 +15,35 @@ echo
 echo start gensimhlt step at `date`
 echo
 cmsRun pset.py $1
-set exit_code=$?
-if ($exit_code) then
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
   echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   echo @@@@ cmsRun exited gensimhlt step with error code $exit_code
   echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   exit $exit_code
-endif
+fi
 echo
 echo done with gensimhlt step at `date`, starting reco step
 echo
 cmsRun reco.py
-set exit_code=$?
-if ($exit_code) then
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
   echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   echo @@@@ cmsRun exited reco step with error code $exit_code
   echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   exit $exit_code
-endif
+fi
 echo
 echo done with reco at `date`, starting pat step
 echo
 cmsRun -j $RUNTIME_AREA/crab_fjr_$NJob.xml pat.py
-set exit_code=$?
-if ($exit_code) then
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
   echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   echo cmsRun exited pat step with error code $exit_code
   echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   exit $exit_code
-endif
+fi
 echo
 echo done at `date`
 echo TWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEP
