479cdbbc53f69b3e3c76ed304eee607c753fef7f
tucker <>
2013-07-02 15:12:45 +0000

    plot_saver: put link to .root file next to anchor for each plot.

--- Tools/python/ROOTTools.py
+++ Tools/python/ROOTTools.py
@@ -874,7 +874,8 @@ class plot_saver:
                 continue # skip dir entries
             fn, log, root, pdf, pdf_log, C, C_log = save
             bn = os.path.basename(fn)
-            html.write('<a href="#%s"><h4 id="%s">%s</h4></a><br>\n' % (self.anchor_name(fn), self.anchor_name(fn), bn.replace('.png', '')))
+            rootlink = ', <a href="%s">root</a>' % root
+            html.write('<a href="#%s"><h4 id="%s">%s</h4></a>%s<br>\n' % (self.anchor_name(fn), self.anchor_name(fn), bn.replace('.png', ''), rootlink))
             if log:
                 html.write('<img src="%s"><img src="%s"><br><br>\n' % (bn, os.path.basename(log)))
             else:
