81d252d991069a128b657626807317e6c4b4aa4d
tucker <>
2013-07-02 14:01:00 +0000

    Use JMTParticleListDrawer; replay_event takes filename/rle from argv.

--- MFVNeutralino/test/MakeSamples/debug.py
+++ MFVNeutralino/test/MakeSamples/debug.py
@@ -1,19 +1,18 @@
+import sys
 from gensimhlt import cms, process
 
 if 'replay' in sys.argv:
     from JMTucker.Tools.CMSSWTools import replay_event
-    replay_event(process, '/store/user/tucker/mfv_gensimhlt_gluino_tau1000um_M0400/mfv_gensimhlt_gluino_tau1000um_M0400/11e502b9027fe454bec38485095c4f53/gensimhlt_7_1_mcS.root', (1,7,35))
+    replay_event(process, filename=None, rle=None)
 
+process.maxEvents.input = 1
 process.options.wantSummary = True
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 process.generator.maxEventsToPrint = 2
 process.generator.pythiaPylistVerbosity = 13
 process.generator.pythiaHepMCVerbosity = True
-process.printList = cms.EDAnalyzer('ParticleListDrawer',
-                                   maxEventsToPrint = cms.untracked.int32(100),
+process.printList = cms.EDAnalyzer('JMTParticleListDrawer',
                                    src = cms.InputTag('genParticles'),
-                                   printOnlyHardInteraction = cms.untracked.bool(False),
-                                   useMessageLogger = cms.untracked.bool(False),
                                    printVertex = cms.untracked.bool(True),
                                    )
 process.dumpGraph = cms.EDAnalyzer('GenParticlesGraphDumper',
@@ -22,5 +21,5 @@ process.dumpGraph = cms.EDAnalyzer('GenParticlesGraphDumper',
                                    use_daughters = cms.bool(False),
                                    include_id_and_stat = cms.bool(True),
                                    )
-process.pprint = cms.Path(process.printList * process.dumpGraph)
+process.pprint = cms.Path(process.printList) # * process.dumpGraph)
 process.schedule.extend([process.pprint])
