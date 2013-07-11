b13d31b44ae4a333c0c9c7d4d27358c2ae0d676e
tucker <>
2013-07-11 19:48:36 +0000

    Fix debug mode; submit script uses CRABSubmitter.

--- MFVNeutralino/test/gen_histos.py
+++ MFVNeutralino/test/gen_histos.py
@@ -19,36 +19,17 @@ if debug:
                                        src = cms.InputTag('genParticles'),
                                        printVertex = cms.untracked.bool(True),
                                        )
-    process.mfvGenHistos.print_info = -1
     process.p.insert(0, process.printList)
+    file_event_from_argv(process)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     if debug:
         raise RuntimeError('refusing to submit jobs in debug (verbose print out) mode')
 
-    crab_cfg = '''
-[CRAB]
-jobtype = cmssw
-scheduler = %(scheduler)s
-
-[CMSSW]
-dbs_url = https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_02_writer/servlet/DBSServlet
-datasetpath = %(dataset)s
-pset = gen_histos.py
-total_number_of_events = -1
-events_per_job = 50000
-
-[USER]
-jmt_skip_input_files = src/EGamma/EGammaAnalysisTools/data/*
-ui_working_dir = crab/gen_histos/crab_%(name)s
-return_data = 1
-'''
-
-    testing = 'testing' in sys.argv
-
-    from JMTucker.Tools.Samples import mfv_signal_samples
-    for sample in mfv_signal_samples:
-        open('crab.cfg', 'wt').write(crab_cfg % sample)
-        if not testing:
-            os.system('crab -create -submit')
-            os.system('rm crab.cfg')
+    from JMTucker.Tools.CRABSubmitter import CRABSubmitter
+    cs = CRABSubmitter('GenHistos',
+                       total_number_of_events = -1,
+                       events_per_job = 100000,
+                       USER_jmt_skip_input_files = 'src/EGamma/EGammaAnalysisTools/data/*'
+                       )
+    cs.submit_all(samples)
