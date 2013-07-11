5edf258f247a39dfff32afd434ed66f54c222bf6
tucker <>
2013-07-11 20:25:30 +0000

    Better way to check using detree.

--- MFVNeutralino/test/MakeSamples/check_dups.py
+++ MFVNeutralino/test/MakeSamples/check_dups.py
@@ -1,29 +1,25 @@
-from JMTucker.Tools.ROOTTools import ROOT
-f = ROOT.TFile('gen_ntuple.root')
-t = f.Get('MFVGenNtupleDumper/t')
+import sys
+from JMTucker.Tools.ROOTTools import *
 
-n = 200
-nx, ny, nz = n,n,n
+def check(fn):
+    f = ROOT.TFile(fn)
+    t = f.Get('MFVGenNtupleDumper/t')
 
-def check_it(h):
-    for i in xrange(0,nx+2):
-        for j in xrange(0,ny+2):
-            for k in xrange(0,nz+2):
-                c = h.GetBinContent(i,j) #,k)
-                if c > 1:
-                    x = h.GetXaxis().GetBinLowEdge(i)
-                    y = h.GetYaxis().GetBinLowEdge(j)
-                    z = h.GetZaxis().GetBinLowEdge(k)
-                    print '%3i %3i %3i  %10.7f %10.7f %10.7f   %i' % (i,j,k,x,y,z,c)
+    flt = lambda x: tuple(float(y) for y in x)
+    a = list(detree(t, 'lsp_pt:lsp_eta:lsp_phi', '', flt))
+    b = list(detree(t, 'lsp_pt:lsp_eta:lsp_mass', '', flt))
+    c = list(detree(t, 'lsp_pt:lsp_decay_vx:lsp_decay_vy:lsp_decay_vz', '', flt))
 
-h = ROOT.TH3D('h','',nx,0,1000,ny,-6,6,nz,-3.1416,3.1416)
-t.Draw('lsp_eta:lsp_phi:lsp_pt>>h')
-print 'pt eta phi'
-check_it(h)
-del h
+    n = t.GetEntriesFast()
+    print n, '->', n*2
+    for l in (a,b,c):
+        ll = len(l)
+        ls = len(set(l))
+        print ll, ls
+        assert len(l) == len(set(l))
 
-h = ROOT.TH3D('h','',nx,-5,5,ny,-5,5,nz,-5,5)
-t.Draw('lsp_decay_vy:lsp_decay_vz:lsp_decay_vx>>h')
-print 'vx vy vz'
-check_it(h)
-del h
+for fn in sys.argv[1:]:
+    if fn.endswith('.root'):
+        print fn
+        check(fn)
+        print
