4f7c9e0b3bc00102527ff6790184c258e3acfe01
tucker <>
2013-08-06 19:34:17 +0000

    Make JetVertexAssociator actually work.

--- MFVNeutralino/src/classes.h
+++ MFVNeutralino/src/classes.h
@@ -1,11 +1,18 @@
 #include <map>
+#include <vector>
+#include "DataFormats/Common/interface/Association.h"
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
 #include "JMTucker/MFVNeutralino/interface/LightTrackMatch.h"
 
 namespace {
   namespace {
     edm::Wrapper<LightTrackMatch> ltm;
     std::map<int, LightTrackMatch> dummymiltm;
-    edm::Wrapper<std::map<int, LightTrackMatch> > dummymiltmW;
+    edm::Wrapper<std::map<int, LightTrackMatch> > dummyWmiltm;
+
+    std::vector<reco::Vertex> vrv;
+    edm::Association<std::vector<reco::Vertex> > dummyAvV;
+    edm::Wrapper<edm::Association<std::vector<reco::Vertex> > > dummyWAvV;
   }
 }
