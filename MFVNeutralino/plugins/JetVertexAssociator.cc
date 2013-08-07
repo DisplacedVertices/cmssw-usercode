00dcc53151cad9da19d6491f902caa0ae4abe34f
tucker <>
2013-08-07 19:39:01 +0000

    h_n_matchedjets_v_jets/vertices.

--- MFVNeutralino/plugins/JetVertexAssociator.cc
+++ MFVNeutralino/plugins/JetVertexAssociator.cc
@@ -31,6 +31,8 @@ private:
   TH1F* h_frac;
   TH2F* h_n_fracs;
   TH2F* h_best_fracs;
+  TH2F* h_n_matchedjets_v_jets;
+  TH2F* h_n_matchedjets_v_vertices;
 };
 
 MFVJetVertexAssociator::MFVJetVertexAssociator(const edm::ParameterSet& cfg)
@@ -46,11 +48,13 @@ MFVJetVertexAssociator::MFVJetVertexAssociator(const edm::ParameterSet& cfg)
   if (histos) {
     edm::Service<TFileService> fs;
     
-    h_n_jets_v_vertices = fs->make<TH2F>("h_n_jets_v_vertices", ";# of vertices;# of jets", 30, 0, 30, 30, 0, 30);
+    h_n_jets_v_vertices = fs->make<TH2F>("h_n_jets_v_vertices", ";# of vertices;# of jets", 20, 0, 20, 20, 0, 20);
     h_n_jet_tracks = fs->make<TH1F>("h_n_jet_tracks", ";# tracks per jet;arb. units", 50, 0, 50);
     h_frac = fs->make<TH1F>("h_frac", ";fraction of jet's tracks in a vertex;arb. units", 51, 0, 1.02);
-    h_n_fracs = fs->make<TH2F>("h_n_fracs", ";# non zero fractions;# non zero good fractions", 30, 0, 30, 30, 0, 30);
+    h_n_fracs = fs->make<TH2F>("h_n_fracs", ";# non zero fractions;# non zero good fractions", 20, 0, 20, 20, 0, 20);
     h_best_fracs = fs->make<TH2F>("h_best_fracs", ";second-best fraction;best fraction", 26, 0, 1.04, 26, 0, 1.04);
+    h_n_matchedjets_v_jets = fs->make<TH2F>("h_n_matchedjets_v_jets", ";# of jets;# of matched jets", 20, 0, 20, 20, 0, 20);
+    h_n_matchedjets_v_vertices = fs->make<TH2F>("h_n_matchedjets_v_vertices", ";# of vertices;# of matched jets", 20, 0, 20, 20, 0, 20);
   }
 }
 
@@ -69,6 +73,8 @@ void MFVJetVertexAssociator::produce(edm::Event& event, const edm::EventSetup&) 
 
   if (histos)
     h_n_jets_v_vertices->Fill(n_vertices, n_jets);
+
+  size_t n_matchedjets = 0;
 
   std::vector<int> indices(n_jets);
   for (size_t ijet = 0; ijet < n_jets; ++ijet) {
@@ -90,14 +96,13 @@ void MFVJetVertexAssociator::produce(edm::Event& event, const edm::EventSetup&) 
       double second_best_frac = 0;
       int n_non_zero_fracs = 0;
       int n_non_zero_good_fracs = 0;
-      
 
       for (size_t ivtx = 0; ivtx < n_vertices; ++ivtx) {
         const reco::Vertex& vtx = vertices->at(ivtx);
 
         int in_jet = 0;
         for (auto itk = vtx.tracks_begin(), itke = vtx.tracks_end(); itk != itke; ++itk) {
-          if (vtx.trackWeight(*itk) > min_vertex_track_weight) {
+          if (vtx.trackWeight(*itk) >= min_vertex_track_weight) {
             reco::TrackRef tk = itk->castTo<reco::TrackRef>();
             if (jet_tracks.count(tk) > 0)
               ++in_jet;
@@ -129,7 +134,15 @@ void MFVJetVertexAssociator::produce(edm::Event& event, const edm::EventSetup&) 
       }
     }
 
+    if (histos && assoc_ivtx >= 0)
+      ++n_matchedjets;
+
     indices[ijet] = assoc_ivtx;
+  }
+
+  if (histos) {
+    h_n_matchedjets_v_jets->Fill(n_jets, n_matchedjets);
+    h_n_matchedjets_v_vertices->Fill(n_vertices, n_matchedjets);
   }
 
   filler.insert(jets, indices.begin(), indices.end());
