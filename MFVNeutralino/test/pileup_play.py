22a3de133f26c4286c1e9f4911bd16468a775389
jchu <>
2013-08-13 20:46:54 +0000

    Added analyzer for NoZCheck.

--- MFVNeutralino/test/pileup_play.py
+++ MFVNeutralino/test/pileup_play.py
@@ -2,7 +2,11 @@ import os, sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process, add_analyzer
 
 process.maxEvents.input = 100
-process.source.fileNames = ['/store/user/jchu/mfv_neutralino_tau9900um_M0400/jtuple_pileupremovalstudiesv6/66e8ed2d65a6b86d5a5bb40c50df93ea/aodpat_1_1_1oX.root']
+#process.source.fileNames = ['/store/user/jchu/mfv_neutralino_tau9900um_M0400/jtuple_pileupremovalstudiesv6/66e8ed2d65a6b86d5a5bb40c50df93ea/aodpat_1_1_1oX.root']
+#process.source.fileNames = ['file:/uscmst1b_scratch/lpc1/3DayLifetime/tucker/pat.root']
+#process.source.secondaryFileNames = cms.untracked.vstring('/store/user/tucker/mfv_neutralino_tau1000um_M0400/mfv_neutralino_tau1000um_M0400/a6ab3419cb64660d6c68351b3cff9fb0/aodpat_1_1_X2h.root')
+process.source.fileNames = ['/store/user/jchu/mfv_neutralino_tau9900um_M0400/jtuple_pileupremovalstudies_v6/5c257295836d18fbf020b73449ad9b5f/pat_1_1_luH.root']
+process.source.secondaryFileNames = cms.untracked.vstring('/store/user/tucker/mfv_neutralino_tau9900um_M0400/mfv_neutralino_tau9900um_M0400/3c4ccd1d95a3d8658f6b5a18424712b3/aodpat_254_2_JTs.root','/store/user/tucker/mfv_neutralino_tau9900um_M0400/mfv_neutralino_tau9900um_M0400/3c4ccd1d95a3d8658f6b5a18424712b3/aodpat_255_1_nvB.root')
 process.TFileService.fileName = 'pileup_play.root'
 
 add_analyzer('PileupRemovalPlay',
@@ -12,11 +16,20 @@ add_analyzer('PileupRemovalPlay',
              ltmm_src = cms.InputTag('mfvTrackMatches'),
              )
 
+process.PileupRemovalPlayNoZCheck = process.PileupRemovalPlay.clone(pucands_src = 'pfPileUpNoClosestZVertexPF',
+                                                                    nonpucands_src = 'pfNoPileUpNoClosestZVertexPF')
+process.pPileupRemovalPlay *= process.PileupRemovalPlayNoZCheck
+
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.Samples import *
-    mfv_neutralino_tau9900um_M0400.ana_dataset_override = '/mfv_neutralino_tau9900um_M0400/jchu-jtuple_pileupremovalstudiesv6-66e8ed2d65a6b86d5a5bb40c50df93ea/USER'
-    #samples = [mfv_neutralino_tau0000um_M0400, mfv_neutralino_tau0010um_M0400, mfv_neutralino_tau0100um_M0400, mfv_neutralino_tau1000um_M0400, mfv_neutralino_tau9900um_M0400]
-    samples = [mfv_neutralino_tau9900um_M0400]
+
+    mfv_neutralino_tau0000um_M0400.ana_dataset_override = '/mfv_neutralino_tau0000um_M0400/jchu-jtuple_pileupremovalstudies_v6-5c257295836d18fbf020b73449ad9b5f/USER'
+    mfv_neutralino_tau0100um_M0400.ana_dataset_override = '/mfv_neutralino_tau0100um_M0400/jchu-jtuple_pileupremovalstudies_v6-5c257295836d18fbf020b73449ad9b5f/USER'
+    mfv_neutralino_tau1000um_M0400.ana_dataset_override = '/mfv_neutralino_tau1000um_M0400/jchu-jtuple_pileupremovalstudies_v6-5c257295836d18fbf020b73449ad9b5f/USER'
+    mfv_neutralino_tau1000um_M1000.ana_dataset_override = '/mfv_neutralino_tau1000um_M1000/jchu-jtuple_pileupremovalstudies_v6-5c257295836d18fbf020b73449ad9b5f/USER'
+    mfv_neutralino_tau9900um_M0400.ana_dataset_override = '/mfv_neutralino_tau9900um_M0400/jchu-jtuple_pileupremovalstudies_v6-5c257295836d18fbf020b73449ad9b5f/USER'
+ 
+    samples = [mfv_neutralino_tau0000um_M0400, mfv_neutralino_tau0100um_M0400, mfv_neutralino_tau1000um_M0400, mfv_neutralino_tau1000um_M1000, mfv_neutralino_tau9900um_M0400]
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     cs = CRABSubmitter('PileupPlay',
