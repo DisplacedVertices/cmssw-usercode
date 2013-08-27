09fd017ab3051a16e5a2267249440f80257e3239
tucker <>
2013-08-27 06:34:48 +0000

    dump_ptr.

--- Tools/interface/Framework.h
+++ Tools/interface/Framework.h
@@ -19,6 +19,20 @@ void dump_ref(std::ostream& out, const edm::Ref<T>& ref, const edm::Event* event
 }
 
 template <typename T>
+void dump_ptr(std::ostream& out, const edm::Ptr<T>& ptr, const edm::Event* event=0) {
+  out << "ptr with product id " << ptr.id().id();
+  if (ptr.id().id() == 0) {
+    out << "\n";
+    return;
+  }
+  if (event) {
+    edm::Provenance prov = event->getProvenance(ptr.id());
+    out << ", branch " << prov.branchName() << " (id " << prov.branchID().id() << "),";
+  }
+  out << " with key " << ptr.key() << "\n";
+}
+
+template <typename T>
 void dump_ref2base(std::ostream& out, const edm::RefToBase<T>& ref, const edm::Event* event=0) {
   out << "ref2base with product id " << ref.id().id();
   if (ref.id().id() == 0) {
