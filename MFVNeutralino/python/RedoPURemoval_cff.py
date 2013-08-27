094e1cda5a9aee4fd6ef04a5486d32b2824011e4
tucker <>
2013-08-27 07:19:49 +0000

    pfPileUp.checkClosestZVertex -> False; pfPileUpPFNoClosestZVertex -> pfPileUpPFClosestZVertex.

--- MFVNeutralino/python/RedoPURemoval_cff.py
+++ MFVNeutralino/python/RedoPURemoval_cff.py
@@ -1,23 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
 pfPileUpPF = cms.EDProducer('PFPileUp',
-                                    PFCandidates = cms.InputTag('particleFlow'),
-                                    Vertices = cms.InputTag('goodOfflinePrimaryVertices'),
-                                    Enable = cms.bool(True),
-                                    checkClosestZVertex = cms.bool(True),
-                                    verbose = cms.untracked.bool(False),
-                                    )
+                            PFCandidates = cms.InputTag('particleFlow'),
+                            Vertices = cms.InputTag('goodOfflinePrimaryVertices'),
+                            Enable = cms.bool(True),
+                            checkClosestZVertex = cms.bool(False),
+                            verbose = cms.untracked.bool(False),
+                            )
 
 pfNoPileUpPF = cms.EDProducer('TPPFCandidatesOnPFCandidates',
-                                      bottomCollection = cms.InputTag('particleFlow'),
-                                      topCollection = cms.InputTag('pfPileUpPF'),
-                                      name = cms.untracked.string('pileUpOnPFCandidates'),
-                                      enable = cms.bool(True),
-                                      verbose = cms.untracked.bool(False),
-                                      )
+                              bottomCollection = cms.InputTag('particleFlow'),
+                              topCollection = cms.InputTag('pfPileUpPF'),
+                              name = cms.untracked.string('pileUpOnPFCandidates'),
+                              enable = cms.bool(True),
+                              verbose = cms.untracked.bool(False),
+                              )
 
-pfPileUpPFNoClosestZVertex = pfPileUpPF.clone(checkClosestZVertex = False)
+pfPileUpPFClosestZVertex = pfPileUpPF.clone(checkClosestZVertex = True)
 
-pfNoPileUpPFNoClosestZVertex = pfNoPileUpPF.clone(topCollection = 'pfPileUpPFNoClosestZVertex')
+pfNoPileUpPFClosestZVertex = pfNoPileUpPF.clone(topCollection = 'pfPileUpPFNoClosestZVertex')
 
-mfvRedoPURemoval = cms.Sequence(pfPileUpPF * pfNoPileUpPF * pfPileUpPFNoClosestZVertex * pfNoPileUpPFNoClosestZVertex)
+mfvRedoPURemoval = cms.Sequence(pfPileUpPF * pfNoPileUpPF * pfPileUpPFClosestZVertex * pfNoPileUpPFClosestZVertex)
