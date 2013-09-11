a95fae85c5384533d63867228d94696d2c01c62b
tucker <>
2013-09-11 18:56:05 +0000

    AssociationMap<reco::VertexCollection,pat::JetCollection>.

--- MFVNeutralino/src/classes.h
+++ MFVNeutralino/src/classes.h
@@ -1,7 +1,9 @@
 #include <map>
 #include <vector>
 #include "DataFormats/Common/interface/Association.h"
+#include "DataFormats/Common/interface/AssociationMap.h"
 #include "DataFormats/Common/interface/Wrapper.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "JMTucker/MFVNeutralino/interface/LightTrackMatch.h"
 
@@ -13,5 +15,8 @@ namespace {
 
     edm::Association<std::vector<reco::Vertex> > dummyAvV;
     edm::Wrapper<edm::Association<std::vector<reco::Vertex> > > dummyWAvV;
+
+    edm::AssociationMap<edm::OneToMany<std::vector<reco::Vertex>,std::vector<pat::Jet>, unsigned int > > dummyAMrVpJ;
+    edm::Wrapper<edm::AssociationMap<edm::OneToMany<std::vector<reco::Vertex>,std::vector<pat::Jet>, unsigned int > > > dummyWAMrVpJ;
   }
 }
