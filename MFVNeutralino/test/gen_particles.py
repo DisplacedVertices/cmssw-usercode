3f07287239e2149bde4e27e6e606c6e3611352a9
tucker <>
2013-09-17 00:55:37 +0000

    Use cffs.

--- MFVNeutralino/test/gen_particles.py
+++ MFVNeutralino/test/gen_particles.py
@@ -1,37 +1,28 @@
-import FWCore.ParameterSet.Config as cms
+import sys, FWCore.ParameterSet.Config as cms
 
 process = cms.Process('MFVGenParticles')
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5))
-process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('file:/uscmst1b_scratch/lpc1/3DayLifetime/tucker/fastsim_313_2_yIN.root'))
+process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('/store/user/tucker/mfv_neutralino_tau1000um_M0400/mfv_neutralino_tau1000um_M0400/a6ab3419cb64660d6c68351b3cff9fb0/aodpat_1_1_X2h.root'))
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 process.load('FWCore.MessageLogger.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 100000
 
-process.mfvGenParticles = cms.EDProducer('MFVGenParticles',
-                                         gen_src = cms.InputTag('genParticles'),
-                                         gen_jet_src = cms.InputTag('ak5GenJets'),
-                                         gen_met_src = cms.InputTag('genMetTrue'),
-                                         print_info = cms.bool(True),
-                                         )
+process.load('JMTucker.Tools.ParticleListDrawer_cff')
+process.load('JMTucker.MFVNeutralino.GenParticles_cff')
 
-process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+#process.mfvGenParticles.print_info = True
+process.mfvGenVertices.print_info = True
 
-process.printList = cms.EDAnalyzer('ParticleListDrawer',
-                                   maxEventsToPrint = cms.untracked.int32(100),
-                                   src = cms.InputTag('genParticles'),
-                                   printOnlyHardInteraction = cms.untracked.bool(False),
-                                   useMessageLogger = cms.untracked.bool(False),
-                                   printVertex = cms.untracked.bool(True),
+process.ParticleListDrawer2 = process.ParticleListDrawer.clone(src = cms.InputTag('mfvGenParticles', 'All'))
+process.ParticleListDrawer3 = process.ParticleListDrawer.clone(src = cms.InputTag('mfvGenParticles', 'Visible'))
+
+process.p = cms.Path(process.ParticleListDrawer * process.mfvGenParticles * process.mfvGenVertices)
+#process.p = cms.Path(process.mfvGenParticles * process.ParticleListDrawer2 * process.ParticleListDrawer3)
+
+if 'out' in sys.argv:
+    process.out = cms.OutputModule('PoolOutputModule',
+                                   fileName = cms.untracked.string('gen_particles.root'),
+                                   outputCommands = cms.untracked.vstring('drop *', 'keep recoGenParticles_*_*_MFVGenParticles'),
                                    )
-
-process.printList2 = process.printList.clone(src = cms.InputTag('mfvGenParticles', 'All'))
-process.printList3 = process.printList.clone(src = cms.InputTag('mfvGenParticles', 'Visible'))
-
-process.p = cms.Path(process.mfvGenParticles * process.printList2 * process.printList3)
-
-process.out = cms.OutputModule('PoolOutputModule',
-                               fileName = cms.untracked.string('gen_particles.root'),
-                               outputCommands = cms.untracked.vstring('drop *', 'keep recoGenParticles_*_*_MFVGenParticles'),
-                               )
-process.outp = cms.EndPath(process.out)
+    process.outp = cms.EndPath(process.out)
 
