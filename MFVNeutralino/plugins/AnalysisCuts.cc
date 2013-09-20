8e9fb43b77aef10e3f988b88b7537f903baa20b2
jchu <>
2013-09-20 18:25:06 +0000

    Added cuts on secondary vertex variables.

--- MFVNeutralino/plugins/AnalysisCuts.cc
+++ MFVNeutralino/plugins/AnalysisCuts.cc
@@ -24,6 +24,11 @@ private:
   const double bdisc_min;
   const edm::InputTag muon_src;
   const edm::InputTag electron_src;
+
+  const edm::InputTag vertex_src;
+  const int min_nvertex;
+  const int min_ntracks01;
+  const double min_maxtrackpt01;
 };
 
 MFVAnalysisCuts::MFVAnalysisCuts(const edm::ParameterSet& cfg) 
@@ -39,7 +44,12 @@ MFVAnalysisCuts::MFVAnalysisCuts(const edm::ParameterSet& cfg)
     b_discriminator_name(cfg.getParameter<std::string>("b_discriminator_name")),
     bdisc_min(cfg.getParameter<double>("bdisc_min")),
     muon_src(cfg.getParameter<edm::InputTag>("muon_src")),
-    electron_src(cfg.getParameter<edm::InputTag>("electron_src"))
+    electron_src(cfg.getParameter<edm::InputTag>("electron_src")),
+
+    vertex_src(cfg.getParameter<edm::InputTag>("vertex_src")),
+    min_nvertex(cfg.getParameter<int>("min_nvertex")),
+    min_ntracks01(cfg.getParameter<int>("min_ntracks01")),
+    min_maxtrackpt01(cfg.getParameter<int>("min_maxtrackpt01"))
 {
 }
 
@@ -70,6 +80,44 @@ bool MFVAnalysisCuts::filter(edm::Event& event, const edm::EventSetup&) {
   if (njets < min_njets) return false; //cut on the number of jets
   if (nbtags < min_nbtags) return false; //cut on the number of btags
   if (sum_ht < min_sum_ht) return false; //cut on the sum of the pt's of all the jets
+
+  edm::Handle<reco::VertexCollection> secondary_vertices;
+  event.getByLabel(vertex_src, secondary_vertices);
+
+  const int nsv = int(secondary_vertices->size());
+  if (nsv < min_nvertex) return false; //cut on the number of secondary vertices
+  int ntracks0 = 0;
+  int ntracks1 = 0;
+  double maxtrackpt0 = 0;
+  double maxtrackpt1 = 0;
+  for (int isv = 0; isv < nsv; ++isv) {
+    const reco::Vertex& sv = secondary_vertices->at(isv);
+    const reco::VertexRef svref(secondary_vertices, isv);
+    auto trkb = sv.tracks_begin();
+    auto trke = sv.tracks_end();
+    int ntracks = trke - trkb;
+
+    std::vector<double> trackpts;
+    for (auto trki = trkb; trki != trke; ++trki) {
+      const reco::TrackBaseRef& tri = *trki;
+      double pti = tri->pt();
+      trackpts.push_back(pti);
+    }
+    std::sort(trackpts.begin(), trackpts.end());
+    const double maxtrackpt = trackpts[trackpts.size()-1];
+
+    if (ntracks > ntracks0) {
+      ntracks1 = ntracks0;
+      ntracks0 = ntracks;
+      maxtrackpt1 = maxtrackpt0;
+      maxtrackpt0 = maxtrackpt;
+    } else if (ntracks > ntracks1) {
+      ntracks1 = ntracks;
+      maxtrackpt1 = maxtrackpt;
+    }
+  }
+  if (ntracks0 + ntracks1 < min_ntracks01) return false; //cut on the sum of ntracks for the two SV's with the highest ntracks
+  if (maxtrackpt0 + maxtrackpt1 < min_maxtrackpt01) return false; //cut on the sum of maxtrackpt for the two SV's with the highest ntracks
 
   return true;
 }
