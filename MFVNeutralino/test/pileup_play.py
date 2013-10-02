c2e1e934278638f388b7f2b91a4bb8664066e514
tucker <>
2013-10-02 00:36:04 +0000

    Redo PU removal for running on PAT tuple files.

--- MFVNeutralino/test/pileup_play.py
+++ MFVNeutralino/test/pileup_play.py
@@ -1,27 +1,34 @@
 import os, sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process, add_analyzer
+from JMTucker.Tools.CMSSWTools import set_events_to_process
 
 process.maxEvents.input = 100
-#process.source.fileNames = ['/store/user/jchu/mfv_neutralino_tau9900um_M0400/jtuple_pileupremovalstudiesv6/66e8ed2d65a6b86d5a5bb40c50df93ea/aodpat_1_1_1oX.root']
-#process.source.fileNames = ['file:/uscmst1b_scratch/lpc1/3DayLifetime/tucker/pat.root']
-#process.source.secondaryFileNames = cms.untracked.vstring('/store/user/tucker/mfv_neutralino_tau1000um_M0400/mfv_neutralino_tau1000um_M0400/a6ab3419cb64660d6c68351b3cff9fb0/aodpat_1_1_X2h.root')
-process.source.fileNames = ['/store/user/jchu/mfv_neutralino_tau9900um_M0400/jtuple_pileupremovalstudies_v6/5c257295836d18fbf020b73449ad9b5f/pat_1_1_luH.root']
-process.source.secondaryFileNames = cms.untracked.vstring('/store/user/tucker/mfv_neutralino_tau9900um_M0400/mfv_neutralino_tau9900um_M0400/3c4ccd1d95a3d8658f6b5a18424712b3/aodpat_254_2_JTs.root','/store/user/tucker/mfv_neutralino_tau9900um_M0400/mfv_neutralino_tau9900um_M0400/3c4ccd1d95a3d8658f6b5a18424712b3/aodpat_255_1_nvB.root')
+process.options.wantSummary = True
+process.source.fileNames = ['/store/user/jchu/mfv_neutralino_tau1000um_M0200/jtuple_v7/5d4c2a74c85834550d3f9609274e8548/pat_11_1_css.root']
+process.source.secondaryFileNames = cms.untracked.vstring('/store/user/tucker/mfv_neutralino_tau1000um_M0200/mfv_neutralino_tau1000um_M0200/a6ab3419cb64660d6c68351b3cff9fb0/aodpat_1048_1_yBl.root')
+set_events_to_process(process, [(1,1048,26)])
+
 process.TFileService.fileName = 'pileup_play.root'
 
 add_analyzer('PileupRemovalPlay',
              pucands_src = cms.InputTag('pfPileUpPF'),
              nonpucands_src = cms.InputTag('pfNoPileUpPF'),
+             pf_jets_src = cms.InputTag('ak5PFJets'),
+             pat_jets_src = cms.InputTag('selectedPatJetsPF'),
              pv_src = cms.InputTag('goodOfflinePrimaryVertices'),
              ltmm_src = cms.InputTag('mfvTrackMatches'),
              pt_cut = cms.double(0),
+             verbose = cms.bool(True),
              )
 
 process.PileupRemovalPlayNoZCheck = process.PileupRemovalPlay.clone(pucands_src = 'pfPileUpNoClosestZVertexPF',
                                                                     nonpucands_src = 'pfNoPileUpNoClosestZVertexPF')
 process.PileupRemovalPlayPt5 = process.PileupRemovalPlay.clone(pt_cut = 5)
 process.PileupRemovalPlayNoZCheckPt5 = process.PileupRemovalPlayNoZCheck.clone(pt_cut = 5)
-process.pPileupRemovalPlay *= process.PileupRemovalPlayNoZCheck * process.PileupRemovalPlayPt5 * process.PileupRemovalPlayNoZCheckPt5
+#process.pPileupRemovalPlay *= process.PileupRemovalPlayNoZCheck * process.PileupRemovalPlayPt5 * process.PileupRemovalPlayNoZCheckPt5
+
+process.load('JMTucker.MFVNeutralino.RedoPURemoval_cff')
+process.pPileupRemovalPlay.replace(process.PileupRemovalPlay, process.mfvRedoPURemoval * process.PileupRemovalPlay)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.Samples import *
