ed69de075b7b71bb680c195ecfa38d22a3edcc19
tucker <>
2013-10-07 23:26:19 +0000

    Analysis cuts filter.

---
+++ MFVNeutralino/python/AnalysisCuts_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+mfvAnalysisCuts = cms.EDFilter('MFVAnalysisCuts',
+                               mevent_src = cms.InputTag('mfvEvent'),
+                               min_4th_jet_pt = cms.double(60),
+                               min_5th_jet_pt = cms.double(0),
+                               min_6th_jet_pt = cms.double(0),
+                               min_njets = cms.int32(4),
+                               max_njets = cms.int32(100000),
+                               min_nbtags = cms.int32(0),
+                               min_sum_ht = cms.double(0),
+                               min_nleptons = cms.int32(0),
+                               vertex_src = cms.InputTag('mfvSelectedVerticesTight'),
+                               min_nvertex = cms.int32(2),
+                               min_ntracks01 = cms.int32(15),
+                               min_maxtrackpt01 = cms.int32(15),
+                               )
