4735462110de9965179ad4285667f6e3b62e892f
tucker <>
2013-10-16 20:34:57 +0000

    Submit script.

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -1,15 +1,27 @@
+import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
-process.setName_('duh')
 
-process.source.fileNames = ['file:ntuple.root']
-process.TFileService.fileName = 'ntuple_histos.root'
+process.source.fileNames = ['/store/user/tucker/MultiJet1Parked/mfvntuple_v8/00b5523718eb71b4bef18c6d45967745/ntuple_100_1_M3M.root']
+process.TFileService.fileName = 'histos.root'
 
 process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
-process.mfvSelectedVerticesTight.vertex_aux_src = 'mfvVerticesAux'
-
 process.load('JMTucker.MFVNeutralino.Histos_cff')
 process.mfvVertexHistos        .use_ref = False
 process.mfvVertexHistosNoCuts  .use_ref = False
 process.mfvVertexHistosWAnaCuts.use_ref = False
 
 process.p = cms.Path(process.mfvSelectedVerticesTight * process.mfvHistos)
+
+if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
+    import JMTucker.Tools.Samples as Samples
+    samples = Samples.data_samples + Samples.mfv_signal_samples + Samples.background_samples + Samples.smaller_background_samples
+
+    from JMTucker.Tools.CRABSubmitter import CRABSubmitter
+    cs = CRABSubmitter('MFVHistos',
+                       #job_control_from_sample = True,
+                       total_number_of_events = -1,
+                       events_per_job = 1000000,
+                       use_ana_dataset = True,
+                       USER_jmt_skip_input_files = 'src/EGamma/EGammaAnalysisTools/data/*',
+                       )
+    cs.submit_all(samples[:1])
