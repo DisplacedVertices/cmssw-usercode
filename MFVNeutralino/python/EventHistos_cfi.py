30f4c0638eb7da3c6d95ff23df6b9ae3f829bca5
tucker <>
2013-10-18 20:50:58 +0000

    weight_src.

--- MFVNeutralino/python/EventHistos_cfi.py
+++ MFVNeutralino/python/EventHistos_cfi.py
@@ -2,4 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 mfvEventHistos = cms.EDAnalyzer('MFVEventHistos',
                                  mfv_event_src = cms.InputTag('mfvEvent'),
+                                 weight_src = cms.InputTag('mfvWeight'),
                                  )
--- MFVNeutralino/python/VertexHistos_cfi.py
+++ MFVNeutralino/python/VertexHistos_cfi.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 mfvVertexHistos = cms.EDAnalyzer('MFVVertexHistos',
                                  mfv_event_src = cms.InputTag('mfvEvent'),
                                  vertex_aux_src = cms.InputTag('mfvSelectedVerticesTight'),
+                                 weight_src = cms.InputTag('mfvWeight'),
                                  use_ref = cms.bool(True),
                                  do_scatterplots = cms.bool(False),
                                  )
