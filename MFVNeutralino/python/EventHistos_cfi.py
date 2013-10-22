3b23996b9cd2511eff6e1880ceed1100c6c8e158
tucker <>
2013-10-22 21:23:39 +0000

    re_trigger.

--- MFVNeutralino/python/EventHistos_cfi.py
+++ MFVNeutralino/python/EventHistos_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 mfvEventHistos = cms.EDAnalyzer('MFVEventHistos',
-                                 mfv_event_src = cms.InputTag('mfvEvent'),
-                                 weight_src = cms.InputTag('mfvWeight'),
-                                 )
+                                mfv_event_src = cms.InputTag('mfvEvent'),
+                                weight_src = cms.InputTag('mfvWeight'),
+                                re_trigger = cms.bool(False),
+                                )
