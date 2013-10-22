2efd32e230edc4596cdda79b7d42b69342aa997a
tucker <>
2013-10-22 21:31:06 +0000

    Use set_test_files; try muon+jets control region.

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -2,7 +2,8 @@ import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
 import JMTucker.MFVNeutralino.TestFiles as TestFiles
 
-process.source.fileNames = TestFiles.qcdht1000_nt
+process.options.wantSummary = True
+TestFiles.set_test_files(process)
 process.TFileService.fileName = 'histos.root'
 
 process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
@@ -12,13 +13,21 @@ process.mfvVertexHistosNoCuts  .use_ref = False
 process.mfvVertexHistosWAnaCuts.use_ref = False
 process.mfvVertexHistosNoCutsWAnaCuts.use_ref = False
 
-process.mfvAnalysisCuts.min_nvertex = 0
-process.mfvAnalysisCuts.min_ntracks01 = 0
-process.mfvAnalysisCuts.min_maxtrackpt01 = 0
+control_region = True
+if control_region:
+    process.mfvAnalysisCuts.min_nvertex = 0
+    process.mfvAnalysisCuts.min_ntracks01 = 0
+    process.mfvAnalysisCuts.min_maxtrackpt01 = 0
 
-process.mfvAnalysisCuts.min_4th_jet_pt = 0
-process.mfvAnalysisCuts.min_njets = 0
-process.mfvAnalysisCuts.max_njets = 3
+    process.mfvAnalysisCuts.min_4th_jet_pt = 0
+    process.mfvAnalysisCuts.min_njets = 0
+    process.mfvAnalysisCuts.max_njets = 3
+
+    process.mfvAnalysisCuts     .re_trigger = True # JMTBAD make an "EventRedoer"
+    process.mfvEventHistosNoCuts.re_trigger = True
+    process.mfvEventHistos      .re_trigger = True
+    process.mfvAnalysisCuts.trigger_bit = 4
+    process.mfvAnalysisCuts.min_nmuons = 1
 
 process.p = cms.Path(process.mfvSelectedVerticesTight * process.mfvHistos)
 
