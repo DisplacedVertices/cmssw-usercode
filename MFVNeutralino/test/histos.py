2c77582f7d1e1498004dfb5cda702bf23d90d2ac
tucker <>
2013-10-23 17:41:14 +0000

    Submit script for singlemu.

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -13,7 +13,8 @@ process.mfvVertexHistosNoCuts  .use_ref = False
 process.mfvVertexHistosWAnaCuts.use_ref = False
 process.mfvVertexHistosNoCutsWAnaCuts.use_ref = False
 
-control_region = True
+control_region = 'SemimuLE3jets'
+
 if control_region:
     process.mfvAnalysisCuts.min_nvertex = 0
     process.mfvAnalysisCuts.min_ntracks01 = 0
@@ -23,9 +24,8 @@ if control_region:
     process.mfvAnalysisCuts.min_njets = 0
     process.mfvAnalysisCuts.max_njets = 3
 
-    process.mfvAnalysisCuts     .re_trigger = True # JMTBAD make an "EventRedoer"
-    process.mfvEventHistosNoCuts.re_trigger = True
-    process.mfvEventHistos      .re_trigger = True
+    from JMTucker.MFVNeutralino.Histos_cff import re_trigger
+    re_trigger(process)
     process.mfvAnalysisCuts.trigger_bit = 4
     process.mfvAnalysisCuts.min_nsemilepmuons = 1
 
@@ -33,12 +33,16 @@ process.p = cms.Path(process.mfvSelectedVerticesTight * process.mfvHistos)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
-    samples = Samples.data_samples + Samples.mfv_signal_samples + Samples.background_samples + Samples.smaller_background_samples
+    samples = Samples.mfv_signal_samples + Samples.ttbar_samples + Samples.smaller_background_samples
+    if 'mu' in control_region:
+        samples += [Samples.SingleMu2012B] + Samples.leptonic_background_samples
+    else:
+        samples += [Samples.MultiJetPk2012B] + Samples.qcd_samples
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
-    cs = CRABSubmitter('MFVHistos',
+    cs = CRABSubmitter('MFVHistos' + control_region,
                        job_control_from_sample = True,
                        use_ana_dataset = True,
-                       USER_jmt_skip_input_files = 'src/EGamma/EGammaAnalysisTools/data/*',
+                       use_parent = process.mfvAnalysisCuts.re_trigger.value(),
                        )
     cs.submit_all(samples)
