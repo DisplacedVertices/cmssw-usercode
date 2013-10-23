8a126d3ed18ef3ab7fb4c0d6563c947a608c51ac
tucker <>
2013-10-23 19:36:25 +0000

    {min,max} x {rho,r} x {0,1,bigger,smaller}.

--- MFVNeutralino/plugins/GenParticleFilter.cc
+++ MFVNeutralino/plugins/GenParticleFilter.cc
@@ -24,6 +24,18 @@ private:
   const double max_rho0;
   const double min_rho1;
   const double max_rho1;
+  const double min_r0;
+  const double max_r0;
+  const double min_r1;
+  const double max_r1;
+  const double min_rhobigger;
+  const double max_rhobigger;
+  const double min_rhosmaller;
+  const double max_rhosmaller;
+  const double min_rbigger;
+  const double max_rbigger;
+  const double min_rsmaller;
+  const double max_rsmaller;
   bool mci_warned;
 
   bool cut_lepton(const reco::Candidate* lep) const {
@@ -43,6 +55,18 @@ MFVGenParticleFilter::MFVGenParticleFilter(const edm::ParameterSet& cfg)
     max_rho0(cfg.getParameter<double>("max_rho0")),
     min_rho1(cfg.getParameter<double>("min_rho1")),
     max_rho1(cfg.getParameter<double>("max_rho1")),
+    min_r0(cfg.getParameter<double>("min_r0")),
+    max_r0(cfg.getParameter<double>("max_r0")),
+    min_r1(cfg.getParameter<double>("min_r1")),
+    max_r1(cfg.getParameter<double>("max_r1")),
+    min_rhobigger(cfg.getParameter<double>("min_rhobigger")),
+    max_rhobigger(cfg.getParameter<double>("max_rhobigger")),
+    min_rhosmaller(cfg.getParameter<double>("min_rhosmaller")),
+    max_rhosmaller(cfg.getParameter<double>("max_rhosmaller")),
+    min_rbigger(cfg.getParameter<double>("min_rbigger")),
+    max_rbigger(cfg.getParameter<double>("max_rbigger")),
+    min_rsmaller(cfg.getParameter<double>("min_rsmaller")),
+    max_rsmaller(cfg.getParameter<double>("max_rsmaller")),
     mci_warned(false)
 {
 }
@@ -51,6 +75,11 @@ namespace {
   template <typename T>
   T mag(T x, T y) {
     return sqrt(x*x + y*y);
+  }
+
+  template <typename T>
+  T mag(T x, T y, T z) {
+    return sqrt(x*x + y*y + z*z);
   }
 }
 
@@ -92,13 +121,37 @@ bool MFVGenParticleFilter::filter(edm::Event& event, const edm::EventSetup&) {
   const double rho0 = mag(mci.stranges[0]->vx() - mci.lsps[0]->vx(), mci.stranges[0]->vy() - mci.lsps[0]->vy());
   const double rho1 = mag(mci.stranges[1]->vx() - mci.lsps[1]->vx(), mci.stranges[1]->vy() - mci.lsps[1]->vy());
 
-  if (min_rho0 > 0 && rho0 < min_rho0)
+  if ((min_rho0 > 0 && rho0 < min_rho0) ||
+      (max_rho0 > 0 && rho0 > max_rho0) ||
+      (min_rho1 > 0 && rho1 < min_rho1) ||
+      (max_rho1 > 0 && rho1 > max_rho1))
     return false;
-  if (max_rho0 > 0 && rho0 > max_rho0)
+
+  const double rhobigger  = rho0 < rho1 ? rho1 : rho0;
+  const double rhosmaller = rho0 < rho1 ? rho0 : rho1;
+
+  if ((min_rhobigger > 0 && rhobigger < min_rhobigger)    ||
+      (max_rhobigger > 0 && rhobigger > max_rhobigger)    ||
+      (min_rhosmaller > 0 && rhosmaller < min_rhosmaller) ||
+      (max_rhosmaller > 0 && rhosmaller > max_rhosmaller))
     return false;
-  if (min_rho1 > 0 && rho1 < min_rho1)
+
+  const double r0 = mag(mci.stranges[0]->vx() - mci.lsps[0]->vx(), mci.stranges[0]->vy() - mci.lsps[0]->vy(), mci.stranges[0]->vz() - mci.lsps[0]->vz());
+  const double r1 = mag(mci.stranges[1]->vx() - mci.lsps[1]->vx(), mci.stranges[1]->vy() - mci.lsps[1]->vy(), mci.stranges[1]->vz() - mci.lsps[1]->vz());
+
+  if ((min_r0 > 0 && r0 < min_r0) ||
+      (max_r0 > 0 && r0 > max_r0) ||
+      (min_r1 > 0 && r1 < min_r1) ||
+      (max_r1 > 0 && r1 > max_r1))
     return false;
-  if (max_rho1 > 0 && rho1 > max_rho1)
+
+  const double rbigger  = r0 < r1 ? r1 : r0;
+  const double rsmaller = r0 < r1 ? r0 : r1;
+
+  if ((min_rbigger > 0 && rbigger < min_rbigger)    ||
+      (max_rbigger > 0 && rbigger > max_rbigger)    ||
+      (min_rsmaller > 0 && rsmaller < min_rsmaller) ||
+      (max_rsmaller > 0 && rsmaller > max_rsmaller))
     return false;
 
   return true;
