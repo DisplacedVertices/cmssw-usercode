dabb861e98f4170f32c03402c51c9eaef862a6de
jchu <>
2013-10-25 18:55:26 +0000

    Apply analysis cuts and run over half of ttbar and qcd samples.

--- MFVNeutralino/test/abcd_histos.py
+++ MFVNeutralino/test/abcd_histos.py
@@ -1,3 +1,4 @@
+import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
 import JMTucker.MFVNeutralino.TestFiles as TestFiles
 
@@ -7,19 +8,31 @@ process.TFileService.fileName = 'abcd_histos.root'
 process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
 process.mfvSelectedVerticesTight.min_ntracks = 5
 process.mfvSelectedVerticesTight.min_maxtrackpt = 0
+
+process.load('JMTucker.MFVNeutralino.AnalysisCuts_cfi')
+process.mfvAnalysisCuts.min_ntracks01 = 0
+process.mfvAnalysisCuts.min_maxtrackpt01 = 0
+
 process.abcdHistos = cms.EDAnalyzer('ABCDHistos',
                                     vertex_src = cms.InputTag('mfvSelectedVerticesTight')
                                     )
 
-process.p = cms.Path(process.mfvSelectedVerticesTight * process.abcdHistos)
+process.p = cms.Path(process.mfvSelectedVerticesTight * process.mfvAnalysisCuts * process.abcdHistos)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
-    import JMTucker.Tools.Samples as Samples
-    samples = [Samples.qcdht1000]
-    Samples.qcdht1000.total_events /= 2
+    from JMTucker.Tools.Samples import ttbar_samples, qcd_samples
+    samples = ttbar_samples + qcd_samples
+    for sample in samples:
+        sample.total_events = {'ttbarhadronic': 5268722,
+                               'ttbarsemilep':  12674909,
+                               'ttbardilep':    6059506,
+                               'qcdht0100':     25064759,
+                               'qcdht0250':     13531039,
+                               'qcdht0500':     15274646,
+                               'qcdht1000':     6034431}[sample.name]
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
-    cs = CRABSubmitter('ABCDHistos',
+    cs = CRABSubmitter('ABCDHistosHalf',
                        job_control_from_sample = True,
                        use_ana_dataset = True,
                        )
