47da0907f5f13cfbf27ed7aa8d939ca602898618
Jordan Tucker <jordantucker@gmail.com>
2013-11-21 13:45:43 -0600

    numevents_in_dataset understands ana01/2.

--- Tools/python/DBS.py
+++ Tools/python/DBS.py
@@ -15,8 +15,11 @@ def files_in_dataset(dataset, ana01=False, ana02=False):
         raise RuntimeError('no files for %s (ana01: %s ana02: %s) found. dbs command output:\n' % (dataset, ana01, ana02) + ''.join(cmdout))
     return ret
 
-def numevents_in_dataset(dataset):
-    cmd = 'dbs search --query "find sum(file.numevents) where dataset=%(dataset)s"' % locals()
+def numevents_in_dataset(dataset, ana01=False, ana02=False):
+    url = '--url https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_01_writer/servlet/DBSServlet' if (ana01 or ana02) else ''
+    if ana02:
+        url = url.replace('01', '02')
+    cmd = 'dbs search %(url)s --query "find sum(file.numevents) where dataset=%(dataset)s"' % locals()
     cmdout = os.popen(cmd).readlines()
     ret = None
     for line in cmdout:
