0a706b1031d6011203a69cc7d045454cc8089871
Jordan Tucker <jordantucker@gmail.com>
2013-11-23 14:21:07 -0600

    Use Loose selected vertices for matching jets.

--- MFVNeutralino/python/Vertexer_cff.py
+++ MFVNeutralino/python/Vertexer_cff.py
@@ -3,16 +3,16 @@ import FWCore.ParameterSet.Config as cms
 from JMTucker.MFVNeutralino.Vertexer_cfi import mfvVertices
 from JMTucker.MFVNeutralino.GenParticles_cff import mfvGenVertices
 from JMTucker.MFVNeutralino.VertexAuxProducer_cfi import mfvVerticesAuxTmp, mfvVerticesAux
-from JMTucker.MFVNeutralino.VertexSelector_cfi import mfvSelectedVertices, mfvSelectedVerticesTight
+from JMTucker.MFVNeutralino.VertexSelector_cfi import mfvSelectedVerticesLoose, mfvSelectedVerticesTight
 from JMTucker.MFVNeutralino.JetVertexAssociator_cfi import mfvVerticesToJets
 
-mfvSelectedVerticesTightTmp = mfvSelectedVerticesTight.clone(vertex_aux_src = 'mfvVerticesAuxTmp',
-                                                             produce_refs = True)
+mfvSelectedVerticesTmp = mfvSelectedVerticesLoose.clone(vertex_aux_src = 'mfvVerticesAuxTmp',
+                                                        produce_refs = True)
 
 mfvVertexSequence = cms.Sequence(mfvVertices *
                                  mfvGenVertices *
                                  mfvVerticesAuxTmp *
-                                 mfvSelectedVerticesTightTmp *
+                                 mfvSelectedVerticesTmp *
                                  mfvVerticesToJets *
                                  mfvVerticesAux *
                                  mfvSelectedVerticesTight)
