016281b6691afa0f19709f6a43945191dd6841f0
Jordan Tucker <jordantucker@gmail.com>
2013-12-02 11:28:52 -0600

    V11.

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -2,8 +2,7 @@ import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
 from JMTucker.Tools import SampleFiles
 
-process.options.wantSummary = True
-SampleFiles.set(process, 'MFVNtupleV10_testVtxSeedSum', 'mfv_neutralino_tau1000um_M0400', 500)
+SampleFiles.set(process, 'MFVNtupleV11', 'mfv_neutralino_tau1000um_M0400', 500)
 process.TFileService.fileName = 'histos.root'
 
 process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
@@ -22,9 +21,9 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     from JMTucker.Tools.SampleFiles import SampleFiles
 
-    cs = CRABSubmitter('MFVHistosV10_testVtxSeedSum',
+    cs = CRABSubmitter('MFVHistosV11',
                        total_number_of_events = -1,
-                       events_per_job = 25000,
-                       manual_datasets = SampleFiles['MFVNtupleV10_testVtxSeedSum'],
+                       events_per_job = 200000,
+                       manual_datasets = SampleFiles['MFVNtupleV11'],
                        )
     cs.submit_all(samples)
