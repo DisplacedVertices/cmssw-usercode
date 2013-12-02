8bcaadfe65e67aa31bc678a691323e4cd5e2ab5f
Jordan Tucker <jordantucker@gmail.com>
2013-12-01 20:31:16 -0600

    Use manual_datasets option.

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -1,9 +1,9 @@
 import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
-import JMTucker.MFVNeutralino.TestFiles as TestFiles
+from JMTucker.Tools.SampleFiles import SampleFiles
 
 process.options.wantSummary = True
-TestFiles.set_test_files(process)
+process.source.fileNames = SampleFiles['MFVHistosV10_testVtxSeedSum']['mfv_neutralino_tau1000um_M0400'][:5]
 process.TFileService.fileName = 'histos.root'
 
 process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
@@ -11,13 +11,20 @@ process.load('JMTucker.MFVNeutralino.Histos_cff')
 
 process.p = cms.Path(process.mfvSelectedVerticesSeq * process.mfvHistos)
 
+process.eid = cms.EDAnalyzer('EventIdRecorder')
+process.peid = cms.Path(process.eid)
+
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
-    samples = Samples.mfv_signal_samples + Samples.ttbar_samples + Samples.qcd_samples
+    samples = Samples.ttbar_samples + Samples.qcd_samples
+    samples += [Samples.mfv_neutralino_tau0100um_M0400, Samples.mfv_neutralino_tau1000um_M0400, Samples.mfv_neutralino_tau9900um_M0400]
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
-    cs = CRABSubmitter('MFVHistosMC',
-                       job_control_from_sample = True,
-                       use_ana_dataset = True,
+    from JMTucker.Tools.SampleFiles import SampleFiles
+
+    cs = CRABSubmitter('MFVHistos_testVtxSeedSum',
+                       total_number_of_events = -1,
+                       events_per_job = 25000,
+                       manual_datasets = SampleFiles['MFVNtupleV10_testVtxSeedSum'],
                        )
     cs.submit_all(samples)
