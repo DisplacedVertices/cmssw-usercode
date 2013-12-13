76e2130f870f9343adb210e2cbede62f0874ca10
Jennifer Chu <jyc72@cornell.edu>
2013-12-13 10:52:28 -0600

    Run ABCD with cuts from CutPlayV11_4, and use version11 ntuples.

--- MFVNeutralino/test/abcd_histos.py
+++ MFVNeutralino/test/abcd_histos.py
@@ -1,16 +1,25 @@
 import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
-import JMTucker.MFVNeutralino.TestFiles as TestFiles
+from JMTucker.Tools import SampleFiles
 
-process.source.fileNames = TestFiles.qcdht1000_nt
+SampleFiles.set(process, 'MFVNtupleV11', 'mfv_neutralino_tau0100um_M0400', 500)
 process.TFileService.fileName = 'abcd_histos.root'
 
 process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
-process.mfvSelectedVerticesTight.min_njetssharetks = 2
+process.mfvSelectedVerticesTight.min_ntracks = 5
+process.mfvSelectedVerticesTight.min_maxtrackpt = 4
+process.mfvSelectedVerticesTight.max_drmin = 0.5
+process.mfvSelectedVerticesTight.max_drmax = 4.5
+process.mfvSelectedVerticesTight.min_bs2dsig = 4.5
+process.mfvSelectedVerticesTight.min_njetsntks = 1
+process.mfvSelectedVerticesTight.min_sumpt2 = 40
 
 process.load('JMTucker.MFVNeutralino.AnalysisCuts_cfi')
 process.mfvAnalysisCuts.min_ntracks01 = 0
 process.mfvAnalysisCuts.min_maxtrackpt01 = 0
+process.mfvAnalysisCuts.min_jetsntkmass01 = 60
+process.mfvAnalysisCuts.min_njetsntks01 = 3
+process.mfvAnalysisCuts.min_sumht = 600
 
 process.abcdHistos = cms.EDAnalyzer('ABCDHistos',
                                     vertex_src = cms.InputTag('mfvSelectedVerticesTight')
@@ -21,18 +30,15 @@ process.p = cms.Path(process.mfvSelectedVerticesTight * process.mfvAnalysisCuts 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.Samples import mfv_signal_samples, ttbar_samples, qcd_samples
     for sample in ttbar_samples + qcd_samples:
-        sample.total_events = {'ttbarhadronic': 5268722,
-                               'ttbarsemilep':  12674909,
-                               'ttbardilep':    6059506,
-                               'qcdht0100':     25064759,
-                               'qcdht0250':     13531039,
-                               'qcdht0500':     15274646,
-                               'qcdht1000':     6034431}[sample.name]
+        sample.total_events = sample.nevents_orig/2
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
+    from JMTucker.Tools.SampleFiles import SampleFiles
+
     cs = CRABSubmitter('ABCDHistos',
                        job_control_from_sample = True,
                        use_ana_dataset = True,
+                       manual_datasets = SampleFiles['MFVNtupleV11'],
                        )
     samples = mfv_signal_samples + ttbar_samples + qcd_samples
     cs.submit_all(samples)
