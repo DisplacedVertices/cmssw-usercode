ca921be6b24103b661a5e8a346ec26ea87768d8b
Jordan Tucker <jordantucker@gmail.com>
2014-02-02 21:55:06 -0600

    Run on everything.

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -17,12 +17,15 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     samples = Samples.ttbar_samples + Samples.qcd_samples + Samples.leptonic_background_samples
     samples += [Samples.mfv_neutralino_tau0100um_M0400, Samples.mfv_neutralino_tau1000um_M0400, Samples.mfv_neutralino_tau9900um_M0400]
 
+    samples = Samples.ttbar_samples + Samples.qcd_samples + Samples.leptonic_background_samples + Samples.smaller_background_samples + \
+              [sample for sample in Samples.mfv_signal_samples if '0300' not in sample.name]
+    
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     from JMTucker.Tools.SampleFiles import SampleFiles
 
     cs = CRABSubmitter('MFVHistosV15',
-                       total_number_of_events = 1,
-                       events_per_job = 1,
+                       total_number_of_events = -1,
+                       events_per_job = 200000,
                        manual_datasets = SampleFiles['MFVNtupleV15'],
                        )
-    cs.submit_all([Samples.MultiJetPk2012B])
+    cs.submit_all(samples)
