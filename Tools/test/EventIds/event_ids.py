0dc4ce7a2d0bc35f4de91cc6ca11b71d27a1157a
Jordan Tucker <jordantucker@gmail.com>
2014-02-03 18:55:14 -0600

    V15.

--- Tools/test/EventIds/event_ids.py
+++ Tools/test/EventIds/event_ids.py
@@ -2,7 +2,7 @@ import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
 
 from JMTucker.Tools import SampleFiles
-SampleFiles.setup(process, 'MFVNtupleV13', 'mfv_neutralino_tau1000um_M0400', 500)
+SampleFiles.setup(process, 'MFVNtupleV15', 'mfv_neutralino_tau1000um_M0400', 500)
 
 process.TFileService.fileName = cms.string('evids.root')
 
@@ -14,23 +14,14 @@ process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
 
-    x = Samples.mfv_signal_samples
-    Samples.mfv_signal_samples = []
-    Samples.mfv300 = []
-    for y in x:
-        if '300' in y.name:
-            Samples.mfv300.append(y)
-        else:
-            Samples.mfv_signal_samples.append(y)
-
-    samples = Samples.ttbar_samples + Samples.qcd_samples + Samples.mfv_signal_samples + Samples.leptonic_background_samples
+    samples = Samples.ttbar_samples + Samples.qcd_samples + Samples.mfv_signal_samples + Samples.leptonic_background_samples + Samples.data_samples[:1]
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     from JMTucker.Tools.SampleFiles import SampleFiles
 
-    cs = CRABSubmitter('EventIdsV13',
+    cs = CRABSubmitter('EventIdsV15',
                        total_number_of_events = -1,
                        events_per_job = 234567,
-                       manual_datasets = SampleFiles['MFVNtupleV13'],
+                       manual_datasets = SampleFiles['MFVNtupleV15'],
                        )
     cs.submit_all(samples)
