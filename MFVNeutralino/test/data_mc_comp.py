69561138fbffbfc10e2609f95f0d710001c1c4f6
Jordan Tucker <jordantucker@gmail.com>
2014-02-06 14:05:51 -0600

    latest

--- MFVNeutralino/test/data_mc_comp.py
+++ MFVNeutralino/test/data_mc_comp.py
@@ -5,8 +5,9 @@ plot_dir = 'plots/MFVDataMCCompV15'
 plot_size = (600,600)
 int_lumi = 950. # /pb
 int_lumi_nice = '0.95 fb^{-1}'
-scale_factor = 720/768.
+scale_factor = 1. #720/768.
 root_file_dir = 'crab/MFVHistosV15_looser'
+root_file_dir = 'crab/MFVHistosV15_looser_fixnm1'
 hist_path_for_nevents_check = None # 'mfvEventHistosNoCuts/h_npu',
 
 ################################################################################
@@ -60,25 +61,14 @@ C = partial(data_mc_comparison,
 
 legend_pos = (0.435, 0.687, 0.878, 0.920)
 
-if 1:
-    C('sv_top2_ntracks_nm1',
-      histogram_path = 'hstNoNtracks/h_sv_top2_ntracks',
-      x_title = 'number of tracks/vertex',
-      y_title = 'vertices',
-#      x_range = (5, 20),
-      legend_pos = legend_pos
-      )
-
-raise 1
-
 if 0:
     for i in xrange(20):
         C('clean%i' % i,
           histogram_path = 'mfvEventHistos/h_pass_clean_%i' % i,
-          legend_pos = legend_pos
+          legend_pos = (0.435, 0.687, 0.878, 0.920)
           )
 
-if 1:
+if 0:
     C('npv',
       histogram_path = 'mfvEventHistos/h_npv',
       x_title = 'number of PV',
@@ -86,169 +76,225 @@ if 1:
       x_range = (0, 40),
       y_range = (None, 140),
       rebin = 2,
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('pvntracks',
       histogram_path = 'mfvEventHistos/h_pv_ntracks',
       rebin = 10,
       x_title = 'number of tracks in PV',
       y_title = 'events/10',
       y_range = (None, 250),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('pvsumpt2',
       histogram_path = 'mfvEventHistos/h_pv_sumpt2',
       rebin = 10,
       x_title = '#Sigma p_{T}^{2} of tracks in PV (GeV^{2})',
       y_title = 'events/1000 GeV^{2}',
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('pvrho',
       histogram_path = 'mfvEventHistos/h_pv_rho',
       x_title = 'PV #rho (cm)',
       y_title = 'events/0.01 cm',
       x_range = (0, 0.1),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('pvz',
       histogram_path = 'mfvEventHistos/h_pvz',
       x_title = 'PV z (cm)',
       y_title = 'events/cm',
       rebin = 10,
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('njets',
       histogram_path = 'mfvEventHistos/h_njets',
       x_title = 'number of jets',
       y_title = 'events',
       x_range = (4,16),
       y_range = (None, 230),
-      legend_pos = legend_pos,
+      legend_pos = (0.572, 0.687, 0.884, 0.920),
       )
 
-if 1:
+if 0:
     C('njetsnoputight',
       histogram_path = 'mfvEventHistos/h_njetsnopu_tight',
       x_title = 'number of jets (tight PU id)',
       y_title = 'events',
       x_range = (4,16),
       y_range = (None, 230),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('jetpt4',
       histogram_path = 'mfvEventHistos/h_jetpt4',
       x_title = 'jet #4 p_{T} (GeV)',
       y_title = 'events/5 GeV',
       x_range = (60,250),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('jetpt5',
       histogram_path = 'mfvEventHistos/h_jetpt5',
       x_title = 'jet #5 p_{T} (GeV)',
       y_title = 'events/5 GeV',
       x_range = (60,250),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('jetpt6',
       histogram_path = 'mfvEventHistos/h_jetpt6',
       x_title = 'jet #6 p_{T} (GeV)',
       y_title = 'events/5 GeV',
       x_range = (60,250),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('jetsumht',
       histogram_path = 'mfvEventHistos/h_jet_sum_ht',
       rebin = 4,
       x_title = '#Sigma H_{T} (GeV)',
       y_title = 'events/100 GeV',
       x_range = (500, 3000),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('nbtags',
       histogram_path = 'mfvEventHistos/h_nbtags_medium',
       x_title = '# CSVM b-tagged jets',
       y_title = 'events',
       x_range = (0,10),
       y_range = (None, 6e2),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('nsemimuons',
       histogram_path = 'mfvEventHistos/h_nmuons_semilep',
       x_title = 'number of semilep muons',
       y_title = 'events',
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('nsemielectrons',
       histogram_path = 'mfvEventHistos/h_nelectrons_semilep',
       x_title = 'number of semilep electrons',
       y_title = 'events',
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('nsemileptons',
       histogram_path = 'mfvEventHistos/h_nleptons_semilep',
       x_title = 'number of semilep electrons',
       y_title = 'events',
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('nsvnocut',
       histogram_path = 'mfvVertexHistosTrigCut/h_nsv',
       x_title = 'number of SV',
       y_title = 'events',
       x_range = (0,5),
       y_range = (1, 1e7),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('nsv',
       histogram_path = 'mfvVertexHistosWAnaCuts/h_nsv',
       x_title = 'number of SV',
       y_title = 'events',
       x_range = (2,5),
       y_range = (None, 1e3),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
     C('sv_top2_ntracks',
       histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_ntracks',
       x_title = 'number of tracks/vertex',
       y_title = 'vertices',
       x_range = (5, 20),
-      legend_pos = legend_pos
+      legend_pos = (0.435, 0.687, 0.878, 0.920)
       )
 
-
+if 0:
+    C('sv_top2_ntracksptgt3',
+      histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_ntracksptgt3',
+      x_title = 'number of tracks with p_{T} > 3 GeV/vertex',
+      y_title = 'vertices',
+      x_range = (1, 10),
+      y_range = (None, 700),
+      legend_pos = (0.553, 0.687, 0.878, 0.920)
+      )
 
 if 1:
+    C('sv_top2_ntracksptgt3_nm1',
+      histogram_path = 'hstNoNtracksptgt3/h_sv_top2_ntracksptgt3',
+      x_title = 'number of tracks with p_{T} > 3 GeV/vertex',
+      y_title = 'vertices',
+      x_range = (0, 10),
+    #  y_range = (None, 700),
+      legend_pos = (0.553, 0.687, 0.878, 0.920)
+      )
+
+if 0:
+    C('sv_top2_chi2dof',
+      histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_chi2dof',
+      x_title = '#chi^{2}/dof for vertex fit',
+      y_title = 'vertices/0.14',
+      x_range = (0, 5),
+      y_range = (None, 180),
+      legend_pos = (0.435, 0.687, 0.878, 0.920)
+      )
+
+if 0:
+    C('sv_top2_chi2dofprob',
+      histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_chi2dofprob',
+      x_title = 'P(#chi^{2},dof) for vertex fit',
+      y_title = 'vertices/0.024',
+      x_range = (0,1),
+      legend_pos = (0.435, 0.687, 0.878, 0.920)
+      )
+
+if 0:
+    C('sv_top2_tkonlyp',
+      histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_tkonlyp',
+      rebin = 4,
+      x_title = 'track-only vertex momentum (GeV)',
+      y_title = 'vertices/12 GeV',
+      y_range = (None, 450),
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
+      )
+
+if 0:
+    C('sv_top2_tkonlypt',
+      histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_tkonlypt',
+      rebin = 4,
+      x_title = 'track-only vertex p_{T} (GeV)',
+      y_title = 'vertices/12 GeV',
+      legend_pos = (0.570, 0.748, 0.878, 0.921),
+      )
+
+if 0:
     C('sv_top2_tkonlymass',
       histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_tkonlymass',
       rebin = 4,
@@ -257,75 +303,177 @@ if 1:
       legend_pos = (0.45, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
+    C('sv_top2_jetsntkmass',
+      histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_jetsntkmass',
+      rebin = 4,
+      x_title = 'associated jets\' mass (GeV)',
+      y_title = 'vertices/60 GeV',
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
+      )
+
+if 0:
+    C('sv_top2_tksjetsntkmass',
+      histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_tksjetsntkmass',
+      rebin = 4,
+      x_title = 'tracks + associated jets\' mass (GeV)',
+      y_title = 'vertices/60 GeV',
+      y_range = (None, 500),
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
+      )
+
+if 0:
+    C('sv_top2_costhtkonlymombs',
+      histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_costhtkonlymombs',
+      rebin = 4,
+      y_range = (1e-4, 270),
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
+      )
+
+if 0:
     C('sv_top2_maxtrackpt',
       histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_maxtrackpt',
       rebin = 4,
       x_title = 'max{track p_{T,i}} (GeV)',
       y_title = 'vertices/6 GeV',
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
-if 1:
+if 0:
+    C('sv_top2_sumpt2',
+      histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_sumpt2',
+      rebin = 10,
+      x_title = '#Sigma p_{T}^{2} (GeV^{2})',
+      y_title = 'vertices/200 GeV^{2}',
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
+      )
+
+if 0:
     C('sv_top2_drmin',
       histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_drmin',
-      #rebin = 10,
+      rebin = 2,
       x_title = 'min{#Delta R{track i,j}}',
-      y_title = 'vertices/0.1',
+      y_title = 'vertices/0.02',
       x_range = (0, 0.4),
       #y_range = (1e-1, 2e2),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
 if 1:
+    C('sv_top2_drmin_nm1',
+      histogram_path = 'hstNoDrmin/h_sv_top2_drmin',
+      x_title = 'min{#Delta R{track i,j}}',
+      y_title = 'vertices/0.05',
+      rebin = 5,
+      #x_range = (0, 0.4),
+      #y_range = (1e-1, 2e2),
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
+      )
+
+if 0:
     C('sv_top2_drmax',
       histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_drmax',
-      #rebin = 10,
+      rebin = 3,
       x_title = 'max{#Delta R{track i,j}} for two "best" SV (GeV)',
-      y_title = 'events/0.47',
-      #y_range = (1e-1, 2e2),
-      legend_pos = legend_pos,
+      y_title = 'vertices/0.14',
+      x_range = (0, 4),
+      legend_pos = (0.135, 0.687, 0.578, 0.920)
       )
 
 if 1:
+    C('sv_top2_drmax_nm1',
+      histogram_path = 'hstNoDrmax/h_sv_top2_drmax',
+      rebin = 3,
+      x_title = 'max{#Delta R{track i,j}} for two "best" SV (GeV)',
+      y_title = 'vertices/0.14',
+#      x_range = (0, 4),
+      legend_pos = (0.135, 0.687, 0.578, 0.920)
+      )
+
+if 0:
     C('sv_top2_njetsntks',
       histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_njetsntks',
-      x_title = 'number of jets assoc. by tracks for two "best" SV',
-      y_title = 'events',
-      #y_range = (1e-1, 2e2),
-      legend_pos = legend_pos,
+      x_title = 'number of associated jets',
+      y_title = 'vertices',
+      x_range = (1, 6),
+      y_range = (None, 1100),
+      legend_pos = (0.589, 0.704, 0.878, 0.921),
       )
 
 if 1:
+    C('sv_top2_njetsntks_nm1',
+      histogram_path = 'hstNoNjets/h_sv_top2_njetsntks',
+      x_title = 'number of associated jets',
+      y_title = 'vertices',
+#      x_range = (1, 6),
+      y_range = (None, 1100),
+      legend_pos = (0.589, 0.704, 0.878, 0.921),
+      )
+
+if 0:
     C('sv_top2_bs2ddist',
       histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_bs2ddist',
-      x_title = '2D distance to BS for two "best" SV (cm)',
-#      #rebin = 10,
-      y_title = 'events/0.005 cm',
-      #x_range = (0, 0.1),
-      #y_range = (1e-1, 2e2),
-      legend_pos = legend_pos,
+      x_title = 'xy distance to beamspot (cm)',
+      y_title = 'vertices/50 #mum',
+      x_range = (0, 0.1),
+      legend_pos = (0.547, 0.755, 0.878, 0.921),
       )
 
-if 1:
+if 0:
     C('sv_top2_bs2derr',
       histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_bs2derr',
-      x_title = '#sigma(2D distance to BS) for two "best" SV (cm)',
-#      #rebin = 10,
-      y_title = 'events/0.0005 cm',
-      #x_range = (0, 0.0075),
-      #y_range = (1e-1, 2e2),
-      legend_pos = legend_pos,
+      x_title = '#sigma(xy distance to beamspot) (cm)',
+      y_title = 'vertices/5 #mum',
+      x_range = (0, 0.008),
+      y_range = (None, 275),
       )
 
 if 1:
+    C('sv_top2_bs2derr_nm1',
+      histogram_path = 'hstNoBs2derr/h_sv_top2_bs2derr',
+      x_title = '#sigma(xy distance to beamspot) (cm)',
+      y_title = 'vertices/25 #mum',
+      rebin = 5,
+#      y_range = (None, 275),
+      )
+
+if 0:
     C('sv_top2_bs2dsig',
       histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_top2_bs2dsig',
       x_title = 'N#sigma(2D distance to BS) for two "best" SV',
-      #rebin = 4,
-      y_title = 'events/4',
-      #x_range = (0, 28),
+      y_title = 'vertices/2',
+      rebin = 2,
+      x_range = (10, 50),
       #y_range = (1e-1, 2e2),
-      legend_pos = legend_pos,
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
       )
 
+if 1:
+    C('sv_top2_bs2dsig_nm1',
+      histogram_path = 'hstNoBs2dsig/h_sv_top2_bs2dsig',
+      x_title = 'N#sigma(2D distance to BS) for two "best" SV',
+      y_title = 'vertices/2',
+#      rebin = 0,
+      x_range = (0, 10),
+      #y_range = (1e-1, 2e2),
+      legend_pos = (0.435, 0.687, 0.878, 0.920),
+      )
+
+if 0:
+    C('sv_sumtop2_ntracks',
+      histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_sumtop2_ntracks',
+      x_title = 'ntracks01',
+      y_title = 'events',
+      x_range = (10, 40),
+      legend_pos = (0.435, 0.687, 0.878, 0.920)
+      )
+
+if 0:
+    C('sv_sumtop2_maxtrackpt',
+      histogram_path = 'mfvVertexHistosWAnaCuts/h_sv_sumtop2_maxtrackpt',
+      x_title = 'maxtrackpt01 (GeV)',
+      y_title = 'events/6 GeV',
+      rebin = 4,
+#      x_range = (0, 40),
+      legend_pos = (0.435, 0.687, 0.878, 0.920)
+      )
