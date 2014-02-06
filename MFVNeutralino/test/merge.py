d3e738093dfe224fa79db9c73ab55eaeea51ce8a
Jordan Tucker <jordantucker@gmail.com>
2014-02-06 14:06:12 -0600

    Tweak submit script.

--- MFVNeutralino/test/merge.py
+++ MFVNeutralino/test/merge.py
@@ -7,18 +7,17 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     import JMTucker.Tools.Samples as Samples
     from JMTucker.Tools.SampleFiles import SampleFiles
-    from JMTucker.Tools.PATTuple_cfg import version as tuple_version
-
-    cs = CRABSubmitter('MFVNtuple' + tuple_version.upper() + 'Merged',
+    version = 'V15'
+    cs = CRABSubmitter('MFVNtuple' + version + 'Merged',
                        total_number_of_events = -1,
-                       events_per_job = 50000,
+                       events_per_job = 100000,
                        get_edm_output = True,
                        data_retrieval = 'fnal_eos',
                        max_threads = 3,
-                       manual_datasets = SampleFiles['MFVNtupleV14'],
+                       manual_datasets = SampleFiles['MFVNtuple' + version],
                        )
 
-    samples = [Samples.dyjetstollM50]
+    samples = Samples.mfv_signal_samples
 
     for sample in samples:
         if sample.is_mc:
