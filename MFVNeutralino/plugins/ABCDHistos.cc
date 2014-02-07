70754ddef2fe8c9cc6ff8e375358b6966eb79ced
Jennifer Chu <jyc72@cornell.edu>
2014-02-07 10:50:56 -0600

    Add lifetime vs. mass plots.

--- MFVNeutralino/plugins/ABCDHistos.cc
+++ MFVNeutralino/plugins/ABCDHistos.cc
@@ -5,6 +5,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "JMTucker/MFVNeutralinoFormats/interface/VertexAux.h"
+#include "JMTucker/Tools/interface/Utilities.h"
 
 class ABCDHistos : public edm::EDAnalyzer {
  public:
@@ -17,6 +18,40 @@ class ABCDHistos : public edm::EDAnalyzer {
   TH1F* h_nsv;
   TH2F* h_ntracks01_maxtrackpt01;
 
+  TH2F* h_bs2ddist0_bs2ddist1;
+  TH2F* h_tkonlymass0_tkonlymass1;
+
+  TH2F* h_bs2ddist01_tkonlymass01;
+  TH2F* h_bs2ddist0_tkonlymass0;
+  TH2F* h_bs2ddist1_tkonlymass1;
+
+  TH2F* h_bs2ddist1_tkonlymass0;
+  TH2F* h_bs2ddist0_tkonlymass1;
+
+  TH2F* h_pv2ddist01_tkonlymass01;
+  TH2F* h_pv2ddist0_tkonlymass0;
+
+  TH2F* h_pv3ddist01_tkonlymass01;
+  TH2F* h_pv3ddist0_tkonlymass0;
+
+  TH2F* h_pv3dtkonlyctau01_tkonlymass01;
+  TH2F* h_pv3dtkonlyctau0_tkonlymass0;
+
+  TH2F* h_pv3djetsntkctau01_tkonlymass01;
+  TH2F* h_pv3djetsntkctau0_tkonlymass0;
+
+  TH2F* h_pv3dtksjetsntkctau01_tkonlymass01;
+  TH2F* h_pv3dtksjetsntkctau0_tkonlymass0;
+
+  TH2F* h_svdist2d_tkonlymass01;
+  TH2F* h_svdist3d_tkonlymass01;
+
+  TH2F* h_ntracks01_tkonlymass01;
+  TH2F* h_maxtrackpt01_tkonlymass01;
+
+  TH2F* h_bs2ddist_ntracks;
+  TH2F* h_bs2derr_ntracks;
+  TH2F* h_bs2dsig_ntracks;
 };
 
 ABCDHistos::ABCDHistos(const edm::ParameterSet& cfg)
@@ -25,6 +60,41 @@ ABCDHistos::ABCDHistos(const edm::ParameterSet& cfg)
   edm::Service<TFileService> fs;
   h_nsv = fs->make<TH1F>("h_nsv", ";number of secondary vertices;events", 15, 0, 15);
   h_ntracks01_maxtrackpt01 = fs->make<TH2F>("h_ntracks01_maxtrackpt01", ";sum of maxtrackpt for the two SV's with the highest ntracks;sum of ntracks for the two SV's with the highest ntracks", 300, 0, 300, 80, 0, 80);
+
+  h_bs2ddist0_bs2ddist1 = fs->make<TH2F>("h_bs2ddist0_bs2ddist1", ";bs2ddist1;bs2ddist0", 50, 0, 0.5, 50, 0, 0.5);
+  h_tkonlymass0_tkonlymass1 = fs->make<TH2F>("h_tkonlymass0_tkonlymass1", ";tkonlymass1;tkonlymass0", 250, 0, 250, 250, 0, 250);
+
+  h_bs2ddist01_tkonlymass01 = fs->make<TH2F>("h_bs2ddist01_tkonlymass01", ";tkonlymass01;bs2ddist01", 500, 0, 500, 100, 0, 1);
+  h_bs2ddist0_tkonlymass0 = fs->make<TH2F>("h_bs2ddist0_tkonlymass0", ";tkonlymass0;bs2ddist0", 250, 0, 250, 50, 0, 0.5);
+  h_bs2ddist1_tkonlymass1 = fs->make<TH2F>("h_bs2ddist1_tkonlymass1", ";tkonlymass1;bs2ddist1", 250, 0, 250, 50, 0, 0.5);
+
+  h_bs2ddist1_tkonlymass0 = fs->make<TH2F>("h_bs2ddist1_tkonlymass0", ";tkonlymass0;bs2ddist1", 250, 0, 250, 50, 0, 0.5);
+  h_bs2ddist0_tkonlymass1 = fs->make<TH2F>("h_bs2ddist0_tkonlymass1", ";tkonlymass1;bs2ddist0", 250, 0, 250, 50, 0, 0.5);
+
+  h_pv2ddist01_tkonlymass01 = fs->make<TH2F>("h_pv2ddist01_tkonlymass01", ";tkonlymass01;pv2ddist01", 500, 0, 500, 100, 0, 1);
+  h_pv2ddist0_tkonlymass0 = fs->make<TH2F>("h_pv2ddist0_tkonlymass0", ";tkonlymass0;pv2ddist0", 250, 0, 250, 50, 0, 0.5);
+
+  h_pv3ddist01_tkonlymass01 = fs->make<TH2F>("h_pv3ddist01_tkonlymass01", ";tkonlymass01;pv3ddist01", 500, 0, 500, 100, 0, 1);
+  h_pv3ddist0_tkonlymass0 = fs->make<TH2F>("h_pv3ddist0_tkonlymass0", ";tkonlymass0;pv3ddist0", 250, 0, 250, 50, 0, 0.5);
+
+  h_pv3dtkonlyctau01_tkonlymass01 = fs->make<TH2F>("h_pv3dtkonlyctau01_tkonlymass01", ";tkonlymass01;pv3dtkonlyctau01", 500, 0, 500, 100, 0, 1);
+  h_pv3dtkonlyctau0_tkonlymass0 = fs->make<TH2F>("h_pv3dtkonlyctau0_tkonlymass0", ";tkonlymass0;pv3dtkonlyctau0", 250, 0, 250, 50, 0, 0.5);
+
+  h_pv3djetsntkctau01_tkonlymass01 = fs->make<TH2F>("h_pv3djetsntkctau01_tkonlymass01", ";tkonlymass01;pv3djetsntkctau01", 500, 0, 500, 100, 0, 1);
+  h_pv3djetsntkctau0_tkonlymass0 = fs->make<TH2F>("h_pv3djetsntkctau0_tkonlymass0", ";tkonlymass0;pv3djetsntkctau0", 250, 0, 250, 50, 0, 0.5);
+
+  h_pv3dtksjetsntkctau01_tkonlymass01 = fs->make<TH2F>("h_pv3dtksjetsntkctau01_tkonlymass01", ";tkonlymass01;pv3dtksjetsntkctau01", 500, 0, 500, 100, 0, 1);
+  h_pv3dtksjetsntkctau0_tkonlymass0 = fs->make<TH2F>("h_pv3dtksjetsntkctau0_tkonlymass0", ";tkonlymass0;pv3dtksjetsntkctau0", 250, 0, 250, 50, 0, 0.5);
+
+  h_svdist2d_tkonlymass01 = fs->make<TH2F>("h_svdist2d_tkonlymass01", ";tkonlymass01;svdist2d", 500, 0, 500, 100, 0, 1);
+  h_svdist3d_tkonlymass01 = fs->make<TH2F>("h_svdist3d_tkonlymass01", ";tkonlymass01;svdist3d", 500, 0, 500, 100, 0, 1);
+
+  h_ntracks01_tkonlymass01 = fs->make<TH2F>("h_ntracks01_tkonlymass01", ";tkonlymass01;ntracks01", 500, 0, 500, 80, 0, 80);
+  h_maxtrackpt01_tkonlymass01 = fs->make<TH2F>("h_maxtrackpt01_tkonlymass01", ";tkonlymass01;maxtrackpt01", 500, 0, 500, 300, 0, 300);
+
+  h_bs2ddist_ntracks = fs->make<TH2F>("h_bs2ddist_ntracks", ";ntracks;bs2ddist", 40, 0, 40, 100, 0, 0.5);
+  h_bs2derr_ntracks = fs->make<TH2F>("h_bs2derr_ntracks", ";ntracks;bs2derr", 40, 0, 40, 100, 0, 0.05);
+  h_bs2dsig_ntracks = fs->make<TH2F>("h_bs2dsig_ntracks", ";ntracks;bs2dsig", 40, 0, 40, 100, 0, 100);
 }
 
 void ABCDHistos::analyze(const edm::Event& event, const edm::EventSetup&) {
@@ -39,6 +109,52 @@ void ABCDHistos::analyze(const edm::Event& event, const edm::EventSetup&) {
     const MFVVertexAux& v0 = vertices->at(0);
     const MFVVertexAux& v1 = vertices->at(1);
     h_ntracks01_maxtrackpt01->Fill(v0.maxtrackpt + v1.maxtrackpt, v0.ntracks + v1.ntracks);
+
+    h_bs2ddist0_bs2ddist1->Fill(v1.bs2ddist, v0.bs2ddist);
+    h_tkonlymass0_tkonlymass1->Fill(v1.mass[mfv::PTracksOnly], v0.mass[mfv::PTracksOnly]);
+
+    h_bs2ddist01_tkonlymass01->Fill(v0.mass[mfv::PTracksOnly] + v1.mass[mfv::PTracksOnly], v0.bs2ddist + v1.bs2ddist);
+    h_bs2ddist0_tkonlymass0->Fill(v0.mass[mfv::PTracksOnly], v0.bs2ddist);
+    h_bs2ddist1_tkonlymass1->Fill(v1.mass[mfv::PTracksOnly], v1.bs2ddist);
+
+    h_bs2ddist1_tkonlymass0->Fill(v0.mass[mfv::PTracksOnly], v1.bs2ddist);
+    h_bs2ddist0_tkonlymass1->Fill(v1.mass[mfv::PTracksOnly], v0.bs2ddist);
+
+    h_pv2ddist01_tkonlymass01->Fill(v0.mass[mfv::PTracksOnly] + v1.mass[mfv::PTracksOnly], v0.pv2ddist + v1.pv2ddist);
+    h_pv2ddist0_tkonlymass0->Fill(v0.mass[mfv::PTracksOnly], v0.pv2ddist);
+
+    h_pv3ddist01_tkonlymass01->Fill(v0.mass[mfv::PTracksOnly] + v1.mass[mfv::PTracksOnly], v0.pv3ddist + v1.pv3ddist);
+    h_pv3ddist0_tkonlymass0->Fill(v0.mass[mfv::PTracksOnly], v0.pv3ddist);
+
+    double pv3dtkonlyctau0 = v0.pv3ddist / (v0.p4(mfv::PTracksOnly).Beta() * v0.p4(mfv::PTracksOnly).Gamma());
+    double pv3dtkonlyctau1 = v1.pv3ddist / (v1.p4(mfv::PTracksOnly).Beta() * v1.p4(mfv::PTracksOnly).Gamma());
+    h_pv3dtkonlyctau01_tkonlymass01->Fill(v0.mass[mfv::PTracksOnly] + v1.mass[mfv::PTracksOnly], pv3dtkonlyctau0 + pv3dtkonlyctau1);
+    h_pv3dtkonlyctau0_tkonlymass0->Fill(v0.mass[mfv::PTracksOnly], pv3dtkonlyctau0);
+
+    double pv3djetsntkctau0 = v0.pv3ddist / (v0.p4(mfv::PJetsByNtracks).Beta() * v0.p4(mfv::PJetsByNtracks).Gamma());
+    double pv3djetsntkctau1 = v1.pv3ddist / (v1.p4(mfv::PJetsByNtracks).Beta() * v1.p4(mfv::PJetsByNtracks).Gamma());
+    h_pv3djetsntkctau01_tkonlymass01->Fill(v0.mass[mfv::PTracksOnly] + v1.mass[mfv::PTracksOnly], pv3djetsntkctau0 + pv3djetsntkctau1);
+    h_pv3djetsntkctau0_tkonlymass0->Fill(v0.mass[mfv::PTracksOnly], pv3djetsntkctau0);
+
+    double pv3dtksjetsntkctau0 = v0.pv3ddist / (v0.p4(mfv::PTracksPlusJetsByNtracks).Beta() * v0.p4(mfv::PTracksPlusJetsByNtracks).Gamma());
+    double pv3dtksjetsntkctau1 = v1.pv3ddist / (v1.p4(mfv::PTracksPlusJetsByNtracks).Beta() * v1.p4(mfv::PTracksPlusJetsByNtracks).Gamma());
+    h_pv3dtksjetsntkctau01_tkonlymass01->Fill(v0.mass[mfv::PTracksOnly] + v1.mass[mfv::PTracksOnly], pv3dtksjetsntkctau0 + pv3dtksjetsntkctau1);
+    h_pv3dtksjetsntkctau0_tkonlymass0->Fill(v0.mass[mfv::PTracksOnly], pv3dtksjetsntkctau0);
+
+    double svdist2d = mag(v0.x - v1.x, v0.y - v1.y);
+    double svdist3d = mag(v0.x - v1.x, v0.y - v1.y, v0.z - v1.z);
+    h_svdist2d_tkonlymass01->Fill(v0.mass[mfv::PTracksOnly] + v1.mass[mfv::PTracksOnly], svdist2d);
+    h_svdist3d_tkonlymass01->Fill(v0.mass[mfv::PTracksOnly] + v1.mass[mfv::PTracksOnly], svdist3d);
+
+    h_ntracks01_tkonlymass01->Fill(v0.mass[mfv::PTracksOnly] + v1.mass[mfv::PTracksOnly], v0.ntracks + v1.ntracks);
+    h_maxtrackpt01_tkonlymass01->Fill(v0.mass[mfv::PTracksOnly] + v1.mass[mfv::PTracksOnly], v0.maxtrackpt + v1.maxtrackpt);
+  }
+
+  for (int isv = 0; isv < nsv; ++isv) {
+    const MFVVertexAux& vtx = vertices->at(isv);
+    h_bs2ddist_ntracks->Fill(vtx.ntracks, vtx.bs2ddist);
+    h_bs2derr_ntracks->Fill(vtx.ntracks, vtx.bs2derr);
+    h_bs2dsig_ntracks->Fill(vtx.ntracks, vtx.bs2dsig());
   }
 
 }
--- MFVNeutralino/test/lifetime_v_mass.C
+++ MFVNeutralino/test/lifetime_v_mass.C
@@ -1,39 +1,22 @@
-int iteration = 30;
 double xcut = 90;
-double ycut = 0.02;
+double ycut = 0.1;
 
-void compareShapes(char* sampleName) {
+void compareShapes(char* sampleName, char* histName, char* yname) {
   TH1::SetDefaultSumw2();
-  TFile* file = TFile::Open(TString::Format("crab/ABCDHistosV13_%db/%s_scaled.root", iteration, sampleName));
-  TH2F* hist = (TH2F*)abcdHistos->Get("h_bs2ddist01_tkonlymass01");
+  TFile* file = TFile::Open(TString::Format("crab/ABCDHistosV13_30f/%s_scaled.root", sampleName));
+  TH2F* hist = (TH2F*)abcdHistos->Get(histName);
   char* xname = "tkonlymass01";
-  char* yname = "bs2ddist01";
 
-  hist->Rebin2D(10,100);
+  hist->Rebin2D(10,10);
 
   int xbin = hist->GetXaxis()->FindBin(xcut);
   int ybin = hist->GetYaxis()->FindBin(ycut);
 
   int nbinsx = hist->GetNbinsX();
-  double xlow = hist->GetXaxis()->GetXmin();
-  double xup = hist->GetXaxis()->GetXmax();
   int nbinsy = hist->GetNbinsY();
-  double ylow = hist->GetYaxis()->GetXmin();
-  double yup = hist->GetYaxis()->GetXmax();
 
   TH1D* h_low = hist->ProjectionY(TString::Format("h_%s_low_%s", yname, xname), 0, xbin-1);
   TH1D* h_high = hist->ProjectionY(TString::Format("h_%s_high_%s", yname, xname), xbin, nbinsx+1);
-
-  int bin30 = hist->GetXaxis()->FindBin(30);
-  int bin60 = hist->GetXaxis()->FindBin(60);
-  int bin90 = hist->GetXaxis()->FindBin(90);
-  int bin120 = hist->GetXaxis()->FindBin(120);
-
-  TH1D* h_py_0_30 = hist->ProjectionY("h_py_0_30", 0, bin30-1);
-  TH1D* h_py_30_60 = hist->ProjectionY("h_py_30_60", bin30, bin60-1);
-  TH1D* h_py_60_90 = hist->ProjectionY("h_py_60_90", bin60, bin90-1);
-  TH1D* h_py_90_120 = hist->ProjectionY("h_py_90_120", bin90, bin120-1);
-  TH1D* h_py_120_inf = hist->ProjectionY("h_py_120_inf", bin120, nbinsx+1);
 
   TCanvas* c1 = new TCanvas();
   c1->Divide(2,2);
@@ -43,22 +26,10 @@ void compareShapes(char* sampleName) {
   h_high->Draw();
   c1->cd(2);
   hist->Draw("colz");
-
   c1->cd(4);
-//  h_py_0_30->SetLineColor(kRed);
-//  h_py_0_30->DrawNormalized();
-  h_py_30_60->SetLineColor(kRed-7);
-  h_py_30_60->SetLineWidth(2);
-  h_py_30_60->DrawNormalized();
-  h_py_60_90->SetLineColor(kRed);
-  h_py_60_90->SetLineWidth(2);
-  h_py_60_90->DrawNormalized("sames");
-  h_py_90_120->SetLineColor(kRed+3);
-  h_py_90_120->SetLineWidth(2);
-  h_py_90_120->DrawNormalized("sames");
-  h_py_120_inf->SetLineColor(kBlack);
-  h_py_120_inf->SetLineWidth(2);
-  h_py_120_inf->DrawNormalized("sames");
+  h_low->SetLineColor(2);
+  h_low->DrawNormalized();
+  h_high->DrawNormalized("same");
 
   double errA, errB, errC, errD;
   double A = hist->IntegralAndError(0, xbin-1, 0, ybin-1, errA);
@@ -73,75 +44,66 @@ void compareShapes(char* sampleName) {
   printf("\tA = %5.2f +/- %5.2f, B = %5.2f +/- %5.2f, C = %5.2f +/- %5.2f, D = %5.2f +/- %5.2f\n", A, errA, B, errB, C, errC, D, errD);
   printf("\tD = %5.2f +/- %5.2f, B/A*C = %5.2f +/- %5.2f, correlation factor = %5.2f\n", D, errD, Dpred, errPred, hist->GetCorrelationFactor());
 
-/*
-  c1->cd(4);
-  int nevents = h_low->Integral();
-  double normalization = C/A * nevents;
-  h_low->SetLineColor(2);
-  if (h_high->GetMaximum() >= C/A * h_low->GetMaximum()) {
-    h_high->Draw();
-    h_low->DrawNormalized("same", normalization);
-  } else {
-    h_low->DrawNormalized("", normalization);
-    h_high->Draw("same");
-  }
-*/
-
-//  c1->SaveAs(TString::Format("plots/ABCD/CutPlayV13/iter%d/%d_%d/%s.root", iteration, int(xcut), int(ycut), sampleName));
+//  c1->SaveAs(TString::Format("plots/ABCD/lifetime_v_mass/%s/%s.pdf", histName, sampleName));
+//  c1->SaveAs(TString::Format("plots/ABCD/lifetime_v_mass/%s/%s.root", histName, sampleName));
 
 }
 
+void plot_all_samples(char* histName, char* yname) {
+  compareShapes("mfv_neutralino_tau0000um_M0200", histName, yname);
+  compareShapes("mfv_neutralino_tau0000um_M0300", histName, yname);
+  compareShapes("mfv_neutralino_tau0000um_M0400", histName, yname);
+  compareShapes("mfv_neutralino_tau0000um_M0600", histName, yname);
+  compareShapes("mfv_neutralino_tau0000um_M0800", histName, yname);
+  compareShapes("mfv_neutralino_tau0010um_M0200", histName, yname);
+  compareShapes("mfv_neutralino_tau0010um_M0300", histName, yname);
+  compareShapes("mfv_neutralino_tau0010um_M0400", histName, yname);
+  compareShapes("mfv_neutralino_tau0010um_M0600", histName, yname);
+  compareShapes("mfv_neutralino_tau0010um_M0800", histName, yname);
+  compareShapes("mfv_neutralino_tau0010um_M1000", histName, yname);
+  compareShapes("mfv_neutralino_tau0100um_M0200", histName, yname);
+  compareShapes("mfv_neutralino_tau0100um_M0300", histName, yname);
+  compareShapes("mfv_neutralino_tau0100um_M0400", histName, yname);
+  compareShapes("mfv_neutralino_tau0100um_M0600", histName, yname);
+  compareShapes("mfv_neutralino_tau0100um_M0800", histName, yname);
+  compareShapes("mfv_neutralino_tau0100um_M1000", histName, yname);
+  compareShapes("mfv_neutralino_tau0300um_M0200", histName, yname);
+  compareShapes("mfv_neutralino_tau0300um_M0300", histName, yname);
+  compareShapes("mfv_neutralino_tau0300um_M0400", histName, yname);
+  compareShapes("mfv_neutralino_tau0300um_M0600", histName, yname);
+  compareShapes("mfv_neutralino_tau0300um_M0800", histName, yname);
+  compareShapes("mfv_neutralino_tau0300um_M1000", histName, yname);
+  compareShapes("mfv_neutralino_tau1000um_M0200", histName, yname);
+  compareShapes("mfv_neutralino_tau1000um_M0300", histName, yname);
+  compareShapes("mfv_neutralino_tau1000um_M0400", histName, yname);
+  compareShapes("mfv_neutralino_tau1000um_M0600", histName, yname);
+  compareShapes("mfv_neutralino_tau1000um_M0800", histName, yname);
+  compareShapes("mfv_neutralino_tau1000um_M1000", histName, yname);
+  compareShapes("mfv_neutralino_tau9900um_M0200", histName, yname);
+  compareShapes("mfv_neutralino_tau9900um_M0300", histName, yname);
+  compareShapes("mfv_neutralino_tau9900um_M0400", histName, yname);
+  compareShapes("mfv_neutralino_tau9900um_M0600", histName, yname);
+  compareShapes("mfv_neutralino_tau9900um_M0800", histName, yname);
+  compareShapes("mfv_neutralino_tau9900um_M1000", histName, yname);
+  compareShapes("ttbarhadronic", histName, yname);
+  compareShapes("ttbarsemilep", histName, yname);
+  compareShapes("ttbardilep", histName, yname);
+  compareShapes("ttbar", histName, yname);
+  compareShapes("qcdht0100", histName, yname);
+  compareShapes("qcdht0250", histName, yname);
+  compareShapes("qcdht0500", histName, yname);
+  compareShapes("qcdht1000", histName, yname);
+  compareShapes("qcd", histName, yname);
+  compareShapes("background", histName, yname);
+}
+
 void lifetime_v_mass() {
-
-/*
-compareShapes("mfv_neutralino_tau0000um_M0200");
-compareShapes("mfv_neutralino_tau0000um_M0300");
-compareShapes("mfv_neutralino_tau0000um_M0400");
-compareShapes("mfv_neutralino_tau0000um_M0600");
-compareShapes("mfv_neutralino_tau0000um_M0800");
-compareShapes("mfv_neutralino_tau0010um_M0200");
-compareShapes("mfv_neutralino_tau0010um_M0300");
-compareShapes("mfv_neutralino_tau0010um_M0400");
-compareShapes("mfv_neutralino_tau0010um_M0600");
-compareShapes("mfv_neutralino_tau0010um_M0800");
-compareShapes("mfv_neutralino_tau0010um_M1000");
-compareShapes("mfv_neutralino_tau0100um_M0200");
-compareShapes("mfv_neutralino_tau0100um_M0300");
-compareShapes("mfv_neutralino_tau0100um_M0400");
-compareShapes("mfv_neutralino_tau0100um_M0600");
-compareShapes("mfv_neutralino_tau0100um_M0800");
-compareShapes("mfv_neutralino_tau0100um_M1000");
-compareShapes("mfv_neutralino_tau0300um_M0200");
-compareShapes("mfv_neutralino_tau0300um_M0300");
-compareShapes("mfv_neutralino_tau0300um_M0400");
-compareShapes("mfv_neutralino_tau0300um_M0600");
-compareShapes("mfv_neutralino_tau0300um_M0800");
-compareShapes("mfv_neutralino_tau0300um_M1000");
-compareShapes("mfv_neutralino_tau1000um_M0200");
-compareShapes("mfv_neutralino_tau1000um_M0300");
-compareShapes("mfv_neutralino_tau1000um_M0400");
-compareShapes("mfv_neutralino_tau1000um_M0600");
-compareShapes("mfv_neutralino_tau1000um_M0800");
-compareShapes("mfv_neutralino_tau1000um_M1000");
-compareShapes("mfv_neutralino_tau9900um_M0200");
-compareShapes("mfv_neutralino_tau9900um_M0300");
-compareShapes("mfv_neutralino_tau9900um_M0400");
-compareShapes("mfv_neutralino_tau9900um_M0600");
-compareShapes("mfv_neutralino_tau9900um_M0800");
-compareShapes("mfv_neutralino_tau9900um_M1000");
-printf("\n");
-*/
-
-//  compareShapes("mfv_neutralino_tau0100um_M0400");
-  compareShapes("mfv_neutralino_tau1000um_M0400");
-//  compareShapes("ttbarhadronic");
-//  compareShapes("ttbarsemilep");
-//  compareShapes("ttbardilep");
-//  compareShapes("ttbar");
-//  compareShapes("qcdht0100");
-//  compareShapes("qcdht0250");
-//  compareShapes("qcdht0500");
-//  compareShapes("qcdht1000");
-//  compareShapes("qcd");
-  compareShapes("background");
+  plot_all_samples("h_bs2ddist01_tkonlymass01", "bs2ddist01");
+  plot_all_samples("h_pv2ddist01_tkonlymass01", "pv2ddist01");
+  plot_all_samples("h_pv3ddist01_tkonlymass01", "pv3ddist01");
+  plot_all_samples("h_pv3dtkonlyctau01_tkonlymass01", "pv3dtkonlyctau01");
+  plot_all_samples("h_pv3djetsntkctau01_tkonlymass01", "pv3djetsntkctau01");
+  plot_all_samples("h_pv3dtksjetsntkctau01_tkonlymass01", "pv3dtksjetsntkctau01");
+  plot_all_samples("h_svdist2d_tkonlymass01", "svdist2d");
+  plot_all_samples("h_svdist3d_tkonlymass01", "svdist3d");
 }
