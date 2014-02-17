84b170322977bff5ca64644a9b12f5621ecb36cc
Jordan Tucker <jordantucker@gmail.com>
2014-02-17 10:20:03 -0600

    Samples.

--- MFVNeutralino/test/gen_histos.py
+++ MFVNeutralino/test/gen_histos.py
@@ -2,7 +2,7 @@ import os, sys, glob
 from JMTucker.Tools.BasicAnalyzer_cfg import *
 debug = 'debug' in sys.argv
 
-process.source.fileNames = ['file:/uscms/home/tucker/nobackup/fromt3/mfv_neutralino_tau1000um_M0400_jtuple_v6_547d3313903142038335071634b26604_pat_1_1_Dpa.root']
+process.source.fileNames = ['/store/user/tucker/mfv_neutralino_tau1000um_M0400/mfv_neutralino_tau1000um_M0400/a6ab3419cb64660d6c68351b3cff9fb0/aodpat_1_1_X2h.root']
 process.TFileService.fileName = 'gen_histos.root'
 
 process.load('JMTucker.MFVNeutralino.GenParticleFilter_cfi')
@@ -24,12 +24,14 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     if debug:
         raise RuntimeError('refusing to submit jobs in debug (verbose print out) mode')
 
-    from JMTucker.Tools.Samples import mfv_signal_samples, background_samples, auxiliary_background_samples
-    samples = [s for s in mfv_signal_samples + background_samples + auxiliary_background_samples if 'qcdmu' not in s.name]
+    import JMTucker.Tools.Samples as Samples
+    samples = [s for s in Samples.mfv_signal_samples + Samples.background_samples + Samples.auxiliary_background_samples if 'qcdmu' not in s.name]
+
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     cs = CRABSubmitter('GenHistos',
                        total_number_of_events = -1,
                        events_per_job = 20000,
                        USER_jmt_skip_input_files = 'src/EGamma/EGammaAnalysisTools/data/*'
                        )
-    cs.submit_all(mfv_signal_samples)
+    samples = [Samples.mfv_neutralino_tau0100um_M0400, Samples.mfv_neutralino_tau1000um_M0400, Samples.mfv_neutralino_tau1000um_M1000, Samples.mfv_neutralino_tau9900um_M0400, Samples.mfv_neutralino_tau9900um_M1000] + Samples.mfv_signal_samples
+    cs.submit_all(samples)
