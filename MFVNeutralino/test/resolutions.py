8816bffb0e0dc852f8567b4cd647004487aace25
Jordan Tucker <jordantucker@gmail.com>
2014-02-18 17:24:50 -0600

    Add version with ana cuts.

--- MFVNeutralino/test/resolutions.py
+++ MFVNeutralino/test/resolutions.py
@@ -6,52 +6,38 @@ SampleFiles.setup(process, 'MFVNtupleV15', 'mfv_neutralino_tau1000um_M0400', 100
 process.TFileService.fileName = 'resolutions.root'
 
 process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
+process.load('JMTucker.MFVNeutralino.AnalysisCuts_cfi')
 
 mfvResolutions = cms.EDAnalyzer('MFVResolutions',
                                 vertex_src = cms.InputTag('mfvSelectedVerticesTight'),
                                 mevent_src = cms.InputTag('mfvEvent'),
                                 which_mom = cms.int32(0),
                                 max_dr = cms.double(-1),
-                                max_dist = cms.double(1e99),
+                                max_dist = cms.double(0.005),
                                 )
 
 process.p = cms.Path(process.mfvSelectedVerticesSeq)
 
-process.mfvResolutionsByDistAllTrks = mfvResolutions.clone()
-process.p *= process.mfvResolutionsByDistAllTrks
-
-process.mfvResolutionsByDistCutTrks = mfvResolutions.clone(max_dist = 0.02)
+process.mfvResolutionsByDistCutTrks = mfvResolutions.clone()
 process.p *= process.mfvResolutionsByDistCutTrks
 
-process.mfvResolutionsByDistAllJets = mfvResolutions.clone(which_mom = 1)
-process.p *= process.mfvResolutionsByDistAllJets
-
-process.mfvResolutionsByDistCutJets = mfvResolutions.clone(which_mom = 1, max_dist = 0.02)
+process.mfvResolutionsByDistCutJets = mfvResolutions.clone(which_mom = 1)
 process.p *= process.mfvResolutionsByDistCutJets
 
-process.mfvResolutionsByDistAllTrksJets = mfvResolutions.clone(which_mom = 2)
-process.p *= process.mfvResolutionsByDistAllTrksJets
-
-process.mfvResolutionsByDistCutTrksJets = mfvResolutions.clone(which_mom = 2, max_dist = 0.02)
+process.mfvResolutionsByDistCutTrksJets = mfvResolutions.clone(which_mom = 2)
 process.p *= process.mfvResolutionsByDistCutTrksJets
 
-process.mfvResolutionsByDRAllTrks = mfvResolutions.clone(max_dr = 1e99, max_dist = -1)
-process.p *= process.mfvResolutionsByDRAllTrks
+process.p *= process.mfvAnalysisCuts
 
-process.mfvResolutionsByDRCutTrks = mfvResolutions.clone(max_dr = 0.3, max_dist = -1)
-process.p *= process.mfvResolutionsByDRCutTrks
+process.mfvResolutionsFullSelByDistCutTrks = mfvResolutions.clone()
+process.p *= process.mfvResolutionsFullSelByDistCutTrks
 
-process.mfvResolutionsByDRAllJets = mfvResolutions.clone(max_dr = 1e99, max_dist = -1, which_mom = 1)
-process.p *= process.mfvResolutionsByDRAllJets
+process.mfvResolutionsFullSelByDistCutJets = mfvResolutions.clone(which_mom = 1)
+process.p *= process.mfvResolutionsFullSelByDistCutJets
 
-process.mfvResolutionsByDRCutJets = mfvResolutions.clone(max_dr = 0.3, max_dist = -1, which_mom = 1)
-process.p *= process.mfvResolutionsByDRCutJets
+process.mfvResolutionsFullSelByDistCutTrksJets = mfvResolutions.clone(which_mom = 2)
+process.p *= process.mfvResolutionsFullSelByDistCutTrksJets
 
-process.mfvResolutionsByDRAllTrksJets = mfvResolutions.clone(max_dr = 1e99, max_dist = -1, which_mom = 2)
-process.p *= process.mfvResolutionsByDRAllTrksJets
-
-process.mfvResolutionsByDRCutTrksJets = mfvResolutions.clone(max_dr = 0.3, max_dist = -1, which_mom = 2)
-process.p *= process.mfvResolutionsByDRCutTrksJets
 
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
