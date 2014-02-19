e2fcb91c3cafc35e7fd2d47b1dc3b77d88722a40
Jordan Tucker <jordantucker@gmail.com>
2014-02-19 13:35:52 -0600

    Merge branch 'master' of github.com:jordantucker/cmssw-usercode

--- MFVNeutralino/plugins/Resolutions.cc
+++ MFVNeutralino/plugins/Resolutions.cc
@@ -23,6 +23,49 @@ class MFVResolutions : public edm::EDAnalyzer {
   const int which_mom;
   const double max_dr;
   const double max_dist;
+
+  TH1F* h_lsp_nmatch[2];
+  TH1F* h_dr;
+  TH1F* h_dist;
+
+  TH1F* h_dx;
+  TH1F* h_dy;
+  TH1F* h_dz;
+  TH1F* h_dist2d;
+  TH1F* h_dist3d;
+
+  TH1F* h_r_p;
+  TH1F* h_r_pt;
+  TH1F* h_r_eta;
+  TH1F* h_r_phi;
+  TH1F* h_r_mass;
+  TH1F* h_r_px;
+  TH1F* h_r_py;
+  TH1F* h_r_pz;
+  TH1F* h_r_rapidity;
+  TH1F* h_r_theta;
+  TH1F* h_r_betagamma;
+  TH1F* h_r_avgbetagammalab;
+  TH1F* h_r_avgbetagammacmz;
+
+  TH1F* h_f_p;
+  TH1F* h_f_pt;
+  TH1F* h_f_mass;
+
+  TH2F* h_s_p;
+  TH2F* h_s_pt;
+  TH2F* h_s_eta;
+  TH2F* h_s_phi;
+  TH2F* h_s_mass;
+  TH2F* h_s_px;
+  TH2F* h_s_py;
+  TH2F* h_s_pz;
+  TH2F* h_s_rapidity;
+  TH2F* h_s_theta;
+  TH2F* h_s_betagamma;
+  TH2F* h_s_avgbetagammalab;
+  TH2F* h_s_avgbetagammacmz;
+
 };
 
 MFVResolutions::MFVResolutions(const edm::ParameterSet& cfg)
@@ -35,6 +78,50 @@ MFVResolutions::MFVResolutions(const edm::ParameterSet& cfg)
   die_if_not(which_mom >= 0 && which_mom < mfv::NMomenta, "invalid which_mom");
 
   edm::Service<TFileService> fs;
+
+  for (int i = 0; i < 2; ++i) {
+    h_lsp_nmatch[i] = fs->make<TH1F>(TString::Format("h_lsp%d_nmatch", i), TString::Format(";number of vertices that match lsp%d;events", i), 15, 0, 15);
+  }
+  h_dr = fs->make<TH1F>("h_dr", ";deltaR to closest lsp;number of vertices", 150, 0, 7);
+  h_dist = fs->make<TH1F>("h_dist", ";distance to closest lsp;number of vertices", 100, 0, 0.02);
+
+  h_dx = fs->make<TH1F>("h_dx", ";x resolution (cm);number of vertices", 200, -0.02, 0.02);
+  h_dy = fs->make<TH1F>("h_dy", ";y resolution (cm);number of vertices", 200, -0.02, 0.02);
+  h_dz = fs->make<TH1F>("h_dz", ";z resolution (cm);number of vertices", 200, -0.02, 0.02);
+  h_dist2d = fs->make<TH1F>("h_dist2d", ";dist2d(lsp,vtx) (cm);number of vertices", 100, 0, 0.02);
+  h_dist3d = fs->make<TH1F>("h_dist3d", ";dist3d(lsp,vtx) (cm);number of vertices", 100, 0, 0.02);
+
+  h_r_p = fs->make<TH1F>("h_r_p", ";p resolution (GeV);number of vertices", 300, -1500, 1500);
+  h_r_pt = fs->make<TH1F>("h_r_pt", ";p_{T} resolution (GeV);number of vertices", 300, -1500, 1500);
+  h_r_eta = fs->make<TH1F>("h_r_eta", ";eta resolution;number of vertices", 50, -4, 4);
+  h_r_phi = fs->make<TH1F>("h_r_phi", ";phi resolution;number of vertices", 50, -3.15, 3.15);
+  h_r_mass = fs->make<TH1F>("h_r_mass", ";mass resolution (GeV);number of vertices", 300, -1500, 1500);
+  h_r_px = fs->make<TH1F>("h_r_px", ";px resolution (GeV);number of vertices", 300, -1500, 1500);
+  h_r_py = fs->make<TH1F>("h_r_py", ";py resolution (GeV);number of vertices", 300, -1500, 1500);
+  h_r_pz = fs->make<TH1F>("h_r_pz", ";pz resolution (GeV);number of vertices", 300, -1500, 1500);
+  h_r_rapidity = fs->make<TH1F>("h_r_rapidity", ";rapidity resolution;number of vertices", 50, -4, 4);
+  h_r_theta = fs->make<TH1F>("h_r_theta", ";theta resolution;number of vertices", 50, -3.15, 3.15);
+  h_r_betagamma = fs->make<TH1F>("h_r_betagamma", ";betagamma resolution;number of vertices", 200, -10, 10);
+  h_r_avgbetagammalab = fs->make<TH1F>("h_r_avgbetagammalab", ";avgbetagammalab resolution;events", 200, -10, 10);
+  h_r_avgbetagammacmz = fs->make<TH1F>("h_r_avgbetagammacmz", ";avgbetagammacmz resolution;events", 200, -10, 10);
+
+  h_f_p = fs->make<TH1F>("h_f_p", ";fractional p resolution;number of vertices", 100, -5, 5);
+  h_f_pt = fs->make<TH1F>("h_f_pt", ";fractional pt resolution;number of vertices", 100, -5, 5);
+  h_f_mass = fs->make<TH1F>("h_f_mass", ";fractional mass resolution;number of vertices", 100, -5, 5);
+
+  h_s_p = fs->make<TH2F>("h_s_p", ";generated p;reconstructed p", 150, 0, 1500, 150, 0, 1500);
+  h_s_pt = fs->make<TH2F>("h_s_pt", ";generated pt;reconstructed pt", 150, 0, 1500, 150, 0, 1500);
+  h_s_eta = fs->make<TH2F>("h_s_eta", ";generated eta;reconstructed eta", 50, -4, 4, 50, -4, 4);
+  h_s_phi = fs->make<TH2F>("h_s_phi", ";generated phi;reconstructed phi", 50, -3.15, 3.15, 50, -3.15, 3.15);
+  h_s_mass = fs->make<TH2F>("h_s_mass", ";generated mass;reconstructed mass", 150, 0, 1500, 150, 0, 1500);
+  h_s_px = fs->make<TH2F>("h_s_px", ";generated px;reconstructed px", 150, 0, 1500, 150, 0, 1500);
+  h_s_py = fs->make<TH2F>("h_s_py", ";generated py;reconstructed py", 150, 0, 1500, 150, 0, 1500);
+  h_s_pz = fs->make<TH2F>("h_s_pz", ";generated pz;reconstructed pz", 150, 0, 1500, 150, 0, 1500);
+  h_s_rapidity = fs->make<TH2F>("h_s_rapidity", ";generated rapidity;reconstructed rapidity", 50, -4, 4, 50, -4, 4);
+  h_s_theta = fs->make<TH2F>("h_s_theta", ";generated theta;reconstructed theta", 50, 0, 3.15, 50, 0, 3.15);
+  h_s_betagamma = fs->make<TH2F>("h_s_betagamma", ";generated betagamma;reconstructed betagamma", 100, 0, 10, 100, 0, 10);
+  h_s_avgbetagammalab = fs->make<TH2F>("h_s_avgbetagammalab", ";generated avgbetagammalab;reconstructed avgbetagammalab", 100, 0, 10, 100, 0, 10);
+  h_s_avgbetagammacmz = fs->make<TH2F>("h_s_avgbetagammacmz", ";generated avgbetagammacmz;reconstructed avgbetagammacmz", 100, 0, 10, 100, 0, 10);
 }
 
 namespace {
@@ -113,9 +200,82 @@ void MFVResolutions::analyze(const edm::Event& event, const edm::EventSetup&) {
     const TLorentzVector& lsp_p4 = lsp_p4s[ilsp];
     const TLorentzVector& vtx_p4 = vtx.p4(which_mom);
 
-    // histogram dr, dist, lsp_nmatch[i]
+    // histogram dr, dist
+    h_dr->Fill(dr);
+    h_dist->Fill(dist);
+
     // histogram space resolutions: x, y, z, dist2d, dist3d
-    // histogram momentum: p, pt, eta, phi, mass, px, py, pz
+    h_dx->Fill(vtx.x - mevent->gen_lsp_decay[ilsp*3+0]);
+    h_dy->Fill(vtx.y - mevent->gen_lsp_decay[ilsp*3+1]);
+    h_dz->Fill(vtx.z - mevent->gen_lsp_decay[ilsp*3+2]);
+    h_dist2d->Fill(mag(mevent->gen_lsp_decay[ilsp*3+0] - vtx.x,
+                       mevent->gen_lsp_decay[ilsp*3+1] - vtx.y));
+    h_dist3d->Fill(mag(mevent->gen_lsp_decay[ilsp*3+0] - vtx.x,
+                       mevent->gen_lsp_decay[ilsp*3+1] - vtx.y,
+                       mevent->gen_lsp_decay[ilsp*3+2] - vtx.z));
+
+    // histogram momentum resolutions: p, pt, eta, phi, mass, px, py, pz, rapidity, theta, betagamma
+    h_r_p->Fill(vtx_p4.P() - lsp_p4.P());
+    h_r_pt->Fill(vtx_p4.Pt() - lsp_p4.Pt());
+    h_r_eta->Fill(vtx_p4.Eta() - lsp_p4.Eta());
+    h_r_phi->Fill(reco::deltaPhi(vtx_p4.Phi(), lsp_p4.Phi()));
+    h_r_mass->Fill(vtx_p4.M() - lsp_p4.M());
+    h_r_px->Fill(vtx_p4.Px() - lsp_p4.Px());
+    h_r_py->Fill(vtx_p4.Py() - lsp_p4.Py());
+    h_r_pz->Fill(vtx_p4.Pz() - lsp_p4.Pz());
+    h_r_rapidity->Fill(vtx_p4.Rapidity() - lsp_p4.Rapidity());
+    h_r_theta->Fill(vtx_p4.Theta() - lsp_p4.Theta());
+    h_r_betagamma->Fill(vtx_p4.Beta()*vtx_p4.Gamma() - lsp_p4.Beta()*lsp_p4.Gamma());
+
+    h_f_p->Fill((vtx_p4.P() - lsp_p4.P()) / lsp_p4.P());
+    h_f_pt->Fill((vtx_p4.Pt() - lsp_p4.Pt()) / lsp_p4.Pt());
+    h_f_mass->Fill((vtx_p4.M() - lsp_p4.M()) / lsp_p4.M());
+
+    h_s_p->Fill(lsp_p4.P(), vtx_p4.P());
+    h_s_pt->Fill(lsp_p4.Pt(), vtx_p4.Pt());
+    h_s_eta->Fill(lsp_p4.Eta(), vtx_p4.Eta());
+    h_s_phi->Fill(lsp_p4.Phi(), vtx_p4.Phi());
+    h_s_mass->Fill(lsp_p4.M(), vtx_p4.M());
+    h_s_px->Fill(lsp_p4.Px(), vtx_p4.Px());
+    h_s_py->Fill(lsp_p4.Py(), vtx_p4.Py());
+    h_s_pz->Fill(lsp_p4.Pz(), vtx_p4.Pz());
+    h_s_rapidity->Fill(lsp_p4.Rapidity(), vtx_p4.Rapidity());
+    h_s_theta->Fill(lsp_p4.Theta(), vtx_p4.Theta());
+    h_s_betagamma->Fill(lsp_p4.Beta()*lsp_p4.Gamma(), vtx_p4.Beta()*vtx_p4.Gamma());
+  }
+
+  // histogram lsp_nmatch[i]
+  for (int i = 0; i < 2; ++i) {
+    h_lsp_nmatch[i]->Fill(lsp_nmatch[i]);
+  }
+
+  // histogram average betagamma resolutions
+  const int nsv = int(vertices->size());
+  if (nsv >= 2) {
+    const MFVVertexAux& v0 = vertices->at(0);
+    const MFVVertexAux& v1 = vertices->at(1);
+
+    TLorentzVector lsp0_p4 = lsp_p4s[0];
+    TLorentzVector lsp1_p4 = lsp_p4s[1];
+    double lsp_avgbetagammalab = (lsp0_p4.Beta()*lsp1_p4.Gamma() + lsp0_p4.Beta()*lsp1_p4.Gamma()) / 2;
+    TVector3 lsp_betacmz = TVector3(0, 0, -(lsp0_p4.Pz() + lsp1_p4.Pz()) / (lsp0_p4.E() + lsp1_p4.E()));
+    lsp0_p4.Boost(lsp_betacmz);
+    lsp1_p4.Boost(lsp_betacmz);
+    double lsp_avgbetagammacmz = (lsp0_p4.Beta()*lsp1_p4.Gamma() + lsp0_p4.Beta()*lsp1_p4.Gamma()) / 2;
+
+    TLorentzVector vtx0_p4 = v0.p4(which_mom);
+    TLorentzVector vtx1_p4 = v1.p4(which_mom);
+    double vtx_avgbetagammalab = (vtx0_p4.Beta()*vtx0_p4.Gamma() + vtx1_p4.Beta()*vtx1_p4.Gamma()) / 2;
+    TVector3 vtx_betacmz = TVector3(0, 0, -(vtx0_p4.Pz() + vtx1_p4.Pz()) / (vtx0_p4.E() + vtx1_p4.E()));
+    vtx0_p4.Boost(vtx_betacmz);
+    vtx1_p4.Boost(vtx_betacmz);
+    double vtx_avgbetagammacmz = (vtx0_p4.Beta()*vtx1_p4.Gamma() + vtx0_p4.Beta()*vtx1_p4.Gamma()) / 2;
+
+    h_r_avgbetagammalab->Fill(vtx_avgbetagammalab - lsp_avgbetagammalab);
+    h_r_avgbetagammacmz->Fill(vtx_avgbetagammacmz - lsp_avgbetagammacmz);
+    h_s_avgbetagammalab->Fill(lsp_avgbetagammalab, vtx_avgbetagammalab);
+    h_s_avgbetagammacmz->Fill(lsp_avgbetagammacmz, vtx_avgbetagammacmz);
+
   }
 }
 
