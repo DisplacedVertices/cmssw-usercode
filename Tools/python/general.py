0ec36742632981972bed92424c10bd30c910e6cd
Jordan Tucker <jordantucker@gmail.com>
2014-03-12 04:27:50 -0500

    typed_from_argv.

--- Tools/python/general.py
+++ Tools/python/general.py
@@ -22,8 +22,24 @@ def to_pickle(obj, fn, proto=-1, comp=False):
         f = open(fn, 'wb')
     cPickle.dump(obj, f, proto)
 
+def typed_from_argv(type_, default_value=None, raise_on_multiple=False):
+    found = []
+    for x in sys.argv:
+        try:
+            z = type_(x)
+            found.append(z)
+        except ValueError:
+            pass
+    if raise_on_multiple and len(found) > 1:
+        raise ValueError('multiple values found in argv')
+    if found:
+        return found[0]
+    else:
+        return default_value
+
 __all__ = [
     'big_warn',
     'from_pickle',
-    'to_pickle'
+    'to_pickle',
+    'typed_from_argv',
     ]
