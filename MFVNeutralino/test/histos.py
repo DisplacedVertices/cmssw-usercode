31944c5b1f9e63abaed321115f0d8dc606e7ab4f
Jordan Tucker <jordantucker@gmail.com>
2014-03-17 05:14:26 -0500

    Do event histos in nm1s.

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -33,11 +33,14 @@ for name, cut in nm1s:
     vtx_name = 'vtxNo' + name
     ana = process.mfvAnalysisCuts.clone(vertex_src = vtx_name)
     ana_name = 'anaNo' + name
-    hst = process.mfvVertexHistos.clone(vertex_aux_src = vtx_name)
-    hst_name = 'hstNo' + name
+    vtx_hst = process.mfvVertexHistos.clone(vertex_aux_src = vtx_name)
+    vtx_hst_name = 'vtxHstNo' + name
+    evt_hst = process.mfvEventHistos.clone(vertex_aux_src = vtx_name)
+    evt_hst_name = 'evtHstNo' + name
     setattr(process, vtx_name, vtx)
     setattr(process, ana_name, ana)
-    setattr(process, hst_name, hst)
+    setattr(process, evt_hst_name, evt_hst)
+    setattr(process, vtx_hst_name, vtx_hst)
     setattr(process, 'p' + name, cms.Path(vtx * ana * hst))
 
 hackrundata = False # JMTBAD
