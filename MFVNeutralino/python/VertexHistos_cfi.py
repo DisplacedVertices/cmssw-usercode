3068708d53eefe7dd193dd396ca122f8df1a791d
Jordan Tucker <jordantucker@gmail.com>
2014-03-18 07:54:54 -0500

    vertex_to_jets_src

--- MFVNeutralino/python/VertexHistos_cfi.py
+++ MFVNeutralino/python/VertexHistos_cfi.py
@@ -4,6 +4,7 @@ mfvVertexHistos = cms.EDAnalyzer('MFVVertexHistos',
                                  mfv_event_src = cms.InputTag('mfvEvent'),
                                  vertex_aux_src = cms.InputTag('mfvSelectedVerticesTight'),
                                  vertex_src = cms.InputTag(''),
+                                 vertex_to_jets_src = cms.InputTag(''),
                                  weight_src = cms.InputTag('mfvWeight'),
                                  do_scatterplots = cms.bool(False),
                                  )
