73521b56263a1b2eceb20ebe57a5616a9cc4f5eb
Jennifer Chu <jyc72@cornell.edu>
2014-03-25 13:19:17 -0500

    Run with pileup weights.

--- MFVNeutralino/plugins/ABCDHistos.cc
+++ MFVNeutralino/plugins/ABCDHistos.cc
@@ -16,6 +16,7 @@ class ABCDHistos : public edm::EDAnalyzer {
 
  private:
   const edm::InputTag mfv_event_src;
+  const edm::InputTag weight_src;
   const edm::InputTag vertex_src;
   const int which_mom;
 
@@ -70,6 +71,7 @@ class ABCDHistos : public edm::EDAnalyzer {
 
 ABCDHistos::ABCDHistos(const edm::ParameterSet& cfg)
   : mfv_event_src(cfg.getParameter<edm::InputTag>("mfv_event_src")),
+    weight_src(cfg.getParameter<edm::InputTag>("weight_src")),
     vertex_src(cfg.getParameter<edm::InputTag>("vertex_src")),
     which_mom(cfg.getParameter<int>("which_mom"))
 {
@@ -129,6 +131,10 @@ void ABCDHistos::analyze(const edm::Event& event, const edm::EventSetup&) {
   edm::Handle<MFVEvent> mevent;
   event.getByLabel(mfv_event_src, mevent);
 
+  edm::Handle<double> weight;
+  event.getByLabel(weight_src, weight);
+  const double w = *weight;
+
   const float pvx = mevent->pvx;
   const float pvy = mevent->pvy;
   const float pvz = mevent->pvz;
@@ -137,84 +143,84 @@ void ABCDHistos::analyze(const edm::Event& event, const edm::EventSetup&) {
   event.getByLabel(vertex_src, vertices);
 
   const int nsv = int(vertices->size());
-  h_nsv->Fill(nsv);
+  h_nsv->Fill(nsv, w);
 
   for (int isv = 0; isv < nsv; ++isv) {
     const MFVVertexAux& vtx = vertices->at(isv);
-    h_bs2derr_drmax->Fill(vtx.drmax, vtx.bs2derr);
-    h_trackdxyerrmax_trackdzerrmax->Fill(vtx.trackdzerrmax, vtx.trackdxyerrmax);
+    h_bs2derr_drmax->Fill(vtx.drmax, vtx.bs2derr, w);
+    h_trackdxyerrmax_trackdzerrmax->Fill(vtx.trackdzerrmax, vtx.trackdxyerrmax, w);
     if (vtx.trackdxyerrmax < 0.1) {
-      h_bs2derr_drmax_trackdxyerrmax0p1->Fill(vtx.drmax, vtx.bs2derr);
+      h_bs2derr_drmax_trackdxyerrmax0p1->Fill(vtx.drmax, vtx.bs2derr, w);
     }
     if (vtx.trackdzerrmax < 0.2) {
-      h_bs2derr_drmax_trackdzerrmax0p2->Fill(vtx.drmax, vtx.bs2derr);
+      h_bs2derr_drmax_trackdzerrmax0p2->Fill(vtx.drmax, vtx.bs2derr, w);
     }
   }
 
   if (nsv >= 2) {
     const MFVVertexAux& v0 = vertices->at(0);
     const MFVVertexAux& v1 = vertices->at(1);
-    h_ntracks01_maxtrackpt01->Fill(v0.maxtrackpt + v1.maxtrackpt, v0.ntracks + v1.ntracks);
+    h_ntracks01_maxtrackpt01->Fill(v0.maxtrackpt + v1.maxtrackpt, v0.ntracks + v1.ntracks, w);
 
-    h_bs2ddist0_bs2ddist1->Fill(v1.bs2ddist, v0.bs2ddist);
-    h_mass0_mass1->Fill(v1.mass[which_mom], v0.mass[which_mom]);
+    h_bs2ddist0_bs2ddist1->Fill(v1.bs2ddist, v0.bs2ddist, w);
+    h_mass0_mass1->Fill(v1.mass[which_mom], v0.mass[which_mom], w);
 
-    h_bs2ddist01_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], v0.bs2ddist + v1.bs2ddist);
-    h_bs2ddist0_mass0->Fill(v0.mass[which_mom], v0.bs2ddist);
-    h_bs2ddist1_mass1->Fill(v1.mass[which_mom], v1.bs2ddist);
+    h_bs2ddist01_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], v0.bs2ddist + v1.bs2ddist, w);
+    h_bs2ddist0_mass0->Fill(v0.mass[which_mom], v0.bs2ddist, w);
+    h_bs2ddist1_mass1->Fill(v1.mass[which_mom], v1.bs2ddist, w);
 
-    h_bs2ddist1_mass0->Fill(v0.mass[which_mom], v1.bs2ddist);
-    h_bs2ddist0_mass1->Fill(v1.mass[which_mom], v0.bs2ddist);
+    h_bs2ddist1_mass0->Fill(v0.mass[which_mom], v1.bs2ddist, w);
+    h_bs2ddist0_mass1->Fill(v1.mass[which_mom], v0.bs2ddist, w);
 
-    h_pv2ddist01_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], v0.pv2ddist + v1.pv2ddist);
-    h_pv2ddist0_mass0->Fill(v0.mass[which_mom], v0.pv2ddist);
+    h_pv2ddist01_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], v0.pv2ddist + v1.pv2ddist, w);
+    h_pv2ddist0_mass0->Fill(v0.mass[which_mom], v0.pv2ddist, w);
 
-    h_pv3ddist01_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], v0.pv3ddist + v1.pv3ddist);
-    h_pv3ddist0_mass0->Fill(v0.mass[which_mom], v0.pv3ddist);
+    h_pv3ddist01_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], v0.pv3ddist + v1.pv3ddist, w);
+    h_pv3ddist0_mass0->Fill(v0.mass[which_mom], v0.pv3ddist, w);
 
     double pv3dctau0 = v0.pv3ddist / (v0.p4(which_mom).Beta() * v0.p4(which_mom).Gamma());
     double pv3dctau1 = v1.pv3ddist / (v1.p4(which_mom).Beta() * v1.p4(which_mom).Gamma());
-    h_pv3dctau01_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], pv3dctau0 + pv3dctau1);
-    h_pv3dctau0_mass0->Fill(v0.mass[which_mom], pv3dctau0);
+    h_pv3dctau01_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], pv3dctau0 + pv3dctau1, w);
+    h_pv3dctau0_mass0->Fill(v0.mass[which_mom], pv3dctau0, w);
 
     double svdist2d = mag(v0.x - v1.x, v0.y - v1.y);
     double svdist3d = mag(v0.x - v1.x, v0.y - v1.y, v0.z - v1.z);
-    h_svdist2d_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svdist2d);
-    h_svdist3d_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svdist3d);
+    h_svdist2d_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svdist2d, w);
+    h_svdist3d_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svdist3d, w);
 
     double phibs0 = atan2(v0.y - mevent->bsy, v0.x - mevent->bsx);
     double phibs1 = atan2(v1.y - mevent->bsy, v1.x - mevent->bsx);
-    h_absdeltaphibs01->Fill(fabs(reco::deltaPhi(phibs0, phibs1)));
+    h_absdeltaphibs01->Fill(fabs(reco::deltaPhi(phibs0, phibs1)), w);
     double phipv0 = atan2(v0.y - mevent->pvy, v0.x - mevent->pvx);
     double phipv1 = atan2(v1.y - mevent->pvy, v1.x - mevent->pvx);
-    h_absdeltaphipv01->Fill(fabs(reco::deltaPhi(phipv0, phipv1)));
+    h_absdeltaphipv01->Fill(fabs(reco::deltaPhi(phipv0, phipv1)), w);
 
     TLorentzVector x0 = TLorentzVector(v0.x - pvx, v0.y - pvy, v0.z - pvz, v0.pv3ddist / v0.p4().Beta());
     TLorentzVector x1 = TLorentzVector(v1.x - pvx, v1.y - pvy, v1.z - pvz, v1.pv3ddist / v1.p4().Beta());
     TLorentzVector p0 = v0.p4(which_mom);
     TLorentzVector p1 = v1.p4(which_mom);
-    h_pz01lab->Fill(p0.Pz() + p1.Pz());
-    h_cosanglemom01lab->Fill(cos(p0.Angle(p1.Vect())));
-    h_betagamma0lab_betagamma1lab->Fill(p1.Beta() * p1.Gamma(), p0.Beta() * p0.Gamma());
+    h_pz01lab->Fill(p0.Pz() + p1.Pz(), w);
+    h_cosanglemom01lab->Fill(cos(p0.Angle(p1.Vect())), w);
+    h_betagamma0lab_betagamma1lab->Fill(p1.Beta() * p1.Gamma(), p0.Beta() * p0.Gamma(), w);
 
     TVector3 betacmz = TVector3(0, 0, -(p0.Pz() + p1.Pz()) / (p0.E() + p1.E()));
     x0.Boost(betacmz);
     x1.Boost(betacmz);
     p0.Boost(betacmz);
     p1.Boost(betacmz);
-    h_pz01cmz->Fill(p0.Pz() + p1.Pz());
-    h_cosanglemom01cmz->Fill(cos(p0.Angle(p1.Vect())));
-    h_betagamma0cmz_betagamma1cmz->Fill(p1.Beta() * p1.Gamma(), p0.Beta() * p0.Gamma());
+    h_pz01cmz->Fill(p0.Pz() + p1.Pz(), w);
+    h_cosanglemom01cmz->Fill(cos(p0.Angle(p1.Vect())), w);
+    h_betagamma0cmz_betagamma1cmz->Fill(p1.Beta() * p1.Gamma(), p0.Beta() * p0.Gamma(), w);
 
     double svdist2dcmz = mag(x0.X() - x1.X(), x0.Y() - x1.Y());
     double svdist3dcmz = mag(x0.X() - x1.X(), x0.Y() - x1.Y(), x0.Z() - x1.Z());
-    h_svdist2dcmz_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svdist2dcmz);
-    h_svdist3dcmz_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svdist3dcmz);
+    h_svdist2dcmz_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svdist2dcmz, w);
+    h_svdist3dcmz_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svdist3dcmz, w);
 
     double svctau2dcmz = 2 * svdist2dcmz / (p0.Beta()*p0.Gamma() + p1.Beta()*p1.Gamma());
     double svctau3dcmz = 2 * svdist3dcmz / (p0.Beta()*p0.Gamma() + p1.Beta()*p1.Gamma());
-    h_svctau2dcmz_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svctau2dcmz);
-    h_svctau3dcmz_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svctau3dcmz);
+    h_svctau2dcmz_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svctau2dcmz, w);
+    h_svctau3dcmz_mass01->Fill(v0.mass[which_mom] + v1.mass[which_mom], svctau3dcmz, w);
   }
 }
 
--- MFVNeutralino/test/abcd_histos.py
+++ MFVNeutralino/test/abcd_histos.py
@@ -5,11 +5,13 @@ from JMTucker.Tools import SampleFiles
 SampleFiles.setup(process, 'MFVNtupleV15_WPixel', 'mfv_neutralino_tau1000um_M0400', 500)
 process.TFileService.fileName = 'abcd_histos.root'
 
+process.load('JMTucker.MFVNeutralino.WeightProducer_cfi')
 process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
 process.load('JMTucker.MFVNeutralino.AnalysisCuts_cfi')
 
 process.abcdHistos = cms.EDAnalyzer('ABCDHistos',
                                     mfv_event_src = cms.InputTag('mfvEvent'),
+                                    weight_src = cms.InputTag('mfvWeight'),
                                     vertex_src = cms.InputTag('mfvSelectedVerticesTight'),
                                     which_mom = cms.int32(0),
                                     )
@@ -17,7 +19,7 @@ process.abcdHistos = cms.EDAnalyzer('ABCDHistos',
 process.abcdHistosTrks = process.abcdHistos.clone()
 process.abcdHistosJets = process.abcdHistos.clone(which_mom = 1)
 process.abcdHistosTrksJets = process.abcdHistos.clone(which_mom = 2)
-process.p = cms.Path(process.mfvSelectedVerticesTight * process.mfvAnalysisCuts * process.abcdHistosTrks * process.abcdHistosJets * process.abcdHistosTrksJets)
+process.p = cms.Path(process.mfvWeight * process.mfvSelectedVerticesTight * process.mfvAnalysisCuts * process.abcdHistosTrks * process.abcdHistosJets * process.abcdHistosTrksJets)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
