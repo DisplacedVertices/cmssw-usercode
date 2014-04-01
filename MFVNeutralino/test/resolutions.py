7942f69be388ac7025219405e95d5a0fea94c446
Jennifer Chu <jyc72@cornell.edu>
2014-04-01 12:21:54 -0500

    Add msptm resolutions, and run over WPixel ntuples.

--- MFVNeutralino/test/resolutions.py
+++ MFVNeutralino/test/resolutions.py
@@ -2,7 +2,7 @@ import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
 from JMTucker.Tools import SampleFiles
 
-SampleFiles.setup(process, 'MFVNtupleV15', 'mfv_neutralino_tau1000um_M0400', 1000)
+SampleFiles.setup(process, 'MFVNtupleV15_WPixel', 'mfv_neutralino_tau1000um_M0400', 1000)
 process.TFileService.fileName = 'resolutions.root'
 
 process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
@@ -45,10 +45,10 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     from JMTucker.Tools.SampleFiles import SampleFiles
 
-    cs = CRABSubmitter('MFVResolutionsV15',
+    cs = CRABSubmitter('MFVResolutionsV15_10',
                        total_number_of_events = -1,
                        events_per_job = 200000,
-                       manual_datasets = SampleFiles['MFVNtupleV15'],
+                       manual_datasets = SampleFiles['MFVNtupleV15_WPixel'],
                        )
 
-    cs.submit_all(Samples.mfv_signal_samples)
+    cs.submit_all([Samples.mfv_neutralino_tau0100um_M0400, Samples.mfv_neutralino_tau1000um_M0400, Samples.mfv_neutralino_tau9900um_M0400])
