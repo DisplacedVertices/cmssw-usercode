afbce930a4f0bb0008e25a5e22dfb8a0e93c6085
Jennifer Chu <jyc72@cornell.edu>
2014-05-08 15:31:08 -0500

    Plot dxyerr, dzerr vs. eta, phi and eta vs. phi in slices.

--- MFVNeutralino/plugins/EffCheck.cc
+++ MFVNeutralino/plugins/EffCheck.cc
@@ -21,6 +21,20 @@ class EffCheck : public edm::EDAnalyzer {
   TH1F* h_tracks_pt[2][3];
   TH1F* h_tracks_eta[2][3];
   TH1F* h_tracks_phi[2][3];
+  TH1F* h_tracks_dxyerr[2][3];
+  TH1F* h_tracks_dzerr[2][3];
+  TH1F* h_tracks_npixel[2][3];
+  TH1F* h_tracks_nstrip[2][3];
+
+  TH2F* h_tracks_dxyerr_eta[2][3];
+  TH2F* h_tracks_dxyerr_phi[2][3];
+  TH2F* h_tracks_dzerr_eta[2][3];
+  TH2F* h_tracks_dzerr_phi[2][3];
+
+  TH2F* h_tracks_eta_phi_dxyerr_eq[2][3][11];
+  TH2F* h_tracks_eta_phi_dxyerr_lt[2][3][11];
+  TH2F* h_tracks_eta_phi_dzerr_eq[2][3][11];
+  TH2F* h_tracks_eta_phi_dzerr_lt[2][3][11];
 
   TH2F* h_tracks_npixel_eta[2][3];
   TH2F* h_tracks_npixel_phi[2][3];
@@ -47,6 +61,28 @@ EffCheck::EffCheck(const edm::ParameterSet& cfg)
       h_tracks_pt[i][j] = fs->make<TH1F>(TString::Format("h_tracks_%s_%s_pt", exi[i], exj[j]), TString::Format("%s tracks%s;tracks pt;arb. units", exi[i], exj[j]), 150, 0, 150);
       h_tracks_phi[i][j] = fs->make<TH1F>(TString::Format("h_tracks_%s_%s_phi", exi[i], exj[j]), TString::Format("%s tracks%s;tracks phi;arb. units", exi[i], exj[j]), 50, -3.15, 3.15);
       h_tracks_eta[i][j] = fs->make<TH1F>(TString::Format("h_tracks_%s_%s_eta", exi[i], exj[j]), TString::Format("%s tracks%s;tracks eta;arb. units", exi[i], exj[j]), 50, -4, 4);
+      h_tracks_dxyerr[i][j] = fs->make<TH1F>(TString::Format("h_tracks_%s_%s_dxyerr", exi[i], exj[j]), TString::Format("%s tracks%s;tracks dxyerr;arb. units", exi[i], exj[j]), 1000, 0, 2);
+      h_tracks_dzerr[i][j] = fs->make<TH1F>(TString::Format("h_tracks_%s_%s_dzerr", exi[i], exj[j]), TString::Format("%s tracks%s;tracks dzerr;arb. units", exi[i], exj[j]), 1000, 0, 2);
+      h_tracks_npixel[i][j] = fs->make<TH1F>(TString::Format("h_tracks_%s_%s_npixel", exi[i], exj[j]), TString::Format("%s tracks%s;tracks npixel;arb. units", exi[i], exj[j]), 40, 0, 40);
+      h_tracks_nstrip[i][j] = fs->make<TH1F>(TString::Format("h_tracks_%s_%s_nstrip", exi[i], exj[j]), TString::Format("%s tracks%s;tracks nstrip;arb. units", exi[i], exj[j]), 40, 0, 40);
+
+      h_tracks_dxyerr_eta[i][j] = fs->make<TH2F>(TString::Format("h_tracks_%s_%s_dxyerr_eta", exi[i], exj[j]), TString::Format("%s tracks%s;eta;dxyerr", exi[i], exj[j]), 50, -4.00, 4.00, 1000, 0, 2);
+      h_tracks_dxyerr_phi[i][j] = fs->make<TH2F>(TString::Format("h_tracks_%s_%s_dxyerr_phi", exi[i], exj[j]), TString::Format("%s tracks%s;phi;dxyerr", exi[i], exj[j]), 50, -4.00, 4.00, 1000, 0, 2);
+      h_tracks_dzerr_eta[i][j] = fs->make<TH2F>(TString::Format("h_tracks_%s_%s_dzerr_eta", exi[i], exj[j]), TString::Format("%s tracks%s;eta;dzerr", exi[i], exj[j]), 50, -4.00, 4.00, 1000, 0, 2);
+      h_tracks_dzerr_phi[i][j] = fs->make<TH2F>(TString::Format("h_tracks_%s_%s_dzerr_phi", exi[i], exj[j]), TString::Format("%s tracks%s;phi;dzerr", exi[i], exj[j]), 50, -4.00, 4.00, 1000, 0, 2);
+
+      for (int k = 0; k < 11; ++k) {
+        h_tracks_eta_phi_dxyerr_eq[i][j][k] = fs->make<TH2F>(TString::Format("h_tracks_%s_%s_eta_phi_dxyerr_eq_%d", exi[i], exj[j], k), TString::Format("%s tracks%s w/ %4.3f <= dxyerr < %4.3f;phi;eta", exi[i], exj[j], 0.005*k, 0.005*(k+1)), 50, -3.15, 3.15, 50, -4, 4);
+      }
+      for (int k = 0; k < 11; ++k) {
+        h_tracks_eta_phi_dxyerr_lt[i][j][k] = fs->make<TH2F>(TString::Format("h_tracks_%s_%s_eta_phi_dxyerr_lt_%d", exi[i], exj[j], k), TString::Format("%s tracks%s w/ dxyerr < %4.3f;phi;eta", exi[i], exj[j], 0.005*(k+1)), 50, -3.15, 3.15, 50, -4, 4);
+      }
+      for (int k = 0; k < 11; ++k) {
+        h_tracks_eta_phi_dzerr_eq[i][j][k] = fs->make<TH2F>(TString::Format("h_tracks_%s_%s_eta_phi_dzerr_eq_%d", exi[i], exj[j], k), TString::Format("%s tracks%s w/ %4.3f <= dzerr < %4.3f;phi;eta", exi[i], exj[j], 0.005*k, 0.005*(k+1)), 50, -3.15, 3.15, 50, -4, 4);
+      }
+      for (int k = 0; k < 11; ++k) {
+        h_tracks_eta_phi_dzerr_lt[i][j][k] = fs->make<TH2F>(TString::Format("h_tracks_%s_%s_eta_phi_dzerr_lt_%d", exi[i], exj[j], k), TString::Format("%s tracks%s w/ dzerr < %4.3f;phi;eta", exi[i], exj[j], 0.005*(k+1)), 50, -3.15, 3.15, 50, -4, 4);
+      }
 
       h_tracks_npixel_eta[i][j] = fs->make<TH2F>(TString::Format("h_tracks_%s_%s_npixel_eta", exi[i], exj[j]), TString::Format("%s tracks%s;eta;number of pixel hits", exi[i], exj[j]), 50, -4.00, 4.00, 40, 0, 40);
       h_tracks_npixel_phi[i][j] = fs->make<TH2F>(TString::Format("h_tracks_%s_%s_npixel_phi", exi[i], exj[j]), TString::Format("%s tracks%s;phi;number of pixel hits", exi[i], exj[j]), 50, -3.15, 3.15, 40, 0, 40);
@@ -84,6 +120,24 @@ void EffCheck::analyze(const edm::Event& event, const edm::EventSetup& setup) {
         h_tracks_pt[i][j]->Fill(tk.pt());
         h_tracks_eta[i][j]->Fill(tk.eta());
         h_tracks_phi[i][j]->Fill(tk.phi());
+        h_tracks_dxyerr[i][j]->Fill(tk.dxyError());
+        h_tracks_dzerr[i][j]->Fill(tk.dzError());
+        h_tracks_npixel[i][j]->Fill(tk.hitPattern().numberOfValidPixelHits());
+        h_tracks_nstrip[i][j]->Fill(tk.hitPattern().numberOfValidStripHits());
+
+        h_tracks_dxyerr_eta[i][j]->Fill(tk.eta(), tk.dxyError());
+        h_tracks_dxyerr_phi[i][j]->Fill(tk.phi(), tk.dxyError());
+        h_tracks_dzerr_eta[i][j]->Fill(tk.eta(), tk.dzError());
+        h_tracks_dzerr_phi[i][j]->Fill(tk.phi(), tk.dzError());
+
+        for (int k = 0; k < 11; ++k) {
+          if ((tk.dxyError() >= 0.005*k && tk.dxyError() < 0.005*(k+1)) || (tk.dxyError() >= 0.05 && k==10)) h_tracks_eta_phi_dxyerr_eq[i][j][k]->Fill(tk.phi(), tk.eta());
+          if (tk.dxyError() < 0.005*(k+1) || k==10) h_tracks_eta_phi_dxyerr_lt[i][j][k]->Fill(tk.phi(), tk.eta());
+        }
+        for (int k = 0; k < 11; ++k) {
+          if ((tk.dzError() >= 0.005*k && tk.dzError() < 0.005*(k+1)) || (tk.dzError() >= 0.05 && k==10)) h_tracks_eta_phi_dzerr_eq[i][j][k]->Fill(tk.phi(), tk.eta());
+          if (tk.dzError() < 0.005*(k+1) || k==10) h_tracks_eta_phi_dzerr_lt[i][j][k]->Fill(tk.phi(), tk.eta());
+        }
 
         h_tracks_npixel_eta[i][j]->Fill(tk.eta(), tk.hitPattern().numberOfValidPixelHits());
         h_tracks_npixel_phi[i][j]->Fill(tk.phi(), tk.hitPattern().numberOfValidPixelHits());
