a3e09815947fab3e81331759548d48366b619318
Jordan Tucker <jordantucker@gmail.com>
2014-05-13 10:46:32 -0500

    Refit vertices, dropping tracks.

---
+++ MFVNeutralino/python/VertexRefitter_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms
+
+mfvVertexRefits = cms.EDProducer('MFVVertexRefitter',
+                                 beamspot_src = cms.InputTag('offlineBeamSpot'),
+                                 vertex_src = cms.InputTag('mfvSelectedVerticesTight'),
+                                 n_tracks_to_drop = cms.uint32(1),
+                                 sort_tracks_by = cms.string('dxyerr'),
+                                 histos = cms.untracked.bool(False),
+                                 verbose = cms.untracked.bool(False),
+                                 )
+
+
