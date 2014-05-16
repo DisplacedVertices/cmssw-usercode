b68e5e1530824ca9d67c061835441161d1f004a9
Jordan Tucker <jordantucker@gmail.com>
2014-05-16 13:49:33 -0500

    use_rechits option.

--- Tools/test/tracker_mapper.py
+++ Tools/test/tracker_mapper.py
@@ -6,13 +6,20 @@ process.TFileService.fileName = 'tracker_mapper.root'
 
 #process.source.fileNames = ['/store/user/tucker/RelValSingleMuPt1000_CMSSW_5_3_6-START53_V14-v2_GEN-SIM-RECO.7C1AEF1C-FF29-E211-BA60-003048678B20.root']
 
-process.TrackerMapper = cms.EDAnalyzer('TrackerMapper', track_src = cms.InputTag('generalTracks'))
+process.TrackerMapper = cms.EDAnalyzer('TrackerMapper',
+                                       track_src = cms.InputTag('generalTracks'),
+                                       use_rechits = cms.bool(False)
+                                       )
 process.p = cms.Path(process.TrackerMapper)
 
-if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
-    from JMTucker.Tools.Samples import ttbarhadronic, myttbar_samples, data_samples
+if 1:
+    process.source.fileNames = ['file:/uscms/home/tucker/scratch/reco_1_2_L1S.root']
+    process.TrackerMapper.use_rechits = True
 
-    for s in data_samples:
+if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
+    import JMTucker.Tools.Samples as Samples
+
+    for s in Samples.data_samples:
         s.json = 'ana_5pc.json'
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
@@ -20,4 +27,4 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
                        total_number_of_events = -1,
                        events_per_job = 500000,
                        )
-    cs.submit_all([ttbarhadronic])
+    cs.submit_all([Samples.myttbarpydesignnoputkex])
