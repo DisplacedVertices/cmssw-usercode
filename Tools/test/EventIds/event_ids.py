d79774a2ec772317aa336bb3e04cca5bf57be76c
Jordan Tucker <jordantucker@gmail.com>
2014-05-21 19:41:10 -0500

    V18.

--- Tools/test/EventIds/event_ids.py
+++ Tools/test/EventIds/event_ids.py
@@ -2,7 +2,7 @@ import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
 
 from JMTucker.Tools import SampleFiles
-SampleFiles.setup(process, 'MFVNtupleV17', 'mfv_neutralino_tau1000um_M0400', 500)
+SampleFiles.setup(process, 'MFVNtupleV18', 'mfv_neutralino_tau1000um_M0400', 500)
 
 process.TFileService.fileName = cms.string('evids.root')
 
@@ -19,9 +19,9 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     from JMTucker.Tools.SampleFiles import SampleFiles
 
-    cs = CRABSubmitter('EventIdsV17',
+    cs = CRABSubmitter('EventIdsV18',
                        total_number_of_events = -1,
                        events_per_job = 234567,
-                       manual_datasets = SampleFiles['MFVNtupleV17'],
+                       manual_datasets = SampleFiles['MFVNtupleV18'],
                        )
     cs.submit_all(samples)
