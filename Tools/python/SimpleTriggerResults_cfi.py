6c6a563f605b11d4ac8d95bd66691e2985d14f0e
Jordan Tucker <jordantucker@gmail.com>
2014-05-28 01:31:39 -0500

    setup_endpath.

--- Tools/python/SimpleTriggerResults_cfi.py
+++ Tools/python/SimpleTriggerResults_cfi.py
@@ -5,3 +5,8 @@ SimpleTriggerResults = cms.EDAnalyzer('SimpleTriggerResults',
                                       weight_src = cms.InputTag(''),
                                       deversion = cms.bool(True)
                                       )
+
+def setup_endpath(process, weight_src = ''):
+    process.SimpleTriggerResults = SimpleTriggerResults.clone(weight_src = weight_src)
+    process.SimpleTriggerResults.trigger_results_src = cms.InputTag('TriggerResults', '', process.name_())
+    process.epSimpleTriggerResults = cms.EndPath(process.SimpleTriggerResults)
