fe558a7acaeee4d218cc6ad74b81428723c63835
Jordan Tucker <jordantucker@gmail.com>
2014-05-28 01:14:28 -0500

    Allow a weight.

--- Tools/python/SimpleTriggerResults_cfi.py
+++ Tools/python/SimpleTriggerResults_cfi.py
@@ -2,5 +2,6 @@ import sys, FWCore.ParameterSet.Config as cms
 
 SimpleTriggerResults = cms.EDAnalyzer('SimpleTriggerResults',
                                       trigger_results_src = cms.InputTag('TriggerResults', '', 'HLT'),
+                                      weight_src = cms.InputTag(''),
                                       deversion = cms.bool(True)
                                       )
