7f5b0bf1ff8fc8465096ac683ecf0b39bf3d7ea5
Jordan Tucker <jordantucker@gmail.com>
2014-05-29 19:20:52 -0500

    Write filtered analysis ntuple with sample info for combining into one big file for easy use with TTree.Scan/etc.

--- MFVNeutralino/test/minintuple.py
+++ MFVNeutralino/test/minintuple.py
@@ -1,39 +1,69 @@
 import sys
-from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
-from JMTucker.Tools import SampleFiles
-
-SampleFiles.setup(process, 'MFVNtupleV15', 'mfv_neutralino_tau1000um_M0400', 5000)
+from JMTucker.Tools.BasicAnalyzer_cfg import *
+process.setName_('Mini')
 del process.TFileService
+
+import JMTucker.Tools.Samples as Samples
+s = Samples.qcdht1000
+
+from JMTucker.Tools import SampleFiles
+SampleFiles.setup(process, 'MFVNtupleV18', s.name, 50000)
 
 process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
 process.load('JMTucker.MFVNeutralino.AnalysisCuts_cfi')
 
-process.p = cms.Path(process.mfvSelectedVerticesTight * process.mfvAnalysisCuts)
+process.mfvAnalysisCuts.min_nvertex = 1
+
+process.mfvSampleInfo = cms.EDProducer('SampleInfoProducer',
+                                       sample = cms.string(s.name),
+                                       numEvents = cms.int32(s.nevents),
+                                       crossSection = cms.double(s.cross_section),
+                                       intLumi = cms.double(20000),
+                                       )
+
+process.p = cms.Path(process.mfvSelectedVerticesSeq * process.mfvAnalysisCuts * process.mfvSampleInfo)
 
 process.out = cms.OutputModule('PoolOutputModule',
-                               fileName = cms.untracked.string('ntuple_filtered.root'),
+                               fileName = cms.untracked.string('minintuple.root'),
                                SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('p')),
                                outputCommands = cms.untracked.vstring(
                                    'drop *',
                                    'keep MFVEvent_mfvEvent__*',
-                                   'keep MFVVertexAuxs_mfvSelectedVerticesTight__*',
+                                   'keep MFVVertexAuxs_mfvSelectedVerticesMedium__*',
+                                   'keep *_mfvSampleInfo_*_*',
                                    )
                                )
 process.outp = cms.EndPath(process.out)
 
+process.options.emptyRunLumiMode = cms.untracked.string('doNotHandleEmptyRunsAndLumis')
+process.source.inputCommands = cms.untracked.vstring('keep *', 'drop *_MEtoEDMConverter_*_*')
+process.out.outputCommands += ['drop LumiDetails_lumiProducer_*_*', 'drop LumiSummary_lumiProducer_*_*', 'drop RunSummary_lumiProducer_*_*']
+process.out.dropMetaData = cms.untracked.string('ALL')
+
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
-    import JMTucker.Tools.Samples as Samples
-    samples = Samples.ttbar_samples + Samples.qcd_samples #+ Samples.leptonic_background_samples
-    samples += [Samples.mfv_neutralino_tau0100um_M0400, Samples.mfv_neutralino_tau1000um_M0400, Samples.mfv_neutralino_tau9900um_M0400]
+    samples = Samples.from_argv([Samples.mfv_neutralino_tau0100um_M0400,
+                                 Samples.mfv_neutralino_tau1000um_M0400,
+                                 Samples.mfv_neutralino_tau0300um_M0400,
+                                 Samples.mfv_neutralino_tau9900um_M0400] + Samples.ttbar_samples + Samples.qcd_samples)
+
+    def modify(sample):
+        to_add = []
+        to_replace = []
+
+        to_add.append('''
+process.mfvSampleInfo.sample = %(name)s
+process.mfvSampleInfo.numEvents = %(nevents)i
+process.mfvSampleInfo.crossSection = %(cross_section)g
+''' % sample)
+
+        return to_add, to_replace
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
-    from JMTucker.Tools.SampleFiles import SampleFiles
-
-    cs = CRABSubmitter('MFVFilteredNtupleV15',
+    cs = CRABSubmitter('MiniNtupleV18',
+                       pset_modifier = modify,
                        total_number_of_events = -1,
                        events_per_job = 200000,
                        get_edm_output = True,
-                       data_retrieval = 'fnal_eos',
-                       manual_datasets = SampleFiles['MFVNtupleV15'],
+                       data_retrieval = 'fnal',
                        )
     cs.submit_all(samples)
