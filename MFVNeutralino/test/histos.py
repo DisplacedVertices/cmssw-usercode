fcb2d6434cfbe5927e96fcba95a627f468eeb249
Jordan Tucker <jordantucker@gmail.com>
2014-05-29 19:40:01 -0500

    Submit script: use Samples.from_argv, clean up ex(n).

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -57,18 +57,18 @@ for name, cut in nm1s:
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
-    samples = [Samples.mfv_neutralino_tau0100um_M0400, Samples.mfv_neutralino_tau0300um_M0400, Samples.mfv_neutralino_tau1000um_M0400, Samples.mfv_neutralino_tau9900um_M0400] + Samples.ttbar_samples + Samples.qcd_samples + Samples.data_samples
+    samples = Samples.from_argv([Samples.mfv_neutralino_tau0100um_M0400,
+                                 Samples.mfv_neutralino_tau1000um_M0400,
+                                 Samples.mfv_neutralino_tau0300um_M0400,
+                                 Samples.mfv_neutralino_tau9900um_M0400] + Samples.ttbar_samples + Samples.qcd_samples)
+
     for s in Samples.data_samples:
         s.json = 'ana_5pc.json'
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
-
-    ex = ''
-    exn = ''
-    cs = CRABSubmitter('HistosV18' + ex + exn,
+    cs = CRABSubmitter('HistosV18',
                        job_control_from_sample = True,
                        use_ana_dataset = True,
                        run_half_mc = True,
                        )
-
     cs.submit_all(samples)
