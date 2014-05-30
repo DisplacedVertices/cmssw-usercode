57b8949f253f4bcd6415b9a6b12c4f528110226d
Jordan Tucker <jordantucker@gmail.com>
2014-05-30 13:01:52 -0500

    Write more parts of the steps, and read extra weight from event.

--- Tools/plugins/SampleInfoProducer.cc
+++ Tools/plugins/SampleInfoProducer.cc
@@ -22,36 +22,51 @@ private:
   }
 
   const int sample;
-  const int numEvents;
-  const double crossSection;
-  const double partialWeight;
-  const double intLumi;
-  const double weight;
+  const int num_events;
+  const double cross_section;
+  const double partial_weight;
+  const double lumi_weight;
+  const edm::InputTag extra_weight_src;
 };
 
 SampleInfoProducer::SampleInfoProducer(const edm::ParameterSet& cfg)
   : sample(sample2number(cfg.getParameter<std::string>("sample"))),
-    numEvents(cfg.getParameter<int>("numEvents")),
-    crossSection(cfg.getParameter<double>("crossSection")),
-    partialWeight(float(crossSection) / numEvents),
-    intLumi(cfg.getParameter<double>("intLumi")),
-    weight(partialWeight * intLumi)
+    num_events(cfg.getParameter<int>("num_events")),
+    cross_section(cfg.getParameter<double>("cross_section")),
+    partial_weight(float(cross_section) / num_events),
+    lumi_weight(partial_weight * cfg.getParameter<double>("int_lumi")),
+    extra_weight_src(cfg.getParameter<edm::InputTag>("extra_weight_src"))
 {
   produces<int>("sample");
   produces<int>("numEvents");
   produces<double>("crossSection");
+  produces<double>("partialWeight");
+  produces<double>("lumiWeight");
+  produces<double>("extraWeight");
   produces<double>("weight");
 }
 
 void SampleInfoProducer::produce(edm::Event& event, const edm::EventSetup&) {
   std::auto_ptr<int> p_sample(new int(sample));
-  std::auto_ptr<int> p_numEvents(new int(numEvents));
-  std::auto_ptr<double> p_crossSection(new double(crossSection));
-  std::auto_ptr<double> p_weight(new double(weight));
+  std::auto_ptr<int> p_num_events(new int(num_events));
+  std::auto_ptr<double> p_cross_section(new double(cross_section));
+  std::auto_ptr<double> p_partial_weight(new double(partial_weight));
+  std::auto_ptr<double> p_lumi_weight(new double(lumi_weight));
+  std::auto_ptr<double> p_extra_weight(new double(1));
+  std::auto_ptr<double> p_weight(new double(lumi_weight));
+
+  if (extra_weight_src.label() != "") {
+    edm::Handle<double> extra_weight;
+    event.getByLabel(extra_weight_src, extra_weight);
+    *p_extra_weight = *extra_weight;
+    *p_weight *= *extra_weight;
+  }
 
   event.put(p_sample, "sample");
-  event.put(p_numEvents, "numEvents");
-  event.put(p_crossSection, "crossSection");
+  event.put(p_num_events, "numEvents");
+  event.put(p_cross_section, "crossSection");
+  event.put(p_partial_weight, "partialWeight");
+  event.put(p_extra_weight, "extraWeight");
   event.put(p_weight, "weight");
 }
 
