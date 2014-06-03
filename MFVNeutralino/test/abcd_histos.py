09b11d09e0bf10685c989875dffbc3df6926970f
Jordan Tucker <jordantucker@gmail.com>
2014-06-03 18:44:48 -0500

    V18.

--- MFVNeutralino/test/abcd_histos.py
+++ MFVNeutralino/test/abcd_histos.py
@@ -2,7 +2,7 @@ import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
 from JMTucker.Tools import SampleFiles
 
-SampleFiles.setup(process, 'MFVNtupleV17', 'mfv_neutralino_tau1000um_M0400', 500)
+SampleFiles.setup(process, 'MFVNtupleV18', 'mfv_neutralino_tau1000um_M0400', 500)
 process.TFileService.fileName = 'abcd_histos.root'
 
 process.load('JMTucker.MFVNeutralino.WeightProducer_cfi')
@@ -23,18 +23,16 @@ process.p = cms.Path(process.mfvWeight * process.mfvSelectedVerticesTight * proc
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
-    bkg_samples = Samples.ttbar_samples + Samples.qcd_samples
-    samples = [Samples.mfv_neutralino_tau0100um_M0400, Samples.mfv_neutralino_tau0300um_M0400, Samples.mfv_neutralino_tau1000um_M0400, Samples.mfv_neutralino_tau9900um_M0400] + bkg_samples
-    for sample in bkg_samples:
-        sample.total_events = int(sample.nevents_orig/2 * sample.ana_filter_eff)
+    samples = Samples.from_argv([Samples.mfv_neutralino_tau0100um_M0400,
+                                 Samples.mfv_neutralino_tau1000um_M0400,
+                                 Samples.mfv_neutralino_tau0300um_M0400,
+                                 Samples.mfv_neutralino_tau9900um_M0400] + Samples.ttbar_samples + Samples.qcd_samples)
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
-    from JMTucker.Tools.SampleFiles import SampleFiles
-
-    cs = CRABSubmitter('ABCDHistosV17',
+    cs = CRABSubmitter('ABCDHistosV18',
                        job_control_from_sample = True,
                        use_ana_dataset = True,
-                       manual_datasets = SampleFiles['MFVNtupleV17'],
+                       run_half_mc = True,
                        )
     cs.submit_all(samples)
 
