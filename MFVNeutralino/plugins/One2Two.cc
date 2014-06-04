9d20464ad8100e9fd9f93ceb74815c618249abc4
Jordan Tucker <jordantucker@gmail.com>
2014-06-03 21:21:48 -0500

    Rejigger to use TTree instead of ascii file.

--- MFVNeutralino/plugins/One2Two.cc
+++ MFVNeutralino/plugins/One2Two.cc
@@ -1,6 +1,7 @@
 #include "TF1.h"
 #include "TH2F.h"
 #include "TRandom3.h"
+#include "TTree.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "DataFormats/Math/interface/deltaPhi.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
@@ -46,13 +47,9 @@ public:
   bool sel_vertex_aft(const MFVVertexAux&) const;
   MFVVertexAux xform_vertex(const MFVEvent&, const MFVVertexAux&) const;
 
-  TF1* f_dphi;
-  TH1F* h_fcn_dphi;
   double prob_dphi(const double) const;
   double prob_dphi(const MFVVertexAux&, const MFVVertexAux&) const;
 
-  TF1* f_dz;
-  TH1F* h_fcn_dz;
   double prob_dz(const double) const;
   double prob_dz(const MFVVertexAux&, const MFVVertexAux&) const;
 
@@ -60,6 +57,7 @@ public:
   void endJob();
 
   const std::string filename;
+  const std::string tree_path;
   const edm::InputTag event_src;
   const edm::InputTag vertex_src;
   const bool wrep;
@@ -75,6 +73,11 @@ public:
 
   TH1F* h_nsv;
   TH1F* h_nsvsel;
+
+  TF1* f_dphi;
+  TH1F* h_fcn_dphi;
+  TF1* f_dz;
+  TH1F* h_fcn_dz;
 
   TH2F* h_2v_xy;
   TH1F* h_2v_bs2ddist;
@@ -107,10 +110,28 @@ public:
   TH1F* h_1v_abs_dphi;
   TH2F* h_1v_svdz_v_dphi;
   TH2F* h_1v_svdz_all_v_dphi;
+
+  struct tree_t {
+    unsigned run;
+    unsigned lumi;
+    unsigned event;
+    unsigned char nvtx;
+    unsigned char ntk0;
+    float x0;
+    float y0;
+    float z0;
+    unsigned char ntk1;
+    float x1;
+    float y1;
+    float z1;
+  };
+  tree_t nt;
+  TTree* tree;
 };
 
 MFVOne2Two::MFVOne2Two(const edm::ParameterSet& cfg)
   : filename(cfg.getParameter<std::string>("filename")),
+    tree_path(cfg.getParameter<std::string>("tree_path")),
     event_src(cfg.getParameter<edm::InputTag>("event_src")),
     vertex_src(cfg.getParameter<edm::InputTag>("vertex_src")),
     wrep(cfg.getParameter<bool>("wrep")),
@@ -119,7 +140,9 @@ MFVOne2Two::MFVOne2Two(const edm::ParameterSet& cfg)
     min_ntracks_aft(cfg.getParameter<int>("min_ntracks_aft")),
     use_f_dz(cfg.getParameter<bool>("use_f_dz")),
     max_1v_dz(cfg.getParameter<double>("max_1v_dz")),
-    max_1v_ntracks(cfg.getParameter<int>("max_1v_ntracks"))
+    max_1v_ntracks(cfg.getParameter<int>("max_1v_ntracks")),
+    f_dphi(0),
+    f_dz(0)
 {
   edm::Service<TFileService> fs;
   gRandom = new TRandom3(121982);
@@ -127,45 +150,62 @@ MFVOne2Two::MFVOne2Two(const edm::ParameterSet& cfg)
   h_nsv = new TH1F("h_nsv", "", 10, 0, 10);
   h_nsvsel = new TH1F("h_nsvsel", "", 10, 0, 10);
 
-  f_dphi = new TF1("f_dphi", "x*x*x*x/122.4078739141", -M_PI, M_PI);
-  h_fcn_dphi = fs->make<TH1F>("h_fcn_dphi", "", 10, -M_PI, M_PI);
-  h_fcn_dphi->FillRandom("f_dphi", 100000);
+  if (filename == "") {
+    tree = fs->make<TTree>("t", "");
+    tree->Branch("run", &nt.run, "run/i");
+    tree->Branch("lumi", &nt.lumi, "lumi/i");
+    tree->Branch("event", &nt.event, "event/i");
+    tree->Branch("nvtx", &nt.nvtx, "nvtx/b");
+    tree->Branch("ntk0", &nt.ntk0, "ntk0/b");
+    tree->Branch("x0", &nt.x0, "x0/F");
+    tree->Branch("y0", &nt.y0, "y0/F");
+    tree->Branch("z0", &nt.z0, "z0/F");
+    tree->Branch("ntk1", &nt.ntk1, "ntk1/b");
+    tree->Branch("x1", &nt.x1, "x1/F");
+    tree->Branch("y1", &nt.y1, "y1/F");
+    tree->Branch("z1", &nt.z1, "z1/F");
+  }
+  else {
+    f_dphi = new TF1("f_dphi", "x*x*x*x/122.4078739141", -M_PI, M_PI);
+    h_fcn_dphi = fs->make<TH1F>("h_fcn_dphi", "", 10, -M_PI, M_PI);
+    h_fcn_dphi->FillRandom("f_dphi", 100000);
 
-  f_dz = new TF1("f_dz", "1/sqrt(2*3.14159265*0.01635**2)*exp(-x*x/2/0.01635**2)", -50, 50);
-  h_fcn_dz = fs->make<TH1F>("h_fcn_dz", "", 10, -0.1, 0.1);
-  h_fcn_dz->FillRandom("f_dz", 100000);
+    f_dz = new TF1("f_dz", "1/sqrt(2*3.14159265*0.01635**2)*exp(-x*x/2/0.01635**2)", -50, 50);
+    h_fcn_dz = fs->make<TH1F>("h_fcn_dz", "", 10, -0.1, 0.1);
+    h_fcn_dz->FillRandom("f_dz", 100000);
 
-  h_2v_xy = fs->make<TH2F>("h_2v_xy", "", 100, -0.05, 0.05, 100, 0.05, 0.05);
-  h_2v_bs2ddist = fs->make<TH1F>("h_2v_bs2ddist", "", 100, 0, 0.1);
-  h_2v_bs2ddist_v_bsdz = fs->make<TH2F>("h_2v_bs2ddist_v_bsdz", "", 200, -20, 20, 100, 0, 0.1);
-  h_2v_bsdz = fs->make<TH1F>("h_2v_bsdz", "", 200, -20, 20);
-  h_2v_bs2ddist_0 = fs->make<TH1F>("h_2v_bs2ddist_0", "", 100, 0, 0.1);
-  h_2v_bs2ddist_v_bsdz_0 = fs->make<TH2F>("h_2v_bs2ddist_v_bsdz_0", "", 200, -20, 20, 100, 0, 0.1);
-  h_2v_bsdz_0 = fs->make<TH1F>("h_2v_bsdz_0", "", 200, -20, 20);
-  h_2v_bs2ddist_1 = fs->make<TH1F>("h_2v_bs2ddist_1", "", 100, 0, 0.1);
-  h_2v_bs2ddist_v_bsdz_1 = fs->make<TH2F>("h_2v_bs2ddist_v_bsdz_1", "", 200, -20, 20, 100, 0, 0.1);
-  h_2v_bsdz_1 = fs->make<TH1F>("h_2v_bsdz_1", "", 200, -20, 20);
-  h_2v_ntracks = fs->make<TH2F>("h_2v_ntracks", "", 20, 0, 20, 20, 0, 20);
-  h_2v_ntracks01 = fs->make<TH1F>("h_2v_ntracks01", "", 30, 0, 30);
-  h_2v_svdist2d = fs->make<TH1F>("h_2v_svdist2d", "", 100, 0, 0.1);
-  h_2v_svdz = fs->make<TH1F>("h_2v_svdz", "", 50, -0.1, 0.1);
-  h_2v_dphi = fs->make<TH1F>("h_2v_dphi", "", 10, -M_PI, M_PI);
-  h_2v_abs_dphi = fs->make<TH1F>("h_2v_abs_dphi", "", 10, 0, M_PI);
-  h_2v_svdz_v_dphi = fs->make<TH2F>("h_2v_svdz_v_dphi", "", 10, -M_PI, M_PI, 50, -0.1, 0.1);
+    h_2v_xy = fs->make<TH2F>("h_2v_xy", "", 100, -0.05, 0.05, 100, 0.05, 0.05);
+    h_2v_bs2ddist = fs->make<TH1F>("h_2v_bs2ddist", "", 100, 0, 0.1);
+    h_2v_bs2ddist_v_bsdz = fs->make<TH2F>("h_2v_bs2ddist_v_bsdz", "", 200, -20, 20, 100, 0, 0.1);
+    h_2v_bsdz = fs->make<TH1F>("h_2v_bsdz", "", 200, -20, 20);
+    h_2v_bs2ddist_0 = fs->make<TH1F>("h_2v_bs2ddist_0", "", 100, 0, 0.1);
+    h_2v_bs2ddist_v_bsdz_0 = fs->make<TH2F>("h_2v_bs2ddist_v_bsdz_0", "", 200, -20, 20, 100, 0, 0.1);
+    h_2v_bsdz_0 = fs->make<TH1F>("h_2v_bsdz_0", "", 200, -20, 20);
+    h_2v_bs2ddist_1 = fs->make<TH1F>("h_2v_bs2ddist_1", "", 100, 0, 0.1);
+    h_2v_bs2ddist_v_bsdz_1 = fs->make<TH2F>("h_2v_bs2ddist_v_bsdz_1", "", 200, -20, 20, 100, 0, 0.1);
+    h_2v_bsdz_1 = fs->make<TH1F>("h_2v_bsdz_1", "", 200, -20, 20);
+    h_2v_ntracks = fs->make<TH2F>("h_2v_ntracks", "", 20, 0, 20, 20, 0, 20);
+    h_2v_ntracks01 = fs->make<TH1F>("h_2v_ntracks01", "", 30, 0, 30);
+    h_2v_svdist2d = fs->make<TH1F>("h_2v_svdist2d", "", 100, 0, 0.1);
+    h_2v_svdz = fs->make<TH1F>("h_2v_svdz", "", 50, -0.1, 0.1);
+    h_2v_dphi = fs->make<TH1F>("h_2v_dphi", "", 10, -M_PI, M_PI);
+    h_2v_abs_dphi = fs->make<TH1F>("h_2v_abs_dphi", "", 10, 0, M_PI);
+    h_2v_svdz_v_dphi = fs->make<TH2F>("h_2v_svdz_v_dphi", "", 10, -M_PI, M_PI, 50, -0.1, 0.1);
 
-  h_1v_xy = fs->make<TH2F>("h_1v_xy", "", 100, -0.05, 0.05, 100, 0.05, 0.05);
-  h_1v_bs2ddist = fs->make<TH1F>("h_1v_bs2ddist", "", 100, 0, 0.1);
-  h_1v_bs2ddist_v_bsdz = fs->make<TH2F>("h_1v_bs2ddist_v_bsdz", "", 200, -20, 20, 100, 0, 0.1);
-  h_1v_bsdz = fs->make<TH1F>("h_1v_bsdz", "", 200, -20, 20);
-  h_1v_ntracks = fs->make<TH2F>("h_1v_ntracks", "", 20, 0, 20, 20, 0, 20);
-  h_1v_ntracks01 = fs->make<TH1F>("h_1v_ntracks01", "", 30, 0, 30);
-  h_1v_svdist2d = fs->make<TH1F>("h_1v_svdist2d", "", 100, 0, 0.1);
-  h_1v_svdz = fs->make<TH1F>("h_1v_svdz", "", 50, -0.1, 0.1);
-  h_1v_svdz_all = fs->make<TH1F>("h_1v_svdz_all", "", 200, -10, 10);
-  h_1v_dphi = fs->make<TH1F>("h_1v_dphi", "", 10, -M_PI, M_PI);
-  h_1v_abs_dphi = fs->make<TH1F>("h_1v_abs_dphi", "", 10, 0, M_PI);
-  h_1v_svdz_v_dphi = fs->make<TH2F>("h_1v_svdz_v_dphi", "", 10, -M_PI, M_PI, 50, -0.1, 0.1);
-  h_1v_svdz_all_v_dphi = fs->make<TH2F>("h_1v_svdz_all_v_dphi", "", 10, -M_PI, M_PI, 200, -10, 10);
+    h_1v_xy = fs->make<TH2F>("h_1v_xy", "", 100, -0.05, 0.05, 100, 0.05, 0.05);
+    h_1v_bs2ddist = fs->make<TH1F>("h_1v_bs2ddist", "", 100, 0, 0.1);
+    h_1v_bs2ddist_v_bsdz = fs->make<TH2F>("h_1v_bs2ddist_v_bsdz", "", 200, -20, 20, 100, 0, 0.1);
+    h_1v_bsdz = fs->make<TH1F>("h_1v_bsdz", "", 200, -20, 20);
+    h_1v_ntracks = fs->make<TH2F>("h_1v_ntracks", "", 20, 0, 20, 20, 0, 20);
+    h_1v_ntracks01 = fs->make<TH1F>("h_1v_ntracks01", "", 30, 0, 30);
+    h_1v_svdist2d = fs->make<TH1F>("h_1v_svdist2d", "", 100, 0, 0.1);
+    h_1v_svdz = fs->make<TH1F>("h_1v_svdz", "", 50, -0.1, 0.1);
+    h_1v_svdz_all = fs->make<TH1F>("h_1v_svdz_all", "", 200, -10, 10);
+    h_1v_dphi = fs->make<TH1F>("h_1v_dphi", "", 10, -M_PI, M_PI);
+    h_1v_abs_dphi = fs->make<TH1F>("h_1v_abs_dphi", "", 10, 0, M_PI);
+    h_1v_svdz_v_dphi = fs->make<TH2F>("h_1v_svdz_v_dphi", "", 10, -M_PI, M_PI, 50, -0.1, 0.1);
+    h_1v_svdz_all_v_dphi = fs->make<TH2F>("h_1v_svdz_all_v_dphi", "", 10, -M_PI, M_PI, 200, -10, 10);
+  }
 }
 
 MFVOne2Two::~MFVOne2Two() {
@@ -219,6 +259,11 @@ void MFVOne2Two::analyze(const edm::Event& event, const edm::EventSetup&) {
   if (!sel_event(*mevent))
     return;
 
+  memset(&nt, 0, sizeof(tree_t));
+  nt.run   = event.id().run();
+  nt.lumi  = event.luminosityBlock();
+  nt.event = event.id().event();
+
   edm::Handle<MFVVertexAuxCollection> input_vertices;
   event.getByLabel(vertex_src, input_vertices);
 
@@ -234,48 +279,70 @@ void MFVOne2Two::analyze(const edm::Event& event, const edm::EventSetup&) {
   if (vertices.size() == 1) {
     const MFVVertexAux& v0 = vertices[0];
     one_vertices.push_back(v0);
-    printf("TREETHIS1v %i %.6g %.6g %.6g\n", v0.ntracks(), v0.x, v0.y, v0.z);
+
+    nt.nvtx = 1;
+    nt.ntk0 = v0.ntracks();
+    nt.x0 = v0.x;
+    nt.y0 = v0.y;
+    nt.z0 = v0.z;
   }
   else if (vertices.size() >= 2) {
     const MFVVertexAux& v0 = vertices[0];
     const MFVVertexAux& v1 = vertices[1];
     two_vertices.push_back(std::make_pair(v0, v1));
-    printf("TREETHIS2v %i %.6g %.6g %.6g %i %.6g %.6g %.6g\n", v0.ntracks(), v0.x, v0.y, v0.z, v1.ntracks(), v1.x, v1.y, v1.z);
+
+    nt.nvtx = 2;
+    nt.ntk0 = v0.ntracks();
+    nt.ntk1 = v1.ntracks();
+    nt.x0 = v0.x; nt.y0 = v0.y; nt.z0 = v0.z;
+    nt.x1 = v1.x; nt.y1 = v1.y; nt.z1 = v1.z;
   }
+  else
+    return;
+
+  tree->Fill();
 }
 
 void MFVOne2Two::endJob() {
   if (filename != "") {
-    FILE* f = fopen(filename.c_str(), "rt");
+    TFile* f = new TFile(filename.c_str());
     if (!f)
       throw cms::Exception("One2Two") << "could not read file " << filename;
+    tree = (TTree*)f->Get(tree_path.c_str());
+    if (!tree)
+      throw cms::Exception("One2Two") << "could not get " << tree_path << " from file " << filename;
+    tree->SetBranchAddress("run", &nt.run);
+    tree->SetBranchAddress("lumi", &nt.lumi);
+    tree->SetBranchAddress("event", &nt.event);
+    tree->SetBranchAddress("nvtx", &nt.nvtx);
+    tree->SetBranchAddress("ntk0", &nt.ntk0);
+    tree->SetBranchAddress("x0", &nt.x0);
+    tree->SetBranchAddress("y0", &nt.y0);
+    tree->SetBranchAddress("z0", &nt.z0);
+    tree->SetBranchAddress("ntk1", &nt.ntk1);
+    tree->SetBranchAddress("x1", &nt.x1);
+    tree->SetBranchAddress("y1", &nt.y1);
+    tree->SetBranchAddress("z1", &nt.z1);
 
-    const bool debug = false;
-    char line[1024];
-    while (fgets(line, 1024, f) != 0) {
-      if (debug) printf("One2Two debug: file line read: %s", line);
-      int res = 0;
-      int ntk0, ntk1;
-      float x0,y0,z0, x1,y1,z1;
-      if      ((res = sscanf(line, "TREETHIS1v %i %f %f %f",             &ntk0, &x0, &y0, &z0                      )) == 4)
-	;
-      else if ((res = sscanf(line, "TREETHIS2v %i %f %f %f %i %f %f %f", &ntk0, &x0, &y0, &z0, &ntk1, &x1, &y1, &z1)) == 8)
-	;
+    int j = 0, je = tree->GetEntries();
+    for (; j < je; ++j) {
+      if (tree->LoadTree(j) < 0) break;
+      if (tree->GetEntry(j) <= 0) continue;
 
-      if (res == 4) {
+      if (nt.nvtx == 1) {
 	MFVVertexAux v0;
-	v0.x = x0; v0.y = y0; v0.z = z0; v0.bs2ddist = mag(x0, y0);
-	for (int i = 0; i < ntk0; ++i)
+	v0.x = nt.x0; v0.y = nt.y0; v0.z = nt.z0; v0.bs2ddist = mag(nt.x0, nt.y0);
+	for (int i = 0; i < nt.ntk0; ++i)
 	  v0.insert_track();
 	if (sel_vertex_aft(v0))
 	  one_vertices.push_back(v0);
       }
-      else if (res == 8) {
+      else if (nt.nvtx == 2) {
 	MFVVertexAux v0, v1;
-	v0.x = x0; v0.y = y0; v0.z = z0; v0.bs2ddist = mag(x0, y0);
-	v1.x = x1; v1.y = y1; v1.z = z1; v1.bs2ddist = mag(x1, y1);
-	for (int i = 0; i < ntk0; ++i) v0.insert_track();
-	for (int i = 0; i < ntk1; ++i) v1.insert_track();
+	v0.x = nt.x0; v0.y = nt.y0; v0.z = nt.z0; v0.bs2ddist = mag(nt.x0, nt.y0);
+	v1.x = nt.x1; v1.y = nt.y1; v1.z = nt.z1; v1.bs2ddist = mag(nt.x1, nt.y1);
+	for (int i = 0; i < nt.ntk0; ++i) v0.insert_track();
+	for (int i = 0; i < nt.ntk1; ++i) v1.insert_track();
 	bool sel0 = sel_vertex_aft(v0);
 	bool sel1 = sel_vertex_aft(v1);
 	if (sel0 && sel1)
@@ -287,7 +354,8 @@ void MFVOne2Two::endJob() {
       }
     }
 
-    fclose(f);
+    f->Close();
+    delete f;
   }
 
   printf("h_nsv:");      for (int i = 1; i <= 10; ++i) printf(" %i", int(h_nsv   ->GetBinContent(i)));
