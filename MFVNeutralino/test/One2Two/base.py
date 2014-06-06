2dc81f956626c60b7cc2dabdcc30a75801532578
Jordan Tucker <jordantucker@gmail.com>
2014-06-06 10:40:08 -0400

    svdz alias; FitResult.

--- MFVNeutralino/test/One2Two/base.py
+++ MFVNeutralino/test/One2Two/base.py
@@ -22,8 +22,14 @@ def get_f_t(x, min_ntracks):
     t = f.Get('mfvOne2Two/t')
     t.SetAlias('svdist', 'sqrt((x0-x1)**2 + (y0-y1)**2)')
     t.SetAlias('svdphi', 'TVector2::Phi_mpi_pi(atan2(y0,x0)-atan2(y1,x1))')
+    t.SetAlias('svdz',   'z0 - z1')
     t.SetAlias('min_ntracks_ok', 'ntk0 >= %i && ntk1 >= %i' % (min_ntracks, min_ntracks))
     return f, t
 
 bkg_samples = Samples.ttbar_samples + Samples.qcd_samples
 sig_samples = [Samples.mfv_neutralino_tau0100um_M0400, Samples.mfv_neutralino_tau0300um_M0400, Samples.mfv_neutralino_tau1000um_M0400, Samples.mfv_neutralino_tau9900um_M0400]
+
+class FitResult:
+    def __init__(self, *args):
+        self.fcn, self.fit = args
+
