cb9ba6d45886ab45d887ecfe7ade541376102443
Jordan Tucker <jordantucker@gmail.com>
2014-06-08 01:49:52 -0500

    npairs -> 50000; from_dz from qcdht1000.

--- MFVNeutralino/test/One2Two/one2two.py
+++ MFVNeutralino/test/One2Two/one2two.py
@@ -17,13 +17,13 @@ mfvOne2Two = cms.EDAnalyzer('MFVOne2Two',
                             toy_mode = cms.bool(False),
                             poisson_n1vs = cms.bool(False),
                             wrep = cms.bool(True),
-                            npairs = cms.int32(10000),
+                            npairs = cms.int32(50000),
 
                             use_f_dz = cms.bool(True),
                             max_1v_dz = cms.double(0.025),
                             max_1v_ntracks = cms.int32(1000000),
                             form_dphi = cms.string('abs(x)**2.5/15.702056'),
-                            form_dz = cms.string('1/sqrt(2*3.14159265*0.01635**2)*exp(-x*x/2/0.01635**2)'),
+                            form_dz = cms.string('1/sqrt(2*3.14159265*0.01718**2)*exp(-x*x/2/0.01718**2)'),
                             form_g_dz = cms.string('1/sqrt(2*3.14159265*9.273**2)*exp(-x*x/2/9.273**2)'),
                             )
 
