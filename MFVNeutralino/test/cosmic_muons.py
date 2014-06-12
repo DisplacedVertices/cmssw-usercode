28bd2f3945ba05072b0a6b70968b30e1e6f80368
Jennifer Chu <jyc72@cornell.edu>
2014-06-12 14:57:55 -0500

    Plot relative pT error, dxy and dz w.r.t. primary vertex; require trigger; make sets of plots with different parameters; run over mfv_signal_samples.

--- MFVNeutralino/test/cosmic_muons.py
+++ MFVNeutralino/test/cosmic_muons.py
@@ -1,3 +1,4 @@
+import os, sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
 
 process.source.fileNames = '''/store/user/tucker/mfv_neutralino_tau1000um_M0400/mfv_neutralino_tau1000um_M0400/a6ab3419cb64660d6c68351b3cff9fb0/aodpat_1_1_X2h.root
@@ -12,7 +13,51 @@ process.source.fileNames = '''/store/user/tucker/mfv_neutralino_tau1000um_M0400/
 /store/user/tucker/mfv_neutralino_tau1000um_M0400/mfv_neutralino_tau1000um_M0400/a6ab3419cb64660d6c68351b3cff9fb0/aodpat_10_1_VPN.root'''.split('\n')
 process.TFileService.fileName = 'cosmic_muons.root'
 
+from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
+process.triggerFilter = hltHighLevel.clone()
+process.triggerFilter.HLTPaths = ['HLT_QuadJet50_v*']
+process.triggerFilter.andOr = True # = OR
+
 process.CosmicMuons = cms.EDAnalyzer('CosmicMuons',
                                      track_src = cms.InputTag('globalCosmicMuons'),
+                                     primary_vertex_src = cms.InputTag('goodOfflinePrimaryVertices'),
+                                     gen_particle_src = cms.InputTag('genParticles'),
+                                     min_pt = cms.double(5),
+                                     max_eta = cms.double(2.4),
+                                     min_npxhits = cms.int32(1),
+                                     min_nsthits = cms.int32(6),
+                                     min_nmuhits = cms.int32(2),
+                                     min_dxy = cms.double(0),
+                                     max_relpterr = cms.double(0.5),
                                      )
-process.p = cms.Path(process.CosmicMuons)
+
+process.CosmicMuonsPt1Nsthits6Dxy0 = process.CosmicMuons.clone(min_pt = 1)
+process.CosmicMuonsPt1Nsthits6Dxy100 = process.CosmicMuons.clone(min_pt = 1, min_dxy = 0.01)
+process.CosmicMuonsPt1Nsthits8Dxy0 = process.CosmicMuons.clone(min_pt = 1, min_nsthits = 8)
+process.CosmicMuonsPt1Nsthits8Dxy100 = process.CosmicMuons.clone(min_pt = 1, min_nsthits = 8, min_dxy = 0.01)
+process.CosmicMuonsPt5Nsthits6Dxy0 = process.CosmicMuons.clone()
+process.CosmicMuonsPt5Nsthits6Dxy100 = process.CosmicMuons.clone(min_dxy = 0.01)
+process.CosmicMuonsPt5Nsthits8Dxy0 = process.CosmicMuons.clone(min_nsthits = 8)
+process.CosmicMuonsPt5Nsthits8Dxy100 = process.CosmicMuons.clone(min_nsthits = 8, min_dxy = 0.01)
+
+process.p = cms.Path(process.triggerFilter
+                   * process.CosmicMuonsPt1Nsthits6Dxy0
+                   * process.CosmicMuonsPt1Nsthits6Dxy100
+                   * process.CosmicMuonsPt1Nsthits8Dxy0
+                   * process.CosmicMuonsPt1Nsthits8Dxy100
+                   * process.CosmicMuonsPt5Nsthits6Dxy0
+                   * process.CosmicMuonsPt5Nsthits6Dxy100
+                   * process.CosmicMuonsPt5Nsthits8Dxy0
+                   * process.CosmicMuonsPt5Nsthits8Dxy100)
+
+if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
+    from JMTucker.Tools.Samples import mfv_signal_samples
+    from JMTucker.Tools.CRABSubmitter import CRABSubmitter
+
+    cs = CRABSubmitter('CosmicMuons',
+                       total_number_of_events = -1,
+                       events_per_job = 10000,
+                       )
+
+    samples = mfv_signal_samples
+    cs.submit_all(samples)
