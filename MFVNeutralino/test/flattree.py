37f386bc3cf98af3a08f9c64592ef3c382a945f6
Jordan Tucker <jordantucker@gmail.com>
2014-06-15 17:23:18 -0500

    Full flat ntuple (for ugrads).

---
+++ MFVNeutralino/test/flattree.py
@@ -0,0 +1,31 @@
+import sys
+from JMTucker.Tools.BasicAnalyzer_cfg import *
+
+process.options.emptyRunLumiMode = cms.untracked.string('doNotHandleEmptyRunsAndLumis')
+process.source.fileNames = ['/store/user/tucker/QCD_HT-1000ToInf_TuneZ2star_8TeV-madgraph-pythia6/mfvntuple_v18/c761ddfa7f093d8f86a338439e06a1d4/ntuple_1_1_URD.root']
+process.source.noEventSort = cms.untracked.bool(True)
+process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
+process.TFileService.fileName = 'flattree.root'
+
+process.mfvFlatTree = cms.EDAnalyzer('MFVFlatTreer',
+                                     event_src = cms.InputTag('mfvEvent'),
+                                     vertex_src = cms.InputTag('mfvVerticesAux'),
+                                     sample = cms.uint32(0),
+                                     )
+
+process.p = cms.Path(process.mfvFlatTree)
+        
+if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
+    import JMTucker.Tools.Samples as Samples
+    samples = Samples.from_argv([Samples.mfv_neutralino_tau0100um_M0400,
+                                 Samples.mfv_neutralino_tau1000um_M0400,
+                                 Samples.mfv_neutralino_tau0300um_M0400,
+                                 Samples.mfv_neutralino_tau9900um_M0400] + Samples.ttbar_samples + Samples.qcd_samples)
+
+    from JMTucker.Tools.CRABSubmitter import CRABSubmitter
+    cs = CRABSubmitter('FlatTreeV18',
+                       job_control_from_sample = True,
+                       use_ana_dataset = True,
+                       run_half_mc = True,
+                       )
+    cs.submit_all(samples)
