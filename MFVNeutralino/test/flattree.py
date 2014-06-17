36e3fe60cfffb28df023e3661525f63c2c74dafe
Jordan Tucker <jordantucker@gmail.com>
2014-06-16 21:28:21 -0500

    LKSADFLKJASLKJFDALKJDSGKJAGSD put to eos since these stupid things are going to be just as big as ntuples

--- MFVNeutralino/test/flattree.py
+++ MFVNeutralino/test/flattree.py
@@ -31,6 +31,8 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     cs = CRABSubmitter('FlatTreeV18',
                        pset_modifier = modify,
+                       get_edm_output = True,
+                       data_retrieval = 'fnal',
                        job_control_from_sample = True,
                        use_ana_dataset = True,
                        run_half_mc = True,
