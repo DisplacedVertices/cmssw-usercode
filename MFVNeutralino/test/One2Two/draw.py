ad36e53c39fd7f0aee899ec8a29109a56de1eb31
Jordan Tucker <jordantucker@gmail.com>
2014-06-23 11:32:44 -0500

    move some to base; better shifting; more fit stats.

--- MFVNeutralino/test/One2Two/draw.py
+++ MFVNeutralino/test/One2Two/draw.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python
 
-import sys, os
-from JMTucker.Tools.general import typed_from_argv
+from base import *
 input_fn = [x for x in sys.argv if x.endswith('.root') and os.path.isfile(x)][0]
-
-from array import array
-from math import pi
-from JMTucker.Tools.ROOTTools import *
-set_style()
-ROOT.TH1.AddDirectory(0)
-ROOT.gStyle.SetOptStat(2222222)
-ROOT.gStyle.SetOptFit(2222)
 plot_path = os.path.join('plots/one2two', os.path.basename(input_fn).replace('.root', ''))
 ps = plot_saver(plot_path, size=(600,600))
 
@@ -21,7 +12,7 @@ def get_h(name):
     return f.Get('mfvOne2Two/%s' % name)
 
 ####
-
+'''
 for name in 'h_1v_xy h_2v_xy'.split():
     h = get_h(name)
     h.SetTitle(';vertex x (cm);vertex y (cm)')
@@ -147,18 +138,17 @@ differentiate_stat_box(h2v, (1,0), new_size=(0.25, 0.25))
 differentiate_stat_box(h1v, (1,1), new_size=(0.25, 0.25))
 differentiate_stat_box(hfn, (1,2), new_size=(0.25, 0.25))
 ps.save('dz')
-
+'''
 ####
 
-def svdist2d_comp(norm_below, shift=None, rebin=None, save=True):
+def svdist2d_comp(norm_below, rebin=None, shift=None, save=None):
     name = ('norm%.3f' % norm_below).replace('.','p')
     if shift is not None:
         name += '_shift%i' % shift
-    
+
     h2v = get_h('h_2v_svdist2d').Clone('h2v')
     h1v = get_h('h_1v_svdist2d').Clone('h1v')
-
-    if rebin is not None:
+    if rebin:
         h2v.Rebin(rebin)
         h1v.Rebin(rebin)
 
@@ -166,17 +156,22 @@ def svdist2d_comp(norm_below, shift=None, rebin=None, save=True):
     nbins = h1v.GetNbinsX()
 
     if shift is not None:
-        h1v_vals = [h1v.GetBinContent(i+1) for i in xrange(nbins)]
-        h1v_vals_new = []
-        for i in xrange(nbins):
-            ifrom = i - shift
-            h1v_vals_new.append(h1v_vals[ifrom] if ifrom >= 0 else 0.)
-
-        h1v = ROOT.TH1F('h1v_shift%i' % shift, '', nbins, h1v.GetXaxis().GetXmin(), h1v.GetXaxis().GetXmax())
-        for i in xrange(nbins):
-            h1v.SetBinContent(i+1, h1v_vals_new[i])
-            h1v.SetBinError(i+1, 0)
-        #print 'shift', shift, 'mean', h1v.GetMean()
+        h1v_shift = ROOT.TH1F('h1v_shift%i' % shift, '', nbins, h1v.GetXaxis().GetXmin(), h1v.GetXaxis().GetXmax())
+        for ibin in xrange(1, nbins+2):
+            ifrom = ibin - shift
+            val, err = 0., 0.
+            if ifrom >= 0:
+                val = h1v.GetBinContent(ifrom)
+                err = h1v.GetBinError  (ifrom)
+            if ibin == nbins+1:
+                var = err**2
+                for irest in xrange(ifrom+1, nbins+2):
+                    val += h1v.GetBinContent(ifrom)
+                    var += h1v.GetBinError  (ifrom)**2
+                err = var**0.5
+            h1v_shift.SetBinContent(ibin, val)
+            h1v_shift.SetBinError  (ibin, err)
+        h1v = h1v_shift
 
     ksdist = h1v.KolmogorovTest(h2v, 'M')
     ksprob = h1v.KolmogorovTest(h2v)
@@ -185,17 +180,43 @@ def svdist2d_comp(norm_below, shift=None, rebin=None, save=True):
     h2v.SetLineWidth(2)
     h1v.SetLineColor(ROOT.kRed)
 
-    h1v.Scale(h2v.Integral(1, h2v.FindBin(norm_below))/h1v.Integral(1, h1v.FindBin(norm_below)))
+    lb = h2v.FindBin(norm_below)
+    h1v.Scale(h2v.Integral(1, lb)/h1v.Integral(1, lb))
 
-    h2v.SetTitle('norm_below %f shift %i;xy distance between vertex 0 and 1 (cm);events/40 #mum' % (norm_below, shift))
+    h2v.SetTitle('norm_below %f shift %i;xy distance between vertex 0 and 1 (cm);events/10 #mum' % (norm_below, shift))
 
     h2v.Draw('e')
     h1v.Draw('sames')
     ps.c.Update()
     differentiate_stat_box(h2v, 0, new_size=(0.3, 0.3))
     differentiate_stat_box(h1v,    new_size=(0.3, 0.3))
-    if save:
-        ps.save('svdist2d_%s' % name)
+
+    chi2 = h2v.Clone(h2v.GetName() + '_chi2')
+    chi2.Add(h1v, -1)
+    for ibin in xrange(1, chi2.GetNbinsX()+1):
+        dif = chi2.GetBinContent(ibin)
+        err = chi2.GetBinError  (ibin)
+        if h2v.GetBinContent(ibin):
+            if err < 1e-9:
+                err = 1e-9
+            chi2.SetBinContent(ibin, dif**2/err)
+    chi2_rescale = h2v.GetYaxis().GetXmax()/chi2.GetMaximum()
+    chi2.Scale(chi2_rescale)
+    tpl = ROOT.TPaveLabel(0.214, 0.898, 0.875, 0.998, '#chi^{2} rescale = %f' % chi2_rescale, 'brNDC')
+    tpl.SetTextSize(0.25)
+    tpl.SetBorderSize(0)
+    tpl.SetFillColor(0)
+    tpl.SetFillStyle(0)
+    tpl.Draw()
+    chi2.SetMarkerStyle(20)
+    chi2.SetMarkerSize(0.8)
+    chi2.SetMarkerColor(ROOT.kBlack)
+    chi2.Draw("same")
+    chi2.SetStats(0)
+    if save is not None:
+        if save != '':
+            save = '_' + save
+        ps.save('svdist2d_%s%s' % (name, save))
 
     for opt in ('ge', 'le'):
         ch2v = cumulative_histogram(h2v, opt)
@@ -208,38 +229,122 @@ def svdist2d_comp(norm_below, shift=None, rebin=None, save=True):
         ch1v.SetLineColor(ROOT.kRed)
         ch2v.Draw()
         ch1v.Draw('hist same')
-        if save:
-            ps.save('svdist2d_%s_cumul%s' % (name, opt))
+        if save is not None:
+            ps.save('svdist2d_%s%s_cumul%s' % (name, save, opt))
 
     return ks
 
-for norm_below in (1, 0.024, 0.048):
-    svdist2d_comp(norm_below, 0)
+svdist2d_cut = 0.05
 
-n = 30
-shifts = range(n)
-kses = [svdist2d_comp(1, shift, save=False) for shift in shifts]
-min_ksdist = 1e99
-best_shift = None
-for i, (ksdist, ksprob) in enumerate(kses):
-    if ksdist < min_ksdist:
-        min_ksdist = ksdist
-        best_shift = shifts[i]
+for shift in xrange(20):
+    svdist2d_comp(svdist2d_cut, None, shift, 'shift%i' % shift)
 
-shifts = array('d', shifts)
-for i,name in enumerate(('dist', 'prob')):
-    g = ROOT.TGraph(n, shifts, array('d', [x[i] for x in kses]))
-    g.SetTitle(';shift;KS %s' % name)
+h2v = get_h('h_2vsideband_svdist2d').Clone('h2v')
+h1v = get_h('h_1v_svdist2d').Clone('h1v')
+bin_size = h2v.GetBinWidth(1)
+shift_by_means = int(round((h2v.GetMean() - h1v.GetMean())/bin_size))
+svdist2d_comp(svdist2d_cut, None, shift_by_means, 'bymeans')
+
+def shifted(h, shift, last_bin):
+    B = [(h.GetBinContent(i), h.GetBinError(i)) for i in xrange(0, h.GetNbinsX()+1)]
+    if shift:
+        B = [(0.,0.)]*shift + B[:-shift]
+    return B[:last_bin+2]
+
+def get_F(h, last_bin, shift):
+    B = shifted(h, shift, last_bin)
+    I = sum(b for b,e in B)
+    B = [(b/I, e/I) for b,e in B]
+    F = [B[0]]
+    for b,e in B[1:]:
+        bl, el = F[-1]
+        F.append((b + bl, (e + el)**0.5))
+    return F
+
+def get_hF(h, last_bin, shift):
+    F = get_F(h, last_bin, shift)
+    h = h.Clone(h.GetName() + 'F')
+    for ibin in xrange(h.GetNbinsX()+2):
+        if ibin <= last_bin:
+            h.SetBinContent(ibin, F[ibin][0])
+            h.SetBinError  (ibin, 0)
+        else:
+            h.SetBinContent(ibin, 0)
+            h.SetBinError  (ibin, 0)
+    return h
+
+def svdist2d_sideband_fitstat(h2v, h1v, shift, last_bin, stat):
+    if stat in ('ad', 'cvm', 'ks'):
+        F2v = get_F(h2v, last_bin)
+        F1v = get_F(h1v, last_bin, shift)
+        s = 0.
+        for (F,Fe),(Fn,Fne) in zip(F1v, F2v):
+            if stat == 'ad':
+                if F > 0 and F < 1:
+                    w = F*(1-F)
+                else:
+                    w = 1e-6
+            else:
+                w = 1.
+            if stat == 'ks':
+                s = max(s, abs(Fn - F))
+            else:
+                s += abs(Fn - F)**2 / w
+        return s
+    elif stat == 'chi2':
+        h2v = shifted(h1v,     0, last_bin)
+        h1v = shifted(h1v, shift, last_bin)
+        s2v = sum(b for b,e in h2v)
+        s1v = sum(b for b,e in h1v)
+        scale = s2v/s1v
+        h1v = [(b*scale, e*scale) for b,e in h1v]
+        print sum(b for b,e in h2v), sum(b for b,e in h1v)
+        chi2 = 0.
+        #print '  shift: %i' % shift
+        for (v,ve), (vn,vne) in zip(h1v, h2v):
+            if vn:
+                dif2 = (vn - v)**2
+                err = (ve**2 + vne**2)**0.5
+                if err < 1e-9:
+                    err = 1e-9
+                chi2 += dif2/err
+            #print '    dif2:', dif2, 'err:', err, 'd/e:', dif2/err, 'cchi2:', chi2
+        return chi2
+        
+for stat_name in ('chi2',): #, 'ks', 'ad', 'cvm'):
+    h2v = get_h('h_2v_svdist2d').Clone('h2v')
+    h1v = get_h('h_1v_svdist2d').Clone('h1v')
+    rebin = 1
+    h2v.Rebin(rebin)
+    h1v.Rebin(rebin)
+    last_bin = h1v.FindBin(svdist2d_cut) - 1
+
+    nshifts = 20
+    shifts = range(nshifts)
+    stats = [svdist2d_sideband_fitstat(h2v, h1v, shift, last_bin, stat_name) for shift in shifts]
+    print 'stat:', stat_name
+    for shift, stat in zip(shifts, stats):
+        print '%2i' % shift, stat
+    best_shift, best_stat = min(zip(shifts, stats), key=lambda x:x[1])
+    print 'best:', best_shift
+
+    Fh2v = get_hF(h2v, last_bin, best_shift)
+    Fh1v = get_hF(h1v, last_bin, best_shift)
+    Fh1v.Draw()
+    Fh2v.Draw('same')
+    ps.save('%s_Fs' % stat_name)
+
+    g = ROOT.TGraph(nshifts, arrit(shifts), arrit(stats))
+    g.SetTitle('%s;shift;stat' % stat_name)
     g.SetMarkerStyle(20)
     g.SetMarkerSize(1)
     g.Draw('ALP')
-    ps.save('KS_%s_v_shift' % name)
+    ps.save('%s_v_shift' % stat_name)
 
-for norm_below in (1, 0.024, 0.048):
-    svdist2d_comp(norm_below, best_shift)
+    svdist2d_comp(svdist2d_cut, rebin, best_shift, '%sfit' % stat_name)
 
 ####
-
+'''
 verbose = 'Q'
 h = get_h('h_2v_dphi')
 
@@ -260,3 +365,5 @@ for i in xrange(2, 17, 2):
     res = h.Fit(fcn, 'IRS' + verbose)
     ps.save('abs_power_%i' % i)
     #res.Print()
+'''
+
