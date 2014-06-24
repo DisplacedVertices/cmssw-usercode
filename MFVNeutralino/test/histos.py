c93cd657a426832ce6e7e84cef46c19e1ac37cd3
Jordan Tucker <jordantucker@gmail.com>
2014-06-24 13:54:20 -0500

    Ntracks nm1 also does njets=0; add nm1s for exactly one vtx sel; submit script: ana_all.json and data samples.

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -11,7 +11,7 @@ import JMTucker.Tools.SimpleTriggerResults_cfi as SimpleTriggerResults
 SimpleTriggerResults.setup_endpath(process, weight_src='mfvWeight')
 
 nm1s = [
-    ('Ntracks', 'min_ntracks = 0'),
+    ('Ntracks', 'min_ntracks = 0, min_njetsntks = 0'),
     ('Drmin',   'max_drmin = 1e9'),
     ('Drmax',   'max_drmax = 1e9'),
     ('Mindrmax','min_drmax = 0'),
@@ -30,23 +30,29 @@ for name, cut in nm1s:
     vtx = eval('process.mfvSelectedVerticesTight.clone(%s)' % cut)
     vtx_name = 'vtxNo' + name
 
-    for nv in (1,2):
-        ana = eval('process.mfvAnalysisCuts.clone(%s)' % evt_cut)
-        ana.vertex_src = vtx_name
-        ana.min_nvertex = nv
-        ana_name = 'ana%iVNo' % nv + name
+    for only in ('', 'Only'):
+        for nv in (1,2):
+            if nv == 2 and only == 'Only':
+                continue
 
-        evt_hst = process.mfvEventHistos.clone()
-        evt_hst_name = 'evtHst%iVNo' % nv + name
+            ana = eval('process.mfvAnalysisCuts.clone(%s)' % evt_cut)
+            ana.vertex_src = vtx_name
+            if only == 'Only':
+                ana.max_nvertex = nv
+            ana.min_nvertex = nv
+            ana_name = 'ana%s%iVNo' % (only, nv) + name
 
-        vtx_hst = process.mfvVertexHistos.clone(vertex_aux_src = vtx_name)
-        vtx_hst_name = 'vtxHst%iVNo' % nv + name
+            evt_hst = process.mfvEventHistos.clone()
+            evt_hst_name = 'evtHst%s%iVNo' % (only, nv) + name
 
-        setattr(process, vtx_name, vtx)
-        setattr(process, ana_name, ana)
-        setattr(process, evt_hst_name, evt_hst)
-        setattr(process, vtx_hst_name, vtx_hst)
-        setattr(process, 'p%iV' % nv + name, cms.Path(vtx * ana * evt_hst * vtx_hst))
+            vtx_hst = process.mfvVertexHistos.clone(vertex_aux_src = vtx_name)
+            vtx_hst_name = 'vtxHst%s%iVNo' % (only, nv) + name
+
+            setattr(process, vtx_name, vtx)
+            setattr(process, ana_name, ana)
+            setattr(process, evt_hst_name, evt_hst)
+            setattr(process, vtx_hst_name, vtx_hst)
+            setattr(process, 'p%s%iV' % (only, nv) + name, cms.Path(vtx * ana * evt_hst * vtx_hst))
 
 def force_bs(process, bs):
     for ana in process.analyzers:
@@ -58,10 +64,10 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     samples = Samples.from_argv([Samples.mfv_neutralino_tau0100um_M0400,
                                  Samples.mfv_neutralino_tau1000um_M0400,
                                  Samples.mfv_neutralino_tau0300um_M0400,
-                                 Samples.mfv_neutralino_tau9900um_M0400] + Samples.ttbar_samples + Samples.qcd_samples)
+                                 Samples.mfv_neutralino_tau9900um_M0400] + Samples.ttbar_samples + Samples.qcd_samples + Samples.data_samples)
 
     for s in Samples.data_samples:
-        s.json = 'ana_5pc.json'
+        s.json = 'ana_all.json'
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     cs = CRABSubmitter('HistosV18',
