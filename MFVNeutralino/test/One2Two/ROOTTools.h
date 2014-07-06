ebe8ad5dbbfdd0948c5543e232a88527de4631ee
Jordan Tucker <jordantucker@gmail.com>
2014-07-05 23:06:34 -0500

    shift_hist; rename set_root_style

--- MFVNeutralino/test/One2Two/ROOTTools.h
+++ MFVNeutralino/test/One2Two/ROOTTools.h
@@ -1,8 +1,11 @@
 #ifndef JMTucker_MFVNeutralino_One2Two_ROOTTools_h
 #define JMTucker_MFVNeutralino_One2Two_ROOTTools_h
 
+class TH1D;
+
 namespace jmt {
-  void SetROOTStyle();
+  void set_root_style();
+  TH1D* shift_hist(const TH1D* h, const int shift);
 }
 
 #endif
