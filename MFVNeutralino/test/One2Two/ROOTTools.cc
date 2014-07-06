f756133a29ce65b8d4ebf850bd8959d0e8347c6e
Jordan Tucker <jordantucker@gmail.com>
2014-07-05 21:59:57 -0500

    more style

--- MFVNeutralino/test/One2Two/ROOTTools.cc
+++ MFVNeutralino/test/One2Two/ROOTTools.cc
@@ -5,6 +5,21 @@
 
 namespace jmt {
   void SetROOTStyle() {
+    gROOT->SetStyle("Plain");
+    gStyle->SetPalette(1);
+    gStyle->SetFillColor(0);
+    gStyle->SetOptDate(0);
+    gStyle->SetOptStat(1222222);
+    gStyle->SetOptFit(2222);
+    gStyle->SetPadTickX(1);
+    gStyle->SetPadTickY(1);
+    gStyle->SetMarkerSize(.1);
+    gStyle->SetMarkerStyle(8);
+    gStyle->SetGridStyle(3);
+    gROOT->ProcessLine("gErrorIgnoreLevel = 1001;");
+    double palinfo[4][5] = {{0,0,0,1,1},{0,1,1,1,0},{1,1,0,0,0},{0,0.25,0.5,0.75,1}};
+    TColor::CreateGradientColorTable(5, palinfo[3], palinfo[0], palinfo[1], palinfo[2], 500);
+    gStyle->SetNumberContours(500);
     TH1::SetDefaultSumw2();
   }
 }
