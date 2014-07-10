95d23a56fd0c05b4c0d4223b354b0e4ce6485af6
Jordan Tucker <jordantucker@gmail.com>
2014-07-10 07:40:02 -0500

    yowza

--- MFVNeutralino/test/One2Two/Fitter.cc
+++ MFVNeutralino/test/One2Two/Fitter.cc
@@ -15,6 +15,9 @@
 namespace mfv {
   namespace fit {
     int n_bins = -1;
+    TH1D* h_data_real = 0;
+    TH1D* h_data_sig = 0;
+    TH1D* h_data_bkg = 0;
     TH1D* h_data = 0;
     Template* curr_bkg_template = 0;
     TH1D* h_sig = 0;
@@ -26,12 +29,14 @@ namespace mfv {
         const double nu_sig = mu_sig * h_sig->GetBinContent(i);
         const double nu_bkg = mu_bkg * h_bkg->GetBinContent(i);
         const double nu_sum = nu_sig + nu_bkg;
-        const double nu = nu_sum > 0 ? nu_sum : 1e-300;
+        const double nu = nu_sum > 1e-12 ? nu_sum : 1e-12;
         const double n = h_data->GetBinContent(i);
-        const double dlnL = -nu + n * log(nu); // log(nu/n);
+        if (n > 0 && nu_sum == 0)
+          jmt::vthrow("in twolnL, n = %f and nu = 0 for bin i = %i", n, i);
+        const double dlnL = -nu + n * log(nu);
         lnL += dlnL;
         //printf("i: %i   mu_sig, mu_bkg (%f, %f)   nu_bkg: %f  nu_sig: %f  nu: %f  n: %f    dlnL: %f   lnL: %f\n",
-        //i, mu_sig, mu_bkg, nu_bkg, nu_sig, nu, n, dlnL, lnL);
+        //               i, mu_sig, mu_bkg, nu_bkg, nu_sig, nu, n, dlnL, lnL);
       }
       return 2*lnL;
     }
@@ -43,12 +48,28 @@ namespace mfv {
 
   const int Fitter::npars = 2 + Template::max_npars;
 
+  void Fitter::min_lik_t::print(const char* header, const char* indent) const {
+    printf("%s%s  istat = %i  max_value = %10.4e  mu_sig = %7.3f +- %7.3f  mu_bkg = %7.3f +- %7.3f\n", indent, header, istat, maxtwolnL, mu_sig, err_mu_sig, mu_bkg, err_mu_bkg);
+  }
+
+  void Fitter::test_stat_t::print(const char* header, const char* indent) const {
+    printf("%s:  ok? %i  t = %f\n", header, ok(), t);
+    h1.print("h1", indent);
+    h0.print("h0", indent);
+  }
+
+  std::pair<std::vector<double>, std::vector<double> > Fitter::min_lik_t::get_template_par_ranges(const double up) const {
+    std::pair<std::vector<double>, std::vector<double> > ret;
+    jmt::vthrow("implement get_template_par_ranges %f", up);
+    return ret;
+  }
+
   Fitter::Fitter(const std::string& name_, TFile* f, TRandom* r)
     : name (name_.size() ? " " + name_ : ""),
       uname(name_.size() ? "_" + name_ : ""),
 
       env("mfvo2t_fitter" + uname),
-      print_level(env.get_int("print_level", 2)),
+      print_level(env.get_int("print_level", -1)),
 
       fout(f),
       dout(f->mkdir(TString::Format("Fitter%s", uname.c_str()))),
@@ -75,15 +96,6 @@ namespace mfv {
     t_fit_info->Branch("seed", const_cast<int*>(&seed), "seed/I");
     t_fit_info->Branch("toy", &toy, "toy/I");
     t_fit_info->Branch("true_pars", &true_pars);
-    t_fit_info->Branch("glb_scan_maxtwolnL", &glb_scan_maxtwolnL, "glb_scan_maxtwolnL/D");
-    t_fit_info->Branch("glb_scan_max_pars", &glb_scan_max_pars);
-    t_fit_info->Branch("glb_scan_max_pars_errs", &glb_scan_max_pars_errs);
-    t_fit_info->Branch("glb_scanmin_sb_maxtwolnL", &glb_scanmin_sb_maxtwolnL, "glb_scanmin_sb_maxtwolnL/D");
-    t_fit_info->Branch("glb_scanmin_sb_max_pars", &glb_scanmin_sb_max_pars);
-    t_fit_info->Branch("glb_scanmin_sb_max_pars_errs", &glb_scanmin_sb_max_pars_errs);
-    t_fit_info->Branch("glb_scanmin_b_maxtwolnL", &glb_scanmin_b_maxtwolnL, "glb_scanmin_b_maxtwolnL/D");
-    t_fit_info->Branch("glb_scanmin_b_max_pars", &glb_scanmin_b_max_pars);
-    t_fit_info->Branch("glb_scanmin_b_max_pars_errs", &glb_scanmin_b_max_pars_errs);
   }    
 
   std::vector<double> Fitter::binning() const {
@@ -94,7 +106,6 @@ namespace mfv {
     bins.push_back(0.4);
     bins.push_back(0.6);
     bins.push_back(1);
-    bins.push_back(2);
     bins.push_back(3);
     if (fit::n_bins < 0)
       fit::n_bins = bins.size() - 1;
@@ -133,6 +144,7 @@ namespace mfv {
                   fit::n_bins, fit::h_data, fit::curr_bkg_template, fit::h_sig);
   }
 
+#if 0
   bool Fitter::scan_likelihood() {
     fit_globals_ok();
 
@@ -198,151 +210,88 @@ namespace mfv {
 
     return glb_scan_maxtwolnL > -1e300;
   }
+#endif
 
-  bool Fitter::scanmin_likelihood(bool bkg_only) {
-    fit_globals_ok();
+  void Fitter::draw_likelihood(const test_stat_t& t) {
+    printf("draw_likelihood: ");
 
-    std::vector<int> istats;
-    bool all_ok = true;
+    const int n_mu_sig = 200;
+    const int n_mu_bkg = 200;
+    const double mu_sig_min = 0;
+    const double mu_sig_max = 200;
+    const double mu_bkg_min = 0;
+    const double mu_bkg_max = 200;
+    const double d_mu_sig = (mu_sig_max - mu_sig_min)/n_mu_sig;
+    const double d_mu_bkg = (mu_bkg_max - mu_bkg_min)/n_mu_bkg;
 
-    double* glb_scanmin_maxtwolnL = 0;
-    std::vector<double>* glb_scanmin_max_pars = 0;
-    std::vector<double>* glb_scanmin_max_pars_errs = 0;
-    Template** glb_scanmin_template = 0;
-
-    if (bkg_only) {
-      glb_scanmin_maxtwolnL = &glb_scanmin_b_maxtwolnL;
-      glb_scanmin_max_pars = &glb_scanmin_b_max_pars;
-      glb_scanmin_max_pars_errs = &glb_scanmin_b_max_pars_errs;
-      glb_scanmin_template = &glb_scanmin_b_template;
-    }
-    else {
-      glb_scanmin_maxtwolnL = &glb_scanmin_sb_maxtwolnL;
-      glb_scanmin_max_pars = &glb_scanmin_sb_max_pars;
-      glb_scanmin_max_pars_errs = &glb_scanmin_sb_max_pars_errs;
-      glb_scanmin_template = &glb_scanmin_sb_template;
-    }
-
-    *glb_scanmin_maxtwolnL = -1e300;
-    glb_scanmin_max_pars->assign(5, -1);
-    glb_scanmin_max_pars_errs->assign(5, -1);
-    *glb_scanmin_template = 0;
-
-    printf("scanminning (bkg_only = %i): ", bkg_only);
-
-    TDirectory* d = dtoy->mkdir(TString::Format("scanmin_likelihood_%s", bkg_only ? "b" : "sb"));
-    d->cd();
-
-    for (size_t i_template = 0, i_template_e = bkg_templates->size(); i_template < i_template_e; ++i_template) {
-      Template* bkg_template = bkg_templates->at(i_template);
-      fit::curr_bkg_template = bkg_template;
-
-      TMinuit* m = new TMinuit(2);
-      m->SetPrintLevel(print_level);
-      m->SetFCN(fit::minfcn);
-      int ierr;
-      m->mnparm(0, "mu_sig", 0 , 0.1, 0, 1e9, ierr);
-      m->mnparm(1, "mu_bkg", 40, 0.1, 0, 1e9, ierr);
-
-      if (bkg_only)
-        m->FixParameter(0);
-
-      m->Migrad();
-      //m->mnmnos();
-      double fmin, fedm, errdef;
-      int npari, nparx, istat;
-      m->mnstat(fmin, fedm, errdef, npari, nparx, istat);
-
-      const double maxtwolnL = -fmin;
-      double mu_sig, err_mu_sig, mu_bkg, err_mu_bkg;
-      m->GetParameter(0, mu_sig, err_mu_sig);
-      m->GetParameter(1, mu_bkg, err_mu_bkg);
-
-      printf("scanmin_likelihood: %s  istat: %i   maxtwolnL: %e   mu_sig: %f +- %f  mu_bkg: %f +- %f\n", bkg_template->title().c_str(), istat, maxtwolnL, mu_sig, err_mu_sig, mu_bkg, err_mu_bkg);
-
-      if (maxtwolnL > *glb_scanmin_maxtwolnL) {
-        printf("  ^ new global max!\n");
-        *glb_scanmin_maxtwolnL = maxtwolnL;
-        (*glb_scanmin_max_pars)[0] = mu_sig;
-        (*glb_scanmin_max_pars)[1] = mu_bkg;
-        (*glb_scanmin_max_pars_errs)[0] = err_mu_sig;
-        (*glb_scanmin_max_pars_errs)[1] = err_mu_bkg;
-        for (int ipar = 2; ipar < npars; ++ipar)
-          (*glb_scanmin_max_pars)[ipar] = bkg_template->par(ipar-2); 
-        *glb_scanmin_template = bkg_template;
-      }
-
-      istats.push_back(istat);
-      if (istat != 3)
-        all_ok = false;
-
-      delete m;
-    }
-
-    return all_ok;
-  }
-
-  void Fitter::fit(int toy_, Templates* bkg_templates_, TH1D* sig_template, const VertexPairs& v2v, const std::vector<double>& true_pars_) {
-    toy = toy_;
-    true_pars = true_pars_;
-
-    dtoy = dout->mkdir(TString::Format("seed%04i_toy%04i", seed, toy));
-
-    dtoy->mkdir("finalized_templates")->cd();
-    bkg_templates = bkg_templates_;
-    for (Template* t : *bkg_templates)
-      t->h_final = finalize_template(t->h);
-
-    fit::curr_bkg_template = (*bkg_templates)[0];
-    fit::h_sig = finalize_template(sig_template);
-
-    fit::h_data = hist_with_binning("h_data", TString::Format("toy %i", toy));
-    for (const VertexPair& p : v2v)
-      fit::h_data->Fill(p.d2d());
-
-    scan_likelihood();
-
-    scanmin_likelihood(false);
-    scanmin_likelihood(true);
-
-    dtoy->mkdir("fit_results")->cd();
     for (int sb = 1; sb >= 0; --sb) {
       const char* sb_or_b = sb ? "sb" : "b";
+      printf("%s ", sb_or_b); fflush(stdout);
+      const char* sb_or_b_nice = sb ? "sig + bkg" : "b only";
+      const min_lik_t& ml = sb ? t.h1 : t.h0;
+      TH1F* h = new TH1F(TString::Format("h_likelihood_%s_scannuis", sb_or_b),
+                         TString::Format("Best %s fit: #hat{#mu_{sig}} = %.3f  #hat{#mu_{bkg}} = %.3f  #hat{nuis}: %s", sb_or_b_nice, ml.mu_sig, ml.mu_bkg, ml.nuis->title().c_str()),
+                         bkg_templates->size(), 0, bkg_templates->size() );
+
+      for (size_t i_template = 0, i_template_e = bkg_templates->size(); i_template < i_template_e; ++i_template) {
+        Template* bkg_template = fit::curr_bkg_template = bkg_templates->at(i_template);
+        const int ibin = int(i_template + 1);
+
+        h->SetBinContent(ibin, fit::twolnL(ml.mu_sig, ml.mu_bkg));
+        h->GetXaxis()->SetBinLabel(ibin, bkg_template->title().c_str());
+      }
+
+      h->GetXaxis()->LabelsOption("v");
+
+      TH2F* h2 = new TH2F(TString::Format("h_likelihood_%s_scanmus", sb_or_b),
+                          TString::Format("Best %s fit: %s;#mu_{sig};#mu_{bkg}", sb_or_b_nice, ml.nuis->title().c_str()),
+                          n_mu_sig, mu_sig_min, mu_sig_max,
+                          n_mu_bkg, mu_bkg_min, mu_bkg_max
+                          );
+
+      fit::curr_bkg_template = ml.nuis;
+
+      for (int i_mu_sig = 1; i_mu_sig < n_mu_sig; ++i_mu_sig) {
+        const double mu_sig = mu_sig_min + i_mu_sig * d_mu_sig;
+        for (int i_mu_bkg = 1; i_mu_bkg < n_mu_bkg; ++i_mu_bkg) {
+          const double mu_bkg = mu_bkg_min + i_mu_bkg * d_mu_bkg;
+          h2->SetBinContent(i_mu_sig, i_mu_bkg, fit::twolnL(mu_sig, mu_bkg));
+        }
+      }
+    }
+
+    printf("\n");
+  }
+
+  void Fitter::draw_fit(const test_stat_t& t) {
+    for (int sb = 1; sb >= 0; --sb) {
+      const char* sb_or_b = sb ? "sb" : "b";
+      const char* sb_or_b_nice = sb ? "sig + bkg" : "b only";
+      const min_lik_t& ml = sb ? t.h1 : t.h0;
       TCanvas* c = new TCanvas(TString::Format("c_%s_fit", sb_or_b));
-      Template* bkg_template = sb ? glb_scanmin_sb_template : glb_scanmin_b_template;
-      TH1D* h_bkg_fit = (TH1D*)bkg_template->h_final->Clone(TString::Format("h_bkg_%s_fit", sb_or_b));
-      TH1D* h_sig_fit = (TH1D*)fit::h_sig->CloneTString::Format("h_sig_%s_fit", sb_or_b));
-      TH1D* h_data_fit = (TH1D*)fit::h_data->CloneTString::Format("h_data_%s_fit", sb_or_b));
+
+      TH1D* h_bkg_fit  = (TH1D*)ml.nuis->h_final->Clone(TString::Format("h_bkg_%s_fit",  sb_or_b));
+      TH1D* h_sig_fit  = (TH1D*)fit::h_sig      ->Clone(TString::Format("h_sig_%s_fit",  sb_or_b));
+      TH1D* h_data_fit = (TH1D*)fit::h_data     ->Clone(TString::Format("h_data_%s_fit", sb_or_b));
+
       for (TH1D* h : {h_bkg_fit, h_sig_fit, h_data_fit}) {
         h->SetLineWidth(2);
         jmt::divide_by_bin_width(h);
       }
+
       h_sig_fit->SetLineColor(kRed);
       h_sig_fit->SetFillStyle(3004);
       h_bkg_fit->SetLineColor(kBlue);
       h_bkg_fit->SetFillStyle(3005);
-      double mu_sig, err_mu_sig, mu_bkg, err_mu_bkg;
-      if (sb) {
-        mu_sig = glb_scanmin_sb_max_pars[0];
-        mu_bkg = glb_scanmin_sb_max_pars[1];
-        err_mu_sig = glb_scanmin_sb_max_pars_errs[0];
-        err_mu_bkg = glb_scanmin_sb_max_pars_errs[1];
-      }
-      else {
-        mu_sig = glb_scanmin_b_max_pars[0];
-        mu_bkg = glb_scanmin_b_max_pars[1];
-        err_mu_sig = glb_scanmin_b_max_pars_errs[0];
-        err_mu_bkg = glb_scanmin_b_max_pars_errs[1];
-      }
 
-      h_sig_fit->Scale(mu_sig);
-      h_bkg_fit->Scale(mu_bkg);
+      h_sig_fit->Scale(ml.mu_sig);
+      h_bkg_fit->Scale(ml.mu_bkg);
 
-      TH1D* h_sum_fit = (TH1D*)h_sig_fit->Clone("h_sum_fit");
+      TH1D* h_sum_fit = (TH1D*)h_sig_fit->Clone(TString::Format("h_sum_%s_fit", sb_or_b));
       h_sum_fit->SetLineColor(kMagenta);
       h_sum_fit->Add(h_bkg_fit);
       for (TH1D* h : {h_sum_fit, h_data_fit})
-        h->SetTitle(TString::Format("best %s fit with #mu_{sig} = %.2f #pm %.2f, #mu_{bkg} = %.2f #pm %.2f, %s;svdist2d (cm);events/bin width", (sb ? "sig + bkg" : "b only"), mu_sig, err_mu_sig, mu_bkg, err_mu_bkg, bkg_template->title().c_str()));
+        h->SetTitle(TString::Format("best %s fit with #mu_{sig} = %.2f #pm %.2f, #mu_{bkg} = %.2f #pm %.2f, %s;svdist2d (cm);events/bin width", sb_or_b_nice, ml.mu_sig, ml.err_mu_sig, ml.mu_bkg, ml.err_mu_bkg, ml.nuis->title().c_str()));
 
       if (h_data_fit->GetMaximum() > h_sum_fit->GetMaximum()) {
         h_data_fit->Draw("e");
@@ -357,6 +306,133 @@ namespace mfv {
       c->Write();
       delete c;
     }
+  }
+
+  Fitter::min_lik_t Fitter::scanmin_likelihood(double mu_sig_start, bool fix_mu_sig) {
+    fit_globals_ok();
+
+    min_lik_t ret;
+
+    printf("scanminning (mu_sig_start = %f, fix? %i):\n", mu_sig_start, fix_mu_sig);
+
+    //TDirectory* d = dtoy->mkdir(TString::Format("scanmin_likelihood_%s", bkg_only ? "b" : "sb"));
+    //d->cd();
+
+    for (size_t i_template = 0, i_template_e = bkg_templates->size(); i_template < i_template_e; ++i_template) {
+      Template* bkg_template = bkg_templates->at(i_template);
+      fit::curr_bkg_template = bkg_template;
+
+      TMinuit* m = new TMinuit(2);
+      m->SetPrintLevel(print_level);
+      m->SetFCN(fit::minfcn);
+      int ierr;
+      m->mnparm(0, "mu_sig", (mu_sig_start > 0 ? mu_sig_start : 0), 0.1, 0, (mu_sig_start > 0 ? mu_sig_start : 5000), ierr);
+      m->mnparm(1, "mu_bkg", 50, 0.1, 0, 5000, ierr);
+
+      if (fix_mu_sig)
+        m->FixParameter(0);
+
+      m->Migrad();
+      m->mnmnos();
+      double fmin, fedm, errdef;
+      int npari, nparx, istat;
+      m->mnstat(fmin, fedm, errdef, npari, nparx, istat);
+
+      const double maxtwolnL = -fmin;
+      double mu_sig, err_mu_sig, mu_bkg, err_mu_bkg;
+      m->GetParameter(0, mu_sig, err_mu_sig);
+      m->GetParameter(1, mu_bkg, err_mu_bkg);
+
+      //printf("scanmin_likelihood: %s  istat: %i   maxtwolnL: %e   mu_sig: %f +- %f  mu_bkg: %f +- %f\n", bkg_template->title().c_str(), istat, maxtwolnL, mu_sig, err_mu_sig, mu_bkg, err_mu_bkg);
+
+      if (maxtwolnL > ret.maxtwolnL) {
+        ret.ok = istat == 3;
+        ret.istat = istat;
+        ret.maxtwolnL = maxtwolnL;
+        ret.mu_sig = mu_sig;
+        ret.mu_bkg = mu_bkg;
+        ret.err_mu_sig = err_mu_sig;
+        ret.err_mu_bkg = err_mu_bkg;
+        ret.nuis = bkg_template;
+      }
+
+      if (istat != 3)
+        ret.all_ok = false;
+
+      ret.nuis_region.push_back(std::make_pair(maxtwolnL, bkg_template));
+
+      delete m;
+    }
+
+    return ret;
+  }
+
+  Fitter::test_stat_t Fitter::calc_test_stat(double fix_mu_sig_val) {
+    test_stat_t t;
+    t.h1 = scanmin_likelihood(fix_mu_sig_val, false);
+    t.h0 = scanmin_likelihood(fix_mu_sig_val, true);
+    t.t = t.h1.maxtwolnL - t.h0.maxtwolnL;
+    return t;
+  }
+
+  void Fitter::make_toy_data(int i_toy_signif, int i_toy_limit, int n_sig, int n_bkg) {
+    char s[128], s2[128];
+    if (i_toy_signif < 0 && i_toy_limit < 0)
+      snprintf(s, 128, "h_%%s_seed%02i_toy%02i", seed, toy);
+    else if (i_toy_limit < 0)
+      snprintf(s, 128, "h_%%s_seed%02i_toy%02i_signif%02i", seed, toy, i_toy_signif);
+    else if (i_toy_signif < 0)
+      snprintf(s, 128, "h_%%s_seed%02i_toy%02i_limit%02i", seed, toy, i_toy_limit);
+
+    snprintf(s2, 128, s, "data_sig"); fit::h_data_sig = hist_with_binning(s2, "");
+    snprintf(s2, 128, s, "data_bkg"); fit::h_data_bkg = hist_with_binning(s2, "");
+    snprintf(s2, 128, s, "data");     fit::h_data     = hist_with_binning(s2, "");
+
+    bool save = i_toy_signif < 0 && i_toy_limit < 0;
+    for (TH1D* h : {fit::h_data_sig, fit::h_data_bkg, fit::h_data}) {
+      h->SetLineWidth(2);
+      if (!save)
+        h->SetDirectory(0);
+    }
+  
+    fit::h_data_sig->FillRandom(fit::h_sig, n_sig);
+    fit::h_data_bkg->FillRandom(fit::curr_bkg_template->h_final, n_bkg);
+
+    fit::h_data->Add(fit::h_data_sig);
+    fit::h_data->Add(fit::h_data_bkg);
+  }
+
+  void Fitter::fit(int toy_, Templates* bkg_templates_, TH1D* sig_template, const VertexPairs& v2v, const std::vector<double>& true_pars_) {
+    toy = toy_;
+    true_pars = true_pars_;
+
+    dtoy = dout->mkdir(TString::Format("seed%02i_toy%02i", seed, toy));
+
+    ////
+
+    dtoy->mkdir("finalized_templates")->cd();
+
+    bkg_templates = bkg_templates_;
+    for (Template* t : *bkg_templates)
+      t->h_final = finalize_template(t->h);
+
+    fit::curr_bkg_template = (*bkg_templates)[0];
+    fit::h_sig = finalize_template(sig_template);
+
+    fit::h_data_real = fit::h_data = hist_with_binning("h_data", TString::Format("toy %i", toy));
+    for (const VertexPair& p : v2v)
+      fit::h_data->Fill(p.d2d());
+
+    ////
+
+    dtoy->mkdir("fit_results")->cd();
+
+    const test_stat_t t_obs_0 = calc_test_stat(0);
+    t_obs_0.print("t_obs_0");
+
+    //scan_likelihood();
+    draw_likelihood(t_obs_0);
+    draw_fit(t_obs_0);
 
     t_fit_info->Fill();
   }
--- MFVNeutralino/test/One2Two/Fitter.h
+++ MFVNeutralino/test/One2Two/Fitter.h
@@ -41,14 +41,32 @@ namespace mfv {
     std::vector<double> glb_scan_max_pars;
     std::vector<double> glb_scan_max_pars_errs;
 
-    double glb_scanmin_sb_maxtwolnL;
-    std::vector<double> glb_scanmin_sb_max_pars;
-    std::vector<double> glb_scanmin_sb_max_pars_errs;
-    Template* glb_scanmin_sb_template;
-    double glb_scanmin_b_maxtwolnL;
-    std::vector<double> glb_scanmin_b_max_pars;
-    std::vector<double> glb_scanmin_b_max_pars_errs;
-    Template* glb_scanmin_b_template;
+    struct min_lik_t {
+      bool ok;
+      int istat;
+      double maxtwolnL;
+      double mu_sig;
+      double err_mu_sig;
+      double mu_bkg;
+      double err_mu_bkg;
+      bool all_ok;
+      Template* nuis;
+      std::vector<std::pair<double, Template*> > nuis_region;
+
+      min_lik_t() : ok(true), maxtwolnL(-1e300), mu_sig(-1), err_mu_sig(-1), mu_bkg(-1), err_mu_bkg(-1), all_ok(true) {}
+
+      std::pair<std::vector<double>, std::vector<double> > get_template_par_ranges(const double up) const;
+      void print(const char* header, const char* indent="  ") const;
+    };
+
+    struct test_stat_t {
+      min_lik_t h1;
+      min_lik_t h0;
+      double t;
+      bool ok() const { return h1.ok && h0.ok; }
+
+      void print(const char* header, const char* indent="  ") const;
+    };
 
     ////////////////////////////////////////////////////////////////////////////
 
@@ -66,8 +84,12 @@ namespace mfv {
     TH1D* finalize_template(TH1D* h);
     void book_toy_fcns_and_histos();
     void fit_globals_ok();
-    bool scan_likelihood();
-    bool scanmin_likelihood(bool bkg_only);
+    //    bool scan_likelihood();
+    void draw_likelihood(const test_stat_t& t);
+    void draw_fit(const test_stat_t& t);
+    min_lik_t scanmin_likelihood(double mu_sig, bool fix_mu_sig);
+    test_stat_t calc_test_stat(double fix_mu_sig_val=0);
+    void make_toy_data(int i_toy_signif, int i_toy_limit, int n_sig, int n_bkg);
     void fit(int toy_, Templates* bkg_templates, TH1D* sig_template, const VertexPairs& v2v, const std::vector<double>& true_pars_);
   };
 }
