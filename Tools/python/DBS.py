eaa4a6057c25e048941d517f510b4d17408b5afc
Jordan Tucker <jordantucker@gmail.com>
2014-07-30 08:51:48 -0500

    das_query takes json option to return the json output from das_client; files_for_events.

--- Tools/python/DBS.py
+++ Tools/python/DBS.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import os
+from collections import defaultdict
+from pprint import pprint
 
 # Could use DBSAPI or DASAPI or whatever, but I'm too lazy to learn it.
 
 class das_query:
-    def __init__(self, ana01=False, ana02=False, ana03=False):
+    def __init__(self, ana01=False, ana02=False, ana03=False, json=False):
         if sum((ana01, ana02, ana03)) > 1:
             raise ValueError('only one of ana0X options allowed')
         self.ana01 = ana01
@@ -17,10 +19,15 @@ class das_query:
         if ana03:
             self.extra = self.extra.replace('01', '03')
         self.cmd = "das_client.py --limit=0 --query '%s %%s'" % self.extra
+        self.json = json
+        if json:
+            self.cmd += ' --format=json'
         
     def __call__(self, query, line_filter=lambda s: True, line_xform=lambda s: s):
         full_cmd = self.cmd % query
         cmdout = os.popen(full_cmd).readlines()
+        if self.json:
+            return cmdout[0]
         ret = []
         for line in cmdout:
             line = line.strip()
@@ -61,3 +68,52 @@ def sites_for_dataset(dataset, ana01=False, ana02=False, ana03=False):
     return das_query(ana01, ana02, ana03)('dataset=%s site' % dataset,
                                           line_filter=lambda s: s.startswith('T'))
 
+def files_for_events(run_events, dataset, ana01=False, ana02=False, ana03=False):
+    run_lumis = defaultdict(list)
+    for x in run_events: # list of runs, or list of (run, event), or list of (run, lumi, event)
+        if type(x) == int:
+            run_lumis[x] = None
+        elif len(x) == 2:
+            run_lumis[x[0]] = None
+        else:
+            run_lumis[x[0]].append(x[1])
+
+    files = []
+
+    json_str = das_query(ana01, ana02, ana03, json=True)('file,run,lumi dataset=%s' % dataset)
+    #open('json_str','wt').write(json_str)
+    #json_str = open('json_str').read()
+    obj = eval(json_str) # json.loads doesn't work...
+    #pprint(obj)
+    for x in obj:
+        #assert len(x['run']) == len(x['lumi'])
+        assert len(set(y['name'] for y in x['file'])) == 1
+
+        keep = False
+        class StopIt(Exception):
+            pass
+
+        try:
+            for run_d in x['run']:
+                run = run_d['run_number']
+                allowed = run_lumis[run]
+                if allowed is None:
+                    keep = True
+                    raise StopIt()
+
+                for lumi_d in x['lumi']:
+                    for lumi_lo, lumi_hi in lumi_d['number']:
+                        for lumi in allowed:
+                            if lumi >= lumi_lo and lumi <= lumi_hi:
+                                keep = True
+                                raise StopIt()
+        except StopIt:
+            pass
+
+        if keep:
+            files.append(str(x['file'][0]['name']))
+
+    return files
+
+if __name__ == '__main__':
+    pprint(files_for_events([(194711,1,380339)], '/MultiJet1Parked/Run2012B-05Nov2012-v2/AOD'))
