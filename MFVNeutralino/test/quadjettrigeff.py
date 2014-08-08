f1c6449035dc043ccfcdf2159f7ad2a317a1bbec
Jordan Tucker <jordantucker@gmail.com>
2014-08-08 08:41:10 -0500

    SingleMu in Samples.py.

--- MFVNeutralino/test/quadjettrigeff.py
+++ MFVNeutralino/test/quadjettrigeff.py
@@ -84,12 +84,7 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
 
     mc_samples = [Samples.qcdmupt15] + Samples.ttbar_samples + Samples.leptonic_background_samples
 
-    data_samples = [
-        Samples.DataSample('SingleMu2012A', '/SingleMu/Run2012A-22Jan2013-v1/AOD'),
-        Samples.DataSample('SingleMu2012B', '/SingleMu/Run2012B-22Jan2013-v1/AOD'),
-        Samples.DataSample('SingleMu2012C', '/SingleMu/Run2012C-22Jan2013-v1/AOD'),
-        Samples.DataSample('SingleMu2012D', '/SingleMu/Run2012D-22Jan2013-v1/AOD'),
-        ]
+    data_samples = Samples.auxiliary_data_samples
 
     for sample in mc_samples:
         sample.events_per = 20000
