f49382aa28bfb9143cc86bd6bc3106d293edf5be
Jordan Tucker <jordantucker@gmail.com>
2014-08-20 07:33:14 -0500

    setup_endpath: fix case where weight_src is empty.

--- Tools/python/SimpleTriggerEfficiency_cfi.py
+++ Tools/python/SimpleTriggerEfficiency_cfi.py
@@ -10,5 +10,8 @@ def setup_endpath(process, weight_src = ''):
     process.SimpleTriggerEfficiency.trigger_results_src = cms.InputTag('TriggerResults', '', process.name_())
     if type(weight_src) == cms.InputTag:
         weight_src = weight_src.moduleLabel
-    weight_obj = getattr(process, weight_src)
-    process.epSimpleTriggerEfficiency = cms.EndPath(weight_obj * process.SimpleTriggerEfficiency)
+    if weight_src:
+        weight_obj = getattr(process, weight_src)
+        process.epSimpleTriggerEfficiency = cms.EndPath(weight_obj * process.SimpleTriggerEfficiency)
+    else:
+        process.epSimpleTriggerEfficiency = cms.EndPath(process.SimpleTriggerEfficiency)
