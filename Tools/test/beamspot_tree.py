890c81563644ef5f2aaaa731723c604466f33e91
Jordan Tucker <jordantucker@gmail.com>
2014-08-26 08:18:16 -0500

    Run on AOD.

--- Tools/test/beamspot_tree.py
+++ Tools/test/beamspot_tree.py
@@ -1,29 +1,27 @@
 import sys
-from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
+from JMTucker.Tools.BasicAnalyzer_cfg import *
+geometry_etc(process, 'FT_P_V42_AN4::All')
 
-process.source.fileNames = ['/store/user/tucker/TTJets_HadronicMGDecays_8TeV-madgraph/mfvntuple_v18/c761ddfa7f093d8f86a338439e06a1d4/ntuple_1_1_nWG.root']
+process.source.fileNames = ['/store/data/Run2012D/MultiJet1Parked/AOD/part1_10Dec2012-v1/10000/002C29AE-3344-E211-85F6-001E673973D2.root']
 process.TFileService.fileName = 'beamspot_tree.root'
 
-process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
-process.load('JMTucker.MFVNeutralino.AnalysisCuts_cfi')
-process.mfvAnalysisCutsOnlyOneVtx = process.mfvAnalysisCuts.clone(min_nvertex = 1, max_nvertex = 1)
-
-process.BeamSpotTree = cms.EDAnalyzer('BeamSpotTreeProducer',
-                                      event_src = cms.InputTag('mfvEvent'),
-                                      )
-process.p = cms.Path(process.mfvSelectedVerticesTight * process.mfvAnalysisCutsOnlyOneVtx * process.BeamSpotTree)
+process.bs = cms.EDAnalyzer('BeamSpotTreer',
+                            beamspot_src = cms.InputTag('offlineBeamSpot'),
+                            primary_vertex_src = cms.InputTag('offlinePrimaryVertices'),
+                            )
+process.p = cms.Path(process.bs)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
     samples = Samples.from_argv(Samples.data_samples)
 
     for s in Samples.data_samples:
-        s.json = '../../MFVNeutralino/test/ana_all.json'
+        s.json = '207477.json'
+        s.lumis_per = 1
+        s.total_lumis = -1
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     cs = CRABSubmitter('BeamSpotTree',
                        job_control_from_sample = True,
-                       use_ana_dataset = True,
-                       run_half_mc = True,
                        )
-    cs.submit_all(samples)
+    cs.submit_all([Samples.MultiJetPk2012D1])
