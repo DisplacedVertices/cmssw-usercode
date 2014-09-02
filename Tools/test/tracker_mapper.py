4e4291aad4e1d3318ac1ced3739ef11521b02537
Jennifer Chu <jyc72@cornell.edu>
2014-09-02 09:55:27 -0500

    Plot phi of track reference point w.r.t. beamspot, remove genParticle plot to run on data.

--- Tools/test/tracker_mapper.py
+++ Tools/test/tracker_mapper.py
@@ -4,27 +4,23 @@ from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
 process.source.fileNames = ['/store/mc/Summer12_DR53X/TTJets_HadronicMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/002A756C-FA15-E211-9FA6-485B39800B75.root']
 process.TFileService.fileName = 'tracker_mapper.root'
 
-#process.source.fileNames = ['/store/user/tucker/RelValSingleMuPt1000_CMSSW_5_3_6-START53_V14-v2_GEN-SIM-RECO.7C1AEF1C-FF29-E211-BA60-003048678B20.root']
-
 process.TrackerMapper = cms.EDAnalyzer('TrackerMapper',
                                        track_src = cms.InputTag('generalTracks'),
+                                       beamspot_src = cms.InputTag('offlineBeamSpot'),
                                        use_rechits = cms.bool(False)
                                        )
 process.p = cms.Path(process.TrackerMapper)
-
-if 1:
-    process.source.fileNames = ['file:/uscms/home/tucker/scratch/reco_1_2_L1S.root']
-    process.TrackerMapper.use_rechits = True
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
 
     for s in Samples.data_samples:
-        s.json = 'ana_5pc.json'
+        s.json = '../../MFVNeutralino/test/ana_all.json'
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     cs = CRABSubmitter('TrackerMapper',
-                       total_number_of_events = -1,
-                       events_per_job = 500000,
+                       job_control_from_sample = True,
+                       #total_number_of_events = -1,
+                       #events_per_job = 500000,
                        )
-    cs.submit_all([Samples.myttbarpydesignnoputkex])
+    cs.submit_all(Samples.data_samples)
