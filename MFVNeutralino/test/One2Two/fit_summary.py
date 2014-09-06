0fda7ce1d1260090404da2921063f169653805a9
Jordan Tucker <jordantucker@gmail.com>
2014-09-06 10:02:53 -0400

    Fix empty plots by cding into the canvas; plot style.

--- MFVNeutralino/test/One2Two/fit_summary.py
+++ MFVNeutralino/test/One2Two/fit_summary.py
@@ -2,10 +2,14 @@
 
 import sys, os, glob
 from JMTucker.Tools.ROOTTools import *
+
+from JMTucker.Tools.Samples import mfv_signal_samples
+mfv_signal_samples.sort(key=lambda s: s.name)
+
 set_style()
 ROOT.gStyle.SetOptStat(1000000001)
-ps = plot_saver('plots/o2t_fit_summary', size=(600,600), log=False)
 ROOT.TH1.AddDirectory(0)
+ps = plot_saver('plots/o2t_fit_summary', size=(600,600), log=False)
 
 def wd(s, i):
     if s > 0:
@@ -17,19 +21,130 @@ def file(s, i, n):
     return ROOT.TFile(os.path.join(wd(s, i), n))
 
 def make_h(name):
-    return ROOT.TH1D(name, '', 25, 0, 25)
+    return ROOT.TH1D(name, '', 24, 0, 24)
 
+def draw_h_labels(h, thing, stat, obj_cache=[]):
+    mean = stat in 'mean mu'
+
+    y_range = None
+    y_label_min = None
+    y_label_height = None
+    lifetime_pos = None
+    mass_pos = None
+    yax_title = None
+    
+    if mean:
+        if 'pull' in thing:
+            y_range = (-3, 3)
+            y_label_min = 2.5
+            y_label_height = 0.1
+            lifetime_pos = (0.965, 1.757, 3.639, 2.255)
+            mass_pos = (-2.129, -3.698, -0.136, -3.108)
+            yax_title = '%s mean of pull'
+        elif thing == 'mu_sig':
+            y_range = (-1, 10)
+            y_label_min = 9
+            y_label_height = 0.1
+    else:
+        if 'pull' in thing:
+            y_range = (0, 6)
+            y_label_min = 5.5
+            y_label_height = 0.1
+            lifetime_pos = (0.965, 4.757, 3.639, 5.255)
+            mass_pos = (-2.129, -0.698, -0.136, -0.108)
+            yax_title = '%s width of pull'
+
+    if y_range is None:
+        print 'need y_range etc. for thing %s stat %s' % (thing, stat)
+        return
+
+    yax_title = yax_title % ('fitted' if stat in 'mu sigma' else 'sample') + (', s' if 'sig' in thing else ', b')
+
+    xax = h.GetXaxis()
+    for i, sample in enumerate(mfv_signal_samples):
+        mass = sample.name.replace('M0','M').split('M')[1] + ' GeV'
+        xax.SetBinLabel(i+1, mass)
+    xax.SetLabelSize(0.035)
+    xax.SetLabelOffset(0.0035)
+    xax.SetLabelFont(62)
+    xax.LabelsOption('v')
+    xax.SetRangeUser(0, 24)
+
+    yax = h.GetYaxis()
+    yax.SetRangeUser(*y_range)
+    yax.SetTitle(yax_title)
+    
+    per_div = 6
+    taus = ['100 #mum', '300 #mum', '1 mm', '10 mm']
+    y_label_min = [y_label_min]*4
+    for i in xrange(2):
+        y_label_min[i] -= 0.005 # for the #mu descender
+    lines = []
+    paves = []
+    for i in xrange(0, len(mfv_signal_samples), per_div):
+        p = ROOT.TPaveText(i+1, y_label_min[i/per_div], i+per_div-1, y_label_min[i/per_div] + y_label_height)
+        p.AddText(taus[i/per_div])
+        p.SetTextSize(0.05)
+        p.SetBorderSize(0)
+        p.Draw()
+        paves.append(p)
+
+        if i == 0:
+            continue
+
+        l = ROOT.TLine(i, y_range[0], i, y_range[1])
+        l.SetLineWidth(2)
+        l.SetLineColor(ROOT.kWhite)
+        l.Draw()
+        lines.append(l)
+
+        l = ROOT.TLine(i, y_range[0], i, y_range[1])
+        l.SetLineWidth(2)
+        l.SetLineStyle(2)
+        l.Draw()
+        lines.append(l)
+
+    lifetime = ROOT.TPaveText(*lifetime_pos)
+    lifetime.AddText('#tau_{#chi^{0}}')
+    lifetime.SetBorderSize(0)
+    lifetime.Draw()
+
+    mass = ROOT.TPaveText(*mass_pos)
+    mass.AddText('M_{#chi^{0}}')
+    mass.SetBorderSize(0)
+    mass.Draw()
+
+    h.SetLineWidth(2)
+    h.SetStats(0)
+    y = 0 if mean else 1
+    eyeline = ROOT.TLine(0, y, 24, y)
+    eyeline.SetLineStyle(ROOT.kDashed)
+    eyeline.Draw()
+
+    obj_cache.extend(lines)
+    obj_cache.extend(paves)
+    obj_cache.append(lifetime)
+    obj_cache.append(mass)
+    obj_cache.append(eyeline)
+    
 for sig_strength in (0, 1, 5, 10):
+    #if sig_strength != 1:
+    #0    continue
+
     h = make_h('ss%i_nistat3' % sig_strength)
     for i in xrange(1, 25):
         f = file(sig_strength, i, 'h_seed.root')
         hh = f.Get('c0').FindObject('h_seed')
         h.SetBinContent(i, hh.GetEntries())
+    ps.c.cd()
     h.Draw()
     ps.save(h.GetName())
 
     for hyp in 'h1 h0'.split():
         for thing in 'mu_sig mu_bkg mu_sig_err mu_bkg_err mu_sig_pull mu_bkg_pull nuis0 nuis0_err nuis1 nuis1_err'.split():
+            if hyp != 'h1' or (thing != 'mu_bkg_pull' and thing != 'mu_sig_pull'):
+                continue
+            
             if hyp == 'h0' and 'sig' in thing:
                 continue
 
@@ -56,25 +171,10 @@ for sig_strength in (0, 1, 5, 10):
                     hs['sigma'].SetBinContent(i, fcn.GetParameter(2))
                     hs['sigma'].SetBinError(i, fcn.GetParError(2))
 
+            ps.c.cd()
             for stat in stats:
-                mean = stat in 'mean mu'
                 h = hs[stat]
                 h.Draw('hist e')
-                h.Fit('pol0', 'Q')
-                fcn = h.FindObject('pol0')
-                fcn.SetLineWidth(1)
-                fcn.SetLineColor(2)
-                h.SetLineWidth(2)
-                h.SetStats(0)
-                y = 0 if mean else 1
-                l = ROOT.TLine(0, y, 25, y)
-                l.SetLineStyle(ROOT.kDashed)
-                l.Draw()
-#                if mean:
-#                    if 'pull' in thing:
-#                        h.GetYaxis().SetRangeUser(-3, 3)
-#                else:
-#                    if 'pull' in thing:
-#                        h.GetYaxis().SetRangeUser(0, 6)
+                draw_h_labels(h, thing, stat)
                 ps.save(h.GetName())
 
