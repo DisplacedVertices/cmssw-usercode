4aad2a3b1ac8627605a7c094bb0774e2b000fe70
Jennifer Chu <jyc72@cornell.edu>
2014-09-09 16:55:14 -0500

    Add triggerFilter, run over data and MC samples.

--- Tools/test/tracker_mapper.py
+++ Tools/test/tracker_mapper.py
@@ -4,23 +4,29 @@ from JMTucker.Tools.BasicAnalyzer_cfg import cms, process
 process.source.fileNames = ['/store/mc/Summer12_DR53X/TTJets_HadronicMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/002A756C-FA15-E211-9FA6-485B39800B75.root']
 process.TFileService.fileName = 'tracker_mapper.root'
 
+from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
+process.triggerFilter = hltHighLevel.clone()
+process.triggerFilter.HLTPaths = ['HLT_QuadJet50_v*']
+process.triggerFilter.andOr = True # = OR
+
 process.TrackerMapper = cms.EDAnalyzer('TrackerMapper',
                                        track_src = cms.InputTag('generalTracks'),
                                        beamspot_src = cms.InputTag('offlineBeamSpot'),
                                        use_rechits = cms.bool(False)
                                        )
-process.p = cms.Path(process.TrackerMapper)
+
+process.p = cms.Path(process.triggerFilter * process.TrackerMapper)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
 
+    for s in Samples.ttbar_samples + Samples.qcd_samples:
+        s.events_per = 500000
     for s in Samples.data_samples:
         s.json = '../../MFVNeutralino/test/ana_all.json'
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     cs = CRABSubmitter('TrackerMapper',
                        job_control_from_sample = True,
-                       #total_number_of_events = -1,
-                       #events_per_job = 500000,
                        )
-    cs.submit_all(Samples.data_samples)
+    cs.submit_all(Samples.ttbar_samples + Samples.qcd_samples + Samples.data_samples)
