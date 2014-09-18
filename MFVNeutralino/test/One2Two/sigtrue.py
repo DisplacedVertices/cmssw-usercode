38425f7d01575ace44e639de78dfa2eb5e34fa58
Jordan Tucker <jordantucker@gmail.com>
2014-09-18 11:29:19 -0500

    For getting the true means for pulls in draw_fit.

---
+++ MFVNeutralino/test/One2Two/sigtrue.py
@@ -0,0 +1,22 @@
+from base import *
+
+path = 'crab/MiniTreeV20'
+
+r = []
+
+for s in Samples.mfv_signal_samples:
+    w = s.partial_weight * ac.int_lumi * ac.scale_factor
+    f, t = get_f_t(os.path.join(path, s.name + '.root'))
+    n = t.Draw('nvtx', 'nvtx == 2')
+    nn = n*w
+    print s.name, n, w, nn
+    r.append('%.4f' % nn)
+
+print '[' + ', '.join(r) + ']'
+
+
+
+
+    
+
+    
