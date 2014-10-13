0a3b088dfaa82e1ecf72a997d9231f3043729429
Jordan Tucker <jordantucker@gmail.com>
2014-10-13 13:49:41 -0500

    Revert the BB stuff; bend_bkg option.

--- MFVNeutralino/test/One2Two/Fitter.cc
+++ MFVNeutralino/test/One2Two/Fitter.cc
@@ -25,6 +25,7 @@ namespace mfv {
 
     int n_bins = -1;
 
+    bool bend_bkg = false;
     std::vector<double> eta_bkg;
     std::vector<double> a_bkg;
     TemplateInterpolator* interp;
@@ -83,15 +84,6 @@ namespace mfv {
     }
 
     double twolnL(double mu_sig, double mu_bkg, double par0, double par1) {
-      //return -pow(mu_sig - 0, 2) -pow(mu_bkg - 234, 2) - pow(par0 - 0.03, 2) - pow(par1 - 0.01, 2);
-
-      //static int ncalls = 0;
-      //printf("call %i\n", ++ncalls);
-      //if (ncalls == 5075000) {
-      //  extra_prints=1;
-      //  TemplateInterpolator::extra_prints=1;
-      //}
-
       //if (TMath::IsNaN(mu_sig) || TMath::IsNaN(mu_bkg) || TMath::IsNaN(par0) || TMath::IsNaN(par1))
       //  jmt::vthrow("NaN in twolnL(%f, %f, %f, %f)", mu_sig, mu_bkg, par0, par1);
 
@@ -104,7 +96,8 @@ namespace mfv {
       if (par1 < 0.0005 || TMath::IsNaN(par1))
         par1 = 0.0005;
 
-      interp->interpolate(std::vector<double>({par0, par1}), &a_bkg);
+      interp->interpolate(par0, par1);
+
       for (int i = 1; i < n_bins; ++i) {
         if (TMath::IsNaN(a_bkg[i]))
           jmt::vthrow("NaN in interpolation (%f, %f) in bin %i", par0, par1, i);
@@ -119,57 +112,9 @@ namespace mfv {
       std::vector<double> A_bkg(n_bins+2, 0.);
       double A_sig_sum = 0, A_bkg_sum = 0;
 
-      const double eps = 1e-9;
-      const double sqrteps = sqrt(eps);
-      const int maxit = 10000;
       for (int i = 1; i <= n_bins; ++i) {
-        double t = 1;
-
-        if (a_data[i] > eps) {
-          double t_lo = -n_sig_orig/mu_sig + eps;
-          double t_hi = 1 - eps;
-          double y = 1e99;
-          bool found = false;
-
-          if (extra_prints)
-            printf("find t: bin %i  d: %5.1f  a_bkg: %10.3e  a_sig: %10.3e\n", i, a_data[i], a_bkg[i], a_sig[i]);
-
-          int it = 0;
-          for (; it < maxit; ++it) {
-            t = 0.5*(t_lo + t_hi);
-
-            y = a_data[i] / (1 - t) - mu_bkg * (a_bkg[i] - t * mu_bkg * eta_bkg[i] * eta_bkg[i] * a_bkg[i] * a_bkg[i]) - mu_sig * a_sig[i] / (1 + t * mu_sig / n_sig_orig);
-
-            if (y > 0)
-              t_hi = t;
-            else if (y < 0)
-              t_lo = t;
-
-            if (extra_prints)
-              printf("  #%3i:  t: %13.6e  y: %13.6e  new t: [%13.6e %13.6e]\n", it, t, y, t_lo, t_hi);
-
-            if (fabs(y) < eps || (t_hi - t_lo < eps && fabs(y) < sqrteps)) {
-              found = true;
-              break;
-            }
-          }
-
-          if (!found)
-            jmt::vthrow("zero finding with mu_b %f mu_s %f par0 %f par1 %f failed at it %i with bin %i  d: %5.1f  a_bkg: %10.3e  a_sig: %10.3e t %f [%f %f] y %f\n", mu_bkg, mu_sig, par0, par1, it, i, a_data[i], a_bkg[i], a_sig[i], t, t_lo, t_hi, y);
-        }
-        else if (extra_prints)
-          printf("find t: bin %i  d: 0 -> t = 1\n", i);
-
-        A_sig[i] = a_sig[i] / (t * mu_sig / n_sig_orig + 1);
-        A_bkg[i] = a_bkg[i] - t * mu_bkg * a_bkg[i] * a_bkg[i] * eta_bkg[i] * eta_bkg[i];
-        if (A_bkg[i] < 0)
-          A_bkg[i] = 0;
-
-        if (A_sig[i] < 0 || A_bkg[i] < 0)
-          jmt::vthrow("negative A_sig");
-
-        A_sig_sum += A_sig[i];
-        A_bkg_sum += A_bkg[i];
+        A_sig_sum += (A_sig[i] = a_sig[i]);
+        A_bkg_sum += (A_bkg[i] = a_bkg[i]*(1 + bend_bkg ? eta_bkg[i] : 0));
       }
 
       if (extra_prints) {
@@ -184,35 +129,22 @@ namespace mfv {
         printf("   %10.3e %10.3e %10.3e %10.3e %10.3e %10.3e\n", a_bkg_sum, A_bkg_sum, A_bkg_sum - a_bkg_sum, a_sig_sum, A_sig_sum, A_sig_sum - a_sig_sum);
       }
 
-      double lnL_bb_bkg = 0;
-      double lnL_bb_sig = 0;
-      double lnL_fit = 0;
+      double lnL = 0;
 
       for (int i = 1; i <= n_bins; ++i) {
         A_bkg[i] /= A_bkg_sum;
         A_sig[i] /= A_sig_sum;
 
-        const double dlnL_bb_bkg = a_bkg[i] == 0. ? 0. : -0.5 * pow((a_bkg[i] - A_bkg[i])/a_bkg[i]/eta_bkg[i], 2);
-        const double dlnL_bb_sig = n_sig_orig * a_sig[i] * log(n_sig_orig * A_sig[i]) - n_sig_orig * A_sig[i]; 
-
         const double nu_sig = mu_sig * A_sig[i];
         const double nu_bkg = mu_bkg * A_bkg[i];
         const double nu_sum = nu_sig + nu_bkg;
-        const double dlnL_fit = -nu_sum + a_data[i] * log(nu_sum);
+        const double dlnL = -nu_sum + a_data[i] * log(nu_sum);
 
-        lnL_fit += dlnL_fit;
-        lnL_bb_bkg += dlnL_bb_bkg;
-        lnL_bb_sig += dlnL_bb_sig;
+        lnL += dlnL;
 
         if (extra_prints)
-          printf("i: %2i  nu_bkg: %7.3f  nu_sig: %7.3f  nu: %7.3f  n: %6.1f    dlnL: %10.6f + %10.6f + %10.6f  lnL: %10.6f + %10.6f + %10.6f\n", i, nu_bkg, nu_sig, nu_sum, a_data[i], dlnL_fit, dlnL_bb_bkg, dlnL_bb_sig, lnL_fit, lnL_bb_bkg, lnL_bb_sig);
+          printf("i: %2i  nu_bkg: %7.3f  nu_sig: %7.3f  nu: %7.3f  n: %6.1f    dlnL: %10.6f  lnL: %10.6f\n", i, nu_bkg, nu_sig, nu_sum, a_data[i], dlnL, lnL);
       }
-
-      double lnL = lnL_fit; 
-      lnL += lnL_bb_bkg;
-      lnL += lnL_offset + lnL_bb_sig;
-
-      //lnL += -0.5*pow((par0 - 0.03)/0.01, 2) - 0.5*pow((par1 - 0.01)/0.01, 2);
 
       if (TMath::IsNaN(lnL))
         jmt::vthrow("lnL is NaN");
@@ -262,7 +194,8 @@ namespace mfv {
 
       env("mfvo2t_fitter" + uname),
       print_level(env.get_int("print_level", -1)),
-      bkg_gaussians(env.get_bool("bkg_gaussians", false)),
+      bkg_gaussians(env.get_bool("bkg_gaussians", true)),
+      bend_bkg(env.get_bool("bend_bkg", false)),
       allow_negative_mu_sig(env.get_bool("allow_negative_mu_sig", false)),
       run_minos(env.get_bool("run_minos", true)),
       draw_bkg_templates(env.get_bool("draw_bkg_templates", 0)),
@@ -290,6 +223,7 @@ namespace mfv {
     printf("Fitter%s config:\n", name.c_str());
     printf("print_level: %i\n", print_level);
     printf("bkg_gaussians: %i\n", bkg_gaussians);
+    printf("bend_bkg: %i\n", bend_bkg);
     printf("allow_negative_mu_sig: %i\n", allow_negative_mu_sig);
     printf("run_minos: %i\n", run_minos);
     printf("draw_bkg_templates: %i\n", draw_bkg_templates);
@@ -761,6 +695,8 @@ namespace mfv {
       fit::eta_bkg = { -1, 0.001, 0.001, 0.01, 0.35, 1.5, 15. };
     else
       fit::eta_bkg = { -1, 1e-4, 1e-4, 1e-4, 1e-4, 1e-4, 1e-4 };
+
+    fit::bend_bkg = bend_bkg;
 
     bkg_templates = bkg_templater->get_templates();
     fit::interp = new TemplateInterpolator(bkg_templates, fit::n_bins, bkg_templater->par_info(), fit::a_bkg);
