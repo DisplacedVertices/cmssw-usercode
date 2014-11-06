7a67b4559a997788b344d0bc4b0c5aeacbd121c3
Jordan Tucker <jordantucker@gmail.com>
2014-11-05 19:29:32 -0600

    BBv7

--- MFVNeutralino/test/One2Two/fit_summary.py
+++ MFVNeutralino/test/One2Two/fit_summary.py
@@ -5,7 +5,7 @@ from JMTucker.Tools.ROOTTools import *
 
 from JMTucker.Tools.Samples import mfv_signal_samples
 
-special = 'BBv5'
+special = 'BBv7'
 
 set_style()
 ROOT.gStyle.SetOptStat(1000000001)
