cdb9dff555af2965369b090c1b374019a4323d6c
Jennifer Chu <jyc72@cornell.edu>
2014-11-12 05:51:22 -0600

    Run separately for 0v,1v,2v events.

--- MFVNeutralino/test/gen_histos.py
+++ MFVNeutralino/test/gen_histos.py
@@ -10,6 +10,16 @@ process.load('JMTucker.MFVNeutralino.GenHistos_cff')
 
 process.p = cms.Path(process.mfvGenParticleFilter * process.mfvGenHistos)
 
+process.histos1v = process.mfvGenHistos.clone()
+process.histos2v = process.mfvGenHistos.clone()
+process.histos0v = process.mfvGenHistos.clone()
+
+process.events1v = cms.EDFilter('EventIdVeto', list_fn = cms.string('events1v.txt.gz'), use_run = cms.bool(True))
+process.events2v = cms.EDFilter('EventIdVeto', list_fn = cms.string('events2v.txt.gz'), use_run = cms.bool(True))
+process.p1v = cms.Path(~process.events1v * process.histos1v)
+process.p2v = cms.Path(~process.events2v * process.histos2v)
+process.p0v = cms.Path(process.events1v * process.events2v * process.histos0v)
+
 if debug:
     process.printList = cms.EDAnalyzer('JMTParticleListDrawer',
                                        src = cms.InputTag('genParticles'),
@@ -20,17 +30,25 @@ if debug:
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
-    samples = [
-        Samples.TupleOnlyMCSample('mfv01000', '/mfv_neutralino_tau01000um_M0400/tucker-mfv_neutralino_tau01000um_M0400-554987f53c5d1493b23246ebbecfd44a/USER')
-        ]
-    for sample in samples:
-        sample.dbs_url_num = 3
-        sample.is_pythia8 = True
+    samples = Samples.ttbar_samples + Samples.qcd_samples
+
+    def pset_modifier(sample):
+        to_add = []
+        to_modify = []
+
+        to_add.append("process.events1v.list_fn = '%s_1v.txt.gz'" % sample.name)
+        to_add.append("process.events2v.list_fn = '%s_2v.txt.gz'" % sample.name)
+        return to_add, to_modify
+
+    def crab_cfg_modifier(sample):
+        return [('USER', 'additional_input_files', '%s_1v.txt.gz,%s_2v.txt.gz' % (sample.name, sample.name))]
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     cs = CRABSubmitter('GenHistos',
                        total_number_of_events = -1,
                        events_per_job = 20000,
                        skip_common = True,
+                       pset_modifier = pset_modifier,
+                       crab_cfg_modifier = crab_cfg_modifier,
                        )
     cs.submit_all(samples)
