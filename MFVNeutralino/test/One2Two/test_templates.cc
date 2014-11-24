abb1ceac8bd058e5063dc827af4e137f04c28d49
Jordan Tucker <jordantucker@gmail.com>
2014-11-24 12:24:18 -0600

    Dump templates for comparing interpolation levels.

--- MFVNeutralino/test/One2Two/test_templates.cc
+++ MFVNeutralino/test/One2Two/test_templates.cc
@@ -14,42 +14,62 @@ int main() {
 
   TFile* out_f = new TFile("test_templates.root", "recreate");
   TRandom3* rand = new TRandom3(jmt::seed_base);
-  mfv::ToyThrower* tt = new mfv::ToyThrower("", "jen_crab", out_f, rand);
+  mfv::ToyThrower* tt = new mfv::ToyThrower("", "trees", out_f, rand);
   mfv::Templater* ter = new mfv::ClearedJetsTemplater("", out_f, rand);
 
-  tt->throw_toy();
-  ter->process(tt->toy_dataset);
+  ter->process(tt->data);
 
-  std::vector<double> a_bkg;
-  mfv::TemplateInterpolator* interp = new mfv::TemplateInterpolator(ter->get_templates(), mfv::Template::binning().size(), ter->par_info(), a_bkg);
+  std::vector<double> a_bkg(6+2,0.);
+  mfv::TemplateInterpolator* interp = new mfv::TemplateInterpolator(ter->get_templates(), mfv::Template::binning().size()-1, ter->par_info(), a_bkg);
 
-  mfv::Templates* ts = ter->get_templates();
-  for (int i = 0; i < 4; ++i) {
-    mfv::Template* t = (*ts)[i];
-    printf("%s\n", t->title().c_str());
-    for (int j = 0; j < t->h->GetNbinsX()+2; ++j)
-      printf("  %2i: %f\n", j, t->h->GetBinContent(j));
-    printf("\n");
-  }
+  /*
+  mfv::TemplateInterpolator::extra_prints=1;
 
+  printf("0.029500,0.0455:\n");
+  interp->interpolate(0.029500,0.0455);
+  for (int i = 1; i <= 6; ++i)
+    printf(" %.6f", a_bkg[i]);
   printf("\n");
 
-  double z[5][2] = {
-    { 0, 0.0005 },
-    { 0, 0.001 },
-    { 0.001, 0.0005 },
-    { 0.001, 0.001 },
-    { 0.0005, 0.00075 }
-  };
+  printf("0.029500,0.046:\n");
+  interp->interpolate(0.029500,0.046);
+  for (int i = 1; i <= 6; ++i)
+    printf(" %.6f", a_bkg[i]);
+  printf("\n");
 
-  for (auto x : z) {
-    interp->interpolate(x[0], x[1]);
-    printf("interpolate %f, %f:\n", x[0], x[1]);
-    for (int i = 0; i < int(a_bkg.size()); ++i)
-      printf("  %2i: %f\n", i, a_bkg[i]);
+  printf("0.029500,0.0465\n");
+  interp->interpolate(0.029500,0.0465);
+  for (int i = 1; i <= 6; ++i)
+    printf(" %.6f", a_bkg[i]);
+  printf("\n");
+
+  return 1;
+  */
+
+  printf("%i %f %f %i %f %f\n", ter->par_info()[0].nsteps, ter->par_info()[0].start, ter->par_info()[0].step, ter->par_info()[1].nsteps, ter->par_info()[1].start, ter->par_info()[1].step);
+  mfv::Templates* ts = ter->get_templates();
+  for (mfv::Template* t : *ts) { 
+    printf("%.6f %.6f", t->par(0), t->par(1));
+    for (int i = 1; i <= 6; ++i)
+      printf(" %.6f", t->h->GetBinContent(i));
     printf("\n");
   }
-  
+
+  printf("interp:\n");
+
+  for (int imu = 0; imu < 180; ++imu) {
+    double mu = 0 + 0.0005 * imu;
+    for (int isig = 0; isig < 100; ++isig) {
+      double sig = 0.0005 + 0.0005 * isig;
+      interp->interpolate(mu, sig);
+
+      printf("%.6f %.6f", mu, sig);
+      for (int i = 1; i <= 6; ++i)
+        printf(" %.6f", a_bkg[i]);
+      printf("\n");
+    }
+  }
+    
   out_f->Write();
   out_f->Close();
   delete out_f;
