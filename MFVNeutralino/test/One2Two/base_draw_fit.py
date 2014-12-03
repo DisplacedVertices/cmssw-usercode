5d9a9d6f8632c225ffce69513b52332588ac5cad
Jordan Tucker <jordantucker@gmail.com>
2014-12-03 09:47:08 -0600

    Don't do the detree here, but in function call in importer.

--- MFVNeutralino/test/One2Two/base_draw_fit.py
+++ MFVNeutralino/test/One2Two/base_draw_fit.py
@@ -34,14 +34,14 @@ t.SetMarkerSize(1.)
 for fn in batch_fns:
     t.Add(fn)
 
-vars = 'seed toy true_pars[0] true_pars[1] true_pars[2] true_pars[3]'
+tree_vars = 'seed toy true_pars[0] true_pars[1] true_pars[2] true_pars[3]'
 for y in 'h1 h0'.split():
     for x in 'istat maxtwolnL mu_sig err_mu_sig mu_bkg err_mu_bkg nuis0 err_nuis0 nuis1 err_nuis1'.split():
-        vars += ' t_obs_0__%s_%s' % (y,x)
-vars += ' pval_signif sig_limit sig_limit_err sig_limit_fit_n sig_limit_fit_a sig_limit_fit_b sig_limit_fit_a_err sig_limit_fit_b_err sig_limit_fit_prob'
+        tree_vars += ' t_obs_0__%s_%s' % (y,x)
+tree_vars += ' pval_signif sig_limit sig_limit_err sig_limit_fit_n sig_limit_fit_a sig_limit_fit_b sig_limit_fit_a_err sig_limit_fit_b_err sig_limit_fit_prob'
 i_pval_signif = -9
 
-def xform(x):
+def tree_xform(x):
     z = []
     for y in x:
         y = y.strip()
@@ -51,11 +51,8 @@ def xform(x):
             z.append(float(y))
     return tuple(z)
 
-d = list(detree(t, vars.replace(' ', ':'), xform=xform))
-
-#for seed,toy,true_pars_0,true_pars_1,true_pars_2,true_pars_3,h1_istat,h1_maxtwolnL,h1_mu_sig,h1_err_mu_sig,h1_mu_bkg,h1_err_mu_bkg,h1_nuis0,h1_err_nuis0,h1_nuis1,h1_err_nuis1,h0_istat,h0_maxtwolnL,h0_mu_sig,h0_err_mu_sig,h0_mu_bkg,h0_err_mu_bkg,h0_nuis0,h0_err_nuis0,h0_nuis1,h0_err_nuis1,pval_signif,sig_limit,sig_limit_err,sig_limit_fit_n,sig_limit_fit_a,sig_limit_fit_b,sig_limit_fit_a_err,sig_limit_fit_b_err,sig_limit_fit_prob in d:
-#    if skip(h0_istat, h1_istat, sig_limit_fit_n):
-#        continue
+def do_detree():
+    d = list(detree(t, tree_vars.replace(' ', ':'), xform=tree_xform))
 
 ################################################################################
 
@@ -71,3 +68,8 @@ mu_bkg_true_mean = 39.47 * fudge
 
 def skip(h0_istat, h1_istat, sig_limit_fit_n):
     return h1_istat == 0 or (sig_scale < 0 and sig_limit_fit_n <= 3) #h0_istat <= 1 or h1_istat <= 1 or sig_limit_fit_n < 4
+
+#for seed,toy,true_pars_0,true_pars_1,true_pars_2,true_pars_3,h1_istat,h1_maxtwolnL,h1_mu_sig,h1_err_mu_sig,h1_mu_bkg,h1_err_mu_bkg,h1_nuis0,h1_err_nuis0,h1_nuis1,h1_err_nuis1,h0_istat,h0_maxtwolnL,h0_mu_sig,h0_err_mu_sig,h0_mu_bkg,h0_err_mu_bkg,h0_nuis0,h0_err_nuis0,h0_nuis1,h0_err_nuis1,pval_signif,sig_limit,sig_limit_err,sig_limit_fit_n,sig_limit_fit_a,sig_limit_fit_b,sig_limit_fit_a_err,sig_limit_fit_b_err,sig_limit_fit_prob in d:
+#    if skip(h0_istat, h1_istat, sig_limit_fit_n):
+#        continue
+#    ...
