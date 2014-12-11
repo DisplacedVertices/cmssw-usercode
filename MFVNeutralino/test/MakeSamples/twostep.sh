7c115fdc26cba65ab04ff9fdf12fe2c6a37746a9
Jordan Tucker <jordantucker@gmail.com>
2014-12-11 09:37:13 -0600

    Option for running minitree (now we are sixstep.sh).

--- MFVNeutralino/test/MakeSamples/twostep.sh
+++ MFVNeutralino/test/MakeSamples/twostep.sh
@@ -89,6 +89,22 @@ if [ -f ntuple.py ]; then
   echo NEV `edmEventSize -v ntuple.root | grep Events | head -1`
 fi
 
+if [ -f minitree.py ]; then
+  echo
+  echo done with ntuple at `date`, starting minitree step
+  echo
+  cmsRun -j $RUNTIME_AREA/crab_fjr_$NJob.xml minitree.py
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    echo cmsRun exited minitree step with error code $exit_code
+    echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    exit $exit_code
+  fi
+
+  echo NEV `python -c "import sys; sys.argv.append('-b'); import ROOT; f=ROOT.TFile('minitree.root'); print f.Get('mfvMiniTree/t').GetEntries()"`
+fi
+
 echo
 echo done at `date`
 echo TWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEPTWOSTEP
