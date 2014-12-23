3dda68732d4d396f03dd68142b0f2b98a4a3d0fc
Jordan Tucker <jordantucker@gmail.com>
2014-12-23 14:55:18 -0600

    Back to just a producer; put empty collections into the event if not passing presel.

--- MFVNeutralino/plugins/TrackMover.cc
+++ MFVNeutralino/plugins/TrackMover.cc
@@ -2,27 +2,24 @@
 #include "CLHEP/Random/RandomEngine.h"
 #include "CLHEP/Random/RandExponential.h"
 #include "CLHEP/Random/RandGauss.h"
-#include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "DataFormats/Math/interface/deltaR.h"
 #include "DataFormats/PatCandidates/interface/Jet.h"
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
-#include "FWCore/Framework/interface/EDFilter.h"
+#include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/Utilities/interface/RandomNumberGenerator.h"
 
-class MFVTrackMover : public edm::EDFilter {
+class MFVTrackMover : public edm::EDProducer {
 public:
   explicit MFVTrackMover(const edm::ParameterSet&);
 
 private:
-  bool select_track(const reco::TrackRef& tk) const;
-
-  virtual bool filter(edm::Event&, const edm::EventSetup&);
+  virtual void produce(edm::Event&, const edm::EventSetup&);
 
   const edm::InputTag tracks_src;
   const edm::InputTag primary_vertices_src;
@@ -76,13 +73,15 @@ MFVTrackMover::MFVTrackMover(const edm::ParameterSet& cfg)
 
   produces<reco::TrackCollection>();
   produces<reco::TrackCollection>("moved");
+  produces<int>("npreseljets");
+  produces<int>("npreselbjets");
   produces<pat::JetCollection>("jetsUsed");
   produces<pat::JetCollection>("bjetsUsed");
   produces<std::vector<double> >("flightAxis");
   produces<std::vector<double> >("moveVertex");
 }
 
-bool MFVTrackMover::filter(edm::Event& event, const edm::EventSetup&) {
+void MFVTrackMover::produce(edm::Event& event, const edm::EventSetup&) {
   edm::Handle<reco::TrackCollection> tracks;
   event.getByLabel(tracks_src, tracks);
 
@@ -101,6 +100,8 @@ bool MFVTrackMover::filter(edm::Event& event, const edm::EventSetup&) {
 
   std::auto_ptr<reco::TrackCollection> output_tracks(new reco::TrackCollection);
   std::auto_ptr<reco::TrackCollection> moved_tracks(new reco::TrackCollection);
+  std::auto_ptr<int> npreseljets(new int);
+  std::auto_ptr<int> npreselbjets(new int);
   std::auto_ptr<pat::JetCollection> jets_used(new pat::JetCollection);
   std::auto_ptr<pat::JetCollection> bjets_used(new pat::JetCollection);
   std::auto_ptr<std::vector<double> > flight_vect(new std::vector<double>(3, 0.));
@@ -109,6 +110,8 @@ bool MFVTrackMover::filter(edm::Event& event, const edm::EventSetup&) {
   std::vector<const pat::Jet*> presel_jets;
   std::vector<const pat::Jet*> presel_bjets;
   std::vector<const pat::Jet*> selected_jets;
+
+  TVector3 move;
 
   // Pick the (b-)jets we'll use.
 
@@ -130,42 +133,44 @@ bool MFVTrackMover::filter(edm::Event& event, const edm::EventSetup&) {
       presel_bjets.push_back(&jet);
   }
 
-  if (presel_jets.size() < njets || presel_bjets.size() < nbjets)
-    return false;
+  *npreseljets = presel_jets.size();
+  *npreselbjets = presel_bjets.size();
+  const bool pass_presel = presel_jets.size() >= njets && presel_bjets.size() >= nbjets;
 
-  for (int i : knuth_select(njets, presel_jets.size())) {
-    selected_jets.push_back(presel_jets[i]);
-    jets_used->push_back(*presel_jets[i]);
+  if (pass_presel) {
+    for (int i : knuth_select(njets, presel_jets.size())) {
+      selected_jets.push_back(presel_jets[i]);
+      jets_used->push_back(*presel_jets[i]);
+    }
+
+    for (int i : knuth_select(nbjets, presel_bjets.size())) {
+      selected_jets.push_back(presel_bjets[i]);
+      bjets_used->push_back(*presel_bjets[i]);
+    }
+
+    // Find the energy-weighted average direction of all the (b-)jets to
+    // be the flight axis.
+
+    TVector3 flight_axis;
+    for (const pat::Jet* jet : selected_jets)
+      flight_axis += TVector3(jet->px(), jet->py(), jet->pz());
+    flight_axis.SetMag(1.);
+    flight_vect->at(0) = flight_axis.x();
+    flight_vect->at(1) = flight_axis.y();
+    flight_vect->at(2) = flight_axis.z();
+
+    // Find the move vertex: pick a flight distance using Exp(dist|tau)
+    // and a direction around the flight axis using
+    // Gaus(theta|sig_theta) * Gaus(phi|sig_phi).
+
+    const double dist = rexp.fire(tau);
+    const double theta = rgau.fire(flight_axis.Theta(), sig_theta);
+    const double phi   = rgau.fire(flight_axis.Phi(),   sig_phi);
+    move.SetMagThetaPhi(dist, theta, phi);
+    move_vertex->at(0) = primary_vertex.x() + move.x();
+    move_vertex->at(1) = primary_vertex.y() + move.y();
+    move_vertex->at(2) = primary_vertex.z() + move.z();
   }
-
-  for (int i : knuth_select(nbjets, presel_bjets.size())) {
-    selected_jets.push_back(presel_bjets[i]);
-    bjets_used->push_back(*presel_bjets[i]);
-  }
-
-  // Find the energy-weighted average direction of all the (b-)jets to
-  // be the flight axis.
-
-  TVector3 flight_axis;
-  for (const pat::Jet* jet : selected_jets)
-    flight_axis += TVector3(jet->px(), jet->py(), jet->pz());
-  flight_axis.SetMag(1.);
-  flight_vect->at(0) = flight_axis.x();
-  flight_vect->at(1) = flight_axis.y();
-  flight_vect->at(2) = flight_axis.z();
-
-  // Find the move vertex: pick a flight distance using Exp(dist|tau)
-  // and a direction around the flight axis using
-  // Gaus(theta|sig_theta) * Gaus(phi|sig_phi).
-
-  const double dist = rexp.fire(tau);
-  const double theta = rgau.fire(flight_axis.Theta(), sig_theta);
-  const double phi   = rgau.fire(flight_axis.Phi(),   sig_phi);
-  TVector3 move;
-  move.SetMagThetaPhi(dist, theta, phi);
-  move_vertex->at(0) = primary_vertex.x() + move.x();
-  move_vertex->at(1) = primary_vertex.y() + move.y();
-  move_vertex->at(2) = primary_vertex.z() + move.z();
   
   // Copy all the input tracks, except for those corresponding to the
   // above jets; for the latter, clone the tracks but move their
@@ -174,6 +179,8 @@ bool MFVTrackMover::filter(edm::Event& event, const edm::EventSetup&) {
   for (size_t i = 0, ie = tracks->size(); i < ie; ++i) {
     reco::TrackRef tk(tracks, i);
     bool to_move = false;
+    if (!pass_presel)
+      goto done_check_to_move;
     for (const pat::Jet* jet : selected_jets)
       for (const reco::PFCandidatePtr& pfcand : jet->getPFConstituents())
         if (tk == pfcand->trackRef()) {
@@ -202,12 +209,12 @@ bool MFVTrackMover::filter(edm::Event& event, const edm::EventSetup&) {
 
   event.put(output_tracks);
   event.put(moved_tracks, "moved");
+  event.put(npreseljets, "npreseljets");
+  event.put(npreselbjets, "npreselbjets");
   event.put(jets_used, "jetsUsed");
   event.put(bjets_used, "bjetsUsed");
   event.put(flight_vect, "flightAxis");
   event.put(move_vertex, "moveVertex");
-
-  return true;
 }
 
 DEFINE_FWK_MODULE(MFVTrackMover);
