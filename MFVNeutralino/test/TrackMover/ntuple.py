c533d82f4288b495b20beebff0eaf57367df3311
Jordan Tucker <jordantucker@gmail.com>
2014-12-23 17:40:08 -0600

    Loop over n(b)jets.

--- MFVNeutralino/test/TrackMover/ntuple.py
+++ MFVNeutralino/test/TrackMover/ntuple.py
@@ -25,27 +25,58 @@ process.out.outputCommands = [
 process.load('JMTucker.MFVNeutralino.Vertexer_cff')
 process.load('JMTucker.MFVNeutralino.EventProducer_cfi')
 
-process.mfvMovedTracks = cms.EDProducer('MFVTrackMover',
-                                        tracks_src = cms.InputTag('generalTracks'),
-                                        primary_vertices_src = cms.InputTag('offlinePrimaryVertices'),
-                                        jets_src = cms.InputTag('selectedPatJetsPF'),
-                                        min_jet_pt = cms.double(50),
-                                        min_jet_ntracks = cms.uint32(4),
-                                        b_discriminator = cms.string('combinedSecondaryVertexBJetTags'),
-                                        b_discriminator_veto = cms.double(0.244),
-                                        b_discriminator_tag = cms.double(0.898),
-                                        njets = cms.uint32(2),
-                                        nbjets = cms.uint32(1),
-                                        tau = cms.double(1),
-                                        sig_theta = cms.double(0.2),
-                                        sig_phi = cms.double(0.2),
-                                        )
-
-process.mfvVertices.track_src = 'mfvMovedTracks'
-process.p = cms.Path(common_seq * process.mfvMovedTracks * process.mfvVertexSequence)
-
 del process.outp
 process.outp = cms.EndPath(process.mfvEvent * process.out)
 
+process.p = cms.Path(common_seq * process.mfvGenVertices)
+
 import JMTucker.MFVNeutralino.TriggerFilter
 JMTucker.MFVNeutralino.TriggerFilter.setup_trigger_filter(process)
+
+for njets in xrange(1,5):
+    for nbjets in xrange(0,3):
+        if njets == 1 and nbjets == 0:
+            continue
+        ex = '%i%i' % (njets, nbjets)
+
+        def app(name, obj):
+            setattr(process, name, obj)
+            process.p *= obj
+
+        mover = cms.EDProducer('MFVTrackMover',
+                               tracks_src = cms.InputTag('generalTracks'),
+                               primary_vertices_src = cms.InputTag('offlinePrimaryVertices'),
+                               jets_src = cms.InputTag('selectedPatJetsPF'),
+                               min_jet_pt = cms.double(50),
+                               min_jet_ntracks = cms.uint32(4),
+                               b_discriminator = cms.string('combinedSecondaryVertexBJetTags'),
+                               b_discriminator_veto = cms.double(0.244),
+                               b_discriminator_tag = cms.double(0.898),
+                               njets = cms.uint32(njets),
+                               nbjets = cms.uint32(nbjets),
+                               tau = cms.double(1),
+                               sig_theta = cms.double(0.2),
+                               sig_phi = cms.double(0.2),
+                               )
+
+        mover_name = 'mfvMovedTracks' + ex
+        app(mover_name, mover)
+
+        setattr(process.RandomNumberGeneratorService, mover_name, cms.PSet(initialSeed = cms.untracked.uint32(1220 + njets*10 + nbjets)))
+
+        vertices_name = 'mfvVertices' + ex
+        app(vertices_name, process.mfvVertices.clone(track_src = mover_name))
+
+        auxtmp_name = 'mfvVerticesAuxTmp' + ex
+        app(auxtmp_name, process.mfvVerticesAuxTmp.clone(vertex_src = vertices_name))
+
+        seltmp_name = 'mfvSelectedVerticesTmp' + ex
+        app(seltmp_name, process.mfvSelectedVerticesTmp.clone(vertex_src = vertices_name,
+                                                              vertex_aux_src = auxtmp_name))
+
+        jet2vtx_name = 'mfvVerticesToJets' + ex
+        app(jet2vtx_name, process.mfvVerticesToJets.clone(vertex_src = seltmp_name))
+
+        aux_name = 'mfvVerticesAux' + ex
+        app(aux_name, process.mfvVerticesAux.clone(vertex_src = vertices_name,
+                                                   sv_to_jets_src = jet2vtx_name))
--- MFVNeutralino/test/TrackMover/treer.py
+++ MFVNeutralino/test/TrackMover/treer.py
@@ -5,12 +5,19 @@ process.TFileService.fileName = 'movedtree.root'
 
 process.load('JMTucker.MFVNeutralino.WeightProducer_cfi')
 
-process.mfvMovedTree = cms.EDAnalyzer('MFVMovedTracksTreer',
-                                      event_src = cms.InputTag('mfvEvent'),
-                                      vertices_src = cms.InputTag('mfvVerticesAux'),
-                                      weight_src = cms.InputTag('mfvWeight'),
-                                      mover_src = cms.string('mfvMovedTracks'),
-                                      max_dist2move = cms.double(0.02),
-                                      )
+process.p = cms.Path(process.mfvWeight)
 
-process.p = cms.Path(process.mfvWeight * process.mfvMovedTree)
+for njets in xrange(1,5):
+    for nbjets in xrange(0,3):
+        if njets == 1 and nbjets == 0:
+            continue
+        ex = '%i%i' % (njets, nbjets)
+        obj = cms.EDAnalyzer('MFVMovedTracksTreer',
+                             event_src = cms.InputTag('mfvEvent'),
+                             weight_src = cms.InputTag('mfvWeight'),
+                             mover_src = cms.string('mfvMovedTracks' + ex),
+                             vertices_src = cms.InputTag('mfvVerticesAux' + ex),
+                             max_dist2move = cms.double(0.02),
+                             )
+        setattr(process, 'mfvMovedTree' + ex, obj)
+        process.p *= obj
