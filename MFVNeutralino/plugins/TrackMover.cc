47b5afeced8ef5fa3c3e08d4ddfd3a9691ca7741
Jordan Tucker <jordantucker@gmail.com>
2014-12-24 14:55:27 -0600

    Don't do anything when there is no primary vertex or the primary vertex has no tracks.

--- MFVNeutralino/plugins/TrackMover.cc
+++ MFVNeutralino/plugins/TrackMover.cc
@@ -82,22 +82,6 @@ MFVTrackMover::MFVTrackMover(const edm::ParameterSet& cfg)
 }
 
 void MFVTrackMover::produce(edm::Event& event, const edm::EventSetup&) {
-  edm::Handle<reco::TrackCollection> tracks;
-  event.getByLabel(tracks_src, tracks);
-
-  edm::Handle<reco::VertexCollection> primary_vertices;
-  event.getByLabel(primary_vertices_src, primary_vertices);
-  const reco::Vertex& primary_vertex = primary_vertices->at(0);
-  const bool primary_vertex_has_tracks = primary_vertex.tracks_end() - primary_vertex.tracks_begin();
-  if (!primary_vertex_has_tracks)
-    throw cms::Exception("MFVTrackMover", "no trackrefs in primary vertex");
-
-  edm::Handle<pat::JetCollection> jets;
-  event.getByLabel(jets_src, jets);
-
-  CLHEP::RandExponential rexp(rng->getEngine());
-  CLHEP::RandGauss rgau(rng->getEngine());
-
   std::auto_ptr<reco::TrackCollection> output_tracks(new reco::TrackCollection);
   std::auto_ptr<reco::TrackCollection> moved_tracks(new reco::TrackCollection);
   std::auto_ptr<int> npreseljets(new int);
@@ -107,104 +91,116 @@ void MFVTrackMover::produce(edm::Event& event, const edm::EventSetup&) {
   std::auto_ptr<std::vector<double> > flight_vect(new std::vector<double>(3, 0.));
   std::auto_ptr<std::vector<double> > move_vertex(new std::vector<double>(3, 0.));
 
-  std::vector<const pat::Jet*> presel_jets;
-  std::vector<const pat::Jet*> presel_bjets;
-  std::vector<const pat::Jet*> selected_jets;
+  edm::Handle<reco::VertexCollection> primary_vertices;
+  event.getByLabel(primary_vertices_src, primary_vertices);
 
-  TVector3 move;
+  if (!primary_vertices->empty() && primary_vertices->at(0).tracks_end() - primary_vertices->at(0).tracks_begin() > 0) {
+    edm::Handle<pat::JetCollection> jets;
+    event.getByLabel(jets_src, jets);
 
-  // Pick the (b-)jets we'll use.
+    CLHEP::RandExponential rexp(rng->getEngine());
+    CLHEP::RandGauss rgau(rng->getEngine());
 
-  for (const pat::Jet& jet : *jets) {
-    if (jet.pt() < min_jet_pt)
-      continue;
+    std::vector<const pat::Jet*> presel_jets;
+    std::vector<const pat::Jet*> presel_bjets;
+    std::vector<const pat::Jet*> selected_jets;
 
-    unsigned jet_ntracks = 0;
-    for (const reco::PFCandidatePtr& pfcand : jet.getPFConstituents())
-      if (pfcand->trackRef().isNonnull())
-        ++jet_ntracks;
-    if (jet_ntracks < min_jet_ntracks)
-      continue;
+    TVector3 move;
 
-    const double b_disc = jet.bDiscriminator(b_discriminator);
-    if (b_disc < b_discriminator_veto)
-      presel_jets.push_back(&jet);
-    else if (b_disc > b_discriminator_tag)
-      presel_bjets.push_back(&jet);
-  }
+    // Pick the (b-)jets we'll use.
 
-  *npreseljets = presel_jets.size();
-  *npreselbjets = presel_bjets.size();
-  const bool pass_presel = presel_jets.size() >= njets && presel_bjets.size() >= nbjets;
+    for (const pat::Jet& jet : *jets) {
+      if (jet.pt() < min_jet_pt)
+        continue;
 
-  if (pass_presel) {
-    for (int i : knuth_select(njets, presel_jets.size())) {
-      selected_jets.push_back(presel_jets[i]);
-      jets_used->push_back(*presel_jets[i]);
+      unsigned jet_ntracks = 0;
+      for (const reco::PFCandidatePtr& pfcand : jet.getPFConstituents())
+        if (pfcand->trackRef().isNonnull())
+          ++jet_ntracks;
+      if (jet_ntracks < min_jet_ntracks)
+        continue;
+
+      const double b_disc = jet.bDiscriminator(b_discriminator);
+      if (b_disc < b_discriminator_veto)
+        presel_jets.push_back(&jet);
+      else if (b_disc > b_discriminator_tag)
+        presel_bjets.push_back(&jet);
     }
 
-    for (int i : knuth_select(nbjets, presel_bjets.size())) {
-      selected_jets.push_back(presel_bjets[i]);
-      bjets_used->push_back(*presel_bjets[i]);
+    *npreseljets = presel_jets.size();
+    *npreselbjets = presel_bjets.size();
+    const bool pass_presel = presel_jets.size() >= njets && presel_bjets.size() >= nbjets;
+
+    if (pass_presel) {
+      for (int i : knuth_select(njets, presel_jets.size())) {
+        selected_jets.push_back(presel_jets[i]);
+        jets_used->push_back(*presel_jets[i]);
+      }
+
+      for (int i : knuth_select(nbjets, presel_bjets.size())) {
+        selected_jets.push_back(presel_bjets[i]);
+        bjets_used->push_back(*presel_bjets[i]);
+      }
+
+      // Find the energy-weighted average direction of all the (b-)jets to
+      // be the flight axis.
+
+      TVector3 flight_axis;
+      for (const pat::Jet* jet : selected_jets)
+        flight_axis += TVector3(jet->px(), jet->py(), jet->pz());
+      flight_axis.SetMag(1.);
+      flight_vect->at(0) = flight_axis.x();
+      flight_vect->at(1) = flight_axis.y();
+      flight_vect->at(2) = flight_axis.z();
+
+      // Find the move vertex: pick a flight distance using Exp(dist|tau)
+      // and a direction around the flight axis using
+      // Gaus(theta|sig_theta) * Gaus(phi|sig_phi).
+
+      const double dist = rexp.fire(tau);
+      const double theta = rgau.fire(flight_axis.Theta(), sig_theta);
+      const double phi   = rgau.fire(flight_axis.Phi(),   sig_phi);
+      move.SetMagThetaPhi(dist, theta, phi);
+      move_vertex->at(0) = primary_vertices->at(0).x() + move.x();
+      move_vertex->at(1) = primary_vertices->at(0).y() + move.y();
+      move_vertex->at(2) = primary_vertices->at(0).z() + move.z();
     }
-
-    // Find the energy-weighted average direction of all the (b-)jets to
-    // be the flight axis.
-
-    TVector3 flight_axis;
-    for (const pat::Jet* jet : selected_jets)
-      flight_axis += TVector3(jet->px(), jet->py(), jet->pz());
-    flight_axis.SetMag(1.);
-    flight_vect->at(0) = flight_axis.x();
-    flight_vect->at(1) = flight_axis.y();
-    flight_vect->at(2) = flight_axis.z();
-
-    // Find the move vertex: pick a flight distance using Exp(dist|tau)
-    // and a direction around the flight axis using
-    // Gaus(theta|sig_theta) * Gaus(phi|sig_phi).
-
-    const double dist = rexp.fire(tau);
-    const double theta = rgau.fire(flight_axis.Theta(), sig_theta);
-    const double phi   = rgau.fire(flight_axis.Phi(),   sig_phi);
-    move.SetMagThetaPhi(dist, theta, phi);
-    move_vertex->at(0) = primary_vertex.x() + move.x();
-    move_vertex->at(1) = primary_vertex.y() + move.y();
-    move_vertex->at(2) = primary_vertex.z() + move.z();
-  }
   
-  // Copy all the input tracks, except for those corresponding to the
-  // above jets; for the latter, clone the tracks but move their
-  // reference points to the move vertex.
+    // Copy all the input tracks, except for those corresponding to the
+    // above jets; for the latter, clone the tracks but move their
+    // reference points to the move vertex.
 
-  for (size_t i = 0, ie = tracks->size(); i < ie; ++i) {
-    reco::TrackRef tk(tracks, i);
-    bool to_move = false;
-    if (!pass_presel)
-      goto done_check_to_move;
-    for (const pat::Jet* jet : selected_jets)
-      for (const reco::PFCandidatePtr& pfcand : jet->getPFConstituents())
-        if (tk == pfcand->trackRef()) {
-          to_move = true;
-          goto done_check_to_move;
-        }
+    edm::Handle<reco::TrackCollection> tracks;
+    event.getByLabel(tracks_src, tracks);
+
+    for (size_t i = 0, ie = tracks->size(); i < ie; ++i) {
+      reco::TrackRef tk(tracks, i);
+      bool to_move = false;
+      for (const pat::Jet* jet : selected_jets)
+        for (const reco::PFCandidatePtr& pfcand : jet->getPFConstituents())
+          if (tk == pfcand->trackRef()) {
+            to_move = true;
+            goto done_check_to_move;
+          }
     done_check_to_move:
     
-    if (to_move) {
-      reco::TrackBase::Point new_point(tk->vx() + move.x(),
-                                       tk->vy() + move.y(),
-                                       tk->vz() + move.z());
+      if (to_move) {
+        reco::TrackBase::Point new_point(tk->vx() + move.x(),
+                                         tk->vy() + move.y(),
+                                         tk->vz() + move.z());
 
-      output_tracks->push_back(reco::Track(tk->chi2(), tk->ndof(), new_point, tk->momentum(), tk->charge(), tk->covariance(), tk->algo()));
-      reco::Track& new_tk = output_tracks->back();
-      new_tk.setQualityMask(tk->qualityMask());
-      new_tk.setHitPattern(tk->hitPattern());
-      new_tk.setNLoops(tk->nLoops());
-      new_tk.setTrackerExpectedHitsInner(tk->trackerExpectedHitsInner());
-      new_tk.setTrackerExpectedHitsOuter(tk->trackerExpectedHitsOuter());
-      moved_tracks->push_back(new_tk);
+        output_tracks->push_back(reco::Track(tk->chi2(), tk->ndof(), new_point, tk->momentum(), tk->charge(), tk->covariance(), tk->algo()));
+        reco::Track& new_tk = output_tracks->back();
+        new_tk.setQualityMask(tk->qualityMask());
+        new_tk.setHitPattern(tk->hitPattern());
+        new_tk.setNLoops(tk->nLoops());
+        new_tk.setTrackerExpectedHitsInner(tk->trackerExpectedHitsInner());
+        new_tk.setTrackerExpectedHitsOuter(tk->trackerExpectedHitsOuter());
+        moved_tracks->push_back(new_tk);
+      }
+      else
+        output_tracks->push_back(*tk);
     }
-    else
-      output_tracks->push_back(*tk);
   }
 
   event.put(output_tracks);
