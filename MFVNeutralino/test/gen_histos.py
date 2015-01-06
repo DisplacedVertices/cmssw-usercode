528ae2b90c3878d90ed9219907e1ad5a93c4e2cc
Jordan Tucker <jordantucker@gmail.com>
2015-01-06 13:38:47 -0600

    separate_by_nrecovtx flag.

--- MFVNeutralino/test/gen_histos.py
+++ MFVNeutralino/test/gen_histos.py
@@ -8,17 +8,21 @@ process.TFileService.fileName = 'gen_histos.root'
 process.load('JMTucker.MFVNeutralino.GenParticleFilter_cfi')
 process.load('JMTucker.MFVNeutralino.GenHistos_cff')
 
+#process.mfvGenParticleFilter.required_num_leptonic = 0
+
 process.p = cms.Path(process.mfvGenParticleFilter * process.mfvGenHistos)
 
-process.histos1v = process.mfvGenHistos.clone()
-process.histos2v = process.mfvGenHistos.clone()
-process.histos0v = process.mfvGenHistos.clone()
+separate_by_nrecovtx = False
+if separate_by_nrecovtx:
+    process.histos1v = process.mfvGenHistos.clone()
+    process.histos2v = process.mfvGenHistos.clone()
+    process.histos0v = process.mfvGenHistos.clone()
 
-process.events1v = cms.EDFilter('EventIdVeto', list_fn = cms.string('events1v.txt.gz'), use_run = cms.bool(True))
-process.events2v = cms.EDFilter('EventIdVeto', list_fn = cms.string('events2v.txt.gz'), use_run = cms.bool(True))
-process.p1v = cms.Path(~process.events1v * process.histos1v)
-process.p2v = cms.Path(~process.events2v * process.histos2v)
-process.p0v = cms.Path(process.events1v * process.events2v * process.histos0v)
+    process.events1v = cms.EDFilter('EventIdVeto', list_fn = cms.string('events1v.txt.gz'), use_run = cms.bool(True))
+    process.events2v = cms.EDFilter('EventIdVeto', list_fn = cms.string('events2v.txt.gz'), use_run = cms.bool(True))
+    process.p1v = cms.Path(~process.events1v * process.histos1v)
+    process.p2v = cms.Path(~process.events2v * process.histos2v)
+    process.p0v = cms.Path(process.events1v * process.events2v * process.histos0v)
 
 if debug:
     process.printList = cms.EDAnalyzer('JMTParticleListDrawer',
@@ -36,12 +40,15 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         to_add = []
         to_modify = []
 
-        to_add.append("process.events1v.list_fn = '%s_1v.txt.gz'" % sample.name)
-        to_add.append("process.events2v.list_fn = '%s_2v.txt.gz'" % sample.name)
+        if separate_by_nrecovtx:
+            to_add.append("process.events1v.list_fn = '%s_1v.txt.gz'" % sample.name)
+            to_add.append("process.events2v.list_fn = '%s_2v.txt.gz'" % sample.name)
         return to_add, to_modify
 
     def crab_cfg_modifier(sample):
-        return [('USER', 'additional_input_files', '%s_1v.txt.gz,%s_2v.txt.gz' % (sample.name, sample.name))]
+        if separate_by_nrecovtx:
+            return [('USER', 'additional_input_files', '%s_1v.txt.gz,%s_2v.txt.gz' % (sample.name, sample.name))]
+        return []
 
     from JMTucker.Tools.CRABSubmitter import CRABSubmitter
     cs = CRABSubmitter('GenHistos',
