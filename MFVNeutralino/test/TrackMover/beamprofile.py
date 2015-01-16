9e677e6eb4f506e0f5101df68f4fb4808eb2dd32
Jordan Tucker <jordantucker@gmail.com>
2015-01-16 11:02:43 -0600

    Needs events_per setting.

--- MFVNeutralino/test/TrackMover/beamprofile.py
+++ MFVNeutralino/test/TrackMover/beamprofile.py
@@ -69,9 +69,18 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
                        job_control_from_sample = True,
                        get_edm_output = False,
                        data_retrieval = 'cornell',
-                       publish_data_name = 'beamprofile' + ex,
+                       publish_data_name = 'beamprofile',
                        max_threads = 3,
                        )
+
+    timing = { 'dyjetstollM10': 0.011203, 'dyjetstollM50': 0.019867, 'qcdbce020': 0.008660, 'qcdbce030': 0.007796, 'qcdbce080': 0.061260, 'qcdbce170': 0.328891, 'qcdbce250': 0.481813, 'qcdbce350': 0.519482, 'qcdem020': 0.010137, 'qcdem030': 0.01, 'qcdem080': 0.037925, 'qcdem170': 0.286123, 'qcdem250': 0.471398, 'qcdem350': 0.686303, 'qcdht0100': 0.008273, 'qcdht0250': 0.116181, 'qcdht0500': 0.738374, 'qcdht1000': 1.002745, 'qcdmu0020': 0.012301, 'qcdmu0030': 0.015762, 'qcdmu0050': 0.018178, 'qcdmu0080': 0.119300, 'qcdmu0120': 0.245562, 'qcdmu0170': 0.32, 'qcdmu0300': 0.419818, 'qcdmu0470': 0.584266, 'qcdmu0600': 0.455305, 'qcdmu0800': 0.879171, 'qcdmu1000': 1.075712, 'singletop_s': 0.093429, 'singletop_s_tbar': 0.146642, 'singletop_tW': 0.327386, 'singletop_tW_tbar': 0.184349, 'singletop_t': 0.092783, 'singletop_t_tbar': 0.060983, 'ttbarhadronic': 0.752852, 'ttbarsemilep': 0.419073, 'ttbardilep': 0.295437, 'ttgjets': 0.861987, 'ttwjets': 0.714156, 'ttzjets': 0.827464, 'wjetstolnu': 0.010842, 'ww': 0.046754, 'wz': 0.049839, 'zz': 0.059791, }
+
+    for sample in Samples.all_mc_samples:
+        if timing.has_key(sample.name):
+            sample.events_per = int(3.5*3600/timing[sample.name] * 2)
+            sample.timed = True
+            nj = int(sample.nevents_orig / float(sample.events_per)) + 1
+            assert nj < 5000
 
     samples = Samples.from_argv(Samples.data_samples + Samples.ttbar_samples + Samples.qcd_samples)
 
