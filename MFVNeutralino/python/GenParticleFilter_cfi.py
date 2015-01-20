8e6263dda42f5dd19386334565678b1bde53b939
Jennifer Chu <jyc72@cornell.edu>
2015-01-20 07:19:17 -0600

    Implement analysis cuts at generator level.

--- MFVNeutralino/python/GenParticleFilter_cfi.py
+++ MFVNeutralino/python/GenParticleFilter_cfi.py
@@ -1,6 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 mfvGenParticleFilter = cms.EDFilter('MFVGenParticleFilter',
+                                    gen_jet_src = cms.InputTag('ak5GenJets'),
+                                    min_njets = cms.int32(0),
+                                    min_jet_pt = cms.double(0),
+                                    min_sumht = cms.double(0),
                                     gen_src = cms.InputTag('genParticles'),
                                     print_info = cms.bool(False),
                                     cut_invalid = cms.bool(False),
@@ -24,4 +28,7 @@ mfvGenParticleFilter = cms.EDFilter('MFVGenParticleFilter',
                                     max_rbigger = cms.double(-1),
                                     min_rsmaller = cms.double(-1),
                                     max_rsmaller = cms.double(-1),
+                                    max_drmin = cms.double(1e9),
+                                    min_drmax = cms.double(0),
+                                    max_drmax = cms.double(1e9),
                                     )
