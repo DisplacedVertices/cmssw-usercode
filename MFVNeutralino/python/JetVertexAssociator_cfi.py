e7a87b27bc0f5bb9f2381d3973506fc9162e48ef
Jordan Tucker <jordantucker@gmail.com>
2015-02-17 15:58:52 -0600

    jet_src tag.

--- MFVNeutralino/python/JetVertexAssociator_cfi.py
+++ MFVNeutralino/python/JetVertexAssociator_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 mfvVerticesToJets = cms.EDProducer('MFVJetVertexAssociator',
-                                   jet_src = cms.InputTag('selectedPatJetsPF'),
+                                   jet_src = cms.InputTag('selectedPatJets'),
                                    vertex_src = cms.InputTag('mfvSelectedVerticesTmp'),
                                    input_is_refs = cms.bool(True),
                                    tag_info_name = cms.string('secondaryVertexMaxDR6p0'),
