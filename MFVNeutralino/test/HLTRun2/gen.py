6b06ee1fb75ae3446ab092ea89ecd620f829ed42
Jordan Tucker <jordantucker@gmail.com>
2015-04-02 18:30:59 -0500

    Submit script

--- MFVNeutralino/test/HLTRun2/gen.py
+++ MFVNeutralino/test/HLTRun2/gen.py
@@ -1,6 +1,6 @@
 # 740pre6: cmsDriver.py TTbar_13TeV_TuneCUETP8M1_cfi --no_exec -n 10 --conditions MCRUN2_74_V7 --eventcontent RAWSIM -s GEN --beamspot NominalCollision2015 --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --magField 38T_PostLS1
 
-import FWCore.ParameterSet.Config as cms
+import sys, os, FWCore.ParameterSet.Config as cms
 
 process = cms.Process('GEN')
 
@@ -87,3 +87,73 @@ for path in process.paths:
 
 from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
 process = customisePostLS1(process)
+
+if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
+    crab_cfg = '''
+[CRAB]
+jobtype = cmssw
+scheduler = remoteGlidein
+
+[CMSSW]
+datasetpath = None
+pset = gen.py
+get_edm_output = 1
+events_per_job = 4000
+total_number_of_events = 100000
+first_lumi = 1
+use_dbs3 = 1
+
+[USER]
+additional_input_files = minSLHA.spc
+ui_working_dir = %(ui_working_dir)s
+copy_data = 1
+storage_element = T3_US_Cornell
+publish_data = 1
+publish_data_name = mfv_hltrun2_%(name)s
+dbs_url_for_publication = phys03
+
+[GRID]
+#se_black_list =
+'''
+
+    slha = '''
+BLOCK SPINFO  # Spectrum calculator information
+     1   Minimal    # spectrum calculator
+     2   1.0.0         # version number
+#
+BLOCK MODSEL  # Model selection
+     1     1   #
+#
+
+BLOCK MASS  # Mass Spectrum
+# PDG code           mass       particle
+  1000021     %(m_gluino)E       # ~g
+  1000022     %(m_neutralino)E   # ~chi_10
+
+DECAY   1000021     0.01E+00   # gluino decays
+#          BR         NDA      ID1       ID2
+    1.0E00            2      1000022    21   # BR(~g -> ~chi_10  g)
+
+DECAY   1000022     0.01E+00   # neutralino decays
+#           BR         NDA      ID1       ID2       ID3
+     0.5E+00          3            3          5           6   # BR(~chi_10 -> s b t)
+     0.5E+00          3           -3         -5          -6   # BR(~chi_10 -> sbar bbar tbar)
+'''
+
+    testing = 'testing' in sys.argv
+    dir_root = 'crab/HLTRun2/Gen'
+    os.system('mkdir -p %s' % dir_root)
+
+    masses = range(200, 1001, 100)
+
+    for m_neutralino in masses:
+        m_gluino = m_neutralino + 5
+        name = 'M%04i' % m_neutralino
+
+        ui_working_dir = os.path.join(dir_root, 'crab_' + name)
+
+        open('minSLHA.spc', 'wt').write(slha % locals())
+        open('crab.cfg', 'wt').write(crab_cfg % locals())
+        if not testing:
+            os.system('crab -create -submit all')
+            os.system('rm minSLHA.spc crab.cfg')
