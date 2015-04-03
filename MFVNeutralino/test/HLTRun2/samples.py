830258bb61bbcd494c5978908b7f8df737caabcf
Jordan Tucker <jordantucker@gmail.com>
2015-04-03 11:55:52 -0500

    last hltrun2 3j samples

--- MFVNeutralino/test/HLTRun2/samples.py
+++ MFVNeutralino/test/HLTRun2/samples.py
@@ -1,11 +1,15 @@
 from JMTucker.Tools.Samples import MCSample
 
 samples = [
+    MCSample('mfv_hltrun2_3j_M0200', '', '/mfv_hltrun2_3j_M0200/tucker-mfv_hltrun2_3j_M0200-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
     MCSample('mfv_hltrun2_3j_M0300', '', '/mfv_hltrun2_3j_M0300/tucker-mfv_hltrun2_3j_M0300-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
     MCSample('mfv_hltrun2_3j_M0400', '', '/mfv_hltrun2_3j_M0400/tucker-mfv_hltrun2_3j_M0400-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
     MCSample('mfv_hltrun2_3j_M0500', '', '/mfv_hltrun2_3j_M0500/tucker-mfv_hltrun2_3j_M0500-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
     MCSample('mfv_hltrun2_3j_M0600', '', '/mfv_hltrun2_3j_M0600/tucker-mfv_hltrun2_3j_M0600-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
+    MCSample('mfv_hltrun2_3j_M0700', '', '/mfv_hltrun2_3j_M0700/tucker-mfv_hltrun2_3j_M0700-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
     MCSample('mfv_hltrun2_3j_M0800', '', '/mfv_hltrun2_3j_M0800/tucker-mfv_hltrun2_3j_M0800-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
+    MCSample('mfv_hltrun2_3j_M0900', '', '/mfv_hltrun2_3j_M0900/tucker-mfv_hltrun2_3j_M0900-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
+    MCSample('mfv_hltrun2_3j_M1000', '', '/mfv_hltrun2_3j_M1000/tucker-mfv_hltrun2_3j_M1000-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
     MCSample('mfv_hltrun2_M0200', '', '/mfv_hltrun2_M0200/tucker-mfv_hltrun2_M0200-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
     MCSample('mfv_hltrun2_M0300', '', '/mfv_hltrun2_M0300/tucker-mfv_hltrun2_M0300-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
     MCSample('mfv_hltrun2_M0400', '', '/mfv_hltrun2_M0400/tucker-mfv_hltrun2_M0400-b0221db6bdc577a56c275739395c3940/USER', 100000, 1, 1, 1),
