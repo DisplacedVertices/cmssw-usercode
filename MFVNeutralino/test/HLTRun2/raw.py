d07dad9f6d3b57769f9924132347787a454500fa
Jordan Tucker <jordantucker@gmail.com>
2015-04-06 16:47:18 -0500

    hltrun2 raw: forgot minbias filenames; submit script.

--- MFVNeutralino/test/HLTRun2/raw.py
+++ MFVNeutralino/test/HLTRun2/raw.py
@@ -1,6 +1,6 @@
 # in 740: cmsDriver.py raw2 --no_exec -n 10 --mc --conditions MCRUN2_74_V7 --eventcontent RAWSIM -s DIGI,L1,DIGI2RAW --beamspot NominalCollision2015 --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --magField 38T_PostLS1 --pileup AVE_40_BX_25ns
 
-import FWCore.ParameterSet.Config as cms
+import sys, FWCore.ParameterSet.Config as cms
 
 process = cms.Process('DIGI2RAW')
 
@@ -19,7 +19,7 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condD
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5))
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
 process.source = cms.Source('PoolSource',
-    fileNames = cms.untracked.vstring('root://osg-se.cac.cornell.edu//xrootd/path/cms/store/user/tucker/mfv_hltrun2_M0900/sim/6f2bea2f4650fd314ffd47e16b4b2771/sim_1_1_InE.root'),
+    fileNames = cms.untracked.vstring('file:/uscms_data/d1/tucker/sim_100_1_qdc.root'),
     inputCommands = cms.untracked.vstring(
         'keep *', 
         'drop *_genParticles_*_*', 
@@ -54,6 +54,8 @@ process.mix.input.nbPileupEvents.averageNumber = cms.double(40)
 process.mix.bunchspace = cms.int32(25)
 process.mix.minBunch = cms.int32(-12)
 process.mix.maxBunch = cms.int32(3)
+from minbias import files
+process.mix.input.fileNames = files
 
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_74_V7', '')
@@ -67,3 +69,21 @@ process.schedule = cms.Schedule(process.digitisation_step, process.L1simulation_
 
 from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
 process = customisePostLS1(process)
+
+if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
+    from samples import samples
+    samples = [s for s in samples if s.sim_done = True]
+    for s in samples:
+        s.dataset = s.sim_dataset
+
+    from JMTucker.Tools.CRABSubmitter import CRABSubmitter
+    cs = CRABSubmitter('HLTRun2_Raw',
+                       events_per_job = 400,
+                       total_number_of_events = -1,
+                       get_edm_output = 1,
+                       data_retrieval = 'cornell',
+                       publish_data_name = 'rawpu40',
+                       aaa = True,
+                       storage_catalog_override = 'cornell',
+                       )
+    cs.submit_all(samples)
