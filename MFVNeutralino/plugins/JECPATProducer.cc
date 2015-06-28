9c41399be7a8edc2b15abd66b0d040a49ed9f4a5
Jordan Tucker <jordantucker@gmail.com>
2015-06-28 13:22:40 -0500

    Rework for no real reason

--- MFVNeutralino/plugins/JECPATProducer.cc
+++ MFVNeutralino/plugins/JECPATProducer.cc
@@ -1,99 +1,92 @@
 #include "CondFormats/JetMETObjects/interface/JetCorrectionUncertainty.h"
-#include "CondFormats/JetMETObjects/interface/SimpleJetCorrectionUncertainty.h"
 #include "DataFormats/PatCandidates/interface/Jet.h"
 #include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-class JECPATProducer : public edm::EDProducer {
+class MFVJECPATProducer : public edm::EDProducer {
 public:
-  explicit JECPATProducer(const edm::ParameterSet&);
+  explicit MFVJECPATProducer(const edm::ParameterSet&);
 private:
   void produce(edm::Event&, const edm::EventSetup&);
+
   const edm::InputTag jet_src;
-  const bool use_correction;
-  const bool jes_uncertainty;
-  const bool jes_way;
-  const bool jer_way;
+  const bool enable;
+  const bool jes;
+  const bool up;
 };
 
-JECPATProducer::JECPATProducer(const edm::ParameterSet& cfg)
+MFVJECPATProducer::MFVJECPATProducer(const edm::ParameterSet& cfg)
   : jet_src(cfg.getParameter<edm::InputTag>("jet_src")),
-    use_correction(cfg.getParameter<bool>("use_correction")),
-    jes_uncertainty(cfg.getParameter<bool>("jes_uncertainty")),
-    jes_way(cfg.getParameter<bool>("jes_way")),
-    jer_way(cfg.getParameter<bool>("jer_way"))
+    enable(cfg.getParameter<bool>("enable")),
+    jes(cfg.getParameter<bool>("jes")),
+    up(cfg.getParameter<bool>("up"))
 {
   produces<pat::JetCollection>();
 }
 
-void JECPATProducer::produce(edm::Event& event, const edm::EventSetup& setup) {
+void MFVJECPATProducer::produce(edm::Event& event, const edm::EventSetup&) {
+  throw cms::Exception("NotImplemented");
+
   edm::Handle<pat::JetCollection> jets;
   event.getByLabel(jet_src, jets);
   
-  std::auto_ptr<pat::JetCollection> mjets(new pat::JetCollection);
-  JetCorrectionUncertainty *jecUnc_ = new JetCorrectionUncertainty("Summer13_V4_MC_Uncertainty_AK5PF.txt");
-  //std::cout<<"Making Jets"<<std::endl;
+  std::auto_ptr<pat::JetCollection> new_jets(new pat::JetCollection);
 
-  for(std::vector<pat::Jet>::const_iterator jet = jets->begin(); jet != jets->end(); ++jet) {
+  JetCorrectionUncertainty jec_unc("Summer13_V4_MC_Uncertainty_AK5PF.txt");
 
-    float uncertainty = -1000;
-    float scale = 999;
-    pat::Jet mjet = *jet;
+  for (std::vector<pat::Jet>::const_iterator jet = jets->begin(), jete = jets->end(); jet != jete; ++jet) {
+    if (!jet->genJet())
+      throw cms::Exception("BadJet") << "jet with pt " << jet->pt() << " eta " << jet->eta() << " has no gen jet";
 
-    if(jes_uncertainty){
-      jecUnc_->setJetEta( jet->eta() );
-      jecUnc_->setJetPt( jet->pt() ); // here you must use the CORRECTED jet pt
-      uncertainty = jecUnc_->getUncertainty(jes_way);
-      if(jes_way)
-	scale = (1+uncertainty);
-      else
-	scale = (1-uncertainty);
+    float scale = 1e9;
+    pat::Jet new_jet(*jet);
 
+    if (jes) {
+      jec_unc.setJetEta(jet->eta());
+      jec_unc.setJetPt(jet->pt());
+      const float uncertainty = jec_unc.getUncertainty(up);
+      if (up) scale = 1 + uncertainty;
+      else    scale = 1 - uncertainty;
     }
-    else{
-      float JER_scale_up,JER_scale_down;
-      JER_scale_up=JER_scale_down=-999;
-      if( fabs(jet->eta()) < 0.5 ){
-	JER_scale_up   = 1.115;
-	JER_scale_down = 0.990;
-      }
-      if( fabs(jet->eta()) < 1.1 && fabs(jet->eta()) > 0.5 ){
-	JER_scale_up   = 1.114;
-	JER_scale_down = 1.001;
-	}
-      if( fabs(jet->eta()) < 1.7 && fabs(jet->eta()) > 1.1 ){
-	JER_scale_up   = 1.161;
-	JER_scale_down = 1.032;
-      }
-      if( fabs(jet->eta()) < 2.3 && fabs(jet->eta()) > 1.7 ){
-	JER_scale_up   = 1.228;
-	JER_scale_down = 1.042;
-      }
-      if( fabs(jet->eta()) < 5.0 && fabs(jet->eta()) > 2.3 ){
-	JER_scale_up   = 1.488;
-	JER_scale_down = 1.089;
-      }
+    else {
+      float factor;
 
-      float gen_jet_energy = -999;
-      if(jet->genJet())
-	gen_jet_energy = jet->genJet()->energy();
-      else {
-	continue;
+      const float aeta = fabs(jet->eta());
+      if (aeta < 0.5) {
+        if (up) factor = 1.115;
+        else    factor = 0.990;
       }
-      if(jer_way)
-	scale = (gen_jet_energy + JER_scale_up*(jet->energy() - gen_jet_energy))/jet->energy();
+      else if (aeta < 1.1 && aeta > 0.5) {
+        if (up) factor = 1.114;
+        else    factor = 1.001;
+      }
+      else if (aeta < 1.7 && aeta > 1.1) {
+	if (up) factor = 1.161;
+	else    factor = 1.032;
+      }
+      else if (aeta < 2.3 && aeta > 1.7) {
+	if (up) factor = 1.228;
+	else    factor = 1.042;
+      }
+      else if (aeta < 5.0 && aeta > 2.3) {
+	if (up) factor = 1.488;
+	else    factor = 1.089;
+      }
       else
-	scale = (gen_jet_energy + JER_scale_down*(jet->energy() - gen_jet_energy))/jet->energy();
+        throw cms::Exception("BadJet") << "jet with pt " << jet->pt() << " eta " << jet->eta();
+
+      const float gen_jet_energy = jet->genJet()->energy();
+      scale = (gen_jet_energy + factor*(jet->energy() - jet->genJet()->energy()))/jet->energy();
     }
     
-    if(use_correction)
-      mjet.scaleEnergy(scale);
-    mjets->push_back(mjet);
+    if (enable)
+      new_jet.scaleEnergy(scale);
 
+    new_jets->push_back(new_jet);
   }
-  event.put(mjets);
 
+  event.put(new_jets);
 }
 
-DEFINE_FWK_MODULE(JECPATProducer);
+DEFINE_FWK_MODULE(MFVJECPATProducer);
--- MFVNeutralino/python/JECPATProducer_cfi.py
+++ MFVNeutralino/python/JECPATProducer_cfi.py
@@ -1,9 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
-
-JECPATProducer = cms.EDProducer('JECPATProducer',
-                                jet_src =cms.InputTag('selectedPatJetsPF'),
-                                use_correction = cms.bool(True), # Use correction at all
-                                jes_uncertainty = cms.bool(True), # True for JES and False for JER
-                                jes_way = cms.bool(True), # True = up, False = down
-                                jer_way = cms.bool(True) ) # True = up, False = down
+mfvJECPATProducer = cms.EDProducer('mfvJECPATProducer',
+                                   jet_src = cms.InputTag('selectedPatJetsPF'),
+                                   enable = cms.bool(True),
+                                   jes = cms.bool(True), # True for JES and False for JER
+                                   up = cms.bool(True),
+                                   )
