a5ec9eb81b2696e3db81443743d212b41aa4f727
Jordan Tucker <jordantucker@gmail.com>
2015-06-28 13:32:33 -0500

    Dump histogram content.

---
+++ Tools/scripts/dumphisto.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+from JMTucker.Tools.ROOTTools import ROOT
+
+fn = sys.argv[1]
+try:
+    path = sys.argv[2]
+except IndexError:
+    path = None
+    print 'just print file'
+ls = 'ls' in sys.argv
+
+f = ROOT.TFile(fn)
+if path is None:
+    f.ls()
+else:
+    x = f.Get(path)
+    if ls:
+        x.ls()
+    else:
+        x.Print()
+        nbins = x.GetNbinsX()
+        print 'nbins:', nbins
+        for i in xrange(0, nbins+2):
+            print '%4i %6.1f %6.1f' % (i, x.GetBinLowEdge(i), x.GetBinContent(i))
