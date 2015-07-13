e4f2994681176300acea47d818de86fc34fcf4bb
Jordan Tucker <jordantucker@gmail.com>
2015-07-13 16:55:27 -0500

    Move maxEvents setting so genonly can override

--- MFVNeutralino/test/MakeSamples/debug.py
+++ MFVNeutralino/test/MakeSamples/debug.py
@@ -6,6 +6,8 @@ if 'replay' in sys.argv:
     from JMTucker.Tools.CMSSWTools import replay_event
     replay_event(process, filename=None, rle=None)
 
+process.maxEvents.input = 1
+
 if 'genonly' in sys.argv:
     process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.output_step)
     process.maxEvents.input = 100
@@ -13,7 +15,6 @@ if 'genonly' in sys.argv:
 #set_neutralino_tau0(process, 10)
 #set_empirical_decay(405, 400, [[5,-5]])
 
-process.maxEvents.input = 1
 process.options.wantSummary = True
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 process.generator.maxEventsToPrint = 2
