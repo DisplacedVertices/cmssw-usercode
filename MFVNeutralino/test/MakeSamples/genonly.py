c330753d6e520531eb0f7b596c7b162265537880
Jordan Tucker <jordantucker@gmail.com>
2015-07-13 16:55:35 -0500

    Down to 500 events

--- MFVNeutralino/test/MakeSamples/genonly.py
+++ MFVNeutralino/test/MakeSamples/genonly.py
@@ -1,3 +1,3 @@
 from gensimhlt import cms, process
 process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.output_step)
-process.maxEvents.input = 1000
+process.maxEvents.input = 500
