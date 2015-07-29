08614013fab6ca08eb61c36a35969a43cec7154c
Jordan Tucker <jordantucker@gmail.com>
2015-07-29 13:53:00 -0500

    Demo new CRAB3Submitter; need to come back to GenParticleFilter and separate_by_nrecovtx.

--- MFVNeutralino/test/gen_histos.py
+++ MFVNeutralino/test/gen_histos.py
@@ -2,29 +2,17 @@ import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import *
 debug = 'debug' in sys.argv
 
-process.source.fileNames = ['root://osg-se.cac.cornell.edu//xrootd/path/cms/store/user/tucker/mfv_hltrun2_M0400/mfv_hltrun2_M0400/b0221db6bdc577a56c275739395c3940/gen_10_1_r4s.root']
-file_event_from_argv(process)
+process.source.fileNames = ['root://osg-se.cac.cornell.edu//xrootd/path/cms/store/user/tucker/mfv_neu_tau01000um_M0800/gen/150728_203042/0000/gen_1.root']
 process.TFileService.fileName = 'gen_histos.root'
 file_event_from_argv(process)
 
-process.load('JMTucker.MFVNeutralino.GenParticleFilter_cfi')
-process.load('JMTucker.MFVNeutralino.GenHistos_cff')
-
+#process.load('JMTucker.MFVNeutralino.GenParticleFilter_cfi')
 #process.mfvGenParticleFilter.required_num_leptonic = 0
 
-process.p = cms.Path(process.mfvGenParticleFilter * process.mfvGenHistos)
+process.load('JMTucker.MFVNeutralino.GenHistos_cff')
 
-separate_by_nrecovtx = False
-if separate_by_nrecovtx:
-    process.histos1v = process.mfvGenHistos.clone()
-    process.histos2v = process.mfvGenHistos.clone()
-    process.histos0v = process.mfvGenHistos.clone()
-
-    process.events1v = cms.EDFilter('EventIdVeto', list_fn = cms.string('events1v.txt.gz'), use_run = cms.bool(True))
-    process.events2v = cms.EDFilter('EventIdVeto', list_fn = cms.string('events2v.txt.gz'), use_run = cms.bool(True))
-    process.p1v = cms.Path(~process.events1v * process.histos1v)
-    process.p2v = cms.Path(~process.events2v * process.histos2v)
-    process.p0v = cms.Path(process.events1v * process.events2v * process.histos0v)
+#process.p = cms.Path(process.mfvGenParticleFilter * process.mfvGenHistos)
+process.p = cms.Path(process.mfvGenHistos)
 
 if debug:
     process.printList = cms.EDAnalyzer('JMTParticleListDrawer',
@@ -34,29 +22,32 @@ if debug:
     process.p.insert(0, process.printList)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
-    import JMTucker.Tools.Samples as Samples
-    samples = Samples.ttbar_samples + Samples.qcd_samples
+    
+    from JMTucker.Tools.Sample import anon_samples
 
-    def pset_modifier(sample):
-        to_add = []
-        to_modify = []
+    samples = anon_samples('''
+/mfv_neu_tau00100um_M0400/tucker-gen-30031245991cdc59a4e779a57f211d2a/USER
+/mfv_neu_tau00100um_M0800/tucker-gen-30031245991cdc59a4e779a57f211d2a/USER
+/mfv_neu_tau00100um_M1200/tucker-gen-30031245991cdc59a4e779a57f211d2a/USER
+/mfv_neu_tau00100um_M1600/tucker-gen-30031245991cdc59a4e779a57f211d2a/USER
+/mfv_neu_tau00300um_M0400/tucker-gen-297aaf8461e8651140243c6762af3145/USER
+/mfv_neu_tau00300um_M0800/tucker-gen-297aaf8461e8651140243c6762af3145/USER
+/mfv_neu_tau00300um_M1200/tucker-gen-297aaf8461e8651140243c6762af3145/USER
+/mfv_neu_tau00300um_M1600/tucker-gen-297aaf8461e8651140243c6762af3145/USER
+/mfv_neu_tau01000um_M0400/tucker-gen-80424ff51c64a4a42ab32d70ea13233c/USER
+/mfv_neu_tau01000um_M0800/tucker-gen-80424ff51c64a4a42ab32d70ea13233c/USER
+/mfv_neu_tau01000um_M1200/tucker-gen-80424ff51c64a4a42ab32d70ea13233c/USER
+/mfv_neu_tau01000um_M1600/tucker-gen-80424ff51c64a4a42ab32d70ea13233c/USER
+/mfv_neu_tau10000um_M0400/tucker-gen-d9ae25c69fb344d73087168fa6b951ad/USER
+/mfv_neu_tau10000um_M0800/tucker-gen-d9ae25c69fb344d73087168fa6b951ad/USER
+/mfv_neu_tau10000um_M1200/tucker-gen-d9ae25c69fb344d73087168fa6b951ad/USER
+/mfv_neu_tau10000um_M1600/tucker-gen-d9ae25c69fb344d73087168fa6b951ad/USER
+''', dbs_inst='phys03')
 
-        if separate_by_nrecovtx:
-            to_add.append("process.events1v.list_fn = '%s_1v.txt.gz'" % sample.name)
-            to_add.append("process.events2v.list_fn = '%s_2v.txt.gz'" % sample.name)
-        return to_add, to_modify
-
-    def crab_cfg_modifier(sample):
-        if separate_by_nrecovtx:
-            return [('USER', 'additional_input_files', '%s_1v.txt.gz,%s_2v.txt.gz' % (sample.name, sample.name))]
-        return []
-
-    from JMTucker.Tools.CRABSubmitter import CRABSubmitter
+    from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
     cs = CRABSubmitter('GenHistos',
-                       total_number_of_events = -1,
-                       events_per_job = 20000,
-                       skip_common = True,
-                       pset_modifier = pset_modifier,
-                       crab_cfg_modifier = crab_cfg_modifier,
+                       splitting = 'EventAwareLumiBased',
+                       units_per_job = 20000,
+                       total_units = -1,
                        )
     cs.submit_all(samples)
