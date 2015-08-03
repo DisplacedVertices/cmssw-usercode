061fd943e1824a09e837c0cb7d722d418975660c
Jordan Tucker <jordantucker@gmail.com>
2015-08-03 15:36:10 -0500

    Working on crab_hadd.

--- Tools/python/CRAB3Tools.py
+++ Tools/python/CRAB3Tools.py
@@ -389,6 +389,95 @@ def crab_output_files(working_dir, jobs=None):
         d = crab_command('getoutput', '--xrootd', dir=working_dir)
     return d.get('xrootd', [])
 
+'''
+def crab_hadd(working_dir, new_name=None, new_dir=None, raise_on_empty=False, chunk_size=900):
+    if working_dir.endswith('/'):
+        working_dir = working_dir[:-1]
+    if new_name is None:
+        new_name = os.path.basename(working_dir).replace('crab_','')
+    if not new_name.endswith('.root'):
+        new_name += '.root'
+    if new_dir is not None:
+        new_name = os.path.join(new_dir, new_name)
+
+    expected = crab_get_njobs(working_dir)
+    print '%s: expecting %i files if all jobs succeeded' % (working_dir, expected)
+
+    on_store = False
+    cfg = crab_cfg_parser(working_dir)
+    try:
+        storage_element = cfg.get('USER', 'storage_element')
+        on_store = storage_element == 'T3_US_FNALLPC'
+    except NoOptionError:
+        pass
+
+    files = []
+    
+    if on_store:
+        pfns = []
+        for path in glob.glob(os.path.join(working_dir, 'res/crab_fjr*xml')):
+            if crab_get_exit_codes(path) == (0,0):
+                pfns.append(crab_analysis_file_pfn(path))
+        files = ['root://cmsxrootd.fnal.gov//store' + pfn.split('/store')[1] for pfn in pfns] # JMTBAD
+    else:    
+        files = glob.glob(os.path.join(working_dir, 'res/*root'))
+
+    job_nums = defaultdict(lambda: defaultdict(list))
+    for f in files:
+        f_ = f.split('_')
+        job, num = int(f_[-3]), int(f_[-2])
+        job_nums[job][num].append(f)
+
+    sexpected = set(xrange(1,expected+1))
+    sjobs = set(job_nums)
+    if sjobs != sexpected:
+        print '\033[36;7m %i files found %s not what expected \033[m' % (len(sjobs), crabify_list(sorted(sjobs)))
+
+        missing = sorted(sexpected - sjobs)
+        print '\033[36;7m    %i missing: %r \033[m' % (len(missing), crabify_list(missing))
+
+        to_drop = []
+        for job, nums_and_fs in job_nums.iteritems():
+            for num, fs in nums_and_fs.iteritems():
+                if len(fs) > 1:
+                    print '\033[36;7m     for job %i, more than one file with resub num %i *** will keep latest by mtime *** \033[m' % (job, num)
+                    fs.sort(key=lambda f: os.stat(f).st_mtime)
+                    good_f = fs[-1]
+                    while len(fs) > 1:
+                        bad_f = fs.pop(0)
+                        to_drop.append((bad_f, good_f))
+            if len(nums_and_fs) > 1:
+                good_num = max(nums_and_fs)
+                assert len(nums_and_fs[good_num]) == 1
+                good_f = nums_and_fs[good_num][0]
+                for num, fs in nums_and_fs.iteritems():
+                    if num != good_num:
+                        assert len(fs) == 1
+                        to_drop.append((fs[0], good_f))
+
+        for f, good_f in to_drop:
+            print '\033[36;7m     dropping %s in favor of %s \033[m' % (f, good_f)
+            files.remove(f)
+
+    
+    l = len(files)
+    if l == 0:
+        msg = 'crab_hadd: no files found in %s' % working_dir
+        if raise_on_empty:
+            raise CrabError(msg)
+        else:
+            print '\033[36;7m', msg, '\033[m'
+    elif l == 1:
+        print working_dir, ': just one file found, copying'
+        cmd = '%scp %s %s' % ('dc' if 'dcap' in files[0] else '', files[0], new_name)
+        os.system(cmd)
+        os.chmod(new_name, 0644)
+    else:
+        hadd(new_name, files, chunk_size)
+
+    return new_name
+'''
+
 def crab_requestcache(working_dir):
     return cPickle.load(open(os.path.join(working_dir, '.requestcache'), 'rb'))
 
