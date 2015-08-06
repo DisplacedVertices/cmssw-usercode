b94ca229ec46d9a7f49f2518327b376aa9122f4f
Jordan Tucker <jordantucker@gmail.com>
2015-08-06 14:27:10 -0500

    In submit script, 25 and 50 ns sample loop (yuck); just doing timing study for now.

--- MFVNeutralino/test/ntuple.py
+++ MFVNeutralino/test/ntuple.py
@@ -143,102 +143,109 @@ if debug:
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
     from JMTucker.Tools.Sample import anon_samples
+    from JMTucker.Tools.Samples import *
 
-    samples = anon_samples('''
-/QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/AODSIM
-/QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/AODSIM
-/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/AODSIM
-/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v4/AODSIM
-''')
-
-    def modify(sample):
-        to_add = []
-        to_replace = []
-
-        if sample.is_mc:
-            if sample.is_fastsim:
-                raise NotImplementedError('zzzzzzz')
+    for is_25ns in (True, False):
+        if is_25ns:
+            samples = ttbar_mgnlo_25ns_samples + qcd_ht_mg_25ns_samples
         else:
-            raise NotImplementedError('zzzzzzz')
-            magic = 'is_mc = True'
-            err = 'trying to submit on data, and tuple template does not contain the magic string "%s"' % magic
-            to_replace.append((magic, 'is_mc = False', err))
-            if sample.name.startswith('MultiJetPk2012'):
-                for name_part, tag in [
-                    ('2012B', 'FT_53_V6C_AN4'),
-                    ('2012C1', 'FT53_V10A_AN4'),
-                    ('2012C2', 'FT_P_V42C_AN4'),
-                    ('2012D1', 'FT_P_V42_AN4'),
-                    ('2012D2', 'FT_P_V42D_AN4'),
-                    ]:
-                    if name_part in sample.name:
-                        to_add.append('process.GlobalTag.globaltag = "%s::All"' % tag)
-            to_add.append('process.dummyToMakeDiffHash = cms.PSet(submitName = cms.string("%s"))' % (sample.name + 'hello'))
+            samples = ttbar_mgnlo_50ns_samples # + qcd_ht_mg_25ns_samples
 
-        return to_add, to_replace
+        def modify(sample):
+            to_add = []
+            to_replace = []
+
+            if sample.is_mc:
+                if sample.is_fastsim:
+                    raise NotImplementedError('zzzzzzz')
+
+                if not is_25ns:
+                    to_add.append('process.GlobalTag.globaltag = "MCRUN2_74_V9A"')
+
+            else:
+                raise NotImplementedError('zzzzzzz')
+                magic = 'is_mc = True'
+                err = 'trying to submit on data, and tuple template does not contain the magic string "%s"' % magic
+                to_replace.append((magic, 'is_mc = False', err))
+                if sample.name.startswith('MultiJetPk2012'):
+                    for name_part, tag in [
+                        ('2012B', 'FT_53_V6C_AN4'),
+                        ('2012C1', 'FT53_V10A_AN4'),
+                        ('2012C2', 'FT_P_V42C_AN4'),
+                        ('2012D1', 'FT_P_V42_AN4'),
+                        ('2012D2', 'FT_P_V42D_AN4'),
+                        ]:
+                        if name_part in sample.name:
+                            to_add.append('process.GlobalTag.globaltag = "%s::All"' % tag)
+                to_add.append('process.dummyToMakeDiffHash = cms.PSet(submitName = cms.string("%s"))' % (sample.name + 'hello'))
+
+            return to_add, to_replace
+
+        batch_name_extra = '25ns' if is_25ns else '50ns'
+
+        batch_name_extra = += 'DeleteMe_ForTiming'
+
+        if jumble_tracks:
+            batch_name_extra += '_JumbleTks'
+
+        if remove_tracks:
+            batch_name_extra += '_RemoveTks%i' % remove_tracks[1]
+
+        if track_used_req == 'nopv':
+            batch_name_extra += '_NoPVTks'
+        elif track_used_req == 'nopvs':
+            batch_name_extra += '_NoPVsTks'
+
+        if keep_extra:
+            batch_name_extra += '_WExtra'
+        elif prepare_vis:
+            batch_name_extra += '_WVis'
+        elif keep_all:
+            batch_name_extra += '_WAll'
+
+        if track_histos_only:
+            batch_name_extra += '_TrackHistosOnly'
+
+        if debug:
+            batch_name_extra += '_WDebug'
 
 
-    batch_name_extra = ''
+        cs = CRABSubmitter('Ntuple' + tuple_version.upper() + batch_name_extra,
+                           pset_modifier = modify,
+                           #job_control_from_sample = True,
+                           splitting = 'EventAwareLumiBased',
+                           units_per_job = 500,
+                           total_units = 500,
+                           publish_name = 'ntuple_' + tuple_version + batch_name_extra.lower(),
+                           )
 
-    if jumble_tracks:
-        batch_name_extra += '_JumbleTks'
-
-    if remove_tracks:
-        batch_name_extra += '_RemoveTks%i' % remove_tracks[1]
-
-    if track_used_req == 'nopv':
-        batch_name_extra += '_NoPVTks'
-    elif track_used_req == 'nopvs':
-        batch_name_extra += '_NoPVsTks'
-
-    if keep_extra:
-        batch_name_extra += '_WExtra'
-    elif prepare_vis:
-        batch_name_extra += '_WVis'
-    elif keep_all:
-        batch_name_extra += '_WAll'
-
-    if track_histos_only:
-        batch_name_extra += '_TrackHistosOnly'
-
-    if debug:
-        batch_name_extra += '_WDebug'
-
-
-    cs = CRABSubmitter('Ntuple' + tuple_version.upper() + batch_name_extra,
-                       pset_modifier = modify,
-                       job_control_from_sample = True,
-                       publish_name = 'ntuple_' + tuple_version + batch_name_extra.lower(),
-                       max_threads = 2,
-                       )
-
-#    timing = { 'dyjetstollM10': 0.011203, 'dyjetstollM50': 0.019867, 'qcdbce020': 0.008660, 'qcdbce030': 0.007796, 'qcdbce080': 0.061260, 'qcdbce170': 0.328891, 'qcdbce250': 0.481813, 'qcdbce350': 0.519482, 'qcdem020': 0.010137, 'qcdem030': 0.01, 'qcdem080': 0.037925, 'qcdem170': 0.286123, 'qcdem250': 0.471398, 'qcdem350': 0.686303, 'qcdht0100': 0.008273, 'qcdht0250': 0.116181, 'qcdht0500': 0.738374, 'qcdht1000': 1.002745, 'qcdmu0020': 0.012301, 'qcdmu0030': 0.015762, 'qcdmu0050': 0.018178, 'qcdmu0080': 0.119300, 'qcdmu0120': 0.245562, 'qcdmu0170': 0.32, 'qcdmu0300': 0.419818, 'qcdmu0470': 0.584266, 'qcdmu0600': 0.455305, 'qcdmu0800': 0.879171, 'qcdmu1000': 1.075712, 'singletop_s': 0.093429, 'singletop_s_tbar': 0.146642, 'singletop_tW': 0.327386, 'singletop_tW_tbar': 0.184349, 'singletop_t': 0.092783, 'singletop_t_tbar': 0.060983, 'ttbarhadronic': 0.752852, 'ttbarsemilep': 0.419073, 'ttbardilep': 0.295437, 'ttgjets': 0.861987, 'ttwjets': 0.714156, 'ttzjets': 0.827464, 'wjetstolnu': 0.010842, 'ww': 0.046754, 'wz': 0.049839, 'zz': 0.059791, }
-#    timing.update({ 'qcdpt0000': 0.005460, 'qcdpt0005': 0.008174, 'qcdpt0015': 0.007915, 'qcdpt0030': 0.014118, 'qcdpt0050': 0.011146, 'qcdpt0080': 0.040685, 'qcdpt0120': 0.141580, 'qcdpt0170': 0.374554, 'qcdpt0300': 0.509502, 'qcdpt0470': 0.830390, 'qcdpt0600': 0.996345, 'qcdpt0800': 0.428342, 'qcdpt1000': 0.546948, 'qcdpt1400': 0.544585, 'qcdpt1800': 2.078266, })
-#    timing.update({ 'bjetsht0100': 0.008273, 'bjetsht0250': 0.116181, 'bjetsht0500': 0.738374, 'bjetsht1000': 1.002745, })
-#    timing.update(dict((s.name, 0.752852) for s in Samples.myttbar_samples + Samples.ttbar_systematics_samples))
-#    timing.update({ 'myttbarpynopu': 0.25, 'myttbarpydesignnopu': 0.25 })
+#        timing = { 'dyjetstollM10': 0.011203, 'dyjetstollM50': 0.019867, 'qcdbce020': 0.008660, 'qcdbce030': 0.007796, 'qcdbce080': 0.061260, 'qcdbce170': 0.328891, 'qcdbce250': 0.481813, 'qcdbce350': 0.519482, 'qcdem020': 0.010137, 'qcdem030': 0.01, 'qcdem080': 0.037925, 'qcdem170': 0.286123, 'qcdem250': 0.471398, 'qcdem350': 0.686303, 'qcdht0100': 0.008273, 'qcdht0250': 0.116181, 'qcdht0500': 0.738374, 'qcdht1000': 1.002745, 'qcdmu0020': 0.012301, 'qcdmu0030': 0.015762, 'qcdmu0050': 0.018178, 'qcdmu0080': 0.119300, 'qcdmu0120': 0.245562, 'qcdmu0170': 0.32, 'qcdmu0300': 0.419818, 'qcdmu0470': 0.584266, 'qcdmu0600': 0.455305, 'qcdmu0800': 0.879171, 'qcdmu1000': 1.075712, 'singletop_s': 0.093429, 'singletop_s_tbar': 0.146642, 'singletop_tW': 0.327386, 'singletop_tW_tbar': 0.184349, 'singletop_t': 0.092783, 'singletop_t_tbar': 0.060983, 'ttbarhadronic': 0.752852, 'ttbarsemilep': 0.419073, 'ttbardilep': 0.295437, 'ttgjets': 0.861987, 'ttwjets': 0.714156, 'ttzjets': 0.827464, 'wjetstolnu': 0.010842, 'ww': 0.046754, 'wz': 0.049839, 'zz': 0.059791, }
+#        timing.update({ 'qcdpt0000': 0.005460, 'qcdpt0005': 0.008174, 'qcdpt0015': 0.007915, 'qcdpt0030': 0.014118, 'qcdpt0050': 0.011146, 'qcdpt0080': 0.040685, 'qcdpt0120': 0.141580, 'qcdpt0170': 0.374554, 'qcdpt0300': 0.509502, 'qcdpt0470': 0.830390, 'qcdpt0600': 0.996345, 'qcdpt0800': 0.428342, 'qcdpt1000': 0.546948, 'qcdpt1400': 0.544585, 'qcdpt1800': 2.078266, })
+#        timing.update({ 'bjetsht0100': 0.008273, 'bjetsht0250': 0.116181, 'bjetsht0500': 0.738374, 'bjetsht1000': 1.002745, })
+#        timing.update(dict((s.name, 0.752852) for s in Samples.myttbar_samples + Samples.ttbar_systematics_samples))
+#        timing.update({ 'myttbarpynopu': 0.25, 'myttbarpydesignnopu': 0.25 })
+#        
+#        for sample in Samples.all_mc_samples:
+#            if timing.has_key(sample.name):
+#                sample.events_per = int(3.5*3600/timing[sample.name])
+#                sample.timed = True
+#                nj = int(sample.nevents_orig / float(sample.events_per)) + 1
+#                assert nj < 5000
 #    
-#    for sample in Samples.all_mc_samples:
-#        if timing.has_key(sample.name):
-#            sample.events_per = int(3.5*3600/timing[sample.name])
-#            sample.timed = True
-#            nj = int(sample.nevents_orig / float(sample.events_per)) + 1
-#            assert nj < 5000
-#
-#    for s in Samples.mfv_signal_samples + Samples.mfv_signal_samples_systematics + Samples.exo12038_samples:
-#        s.events_per = 2000
-#        s.timed = True
-#
-#    samples = Samples.from_argv([Samples.mfv_neutralino_tau0100um_M0400,
-#                                 Samples.mfv_neutralino_tau1000um_M0400,
-#                                 Samples.mfv_neutralino_tau0300um_M0400,
-#                                 Samples.mfv_neutralino_tau9900um_M0400] + Samples.ttbar_samples + Samples.qcd_samples + Samples.data_samples)
-#
-#    for sample in samples:
-#        if sample.is_mc:
-#            sample.total_events = -1
-#            assert hasattr(sample, 'timed')
-#        else:
-#            sample.json = 'ana_all.json'
+#        for s in Samples.mfv_signal_samples + Samples.mfv_signal_samples_systematics + Samples.exo12038_samples:
+#            s.events_per = 2000
+#            s.timed = True
+#    
+#        samples = Samples.from_argv([Samples.mfv_neutralino_tau0100um_M0400,
+#                                     Samples.mfv_neutralino_tau1000um_M0400,
+#                                     Samples.mfv_neutralino_tau0300um_M0400,
+#                                     Samples.mfv_neutralino_tau9900um_M0400] + Samples.ttbar_samples + Samples.qcd_samples + Samples.data_samples)
+#    
+#        for sample in samples:
+#            if sample.is_mc:
+#                sample.total_events = -1
+#                assert hasattr(sample, 'timed')
+#            else:
+#                sample.json = 'ana_all.json'
 
     cs.submit_all(samples)
