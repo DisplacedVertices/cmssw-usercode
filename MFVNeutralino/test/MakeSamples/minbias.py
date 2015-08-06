f06c31b6974a6ca14e57ea926a94685c15c2764f
Jordan Tucker <jordantucker@gmail.com>
2015-08-06 12:54:57 -0500

    Write compressed pickle

--- MFVNeutralino/test/MakeSamples/minbias.py
+++ MFVNeutralino/test/MakeSamples/minbias.py
@@ -6,5 +6,7 @@ try:
 except ImportError:
     print 'nope, getting minbias file list from DBS and caching'
     from JMTucker.Tools.DBS import files_in_dataset
+    from JMTucker.Tools.general import to_pickle
     files = files_in_dataset('/MinBias_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-MCRUN2_71_V1-v1/GEN-SIM')
-    open('minbias_files.py', 'wt').write('files = %r\n' % files)
+    to_pickle(files, 'minbias_files.pkl', comp=True)
+    open('minbias_files.py', 'wt').write("import cPickle, gzip; files = cPickle.load(gzip.GzipFile('minbias_files.pkl', 'rb'))\n")
