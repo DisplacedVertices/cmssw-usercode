26ec6565fd037bbc9909b438164dc9ddb9bebf7e
Jordan Tucker <jordantucker@gmail.com>
2015-08-06 12:44:13 -0500

    13 TeV minbias sample

--- MFVNeutralino/test/MakeSamples/minbias.py
+++ MFVNeutralino/test/MakeSamples/minbias.py
@@ -4,7 +4,7 @@ try:
     print 'getting minbias file list from cache'
     from minbias_files import files
 except ImportError:
-    print 'getting minbias file list from DBS and caching'
+    print 'nope, getting minbias file list from DBS and caching'
     from JMTucker.Tools.DBS import files_in_dataset
-    files = files_in_dataset('/MinBias_TuneZ2star_8TeV-pythia6/Summer12-START50_V13-v3/GEN-SIM')
+    files = files_in_dataset('/MinBias_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-MCRUN2_71_V1-v1/GEN-SIM')
     open('minbias_files.py', 'wt').write('files = %r\n' % files)
