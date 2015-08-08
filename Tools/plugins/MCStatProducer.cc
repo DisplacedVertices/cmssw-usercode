33f399691e334cf78d1c03f3111f554e69437207
Jordan Tucker <jordantucker@gmail.com>
2015-08-08 13:10:22 -0500

    Write numbers per lumi since per run won't work when reading back later.

--- Tools/plugins/MCStatProducer.cc
+++ Tools/plugins/MCStatProducer.cc
@@ -5,13 +5,13 @@
 #include "FWCore/Framework/interface/Run.h"
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
 
-class MCStatProducer : public edm::one::EDProducer<edm::EndRunProducer> {
+class MCStatProducer : public edm::one::EDProducer<edm::EndLuminosityBlockProducer> {
 public:
   explicit MCStatProducer(const edm::ParameterSet&);
 
 private:
   virtual void produce(edm::Event&, const edm::EventSetup&) override;
-  virtual void endRunProduce(edm::Run&, const edm::EventSetup&) override;
+  virtual void endLuminosityBlockProduce(edm::LuminosityBlock&, const edm::EventSetup&) override;
 
   const edm::EDGetTokenT<GenEventInfoProduct> gen_info_token;
 
@@ -21,11 +21,14 @@ private:
 };
 
 MCStatProducer::MCStatProducer(const edm::ParameterSet& cfg)
-  : gen_info_token(consumes<GenEventInfoProduct>(cfg.getParameter<edm::InputTag>("gen_info_src")))
+  : gen_info_token(consumes<GenEventInfoProduct>(cfg.getParameter<edm::InputTag>("gen_info_src"))),
+    nevents(0),
+    sumweight(0),
+    sumweightprod(0)
 {
-  produces<int,   edm::InRun>("nEvents");
-  produces<float, edm::InRun>("sumWeight");
-  produces<float, edm::InRun>("sumWeightProd");
+  produces<int,   edm::InLumi>("nEvents");
+  produces<float, edm::InLumi>("sumWeight");
+  produces<float, edm::InLumi>("sumWeightProd");
 }
 
 void MCStatProducer::produce(edm::Event& event, const edm::EventSetup& setup) {
@@ -37,15 +40,18 @@ void MCStatProducer::produce(edm::Event& event, const edm::EventSetup& setup) {
   sumweightprod += gen_info->weightProduct();
 }
 
-void MCStatProducer::endRunProduce(edm::Run& run, const edm::EventSetup&) {
+void MCStatProducer::endLuminosityBlockProduce(edm::LuminosityBlock& lumi, const edm::EventSetup&) {
   std::unique_ptr<int> pnevents(new int(nevents));
-  run.put(std::move(pnevents), "nEvents");
+  lumi.put(std::move(pnevents), "nEvents");
 
   std::unique_ptr<float> psumweight(new float(sumweight));
-  run.put(std::move(psumweight), "sumWeight");
+  lumi.put(std::move(psumweight), "sumWeight");
 
   std::unique_ptr<float> psumweightprod(new float(sumweightprod));
-  run.put(std::move(psumweightprod), "sumWeightProd");
+  lumi.put(std::move(psumweightprod), "sumWeightProd");
+
+  nevents = 0;
+  sumweight = sumweightprod = 0;
 }
 
 DEFINE_FWK_MODULE(MCStatProducer);
