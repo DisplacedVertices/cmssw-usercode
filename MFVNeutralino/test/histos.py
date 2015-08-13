78d3b930adca1eee51fa62a0acd405c919c697cd
Jordan Tucker <jordantucker@gmail.com>
2015-08-13 15:17:57 -0500

    Submit script

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -81,19 +81,15 @@ def force_bs(process, bs):
             ana.force_bs = bs
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
-    import JMTucker.Tools.Samples as Samples
-    samples = Samples.from_argv([Samples.mfv_neutralino_tau0100um_M0400,
-                                 Samples.mfv_neutralino_tau1000um_M0400,
-                                 Samples.mfv_neutralino_tau0300um_M0400,
-                                 Samples.mfv_neutralino_tau9900um_M0400] + Samples.ttbar_samples + Samples.qcd_samples + Samples.data_samples)
+    from JMTucker.Tools.Sample import anon_samples
+    samples = anon_samples('''
+''', dbs_inst='phys03')
 
-    for s in Samples.data_samples:
-        s.json = 'ana_all.json'
-
-    from JMTucker.Tools.CRABSubmitter import CRABSubmitter
-    cs = CRABSubmitter('HistosV20',
-                       job_control_from_sample = True,
-                       use_ana_dataset = True,
-                       #USER_additional_input_files = 'aaaa.root',
+    from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
+    cs = CRABSubmitter('HistosV2',
+                       splitting = 'EventAwareLumiBased',
+                       units_per_job = 50000,
+                       total_units = -1,
+                       aaa = True,
                        )
     cs.submit_all(samples)
