cdd316489b20c8acaed8ecaa004d3072e9a8915f
Jordan Tucker <jordantucker@gmail.com>
2015-10-02 14:05:29 -0500

    prescales for run2

--- MFVNeutralino/test/TriggerStudies/prescales.py
+++ MFVNeutralino/test/TriggerStudies/prescales.py
@@ -3,18 +3,18 @@
 import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import *
 
-process.TFileService.fileName = 'quadjettrigprescales.root'
+process.TFileService.fileName = 'prescales.root'
 
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'FT53_V21A_AN6::All'
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '74X_dataRun2_Prompt_v2', '')
 
 process.source.fileNames = [
-    '/store/data/Run2012B/MultiJet1Parked/AOD/05Nov2012-v2/10000/2A072028-5949-E211-947E-00304866C368.root',
-    '/store/data/Run2012C/MultiJet1Parked/AOD/part1_05Nov2012-v2/10000/1E337BCE-2D4E-E211-8B97-003048D47A7C.root',
-    '/store/data/Run2012C/MultiJet1Parked/AOD/part2_05Nov2012-v2/10012/70E96449-E65F-E211-B550-003048D45FB0.root',
+    '/store/data/Run2015D/SingleMuon/AOD/PromptReco-v3/000/256/729/00000/0283F591-A25F-E511-93AB-02163E011BBE.root'
     ]
 
-add_analyzer('QuadJetTrigPrescales')
+add_analyzer(process, 'MFVTriggerPrescales')
+process.maxEvents.input = 100
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
