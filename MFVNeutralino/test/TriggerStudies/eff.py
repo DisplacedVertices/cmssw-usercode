71dcb328b9424e1f7353d5e25efaddba0251e6eb
Jordan Tucker <jordantucker@gmail.com>
2015-10-13 13:24:26 -0500

    Filter version and a debug event; submit v2

--- MFVNeutralino/test/TriggerStudies/eff.py
+++ MFVNeutralino/test/TriggerStudies/eff.py
@@ -2,6 +2,7 @@
 
 import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import *
+from JMTucker.Tools.CMSSWTools import *
 
 is_mc = True
 
@@ -10,6 +11,8 @@ process.maxEvents.input = 1000
 process.source.fileNames = ['/store/mc/RunIISpring15DR74/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/80000/CA0ABB76-43FC-E411-A207-1CC1DE1CEDB2.root' if is_mc else '/store/data/Run2015D/SingleMuon/MINIAOD/PromptReco-v3/000/256/630/00000/BCD78EF7-2B5F-E511-A3A3-02163E0170B5.root']
 #process.source.fileNames = ['/store/mc/RunIISpring15DR74/QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/0270C6BA-AE17-E511-9D04-549F358EB755.root']
 #process.source.fileNames = ['/store/mc/RunIISpring15DR74/QCD_HT300to500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v2/00000/00F9B1F1-3B18-E511-B17D-A0369F30FFD2.root']
+#process.source.fileNames = ['/store/mc/RunIISpring15DR74/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/70000/AAC7A1D1-F6FB-E411-AFC9-E0CB4E1A114B.root']
+#set_events_to_process(process, [(1, 205534, 50556064)])
 process.TFileService.fileName = 'eff.root'
 if not is_mc:
     from JMTucker.Tools.Sample import DataSample
@@ -26,7 +29,7 @@ from JMTucker.Tools.PATTupleSelection_cfi import jtupleParams
 process.RandomNumberGeneratorService = cms.Service('RandomNumberGeneratorService')
 process.RandomNumberGeneratorService.SimpleTriggerEfficiency = cms.PSet(initialSeed = cms.untracked.uint32(1219))
 
-process.emu = cms.EDAnalyzer('MFVEmulateHT800',
+process.emu = cms.EDFilter('MFVEmulateHT800',
                              trigger_results_src = cms.InputTag('TriggerResults', '', 'HLT'),
                              trigger_objects_src = cms.InputTag('selectedPatTrigger'),
                              throw_not_found = cms.bool(False),
@@ -73,7 +76,7 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         return to_add, to_replace
 
     from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
-    cs = CRABSubmitter('TestEmu800',
+    cs = CRABSubmitter('TestEmu800v2',
                        pset_modifier = pset_modifier,
                        job_control_from_sample = True,
                        dataset = 'miniaod',
