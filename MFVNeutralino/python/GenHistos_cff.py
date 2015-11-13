ab42c8a436ebd62f16733509d7aaddf7a6574ab7
Jordan Tucker <jordantucker@gmail.com>
2015-11-13 04:47:48 -0600

    Instead of bkg or no, mci_mode string to use mfv3j or xx4j. Gutted tops.

--- MFVNeutralino/python/GenHistos_cff.py
+++ MFVNeutralino/python/GenHistos_cff.py
@@ -6,5 +6,5 @@ mfvGenHistos = cms.EDAnalyzer('MFVGenHistos',
                               gen_src = cms.InputTag('genParticles'),
                               gen_jet_src = cms.InputTag('ak4GenJets'),
                               check_all_gen_particles = cms.bool(False),
-                              mci_bkg = cms.bool(False)
+                              mci_mode = cms.string('mfv3j')
                               )
