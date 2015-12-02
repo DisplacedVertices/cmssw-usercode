67ea047b086fb09e7af66b32e29428a3c35bded7
Jordan Tucker <jordantucker@gmail.com>
2015-12-02 10:08:09 -0600

    Follow MINIAODv2.

--- Tools/python/MiniAOD_cfg.py
+++ Tools/python/MiniAOD_cfg.py
@@ -18,16 +18,17 @@ def pat_tuple_process(customize_before_unscheduled, is_mc):
     process.load('SimGeneral.MixingModule.mixNoPU_cfi')
     process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
     process.load('Configuration.StandardSequences.MagneticField_38T_cff')
-    #process.load('Configuration.StandardSequences.EndOfProcess_cff')
+    process.load('PhysicsTools.PatAlgos.slimming.metFilterPaths_cff')
 
     process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
     from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-    process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_74_V9', '')
+    tag = '74X_mcRun2_asymptotic_v4' if is_mc else '74X_dataRun2_v5'
+    process.GlobalTag = GlobalTag(process.GlobalTag, tag, '')
 
     process.options = cms.untracked.PSet(allowUnscheduled = cms.untracked.bool(True),
                                          wantSummary = cms.untracked.bool(False),
                                          )
-    process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
+    process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
     process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('file:/uscms_data/d2/tucker/F47E7F59-8A29-E511-8667-002590A52B4A.root'))
 
     process.out = cms.OutputModule('PoolOutputModule',
@@ -43,19 +44,29 @@ def pat_tuple_process(customize_before_unscheduled, is_mc):
 
     process.outp = cms.EndPath(process.out)
 
+    if is_mc:
+        from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
+        process = customisePostLS1(process)
+    else:
+        from Configuration.DataProcessing.RecoTLR import customiseDataRun2Common_25ns
+        process = customiseDataRun2Common_25ns(process)
+
     if customize_before_unscheduled is not None:
         customize_before_unscheduled(process)
 
-    from FWCore.ParameterSet.Utilities import convertToUnscheduled
+    from FWCore.ParameterSet.Utilities import convertToUnscheduled, cleanUnscheduled
     process = convertToUnscheduled(process)
-
+    
     from PhysicsTools.PatAlgos.slimming.miniAOD_tools import miniAOD_customizeAllData, miniAOD_customizeAllMC
     if is_mc:
         process.load('Configuration.StandardSequences.PATMC_cff') # right now, same as PAT_cff
-        process = miniAOD_customizeAllMC(process)
+        customize = miniAOD_customizeAllMC
     else:
         process.load('Configuration.StandardSequences.PAT_cff')
-        process = miniAOD_customizeAllData(process)
+        customize = miniAOD_customizeAllData
+
+    process = cleanUnscheduled(process)
+    process = customize(process)
 
     process.load('JMTucker.Tools.PATTupleSelection_cfi')
     process.selectedPatJets.cut = process.jtupleParams.jetCut
