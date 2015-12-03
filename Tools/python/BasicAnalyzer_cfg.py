e5c97e726a76c20a94287c64f7970c6906bec735
Jordan Tucker <jordantucker@gmail.com>
2015-12-03 12:24:11 -0600

    Just grab everything from CMSSWTools, and export everything

--- Tools/python/BasicAnalyzer_cfg.py
+++ Tools/python/BasicAnalyzer_cfg.py
@@ -1,29 +1,12 @@
-import sys, FWCore.ParameterSet.Config as cms
-from JMTucker.Tools.CMSSWTools import \
-    basic_process, \
-    files_from_file, \
-    file_event_from_argv, \
-    global_tag, \
-    geometry_etc, \
-    report_every, \
-    tfileservice, \
-    add_analyzer as _add_analyzer
+#!/usr/bin/env python
+
+import FWCore.ParameterSet.Config as cms
+from JMTucker.Tools.CMSSWTools import *
 
 process = basic_process('BasicAnalyzer')
 report_every(process, 1000000)
 tfileservice(process)
 
+_add_analyzer = add_analyzer 
 def add_analyzer(process, name, **kwargs):
     return _add_analyzer(process, name, **kwargs)
-
-__all__ = [
-    'cms',
-    'process',
-    'files_from_file',
-    'file_event_from_argv',
-    'geometry_etc',
-    'global_tag',
-    'tfileservice',
-    'add_analyzer',
-    'report_every'
-    ]
