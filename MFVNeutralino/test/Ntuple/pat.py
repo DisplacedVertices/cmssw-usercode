093e736c3e9335a31942b088dfcc5f86e3dfa6a3
Jordan Tucker <jordantucker@gmail.com>
2015-12-03 06:47:24 -0600

    Split out pat step of ntupler.

---
+++ MFVNeutralino/test/Ntuple/pat.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+from JMTucker.Tools.general import bool_from_argv
+from JMTucker.Tools.MiniAOD_cfg import cms, pat_tuple_process, report_every
+
+is_mc = not bool_from_argv('is_data')
+keep_all = bool_from_argv('keep_all')
+trig_filter = not keep_all
+# JMTBAD different globaltags?
+
+def customize_before_unscheduled(process):
+    process.load('JMTucker.Tools.MCStatProducer_cff')
+
+    process.out.outputCommands = [
+            'drop *',
+            'keep *_mcStat_*_*',
+            'keep edmTriggerResults_TriggerResults__HLT',
+            'keep edmTriggerResults_TriggerResults__PAT',
+            'keep *_addPileupInfo_*_*',
+            'keep *_generator_*_*',
+            'keep *_genParticles_*_*',
+            'keep *_ak4GenJets*_*_*',
+            'keep *_generalTracks_*_*',
+            'keep *_offlineBeamSpot_*_*',
+            'keep *_goodOfflinePrimaryVertices_*_*',
+            'keep *_selectedPatJets_*_*',
+            'keep *_selectedPatMuons_*_*',
+            'keep *_selectedPatElectrons_*_*',
+            'keep patMETs_patMETsNoHF_*_*',
+            'drop CaloTowers_selectedPatJets_caloTowers_*',
+            'keep recoGsfElectronCores_*_*_*', # sigh
+            # not sure how these three sneak past the drop *
+            'drop patMETs_patPFMetPuppi__PAT',
+            'drop patMETs_patCaloMet__PAT',
+            'drop patMETs_patMETPuppi__PAT',
+            ]
+
+    if keep_all:
+        if is_mc:
+            process.out.outputCommands += \
+                process.AODSIMEventContent.outputCommands + \
+                process.MINIAODSIMEventContent.outputCommands
+        else:
+            process.out.outputCommands += \
+                process.AODEventContent.outputCommands + \
+                process.MINIAODEventContent.outputCommands
+
+process = pat_tuple_process(customize_before_unscheduled, is_mc)
+
+# If the embedding is on for these, then we can't match leptons by track to vertices.
+process.patMuons.embedTrack = False
+process.patElectrons.embedTrack = False
+
+# Instead of keeping the original branches in the temp file.
+process.patElectrons.embedSuperCluster = True
+process.patJets.embedPFCandidates = True
+
+if trig_filter:
+    import JMTucker.MFVNeutralino.TriggerFilter
+    JMTucker.MFVNeutralino.TriggerFilter.setup_trigger_filter(process)
+
+#process.maxEvents.input = 10
+#process.options.wantSummary = True
