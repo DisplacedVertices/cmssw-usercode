f84d10dadef1210fee580c71be28789d357bdaa5
Jordan Tucker <jordantucker@gmail.com>
2015-12-04 00:20:42 -0600

    v7, make it work with data only

--- MFVNeutralino/test/TriggerStudies/draw_eff.py
+++ MFVNeutralino/test/TriggerStudies/draw_eff.py
@@ -3,16 +3,18 @@ from array import array
 from JMTucker.Tools.ROOTTools import *
 from JMTucker.Tools.Samples import *
 
+v = 'v7'
+root_dir = '/eos/uscms/store/user/tucker/crab_dirs/TrigEff' + v
+plot_dir = 'plots/TrigEff_' + v
+
 set_style()
 ROOT.gStyle.SetOptStat(0)
-ps = plot_saver('plots/TrigEff_v3', size=(600,600), log=False)
+ps = plot_saver(plot_dir, size=(600,600), log=False)
 ROOT.TH1.AddDirectory(0)
 
 save_more = False
 
-root_dir = '/eos/uscms/store/user/tucker/crab_dirs/TestEmu800v3'
-
-int_lumi = 600.
+int_lumi = 2440.
 data_fn = os.path.join(root_dir, 'SingleMuon2015D.root')
 data_f = ROOT.TFile(data_fn)
 
@@ -27,29 +29,8 @@ for sample in sig_samples:
     sample.fn = os.path.join(root_dir, sample.name + '.root')
     sample.f = ROOT.TFile(sample.fn)
 
-kinds = 'Mu04C0pf Mu04C0cl Mu04C0cl3'.split()
-ns = 'h_jet_pt_3 h_jet_pt_4 h_jet_pt_5 h_jet_eta_3 h_jet_eta_4 h_jet_eta_5'.split()
-
-kinds = ['Mu04C0pf', 'Mu04C0cl3', 'Mu14C0pf', 'Mu14C0cl3']
-ns = ['h_jet_pt_4', 'h_jet_pt_5', 'h_jet_ht']
-
-kinds = ['Mu1pf', 'Mu1cl3']
-ns = ['h_jet_pt_4', 'h_jet_pt_5', 'h_jet_ht']
-
-kinds = ['Mu14C0pf', 'Mu14C0cl3', 'Mu14C60pf', 'Mu14C60cl3']
-ns = ['h_jet_pt_4', 'h_jet_ht']
-
 kinds = ['']
-ns = ['h_jet_pt_4', 'h_jet_ht']
-
-#kinds = ['Mu1pf', 'Mu1cl3']
-#ns = ['h_jet_pt_4', 'h_jet_pt_5']
-
-#kinds = ['Mu1cl3']
-#ns = ['h_genjet_pt_3', 'h_genjet_pt_4', 'h_genjet_pt_5', 'h_genjet_pt_6']
-
-#kinds = ['Mu1pf']
-#ns = ['h_jet_pt_4']
+ns = ['h_jet_ht']
 
 def limits(kind, n):
     if 'ht' in n:
@@ -64,26 +45,17 @@ def limits(kind, n):
             return 60, 250
 
 def make_fcn(name, kind, n):
-    if '4C6' in kind or '4C5' in kind:
-        if 'ht' in n:
-            limits = (500, 2000)
-        else:
-            limits = (60, 250)
-        fcn = ROOT.TF1(name, 'pol0', *limits)
-        fcn.SetParNames('eff')
-        fcn.SetParameter(0, 0.99)
+    if 'ht' in n:
+        limits = (50, 2000)
     else:
-        if 'ht' in n:
-            limits = (50, 2000)
-        else:
-            limits = (20, 250)
-        fcn = ROOT.TF1(name, '[0] + [1]*(0.5 + 0.5 * TMath::Erf((x - [2])/[3]))', *limits)
-        fcn.SetParNames('floor', 'ceil', 'turnmu', 'turnsig')
-        if 'ht' in n:
-            fcn.SetParameters(0, 1, 500, 50)
-        else:
-            fcn.SetParameters(0, 1, 48, 5)
-        fcn.SetParLimits(1, 0, 1)
+        limits = (20, 250)
+    fcn = ROOT.TF1(name, '[0] + [1]*(0.5 + 0.5 * TMath::Erf((x - [2])/[3]))', *limits)
+    fcn.SetParNames('floor', 'ceil', 'turnmu', 'turnsig')
+    if 'ht' in n:
+        fcn.SetParameters(0, 1, 500, 50)
+    else:
+        fcn.SetParameters(0, 1, 48, 5)
+    fcn.SetParLimits(1, 0, 1)
     fcn.SetLineWidth(1)
     return fcn
 
@@ -166,34 +138,35 @@ for kind in kinds:
                 bkg_num.Add(num)
                 bkg_den.Add(den)
 
-        pars = [reses[0].GetParameterName(i) for i in xrange(reses[0].NPar())]
-        for ipar, parname in enumerate(pars):
-            subsubname = subname + '_' + parname
-            hpar = ROOT.TH1F(parname, '', n_bkg_samples, 0, n_bkg_samples)
-            for isam, sample in enumerate(bkg_samples):
-                if sample.name == 'qcdmupt15' and 'Mu1' in kind:
-                    continue
-                #print isam, reses[isam].Parameter(ipar), reses[isam].ParError(ipar)
-                hpar.SetBinContent(isam+1, reses[isam].Parameter(ipar))
-                hpar.SetBinError  (isam+1, reses[isam].ParError (ipar))
-                hpar.GetXaxis().SetBinLabel(isam+1, sample.name)
-            hpar.Draw('hist e')
-            hpar.SetLineWidth(2)
-            fcnpar = ROOT.TF1('f_'  + subsubname, 'pol0')
-            fcnpar.SetLineWidth(1)
-            hpar.Fit(fcnpar, 'Q')
-            ps.c.Update()
-            move_stat_box(hpar, (0.507, 0.664, 0.864, 0.855))
-            if parname == 'floor':
-                hpar.GetYaxis().SetRangeUser(0, 0.3)
-            elif parname == 'ceil':
-                hpar.GetYaxis().SetRangeUser(0.2, 1)
-                move_stat_box(hpar, (0.143, 0.147, 0.5, 0.339))
-            elif parname == 'turnmu':
-                hpar.GetYaxis().SetRangeUser(0, 100)
-            elif parname == 'turnsig':
-                hpar.GetYaxis().SetRangeUser(0, 30)
-            ps.save(subsubname)
+        if reses:
+            pars = [reses[0].GetParameterName(i) for i in xrange(reses[0].NPar())]
+            for ipar, parname in enumerate(pars):
+                subsubname = subname + '_' + parname
+                hpar = ROOT.TH1F(parname, '', n_bkg_samples, 0, n_bkg_samples)
+                for isam, sample in enumerate(bkg_samples):
+                    if sample.name == 'qcdmupt15' and 'Mu1' in kind:
+                        continue
+                    #print isam, reses[isam].Parameter(ipar), reses[isam].ParError(ipar)
+                    hpar.SetBinContent(isam+1, reses[isam].Parameter(ipar))
+                    hpar.SetBinError  (isam+1, reses[isam].ParError (ipar))
+                    hpar.GetXaxis().SetBinLabel(isam+1, sample.name)
+                hpar.Draw('hist e')
+                hpar.SetLineWidth(2)
+                fcnpar = ROOT.TF1('f_'  + subsubname, 'pol0')
+                fcnpar.SetLineWidth(1)
+                hpar.Fit(fcnpar, 'Q')
+                ps.c.Update()
+                move_stat_box(hpar, (0.507, 0.664, 0.864, 0.855))
+                if parname == 'floor':
+                    hpar.GetYaxis().SetRangeUser(0, 0.3)
+                elif parname == 'ceil':
+                    hpar.GetYaxis().SetRangeUser(0.2, 1)
+                    move_stat_box(hpar, (0.143, 0.147, 0.5, 0.339))
+                elif parname == 'turnmu':
+                    hpar.GetYaxis().SetRangeUser(0, 100)
+                elif parname == 'turnsig':
+                    hpar.GetYaxis().SetRangeUser(0, 30)
+                ps.save(subsubname)
 
         print 'sum bkgs:'
         print '   %10.2f %10.2f %10s %10s  %.6f [%.6f, %.6f]' % ((sum_scaled_nums, sum_scaled_dens, '', '') + clopper_pearson(sum_scaled_nums, sum_scaled_dens))
@@ -205,21 +178,24 @@ for kind in kinds:
         print 'data' #, data_num.GetEntries(), data_den.GetEntries()
         data_num, data_den = get(data_f, kind, n)
         ll = limits(kind, n)[0]
-        ib = num.FindBin(ll)
+        ib = data_num.FindBin(ll)
         ni = data_num.Integral(ib, 10000)
         di = data_den.Integral(ib, 10000)
         print '   %10i %10i %10s %10s  %.6f [%.6f, %.6f]' % ((ni, di, '', '') + clopper_pearson(ni, di))
         print '+- %10.2f %10.2f' % (ni**0.5, di**0.5)
-        print 'data/mc:'
-        print '   %10.4f %10.4f' % (ni/sum_scaled_nums, di/sum_scaled_dens)
-        ivnum = clopper_pearson_poisson_means(ni, sum_scaled_nums)
-        ivden = clopper_pearson_poisson_means(di, sum_scaled_dens)
-        print '+- %10.4f %10.4f' % ((ivnum[2] - ivnum[1])/2, (ivden[2] - ivden[1])/2)
+        if sum_scaled_nums and sum_scaled_dens:
+            print 'data/mc:'
+            print '   %10.4f %10.4f' % (ni/sum_scaled_nums, di/sum_scaled_dens)
+            ivnum = clopper_pearson_poisson_means(ni, sum_scaled_nums)
+            ivden = clopper_pearson_poisson_means(di, sum_scaled_dens)
+            print '+- %10.4f %10.4f' % ((ivnum[2] - ivnum[1])/2, (ivden[2] - ivden[1])/2)
 
         data_rat = histogram_divide(data_num, data_den)
-        bkg_rat = histogram_divide(bkg_num, bkg_den)
+        bkg_rat = histogram_divide(bkg_num, bkg_den) if bkg_num and bkg_den else None
 
         for r in (data_rat, bkg_rat):
+            if not r:
+                continue
             if 'pt' in n:
                 r.GetXaxis().SetLimits(0, 260)
                 i = int(n.split('_')[-1])
@@ -227,6 +203,7 @@ for kind in kinds:
                 r.SetTitle(';%ith %s jet p_{T} (GeV);efficiency' % (i, k))
             elif 'ht' in n:
                 r.GetXaxis().SetLimits(0, 2000)
+                k = ''
                 r.SetTitle(';%s #Sigma H_{T} (GeV);efficiency' % k)
             r.GetHistogram().SetMinimum(0)
             r.GetHistogram().SetMaximum(1.1)
@@ -235,22 +212,24 @@ for kind in kinds:
         ROOT.gStyle.SetOptFit(1111)
         data_fcn = make_fcn('f_data', kind, n)
         data_res = data_rat.Fit(data_fcn, 'RQS')
-        bkg_fcn = make_fcn('f_bkg', kind, n)
-        bkg_fcn.SetLineColor(2)
-        bkg_res = bkg_rat.Fit(bkg_fcn, 'RQS')
-        print '\nbkg:'
-        bkg_res.Print()
         print '\ndata:'
         data_res.Print()
+        if bkg_rat:
+            bkg_fcn = make_fcn('f_bkg', kind, n)
+            bkg_fcn.SetLineColor(2)
+            bkg_res = bkg_rat.Fit(bkg_fcn, 'RQS')
+            print '\nbkg:'
+            bkg_res.Print()
 
-        bkg_rat.SetFillStyle(3001)
-        bkg_rat.SetFillColor(2)
-        bkg_rat.Draw('E2 same')
+            bkg_rat.SetFillStyle(3001)
+            bkg_rat.SetFillColor(2)
+            bkg_rat.Draw('E2 same')
 
         ps.c.Update()
-        s = move_stat_box(bkg_rat,  (0.518, 0.350, 0.866, 0.560))
-        s.SetLineColor(2)
-        s.SetTextColor(2)
+        if bkg_rat:
+            s = move_stat_box(bkg_rat,  (0.518, 0.350, 0.866, 0.560))
+            s.SetLineColor(2)
+            s.SetTextColor(2)
         move_stat_box(data_rat, (0.518, 0.133, 0.866, 0.343))
 
         ps.save(subname)
