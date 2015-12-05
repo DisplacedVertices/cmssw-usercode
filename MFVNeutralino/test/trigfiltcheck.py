839502e458f614975ee194e921070025188812c9
Jordan Tucker <jordantucker@gmail.com>
2015-12-05 12:15:31 -0600

    Use SimpleTriggerEfficiency.setup_endpath; samples in submit script.

--- MFVNeutralino/test/trigfiltcheck.py
+++ MFVNeutralino/test/trigfiltcheck.py
@@ -2,28 +2,25 @@ import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import cms, process, file_event_from_argv
 
 file_event_from_argv(process)
-process.TFileService.fileName = 'simple_trigger_efficiency.root'
+process.TFileService.fileName = 'trigeff.root'
 
 from JMTucker.MFVNeutralino.TriggerFilter import setup_trigger_filter
 setup_trigger_filter(process, 'pmfvfilt')
 
-process.load('JMTucker.Tools.SimpleTriggerEfficiency_cfi')
-process.SimpleTriggerEfficiency.trigger_results_src = cms.InputTag('TriggerResults', '', process.name_())
-process.RandomNumberGeneratorService = cms.Service('RandomNumberGeneratorService')
-process.RandomNumberGeneratorService.SimpleTriggerEfficiency = cms.PSet(initialSeed = cms.untracked.uint32(1219))
+import JMTucker.Tools.SimpleTriggerEfficiency_cfi as SimpleTriggerEfficiency
+SimpleTriggerEfficiency.setup_endpath(process)
 
-process.ep = cms.EndPath(process.SimpleTriggerEfficiency)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
     from JMTucker.Tools.Sample import anon_samples
     import JMTucker.Tools.Samples as Samples
 
-    samples = Samples.ttbar_mgnlo_25ns_samples + Samples.qcd_ht_mg_25ns_samples + Samples.ttbar_mgnlo_50ns_samples + Samples.mfv_signal_samples
+    samples = Samples.ttbar_samples + Samples.qcd_samples + Samples.xx4j_samples + Samples.mfv_signal_samples
 
     cs = CRABSubmitter('TriggerFilterCheck',
                        splitting = 'EventAwareLumiBased',
-                       units_per_job = 10000,
+                       units_per_job = 20000,
                        total_units = 100000,
                        )
     cs.submit_all(samples)
