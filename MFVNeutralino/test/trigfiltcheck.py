3f99ff33c5e9c7de4232376098a727d024b99bc1
Jordan Tucker <jordantucker@gmail.com>
2015-12-30 07:48:00 -0600

    Check more restricted sets of paths.

--- MFVNeutralino/test/trigfiltcheck.py
+++ MFVNeutralino/test/trigfiltcheck.py
@@ -6,6 +6,29 @@ process.TFileService.fileName = 'trigeff.root'
 
 from JMTucker.MFVNeutralino.TriggerFilter import setup_trigger_filter
 setup_trigger_filter(process, 'pmfvfilt')
+setup_trigger_filter(process, 'ponlyhtpt', 'tfonlyhtpt')
+setup_trigger_filter(process, 'ponlyht', 'tfonlyht')
+setup_trigger_filter(process, 'ponlyht8', 'tfonlyht8')
+
+process.tfonlyhtpt.HLTPaths = [
+    'HLT_PFHT650_v*',
+    'HLT_PFHT800_v*',
+    'HLT_PFHT900_v*',
+    'HLT_PFHT550_4Jet_v*',
+    'HLT_PFHT450_SixJet40_v*',
+    'HLT_PFHT400_SixJet30_v*',
+    ]
+
+process.tfonlyht.HLTPaths = [
+    'HLT_PFHT650_v*',
+    'HLT_PFHT800_v*',
+    'HLT_PFHT900_v*',
+    ]
+
+process.tfonlyht8.HLTPaths = [
+    'HLT_PFHT800_v*',
+    'HLT_PFHT900_v*',
+    ]
 
 import JMTucker.Tools.SimpleTriggerEfficiency_cfi as SimpleTriggerEfficiency
 SimpleTriggerEfficiency.setup_endpath(process)
@@ -18,9 +41,9 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
 
     samples = Samples.ttbar_samples + Samples.qcd_samples + Samples.xx4j_samples + Samples.mfv_signal_samples
 
-    cs = CRABSubmitter('TriggerFilterCheck',
+    cs = CRABSubmitter('TriggerFilterCheckv2',
                        splitting = 'EventAwareLumiBased',
-                       units_per_job = 20000,
-                       total_units = 100000,
+                       units_per_job = 50000,
+                       total_units = -1,
                        )
     cs.submit_all(samples)
