434e05b6178ea8d73c3b98172d4cfca59780c397
Jordan Tucker <jordantucker@gmail.com>
2015-12-31 15:49:48 -0600

    setup_trigger_filter -> setup_trigger_filter_soup; setup_trigger_filter now just uses MFVEmulateHT800.

--- MFVNeutralino/python/TriggerFilter.py
+++ MFVNeutralino/python/TriggerFilter.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-def setup_trigger_filter(process, path_name='pevtsel', filt_name='triggerFilter'):
+def setup_trigger_filter_soup(process, path_name='pevtsel', filt_name='triggerFilter'):
     from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
     triggerFilter = hltHighLevel.clone()
     setattr(process, filt_name, triggerFilter)
@@ -28,5 +28,20 @@ def setup_trigger_filter(process, path_name='pevtsel', filt_name='triggerFilter'
     triggerFilter.andOr = True # = OR
     triggerFilter.throw = False # = HT800 not in MC
     setattr(process, path_name, cms.Path(triggerFilter))
+    if hasattr(process, 'out'):
+        process.out.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring(path_name))
+
+def setup_trigger_filter(process, path_name='pevtsel', filt_name='emuht800', need_pat=False):
+    from JMTucker.MFVNeutralino.EmulateHT800_cfi import emu
+    emu = emu.clone()
+    setattr(process, filt_name, emu)
+    path = cms.Path(emu)
+    setattr(process, path_name, path)
+
+    if need_pat:
+        process.load('PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cfi')
+        process.load('PhysicsTools.PatAlgos.slimming/selectedPatTrigger_cfi')
+        path.insert(0, process.patTrigger * process.selectedPatTrigger)
+
     if hasattr(process, 'out'):
         process.out.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring(path_name))
