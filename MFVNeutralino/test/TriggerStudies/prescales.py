be85c6781873da2010e7fe6ae1ee5b5a7a8e5486
Jordan Tucker <jordantucker@gmail.com>
2016-01-01 18:29:48 -0600

    submit script.

--- MFVNeutralino/test/TriggerStudies/prescales.py
+++ MFVNeutralino/test/TriggerStudies/prescales.py
@@ -14,11 +14,10 @@ process.maxEvents.input = -1
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
-
-    from JMTucker.Tools.CRABSubmitter import CRABSubmitter
-    cs = CRABSubmitter('QuadJetTrigPrescales',
-                       total_number_of_lumis = -1,
-                       lumis_per_job = 500,
-                       skip_common = True,
+    from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
+    cs = CRABSubmitter('TriggerPrescalesV0',
+                       splitting = 'FileBased',
+                       units_per_job = 20,
+                       total_units = -1,
                        )
-    cs.submit_all(Samples.data_samples_orig + Samples.auxiliary_data_samples)
+    cs.submit_all(Samples.data_samples)
