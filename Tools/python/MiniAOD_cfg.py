b4d2552b8cbbc534804c90cc6eec1a80c9975d48
Jordan Tucker <jordantucker@gmail.com>
2016-01-06 01:19:14 -0600

    Silence NoModule warning

--- Tools/python/MiniAOD_cfg.py
+++ Tools/python/MiniAOD_cfg.py
@@ -9,8 +9,8 @@ def pat_tuple_process(customize_before_unscheduled, is_mc):
 
     report_every(process, 1000000)
     registration_warnings(process)
-    print 'suppressing MatchedJetsFarApart, HLTConfigData warnings'
-    silence_messages(process, ['MatchedJetsFarApart', 'HLTConfigData'])
+    print 'suppressing MatchedJetsFarApart, HLTConfigData, NoModule warnings'
+    silence_messages(process, ['MatchedJetsFarApart', 'HLTConfigData', 'NoModule'])
 
     process.load('Configuration.StandardSequences.Services_cff')
     process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
