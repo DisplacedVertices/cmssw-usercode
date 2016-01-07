f020589702556e2a98ff8e7bd6dd9b60fe37d50c
Jordan Tucker <jordantucker@gmail.com>
2016-01-06 19:44:56 -0600

    Drop "Only" and irrelevant nm1s; follow changes to Histos_cff; submit script.

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -16,23 +16,18 @@ SimpleTriggerResults.setup_endpath(process, weight_src='mfvWeight')
 nm1s = [
     ('Ntracks', 'min_ntracks = 0, min_njetsntks = 0'),
     ('Drmin',   'max_drmin = 1e9'),
-    ('Drmax',   'max_drmax = 1e9'),
-    ('Mindrmax','min_drmax = 0'),
+    ('Geo2d',   'max_geo2ddist = 1e9'),
     ('Bs2derr', 'max_bs2derr = 1e9'),
     ('Njets',   'min_njetsntks = 0'),
-    ('Ntracksptgt3', 'min_ntracksptgt3 = 0'),
-    ('Sumnhitsbehind', 'max_sumnhitsbehind = 1000000'),
-    ('ButNtracksAndGt3', 'max_drmin = 1e9, max_drmax = 1e9, min_drmax = 0, max_bs2derr = 1e9, min_njetsntks = 0'),
     ]
 
 if simple:
     nm1s = []
 
-    del process.pOneVtx
     del process.pFullSel
     del process.pOnlyOneVtx
     del process.pPreSel
-    del process.pTrigSel
+    del process.pSkimSel
     del process.pSigReg
 
     process.p = cms.Path(process.mfvSelectedVerticesLoose +
@@ -53,29 +48,24 @@ for name, cut in nm1s:
     vtx = eval('process.mfvSelectedVerticesTight.clone(%s)' % cut)
     vtx_name = 'vtxNo' + name
 
-    for only in ('', 'Only'):
-        for nv in (1,2):
-            if nv == 2 and only == 'Only':
-                continue
+    for nv in (1,2):
+        ana = eval('process.mfvAnalysisCuts.clone(%s)' % evt_cut)
+        ana.vertex_src = vtx_name
+        ana.max_nvertex = nv
+        ana.min_nvertex = nv
+        ana_name = 'ana%iVNo' % nv + name
 
-            ana = eval('process.mfvAnalysisCuts.clone(%s)' % evt_cut)
-            ana.vertex_src = vtx_name
-            if only == 'Only':
-                ana.max_nvertex = nv
-            ana.min_nvertex = nv
-            ana_name = 'ana%s%iVNo' % (only, nv) + name
+        evt_hst = process.mfvEventHistos.clone()
+        evt_hst_name = 'evtHst%iVNo' % nv + name
 
-            evt_hst = process.mfvEventHistos.clone()
-            evt_hst_name = 'evtHst%s%iVNo' % (only, nv) + name
+        vtx_hst = process.mfvVertexHistos.clone(vertex_aux_src = vtx_name)
+        vtx_hst_name = 'vtxHst%iVNo' % nv + name
 
-            vtx_hst = process.mfvVertexHistos.clone(vertex_aux_src = vtx_name)
-            vtx_hst_name = 'vtxHst%s%iVNo' % (only, nv) + name
-
-            setattr(process, vtx_name, vtx)
-            setattr(process, ana_name, ana)
-            setattr(process, evt_hst_name, evt_hst)
-            setattr(process, vtx_hst_name, vtx_hst)
-            setattr(process, 'p%s%iV' % (only, nv) + name, cms.Path(vtx * ana * evt_hst * vtx_hst))
+        setattr(process, vtx_name, vtx)
+        setattr(process, ana_name, ana)
+        setattr(process, evt_hst_name, evt_hst)
+        setattr(process, vtx_hst_name, vtx_hst)
+        setattr(process, 'p%iV' % nv + name, cms.Path(vtx * ana * evt_hst * vtx_hst))
 
 def force_bs(process, bs):
     for ana in process.analyzers:
@@ -83,15 +73,19 @@ def force_bs(process, bs):
             ana.force_bs = bs
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
-    from JMTucker.Tools.Sample import anon_samples
-    samples = anon_samples('''
-''', dbs_inst='phys03')
-
     from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
-    cs = CRABSubmitter('HistosV2',
+    import JMTucker.Tools.Samples as Samples 
+
+    samples = Samples.data_samples + \
+        Samples.ttbar_samples + Samples.qcd_samples + \
+        [Samples.mfv_neu_tau00100um_M0800, Samples.mfv_neu_tau00300um_M0800, Samples.mfv_neu_tau01000um_M0800, Samples.mfv_neu_tau10000um_M0800] + \
+        Samples.xx4j_samples
+
+    cs = CRABSubmitter('HistosV5',
+                       dataset = 'ntuplev5',
                        splitting = 'EventAwareLumiBased',
                        units_per_job = 50000,
                        total_units = -1,
-                       aaa = True,
+                       aaa = True, # stored at FNAL, easy to run on T2_USes
                        )
     cs.submit_all(samples)
