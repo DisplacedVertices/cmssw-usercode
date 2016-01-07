095fddd7d52e22cffd46d752be7681c6bbe5e631
Jordan Tucker <jordantucker@gmail.com>
2016-01-06 20:52:41 -0600

    Dump the contents of mfvWeight/h_sums, and compare to samples' nevents_orig.

---
+++ MFVNeutralino/test/dumpsums.py
@@ -0,0 +1,21 @@
+import sys, os
+from JMTucker.Tools.ROOTTools import ROOT
+import JMTucker.Tools.Samples as Samples
+
+for i, fn in enumerate(sys.argv[1:]):
+    if not fn.endswith('.root') or not os.path.isfile(fn):
+        continue
+    f = ROOT.TFile(fn)
+    h = f.Get('mfvWeight/h_sums')
+
+    sn = fn.replace('.root', '')
+    print sn
+    s = getattr(Samples, sn)
+    print 'sample nevents_orig:', s.nevents_orig
+    for i in xrange(1, h.GetNbinsX()+1):
+        print '%2i %30s %20.1f' % (i, h.GetXaxis().GetBinLabel(i), h.GetBinContent(i)),
+        if i == 1:
+            print '  %.3f' % (h.GetBinContent(i) / s.nevents_orig)
+        else:
+            print
+    print
