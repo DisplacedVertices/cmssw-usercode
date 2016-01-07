6739df55dca7af470ea19c0544eec51212390d64
Jordan Tucker <jordantucker@gmail.com>
2016-01-06 19:16:31 -0600

    Drop ABCDHistos, OneVtx (leave OnlyOneVtx); 'pTrigSel' -> 'pSkimSel' (not yet daring to rename any of the actual modules)

--- MFVNeutralino/python/Histos_cff.py
+++ MFVNeutralino/python/Histos_cff.py
@@ -7,36 +7,29 @@ mfvCommon = cms.Sequence(mfvSelectedVerticesSeq * mfvWeight)
 
 from JMTucker.MFVNeutralino.VertexHistos_cfi import *
 from JMTucker.MFVNeutralino.EventHistos_cfi import *
-from JMTucker.MFVNeutralino.ABCDHistos_cfi import *
 from JMTucker.MFVNeutralino.AnalysisCuts_cfi import *
 
 from JMTucker.Tools.SimpleTriggerEfficiency_cfi import *
 
 mfvEventHistosNoCuts = mfvEventHistos.clone()
 mfvVertexHistosNoCuts = mfvVertexHistos.clone(vertex_aux_src = 'mfvSelectedVerticesLoose')
-pTrigSel = cms.Path(mfvCommon * mfvEventHistosNoCuts * mfvVertexHistos * mfvVertexHistosNoCuts)
+pSkimSel = cms.Path(mfvCommon * mfvEventHistosNoCuts * mfvVertexHistos * mfvVertexHistosNoCuts)
 
 mfvAnalysisCutsPreSel = mfvAnalysisCuts.clone(apply_vertex_cuts = False)
 mfvEventHistosPreSel = mfvEventHistos.clone()
 mfvVertexHistosPreSel = mfvVertexHistos.clone()
 pPreSel = cms.Path(mfvCommon * mfvAnalysisCutsPreSel * mfvEventHistosPreSel * mfvVertexHistosPreSel)
 
-mfvAnalysisCutsOneVtx = mfvAnalysisCuts.clone(min_nvertex = 1)
-mfvEventHistosOneVtx = mfvEventHistos.clone()
-mfvVertexHistosOneVtx = mfvVertexHistos.clone()
-pOneVtx = cms.Path(mfvCommon * mfvAnalysisCutsOneVtx * mfvEventHistosOneVtx * mfvVertexHistosOneVtx)
-
 mfvAnalysisCutsOnlyOneVtx = mfvAnalysisCuts.clone(min_nvertex = 1, max_nvertex = 1)
 mfvEventHistosOnlyOneVtx = mfvEventHistos.clone()
 mfvVertexHistosOnlyOneVtx = mfvVertexHistos.clone()
 pOnlyOneVtx = cms.Path(mfvCommon * mfvAnalysisCutsOnlyOneVtx * mfvEventHistosOnlyOneVtx * mfvVertexHistosOnlyOneVtx)
 
-mfvVertexHistosNoCutsWAnaCuts = mfvVertexHistosNoCuts.clone()
+mfvVertexHistosNoCutsWAnaCuts = mfvVertexHistos.clone(vertex_aux_src = 'mfvSelectedVerticesLoose')
 mfvVertexHistosWAnaCuts = mfvVertexHistos.clone()
-pFullSel = cms.Path(mfvCommon * mfvAnalysisCuts * mfvEventHistos * mfvVertexHistosNoCutsWAnaCuts * mfvVertexHistosWAnaCuts * mfvAbcdHistosSeq)
+pFullSel = cms.Path(mfvCommon * mfvAnalysisCuts * mfvEventHistos * mfvVertexHistosNoCutsWAnaCuts * mfvVertexHistosWAnaCuts)
 
 mfvAnalysisCutsSigReg = mfvAnalysisCuts.clone(min_svdist2d = 0.04)
 mfvEventHistosSigReg = mfvEventHistos.clone()
 mfvVertexHistosSigReg = mfvVertexHistos.clone()
 pSigReg = cms.Path(mfvCommon * mfvAnalysisCutsSigReg * mfvEventHistosSigReg * mfvVertexHistosSigReg)
-
