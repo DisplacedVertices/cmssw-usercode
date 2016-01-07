fb234ff0aa82f560a8ac8820d7c8929b01c61f41
Jordan Tucker <jordantucker@gmail.com>
2016-01-07 13:43:34 -0600

    Use from_argv

--- MFVNeutralino/test/merge.py
+++ MFVNeutralino/test/merge.py
@@ -7,12 +7,12 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
     import JMTucker.Tools.Samples as Samples 
 
-    samples = Samples.data_samples + \
+    samples = Samples.registry.from_argv(
+        Samples.data_samples + \
         Samples.ttbar_samples + Samples.qcd_samples + \
         [Samples.mfv_neu_tau00100um_M0800, Samples.mfv_neu_tau00300um_M0800, Samples.mfv_neu_tau01000um_M0800, Samples.mfv_neu_tau10000um_M0800] + \
         Samples.xx4j_samples
-
-    samples = [Samples.qcdht2000]
+        )
 
     cs = CRABSubmitter('MergeNtupleV5',
                        dataset = 'ntuplev5',
