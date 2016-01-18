93412b9567955f4e6f0d7e423c7f2fa8f65220ef
Jordan Tucker <jordantucker@gmail.com>
2016-01-18 15:06:08 -0600

    v6p1

--- MFVNeutralino/test/ntuple.py
+++ MFVNeutralino/test/ntuple.py
@@ -9,7 +9,7 @@ prepare_vis = False
 keep_all = prepare_vis
 trig_filter = not keep_all
 event_filter = False # not keep_all
-version = 'v6'
+version = 'v6p1'
 batch_name = 'Ntuple' + version.upper()
 #batch_name += '_ChangeMeIfSettingsNotDefault'
 
