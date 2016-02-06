6dfdd974455c61d6165d5d1f9c9021393560aa2a
Jordan Tucker <jordantucker@gmail.com>
2016-02-05 18:16:13 -0600

    RunIISummer15GS minbias sample

--- MFVNeutralino/test/MakeSamples/minbias.py
+++ MFVNeutralino/test/MakeSamples/minbias.py
@@ -7,6 +7,6 @@ except ImportError:
     print 'nope, getting minbias file list from DBS and caching'
     from JMTucker.Tools.DBS import files_in_dataset
     from JMTucker.Tools.general import to_pickle
-    files = files_in_dataset('/MinBias_TuneCUETP8M1_13TeV-pythia8/RunIIWinter15GS-MCRUN2_71_V1-v1/GEN-SIM')
+    files = files_in_dataset('/MinBias_TuneCUETP8M1_13TeV-pythia8/RunIISummer15GS-MCRUN2_71_V1-v2/GEN-SIM')
     to_pickle(files, 'minbias_files.pkl', comp=True)
     open('minbias_files.py', 'wt').write("import cPickle, gzip; files = cPickle.load(gzip.GzipFile('minbias_files.pkl', 'rb'))\n")
