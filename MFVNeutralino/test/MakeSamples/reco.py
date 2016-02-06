c8dbc52b767ec76941163981f950ab7ebba4c712
Jordan Tucker <jordantucker@gmail.com>
2016-02-05 18:22:48 -0600

    76X RECO.

--- MFVNeutralino/test/MakeSamples/reco.py
+++ MFVNeutralino/test/MakeSamples/reco.py
@@ -1,12 +1,9 @@
-# from configs in dbs for /QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/AODSIM
-
-is_25ns = True # False for 50 ns
-
-########################################################################
+# from configs in dbs for /QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall15DR76-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/AODSIM
 
 import sys, FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.Eras import eras
 
-process = cms.Process('RECO')
+process = cms.Process('RECO', eras.Run2_25ns)
 
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
@@ -14,16 +11,19 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mixNoPU_cfi')
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.RawToDigi_cff')
 process.load('Configuration.StandardSequences.L1Reco_cff')
 process.load('Configuration.StandardSequences.Reconstruction_cff')
 process.load('CommonTools.ParticleFlow.EITopPAG_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('file:hlt.root'))
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5))
-process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True),
+    allowUnscheduled = cms.untracked.bool(True),
+    )
 
 if not 'debug' in sys.argv:
     process.options.wantSummary = False
@@ -40,11 +40,8 @@ process.AODSIMoutput = cms.OutputModule("PoolOutputModule",
                                         outputCommands = process.AODSIMEventContent.outputCommands,
                                         )
 
-from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-tag = 'MCRUN2_74_V9'
-if not is_25ns:
-    tag += 'A'
-process.GlobalTag = GlobalTag(process.GlobalTag, tag, '')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '76X_mcRun2_asymptotic_v12', '')
 
 process.raw2digi_step = cms.Path(process.RawToDigi)
 process.L1Reco_step = cms.Path(process.L1Reco)
@@ -54,12 +51,9 @@ process.AODSIMoutput_step = cms.EndPath(process.AODSIMoutput)
 
 process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.eventinterpretaion_step,process.AODSIMoutput_step)
 
-if is_25ns:
-    from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
-    process = customisePostLS1(process)
-else:
-    from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1_50ns
-    process = customisePostLS1_50ns(process)
+from FWCore.ParameterSet.Utilities import convertToUnscheduled, cleanUnscheduled
+process = cleanUnscheduled(convertToUnscheduled(process))
+
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.Sample import anon_samples
