bd9e3480fe9e82a07a84e57ca488bf43e21750fa
Jordan Tucker <jordantucker@gmail.com>
2016-02-08 11:40:42 -0600

    xx4j_samples mostly there

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -81,9 +81,9 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     samples = Samples.registry.from_argv(
         Samples.data_samples + \
         Samples.ttbar_samples + Samples.qcd_samples + \
-        Samples.qcdpt_samples # + \
+        Samples.qcdpt_samples + \
 #        [Samples.mfv_neu_tau00100um_M0800, Samples.mfv_neu_tau00300um_M0800, Samples.mfv_neu_tau01000um_M0800, Samples.mfv_neu_tau10000um_M0800] + \
-#        Samples.xx4j_samples
+        Samples.xx4j_samples
         )
 
     for sample in samples:
