690d8fef3e2a31c9578742a1af3b4df30c32d958
Dan Quach <quach.dan@gmail.com>
2016-02-11 13:35:41 -0600

    Added {min,max,maxpx} x {r,z} hists

--- Tools/test/TrackingTreer/hists.cc
+++ Tools/test/TrackingTreer/hists.cc
@@ -35,7 +35,7 @@ int main(int argc, char** argv) {
 
   bool z_slice = false;
   bool nhits_slice = false;
-  bool pt_slice = true;
+  bool pt_slice = false;
 
   const double z_slice_bounds[2] = { -7.5, -2.5 };
   const double nhits_slice_bounds[2] = { 0, 3 };
@@ -48,7 +48,9 @@ int main(int argc, char** argv) {
 
   //  const double pileup_weights[40] = { 99.15056679507543, 172.99847243562277, 121.89056539480684, 36.648532815731905, 15.130284226820047, 3.388892702882986, 1.986252356518944, 2.553598893553273, 3.391463356559057, 3.329138633709162, 3.078061196234216, 2.7655190007638786, 2.183863508534957, 1.4521096234256572, 0.8040188219328743, 0.3713065789043111, 0.15067662959667333, 0.061442166758053925, 0.029481930242311164, 0.015649765632974884, 0.007675085464470436, 0.003212647741405869, 0.0011654706818925956, 0.0003947719988503597, 0.00013783918071098118, 5.240303150410027e-05, 2.121092370685294e-05, 8.720595168624445e-06, 3.5144188113130866e-06, 1.3327213104566867e-06, 4.428988947779108e-07, 1.1797413268628033e-07, 2.422301652608736e-08, 3.9984662880098295e-09, 5.655318026602206e-10, 7.135693975440923e-11, 8.180332652856529e-12, 8.590503460873983e-13, 8.181362972336421e-14, 7.046437100909094e-15 };
 
-  const double pileup_weights[50] = { 0.0, 0.0, 59.337367298506194, 24.8077111479546, 30.171542595767974, 1.6463976191583929, 1.3492771650541147, 1.9978387363280743, 2.4586851723072107, 2.4908183520463014, 2.4258366625729764, 2.3274230208939786, 2.0002901774528077, 1.5744241110155093, 1.1681251209776078, 0.7736292323891238, 0.4714770290840279, 0.2676959790610609, 0.15524650927133532, 0.10132520917928384, 0.08472177641430646, 0.07927574663947269, 0.08390290731362979, 0.09750091335949468, 0.12058205934514545, 0.16519259956459129, 0.1980959867482783, 0.24105092268610864, 0.4611856198417187, 0.5947794446993874, 1.029106792358867, 0.8974099002954592, 0.7882654444004721, 0.8380983397875282, 0.3218297919915704, 0.7542885512112272, 0.13871973570601948, 0.12068616474074438, 0.11174645158850818, 0.12571475930401357, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }; // For MC 74 vs MC 76 comparison
+  //  const double pileup_weights[50] = { 0.0, 0.0, 59.337367298506194, 24.8077111479546, 30.171542595767974, 1.6463976191583929, 1.3492771650541147, 1.9978387363280743, 2.4586851723072107, 2.4908183520463014, 2.4258366625729764, 2.3274230208939786, 2.0002901774528077, 1.5744241110155093, 1.1681251209776078, 0.7736292323891238, 0.4714770290840279, 0.2676959790610609, 0.15524650927133532, 0.10132520917928384, 0.08472177641430646, 0.07927574663947269, 0.08390290731362979, 0.09750091335949468, 0.12058205934514545, 0.16519259956459129, 0.1980959867482783, 0.24105092268610864, 0.4611856198417187, 0.5947794446993874, 1.029106792358867, 0.8974099002954592, 0.7882654444004721, 0.8380983397875282, 0.3218297919915704, 0.7542885512112272, 0.13871973570601948, 0.12068616474074438, 0.11174645158850818, 0.12571475930401357, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }; // For MC 74 vs MC 76 comparison
+
+  const double pileup_weights[40] = { 0.4430900689834877, 0.7774946230174374, 1.211532146615326, 1.5555095481485082, 1.3656299694579321, 1.9513380206283242, 1.4311409855780286, 1.2555573073998676, 1.3509572272968273, 1.3430021765269047, 1.2671533450149761, 1.1830775671347276, 1.0767805180896135, 0.9110431461207203, 0.6971726104245397, 0.48543658081115554, 0.32209366814732293, 0.22848641867558733, 0.19027360131827362, 0.15527725489164057, 0.09487219082044276, 0.04095796584834362, 0.013672759117602781, 0.003987335129679708, 0.0011389782126837726, 0.00033784224878244623, 0.00010191256127555789, 2.9928259966798484e-05, 8.357594204838038e-06, 2.2051905560049044e-06, 5.493198439639333e-07, 1.2915574040259908e-07, 2.8638852677561204e-08, 5.978815170388724e-09, 1.1718664799704302e-09, 2.147556635829967e-10, 3.658973660525496e-11, 5.759196292572524e-12, 8.200005561089732e-13, 1.0437472148786202e-13 }; // 76 weights
 
   TFile* f_out = new TFile(out_fn, "recreate");
 
@@ -94,7 +96,12 @@ int main(int argc, char** argv) {
   TH1F* h_all_track_nstlays = new TH1F("h_all_track_nstlays", ";all track number strip layers", 20, 0, 20);
   TH1F* h_all_track_sigmadxybs = new TH1F("h_all_track_sigmadxybs", ";all track sigmadxybs", 1000, -10, 10);
   TH1F* h_all_track_charge = new TH1F("h_all_track_charge", ";all track charge", 4, -2, 2);
-  //  TH1F* h_all_track_min_r = new TH1F("h_all_track_min_r", ";all track min r", 200, 0, 20);
+  TH1F* h_all_track_min_r = new TH1F("h_all_track_min_r", ";all track min r", 16, 0, 16);
+  TH1F* h_all_track_min_z = new TH1F("h_all_track_min_z", ";all track min z", 16, 0, 16);
+  TH1F* h_all_track_max_r = new TH1F("h_all_track_max_r", ";all track max r", 16, 0, 16);
+  TH1F* h_all_track_max_z = new TH1F("h_all_track_max_z", ";all track max z", 16, 0, 16);
+  TH1F* h_all_track_maxpx_r = new TH1F("h_all_track_maxpx_r", ";all track maxpx r", 16, 0, 16);
+  TH1F* h_all_track_maxpx_z = new TH1F("h_all_track_maxpx_z", ";all track maxpx z", 16, 0, 16);
   for (int i = 0; i < 9; ++i) { 
     if (i == 2)
       h_all_track_dxybs_v_pars[0][i] = new TH2F(TString::Format("h_all_track_dxybs_v_%s", par_names[i]), TString::Format(";all track %s;dxybs", par_names[i]), par_nbins[i], par_lo[i], par_hi[i], 4000, par_lo[3], par_hi[3]);
@@ -121,7 +128,12 @@ int main(int argc, char** argv) {
   TH1F* h_seed_track_nstlays = new TH1F("h_seed_track_nstlays", ";seed track number strip layers", 20, 0, 20);
   TH1F* h_seed_track_sigmadxybs = new TH1F("h_seed_track_sigmadxybs", ";seed track sigmadxybs", 200, -10, 10);
   TH1F* h_seed_track_charge = new TH1F("h_seed_track_charge", ";seed track charge", 4, -2, 2);
-  //  TH1F* h_seed_track_min_r = new TH1F("h_seed_track_min_r", ";seed track min r", 200, 0, 20);
+  TH1F* h_seed_track_min_r = new TH1F("h_seed_track_min_r", ";seed track min r", 16, 0, 20);
+  TH1F* h_seed_track_min_z = new TH1F("h_seed_track_min_z", ";seed track min z", 16, 0, 16);
+  TH1F* h_seed_track_max_r = new TH1F("h_seed_track_max_r", ";seed track max r", 16, 0, 16);
+  TH1F* h_seed_track_max_z = new TH1F("h_seed_track_max_z", ";seed track max z", 16, 0, 16);
+  TH1F* h_seed_track_maxpx_r = new TH1F("h_seed_track_maxpx_r", ";seed track maxpx r", 16, 0, 16);
+  TH1F* h_seed_track_maxpx_z = new TH1F("h_seed_track_maxpx_z", ";seed track maxpx z", 16, 0, 16);
   for (int i = 0; i < 9; ++i) {
     if (i == 2)
       h_seed_track_dxybs_v_pars[0][i] = new TH2F(TString::Format("h_seed_track_dxybs_v_%s", par_names[i]), TString::Format(";seed track %s;dxybs", par_names[i]), par_nbins[i], par_lo[i], par_hi[i], 4000, par_lo[3], par_hi[3]);
@@ -148,7 +160,12 @@ int main(int argc, char** argv) {
   TH1F* h_seed_nosigcut_track_nstlays = new TH1F("h_seed_nosigcut_track_nstlays", ";seed nosig track number strip layers", 20, 0, 20);
   TH1F* h_seed_nosigcut_track_sigmadxybs = new TH1F("h_seed_nosigcut_track_sigmadxybs", ";seed nosig track sigmadxybs", 1000, -10, 10);
   TH1F* h_seed_nosigcut_track_charge = new TH1F("h_seed_nosigcut_track_charge", ";seed nosig track charge", 4, -2, 2);
-  //  TH1F* h_seed_nosigcut_track_min_r = new TH1F("h_seed_nosigcut_track_min_r", ";seed nosig track min r", 200, 0, 20);
+  TH1F* h_seed_nosigcut_track_min_r = new TH1F("h_seed_nosigcut_track_min_r", ";seed nosig track min r", 16, 0, 20);
+  TH1F* h_seed_nosigcut_track_min_z = new TH1F("h_seed_nosigcut_track_min_z", ";seed nosig track min z", 16, 0, 16);
+  TH1F* h_seed_nosigcut_track_max_r = new TH1F("h_seed_nosigcut_track_max_r", ";seed nosig track max r", 16, 0, 16);
+  TH1F* h_seed_nosigcut_track_max_z = new TH1F("h_seed_nosigcut_track_max_z", ";seed nosig track max z", 16, 0, 16);
+  TH1F* h_seed_nosigcut_track_maxpx_r = new TH1F("h_seed_nosigcut_track_maxpx_r", ";seed nosig track maxpx r", 16, 0, 16);
+  TH1F* h_seed_nosigcut_track_maxpx_z = new TH1F("h_seed_nosigcut_track_maxpx_z", ";seed nosig track maxpx z", 16, 0, 16);
   for (int i = 0; i < 9; ++i) {
     if (i == 2) 
       h_seed_nosigcut_track_dxybs_v_pars[0][i] = new TH2F(TString::Format("h_seed_nosigcut_track_dxybs_v_%s", par_names[i]), TString::Format(";seed nosig track %s;dxybs", par_names[i]), par_nbins[i], par_lo[i], par_hi[i], 4000, par_lo[3], par_hi[3]);
@@ -190,8 +207,8 @@ int main(int argc, char** argv) {
       int n_seed_nosigcut_tracks = 0;
       
       double weight;
-      if (inpu >= 50)
-	weight = pileup_weights[49];
+      if (inpu >= 40)
+	weight = pileup_weights[39];
       else
 	weight = pileup_weights[inpu];
       
@@ -231,7 +248,12 @@ int main(int argc, char** argv) {
 	const int nstlays = nt.p_tk_nstlay->at(itk);
 	const double dz = nt.p_tk_dz->at(itk);
 	const double dzpv = nt.p_tk_dzpv->at(itk);
-	//	const int min_r = nt.tk_min_r(itk);
+	const int min_r = nt.tk_min_r(itk);
+	const int min_z = nt.tk_min_z(itk);
+	const int max_r = nt.tk_max_r(itk);
+	const int max_z = nt.tk_max_z(itk);
+	const int maxpx_r = nt.tk_maxpx_r(itk);
+	const int maxpx_z = nt.tk_maxpx_z(itk);
 
 	int charge;
 	if (nt.p_tk_qpt->at(itk) > 0)
@@ -271,7 +293,12 @@ int main(int argc, char** argv) {
 	    h_seed_track_npxlays->Fill(npxlays, w);
 	    h_seed_track_nstlays->Fill(nstlays, w);
 	    h_seed_track_charge->Fill(charge);
-	    //	    h_seed_track_min_r->Fill(min_r);
+	    h_seed_track_min_r->Fill(min_r);
+	    h_seed_track_min_z->Fill(min_z);
+	    h_seed_track_max_r->Fill(max_r);
+	    h_seed_track_max_z->Fill(max_z);
+	    h_seed_track_maxpx_r->Fill(maxpx_r);
+	    h_seed_track_maxpx_z->Fill(maxpx_z);
 	  }
 
 	  if (use_nosigcut) {
@@ -284,7 +311,12 @@ int main(int argc, char** argv) {
 	    h_seed_nosigcut_track_npxlays->Fill(npxlays, w);
 	    h_seed_nosigcut_track_nstlays->Fill(nstlays, w);
 	    h_seed_nosigcut_track_charge->Fill(charge);
-	    //	    h_seed_nosigcut_track_min_r->Fill(min_r);
+	    h_seed_nosigcut_track_min_r->Fill(min_r);
+	    h_seed_nosigcut_track_min_z->Fill(min_z);
+	    h_seed_nosigcut_track_max_r->Fill(max_r);
+	    h_seed_nosigcut_track_max_z->Fill(max_z);
+	    h_seed_nosigcut_track_maxpx_r->Fill(maxpx_r);
+	    h_seed_nosigcut_track_maxpx_z->Fill(maxpx_z);	    
 	  }
 
 	  for (int i = 0; i < 9; ++i) {
@@ -319,7 +351,12 @@ int main(int argc, char** argv) {
 	  h_all_track_npxlays->Fill(npxlays, w);
 	  h_all_track_nstlays->Fill(nstlays, w);
 	  h_all_track_charge->Fill(charge);
-	  //	  h_all_track_min_r->Fill(min_r);
+	  h_all_track_min_r->Fill(min_r);
+	  h_all_track_min_z->Fill(min_z);
+	  h_all_track_max_r->Fill(max_r);
+	  h_all_track_max_z->Fill(max_z);
+	  h_all_track_maxpx_r->Fill(maxpx_r);
+	  h_all_track_maxpx_z->Fill(maxpx_z);
 	}
       }
 
