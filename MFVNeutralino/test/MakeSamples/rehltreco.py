8b0923a2f1d7fe7e47591e76a22cc76e9aa0885a
Jordan Tucker <jordantucker@gmail.com>
2016-02-18 08:11:03 -0600

    FileBased, about to test whether scriptExe uses hash of scriptExe instead of pset...

--- MFVNeutralino/test/MakeSamples/rehltreco.py
+++ MFVNeutralino/test/MakeSamples/rehltreco.py
@@ -3,16 +3,18 @@ from JMTucker.Tools.Samples import mfv_signal_samples
 samples = mfv_signal_samples[:1]
 
 from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
-cs = CRABSubmitter('mfv_run2_76x_rawhltreco',
+
+cs = CRABSubmitter('mfv_run2_76x_rehltreco_t1',
                    pset_template_fn = 'rawhlt.py',
                    dataset = 'sim',
-                   splitting = 'EventAwareLumiBased',
-                   units_per_job = 100,
-                   total_units = 200,
+                   splitting = 'FileBased',
+                   units_per_job = 1,
+                   total_units = 2,
                    aaa = True,
                    publish_name = '76reco_10k',
                    input_files = ['reco.py', 'merge_fjrs.py'],
                    output_files = 'reco.root',
                    script_exe = 'rehltreco.sh',
                    )
+
 cs.submit_all(samples)
