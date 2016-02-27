98b7049a1b0796555772367acbd8e9c4b4fe1fc2
Jordan Tucker <jordantucker@gmail.com>
2016-02-26 21:02:37 -0600

    Does this finally work

--- MFVNeutralino/test/MakeSamples/rehltreco.py
+++ MFVNeutralino/test/MakeSamples/rehltreco.py
@@ -1,17 +1,17 @@
 from JMTucker.Tools.Samples import mfv_signal_samples
 
-samples = mfv_signal_samples[:1]
+samples = mfv_signal_samples
 
 from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
 
-cs = CRABSubmitter('mfv_run2_76x_rehltreco_t1',
+cs = CRABSubmitter('mfv_run2_76x_rehltreco',
                    pset_template_fn = 'rawhlt.py',
                    dataset = 'sim',
-                   splitting = 'FileBased',
-                   units_per_job = 1,
-                   total_units = 2,
+                   splitting = 'EventAwareLumiBased',
+                   units_per_job = 1000,
+                   total_units = -1,
                    aaa = True,
-                   publish_name = '76reco_10k',
+                   publish_name = '76_reco_10k',
                    input_files = ['reco.py', 'merge_fjrs.py'],
                    output_files = 'reco.root',
                    script_exe = 'rehltreco.sh',
