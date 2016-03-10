bf22d15b27f13d384de362843c093c46be1c6487
Jordan Tucker <jordantucker@gmail.com>
2016-03-10 08:08:37 -0600

    Sanitize input_files; files_per_job -> 10; missed putting quote marks in the crab script

--- MFVNeutralino/test/MakeSamples/copy4pub.py
+++ MFVNeutralino/test/MakeSamples/copy4pub.py
@@ -3,10 +3,10 @@ from pprint import pprint
 
 pubinfo = open(sys.argv[1]).read().split('\n')
 primary_dataset = pubinfo[0]
-input_files = pubinfo[1:]
+input_files = [x for x in pubinfo[1:] if x.strip() and '.root' in x]
 
 n_input_files = len(input_files)
-files_per_job = 5
+files_per_job = 10
 
 tag = 'RunIIFall15DR76-PU25nsData2015v1_76X_mcRun2_asymptotic_v12'
 
@@ -31,18 +31,18 @@ config = Config()
 config.General.transferLogs = False
 config.General.transferOutputs = True
 config.General.workArea = 'crab_copy4pub'
-config.General.requestName = %(primary_dataset)s
+config.General.requestName = '%(primary_dataset)s'
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = %(py_fn)s
+config.JobType.psetName = '%(py_fn)s'
 
 config.Data.userInputFiles = %(input_files)r
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = %(files_per_job)s
 config.Data.totalUnits = %(n_input_files)s
 config.Data.publication = True
-config.Data.outputPrimaryDataset = %(primary_dataset)s
-config.Data.outputDatasetTag = %(tag)s
+config.Data.outputPrimaryDataset = '%(primary_dataset)s'
+config.Data.outputDatasetTag = '%(tag)s'
 
 config.Site.whitelist = ['T1_US_FNAL', 'T3_US_FNALLPC', 'T2_US_Nebraska']
 config.Site.storageSite = 'T3_US_FNALLPC'
