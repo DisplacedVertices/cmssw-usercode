25aa07f69d58c161f3dbfccad74f768c3cb9a102
Jennifer Chu <jyc72@cornell.edu>
2016-06-09 16:12:18 -0500

    Require njets >= 4 and dBV >= 100 um; add ttbar back.

--- MFVNeutralino/test/One2Two/systematics/compare_dbv.py
+++ MFVNeutralino/test/One2Two/systematics/compare_dbv.py
@@ -5,11 +5,11 @@ import JMTucker.Tools.Samples as Samples
 import JMTucker.MFVNeutralino.AnalysisConstants as ac
 
 set_style()
-ps = plot_saver('../../plots/bkgest/MinitreeV6p1_76x_nstlays3/dbv', root=False)
-trees = '~/crabdirs/MinitreeV6p1_76x_nstlays3'
+ps = plot_saver('../../plots/bkgest/MinitreeV6p1_76x_nstlays3/dbv/dBV100um', root=False)
+trees = '~/crabdirs/MinitreeV6p1_76x_nstlays3_0'
 
 def book_dbv(n):
-    return ROOT.TH1F(n, '', 20, 0, 0.1)
+    return ROOT.TH1F(n, '', 40, 0, 0.2)
 
 sc = ac.int_lumi * ac.scale_factor
 
@@ -19,14 +19,14 @@ h_dbv_b = book_dbv('dbv_b')
 h_dbv_qcdb = book_dbv('dbv_qcdb')
 
 hs_nob = []
-for sn in 'qcdht0700 qcdht1000 qcdht1500 qcdht2000'.split():
+for sn in 'qcdht0700 qcdht1000 qcdht1500 qcdht2000 ttbar'.split():
     f = ROOT.TFile('%s/%s.root' % (trees,sn))
     t = f.Get('mfvMiniTree/t')
     s = getattr(Samples, sn)
 
     n = sn + ', no b quarks'
     h_dbv = book_dbv(n)
-    t.Draw('dist0>>%s' % n, 'nvtx == 1 && gen_flavor_code != 2')
+    t.Draw('dist0>>%s' % n, 'nvtx == 1 && dist0 >= 0.01 && gen_flavor_code != 2')
     h_dbv_sum.Add(h_dbv, sc * s.partial_weight_orig)
     h_dbv_nob.Add(h_dbv, sc * s.partial_weight_orig)
     ps.save(n)
@@ -36,7 +36,7 @@ for sn in 'qcdht0700 qcdht1000 qcdht1500 qcdht2000'.split():
     hs_nob.append(h)
 
 hs_b = []
-for sn in 'qcdht0700 qcdht1000 qcdht1500 qcdht2000'.split():
+for sn in 'qcdht0700 qcdht1000 qcdht1500 qcdht2000 ttbar'.split():
     f = ROOT.TFile('%s/%s.root' % (trees,sn))
     t = f.Get('mfvMiniTree/t')
     s = getattr(Samples, sn)
@@ -46,7 +46,7 @@ for sn in 'qcdht0700 qcdht1000 qcdht1500 qcdht2000'.split():
     else:
         n = sn
     h_dbv = book_dbv(n)
-    t.Draw('dist0>>%s' % n, 'nvtx == 1 && gen_flavor_code == 2')
+    t.Draw('dist0>>%s' % n, 'nvtx == 1 && dist0 >= 0.01 && gen_flavor_code == 2')
     h_dbv_sum.Add(h_dbv, sc * s.partial_weight_orig)
     h_dbv_b.Add(h_dbv, sc * s.partial_weight_orig)
     if sn != 'ttbar':
@@ -93,24 +93,24 @@ l.Draw()
 ps.save('dbv')
 
 
-#h_dbv_sum.Draw()
-#l = ROOT.TLegend(0.3,0.78,0.9,0.9)
-#l.AddEntry(h_dbv_sum, 'total background: mean d_{BV} = %4.1f +/- %2.1f #mum' % (10000*h_dbv_sum.GetMean(), 10000*h_dbv_sum.GetRMS()/h_dbv_sum.GetEntries()**0.5))
-#h_dbv_nob.SetStats(0)
-#h_dbv_nob.SetLineColor(ROOT.kBlue)
-#h_dbv_nob.SetLineWidth(3)
-#h_dbv_nob.DrawNormalized('sames')
-#l.AddEntry(h_dbv_nob, 'qcd, no b quarks: mean d_{BV} = %4.1f +/- %2.1f #mum' % (10000*h_dbv_nob.GetMean(), 10000*h_dbv_nob.GetRMS()/h_dbv_nob.GetEntries()**0.5))
-#h_dbv_qcdb.SetStats(0)
-#h_dbv_qcdb.SetLineColor(ROOT.kPink)
-#h_dbv_qcdb.SetLineWidth(3)
-#h_dbv_qcdb.DrawNormalized('sames')
-#l.AddEntry(h_dbv_qcdb, 'qcd, b quarks: mean d_{BV} = %4.1f +/- %2.1f #mum' % (10000*h_dbv_qcdb.GetMean(), 10000*h_dbv_qcdb.GetRMS()/h_dbv_qcdb.GetEntries()**0.5))
-#hs_b[5].DrawNormalized('sames')
-#l.AddEntry(hs_b[5], 'ttbar: mean d_{BV} = %4.1f +/- %2.1f #mum' % (10000*hs_b[5].GetMean(), 10000*hs_b[5].GetRMS()/hs_b[5].GetEntries()**0.5))
-#l.SetFillColor(0)
-#l.Draw()
-#ps.save('dbv_qcdb')
+h_dbv_sum.Draw()
+l = ROOT.TLegend(0.3,0.78,0.9,0.9)
+l.AddEntry(h_dbv_sum, 'total background: mean d_{BV} = %4.1f +/- %2.1f #mum' % (10000*h_dbv_sum.GetMean(), 10000*h_dbv_sum.GetRMS()/h_dbv_sum.GetEntries()**0.5))
+h_dbv_nob.SetStats(0)
+h_dbv_nob.SetLineColor(ROOT.kBlue)
+h_dbv_nob.SetLineWidth(3)
+h_dbv_nob.DrawNormalized('sames')
+l.AddEntry(h_dbv_nob, 'qcd, no b quarks: mean d_{BV} = %4.1f +/- %2.1f #mum' % (10000*h_dbv_nob.GetMean(), 10000*h_dbv_nob.GetRMS()/h_dbv_nob.GetEntries()**0.5))
+h_dbv_qcdb.SetStats(0)
+h_dbv_qcdb.SetLineColor(ROOT.kPink)
+h_dbv_qcdb.SetLineWidth(3)
+h_dbv_qcdb.DrawNormalized('sames')
+l.AddEntry(h_dbv_qcdb, 'qcd, b quarks: mean d_{BV} = %4.1f +/- %2.1f #mum' % (10000*h_dbv_qcdb.GetMean(), 10000*h_dbv_qcdb.GetRMS()/h_dbv_qcdb.GetEntries()**0.5))
+hs_b[4].DrawNormalized('sames')
+l.AddEntry(hs_b[4], 'ttbar: mean d_{BV} = %4.1f +/- %2.1f #mum' % (10000*hs_b[4].GetMean(), 10000*hs_b[4].GetRMS()/hs_b[4].GetEntries()**0.5))
+l.SetFillColor(0)
+l.Draw()
+ps.save('dbv_qcdb')
 
 
 h_dbv_sum.Draw()
