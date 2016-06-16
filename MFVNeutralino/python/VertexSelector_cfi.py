9b4621e540447ae18b2e2537565ce5af9f687c81
Jennifer Chu <jyc72@cornell.edu>
2016-06-16 09:12:55 -0500

    Also remove sumnhitsbehind requirement.

--- MFVNeutralino/python/VertexSelector_cfi.py
+++ MFVNeutralino/python/VertexSelector_cfi.py
@@ -93,7 +93,6 @@ mfvSelectedVerticesTight = mfvSelectedVertices.clone(
     min_bsbs2ddist = 0.01,
     max_geo2ddist = 2.0,
     max_bs2derr = 0.0025,
-    max_sumnhitsbehind = 0,
     )
 
 mfvSelectedVerticesTightLargeErr = mfvSelectedVerticesTight.clone(
