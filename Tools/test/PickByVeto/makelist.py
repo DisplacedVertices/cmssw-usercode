081e4dc5fd50d97373ec1c5970ae1a7976428525
Jordan Tucker <jordantucker@gmail.com>
2016-10-21 12:43:44 -0500

    modularize; fix bug with not writing newlines

--- Tools/test/PickByVeto/makelist.py
+++ Tools/test/PickByVeto/makelist.py
@@ -1,27 +1,35 @@
 import os
 from JMTucker.Tools.ROOTTools import *
 
-ps = [
-    '/uscms_data/d3/jchu/crab_dirs/mfv_763p2/MinitreeV6p1_76x_nstlays3_8/',
-    '/uscms_data/d3/jchu/crab_dirs/mfv_763p2/MinitreeV6p1_76x_nstlays3_7/',
-    '/uscms_data/d3/jchu/crab_dirs/mfv_763p2/MinitreeV6p1_76x_nstlays3_5/',
-]
-
-samples = ['ttbar']
-
-for sample in samples:
+def getlist(root_fn):
     l = []
-    for p in ps:
-        f = ROOT.TFile(os.path.join(p, '%s.root' % sample))
-        t = f.Get('mfvMiniTree/t')
-        for x in detree(t, 'lumi:event'):
-            l.append(x)
-        print sample, len(l)
-    l = sorted(set(l))
-    print 'after set', sample, len(l)
-    fn = 'vetolist.%s' % sample
-    f = open(fn, 'wt')
+    f = ROOT.TFile(root_fn)
+    t = f.Get('mfvMiniTree/t')
+    for x in detree(t, 'lumi:event'):
+        l.append(x)
+    return sorted(set(l))
+    
+def writelist(l, out_fn, gzip):
+    f = open(out_fn, 'wt')
     for x in l:
-        f.write('(%i,%i),' % x)
+        f.write('(%i,%i),\n' % x)
     f.close()
-    os.system('gzip %s' % fn)
+    if gzip:
+        os.system('gzip %s' % out_fn)
+
+def makelist(root_fn, out_fn, gzip):
+    writelist(getlist(root_fn), out_fn, gzip)
+
+def dosamples():
+    ps = [
+        '/uscms_data/d3/jchu/crab_dirs/mfv_763p2/MinitreeV6p1_76x_nstlays3_8/',
+        '/uscms_data/d3/jchu/crab_dirs/mfv_763p2/MinitreeV6p1_76x_nstlays3_7/',
+        '/uscms_data/d3/jchu/crab_dirs/mfv_763p2/MinitreeV6p1_76x_nstlays3_5/',
+    ]
+
+    samples = ['ttbar']
+
+    for sample in samples:
+        writelist(sum(getlist(os.path.join(p, '%s.root' % sample)) for p in ps), 'vetolist.%s' % sample, True)
+
+makelist('/uscms_data/d2/tucker/crab_dirs/MinitreeV9_temp/ttbar.root', 'veto_ttbar_temp', False)
