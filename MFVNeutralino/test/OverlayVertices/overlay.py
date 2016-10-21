94a6c6c87262544cff821c5f564af3847a9cfffb
Jordan Tucker <jordantucker@gmail.com>
2016-10-21 14:07:38 -0500

    toward control by params at top of file

--- MFVNeutralino/test/OverlayVertices/overlay.py
+++ MFVNeutralino/test/OverlayVertices/overlay.py
@@ -1,10 +1,17 @@
 from JMTucker.Tools.MiniAOD_cfg import which_global_tag
 from JMTucker.Tools.CMSSWTools import *
 
+debug = False
 is_mc = True
+which_event = 2
+rest_of_event = True
+min_ntracks = 3
+
+out_fn = 'overlay%s_%i' % ('_wevent' if rest_of_event else '', which_event)
+
 process = basic_process('Overlay')
 geometry_etc(process, which_global_tag(is_mc))
-tfileservice(process, 'overlay.root')
+tfileservice(process, out_fn)
 random_service(process, {'mfvVertices': 12179})
 
 if is_mc:
@@ -14,29 +21,33 @@ if is_mc:
 process.load('CommonTools.ParticleFlow.goodOfflinePrimaryVertices_cfi')
 process.load('JMTucker.MFVNeutralino.Vertexer_cfi')
 
+process.mfvVertices.verbose = debug
+#process.mfvVertices.track_src = 'mfvOverlayTracks'
+#process.mfvVertices.no_track_cuts = True
 process.mfvVertices.use_second_tracks = True
 process.mfvVertices.second_track_src = 'mfvOverlayTracks'
-process.mfvVertices.disregard_event = True
-#process.mfvVertices.no_track_cuts = True
-process.mfvVertices.verbose = True
+if not rest_of_event:
+    process.mfvVertices.disregard_event = True
 
 process.veto = cms.EDFilter('EventIdVeto',
-                         use_run = cms.bool(False),
-                         list_fn = cms.string('veto.gz'),
-                         debug = cms.untracked.bool(False),
-                         )
+                            use_run = cms.bool(False),
+                            list_fn = cms.string('veto.gz'),
+                            debug = cms.untracked.bool(False),
+                            )
 
 process.mfvOverlayTracks = cms.EDProducer('MFVOverlayVertexTracks',
                                           minitree_fn = cms.string('minitree.root'),
-                                          which_event = cms.int32(0),
-                                          only_other_tracks = cms.bool(False),
-                                          verbose = cms.bool(True),
+                                          which_event = cms.int32(which_event),
+                                          only_other_tracks = cms.bool(rest_of_event),
+                                          verbose = cms.bool(debug),
                                           )
 
 process.mfvOverlayHistos = cms.EDAnalyzer('MFVOverlayVertexHistos',
                                           truth_src = cms.InputTag('mfvOverlayTracks'),
+                                          beamspot_src = cms.InputTag('offlineBeamSpot'),
                                           vertices_src = cms.InputTag('mfvVertices'),
-                                          min_ntracks = cms.int32(3),
+                                          min_ntracks = cms.int32(min_ntracks),
+                                          debug = cms.bool(debug),
                                           )
 
 process.p = cms.Path(~process.veto * process.goodOfflinePrimaryVertices * process.mfvOverlayTracks * process.mfvVertices * process.mfvOverlayHistos)
@@ -44,5 +55,24 @@ process.p = cms.Path(~process.veto * process.goodOfflinePrimaryVertices * proces
 process.mfvOverlayTracks.minitree_fn = '/uscms_data/d2/tucker/crab_dirs/MinitreeV9_temp/ttbar.root'
 process.veto.list_fn = 'veto_ttbar_temp'
 process.maxEvents.input = -1
-process.source.fileNames = ['/store/user/tucker/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/pick1vtx/161020_194226/0000/pick_1.root']
-report_every(process, 1)
+process.source.fileNames = ['/store/user/tucker/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/pick1vtx/161020_194226/0000/' + x.strip() for x in \
+'''
+pick_1.root
+pick_2.root
+pick_3.root
+pick_4.root
+pick_5.root
+pick_7.root
+pick_8.root
+pick_10.root
+pick_14.root
+pick_16.root
+pick_17.root
+pick_18.root
+pick_23.root
+pick_25.root
+pick_26.root
+pick_27.root
+'''.split('\n') if x.strip()]
+
+report_every(process, 100)
