8bedb372bf9d06eea683689e92034bddc0ad4f0a
Jordan Tucker <jordantucker@gmail.com>
2016-10-21 14:07:05 -0500

    Assert not data; subtract beamspot from reco vertices since the truth is already bs-subtracted; debugging statements; require nvtx >= 2 on all numerators

--- MFVNeutralino/plugins/OverlayVertexHistos.cc
+++ MFVNeutralino/plugins/OverlayVertexHistos.cc
@@ -15,9 +15,11 @@ class MFVOverlayVertexHistos : public edm::EDAnalyzer {
 
  private:
   const edm::EDGetTokenT<std::vector<double>> truth_token;
+  const edm::EDGetTokenT<reco::BeamSpot> beamspot_token;
   const edm::EDGetTokenT<reco::VertexCollection> vertices_token;
 
   const int min_ntracks;
+  const bool debug;
 
   TH1F* h_dvv_true;
   TH1F* h_dvv_pass_anytwo;
@@ -30,8 +32,10 @@ class MFVOverlayVertexHistos : public edm::EDAnalyzer {
 
 MFVOverlayVertexHistos::MFVOverlayVertexHistos(const edm::ParameterSet& cfg)
   : truth_token(consumes<std::vector<double>>(cfg.getParameter<edm::InputTag>("truth_src"))),
+    beamspot_token(consumes<reco::BeamSpot>(cfg.getParameter<edm::InputTag>("beamspot_src"))),
     vertices_token(consumes<reco::VertexCollection>(cfg.getParameter<edm::InputTag>("vertices_src"))),
-    min_ntracks(cfg.getParameter<int>("min_ntracks"))
+    min_ntracks(cfg.getParameter<int>("min_ntracks")),
+    debug(cfg.getParameter<bool>("debug"))
 {
   edm::Service<TFileService> fs;
 
@@ -50,6 +54,8 @@ namespace {
 }
 
 void MFVOverlayVertexHistos::analyze(const edm::Event& event, const edm::EventSetup&) {
+  assert(!event.isRealData()); // JMTBAD lots of reasons this dosen't work on data yet
+
   edm::Handle<std::vector<double>> truth;
   event.getByToken(truth_token, truth);
 
@@ -58,14 +64,25 @@ void MFVOverlayVertexHistos::analyze(const edm::Event& event, const edm::EventSe
   const double y0 = truth->at(2);
   const double z0 = truth->at(3);
   const int ntk1 = int(truth->at(4));
-  //const double x1 = truth->at(5);
-  //const double y1 = truth->at(6);
-  //const double z1 = truth->at(7);
+  const double x1 = truth->at(5);
+  const double y1 = truth->at(6);
+  const double z1 = truth->at(7);
   const double x1_0 = truth->at(8);
   const double y1_0 = truth->at(9);
   const double z1_0 = truth->at(10);
 
   const double dvv_true = mag(x0 - x1_0, y0 - y1_0);
+
+  if (debug) printf("OverlayVertexHistos: truth: ntk0 %i v0 %f, %f, %f ntk1 %i v1 %f, %f, %f v1_0 %f, %f, %f -> dvv_true %f\n",
+                    ntk0, x0, y0, z0, ntk1, x1, y1, z1, x1_0, y1_0, z1_0, dvv_true);
+
+  edm::Handle<reco::BeamSpot> beamspot;
+  event.getByToken(beamspot_token, beamspot);
+  const double bsx = beamspot->x0();
+  const double bsy = beamspot->y0();
+  const double bsz = beamspot->z0();
+  const double bsdxdz = beamspot->dxdz();
+  const double bsdydz = beamspot->dydz();
 
   edm::Handle<reco::VertexCollection> vertices;
   event.getByToken(vertices_token, vertices);
@@ -78,31 +95,43 @@ void MFVOverlayVertexHistos::analyze(const edm::Event& event, const edm::EventSe
   int found_ntk[2] = {0};
   const double found_dist = 0.012;
 
+  if (debug) printf("%i reco vertices:\n", nvtx);
+
   for (const reco::Vertex& v : *vertices) {
     const int ntk = v.nTracks(0.);
 
     if (ntk >= min_ntracks)
       ++nvtx_minntk;
 
-    if (mag(v.x() - x0, v.y() - y0, v.z() - z0) < found_dist) {
+    const double vx = v.x() - (bsx + bsdxdz * (v.z() - bsz));
+    const double vy = v.y() - (bsy + bsdydz * (v.z() - bsz));
+    const double vz = v.z() - bsz;
+
+    const double d0   = mag(vx - x0,   vy - y0,   vz - z0);
+    const double d1_0 = mag(vx - x1_0, vy - y1_0, vz - z1_0);
+    
+    if (d0 < found_dist) {
       found[0] = true;
       found_ntk[0] = ntk;
     }
 
-    if (mag(v.x() - x1_0, v.y() - y1_0, v.z() - z1_0) < found_dist) {
+    if (d1_0 < found_dist) {
       found[1] = true;
       found_ntk[1] = ntk;
     }
+
+    if (debug) printf("vertex at %f, %f, %f with %i tracks:  d0 %f -> found0? %i  d1_0 %f -> found1? %i \n",
+                      v.x(), v.y(), v.z(), ntk, d0, found[0], d1_0, found[1]);
   } 
 
   auto fillit = [&dvv_true](TH1F* h, bool cond) { if (cond) h->Fill(dvv_true); };
 
-  fillit(h_dvv_pass_anytwo,              nvtx >= 2);                                                            
-  fillit(h_dvv_pass_twominntk,           nvtx_minntk >= 2);                                                     
-  fillit(h_dvv_pass_foundv0,             found[0]);                                                             
-  fillit(h_dvv_pass_foundv0samentk,      found[0] && found_ntk[0] == ntk0);                                     
-  fillit(h_dvv_pass_foundv0andv1,        found[0] && found[1]);                                                 
-  fillit(h_dvv_pass_foundv0andv1samentk, found[0] && found[1] && found_ntk[0] == ntk0 && found_ntk[1] == ntk1); 
+  fillit(h_dvv_pass_anytwo,              nvtx >= 2);
+  fillit(h_dvv_pass_twominntk,           nvtx_minntk >= 2);
+  fillit(h_dvv_pass_foundv0,             nvtx >= 2 && found[0]);
+  fillit(h_dvv_pass_foundv0samentk,      nvtx >= 2 && found[0] && found_ntk[0] >= ntk0);
+  fillit(h_dvv_pass_foundv0andv1,        nvtx >= 2 && found[0] && found[1]);
+  fillit(h_dvv_pass_foundv0andv1samentk, nvtx >= 2 && found[0] && found[1] && found_ntk[0] == ntk0 && found_ntk[1] >= ntk1);
 }
 
 DEFINE_FWK_MODULE(MFVOverlayVertexHistos);
