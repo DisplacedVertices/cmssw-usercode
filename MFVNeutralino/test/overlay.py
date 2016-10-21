d6d35fe14a7c7b89d6bf0f52da59a05deff05009
Jordan Tucker <jordantucker@gmail.com>
2016-10-21 12:47:00 -0500

    Getting there

--- MFVNeutralino/test/overlay.py
+++ MFVNeutralino/test/overlay.py
@@ -16,23 +16,33 @@ process.load('JMTucker.MFVNeutralino.Vertexer_cfi')
 
 process.mfvVertices.use_second_tracks = True
 process.mfvVertices.second_track_src = 'mfvOverlayTracks'
-#process.mfvVertices.disregard_event = True
+process.mfvVertices.disregard_event = True
 #process.mfvVertices.no_track_cuts = True
 process.mfvVertices.verbose = True
+
+process.veto = cms.EDFilter('EventIdVeto',
+                         use_run = cms.bool(False),
+                         list_fn = cms.string('veto.gz'),
+                         debug = cms.untracked.bool(False),
+                         )
 
 process.mfvOverlayTracks = cms.EDProducer('MFVOverlayVertexTracks',
                                           minitree_fn = cms.string('minitree.root'),
                                           which_event = cms.int32(0),
-                                          only_other_tracks = cms.bool(True),
+                                          only_other_tracks = cms.bool(False),
                                           verbose = cms.bool(True),
                                           )
 
-process.p = cms.Path(process.goodOfflinePrimaryVertices * process.mfvOverlayTracks * process.mfvVertices)
+process.mfvOverlayHistos = cms.EDAnalyzer('MFVOverlayVertexHistos',
+                                          truth_src = cms.InputTag('mfvOverlayTracks'),
+                                          vertices_src = cms.InputTag('mfvVertices'),
+                                          min_ntracks = cms.int32(3),
+                                          )
 
-process.maxEvents.input = 2
-process.source.fileNames = ['/store/mc/RunIIFall15DR76/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/00000/0039E642-58BD-E511-B773-002590DE7230.root']
-file_event_from_argv(process)
+process.p = cms.Path(~process.veto * process.goodOfflinePrimaryVertices * process.mfvOverlayTracks * process.mfvVertices * process.mfvOverlayHistos)
 
-set_events_to_process(process, [ (1, 374886, 93627873), (1, 529143, 132153748)])
-
-
+process.mfvOverlayTracks.minitree_fn = '/uscms_data/d2/tucker/crab_dirs/MinitreeV9_temp/ttbar.root'
+process.veto.list_fn = 'veto_ttbar_temp'
+process.maxEvents.input = -1
+process.source.fileNames = ['/store/user/tucker/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/pick1vtx/161020_194226/0000/pick_1.root']
+report_every(process, 1)
