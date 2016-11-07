b9ca1cb2f32bc612b96d5fe32c2b97111513368a
Jordan Tucker <jordantucker@gmail.com>
2016-11-07 12:06:03 -0600

    use argparse

--- MFVNeutralino/test/OverlayVertices/overlay.py
+++ MFVNeutralino/test/OverlayVertices/overlay.py
@@ -1,58 +1,48 @@
 #!/usr/bin/env python
 
-max_events = -1
-debug = False
-is_mc = True
-which_event = None
-rest_of_event = False
-dz_true_max = 1e9
-min_ntracks = 3
-found_dist = 0.008
-z_model = 'deltasv'
-z_width = 0.02
-rotate_x = False
-rotate_p = False
-sample = 'ttbar'
-minitree_path = 'root://cmsxrootd.fnal.gov//store/user/tucker/MinitreeV9/ntk%i' % min_ntracks
+from JMTucker.Tools.MiniAOD_cfg import which_global_tag
+from JMTucker.Tools.CMSSWTools import *
+
+parser, args_printer = friendly_argparse(description='Overlay tracks from pairs of 1-vertex events')
+parser.add_argument('+which-event', '+e', type=int, help='which event from minitree to use', required=True)
+parser.add_argument('+sample', help='which sample to use', choices=['ttbar', 'qcdht1500'], default='ttbar')
+parser.add_argument('+ntracks', type=int, help='ntracks to use', default=3)
+parser.add_argument('+rest-of-event', action='store_true', help='whether to use the rest of the tracks in the edm event')
+parser.add_argument('+z-model', help='z model', choices=['deltasv', 'deltasvgaus', 'deltapv', 'none'], default='deltasv')
+parser.add_argument('+z-width', type=float, help='width of gaus used in z model (cm)', default=0.02)
+parser.add_argument('+dz-true-max', type=float, help='max dz allowed for z model (cm)', default=1e9)
+parser.add_argument('+found-dist', type=float, help='3D distance for matching by position (cm)', default=0.008)
+parser.add_argument('+rotate-x', action='store_true', help='azimuthally rotate x of tracks (around beam line)')
+parser.add_argument('+rotate-p', action='store_true', help='azimuthally rotate p of tracks')
+parser.add_argument('+is-mc', action='store_true', help='whether input is MC / data')
+parser.add_argument('+batch', action='store_true', help='run in batch mode')
+parser.add_argument('+debug', action='store_true', help='turn on debug prints')
+parser.add_argument('+out-fn', help='override output fn')
+parser.add_argument('+minitree-path', help='override minitree path')
+parser.add_argument('+minitree-fn', help='override minitree fn')
+parser.add_argument('+max-events', type=int, help='max edm events to run on', default=-1)
+
+args = parser.parse_args()
+if args.out_fn is None:
+    args.out_fn = 'overlay_%i.root' % args.which_event
+if args.minitree_path is None:
+    args.minitree_path = 'root://cmsxrootd.fnal.gov//store/user/tucker/MinitreeV9/ntk%i' % args.ntracks
+if args.minitree_fn is None:
+    args.minitree_fn = '%s/%s.root' % (args.minitree_path, args.sample)
+
+args_printer('overlay args', args)
 
 ####
 
-from JMTucker.Tools.MiniAOD_cfg import which_global_tag
-from JMTucker.Tools.CMSSWTools import *
-from JMTucker.Tools.general import typed_from_argv
-
-batch = 'batch' in sys.argv
-
-if which_event is None:
-    which_event = typed_from_argv(int)
-    if which_event is None:
-        raise ValueError('which_event from argv but no int found')
-    print 'which_event from argv:', which_event
-
-rest_of_event_s = '_wevent' if rest_of_event else ''
-z_width_s = ('%.3f' % z_width).replace('.', 'p') if 'gaus' in z_model else ''
-found_dist_s = ('%.3f' % found_dist).replace('.', 'p')
-rotate_s = ''
-if rotate_x and rotate_p:
-    rotate_s = '_rotateXP'
-elif rotate_x:
-    rotate_s = '_rotateX'
-elif rotate_p:
-    rotate_s = '_rotateP'
-
-out_fn = 'overlay%(rest_of_event_s)s_%(sample)s_Z%(z_model)s%(z_width_s)s_dist%(found_dist_s)s%(rotate_s)s_%(which_event)i.root' % locals()
-
-minitree_fn = '%s/%s.root' % (minitree_path, sample)
-
-if sample == 'ttbar':
+if args.sample == 'ttbar':
     file_base = '/store/user/tucker/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/pick1vtx/161104_153826/0000'
     file_nums = range(1,94)
-elif sample == 'qcdht1500':
+elif args.sample == 'qcdht1500':
     file_base = '/store/user/tucker/QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/pick1vtx/161104_153718/0000'
     file_nums = range(1,15)
     file_nums.remove(9)
 else:
-    raise ValueError('bad sample %s' % sample)
+    raise ValueError('bad sample %s' % args.sample)
 
 in_fns = [os.path.join(file_base, 'pick_%i.root' % i) for i in file_nums]
 
@@ -60,13 +50,13 @@ in_fns = [os.path.join(file_base, 'pick_%i.root' % i) for i in file_nums]
 
 process = basic_process('Overlay')
 process.source.fileNames = in_fns
-process.maxEvents.input = max_events
-report_every(process, 1000000 if batch else 100)
-geometry_etc(process, which_global_tag(is_mc))
-tfileservice(process, out_fn)
+process.maxEvents.input = args.max_events
+report_every(process, 1000000 if args.batch else 100)
+geometry_etc(process, which_global_tag(args.is_mc))
+tfileservice(process, args.out_fn)
 random_service(process, {'mfvVertices': 12179, 'mfvOverlayTracks': 12180})
 
-if is_mc:
+if args.is_mc:
     process.load('JMTucker.Tools.MCStatProducer_cff')
     process.mcStat.histos = True
 
@@ -74,31 +64,31 @@ process.load('CommonTools.ParticleFlow.goodOfflinePrimaryVertices_cfi')
 process.load('JMTucker.MFVNeutralino.Vertexer_cfi')
 
 process.mfvVertices.histos = False
-process.mfvVertices.verbose = debug
+process.mfvVertices.verbose = args.debug
 process.mfvVertices.use_second_tracks = True
 process.mfvVertices.second_track_src = 'mfvOverlayTracks'
-if not rest_of_event:
+if not args.rest_of_event:
     process.mfvVertices.disregard_event = True
 
 process.mfvOverlayTracks = cms.EDFilter('MFVOverlayVertexTracks',
-                                        minitree_fn = cms.string(minitree_fn),
-                                        which_event = cms.int32(which_event),
-                                        rotate_x = cms.bool(rotate_x),
-                                        rotate_p = cms.bool(rotate_p),
-                                        z_model = cms.string(z_model),
-                                        z_width = cms.double(z_width),
-                                        only_other_tracks = cms.bool(rest_of_event),
-                                        verbose = cms.bool(debug),
+                                        minitree_fn = cms.string(args.minitree_fn),
+                                        which_event = cms.int32(args.which_event),
+                                        rotate_x = cms.bool(args.rotate_x),
+                                        rotate_p = cms.bool(args.rotate_p),
+                                        z_model = cms.string(args.z_model),
+                                        z_width = cms.double(args.z_width),
+                                        only_other_tracks = cms.bool(args.rest_of_event),
+                                        verbose = cms.bool(args.debug),
                                         )
 
 process.mfvOverlayHistos = cms.EDAnalyzer('MFVOverlayVertexHistos',
                                           truth_src = cms.InputTag('mfvOverlayTracks'),
                                           beamspot_src = cms.InputTag('offlineBeamSpot'),
                                           vertices_src = cms.InputTag('mfvVertices'),
-                                          dz_true_max = cms.double(dz_true_max),
-                                          min_ntracks = cms.int32(min_ntracks),
-                                          found_dist = cms.double(found_dist),
-                                          debug = cms.bool(debug),
+                                          dz_true_max = cms.double(args.dz_true_max),
+                                          min_ntracks = cms.int32(args.ntracks),
+                                          found_dist = cms.double(args.found_dist),
+                                          debug = cms.bool(args.debug),
                                           )
 
 process.p = cms.Path(process.goodOfflinePrimaryVertices * process.mfvOverlayTracks * process.mfvVertices * process.mfvOverlayHistos)
