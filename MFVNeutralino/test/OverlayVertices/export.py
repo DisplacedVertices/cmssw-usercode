6ff47c1308b7fe1ed407250b2fff20a96dccc60c
Jordan Tucker <jordantucker@gmail.com>
2016-11-14 06:05:09 -0600

    export curves

---
+++ MFVNeutralino/test/OverlayVertices/export.py
@@ -0,0 +1,16 @@
+import sys
+from JMTucker.Tools.ROOTTools import ROOT
+from binning import binning
+
+def book(out_name, hs=[]):
+    bins = binning()
+    h = ROOT.TH1F(out_name, '', len(bins)-1, bins)
+    hs.append(h)
+    return h
+
+f = ROOT.TFile('export.root', 'recreate')
+
+execfile(sys.argv[1])
+
+f.Write()
+f.Close()
