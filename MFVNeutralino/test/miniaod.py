58a1974cbb13201ba9482db149ce6b87f507aaa9
Jordan Tucker <jordantucker@gmail.com>
2016-12-17 09:47:03 -0600

    attempt at splitting ntuple -> miniaod + ntuple2

---
+++ MFVNeutralino/test/miniaod.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from JMTucker.Tools.MiniAOD_cfg import cms, pat_tuple_process
+from JMTucker.Tools.CMSSWTools import file_event_from_argv
+
+is_mc = True
+
+####
+
+def customize_before_unscheduled(process):
+    process.out.outputCommands = [
+        'drop *',
+        'keep *_mcStat_*_*',
+        'keep *_skimmedGeneralTracks_*_*',
+        'keep recoGenParticles_genParticles_*_*',
+        'keep *_preselectedPatJets_*_*',
+        'keep patMuons_patMuons_*_*',
+        'keep patElectrons_patElectrons_*_*',
+        'keep *_patMETsNoHF_*_*',
+        'keep recoBeamSpot_offlineBeamSpot_*_*',
+        'keep recoVertexs_offlinePrimaryVertices_*_*',
+        'keep *_TriggerResults_*_HLT',
+        'keep *_TriggerResults_*_PAT',
+        'keep GenEventInfoProduct_generator_*_*',
+        'keep *_addPileupInfo_*_*',
+        ]
+
+process = pat_tuple_process(customize_before_unscheduled, is_mc)
+process.out.fileName = 'miniaod.root'
+
+process.out.outputCommands += [
+    # these three get added to the end somehow
+    'drop patMETs_patPFMetPuppi__PAT',
+    'drop patMETs_patCaloMet__PAT',
+    'drop patMETs_patMETPuppi__PAT',
+    ]
+
+process.skimmedGeneralTracks = cms.EDProducer('MFVSkimmedTracks')
+
+process.patJets.embedPFCandidates = True
+process.preselectedPatJets = process.selectedPatJets.clone(cut = process.jtupleParams.jetPresel)
+process.patMuons.embedTrack = True
+process.patElectrons.embedTrack = True
+process.patElectrons.embedGsfElectronCore = True
+
+import JMTucker.MFVNeutralino.TriggerFilter
+JMTucker.MFVNeutralino.TriggerFilter.setup_trigger_filter(process)
+
+process.maxEvents.input = 100
+file_event_from_argv(process)
---
+++ MFVNeutralino/test/ntuple2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from JMTucker.Tools.MiniAOD_cfg import cms, which_global_tag
+from JMTucker.Tools.CMSSWTools import *
+
+is_mc = True
+version = 'v9'
+batch_name = 'Ntuple' + version.upper()
+
+####
+
+process = basic_process('Ntuple')
+report_every(process, 1000000)
+registration_warnings(process)
+geometry_etc(process, which_global_tag(is_mc))
+random_service(process, {'mfvVertices': 1222})
+tfileservice(process, 'vertex_histos.root')
+output_file(process, 'ntuple.root', [
+        'drop *',
+        'keep *_mcStat_*_*',
+        'keep MFVVertexAuxs_mfvVerticesAux_*_*',
+        'keep MFVEvent_mfvEvent__*',
+        ])
+
+process.load('CommonTools.ParticleFlow.goodOfflinePrimaryVertices_cfi')
+process.load('PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi')
+process.load('PhysicsTools.PatAlgos.selectionLayer1.muonSelector_cfi')
+process.load('PhysicsTools.PatAlgos.selectionLayer1.electronSelector_cfi')
+process.load('JMTucker.Tools.PATTupleSelection_cfi')
+process.load('JMTucker.MFVNeutralino.Vertexer_cff')
+process.load('JMTucker.MFVNeutralino.EventProducer_cfi')
+
+process.selectedPatJets.cut = process.jtupleParams.jetCut
+process.selectedPatJets.src = 'preselectedPatJets'
+process.selectedPatMuons.cut = process.jtupleParams.muonCut
+process.selectedPatElectrons.cut = process.jtupleParams.electronCut
+
+process.mfvVertices.track_src = 'skimmedGeneralTracks'
+
+process.p = cms.Path(process.goodOfflinePrimaryVertices *
+                     process.selectedPatJets *
+                     process.selectedPatMuons *
+                     process.selectedPatElectrons *
+                     process.mfvVertices *
+                     process.mfvEvent *
+                     process.mfvGenVertices *
+                     process.mfvVerticesAuxTmp *
+                     process.mfvSelectedVerticesTmp *
+                     process.mfvVerticesToJets *
+                     process.mfvVerticesAux)
+
+#process.options.wantSummary = True
+process.maxEvents.input = 100
+process.source.fileNames = ['file:miniaod.root']
+file_event_from_argv(process)
