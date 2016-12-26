989a2e704ebb87b2233e963ca2d604b52de8500e
Jordan Tucker <jordantucker@gmail.com>
2016-12-26 15:42:25 -0600

    exception_chance.

--- Tools/plugins/NoOP.cc
+++ Tools/plugins/NoOP.cc
@@ -1,7 +1,10 @@
+#include "CLHEP/Random/RandomEngine.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Utilities/interface/RandomNumberGenerator.h"
 
 class NoOP : public edm::EDAnalyzer {
 public:
@@ -9,13 +12,23 @@ public:
 
 private:
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  const double exception_chance;
 };
 
-NoOP::NoOP(const edm::ParameterSet& cfg) 
+NoOP::NoOP(const edm::ParameterSet& cfg)
+  : exception_chance(cfg.getUntrackedParameter<double>("exception_chance", -1))
 {
+  edm::Service<edm::RandomNumberGenerator> rng;
+  if (exception_chance > 0 && !rng.isAvailable())
+    throw cms::Exception("NoOP", "RandomNumberGeneratorService not available");
 }
 
 void NoOP::analyze(const edm::Event& event, const edm::EventSetup&) {
+  if (exception_chance > 0) {
+    edm::Service<edm::RandomNumberGenerator> rng;
+    if (rng->getEngine(event.streamID()).flat() < exception_chance)
+      throw cms::Exception("NoOP", "pop goes the weasel");
+  }
 }
 
 DEFINE_FWK_MODULE(NoOP);
