19c32879efb035d01472dd521983c20ec6f9fe99
Jordan Tucker <jordantucker@gmail.com>
2016-12-29 17:06:01 -0600

    23Sep2016ReReco json

--- Tools/python/Samples.py
+++ Tools/python/Samples.py
@@ -354,7 +354,7 @@ auxiliary_data_samples = [
     ]
 
 for s in data_samples + auxiliary_data_samples:
-    s.json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_Silver.txt'
+    s.json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt' # includes PromptReco for H
 
 ########################################################################
 
