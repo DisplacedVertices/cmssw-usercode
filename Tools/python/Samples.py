8f25f75d8eb98c94741fc41fe578847dc2a8672c
Jordan Tucker <jordantucker@gmail.com>
2017-01-12 09:09:35 -0600

    Silver v2 json

--- Tools/python/Samples.py
+++ Tools/python/Samples.py
@@ -332,7 +332,7 @@ auxiliary_data_samples = [
     ]
 
 for s in data_samples + auxiliary_data_samples:
-    s.json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_Silver.txt'
+    s.json = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_Silver_v2.txt'
 
 ########################################################################
 
