987b199d8936cf18d61a61e3e95f9523d6cd98b8
Jordan Tucker <jordantucker@gmail.com>
2017-01-23 15:03:31 -0600

    By-run histogram manager and merge script

---
+++ Tools/interface/ByRunTH1.h
@@ -0,0 +1,46 @@
+#ifndef JMTucker_Tools_ByRunTH1_h
+#define JMTucker_Tools_ByRunTH1_h
+
+#include <map>
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+template <typename T>
+class ByRunTH1 {
+ public:
+  void set(edm::Service<TFileService>* fs, const char* name, const char* title, const int nbins, const double xmin, const double xmax) {
+    fs_ = fs;
+    name_ = name;
+    title_ = title;
+    nbins_ = nbins;
+    xmin_ = xmin;
+    xmax_ = xmax;
+  }
+
+  T* operator[](unsigned run) {
+    if (hist_map_.find(run) == hist_map_.end())
+      book(run); 
+    return hist_map_[run];
+  }
+
+  void book(unsigned run) {
+    if (hist_map_.find(run) == hist_map_.end())
+      hist_map_[run] = (*fs_)->make<T>(TString::Format("%s_run%u", name_.Data(), run), 
+                                       TString::Format("%s (run %u)", title_.Data(), run), 
+                                       nbins_,
+                                       xmin_,
+                                       xmax_);
+  }
+
+ private:
+  edm::Service<TFileService>* fs_;
+  std::map<unsigned, T*> hist_map_;
+
+  TString name_;
+  TString title_;
+  int nbins_;
+  double xmin_;
+  double xmax_;    
+};
+
+#endif
---
+++ Tools/scripts/mergeByRunTH1.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import os, sys, shutil, re
+from fnmatch import fnmatch
+from pprint import pprint
+from collections import defaultdict
+from JMTucker.Tools.ROOTTools import *
+
+if len(sys.argv) < 5:
+    print 'usage: mergeByRunTH1.py in_fn out_fn min_run max_run [glob_pattern_1 ...]'
+    sys.exit(1)
+
+in_fn = sys.argv[1]
+out_fn = sys.argv[2]
+min_run = int(sys.argv[3])
+max_run = int(sys.argv[4])
+patterns = sys.argv[5:]
+def use(path):
+    return not patterns or any(fnmatch(path, pattern) for pattern in patterns)
+
+if not os.path.isfile(in_fn):
+    print 'no file', in_fn
+    sys.exit(1)
+
+if os.path.isfile(out_fn):
+    print 'refusing to clobber', out_fn
+    sys.exit(1)
+
+shutil.copy2(in_fn, out_fn)
+
+f = ROOT.TFile(out_fn, 'update')
+run_re = re.compile(r'(.*)_run(\d+)$')
+title_re = re.compile(r'(.*) \(run \d+\)$')
+groups = defaultdict(list)
+
+for path in flatten_directory(f):
+    if use(path):
+        mo = run_re.search(path)
+        if mo:
+            path_norun, run = mo.groups()
+            groups[path_norun].append(path)
+            o = f.Get(path)
+            if not issubclass(type(o), ROOT.TH1):
+                raise ValueError("object %r isn't a TH1" % o)
+
+groups = dict(groups)
+
+print 'will add these groups:'
+pprint(groups)
+raw_input('<hit enter if ok>')
+
+for path in sorted(groups.keys()):
+    l = groups[path]
+    dn, bn = os.path.split(path)
+    d = f.Get(dn)
+    new_o = f.Get(l[0]).Clone(bn)
+    new_o.SetDirectory(d)
+    mo = title_re.search(new_o.GetTitle())
+    assert mo is not None
+    new_o.SetTitle(mo.group(1))
+    for o in l[1:]:
+        new_o.Add(f.Get(o))
+    d.cd()
+    new_o.Write()
+
+f.Close()
