ef0c2e246173f0d8f26bcbfaf63e97f616a3177c
Jordan Tucker <jordantucker@gmail.com>
2017-01-27 12:35:13 -0600

    for 2015+16

--- MFVNeutralino/test/vertex_xsec.py
+++ MFVNeutralino/test/vertex_xsec.py
@@ -2,23 +2,34 @@ from collections import defaultdict
 from JMTucker.Tools.LumiLines import *
 from JMTucker.Tools.ROOTTools import *
 set_style()
-ps = plot_saver('plots/vertex_xsec_v20', size=(1200,600), log=False)
 
-lls = LumiLines('/uscms/home/tucker/mfvrecipe/lumi.gzpickle')
+year, ntracks, oneortwo = 2015, 3, 1
 
-f = ROOT.TFile('crab/MiniTreeV20_redux/MultiJetPk2012.root')
-t = f.Get('mfvMiniTree/t')
+plot_path = 'vertex_xsec_%i_%itrk_%iV' % (year, ntracks, oneortwo)
+title = '%i, %i-track %i-vtx' % (year, ntracks, oneortwo)
+fns = ['/uscms_data/d2/tucker/crab_dirs/MinitreeV10_sidebanddata2015/JetHT2015D.root'] if year == 2015 else []
+tree_path = 'tre%i%i/t' % (ntracks, ntracks) if ntracks < 5 else 'mfvMiniTree/t'
 
+ps = plot_saver(plot_dir(plot_path), size=(1200,600), log=False, pdf=True)
+lls = LumiLines('/uscms/home/tucker/public/mfv/2015plus2016.gzpickle')
+
+runs = lls.runs(year)
+print runs[:15]
+raise 1
 nvtx = defaultdict(int)
-for jentry in ttree_iterator(t):
-    nvtx[t.run] += 1
 
-runs = sorted(nvtx.keys())
+for fn in fns:
+    f = ROOT.TFile(fn)
+    t = f.Get(tree_path)
+    for (run,) in detree(t, 'run', 'nvtx==%i' % oneortwo):
+        nvtx[run] += 1
 
 excludes = [
     ('all', []),
-    ('exclude_obvious', range(19) + [142,143,144]),
-    ('only_low', range(19, 500)),
+    ('exclude_small_lumi', [9] if year == 2015 else []),
+#    ('exclude_one_small_lumi_run', [1]),
+#    ('exclude_obvious', range(19) + [142,143,144]),
+#    ('only_low', range(19, 500)),
     ]
 
 for exclude_name, exclude in excludes:
@@ -41,10 +52,13 @@ for exclude_name, exclude in excludes:
         min_nvtx = min(n, min_nvtx)
         max_nvtx = max(n, max_nvtx)
         lumi = lls.recorded(run)/1e6
+        lumi_frac_uncert = 0.027 if run <= 260627 else 0.062
+        lumi_uncert = lumi_frac_uncert * lumi
         avg_pu = lls.avg_pu(run) if lumi > 0 else -1
+
         if lumi > 0:
             if not exclude:
-                print '(%3i) %i: %5i in %10.5g/pb -> %10.5g pb,  %10.5g pb/PU' % (i, run, n, lumi, n/lumi, n/lumi/avg_pu)
+                print '(%3i) %i: %5i in %10.5g +- %10.5g/pb -> %10.5g pb,  %10.5g pb/PU' % (i, run, n, lumi, lumi_uncert, n/lumi, n/lumi/avg_pu)
             lsum += lumi
 
             x = i
@@ -53,9 +67,15 @@ for exclude_name, exclude in excludes:
             y  /= lumi
             yl /= lumi
             yh /= lumi
+            el = y - yl
+            eh = yh - y
+            #el = (el**2 + (lumi_frac_uncert*lumi)**2)**0.5
+            #eh = (eh**2 + (lumi_frac_uncert*lumi)**2)**0.5
+            #yl = y - el
+            #yh = y + eh
             xsec.SetPoint(i, x, y)
-            xsec.SetPointEYlow (i, y - yl)
-            xsec.SetPointEYhigh(i, yh - y)
+            xsec.SetPointEYlow (i, el)
+            xsec.SetPointEYhigh(i, eh)
 
             y  /= avg_pu
             yl /= avg_pu
@@ -75,8 +95,10 @@ for exclude_name, exclude in excludes:
         for run in zero_lumis:
             print 'zero lumi for run %i with %i 1V events' % (run, nvtx[run])
 
+    print 'lumi sum:', lsum/1e3, '/fb'
+
     for g in (xsec, xsec_per_pu):
-        g.SetTitle('# runs: %i  total lumi: %.1f/fb;i_{run};1V event #sigma (pb)' % (runs_used, lsum/1e3))
+        g.SetTitle('%s events  # runs: %i;i_{run};#sigma (pb)' % (title, runs_used))
         g.SetMarkerStyle(20)
         g.SetMarkerSize(0.5)
 
@@ -84,7 +106,7 @@ for exclude_name, exclude in excludes:
     xsec.Fit('pol0')
     ps.save(exclude_name + '_xsec')
 
-    xsec_per_pu.GetYaxis().SetTitle('1V event #sigma / PU (pb)')
+    xsec_per_pu.GetYaxis().SetTitle('#sigma / PU (pb)')
     xsec_per_pu.Draw('AP')
     xsec_per_pu.Fit('pol0')
     ps.save(exclude_name + '_xsec_per_pu')
