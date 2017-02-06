6b7eb5cf66e6cd5ab0cdf1de1f79deafea8082fb
Jordan Tucker <jordantucker@gmail.com>
2017-02-06 14:19:00 -0600

    Include 2015C

--- MFVNeutralino/test/ByRun/vertex_xsec.py
+++ MFVNeutralino/test/ByRun/vertex_xsec.py
@@ -4,11 +4,20 @@ ROOT.gStyle.SetOptFit(0)
 
 year, ntracks, oneortwo = 2015, 3, 1
 
+tree_path = 'tre%i%i/t' % (ntracks, ntracks) if ntracks < 5 else 'mfvMiniTree/t'
 plot_path = 'vertex_xsec_%i_%itrk_%iV' % (year, ntracks, oneortwo)
 title = '%i, %i-track %i-vtx events' % (year, ntracks, oneortwo)
-fns = ['/uscms_data/d2/tucker/crab_dirs/MinitreeV10_sidebanddata2015/JetHT2015D.root'] if year == 2015 else []
-mask_fn = '/uscms_data/d2/tucker/crab_dirs/MinitreeV10_sidebanddata2015/condor_JetHT2015D/lumiSummary.json'
-tree_path = 'tre%i%i/t' % (ntracks, ntracks) if ntracks < 5 else 'mfvMiniTree/t'
+if year == 2015:
+    fns = [
+        '/uscms_data/d2/tucker/crab_dirs/MinitreeV10_sidebanddata2015/JetHT2015C.root',
+        '/uscms_data/d2/tucker/crab_dirs/MinitreeV10_sidebanddata2015/JetHT2015D.root',
+        ]
+    mask_fn = '/uscms_data/d2/tucker/crab_dirs/MinitreeV10_sidebanddata2015/lumiSummary.json'
+else:
+    fns = []
+    mask_fn = None
+
+####
 
 ps = plot_saver(plot_dir(plot_path), size=(1000,400), log=False, pdf=True)
 
