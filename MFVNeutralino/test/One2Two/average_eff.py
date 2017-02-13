c43ede8573485e0620a912c663a2193937d2c5a9
Jennifer Chu <jyc72@cornell.edu>
2017-02-13 16:50:49 -0600

    V10 (background = qcd + ext + ttbar); extend efficiency curves from overlay method; overlay 3-track, 4-track, 5-track average efficiency plots; 5-track plots for analysis note.

--- MFVNeutralino/test/One2Two/average_eff.py
+++ MFVNeutralino/test/One2Two/average_eff.py
@@ -1,6 +1,6 @@
 from JMTucker.Tools.ROOTTools import *
 set_style()
-ps = plot_saver('../plots/bkgest/average_eff', size=(600,600), log=False, root=False)
+ps = plot_saver('../plots/bkgest/average_eff', size=(700,700), log=False, root=False)
 
 f1 = ROOT.TFile('eff.root')
 f2 = ROOT.TFile('~tucker/public/export_smoothed.root')
@@ -40,15 +40,19 @@ for n in ['', '_wevent', '_deltasvgaus', '_deltasvgaus_wevent']:
 fh = ROOT.TFile('eff_avg.root', 'recreate')
 for i in [3,4,5]:
     h1 = f1.Get('maxtk%i'%i)
-    h2 = f2.Get('ntk%i_deltasvgaus_wevent'%i)
-    h2.Rebin(100)
-    h2.Scale(0.01)
-    h2.SetBinContent(h2.GetNbinsX()+1, h2.GetBinContent(h2.GetNbinsX()))
+    hh = f2.Get('ntk%i_deltasvgaus_wevent'%i)
+    hh.Rebin(100)
+    hh.Scale(0.01)
+    hh.SetBinContent(hh.GetNbinsX()+1, hh.GetBinContent(hh.GetNbinsX()))
+    h2 = ROOT.TH1F(hh.GetName(), ';d_{VV} (cm);Efficiency', 100, 0, 1)
+    h2.SetStats(0)
+    for j in range(1, h2.GetNbinsX()+1):
+        h2.SetBinContent(j, hh.GetBinContent(j))
     h = ROOT.TH1F('average%i'%i, 'average (%s, %s);d_{VV} (cm);efficiency' % (h1.GetName(), h2.GetName()), 100, 0, 1)
     h.SetStats(0)
     h.GetYaxis().SetRangeUser(0,1.05)
-    for i in range(1, h.GetNbinsX()+1):
-        h.SetBinContent(i, 0.5 * (h1.GetBinContent(i) + h2.GetBinContent(i)))
+    for j in range(1, h.GetNbinsX()+1):
+        h.SetBinContent(j, 0.5 * (h1.GetBinContent(j) + h2.GetBinContent(j)))
     h1.SetLineColor(ROOT.kRed)
     h2.SetLineColor(ROOT.kBlue)
     h.SetLineColor(ROOT.kViolet)
@@ -66,4 +70,40 @@ for i in [3,4,5]:
     fh.cd()
     h.Write()
 
+    if i == 5:
+        h.SetTitle(';d_{VV} (cm);Efficiency')
+        h.GetXaxis().SetRangeUser(0,0.4)
+        h.SetLineWidth(3)
+        h.Draw()
+        ps.save('efficiency5')
+
+        h1.SetLineWidth(3)
+        h2.SetLineWidth(3)
+        h1.Draw('hist sames')
+        h2.Draw('sames')
+        l = ROOT.TLegend(0.50,0.15,0.85,0.30)
+        l.AddEntry(h1, 'vertexer method')
+        l.AddEntry(h2, 'overlay method')
+        l.AddEntry(h, 'average')
+        l.SetFillColor(0)
+        l.Draw()
+        ps.save('compare_efficiency')
+
 fh.Close()
+
+f = ROOT.TFile('eff_avg.root')
+for n in ['']:
+    l = ROOT.TLegend(0.50,0.15,0.85,0.30)
+    for i in [3,4,5]:
+        h = f.Get('average%i%s' % (i,n))
+        h.SetStats(0)
+        h.SetLineColor(colors[i])
+        if i == 3:
+            h.SetTitle('average%s;d_{VV} (cm);efficiency' % n)
+            h.Draw()
+        else:
+            h.Draw('sames')
+        l.AddEntry(h, h.GetName())
+    l.SetFillColor(0)
+    l.Draw()
+    ps.save('average%s' % n)
