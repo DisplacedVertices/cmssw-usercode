3b23af311562c563f792a250786c9548d2fb6b82
Jordan Tucker <jordantucker@gmail.com>
2017-02-19 13:29:17 -0600

    Allow getting skip list from params

--- Tools/plugins/EventIdVeto.cc
+++ Tools/plugins/EventIdVeto.cc
@@ -24,37 +24,55 @@ EventIdVeto::EventIdVeto(const edm::ParameterSet& cfg)
     debug(cfg.getUntrackedParameter<bool>("debug", false))
 {
   std::string fn(cfg.getParameter<std::string>("list_fn"));
-  printf("EventIdVeto: fn is %s\n", fn.c_str());
-  if (fn.length() > 3 && fn.compare(fn.length() - 3, 3, ".gz") == 0) {
-    printf("EventIdVeto: gunzipping\n");
-    if (system(("gunzip " + fn).c_str()) != 0)
-      throw cms::Exception("EventIdVeto") << "could not unzip file " << fn;
-    fn.erase(fn.length() - 3, 3);
-  }
 
-  FILE* f = fopen(fn.c_str(), "rt");
-  if (!f)
-    throw cms::Exception("EventIdVeto") << "could not read file " << fn;
+  if (fn.size()) {
+    printf("EventIdVeto: fn is %s\n", fn.c_str());
+    if (fn.length() > 3 && fn.compare(fn.length() - 3, 3, ".gz") == 0) {
+      printf("EventIdVeto: gunzipping\n");
+      if (system(("gunzip " + fn).c_str()) != 0)
+        throw cms::Exception("EventIdVeto") << "could not unzip file " << fn;
+      fn.erase(fn.length() - 3, 3);
+    }
 
-  char line[1024];
-  while (fgets(line, 1024, f) != 0) {
-    if (debug) printf("EventIdVeto debug: file line read: %s", line);
-    unsigned r,l;
-    unsigned long long e;
-    int res;
-    if (use_run)
-      res = sscanf(line, "(%u,%u,%llu),", &r, &l, &e);
-    else
-      res = sscanf(line, "(%u,%llu),", &l, &e);
-    if (debug) printf("    sscanf returned %i, r,l,e = %u, %u, %llu\n", res, r,l,e);
-    if (res != (use_run ? 3 : 2))
-      continue;
-    if (use_run)
-      mrle.insert(RLE(r,l,e));
-    else
-      mle.insert(LE(l,e));
+    FILE* f = fopen(fn.c_str(), "rt");
+    if (!f)
+      throw cms::Exception("EventIdVeto") << "could not read file " << fn;
+
+    char line[1024];
+    while (fgets(line, 1024, f) != 0) {
+      if (debug) printf("EventIdVeto debug: file line read: %s", line);
+      unsigned r,l;
+      unsigned long long e;
+      int res;
+      if (use_run)
+        res = sscanf(line, "(%u,%u,%llu),", &r, &l, &e);
+      else
+        res = sscanf(line, "(%u,%llu),", &l, &e);
+      if (debug) printf("    sscanf returned %i, r,l,e = %u, %u, %llu\n", res, r,l,e);
+      if (res != (use_run ? 3 : 2))
+        continue;
+      if (use_run)
+        mrle.insert(RLE(r,l,e));
+      else
+        mle.insert(LE(l,e));
+    }
+    fclose(f);
   }
-  fclose(f);
+  else {
+    const std::vector<unsigned> lumis = cfg.getParameter<std::vector<unsigned>>("lumis");
+    const size_t n = lumis.size();
+    const std::vector<unsigned long long> events = cfg.getParameter<std::vector<unsigned long long>>("events");
+    const std::vector<unsigned> runs = use_run ? cfg.getParameter<std::vector<unsigned>>("runs") : std::vector<unsigned>();
+
+    if ((use_run && runs.size() != n) || events.size() != n)
+      throw cms::Exception("EventIdVeto", "mismatched sizes");
+
+    for (size_t i = 0; i < n; ++i)
+      if (use_run)
+        mrle.insert(RLE(runs[i], lumis[i], events[i]));
+      else
+        mle.insert(LE(lumis[i], events[i]));
+  }
 
   const size_t nveto = use_run ? mrle.size() : mle.size();
   if (nveto == 0)
