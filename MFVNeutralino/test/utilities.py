0bbcd16770839e3f3d05fe2d45fc53088954ded2
Jordan Tucker <jordantucker@gmail.com>
2017-02-22 13:34:57 -0600

    Tired of typing: cmd_hadd_qcd_sum, cmd_merge_background

--- MFVNeutralino/test/utilities.py
+++ MFVNeutralino/test/utilities.py
@@ -1,11 +1,51 @@
-import sys
+#!/usr/bin/env python
+
+import sys, os
 from JMTucker.Tools import Samples
 from JMTucker.Tools import SampleFiles
 from JMTucker.Tools.hadd import hadd
+from JMTucker.MFVNeutralino import AnalysisConstants
+ 
+def cmd_hadd_vertexer_histos():
+    ntuple = sys.argv[2]
+    samples = Samples.registry.from_argv()
 
-ntuple = sys.argv[1]
-samples = Samples.registry.from_argv()
+    for s in samples:
+        s.set_curr_dataset(ntuple)
+        hadd(s.name + '.root', ['root://cmseos.fnal.gov/' + fn.replace('ntuple', 'vertex_histos') for fn in s.filenames])
 
-for s in samples:
-    s.set_curr_dataset(ntuple)
-    hadd(s.name + '.root', ['root://cmseos.fnal.gov/' + fn.replace('ntuple', 'vertex_histos') for fn in s.filenames])
+def cmd_hadd_qcd_sum():
+    for x in [500, 700, 1000, 1500, 2000]:
+        base = 'qcdht%04i' % x
+        a = base + '.root'
+        b = base + 'ext.root'
+        if not os.path.isfile(a) or not os.path.isfile(b):
+            print 'skipping', x, 'because at least one input file missing'
+        else:
+            hadd(base + 'sum.root', a, b)
+
+def cmd_merge_background():
+    files = ['ttbar.root']
+    files += ['qcdht%04isum.root' % x for x in [500, 700, 1000, 1500, 2000]]
+    for fn in files:
+        if not os.path.isfile(fn):
+            raise RuntimeError('%s not found' % fn)
+    scale = -AnalysisConstants.int_lumi * AnalysisConstants.scale_factor
+    cmd = 'python ' + os.environ['CMSSW_BASE'] + '/src/JMTucker/Tools/python/Samples.py merge %f background.root ' % scale
+    cmd += ' '.join(files)
+    print cmd
+    os.system(cmd)
+
+####
+
+cmd = sys.argv[1] if len(sys.argv) > 1 else ''
+cmds = locals()
+
+if not cmds.has_key('cmd_' + cmd):
+    print 'valid cmds are:'
+    for cmd in sorted(cmds.keys()):
+        if cmd.startswith('cmd_'):
+            print cmd.replace('cmd_', '')
+    sys.exit(1)
+
+cmds['cmd_' + cmd]()
