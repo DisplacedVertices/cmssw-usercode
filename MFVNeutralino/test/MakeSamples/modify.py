0372e12d2cab805bd824bfbe64efd93311c2abbe
Jordan Tucker <jordantucker@gmail.com>
2017-03-07 14:49:30 -0600

    set_oldduplicatemerge

--- MFVNeutralino/test/MakeSamples/modify.py
+++ MFVNeutralino/test/MakeSamples/modify.py
@@ -432,3 +432,10 @@ def set_cond15(process):
     prefer_it(process, 'tk15pixqual',   'frontier://FrontierProd/CMS_CONDITIONS', 'SiPixelQualityFromDbRcd',       'SiPixelQuality_v28_mc')
     prefer_it(process, 'tk15strqual',   'frontier://FrontierProd/CMS_CONDITIONS', 'SiStripBadChannelRcd',          'SiStripBadComponents_realisticMC_for2015_v1_mc')
     prefer_it(process, 'tkAlign',       'frontier://FrontierProd/CMS_CONDITIONS', 'TrackerAlignmentRcd',           'TrackerAlignment_run2Asymptotic_v1_mc')
+
+def set_oldduplicatemerge(process):
+    process.duplicateTrackClassifier.mva.maxChi2n = [9999., 9999., 9999.]
+    for x in process.duplicateTrackClassifier, process.jetCoreRegionalStep, process.muonSeededTracksInOutClassifier, process.muonSeededTracksOutInClassifier:
+        x.mva.maxDzWrtBS = [3.40282346639e+38, 3.40282346639e+38, 3.40282346639e+38] # I think the PV cuts get used, which are same
+        x.mva.minHits = [0, 0, 0]
+        x.mva.minNVtxTrk = 0
