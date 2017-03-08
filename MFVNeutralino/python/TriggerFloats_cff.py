0c957be2f0aa59cf8fda2186ad3fd49c2567745f
Jordan Tucker <jordantucker@gmail.com>
2017-03-08 15:17:40 -0600

    work with either 2015/6

--- MFVNeutralino/python/TriggerFloats_cff.py
+++ MFVNeutralino/python/TriggerFloats_cff.py
@@ -1,7 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
+from JMTucker.Tools.L1GtUtils_cff import l1GtUtilsTags
+
 mfvTriggerFloats = cms.EDProducer('MFVTriggerFloats',
-                                  l1_results_src = cms.InputTag('gtStage2Digis'),
+                                  l1GtUtilsTags, # will be ignored with 2016
+                                  l1_results_src = cms.InputTag('gtStage2Digis'), # ditto 2015
                                   trigger_results_src = cms.InputTag('TriggerResults', '', 'HLT'),
                                   trigger_objects_src = cms.InputTag('selectedPatTrigger'),
                                   prints = cms.untracked.bool(False),
