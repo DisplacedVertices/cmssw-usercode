f47fc0e81d2e619a0edff0a005e2f1653a3e3c63
Jordan Tucker <jordantucker@gmail.com>
2017-03-10 02:42:31 -0600

    add PFJet450 triggers; MetaSubmitter, 2015/6

--- Tools/test/pileup_dist.py
+++ Tools/test/pileup_dist.py
@@ -4,7 +4,7 @@ from JMTucker.Tools.BasicAnalyzer_cfg import *
 from_miniaod = False
 
 import JMTucker.Tools.SampleFiles as sf
-sf.set_process(process, 'mfv_neu_tau10000um_M0800', 'main', 1)
+sf.set_process(process, 'qcdht1000_2015', 'main', 5)
 
 process.TFileService.fileName = 'mc_pileup.root'
 
@@ -12,25 +12,29 @@ process.MCPileupDist = cms.EDAnalyzer('PileupDist',
                                       pileup_info_src = cms.InputTag('slimmedAddPileupInfo' if from_miniaod else 'addPileupInfo'),
                                       binning = cms.vdouble(100, 0, 100)
                                       )
-process.MCPileupDistHLTPFHT = process.MCPileupDist.clone()
+process.MCPileupDistHLT = process.MCPileupDist.clone()
 
 process.load('HLTrigger.HLTfilters.hltHighLevel_cfi')
-process.hltHighLevel.HLTPaths = ['HLT_PFHT800_v*', 'HLT_PFHT900_v*']
+process.hltHighLevel.HLTPaths = ['HLT_PFHT800_v*', 'HLT_PFHT900_v*', 'HLT_PFJet450_v*', 'HLT_AK8PFJet450_v*']
 process.hltHighLevel.andOr = True
 process.hltHighLevel.throw = False
 
-process.p = cms.Path(process.MCPileupDist * process.hltHighLevel * process.MCPileupDistHLTPFHT)
+process.p = cms.Path(process.MCPileupDist * process.hltHighLevel * process.MCPileupDistHLT)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples 
 
-    samples = [Samples.qcdht0500, Samples.qcdht0500ext, Samples.qcdht2000, Samples.qcdht2000ext]
+    # looks like it's OK to read older samples here just to get the addPileupInfo object
+    samples = [
+        Samples.qcdht0500, Samples.qcdht0500ext, Samples.qcdht2000, Samples.qcdht2000ext,
+        Samples.qcdht0500_2015, Samples.qcdht0500ext_2015, Samples.qcdht2000_2015, Samples.qcdht2000ext_2015,
+        Samples.mfv_neu_tau10000um_M0800_2015, Samples.official_mfv_neu_tau10000um_M0800
+        ]
 
-    from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
-    cs = CRABSubmitter('PileupDist_16_miniaod',
-                       dataset = 'miniaod',
-                       splitting = 'EventAwareLumiBased',
-                       units_per_job = 200000,
-                       total_units = -1,
-                       )
-    cs.submit_all(samples)
+    for s in samples:
+        s.files_per = 10
+
+    from JMTucker.Tools.MetaSubmitter import *
+    ms = MetaSubmitter('PileupDistV2')
+    ms.crab.job_control_from_sample = True
+    ms.submit(samples)
