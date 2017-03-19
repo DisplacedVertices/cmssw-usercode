6c96751edfce79d4d2c768901b38513d464c5935
Jordan Tucker <jordantucker@gmail.com>
2017-03-19 18:28:56 -0500

    better error message

--- MFVNeutralino/test/MakeSamples/fixfjr.py
+++ MFVNeutralino/test/MakeSamples/fixfjr.py
@@ -3,7 +3,9 @@ import xml.etree.ElementTree as ET
 x = ET.parse('tempfjr.xml')
 
 inputs = x.findall('InputFile')
-assert len(inputs) == 1
+if len(inputs) != 1:
+    raise ValueError('tempfjr.xml is weird, len(inputs) is %i' % len(inputs)
+
 x.getroot().remove(inputs[0])
 
 for f in x.findall('File'):
