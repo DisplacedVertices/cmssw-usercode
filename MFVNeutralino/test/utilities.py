56703e32f38d2647e3f7a7c3c6e792c417ca53a1
Jordan Tucker <jordantucker@gmail.com>
2017-03-22 05:56:45 -0500

    cmd_hadd_data

--- MFVNeutralino/test/utilities.py
+++ MFVNeutralino/test/utilities.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 import sys, os
+from glob import glob
 from JMTucker.Tools import Samples
 from JMTucker.Tools import SampleFiles
 from JMTucker.Tools.hadd import hadd
 from JMTucker.MFVNeutralino import AnalysisConstants
-
 
 def cmd_hadd_vertexer_histos():
     ntuple = sys.argv[2]
@@ -17,6 +17,21 @@ def cmd_hadd_vertexer_histos():
     for s in samples:
         s.set_curr_dataset(ntuple)
         hadd(s.name + '.root', ['root://cmseos.fnal.gov/' + fn.replace('ntuple', 'vertex_histos') for fn in s.filenames])
+
+def cmd_hadd_data():
+    for ds in 'SingleMuon', 'JetHT':
+        files = glob(ds + '*.root')
+        if not files:
+            continue
+        files.sort()  
+        if files != [ds + '2015%s.root' % x for  x in 'CD'] + [ds + '2016%s.root' % x for x in ('B3', 'C', 'D', 'E', 'F', 'G', 'H2', 'H3')]:
+            print 'some files missing for', ds
+        else:
+            hadd(ds + '2015.root', [ds + '2015%s.root' % x for x in 'CD'])
+            hadd(ds + '2016BthruG.root', [ds + '2016%s.root' % x for x in ('B3', 'C', 'D', 'E', 'F', 'G')])
+            hadd(ds + '2016BCD.root', [ds + '2016%s.root' % x for x in ('B3', 'C', 'D')])
+            hadd(ds + '2016EFG.root', [ds + '2016%s.root' % x for x in ('E', 'F', 'G')])
+            hadd(ds + '2016H.root', [ds + '2016%s.root' % x for x in ('H2', 'H3')])
 
 def cmd_hadd_qcd_sum():
     for is2015_s in '_2015', '':
