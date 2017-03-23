46e437b041143ba3d43d5e5f44e2ae24b768416d
Jordan Tucker <jordantucker@gmail.com>
2017-03-23 05:59:20 -0500

    do separate fits in each era, improve drawing, print results to stdout instead of on the plot

--- MFVNeutralino/test/ByRun/base.py
+++ MFVNeutralino/test/ByRun/base.py
@@ -102,6 +102,7 @@ class ByRunPlotter:
             r.g.SetPointEYlow(i, el)
             r.g.SetPointEYhigh(i, eh)
 
+        r.runs_missing = len(na)
         r.g_na = ROOT.TGraphAsymmErrors(len(na))
         for i, irun in enumerate(na):
             r.g_na.SetPoint(i, irun, 0)
@@ -120,24 +121,67 @@ class ByRunPlotter:
         t = ROOT.TLatex()
         t.SetTextFont(42)
 
-        r.g.SetTitle('%s  # runs: %i;run index;%s' % (title, r.runs_used, y_title))
+        r.g.SetTitle('%s  %i runs, %i missing;run index;%s' % (title, r.runs_used, r.runs_missing, y_title))
         r.g.Draw('AP')
 
         if do_fits:
-            a = tgraph_getpoint(r.g,  0)[0] - 100
-            b = tgraph_getpoint(r.g, -1)[0] + 100
-            r.fcn0 = ROOT.TF1('fcn0', 'pol0', a,b)
-            r.fcn1 = ROOT.TF1('fcn1', 'pol1', a,b)
-            r.fcn0.SetLineColor(ROOT.kRed)
-            r.fcn1.SetLineColor(ROOT.kGreen+2)
+            miss = []
+            r.fcns, r.frs = [], []
+            ranges = era_boundaries[:]
+            ranges.insert(0, 0)
+            ranges.append(r.runs_used)
+            first = None
+            for i in xrange(len(ranges)-1):
+                a, b = ranges[i], ranges[i+1]-1
+                fcn = ROOT.TF1('fcn%i' % i, 'pol0', a, b)
+                fcn.SetLineColor(ROOT.kRed)
+                r.fcns.append(fcn)
+                fr = r.g.Fit(fcn, 'QRS')
+                fy  = fcn.GetParameter(0)
+                fye = fcn.GetParError(0)
+                if not first:
+                    first = fy, fye, fye/fy
+                    ratio = 1,0
+                else:
+                    ratio = fy/first[0], 0 if i == 0 else ((fye/fy)**2 + first[2]**2)**0.5
+                print '%i (%3i) - %i (%3i): %.3f +- %.3f  (%.4f +- %.4f)  chi2/ndf = %6.3f/%3i -> prob = %f' % (r.runs[a], a, r.runs[b], b, fy, fye, ratio[0], ratio[1], fr.Chi2(), fr.Ndf(), fr.Prob()),
+                r.frs.append(fr)
+                these_miss = []
+                for j in xrange(a, b+1):
+                    x,y = tgraph_getpoint(r.g, j)
+                    yl = y - r.g.GetErrorYlow(j)
+                    yh = y + r.g.GetErrorYhigh(j)
+                    if fy < yl or fy > yh:
+                        these_miss.append((x,y,yl,yh))
+                n = b-a+1
+                print '  %3i / %3i = %0.2f miss' % (len(these_miss), n, float(len(these_miss))/n)
+                miss.extend(these_miss)
+            for fcn in r.fcns:
+                fcn.Draw('same')
 
-            fr0 = r.g.Fit(r.fcn0, 'QRS')
-            fr1 = r.g.Fit(r.fcn1, 'QRS')
-            r.fcn0.Draw('same')
+        if do_fits and miss:
+            r.g_miss = ROOT.TGraphAsymmErrors(len(miss))
+            for i,(x,y,yl,yh) in enumerate(miss):
+                r.g_miss.SetPoint(i,x,y)
+                r.g_miss.SetPointEYlow(i, y-yl)
+                r.g_miss.SetPointEYhigh(i, yh-y)
+            r.g_miss.SetMarkerColor(ROOT.kOrange+2)
+            r.g_miss.SetMarkerStyle(r.g.GetMarkerStyle())
+            r.g_miss.SetMarkerSize(r.g.GetMarkerSize())
+            r.g_miss.SetLineColor(ROOT.kOrange+2)
+            r.g_miss.Draw('P')
+
+        r.g_na.SetMarkerColor(ROOT.kBlue)
+        r.g_na.Draw('P')
+
+        if do_fits:
+            t.DrawLatexNDC(0.12, 0.82, '#color[802]{%i / %i = %.2f points miss fit}' % (len(miss), r.g.GetN(), float(len(miss))/r.g.GetN()))
+
+        r.g.GetXaxis().SetRangeUser(-2, r.nruns+2)
+        r.g.GetYaxis().SetRangeUser(0, 7 * r.fcns[0].GetParameter(0))
 
         hh = r.g.GetHistogram()
         ymin, ymax = hh.GetMinimum(), hh.GetMaximum()
-
         fill_lines = []
         for i in fill_boundaries + era_boundaries:
             l = ROOT.TLine(i-0.5, ymin, i-0.5, ymax)
@@ -148,47 +192,7 @@ class ByRunPlotter:
                 l.SetLineWidth(2)
             l.Draw()
 
-        if do_fits:
-            miss = []
-            fy = r.fcn0.GetParameter(0)
-            for j in xrange(r.g.GetN()):
-                x,y = tgraph_getpoint(r.g, j)
-                yl = y - r.g.GetErrorYlow(j)
-                yh = y + r.g.GetErrorYhigh(j)
-                if fy < yl or fy > yh:
-                    miss.append((x,y,yl,yh))
-            if miss:
-                g_miss = ROOT.TGraphAsymmErrors(len(miss))
-                for i,(x,y,yl,yh) in enumerate(miss):
-                    g_miss.SetPoint(i,x,y)
-                    g_miss.SetPointEYlow(i, y-yl)
-                    g_miss.SetPointEYhigh(i, yh-y)
-                g_miss.SetMarkerColor(ROOT.kOrange+2)
-                g_miss.SetMarkerStyle(r.g.GetMarkerStyle())
-                g_miss.SetMarkerSize(r.g.GetMarkerSize())
-                g_miss.SetLineColor(ROOT.kOrange+2)
-                g_miss.Draw('P')
-
-            s0 = 'pol0 fit #chi^{2}/ndf = %.2f / %i  prob = %.2f  p0 = %.2g #pm %.2g' % (fr0.Chi2(), fr0.Ndf(), fr0.Prob(), r.fcn0.GetParameter(0), r.fcn0.GetParError(0))
-            s1 = 'pol1 fit #chi^{2}/ndf = %.2f / %i  prob = %.2f  p0 = %.2g #pm %.2g  p1 = %.2g #pm %.2g' % (fr1.Chi2(), fr1.Ndf(), fr1.Prob(), r.fcn1.GetParameter(0), r.fcn1.GetParError(0), r.fcn1.GetParameter(1), r.fcn1.GetParError(1))
-
-        r.g_na.SetMarkerColor(ROOT.kBlue)
-        r.g_na.Draw('P')
-
-        if do_fits:
-            t.DrawLatexNDC(0.12, 0.82, '#color[632]{%s}' % s0)
-            t.DrawLatexNDC(0.12, 0.76, '#color[418]{%s}' % s1)
-            t.DrawLatexNDC(0.12, 0.64, '#color[802]{%i / %i = %.2f points miss pol0 fit}' % (len(miss), r.g.GetN(), float(len(miss))/r.g.GetN()))
-        t.DrawLatexNDC(0.12, 0.70, '#color[600]{# missing runs: %i}' % r.g_na.GetN())
-
-        r.g.GetXaxis().SetRangeUser(-2, r.nruns+2)
-
         self.ps.save(name)
-
-        if do_fits:
-            r.g.GetYaxis().SetRangeUser(0, 6 * r.fcn0.GetParameter(0))
-            self.ps.save(name + '_zoom')
-
         return r
 
 class EventFilter:
