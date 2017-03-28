06fbaee58256bf5874f94561f5cc8f8f7096042e
Jennifer Chu <jyc72@cornell.edu>
2017-03-28 12:12:07 -0500

    Run for {2015, 2016, 2015+2016} yields x {3, 4, 5} tracks.

--- MFVNeutralino/test/One2Two/statmodel.sh
+++ MFVNeutralino/test/One2Two/statmodel.sh
@@ -6,17 +6,14 @@ wd=$(pwd)
 job=$1
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd /cvmfs/cms.cern.ch/slc6_amd64_gcc493/cms/cmssw-patch/CMSSW_7_6_3_patch2/src
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_8_0_25/src
 eval `scramv1 runtime -sh`
 cd $wd
 
 ntracks=$((3+job%3))
-oversample_a=(1 2 5 10 20)
-oversample=${oversample_a[$((job/3))]}
-echo ntracks $ntracks oversample $oversample
+year_index=$((job/3))
+echo ntracks $ntracks year_index $year_index
 
-out_fn=sm_ntk${ntracks}_os${oversample}
+out_fn=sm_ntk${ntracks}_year${year_index}
 
-env sm_ntracks=${ntracks} sm_out_fn=${out_fn} sm_oversample=${oversample} ./statmodel.exe 2>&1 > ${out_fn}.log
-
-
+env sm_ntracks=${ntracks} sm_out_fn=${out_fn} sm_year_index=${year_index} ./statmodel.exe 2>&1 > ${out_fn}.log
