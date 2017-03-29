3bfb887497d351b7404d9bd2559b75e4358b32f5
Jordan Tucker <jordantucker@gmail.com>
2017-03-29 14:17:51 -0500

    try to protect against idiots

--- Tools/interface/ByRunTH1.h
+++ Tools/interface/ByRunTH1.h
@@ -8,6 +8,8 @@
 template <typename T>
 class ByRunTH1 {
  public:
+   ByRunTH1() : fs_(nullptr) {}
+
   void set(edm::Service<TFileService>* fs, const char* name, const char* title, const int nbins, const double xmin, const double xmax) {
     fs_ = fs;
     name_ = name;
@@ -18,12 +20,14 @@ class ByRunTH1 {
   }
 
   T* operator[](unsigned run) {
+    if (fs_ == nullptr) throw cms::Exception("NotInitialized", "ByRunTH1 instance used without call to set");
     if (hist_map_.find(run) == hist_map_.end())
       book(run); 
     return hist_map_[run];
   }
 
   void book(unsigned run) {
+    if (fs_ == nullptr) throw cms::Exception("NotInitialized", "ByRunTH1 instance used without call to set");
     if (hist_map_.find(run) == hist_map_.end())
       hist_map_[run] = (*fs_)->make<T>(TString::Format("%s_run%u", name_.Data(), run), 
                                        TString::Format("%s (run %u)", title_.Data(), run), 
