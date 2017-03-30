2ed2106cf97334911543f11ffe771bd2831cb9e4
Jordan Tucker <jordantucker@gmail.com>
2017-03-30 15:29:11 -0500

    ht, myhtt(wbug)_m_l1htt cuts

--- MFVNeutralino/plugins/TriggerFloatsFilter.cc
+++ MFVNeutralino/plugins/TriggerFloatsFilter.cc
@@ -14,11 +14,17 @@ private:
   const edm::EDGetTokenT<mfv::TriggerFloats> triggerfloats_token;
 
   const double hltht_cut;
+  const double ht_cut;
+  const double myhtt_m_l1htt_cut;
+  const double myhttwbug_m_l1htt_cut;
 };
 
 MFVTriggerFloatsFilter::MFVTriggerFloatsFilter(const edm::ParameterSet& cfg) 
   : triggerfloats_token(consumes<mfv::TriggerFloats>(edm::InputTag("mfvTriggerFloats"))),
-    hltht_cut(cfg.getParameter<double>("hltht_cut"))
+    hltht_cut(cfg.getParameter<double>("hltht_cut")),
+    ht_cut(cfg.getParameter<double>("ht_cut")),
+    myhtt_m_l1htt_cut(cfg.getParameter<double>("myhtt_m_l1htt_cut")),
+    myhttwbug_m_l1htt_cut(cfg.getParameter<double>("myhttwbug_m_l1htt_cut"))
 {
 }
 
@@ -27,6 +33,15 @@ bool MFVTriggerFloatsFilter::filter(edm::Event& event, const edm::EventSetup&) {
   event.getByToken(triggerfloats_token, triggerfloats);
 
   if (hltht_cut > 0 && triggerfloats->hltht < hltht_cut)
+    return false;
+
+  if (ht_cut > 0 && triggerfloats->ht < ht_cut)
+    return false;
+
+  if (myhtt_m_l1htt_cut > 0 && fabs(triggerfloats->myhtt - triggerfloats->l1htt) < myhtt_m_l1htt_cut)
+    return false;
+
+  if (myhttwbug_m_l1htt_cut > 0 && fabs(triggerfloats->myhttwbug - triggerfloats->l1htt) < myhttwbug_m_l1htt_cut)
     return false;
 
   return true;
--- MFVNeutralino/python/TriggerFloatsFilter_cfi.py
+++ MFVNeutralino/python/TriggerFloatsFilter_cfi.py
@@ -2,4 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 mfvTriggerFloatsFilter = cms.EDFilter('MFVTriggerFloatsFilter',
                                       hltht_cut = cms.double(-1),
+                                      ht_cut = cms.double(-1),
+                                      myhtt_m_l1htt_cut = cms.double(-1),
+                                      myhttwbug_m_l1htt_cut = cms.double(-1),
                                       )
