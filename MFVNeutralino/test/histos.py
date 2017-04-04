bfc1d373560b2078f9f9a2ef61558ce914c2271d
Jennifer Chu <jyc72@cornell.edu>
2017-04-04 12:23:49 -0500

    Also run nm1 paths for Ntk3, Ntk4, and Ntk3or4.

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -24,35 +24,43 @@ nm1s = [
 
 #nm1s = []
 
-for name, cut in nm1s:
-    evt_cut = ''
-    if type(cut) == tuple:
-        cut, evt_cut = cut
+for ntk in (5,3,4,7):
+    if ntk == 5:
+        EX = ''
+    elif ntk == 7:
+        EX = 'Ntk3or4'
+    else:
+        EX = 'Ntk%i' % ntk
 
-    vtx = eval('process.mfvSelectedVerticesTight.clone(%s)' % cut)
-    vtx_name = 'vtxNo' + name
+    for name, cut in nm1s:
+        evt_cut = ''
+        if type(cut) == tuple:
+            cut, evt_cut = cut
 
-    for nv in (1,2):
-        ana = eval('process.mfvAnalysisCuts.clone(%s)' % evt_cut)
-        ana.vertex_src = vtx_name
-        if nv == 1:
-            ana.max_nvertex = nv
-        ana.min_nvertex = nv
-        ana_name = 'ana%iVNo' % nv + name
+        vtx = eval('process.mfvSelectedVerticesTight%s.clone(%s)' % (EX, cut))
+        vtx_name = '%svtxNo' % EX + name
 
-        evt_hst = process.mfvEventHistos.clone()
-        evt_hst_name = 'evtHst%iVNo' % nv + name
+        for nv in (1,2):
+            ana = eval('process.mfvAnalysisCuts.clone(%s)' % evt_cut)
+            ana.vertex_src = vtx_name
+            if nv == 1:
+                ana.max_nvertex = nv
+            ana.min_nvertex = nv
+            ana_name = '%sana%iVNo' % (EX, nv) + name
 
-        vtx_hst = process.mfvVertexHistos.clone(vertex_src = vtx_name)
-        if nv == 1:
-            vtx_hst.do_only_1v = True
-        vtx_hst_name = 'vtxHst%iVNo' % nv + name
+            evt_hst = process.mfvEventHistos.clone()
+            evt_hst_name = '%sevtHst%iVNo' % (EX, nv) + name
 
-        setattr(process, vtx_name, vtx)
-        setattr(process, ana_name, ana)
-        setattr(process, evt_hst_name, evt_hst)
-        setattr(process, vtx_hst_name, vtx_hst)
-        setattr(process, 'p%iV' % nv + name, cms.Path(vtx * ana * evt_hst * vtx_hst))
+            vtx_hst = process.mfvVertexHistos.clone(vertex_src = vtx_name)
+            if nv == 1:
+                vtx_hst.do_only_1v = True
+            vtx_hst_name = '%svtxHst%iVNo' % (EX, nv) + name
+
+            setattr(process, vtx_name, vtx)
+            setattr(process, ana_name, ana)
+            setattr(process, evt_hst_name, evt_hst)
+            setattr(process, vtx_hst_name, vtx_hst)
+            setattr(process, '%sp%iV' % (EX, nv) + name, cms.Path(vtx * ana * evt_hst * vtx_hst))
 
 def force_bs(process, bs):
     for ana in process.analyzers:
