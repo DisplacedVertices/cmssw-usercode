a22d1ceded6473dc6e79be69b44bee28225d8ca1
Jordan Tucker <jordantucker@gmail.com>
2017-04-05 16:47:34 -0500

    Put Histos_cff stuff into histos.py so there aren't two places for no reason

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -1,17 +1,24 @@
 import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import *
 
-import JMTucker.Tools.SampleFiles as sf
-sf.set_process(process, 'qcdht2000', 'ntuplev12', 10)
-
+sample_files(process, 'qcdht2000', 'ntuplev12', 1)
 process.TFileService.fileName = 'histos.root'
 process.maxEvents.input = -1
 file_event_from_argv(process)
 
-process.load('JMTucker.MFVNeutralino.Histos_cff')
+process.load('JMTucker.MFVNeutralino.VertexSelector_cfi')
+process.load('JMTucker.MFVNeutralino.WeightProducer_cfi')
+process.load('JMTucker.MFVNeutralino.VertexHistos_cfi')
+process.load('JMTucker.MFVNeutralino.EventHistos_cfi')
+process.load('JMTucker.MFVNeutralino.AnalysisCuts_cfi')
 
 import JMTucker.Tools.SimpleTriggerResults_cfi as SimpleTriggerResults
 SimpleTriggerResults.setup_endpath(process, weight_src='mfvWeight')
+
+common = cms.Sequence(process.mfvSelectedVerticesSeq * process.mfvWeight)
+
+process.mfvEventHistosNoCuts = process.mfvEventHistos.clone()
+process.pSkimSel = cms.Path(common * process.mfvEventHistosNoCuts) # just trigger for now
 
 nm1s = [
     ('Njets',      ('', 'min_njets = 0')),
@@ -24,43 +31,74 @@ nm1s = [
 
 #nm1s = []
 
-for ntk in ([3]):
+ntks = [5,3,4,7]
+nvs = [1,2]
+ntks = [3] 
+nvs = [1]
+
+for ntk in ntks:
     if ntk == 5:
-        EX = ''
+        EX1 = EX2 = ''
     elif ntk == 7:
-        EX = 'Ntk3or4'
+        EX1 = 'Ntk3or4'
     else:
-        EX = 'Ntk%i' % ntk
+        EX1 = 'Ntk%i' % ntk
+    if EX1:
+        EX2 = "vertex_src = 'mfvSelectedVerticesTight%s', " % EX1
+
+    exec '''
+process.EX1mfvAnalysisCutsPreSel     = process.mfvAnalysisCuts.clone(EX2apply_vertex_cuts = False)
+process.EX1mfvAnalysisCutsOnlyOneVtx = process.mfvAnalysisCuts.clone(EX2min_nvertex = 1, max_nvertex = 1)
+process.EX1mfvAnalysisCutsFullSel    = process.mfvAnalysisCuts.clone(EX2)
+process.EX1mfvAnalysisCutsSigReg     = process.mfvAnalysisCuts.clone(EX2min_svdist2d = 0.04)
+
+process.EX1mfvEventHistosPreSel     = process.mfvEventHistos.clone()
+process.EX1mfvEventHistosOnlyOneVtx = process.mfvEventHistos.clone()
+process.EX1mfvEventHistosFullSel    = process.mfvEventHistos.clone()
+process.EX1mfvEventHistosSigReg     = process.mfvEventHistos.clone()
+
+process.EX1mfvVertexHistosPreSel     = process.mfvVertexHistos.clone(EX2)
+process.EX1mfvVertexHistosOnlyOneVtx = process.mfvVertexHistos.clone(EX2)
+process.EX1mfvVertexHistosFullSel    = process.mfvVertexHistos.clone(EX2)
+process.EX1mfvVertexHistosSigReg     = process.mfvVertexHistos.clone(EX2)
+
+process.EX1pPreSel     = cms.Path(common * process.EX1mfvAnalysisCutsPreSel     * process.EX1mfvEventHistosPreSel     * process.EX1mfvVertexHistosPreSel)
+process.EX1pOnlyOneVtx = cms.Path(common * process.EX1mfvAnalysisCutsOnlyOneVtx * process.EX1mfvEventHistosOnlyOneVtx * process.EX1mfvVertexHistosOnlyOneVtx)
+'''.replace('EX1', EX1).replace('EX2', EX2)
+
+    if 2 in nvs:
+        exec '''
+process.EX1pFullSel    = cms.Path(common * process.EX1mfvAnalysisCutsFullSel    * process.EX1mfvEventHistosFullSel    * process.EX1mfvVertexHistosFullSel)
+process.EX1pSigReg     = cms.Path(common * process.EX1mfvAnalysisCutsSigReg     * process.EX1mfvEventHistosSigReg     * process.EX1mfvVertexHistosSigReg)
+'''.replace('EX1', EX1).replace('EX2', EX2)
 
     for name, cut in nm1s:
         evt_cut = ''
         if type(cut) == tuple:
             cut, evt_cut = cut
 
-        vtx = eval('process.mfvSelectedVerticesTight%s.clone(%s)' % (EX, cut))
-        vtx_name = '%svtxNo' % EX + name
+        vtx = eval('process.mfvSelectedVerticesTight%s.clone(%s)' % (EX1, cut))
+        vtx_name = '%svtxNo' % EX1 + name
 
-        for nv in ([1]):
+        for nv in nvs:
             ana = eval('process.mfvAnalysisCuts.clone(%s)' % evt_cut)
             ana.vertex_src = vtx_name
             if nv == 1:
                 ana.max_nvertex = nv
             ana.min_nvertex = nv
-            ana_name = '%sana%iVNo' % (EX, nv) + name
+            ana_name = '%sana%iVNo' % (EX1, nv) + name
 
             evt_hst = process.mfvEventHistos.clone()
-            evt_hst_name = '%sevtHst%iVNo' % (EX, nv) + name
+            evt_hst_name = '%sevtHst%iVNo' % (EX1, nv) + name
 
             vtx_hst = process.mfvVertexHistos.clone(vertex_src = vtx_name)
-            if nv == 1:
-                vtx_hst.do_only_1v = True
-            vtx_hst_name = '%svtxHst%iVNo' % (EX, nv) + name
+            vtx_hst_name = '%svtxHst%iVNo' % (EX1, nv) + name
 
             setattr(process, vtx_name, vtx)
             setattr(process, ana_name, ana)
             setattr(process, evt_hst_name, evt_hst)
             setattr(process, vtx_hst_name, vtx_hst)
-            setattr(process, '%sp%iV' % (EX, nv) + name, cms.Path(vtx * ana * evt_hst * vtx_hst))
+            setattr(process, '%sp%iV' % (EX1, nv) + name, cms.Path(vtx * ana * evt_hst * vtx_hst))
 
 def force_bs(process, bs):
     for ana in process.analyzers:
@@ -87,13 +125,13 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     def modify(sample):
         to_add, to_replace = [], []
         if not sample.is_mc:
-            to_add.extend(['del process.pFullSel', 'del process.pSigReg'])
+            to_add.extend(['if hasattr(process, "pFullSel"):\n  del process.pFullSel', 'if hasattr(process, "pSigReg"):\n  del process.pSigReg'])
         return to_add, to_replace
 
     from JMTucker.Tools.CondorSubmitter import CondorSubmitter
     cs = CondorSubmitter('HistosV12',
                          ex = year,
                          dataset = 'ntuplev12',
-                         #pset_modifier = modify
+                         pset_modifier = modify,
                          )
     cs.submit_all(samples)
