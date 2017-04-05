922712c6ab129a44e951a1ec31dbfe126072f107
Jordan Tucker <jordantucker@gmail.com>
2017-04-05 12:02:37 -0500

    fjr2ll: inputFile can have empty Runs

--- Tools/python/LumiJSONTools.py
+++ Tools/python/LumiJSONTools.py
@@ -10,27 +10,30 @@ def fjr2ll(fjr_fn):
     runsLumisDict = {}
     obj = xml2obj(filename=fjr_fn)
     for inputFile in obj.InputFile:
-        try: # Regular XML version, assume only one of these
-            runObjects = inputFile.Runs.Run
-            for run in runObjects:
-                runNumber = int(run.ID)
-                runList = runsLumisDict.setdefault(runNumber, [])
-                for lumiPiece in run.LumiSection:
-                    lumi = int(lumiPiece.ID)
-                    runList.append(lumi)
-        except:
-            if isinstance(inputFile.Runs, basestring):
-                runObjects = [inputFile.Runs]
-            else:
-                runObjects = inputFile.Runs
+        if not inputFile.Runs:
+            assert inputFile.Runs == ''
+        else:
+            try: # Regular XML version, assume only one of these
+                runObjects = inputFile.Runs.Run
+                for run in runObjects:
+                    runNumber = int(run.ID)
+                    runList = runsLumisDict.setdefault(runNumber, [])
+                    for lumiPiece in run.LumiSection:
+                        lumi = int(lumiPiece.ID)
+                        runList.append(lumi)
+            except:
+                if isinstance(inputFile.Runs, basestring):
+                    runObjects = [inputFile.Runs]
+                else:
+                    runObjects = inputFile.Runs
 
-            for runObject in runObjects:
-                try:
-                    runs = ast.literal_eval(runObject)
-                    for run, lumis in runs.iteritems():
-                        runList = runsLumisDict.setdefault(int(run), [])
-                        runList.extend(lumis)
-                except ValueError: # Old style handled above
-                    pass
+                for runObject in runObjects:
+                    try:
+                        runs = ast.literal_eval(runObject)
+                        for run, lumis in runs.iteritems():
+                            runList = runsLumisDict.setdefault(int(run), [])
+                            runList.extend(lumis)
+                    except ValueError: # Old style handled above
+                        pass
 
     return LumiList(runsAndLumis=runsLumisDict)
