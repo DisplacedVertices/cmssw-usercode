e48e638c50561c23017b30c08e1d02ee2357d8c7
Jordan Tucker <jordantucker@gmail.com>
2017-04-11 17:49:18 -0500

    min/max_pt_for_ht instead of calculating 3 random ones

--- MFVNeutralino/python/JetFilter_cfi.py
+++ MFVNeutralino/python/JetFilter_cfi.py
@@ -3,6 +3,8 @@ import FWCore.ParameterSet.Config as cms
 mfvJetFilter = cms.EDFilter('MFVJetFilter',
                             jets_src = cms.InputTag('selectedPatJets'),
                             min_njets = cms.int32(4),
+                            min_pt_for_ht = cms.double(40),
+                            max_pt_for_ht = cms.double(1e9),
                             min_ht = cms.double(1000),
                             debug = cms.untracked.bool(False),
                             )
