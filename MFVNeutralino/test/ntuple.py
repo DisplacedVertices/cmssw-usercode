dc65d8cc8c5d3c1e8d2a864101be250d32416662
Jordan Tucker <jordantucker@gmail.com>
2017-04-12 18:31:57 -0500

    v13

--- MFVNeutralino/test/ntuple.py
+++ MFVNeutralino/test/ntuple.py
@@ -9,7 +9,7 @@ minitree_only = False
 prepare_vis = False
 keep_all = prepare_vis
 event_filter = not keep_all
-version = 'V12'
+version = 'V13'
 batch_name = 'Ntuple' + version
 if minitree_only:
     batch_name = 'MiniNtuple'  + version
