65f14150bd9333458ef9638bac7e5b3e929a40ef
Jordan Tucker <jordantucker@gmail.com>
2017-04-13 16:39:11 -0500

    check for latest trig+event filter

--- MFVNeutralino/test/trigfiltcheck.py
+++ MFVNeutralino/test/trigfiltcheck.py
@@ -1,52 +1,28 @@
 import sys
-from JMTucker.Tools.BasicAnalyzer_cfg import cms, global_tag, process, file_event_from_argv
-from JMTucker.Tools.MiniAOD_cfg import which_global_tag
+from JMTucker.Tools.CMSSWTools import *
+from JMTucker.Tools.MiniAOD_cfg import *
 from JMTucker.MFVNeutralino.Year import year
 
+is_mc = True
+
+process = pat_tuple_process(None, is_mc, year)
+jets_only(process)
+
 file_event_from_argv(process)
-process.TFileService.fileName = 'trigeff.root'
-global_tag(process, which_global_tag(True, year)) # needed for emulator that checks L1 bits
+tfileservice(process, 'trigfiltcheck.root')
 
-from JMTucker.MFVNeutralino.EventFilter import *
-setup_event_filter_soup(process, 'pmfvfilt')
-setup_event_filter_soup(process, 'ponlyhtpt', 'tfonlyhtpt')
-setup_event_filter_soup(process, 'ponlyht', 'tfonlyht')
-setup_event_filter_soup(process, 'ponlyht8', 'tfonlyht8')
-setup_event_filter_soup(process, 'ponlyht9', 'tfonlyht9')
-setup_event_filter(process, 'pemuht8',             need_pat=True)
-setup_event_filter(process, 'pemuht9', 'emuht900', need_pat=True)
-
-process.emuht900.return_actual = False
-process.emuht900.return_ht900 = True
-
-#process.options.wantSummary = True
-
-process.tfonlyhtpt.HLTPaths = [
-    'HLT_PFHT650_v*',
-    'HLT_PFHT800_v*',
-    'HLT_PFHT900_v*',
-    'HLT_PFHT550_4Jet_v*',
-    'HLT_PFHT450_SixJet40_v*',
-    'HLT_PFHT400_SixJet30_v*',
-    ]
-
-process.tfonlyht.HLTPaths = [
-    'HLT_PFHT650_v*',
-    'HLT_PFHT800_v*',
-    'HLT_PFHT900_v*',
-    ]
-
-process.tfonlyht8.HLTPaths = [
-    'HLT_PFHT800_v*',
-    ]
-
-process.tfonlyht9.HLTPaths = [
-    'HLT_PFHT900_v*',
-    ]
+from JMTucker.MFVNeutralino.EventFilter import setup_event_filter
+setup_event_filter(process, event_filter=True)
 
 import JMTucker.Tools.SimpleTriggerEfficiency_cfi as SimpleTriggerEfficiency
 SimpleTriggerEfficiency.setup_endpath(process)
 
+from JMTucker.Tools import Samples
+sample = [x for x in sys.argv if hasattr(Samples, x)][0]
+sample_files(process, sample, 'main', 100)
+tfileservice(process, 'trigfiltcheck_%s.root' % sample)
+process.maxEvents.input = 1000
+want_summary(process)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.CRAB3Submitter import CRABSubmitter
