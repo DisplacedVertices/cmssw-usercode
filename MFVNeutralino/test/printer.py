bead7a21062377668d3addfa624627c1ea2700de
Jordan Tucker <jordantucker@gmail.com>
2017-04-13 10:52:39 -0500

    validation script

--- MFVNeutralino/test/printer.py
+++ MFVNeutralino/test/printer.py
@@ -76,3 +76,38 @@ if __name__ == '__main__' and 'splitlog' in sys.argv:
 
     if buf and rle is not None:
         save()
+
+elif __name__ == '__main__' and 'validation' in sys.argv:
+    x = {
+        'v12': {
+            'mfv_ddbar_tau00300um_M0400': (1, ['/store/user/tucker/mfv_ddbar_tau00300um_M0400/NtupleV12_validation_2016_mfv_ddbar_tau00300um_M0400/170412_192453/0000/ntuple_0.root']),
+            'mfv_ddbar_tau00300um_M0800': (1, ['/store/user/tucker/mfv_ddbar_tau00300um_M0800/NtupleV12_validation_2016_mfv_ddbar_tau00300um_M0800/170412_192453/0000/ntuple_0.root']),
+            'mfv_ddbar_tau01000um_M0400': (1, ['/store/user/tucker/mfv_ddbar_tau01000um_M0400/NtupleV12_validation_2016_mfv_ddbar_tau01000um_M0400/170412_192453/0000/ntuple_0.root']),
+            'mfv_ddbar_tau01000um_M0800': (1, ['/store/user/tucker/mfv_ddbar_tau01000um_M0800/NtupleV12_validation_2016_mfv_ddbar_tau01000um_M0800/170412_192453/0000/ntuple_0.root']),
+            'mfv_neu_tau00300um_M0800': (1, ['/store/user/tucker/mfv_neu_tau00300um_M0800/NtupleV12_validation_2016_mfv_neu_tau00300um_M0800/170412_192453/0000/ntuple_0.root']),
+            'mfv_neu_tau01000um_M0600': (1, ['/store/user/tucker/mfv_neu_tau01000um_M0600/NtupleV12_validation_2016_mfv_neu_tau01000um_M0600/170412_192453/0000/ntuple_0.root']),
+            'mfv_neu_tau30000um_M0800': (1, ['/store/user/tucker/mfv_neu_tau30000um_M0800/NtupleV12_validation_2016_mfv_neu_tau30000um_M0800/170412_192453/0000/ntuple_0.root']),
+            'qcdht0700': (1, ['/store/user/tucker/QCD_HT700to1000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NtupleV12_validation_2016_qcdht0700/170412_192453/0000/ntuple_0.root']),
+            'qcdht2000': (1, ['/store/user/tucker/QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/NtupleV12_validation_2016_qcdht2000/170412_192453/0000/ntuple_0.root']),
+            'ttbar': (1, ['/store/user/tucker/TTJets_TuneCUETP8M2T4_13TeV-amcatnloFXFX-pythia8/NtupleV12_validation_2016_ttbar/170412_192453/0000/ntuple_0.root']),
+            'JetHT2016B3': (1, ['/store/user/tucker/JetHT/NtupleV12_validation_2016_JetHT2016B3/170412_192453/0000/ntuple_0.root']),
+            'JetHT2016C': (1, ['/store/user/tucker/JetHT/NtupleV12_validation_2016_JetHT2016C/170412_192453/0000/ntuple_0.root']),
+            'JetHT2016D': (1, ['/store/user/tucker/JetHT/NtupleV12_validation_2016_JetHT2016D/170412_192453/0000/ntuple_0.root']),
+            'JetHT2016E': (1, ['/store/user/tucker/JetHT/NtupleV12_validation_2016_JetHT2016E/170412_192453/0000/ntuple_0.root']),
+            'JetHT2016F': (1, ['/store/user/tucker/JetHT/NtupleV12_validation_2016_JetHT2016F/170412_192453/0000/ntuple_0.root']),
+            'JetHT2016G': (1, ['/store/user/tucker/JetHT/NtupleV12_validation_2016_JetHT2016G/170412_192453/0000/ntuple_0.root']),
+            'JetHT2016H2': (1, ['/store/user/tucker/JetHT/NtupleV12_validation_2016_JetHT2016H2/170412_192453/0000/ntuple_0.root']),
+            },
+        'v13': {
+            }
+        }
+
+    which = sys.argv[1]
+    for s, (_, fns) in x[which].iteritems():
+        print s
+        out_fn = '/uscmst1b_scratch/lpc1/3DayLifetime/%s/%s.%s' % (os.environ['USER'], which, s)
+        os.system('cmsRun printer.py %s > %s 2>&1' % (fns[0], out_fn))
+        os.system('mlclean.py ' + out_fn)
+        assert os.path.isfile(out_fn + '.mlclean')
+        os.remove(out_fn)
+        print
