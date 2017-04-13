daf4e25ce99dd83b3b4d3095de6362a583c1c07d
Jordan Tucker <jordantucker@gmail.com>
2017-04-13 14:05:03 -0500

    do one/two vertex and ntk3,4 by cmd line argument

--- MFVNeutralino/test/striplayers_signal_eff.py
+++ MFVNeutralino/test/striplayers_signal_eff.py
@@ -6,11 +6,24 @@ from JMTucker.Tools.ROOTTools import *
 set_style()
 ps = plot_saver(plot_dir('stleff'), size=(900,400))
 
+nvtx = 1 if 'one' in sys.argv else 2
+ntk = ''
+if 'ntk3' in sys.argv:
+    ntk = 'Ntk3'
+elif 'ntk4' in sys.argv:
+    ntk = 'Ntk4'
+elif 'ntk3or4' in sys.argv:
+    ntk = 'Ntk3or4'
+
 fns = [x for x in sys.argv[1:] if x.endswith('.root')]
 fs = { fn : ROOT.TFile(fn) for fn in fns }
 samples = defaultdict(list)
 for fn in fns:
     s = os.path.basename(fn).replace('.root', '').replace('stl6_', '').replace('stl8_', '')
+    if s == 'mfv_neu_tau00300um_M0800':
+        continue
+        #s = 'my_' + s
+    s = s.replace('official_', '')
     samples[s].append(fn)
 samples = samples.items()
 samples.sort()
@@ -19,7 +32,7 @@ nsamples = len(samples)
 heff = {}
 seen = {}
 for x in 'nom', 'stl6', 'stl8':
-    for y in 'all', '400', '700':
+    for y in 'all', 'mid', 'far':
         name = '%s_%s' % (x,y)
         heff[name] = h = ROOT.TH1F(name, '', nsamples, 0, nsamples)
         h.Sumw2()
@@ -36,26 +49,36 @@ for isample, (s, s_fns) in enumerate(samples):
     hs = {}
     for ifn, fn in enumerate(s_fns):
         cn = os.path.basename(fn).replace('.root', '')
-        z = cn.replace(s, '').replace('_', '')
+        z = cn.replace(s, '').replace('official_', '').replace('_', '')
         if not z:
             z = 'nom'
 
         n = fs[fn].Get('mcStat/h_sums').GetBinContent(1)
-        t = fs[fn].Get('mfvMiniTree/t')
+        t = fs[fn].Get('mfvMiniTree%s/t' % ntk)
 
-        hs[z] = h = ROOT.TH1F('h_' + cn, ';d_{VV} (cm);events/0.1 cm', 40, 0, 4)
+        if nvtx == 2:
+            hs[z] = h = ROOT.TH1F('h_' + cn, ';d_{VV} (cm);events/0.1 cm', 40, 0, 4)
+        elif nvtx == 1:
+            hs[z] = h = ROOT.TH1F('h_' + cn, ';d_{BV} (cm);events/0.01 cm', 200, 0, 2)
         h.Sumw2()
         h.SetLineWidth(2)
         h.SetStats(0)
         h.SetLineColor(colors[z])
 
-        a = t.Draw('svdist>>%s' % h.GetName(), 'nvtx >= 2')
-        b = get_integral(h, 0.04)[0]
-        c = get_integral(h, 0.07)[0]
+        if nvtx == 2:
+            a = t.Draw('svdist>>%s' % h.GetName(), 'nvtx >= 2')
+            b = get_integral(h, 0.04)[0]
+            c = get_integral(h, 0.07)[0]
+            ranges = ('all', a), ('mid', b), ('far', c)
+        elif nvtx == 1:
+            a = t.Draw('dist0>>%s' % h.GetName(), 'nvtx >= 1')
+            b = get_integral(h, 0.02)[0]
+            c = get_integral(h, 0.035)[0]
+        
+        ranges = ('all', a), ('mid', b), ('far', c)
+        print z.ljust(6), '%6i/%6i -> %7.4f  >%s um: %6i -> %7.4f  >%s um: %6i -> %7.4f' % (int(a), n, a/n, ranges[1][0], int(b), b/n, ranges[2][0], int(c), c/n)
 
-        print z.ljust(6), '%6i/%6i -> %7.4f  >400 um: %6i -> %7.4f  >700 um: %6i -> %7.4f' % (int(a), n, a/n, int(b), b/n, int(c), c/n)
-
-        for which, j in ('all', a), ('400', b), ('700', c):
+        for which, j in ranges:
             e, ea, _ = wilson_score(a, n)
             ee = e - ea
             seen[(isample,z)] = True
@@ -68,7 +91,9 @@ for isample, (s, s_fns) in enumerate(samples):
             continue
         h = hs[name].Clone(s + name + 'norm')
         otherh.append(h)
-        h.Scale(1./h.Integral())
+        n = h.Integral()
+        if n > 0:
+            h.Scale(1./n)
         if len(otherh) == 1:
             h.Draw('hist')
         else:
@@ -89,7 +114,7 @@ for isample, (s, s_fns) in enumerate(samples):
             h_alt.Draw('e same')
     ps.save(s + '_rat', log=False)
 
-for y in 'all', '400', '700':
+for y in 'all', 'mid', 'far':
     h_nom = heff['nom_%s' % y]
     #h_nom.Draw()
     #ps.save('nom_%s' % y)
@@ -101,7 +126,6 @@ for y in 'all', '400', '700':
 
         h_diff = h_nom.Clone('h_diff_' + name)
         h_diff.GetYaxis().SetTitle('difference in absolute efficiency')
-        h_rel
         for ibin in xrange(1, nsamples+1):
             if not seen.has_key((ibin-1,'nom')) or not seen.has_key((ibin-1,x)):
                 h_diff.SetBinContent(ibin, 0)
