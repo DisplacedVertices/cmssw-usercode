66a45e9007a60706c37f1e167aa29d58905953ca
Jordan Tucker <jordantucker@gmail.com>
2017-04-14 21:16:13 -0500

    new file format; be cool with data

--- MFVNeutralino/test/effsprint_from_trees.py
+++ MFVNeutralino/test/effsprint_from_trees.py
@@ -4,7 +4,7 @@ from JMTucker.Tools.ROOTTools import *
 import JMTucker.Tools.Samples as Samples
 import JMTucker.MFVNeutralino.AnalysisConstants as ac
 
-file_path = '/uscms_data/d2/tucker/crab_dirs/MinitreeV12'
+file_path = '/uscms_data/d2/tucker/crab_dirs/MinitreeV13p1'
 year = 2016
 
 int_lumi = ac.int_lumi_2016 * ac.scale_factor_2016
@@ -16,7 +16,7 @@ if year == 2015:
     background_samples = Samples.qcd_samples_sum_2015 + Samples.ttbar_samples_2015
     signal_samples = [Samples.mfv_neu_tau00100um_M0800_2015, Samples.mfv_neu_tau00300um_M0800_2015, Samples.mfv_neu_tau01000um_M0800_2015, Samples.mfv_neu_tau10000um_M0800_2015]
 
-for ntk in ['mfvMiniTree', 'tre33', 'tre34', 'tre44']:
+for ntk in ['mfvMiniTree', 'mfvMiniTreeNtk3', 'mfvMiniTreeNtk3or4', 'mfvMiniTreeNtk4']:
     print ntk
 
     raw_n1v = 0
@@ -26,14 +26,16 @@ for ntk in ['mfvMiniTree', 'tre33', 'tre34', 'tre44']:
     sum_n2v = 0
     var_n2v = 0
     for sample in background_samples:
-        w = int_lumi * sample.xsec / sample.nevents_orig
+        w = int_lumi * sample.xsec / sample.nevents_orig if sample.is_mc else 1.
         f = ROOT.TFile('%s/%s.root' % (file_path, sample.name))
         t = f.Get('%s/t' % ntk)
+        if not t:
+            continue
         n1v = t.Draw('dist0', 'nvtx==1')
         n2v = t.Draw('dist0', 'nvtx==2')
-        if ntk == 'tre34':
+        if ntk == 'mfvMiniTreeNtk3or4':
             n2v = t.Draw('dist0', 'nvtx==2 && ntk0==4 && ntk1==3')
-        print '%24s %d %9.3f %8d %8.5f raw n1v = %6d +/- %3d, weighted n1v = %9.2f +/- %6.2f, raw n2v = %4d +/- %2d, weighted n2v = %7.2f +/- %5.2f' % (sample.name, int_lumi, sample.xsec, sample.nevents_orig, w, n1v, n1v**0.5, w*n1v, w*n1v**0.5, n2v, n2v**0.5, w*n2v, w*n2v**0.5)
+        print '%24s %d %9.3f %8d %8.5f raw n1v = %6d +/- %3d, weighted n1v = %9.2f +/- %6.2f, raw n2v = %4d +/- %2d, weighted n2v = %7.2f +/- %5.2f' % (sample.name, int_lumi, sample.xsec if sample.is_mc else -1, sample.nevents_orig, w, n1v, n1v**0.5, w*n1v, w*n1v**0.5, n2v, n2v**0.5, w*n2v, w*n2v**0.5)
         raw_n1v += n1v
         sum_n1v += n1v * w
         var_n1v += n1v * w**2
@@ -49,7 +51,7 @@ for ntk in ['mfvMiniTree', 'tre33', 'tre34', 'tre44']:
         t = f.Get('%s/t' % ntk)
         n1v = t.Draw('dist0', 'nvtx==1')
         n2v = t.Draw('dist0', 'nvtx==2')
-        if ntk == 'tre34':
+        if ntk == 'mfvMiniTreeNtk3or4':
             n2v = t.Draw('dist0', 'nvtx==2 && ntk0==4 && ntk1==3')
         print '%s %d %.3f %6d %7.5f raw n1v = %5d +/- %3d, weighted n1v = %5.2f +/- %4.2f, raw n2v = %5d +/- %3d, weighted n2v = %6.3f +/- %5.3f' % (sample.name, int_lumi, sample.xsec, sample.nevents_orig, w, n1v, n1v**0.5, w*n1v, w*n1v**0.5, n2v, n2v**0.5, w*n2v, w*n2v**0.5)
     print
