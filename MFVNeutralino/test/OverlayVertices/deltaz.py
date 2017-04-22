2caf2001641d793e162850a65f9b97200fc17c21
Jordan Tucker <jordantucker@gmail.com>
2017-04-22 08:47:31 -0500

    look at delta z in data and mc (blind to norm)

--- MFVNeutralino/test/OverlayVertices/deltaz.py
+++ MFVNeutralino/test/OverlayVertices/deltaz.py
@@ -1,35 +1,63 @@
 from JMTucker.MFVNeutralino.MiniTreeBase import *
 
-ps = plot_saver('plots/deltaz')
+ROOT.gStyle.SetOptStat('ourm')
 
-int_lumi = 26000.
-min_ntracks = 3
-tree_path = 'trees'
+ps = plot_saver(plot_dir('deltaz_new'), size=(600,600))
 
-binning = (100, -0.3, 0.3)
+ntracks = 3
+tree_path = '/uscms_data/d2/tucker/crab_dirs/MiniTreeV14_forpick_Jendontlook'
 
-h_pvsvdz = ROOT.TH1F('h_pvsvdz', '', *binning)
-h_svdz = ROOT.TH1F('h_svdz', '', *binning)
+binning = (100, -0.3, 0.3), (50, -0.3, 0.3)
+title = ';#Delta z (sv - pv) (cm);arb. norm.'
+h_pvsvdz = ROOT.TH1F('h_pvsvdz_data', title, *binning[0]), ROOT.TH1F('h_pvsvdz_mc', title, *binning[0])
+title = ';#Delta z (sv0 - sv1) (cm);arb. norm.'
+h_svdz =   ROOT.TH1F('h_svdz_data',   title, *binning[1]), ROOT.TH1F('h_svdz_mc',   title, *binning[1])
 
-for sample in bkg_samples:
-    f,t = get_f_t(sample, min_ntracks, tree_path)
-    weight = sample.partial_weight_orig * int_lumi
+for sample in bkg_samples + data_samples:
+    f,t = get_f_t(sample, ntracks, tree_path)
+    is_mc = sample.is_mc
+    weight = sample.partial_weight_orig * ac.int_lumi_2016 * ac.scale_factor_2016 if is_mc else 1
 
     hn = 'h_pvsvdz_%s' % sample.name
-    print sample.name, t.Draw('(z0-pvz)>>%s%r' % (hn, binning), '(nvtx == 1 && ntk0 >= %i) * weight * %e' % (min_ntracks, weight))
-    htemp = getattr(ROOT, hn)
-    h_pvsvdz.Add(htemp)
-    ps.save(hn)
+    n = t.Draw('(z0-pvz)>>%s%r' % (hn, binning[0]), '(nvtx == 1 && ntk0 == %i) * weight * %e' % (ntracks, weight))
+    print sample.name, n
+    if n > 0:
+        htemp = getattr(ROOT, hn)
+        h_pvsvdz[is_mc].Add(htemp)
+        ps.save(hn)
 
     hn = 'h_svdz_%s' % sample.name
-    print sample.name, t.Draw('(svdz)>>%s%r' % (hn, binning), '(nvtx == 2 && min_ntracks_ok) * weight * %e' % weight)
-    htemp = getattr(ROOT, hn)
-    h_svdz.Add(htemp)
-    ps.save(hn)
+    n = t.Draw('(svdz)>>%s%r' % (hn, binning[1]), '(nvtx == 2 && ntk0 == %i && ntk1 == %i) * weight * %e' % (ntracks, ntracks, weight))
+    if not is_mc:
+        n = 1e99
+    print sample.name, n
+    if n > 0:
+        htemp = getattr(ROOT, hn)
+        h_svdz[is_mc].Add(htemp)
+        ps.save(hn)
 
-h_pvsvdz.Draw()
+for hs in h_pvsvdz, h_svdz:
+    for i,h in enumerate(hs):
+        h.SetBinErrorOption(ROOT.TH1.kPoisson)
+        h.SetLineWidth(2)
+        h.SetLineColor([ROOT.kBlack, ROOT.kRed][i])
+        h.Scale(1/h.Integral())
+
+h_pvsvdz[1].Draw('hist')
+h_pvsvdz[0].Draw('e sames')
+ps.c.Update()
+for i in 0,1:
+    differentiate_stat_box(h_pvsvdz[i], i, new_size=(0.3,0.3))
 ps.save('pvsvdz')
 
-fcn = ROOT.TF1('fcn', 'gaus', -0.035, 0.035)
-h_svdz.Fit(fcn, 'QR')
+fit_range = 0.2
+fcn = ROOT.TF1('fcn_data', 'gaus', -fit_range, fit_range), ROOT.TF1('fcn_mc', 'gaus', -fit_range, fit_range)
+for i in 0,1:
+    print ['data','mc'][i]
+    h_svdz[i].Fit(fcn[i], 'R')
+h_svdz[1].Draw('hist')
+h_svdz[0].Draw('e sames')
+ps.c.Update()
+for i in 0,1:
+    differentiate_stat_box(h_svdz[i], i, new_size=(0.3,0.3))
 ps.save('svdz')
