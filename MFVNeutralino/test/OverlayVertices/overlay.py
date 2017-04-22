d706eb480a9f549232eaede4397efa19930623b0
Jordan Tucker <jordantucker@gmail.com>
2017-04-22 17:12:41 -0500

    rest_of_event is on by default; +z-model default deltasvgaus; +batch meaningless, dropped; use single file edm input, and specify default paths for input edm files and minitrees; +debug-timing option.

--- MFVNeutralino/test/OverlayVertices/overlay.py
+++ MFVNeutralino/test/OverlayVertices/overlay.py
@@ -12,8 +12,8 @@ parser, args_printer = friendly_argparse(description='Overlay tracks from pairs 
 parser.add_argument('+which-event', '+e', type=int, help='which event from minitree to use', required=True)
 parser.add_argument('+sample', help='which sample to use', choices=[s.name for s in allowed_samples], default='qcdht1500sum')
 parser.add_argument('+ntracks', type=int, help='ntracks to use', default=3, choices=[3,4,5])
-parser.add_argument('+rest-of-event', action='store_true', help='whether to use the rest of the tracks in the edm event')
-parser.add_argument('+z-model', help='z model', choices=['deltasv', 'deltasvgaus', 'deltapv', 'none'], default='deltasv')
+parser.add_argument('+no-rest-of-event', action='store_false', dest='rest_of_event', help='whether to use the rest of the tracks in the edm event')
+parser.add_argument('+z-model', help='z model', choices=['deltasv', 'deltasvgaus', 'deltapv', 'none'], default='deltasvgaus')
 parser.add_argument('+z-width', type=float, help='width of gaus used in z model (cm)', default=99)
 parser.add_argument('+dz-true-max', type=float, help='max dz allowed for z model (cm)', default=1e9)
 parser.add_argument('+found-dist', type=float, help='3D distance for matching by position (cm)', default=0.008)
@@ -21,19 +21,22 @@ parser.add_argument('+rotate-x', action='store_true', help='azimuthally rotate x
 parser.add_argument('+rotate-p', action='store_true', help='azimuthally rotate p of tracks')
 parser.add_argument('+is-data', action='store_true', help='whether input is data / MC')
 parser.add_argument('+is-H', action='store_true', help='if data, whether input is the H dataset')
-parser.add_argument('+batch', action='store_true', help='run in batch mode')
 parser.add_argument('+debug', action='store_true', help='turn on debug prints')
+parser.add_argument('+debug-timing', action='store_true', help='turn on want summary to see the time report')
+parser.add_argument('+in-path', help='override input path', default='root://cmsxrootd.fnal.gov//store/user/tucker/skimpickv14')
+parser.add_argument('+in-fn', help='override input fn')
 parser.add_argument('+out-fn', help='override output fn')
-parser.add_argument('+minitree-path', help='override minitree path')
+parser.add_argument('+minitree-path', help='override minitree path', default='root://cmsxrootd.fnal.gov//store/user/tucker/MiniTreeV14_forpick')
 parser.add_argument('+minitree-fn', help='override minitree fn')
 parser.add_argument('+minitree-treepath', help='override minitree tree path')
 parser.add_argument('+max-events', type=int, help='max edm events to run on', default=-1)
 
 args = parser.parse_args()
+
+if args.in_fn is None:
+    args.in_fn = '%s/%s.root' % (args.in_path, args.sample)
 if args.out_fn is None:
     args.out_fn = 'overlay_%i.root' % args.which_event
-if args.minitree_path is None:
-    args.minitree_path = 'root://cmsxrootd.fnal.gov//store/user/tucker/MiniTreeV14_forpick'
 if args.minitree_fn is None:
     args.minitree_fn = '%s/%s.root' % (args.minitree_path, args.sample)
 if args.minitree_treepath is None:
@@ -46,24 +49,12 @@ args_printer('overlay args', args)
 
 ####
 
-def files_for_sample(s):
-    return ['root://cmsxrootd.fnal.gov/' + x for x in SampleFiles.get(s, 'pick1vtxv14')[1]]
-
-if 'qcd' in args.sample:
-    assert 'sum' in args.sample
-    files  = files_for_sample(args.sample.replace('sum', ''))
-    files += files_for_sample(args.sample.replace('sum', 'ext'))
-else:
-    files = files_for_sample(args.sample)
-
-####
-
 process = basic_process('Overlay')
-process.source.fileNames = files
+process.source.fileNames = [args.in_fn]
 process.maxEvents.input = args.max_events
-want_summary(process, args.debug)
+want_summary(process, args.debug or args.debug_timing)
 silence_messages(process, ['TwoTrackMinimumDistanceHelixLine'])
-report_every(process, 1 if args.debug else 1000000)
+report_every(process, 1 if args.debug else 1000 if args.debug_timing else 1000000)
 geometry_etc(process, which_global_tag(not args.is_data, year, args.is_H))
 tfileservice(process, args.out_fn)
 random_service(process, {'mfvVertices':      12179 + args.which_event,
