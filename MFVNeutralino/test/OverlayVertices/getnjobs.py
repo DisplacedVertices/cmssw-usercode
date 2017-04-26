bd43d5f679d7e68381fd237a0fff305f118240b3
Jordan Tucker <jordantucker@gmail.com>
2017-04-26 13:37:33 -0500

    cleanup obsolete, make README noticeable

--- MFVNeutralino/test/OverlayVertices/getnjobs.py
+++
@@ -1,9 +0,0 @@
-import sys
-from JMTucker.Tools.ROOTTools import *
-for x in sys.argv[1:]:
-    f = ROOT.TFile(x)
-    t = f.Get('mfvMiniTree/t')
-    for ntk in [3,4,5]:
-        n = t.Draw('1', 'nvtx == 1 && ntk0 == %i' % ntk)
-        if n:
-            print x, ntk, n
