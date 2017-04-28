954fc6c7121090ff261dcbbfc3aa1e8bef8e0db3
Jordan Tucker <jordantucker@gmail.com>
2017-04-28 11:13:34 -0500

    v14

--- MFVNeutralino/test/pileup_efficiency.py
+++ MFVNeutralino/test/pileup_efficiency.py
@@ -2,7 +2,7 @@ from JMTucker.Tools.ROOTTools import *
 ROOT.TH1.AddDirectory(0)
 set_style()
 
-ps = plot_saver(plot_dir('pileup_efficiency_run2_15_redo'), size=(600,600), pdf=True)
+ps = plot_saver(plot_dir('pileup_efficiency_run2_v14'), size=(600,600), pdf=True)
 
 bins = to_array([0,10,13,15,65])
 nbins = len(bins)-1
@@ -15,21 +15,21 @@ hists = [
     ]
 
 xxx = [
-    (3, '/uscms_data/d2/tucker/crab_dirs/HistosV10_16_qcd/qcd.root', 'Ntk3'),
-    (4, '/uscms_data/d2/tucker/crab_dirs/HistosV10_16_qcd/qcd.root', 'Ntk4'),
-    (5, '/uscms_data/d2/tucker/crab_dirs/HistosV10_16_qcd/qcd.root', ''),
-]
-
-xxx = [
-    (3, '/uscms_data/d2/tucker/crab_dirs/HistosV10_15_afterreorg/background.root', 'Ntk3'),
-    (4, '/uscms_data/d2/tucker/crab_dirs/HistosV10_15_afterreorg/background.root', 'Ntk4'),
-    (5, '/uscms_data/d2/tucker/crab_dirs/HistosV10_15_afterreorg/background.root', ''),
+    (3, '/uscms_data/d2/tucker/crab_dirs/HistosV14/background.root', 'Ntk3'),
+    (4, '/uscms_data/d2/tucker/crab_dirs/HistosV14/background.root', 'Ntk4'),
+    (5, '/uscms_data/d2/tucker/crab_dirs/HistosV14/background.root', ''),
 ]
 
 for ntk, fn, s2 in xxx:
     print fn, ntk
     f = ROOT.TFile(fn)
-    geth = lambda s, s2: f.Get(s2 + s + '/h_npu')
+    def geth(s,s2):
+        if s.endswith('PreSel'):
+            n = s + '/h_npu'
+        else:
+            n = s2 + s + '/h_npu'
+        print n
+        return f.Get(n)
     #hs = [(n, geth(s).Rebin(nbins, n, bins)) for n,s in hists]
     hs = [(n, geth(s,s2).Clone(n)) for n,s in hists]
 
