2ecc120cdbb9b14513e4565e934a12e224845b88
Jordan Tucker <jordantucker@gmail.com>
2017-04-29 14:55:08 -0500

    drop _ from url and user

--- Tools/python/eos.py
+++ Tools/python/eos.py
@@ -1,7 +1,7 @@
 import subprocess, os
 
-_url = 'root://cmseos.fnal.gov/'
-_user = os.environ['USER']
+url = 'root://cmseos.fnal.gov/'
+user = os.environ['USER']
 
 def _popen(cmd, shell=False):
     if type(cmd) == str:
@@ -16,27 +16,27 @@ def _system(cmd):
 def quota():
     x = popen('eos root://cmseos.fnal.gov quota').communicate()[0].split('\n')
     for i, line in enumerate(x):
-        if _user in line:
+        if user in line:
             return x[i-1] + '\n' + line
-    return "eos quota command didn't work or didn't find user %s" % _user
+    return "eos quota command didn't work or didn't find user %s" % user
 
 def exists(fn):
-    return _system('eos %s ls %s' % (_url, fn))
+    return _system('eos %s ls %s' % (url, fn))
 
 def mkdir(fn):
     dn = os.path.dirname(fn)
     if exists(dn):
         return True
-    return _system('eos %s mkdir -p %s' % (_url, dn))
+    return _system('eos %s mkdir -p %s' % (url, dn))
 
 def cp(src, dst):
-    return _system('xrdcp -s %s%s %s%s' % (_url, src, _url, dst))
+    return _system('xrdcp -s %s%s %s%s' % (url, src, url, dst))
 
 def rm(fn):
-    return _system('eos %s rm %s' % (_url, fn))
+    return _system('eos %s rm %s' % (url, fn))
 
 def md5sum(fn):
-    cmd = 'xrdcp -s %s%s -' % (_url, fn)
+    cmd = 'xrdcp -s %s%s -' % (url, fn)
     p  = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     p2 = subprocess.Popen(('md5sum',), stdin=p.stdout, stdout=subprocess.PIPE)
     p.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
