a36bc0062c0491a2d0d66d02a606f45ec677a2f5
Jordan Tucker <jordantucker@gmail.com>
2017-05-01 14:39:52 -0500

    latest version v14, drop curve for 1vtx

--- MFVNeutralino/test/signal_eff.py
+++ MFVNeutralino/test/signal_eff.py
@@ -5,23 +5,25 @@ from JMTucker.Tools.ROOTTools import *
 from JMTucker.Tools.Samples import *
 
 set_style()
-ps = plot_saver('~/plots/mfvlq2sigeff_V6p1_76X', size=(600,600))
+ps = plot_saver(plot_dir('sigeff_v14'), size=(600,600))
 
-samples = mfv_signal_samples_lq2
+samples = [x for x in mfv_signal_samples if not x.name.startswith('my')]
+print len(samples)
 samples.sort(key=lambda s: s.name)
 
-per_div = 5
+per_div = 7
+assert len(samples) % per_div == 0
 y_range = 1.
 
-def curve(name, root_file_dir, num_path, den_path, color):
+def curve(name, root_file_dir, num_path, color):
     heff = ROOT.TH1F(name, ';;efficiency', len(samples), 0, len(samples))
     xax = heff.GetXaxis()
     for i, sample in enumerate(samples):
         f = ROOT.TFile(os.path.join(root_file_dir, sample.name + '.root'))
         hnum = f.Get(num_path)
-        hden = f.Get(den_path)
+        hden = f.Get('mfvWeight/h_sums')
         num = hnum.Integral(0, hnum.GetNbinsX() + 2)
-        den = hden.Integral(0, hden.GetNbinsX() + 2)
+        den = hden.GetBinContent(1)
         eff = num/float(den)
 #        eff *= sample.filter_eff
         heff.SetBinContent(i+1, eff)
@@ -44,28 +46,19 @@ def curve(name, root_file_dir, num_path, den_path, color):
     heff.SetStats(0)
     return heff
 
-two = curve('two', '/uscms_data/d1/jchu/crab_dirs/mfv_763p2/HistosV6p1_76x_nstlays3_13', 'mfvEventHistos/h_bsx', 'mfvEventHistosNoCuts/h_bsx', ROOT.kRed)
-one = curve('one', '/uscms_data/d1/jchu/crab_dirs/mfv_763p2/HistosV6p1_76x_nstlays3_13/', 'mfvEventHistosOnlyOneVtx/h_bsx', 'mfvEventHistosNoCuts/h_bsx', ROOT.kBlue)
-oneormore = two.Clone('oneormore')
-oneormore.Add(one)
-oneormore.SetLineColor(ROOT.kViolet)
-
+two = curve('two', '/uscms_data/d2/tucker/crab_dirs/HistosV14', 'mfvEventHistosFullSel/h_bsx', ROOT.kRed)
 two.Draw('hist e')
-one.Draw('hist e same')
-oneormore.Draw('hist e same')
 
-hmax = two.GetYaxis().GetXmax()
-print hmax
-
-taus = ['100 #mum', '300 #mum', '1 mm', '10 mm']
+taus = ['100 #mum', '300 #mum', '1 mm', '10 mm', '30 mm']
 #ymin = [0.745, 0.745, 0.75, 0.75]
-ymin = [1.00, 1.00, 1.005, 1.005]
+ymin = [1.00, 1.00, 1.005, 1.005, 1.005]
 lines = []
 paves = []
 for i in xrange(0, len(samples), per_div):
     p = ROOT.TPaveText(i+1,ymin[i/per_div],i+per_div-1,ymin[i/per_div]+0.1)
+    p.SetFillColor(ROOT.kWhite)
     p.AddText(taus[i/per_div])
-    p.SetTextSize(0.05)
+    p.SetTextSize(0.042)
     p.SetBorderSize(0)
     p.Draw()
     paves.append(p)
@@ -86,24 +79,21 @@ for i in xrange(0, len(samples), per_div):
     lines.append(l)
 
 
-leg = ROOT.TLegend(0.1,0.72,0.5,0.80)
-leg.AddEntry(two, 'two-or-more-vertex events', 'L')
-leg.AddEntry(one, 'exactly-one-vertex events', 'L')
-leg.AddEntry(oneormore, 'one-or-more-vertex events', 'L')
-leg.Draw()
-
-decay = ROOT.TPaveText(0.7,0.88,4.5,0.96)
-decay.AddText('LQ #rightarrow #muc')
+decay = ROOT.TPaveText(0.31, 0.89,6.6,0.97)
+decay.AddText('#tilde{N} #rightarrow tbs')
+decay.SetFillColor(ROOT.kWhite)
 decay.SetBorderSize(0)
 decay.Draw()
 
 lifetime = ROOT.TPaveText(-3,1.01,-0.01,1.11)
-lifetime.AddText('#tau_{LQ}')
+lifetime.AddText('#tau_{#tilde{N}}')
+lifetime.SetFillColor(ROOT.kWhite)
 lifetime.SetBorderSize(0)
 lifetime.Draw()
 
 mass = ROOT.TPaveText(-3,-0.1,-0.01,-0.01)
-mass.AddText('M_{LQ}')
+mass.SetFillColor(ROOT.kWhite)
+mass.AddText('M_{#tilde{N}}')
 mass.SetBorderSize(0)
 mass.Draw()
 
