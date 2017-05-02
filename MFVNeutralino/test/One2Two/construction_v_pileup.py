bee5dde66814bd037190e7cb16ea02f5441e7aad
Jordan Tucker <jordantucker@gmail.com>
2017-05-02 18:02:32 -0500

    oops

--- MFVNeutralino/test/One2Two/construction_v_pileup.py
+++ MFVNeutralino/test/One2Two/construction_v_pileup.py
@@ -1,14 +1,15 @@
 # produce the root files below by setting min/max_npu in 2v_from_jets.cc
 
-fns = ['%s/2v_from_jets_2016_3track_default_v14.root' % x) for x in '2vdefault,2v_0_18,2v_19_22,2v_23_27,2v_28_32,2v_33_up'.split(',')]
+from JMTucker.Tools.ROOTTools import *
+
+fns = ['%s/2v_from_jets_2016_3track_default_v14.root' % x for x in '2vdefault,2v_0_18,2v_19_22,2v_23_27,2v_28_32,2v_33_up'.split(',')]
 names = ['default', '0-18', '19-22','23-27','28-32','33-']
 colors = [ROOT.kBlack, ROOT.kRed-9, ROOT.kRed-7, ROOT.kRed, ROOT.kRed+1, ROOT.kRed+2]
-stats = True
-rebin = False
+stats = False
+rebin = True
 
 ####
 
-from JMTucker.Tools.ROOTTools import *
 set_style()
 ROOT.gStyle.SetOptStat(1111)
 ps = plot_saver(plot_dir('construction_v_pileup'), size=(600,600))
