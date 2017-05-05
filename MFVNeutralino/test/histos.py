1b93ab9863bb800b6ab5522a855a7ebde1aa96f1
Jordan Tucker <jordantucker@gmail.com>
2017-05-05 15:01:56 -0500

    new splitting done by MetaSubmitter.set_splitting

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -108,7 +108,7 @@ def force_bs(process, bs):
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.MFVNeutralino.Year import year
-    import JMTucker.Tools.Samples as Samples 
+    from JMTucker.Tools import Samples 
     if year == 2015:
         samples = Samples.data_samples_2015 + \
             Samples.ttbar_samples_2015 + Samples.qcd_samples_2015 + Samples.qcd_samples_ext_2015 + \
@@ -118,13 +118,9 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
             Samples.ttbar_samples + Samples.qcd_samples + Samples.qcd_samples_ext + \
             Samples.mfv_signal_samples + Samples.mfv_ddbar_samples
 
+    from JMTucker.Tools.MetaSubmitter import set_splitting
     dataset = 'ntuplev14'
-    for sample in samples:
-        sample.set_curr_dataset(dataset)
-        sample.split_by = 'files'
-        sample.files_per = 1 if sample.name.startswith('mfv_') else 20
-        if not sample.is_mc:
-            sample.json = 'ana_2015p6_10pc.json'
+    set_splitting(samples, dataset, 'histos', data_json='ana_2015p6_10pc.json')
 
     def modify(sample):
         to_add, to_replace = [], []
@@ -137,7 +133,7 @@ for p in process.paths.keys():
         return to_add, to_replace
 
     from JMTucker.Tools.CondorSubmitter import CondorSubmitter
-    cs = CondorSubmitter('HistosV14',
+    cs = CondorSubmitter('HistosV14_v2',
                          ex = year,
                          dataset = dataset,
                          pset_modifier = modify,
--- MFVNeutralino/test/minitree.py
+++ MFVNeutralino/test/minitree.py
@@ -9,7 +9,7 @@ process.load('JMTucker.MFVNeutralino.MiniTree_cff')
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.MFVNeutralino.Year import year
-    import JMTucker.Tools.Samples as Samples 
+    from JMTucker.Tools import Samples
     if year == 2015:
         samples = Samples.data_samples_2015 + \
             Samples.ttbar_samples_2015 + Samples.qcd_samples_2015 + Samples.qcd_samples_ext_2015 + \
@@ -19,13 +19,9 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
             Samples.ttbar_samples + Samples.qcd_samples + Samples.qcd_samples_ext + \
             Samples.mfv_signal_samples + Samples.mfv_ddbar_samples
 
+    from JMTucker.Tools.MetaSubmitter import set_splitting
     dataset = 'ntuplev14'
-    for sample in samples:
-        sample.set_curr_dataset(dataset)
-        sample.split_by = 'files'
-        sample.files_per = 20
-        if not sample.is_mc:
-            sample.json = 'ana_2015p6_10pc.json'
+    set_splitting(samples, dataset, 'minitree', data_json='ana_2015p6_10pc.json')
 
     def modify(sample):
         to_add, to_replace = [], []
@@ -36,7 +32,7 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         return to_add, to_replace
 
     from JMTucker.Tools.CondorSubmitter import CondorSubmitter
-    cs = CondorSubmitter('MiniTreeV14',
+    cs = CondorSubmitter('MiniTreeV14_v2',
                          ex = year,
                          dataset = dataset,
                          pset_modifier = modify
