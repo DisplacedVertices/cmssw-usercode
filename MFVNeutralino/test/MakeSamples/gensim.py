934c94c65425a31ae04d6cbfb53d1b66dda172e9
Jordan Tucker <jordantucker@gmail.com>
2017-05-10 14:34:55 -0500

    too hastily removed todos-killing in fromlhe

--- MFVNeutralino/test/MakeSamples/gensim.py
+++ MFVNeutralino/test/MakeSamples/gensim.py
@@ -10,6 +10,8 @@ jobnum = 1
 
 for arg in sys.argv:
     if arg == 'fromlhe=1':
+        print 'fromlhe: wiping out todos'
+        todos = [] 
         fromlhe = True
     elif arg.startswith('salt='):
         salt = arg.replace('salt=','')
