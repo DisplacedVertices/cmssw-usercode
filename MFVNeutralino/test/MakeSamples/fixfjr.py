8666f0ad12ee500ed299b48dacd2e94f9ad813ab
Jordan Tucker <jordantucker@gmail.com>
2017-05-10 13:39:35 -0500

    sys exit the no-inputs message so it doesn't scream doom in nstep to just run it always

--- MFVNeutralino/test/MakeSamples/fixfjr.py
+++ MFVNeutralino/test/MakeSamples/fixfjr.py
@@ -1,10 +1,11 @@
+import sys
 import xml.etree.ElementTree as ET
 
 x = ET.parse('tempfjr.xml')
 
 inputs = x.findall('InputFile')
 if len(inputs) != 1:
-    raise ValueError('tempfjr.xml is weird, len(inputs) is %i' % len(inputs))
+    sys.exit("fixfjr.py: tempfjr.xml is weird, len(inputs) is %i--this may be OK if we're not at the last step" % len(inputs))
 
 x.getroot().remove(inputs[0])
 
