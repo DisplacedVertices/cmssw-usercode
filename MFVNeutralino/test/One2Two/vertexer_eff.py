225308cebbcfc3ce7b918206c7014e1d95f56fbb
Jennifer Chu <jyc72@cornell.edu>
2017-05-16 10:18:03 -0500

    V14p2.

--- MFVNeutralino/test/One2Two/vertexer_eff.py
+++ MFVNeutralino/test/One2Two/vertexer_eff.py
@@ -3,10 +3,10 @@ from JMTucker.Tools.ROOTTools import *
 year = '2016'
 
 set_style()
-ps = plot_saver('../plots/bkgest/v14/vertexer_eff_%s' % year, size=(700,700), log=False, root=False)
+ps = plot_saver('../plots/bkgest/v14p2/vertexer_eff_%s' % year, size=(700,700), log=False, root=False)
 
-f = ROOT.TFile('/uscms_data/d2/tucker/crab_dirs/VertexerHistos/v14/background%s.root' % ('' if year=='2016' else '_%s'%year))
-fh = ROOT.TFile('eff_%s_v14.root' % year, 'recreate')
+f = ROOT.TFile('/uscms_data/d2/tucker/crab_dirs/VertexerHistos/v14p2/background%s.root' % ('' if year=='2016' else '_%s'%year))
+fh = ROOT.TFile('eff_%s_v14p2.root' % year, 'recreate')
 
 for maxtk in ['maxtk3', 'maxtk4', 'maxtk5']:
     h_merge = f.Get('mfvVertices/h_merge_d2d_jetht1000_%s' % maxtk)
@@ -54,24 +54,24 @@ for maxtk in ['maxtk3', 'maxtk4', 'maxtk5']:
 fh.Close()
 
 
-f = ROOT.TFile('eff_%s_v14.root' % year)
-h = f.Get('maxtk5')
-h.SetTitle(';d_{VV} (cm);Efficiency')
-h.GetXaxis().SetRangeUser(0,0.4)
-h.SetLineColor(ROOT.kViolet)
-h.SetLineWidth(3)
-h.Draw('hist')
-ps.save('efficiency5')
+f = ROOT.TFile('eff_%s_v14p2.root' % year)
+#h = f.Get('maxtk5')
+#h.SetTitle(';d_{VV} (cm);Efficiency')
+#h.GetXaxis().SetRangeUser(0,0.4)
+#h.SetLineColor(ROOT.kViolet)
+#h.SetLineWidth(3)
+#h.Draw('hist')
+#ps.save('efficiency5')
 
-h2 = f.Get('maxtk5_merge')
-h2.SetLineWidth(3)
-h.Draw('hist')
-h2.Draw('hist sames')
-l = ROOT.TLegend(0.20,0.15,0.85,0.30)
-l.AddEntry(h, 'vertex pair survival efficiency')
-l.AddEntry(h2, 'vertex survival efficiency')
-l.Draw()
-ps.save('compare_efficiency')
+#h2 = f.Get('maxtk5_merge')
+#h2.SetLineWidth(3)
+#h.Draw('hist')
+#h2.Draw('hist sames')
+#l = ROOT.TLegend(0.20,0.15,0.85,0.30)
+#l.AddEntry(h, 'vertex pair survival efficiency')
+#l.AddEntry(h2, 'vertex survival efficiency')
+#l.Draw()
+#ps.save('compare_efficiency')
 
 colors = [0, 0, 0, ROOT.kRed, ROOT.kBlue, ROOT.kGreen+2]
 l = ROOT.TLegend(0.50,0.15,0.85,0.30)
@@ -92,42 +92,42 @@ l.Draw()
 ps.save('efficiency')
 
 
-ROOT.TH1.AddDirectory(0)
-
-fn1 = ['2v_from_jets_%s_3track_default_v14.root' % year, '2v_from_jets_%s_3track_noclearing_v14.root' % year]
-fn2 = ['2v_from_jets_%s_4track_default_v14.root' % year, '2v_from_jets_%s_4track_noclearing_v14.root' % year]
-fn3 = ['2v_from_jets_%s_5track_default_v14.root' % year, '2v_from_jets_%s_5track_noclearing_v14.root' % year]
-
-fns = [fn1, fn2, fn3]
-ntk = ['3-track', '4-track', '5-track']
-
-n2v = [934., 7., 1.]
-if year == '2015':
-    n2v = [44., 1., 1.]
-if year == '2015p6':
-    n2v = [978., 7., 1.]
-
-for i in range(3):
-    h0 = ROOT.TFile(fns[i][0]).Get('h_c1v_dvv')
-    h0.SetTitle(';d_{VV}^{C} (cm);Events')
-    h0.SetStats(0)
-    h0.SetLineColor(ROOT.kRed)
-    h0.SetLineWidth(3)
-    h0.Scale(n2v[i]/h0.Integral())
-    if i == 2:
-        h0.GetYaxis().SetRangeUser(0,0.4)
-    h0.Draw('hist e')
-
-    h1 = ROOT.TFile(fns[i][1]).Get('h_c1v_dvv')
-    h1.SetStats(0)
-    h1.SetLineColor(ROOT.kBlack)
-    h1.SetLineWidth(3)
-    h1.Scale(n2v[i]/h1.Integral())
-    h1.Draw('hist e sames')
-
-    l = ROOT.TLegend(0.35,0.75,0.85,0.85)
-    l.AddEntry(h1, 'without efficiency correction')
-    l.AddEntry(h0, 'with efficiency correction')
-    l.SetFillColor(0)
-    l.Draw()
-    ps.save(ntk[i])
+#ROOT.TH1.AddDirectory(0)
+#
+#fn1 = ['2v_from_jets_%s_3track_default_v14.root' % year, '2v_from_jets_%s_3track_noclearing_v14.root' % year]
+#fn2 = ['2v_from_jets_%s_4track_default_v14.root' % year, '2v_from_jets_%s_4track_noclearing_v14.root' % year]
+#fn3 = ['2v_from_jets_%s_5track_default_v14.root' % year, '2v_from_jets_%s_5track_noclearing_v14.root' % year]
+#
+#fns = [fn1, fn2, fn3]
+#ntk = ['3-track', '4-track', '5-track']
+#
+#n2v = [934., 7., 1.]
+#if year == '2015':
+#    n2v = [44., 1., 1.]
+#if year == '2015p6':
+#    n2v = [978., 7., 1.]
+#
+#for i in range(3):
+#    h0 = ROOT.TFile(fns[i][0]).Get('h_c1v_dvv')
+#    h0.SetTitle(';d_{VV}^{C} (cm);Events')
+#    h0.SetStats(0)
+#    h0.SetLineColor(ROOT.kRed)
+#    h0.SetLineWidth(3)
+#    h0.Scale(n2v[i]/h0.Integral())
+#    if i == 2:
+#        h0.GetYaxis().SetRangeUser(0,0.4)
+#    h0.Draw('hist e')
+#
+#    h1 = ROOT.TFile(fns[i][1]).Get('h_c1v_dvv')
+#    h1.SetStats(0)
+#    h1.SetLineColor(ROOT.kBlack)
+#    h1.SetLineWidth(3)
+#    h1.Scale(n2v[i]/h1.Integral())
+#    h1.Draw('hist e sames')
+#
+#    l = ROOT.TLegend(0.35,0.75,0.85,0.85)
+#    l.AddEntry(h1, 'without efficiency correction')
+#    l.AddEntry(h0, 'with efficiency correction')
+#    l.SetFillColor(0)
+#    l.Draw()
+#    ps.save(ntk[i])
