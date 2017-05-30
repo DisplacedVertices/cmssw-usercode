78a52bfba97949ab081fd289195d204dcb25bf81
Jordan Tucker <jordantucker@gmail.com>
2017-05-30 18:58:23 -0500

    v15, with 5-track 1-vertex data events unblinded

--- MFVNeutralino/test/minitree.py
+++ MFVNeutralino/test/minitree.py
@@ -1,7 +1,8 @@
 import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import *
 
-sample_files(process, 'qcdht2000', 'ntuplev14', 1)
+dataset = 'ntuplev15'
+sample_files(process, 'qcdht2000', dataset, 1)
 #process.source.fileNames = ['file:ntuple.root']
 process.TFileService.fileName = 'minitree.root'
 
@@ -19,18 +20,19 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
             Samples.ttbar_samples + Samples.qcd_samples + Samples.qcd_samples_ext + \
             Samples.mfv_signal_samples + Samples.mfv_ddbar_samples + Samples.mfv_hip_samples
 
+    samples = [s for s in samples if s.has_dataset(dataset)]
+
     from JMTucker.Tools.MetaSubmitter import set_splitting
-    dataset = 'ntuplev14'
     set_splitting(samples, dataset, 'minitree', data_json='ana_2015p6_10pc.json')
 
     def modify(sample):
         to_add, to_replace = [], []
         if not sample.is_mc:
-            to_add.append('del process.pMiniTree')
+            to_add.append('process.mfvAnalysisCutsGE1Vtx.max_nvertex = 1')
         return to_add, to_replace
 
     from JMTucker.Tools.CondorSubmitter import CondorSubmitter
-    cs = CondorSubmitter('MiniTreeV14_v2',
+    cs = CondorSubmitter('MiniTreeV15',
                          ex = year,
                          dataset = dataset,
                          pset_modifier = modify
