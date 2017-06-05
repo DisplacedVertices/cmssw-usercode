14a5f7e420fa5641c47eb110d31b8a6d319d50ec
Jordan Tucker <jordantucker@gmail.com>
2017-06-05 10:40:49 -0500

    qcd hip samples

--- MFVNeutralino/test/minitree.py
+++ MFVNeutralino/test/minitree.py
@@ -18,7 +18,7 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     elif year == 2016:
         samples = Samples.data_samples + \
             Samples.ttbar_samples + Samples.qcd_samples + Samples.qcd_samples_ext + \
-            Samples.mfv_signal_samples + Samples.mfv_ddbar_samples + Samples.mfv_hip_samples
+            Samples.mfv_signal_samples + Samples.mfv_ddbar_samples + Samples.mfv_hip_samples + Samples.qcd_hip_samples
 
     from JMTucker.Tools.MetaSubmitter import set_splitting
     set_splitting(samples, dataset, 'minitree', data_json='ana_2015p6_10pc.json')
