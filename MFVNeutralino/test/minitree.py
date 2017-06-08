cf7699f9822d896b315b0a69036acdd52ed1b057
Jordan Tucker <jordantucker@gmail.com>
2017-06-08 13:30:37 -0500

    v3

--- MFVNeutralino/test/minitree.py
+++ MFVNeutralino/test/minitree.py
@@ -30,7 +30,7 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         return to_add, to_replace
 
     from JMTucker.Tools.CondorSubmitter import CondorSubmitter
-    cs = CondorSubmitter('MiniTreeV15_v2',
+    cs = CondorSubmitter('MiniTreeV15_v3',
                          ex = year,
                          dataset = dataset,
                          pset_modifier = modify
