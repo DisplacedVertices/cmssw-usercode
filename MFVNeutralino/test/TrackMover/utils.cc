8fe0f8a47573ee70d206a986c1b2965c329e083f
Jordan Tucker <jordantucker@gmail.com>
2017-06-15 10:32:44 -0500

    root_setup: simplify since default ROOT style is usable these days

--- MFVNeutralino/test/TrackMover/utils.cc
+++ MFVNeutralino/test/TrackMover/utils.cc
@@ -42,22 +42,12 @@ numden& numdens::operator()(int k) {
 }
 
 void root_setup() {
-  gROOT->SetStyle("Plain");
-  gStyle->SetPalette(1);
-  gStyle->SetFillColor(0);
-  gStyle->SetOptDate(0);
+  TH1::SetDefaultSumw2();
   gStyle->SetOptStat(1222222);
   gStyle->SetOptFit(2222);
   gStyle->SetPadTickX(1);
   gStyle->SetPadTickY(1);
-  gStyle->SetMarkerSize(.1);
-  gStyle->SetMarkerStyle(8);
-  gStyle->SetGridStyle(3);
   gROOT->ProcessLine("gErrorIgnoreLevel = 1001;");
-  double palinfo[4][5] = {{0,0,0,1,1},{0,1,1,1,0},{1,1,0,0,0},{0,0.25,0.5,0.75,1}};
-  TColor::CreateGradientColorTable(5, palinfo[3], palinfo[0], palinfo[1], palinfo[2], 500);
-  gStyle->SetNumberContours(500);
-  TH1::SetDefaultSumw2();
 }
 
 file_and_tree::file_and_tree(const char* in_fn, const char* out_fn) {
