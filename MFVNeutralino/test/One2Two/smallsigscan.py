e38d80efc253e29013c6bc407f0568f6d667fea7
Jordan Tucker <jordantucker@gmail.com>
2017-06-21 14:24:30 -0500

    full small scan

--- MFVNeutralino/test/One2Two/smallsigscan.py
+++ MFVNeutralino/test/One2Two/smallsigscan.py
@@ -14,8 +14,8 @@ for line in open('signals.h'):
         num2name[num] = name
         name2num[name] = num
 
-sample_nums = [-105,-124,-143,-162] # 800 GeV tau scan
-sample_nums += [-134,-137,-140,-146,-149,-152] # 1mm mass scan
+sample_nums = [-11,-30,-49,-68,-87,-39,-42,-46,-52,-55] + [-105,-124,-134,-137,-140,-143,-146,-149,-152,-162,-181]
+sample_nums.sort(reverse=True)
 
 samples = []
 for i in sample_nums:
