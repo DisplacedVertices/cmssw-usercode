3ab0324d723437f3c3ceeb4df789d41a2da1022e
Jordan Tucker <jordantucker@gmail.com>
2017-06-28 19:20:51 -0500

    turn into a filter, optionally cutting events with no good vertex; write #s of all and good vertices into event

--- Tools/plugins/FirstGoodPrimaryVertex.cc
+++ Tools/plugins/FirstGoodPrimaryVertex.cc
@@ -1,37 +1,48 @@
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
-#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/EDFilter.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-class JMTFirstGoodPrimaryVertex : public edm::EDProducer {
+class JMTFirstGoodPrimaryVertex : public edm::EDFilter {
 public:
   explicit JMTFirstGoodPrimaryVertex(const edm::ParameterSet&);
 private:
-  virtual void produce(edm::Event&, const edm::EventSetup&);
+  virtual bool filter(edm::Event&, const edm::EventSetup&) override;
   const edm::EDGetTokenT<reco::VertexCollection> vertices_token;
+  const bool cut;
 };
 
 JMTFirstGoodPrimaryVertex::JMTFirstGoodPrimaryVertex(const edm::ParameterSet& cfg)
-  : vertices_token(consumes<reco::VertexCollection>(edm::InputTag("offlinePrimaryVertices")))
+  : vertices_token(consumes<reco::VertexCollection>(edm::InputTag("offlinePrimaryVertices"))),
+    cut(cfg.getParameter<bool>("cut"))
 {
   produces<reco::VertexCollection>();
+  produces<int>("nAll");
+  produces<int>("nGood");
 }
 
-void JMTFirstGoodPrimaryVertex::produce(edm::Event& event, const edm::EventSetup&) {
+bool JMTFirstGoodPrimaryVertex::filter(edm::Event& event, const edm::EventSetup&) {
   edm::Handle<reco::VertexCollection> vertices;
   event.getByToken(vertices_token, vertices);
   
   std::unique_ptr<reco::VertexCollection> output(new reco::VertexCollection);
+  std::unique_ptr<int> nall(new int(vertices->size()));
+  std::unique_ptr<int> ngood(new int);
 
-  for (const reco::Vertex& v : *vertices)
+  for (const reco::Vertex& v : *vertices) {
     if (!v.isFake() && v.ndof() > 4 && fabs(v.z()) <= 24 && v.position().rho() < 2) {
-      output->push_back(v);
-      break;
+      if (output->empty()) output->push_back(v);
+      ++(*ngood);
     }
+  }
 
+  const bool pass = !cut || *ngood > 0;
   event.put(std::move(output));
+  event.put(std::move(nall), "nAll");
+  event.put(std::move(ngood), "nGood");
+  return pass;
 }
 
 DEFINE_FWK_MODULE(JMTFirstGoodPrimaryVertex);
