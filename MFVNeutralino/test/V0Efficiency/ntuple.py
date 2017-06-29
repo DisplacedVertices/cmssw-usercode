4c2722c1a24f4e16b5f22cff3c3553a7242a91fc
Jordan Tucker <jordantucker@gmail.com>
2017-06-29 16:00:44 -0500

    publish name and condor didn't stageout

--- MFVNeutralino/test/V0Efficiency/ntuple.py
+++ MFVNeutralino/test/V0Efficiency/ntuple.py
@@ -62,8 +62,11 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         s.files_per = 20
 
     from JMTucker.Tools.MetaSubmitter import *
-    ms = MetaSubmitter('V0NtupleV1')
+    batch_name = 'V0NtupleV1'
+    ms = MetaSubmitter(batch_name
     ms.common.ex = year
     ms.common.pset_modifier = chain_modifiers(is_mc_modifier, H_modifier)
+    ms.common.publish_name = batch_name + '_' + str(year)
     ms.crab.job_control_from_sample = True
+    ms.condor.stageout_files = 'all'
     ms.submit(samples)
