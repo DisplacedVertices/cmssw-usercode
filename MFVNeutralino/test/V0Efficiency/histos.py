f7da5e87c8d193cd2163409935d97a433fffd8bc
Jordan Tucker <jordantucker@gmail.com>
2017-06-30 12:10:25 -0500

    start to optimize cuts; fix the submit script

--- MFVNeutralino/test/V0Efficiency/histos.py
+++ MFVNeutralino/test/V0Efficiency/histos.py
@@ -3,13 +3,16 @@ from JMTucker.Tools.MiniAOD_cfg import which_global_tag
 from JMTucker.Tools.PileupWeights import pileup_weights
 from JMTucker.MFVNeutralino.Year import year
 
-is_mc = False
+is_mc = True
 H = False
+zerobias = False
 
 global_tag(process, which_global_tag(is_mc, year, H))
 process.TFileService.fileName = 'v0histos.root'
 
 process.source.fileNames = ['file:v0ntuple.root']
+process.source.fileNames = ['/store/user/tucker/V0NtupleV1_qcdht1000_hip1p0_mit.root']
+report_every(process, 100)
 
 process.load('JMTucker.MFVNeutralino.TriggerFloatsFilter_cfi')
 process.mfvTriggerFloatsFilter.require_hlt = -5 if H else 1
@@ -24,26 +27,39 @@ process.v0eff = cms.EDAnalyzer('MFVV0Efficiency',
                                tracks_src = cms.InputTag('mfvSkimmedTracks'),
                                min_track_pt = cms.double(1),
                                min_track_nsigmadxybs = cms.double(4),
-                               max_chi2ndf = cms.double(7),
+                               max_chi2ndf = cms.double(10),
                                min_p = cms.double(0),
                                max_p = cms.double(1e9),
                                mass_window_lo = cms.double(1e9),
                                mass_window_hi = cms.double(1e9),
                                min_costh3 = cms.double(-2),
-                               debug = cms.untracked.bool(True)
+                               debug = cms.untracked.bool(False)
                                )
 
-process.p = cms.Path(process.mfvTriggerFloatsFilter * process.v0eff)
+process.p = cms.Path(process.v0eff)
+if not zerobias:
+    process.p.insert(0, process.mfvTriggerFloatsFilter)
+
+for c in range(3, 10):
+    v = process.v0eff.clone(max_chi2ndf = c)
+    setattr(process, 'v0effchi2%i' % c, v)
+    process.p *= v
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
-    import JMTucker.Tools.Samples as Samples 
-    samples = Samples.data_samples + [s for s in Samples.auxiliary_data_samples if s.name.startswith('ZeroBias')]
-    for s in samples:
-        s.files_per = 50
-
+    from JMTucker.Tools.CondorSubmitter import CondorSubmitter
     from JMTucker.Tools.MetaSubmitter import *
-    ms = MetaSubmitter('V0EfficiencyV1')
-    ms.common.ex = year
-    ms.common.pset_modifier = H_modifier #chain_modifiers(is_mc_modifier, H_modifier)
-    ms.crab.job_control_from_sample = True
-    ms.submit(samples)
+    import JMTucker.Tools.Samples as Samples 
+
+    dataset = 'v0ntuplev1'
+    samples = [s for s in Samples.registry.all() if s.has_dataset(dataset)]
+    set_splitting(samples, dataset, 'default', '../ana_2015p6.json', 10)
+
+    def zerobias_modifier(sample):
+        return [], [('zerobias =XFalse'.replace('X', ' '), 'zerobias = True', 'no magic zerobias?')]
+
+    cs = CondorSubmitter('V0EfficiencyV1_v0',
+                         ex = year,
+                         dataset = dataset,
+                         pset_modifier = chain_modifiers(is_mc_modifier, H_modifier, zerobias_modifier),
+                         )
+    cs.submit_all(samples)
