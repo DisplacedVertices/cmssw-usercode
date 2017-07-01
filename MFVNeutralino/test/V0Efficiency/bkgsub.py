ed592ab75127e29a8f3192a1b3a7a4b547015ecf
Jordan Tucker <jordantucker@gmail.com>
2017-07-01 13:53:25 -0500

    little more modular; write out hists for comparison

--- MFVNeutralino/test/V0Efficiency/bkgsub.py
+++ MFVNeutralino/test/V0Efficiency/bkgsub.py
@@ -1,12 +1,14 @@
 import sys
 from JMTucker.Tools.ROOTTools import *
 set_style()
-ps = plot_saver(plot_dir('v0bkgsub'), size=(600,600))
 
-f = ROOT.TFile(sys.argv[1])
-ex = sys.argv[2] if len(sys.argv) >= 3 else ''
-h = f.Get('v0eff%s/K0_2pi/h_vtx_mass' % ex)
-h.Sumw2()
+fn = sys.argv[1]
+ex = '_' + sys.argv[2] if len(sys.argv) >= 3 else ''
+
+ps = plot_saver(plot_dir('v0bkgsub%s' % ex), size=(600,600))
+
+f = ROOT.TFile(fn)
+h = f.Get('v0eff/K0_2pi/h_vtx_mass')
 
 fit_range = 0.38, 0.6
 fit_exclude = 0.44, 0.55
@@ -21,7 +23,7 @@ def fitfunc(x, p):
 fcn = ROOT.TF1('fcn', fitfunc, fit_range[0], fit_range[1], 2)
 
 res = h.Fit(fcn, 'WL R N S Q')
-res.Print('V')
+res.Print()
 print 'Chi2 Prob:', res.Prob()
 
 fit_pars = fcn.GetParameter(0), fcn.GetParameter(1)
@@ -75,56 +77,74 @@ xax = h.GetXaxis()
 ibinc, _ = real_hist_max(h, return_bin=True)
 print 'ibinc', ibinc
 max_s, max_p, max_z = 0, 0, 0
-for lo in [7]: #xrange(50):
+def do(lo,hi,prnt=False):
     ibinlo = ibinc - lo
+    ibinhi = ibinc + hi
     xlo = xax.GetBinLowEdge(ibinlo)
-    for hi in [7]: #xrange(50):
-        ibinhi = ibinc + hi
-        xhi = xax.GetBinLowEdge(ibinhi+1)
-        ne, be = ROOT.Double(), ROOT.Double()
-        n = h.IntegralAndError(ibinlo, ibinhi, ne)
-        b = hbkg.IntegralAndError(ibinlo, ibinhi, be)
-        s = n - b
-        se = (ne**2 + be**2)**0.5
-        z = s / (b + be**2)**0.5
-        p = s/n
+    xhi = xax.GetBinLowEdge(ibinhi+1)
+    ne, be = ROOT.Double(), ROOT.Double()
+    n = h.IntegralAndError(ibinlo, ibinhi, ne)
+    b = hbkg.IntegralAndError(ibinlo, ibinhi, be)
+    s = n - b
+    se = (ne**2 + be**2)**0.5
+    z = s / (b + be**2)**0.5
+    p = s/n
+    if prnt:
+        print '%.3f-%.3f: %10.1f +- %5.1f  %10.1f +- %5.1f  %10.1f +- %5.1f : %5.2f : %5.1f' % (xlo, xhi, n, ne, b, be, s, se, p, z)
+    return xlo,xhi,n,ne,b,be,s,se,p,z
+
+for lo in xrange(50):
+    for hi in xrange(50):
+        xlo,xhi,n,ne,b,be,s,se,p,z = do(lo,hi)
         if (s > max_s or p > max_p or z > max_z) and p > 0.85:
-            print '%.3f-%.3f: %10.1f +- %5.1f  %10.1f +- %5.1f  %10.1f +- %5.1f : %5.2f : %5.1f' % (xlo, xhi, n, ne, b, be, s, se, p, z)
+            do(lo,hi,True)
         if s > max_s:
             max_s = s
         if p > max_p:
             max_p = p
         if z > max_z:
             max_z = z
+print
+xlo,xhi,n,ne,b,be,s,se,p,z = do(7,7, True)
+assert abs(xlo-0.490) < 1e-5 and abs(xhi-0.505) < 1e-5
 
 hbkglo = f.Get('v0effbkglo/K0_2pi/h_vtx_rho')
 hbkghi = f.Get('v0effbkghi/K0_2pi/h_vtx_rho')
 hon = f.Get('v0effon/K0_2pi/h_vtx_rho')
-z = hon.GetXaxis().FindBin(1.9)
 
-for i,h in enumerate((hbkghi, hbkglo, hon)):
-    h.Scale(1/h.Integral(0,z))
-    h.SetLineColor(2+i)
+for i,h in enumerate((hbkglo, hbkghi, hon)):
     h.Rebin(10)
     h.SetLineWidth(2)
+    h.SetLineColor(2+i)
+    h.SetStats(0)
+    h.GetXaxis().SetRangeUser(0,2.5)
+
+hsiglo = hon.Clone('hsiglo')
+hsighi = hon.Clone('hsighi')
+hsiglo.SetLineColor(2)
+hsighi.SetLineColor(3)
+hsig = hsiglo, hsighi
+
+for i,h in enumerate((hon, hbkglo, hbkghi)):
+    hint = h.Integral(0,h.GetNbinsX()+2)
+    if i == 0:
+        assert abs(hint - n) < 1e-5
+    else:
+        h.Scale(b/hint)
+        hsig[i-1].Add(h, -1)
+ 
     if i == 0:
         h.Draw('hist')
     else:
         h.Draw('hist same')
 ps.save('rhocmp')
 
-hbkghi = f.Get('v0effbkghi/K0_2pi/h_vtx_rho')
-hon = f.Get('v0effon/K0_2pi/h_vtx_rho')
-z = hon.GetXaxis().FindBin(1.9)
+hsiglo.Draw('hist e')
+hsighi.Draw('hist e same')
+ps.save('sig')
 
-for i,h in enumerate((hbkghi, hon)):
-    if h == hbkghi:
-        h.Scale(23593./h.Integral(0,z))
-    h.SetLineColor(2+i)
-    h.Rebin(10)
-    h.SetLineWidth(2)
-    if i == 0:
-        h.Draw('hist')
-    else:
-        h.Draw('hist same')
-ps.save('rhocmp')
+fout = ROOT.TFile('bkgsub.root', 'recreate')
+for x in hon, hbkglo, hbkghi, hsiglo, hsighi:
+    x.Write()
+fout.Write()
+fout.Close()
