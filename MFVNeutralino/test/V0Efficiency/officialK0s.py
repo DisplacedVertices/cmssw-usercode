7eb7ba68e1c553763f22dc5f4f92debd9acd7d4f
Jordan Tucker <jordantucker@gmail.com>
2017-07-13 14:52:49 -0500

    run with our cuts and not; submit script

--- MFVNeutralino/test/V0Efficiency/officialK0s.py
+++ MFVNeutralino/test/V0Efficiency/officialK0s.py
@@ -10,12 +10,14 @@ zerobias = False
 process = pat_tuple_process(None, is_mc, year, H)
 jets_only(process)
 
+#report_every(process, 100)
+process.maxEvents.input = 10000
 if is_mc:
     process.source.fileNames = ['/store/user/wsun/croncopyeos/qcdht1000/RunIISummer16DR80-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6/170606_165644/0000/reco_%i.root' % i for i in xrange(1,11)]
 else:
     process.source.fileNames = ['/store/data/Run2016E/ZeroBias/AOD/23Sep2016-v1/100000/18151C77-4486-E611-84FD-0CC47A7C357A.root']
 
-tfileservice(process, 'v0histos.root')
+tfileservice(process, 'officialK0s.root')
 
 process.p = cms.Path(process.goodOfflinePrimaryVertices)
 
@@ -33,11 +35,11 @@ process.v0eff = cms.EDAnalyzer('MFVOfficialK0s',
                                primary_vertices_src = cms.InputTag('goodOfflinePrimaryVertices'),
                                tracks_src = cms.InputTag('generalTracks'),
                                limit_set = cms.bool(True),
-                               min_track_pt = cms.double(0),
-                               min_track_nsigmadxybs = cms.double(0),
+                               min_track_pt = cms.double(1),
+                               min_track_nsigmadxybs = cms.double(4),
                                min_track_npxlayers = cms.int32(0),
                                max_track_npxlayers = cms.int32(1000000),
-                               min_track_nstlayers = cms.int32(0),
+                               min_track_nstlayers = cms.int32(6),
                                max_track_nstlayers = cms.int32(1000000),
                                min_track_nstlayersstereo = cms.int32(0),
                                max_track_nstlayersstereo = cms.int32(1000000),
@@ -50,33 +52,44 @@ process.v0eff = cms.EDAnalyzer('MFVOfficialK0s',
                                abs_eta_cut = cms.bool(False),
                                mass_window_lo = cms.double(1e9),
                                mass_window_hi = cms.double(1e9),
-                               min_costh2 = cms.double(-2),
+                               min_costh2 = cms.double(0.995),
                                min_costh3 = cms.double(-2),
                                max_geo2ddist = cms.double(2),
                                debug = cms.untracked.bool(False)
                                )
 
-process.p *= process.v0eff
-
 process.v0effbkglo = process.v0eff.clone(mass_window_lo = -0.420, mass_window_hi = -0.460)
 process.v0effbkghi = process.v0eff.clone(mass_window_lo = -0.540, mass_window_hi = -0.600)
 process.v0effon = process.v0eff.clone(mass_window_lo = -0.490, mass_window_hi = -0.505)
-process.p *= process.v0effbkglo * process.v0effbkghi * process.v0effon
+process.p *= process.v0eff * process.v0effbkglo * process.v0effbkghi * process.v0effon
+
+def addmodified(tag, code):
+    al = eval('process.v0eff     .clone(%s)' % code)
+    lo = eval('process.v0effbkglo.clone(%s)' % code)
+    hi = eval('process.v0effbkghi.clone(%s)' % code)
+    on = eval('process.v0effon   .clone(%s)' % code)
+    setattr(process, 'v0eff' + tag, al)
+    setattr(process, 'v0effbkglo' + tag, lo)
+    setattr(process, 'v0effbkghi' + tag, hi)
+    setattr(process, 'v0effon' + tag, on)
+    process.p *= al * lo * hi * on
+
+addmodified('nocut', 'min_track_pt = 0, min_track_nsigmadxybs = 0, min_track_nstlayers = 0, min_costh2 = -2')
 
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
-    from JMTucker.Tools.CondorSubmitter import CondorSubmitter
-    from JMTucker.Tools.MetaSubmitter import *
     import JMTucker.Tools.Samples as Samples 
+    samples = Samples.data_samples + [s for s in Samples.auxiliary_data_samples if s.name.startswith('ZeroBias')]
+    samples += [s for s in Samples.qcd_samples + Samples.qcd_samples_ext if '1000' in s.name or '1500' in s.name]
+    samples += Samples.qcd_hip_samples[-2:]
 
-    dataset = 'v0ntuplev1'
-    samples = [s for s in Samples.registry.all() if s.has_dataset(dataset)]
-    #samples = [Samples.qcdht1000, Samples.qcdht1000_hip1p0_mit, Samples.JetHT2016G, Samples.ZeroBias2016G]
-    set_splitting(samples, dataset, 'default', '../ana_2015p6.json', 2)
+    for s in samples:
+        s.files_per = 20
 
-    cs = CondorSubmitter('V0EfficiencyV1_v18',
-                         ex = year,
-                         dataset = dataset,
-                         pset_modifier = chain_modifiers(is_mc_modifier, H_modifier, zerobias_modifier),
-                         )
-    cs.submit_all(samples)
+    from JMTucker.Tools.MetaSubmitter import *
+    batch_name = 'V0OfficialK0sV1'
+    ms = MetaSubmitter(batch_name)
+    ms.common.ex = year
+    ms.common.pset_modifier = chain_modifiers(is_mc_modifier, H_modifier, zerobias_modifier)
+    ms.crab.job_control_from_sample = True
+    ms.submit(samples)
