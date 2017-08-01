57d2aea282c434063acbb7422769de4c51240bb8
Jennifer Chu <jyc72@cornell.edu>
2017-08-01 17:27:37 -0500

    Take the luminosity-weighted average of the effects due to the difference in track resolution between data/MC for BCDEFGH.

--- MFVNeutralino/test/compare_signal_eff.py
+++ MFVNeutralino/test/compare_signal_eff.py
@@ -23,9 +23,9 @@ if mode == 'vary_jes':
     ls = ['2016', '2016jesdown', '2016jesup']
 
 if mode == 'vary_sigmadxy':
-    root_file_dirs = ['/uscms_data/d1/jchu/crab_dirs/mfv_8025/HistosV15_0', '/uscms_data/d1/jchu/crab_dirs/mfv_8025/HistosV15_sigmadxy3p9']
-    num_paths = ['mfvEventHistosFullSel/h_bsx', 'mfvEventHistosFullSel/h_bsx']
-    ls = ['sigmadxy4p0', 'sigmadxy3p9']
+    root_file_dirs = ['/uscms_data/d1/jchu/crab_dirs/mfv_8025/HistosV15_0', '/uscms_data/d1/jchu/crab_dirs/mfv_8025/HistosV15_sigmadxy3p7', '/uscms_data/d1/jchu/crab_dirs/mfv_8025/HistosV15_sigmadxy3p8', '/uscms_data/d1/jchu/crab_dirs/mfv_8025/HistosV15_sigmadxy3p9', '/uscms_data/d1/jchu/crab_dirs/mfv_8025/HistosV15_sigmadxy4p1', '/uscms_data/d1/jchu/crab_dirs/mfv_8025/HistosV15_sigmadxy4p2', '/uscms_data/d1/jchu/crab_dirs/mfv_8025/HistosV15_sigmadxy4p3']
+    num_paths = ['mfvEventHistosFullSel/h_bsx', 'mfvEventHistosFullSel/h_bsx', 'mfvEventHistosFullSel/h_bsx', 'mfvEventHistosFullSel/h_bsx', 'mfvEventHistosFullSel/h_bsx', 'mfvEventHistosFullSel/h_bsx', 'mfvEventHistosFullSel/h_bsx']
+    ls = ['sigmadxy4p0', 'sigmadxy3p7', 'sigmadxy3p8', 'sigmadxy3p9', 'sigmadxy4p1', 'sigmadxy4p2', 'sigmadxy4p3']
 
 nevs = []
 for i,root_file_dir in enumerate(root_file_dirs):
@@ -77,7 +77,7 @@ for i,nev in enumerate(nevs):
         sample.yh = r+er
         print '%26s: ratio = %.3f (%.3f, %.3f)' % (sample.name, sample.y, sample.yl, sample.yh)
 
-    per = PerSignal('ratio of efficiencies', y_range=(0.9,1.1) if mode != 'vary_sigmadxy' else (0.7,1.3))
+    per = PerSignal('ratio of efficiencies', y_range=(0.9,1.1) if mode != 'vary_sigmadxy' else (0.,2.))
     per.add(multijet, title='#tilde{N} #rightarrow tbs')
     per.add(dijet, title='X #rightarrow d#bar{d}', color=ROOT.kBlue)
     per.draw(canvas=ps.c)
@@ -92,7 +92,6 @@ for j,sample in enumerate(multijet + dijet):
 
     v1 = nevs[1][j]
     ev1 = v1**0.5
-
     r1 = v1/c
     er1 = (v1/c) * ((ev1/v1)**2 + (ec/c)**2)**0.5 * (abs(r1-1))**0.5 / (1+r1)**0.5
 
@@ -102,19 +101,37 @@ for j,sample in enumerate(multijet + dijet):
     if len(nevs) > 2:
         v2 = nevs[2][j]
         ev2 = v2**0.5
-
         r2 = v2/c
         er2 = (v2/c) * ((ev2/v2)**2 + (ec/c)**2)**0.5 * (abs(r2-1))**0.5 / (1+r2)**0.5
 
         r = (abs(r1-1) + abs(r2-1)) / 2
         er = (er1**2 + er2**2)**0.5 / 2
 
+    if mode == 'vary_sigmadxy':
+        v3 = nevs[3][j]
+        ev3 = v3**0.5
+        r3 = v3/c
+        er3 = (v3/c) * ((ev3/v3)**2 + (ec/c)**2)**0.5 * (abs(r3-1))**0.5 / (1+r3)**0.5
+
+        v4 = nevs[4][j]
+        ev4 = v4**0.5
+        r4 = v4/c
+        er4 = (v4/c) * ((ev4/v4)**2 + (ec/c)**2)**0.5 * (abs(r4-1))**0.5 / (1+r4)**0.5
+
+        v5 = nevs[5][j]
+        ev5 = v5**0.5
+        r5 = v5/c
+        er5 = (v5/c) * ((ev5/v5)**2 + (ec/c)**2)**0.5 * (abs(r5-1))**0.5 / (1+r5)**0.5
+
+        r = (8.65/35.93)*abs(r1-1) + (7.58/35.93)*abs(r2-1) + (3.11/35.93)*abs(r3-1) + (6.81/35.93)*abs(r4-1) + (5.75/35.93)*abs(r5-1)
+        er = (((8.65/35.93)*er1)**2 + ((7.58/35.93)*er2)**2 + ((3.11/35.93)*er3)**2 + ((6.81/35.93)*er4)**2 + ((5.75/35.93)*er5)**2)**0.5
+
     sample.y = r
     sample.yl = r-er
     sample.yh = r+er
     print '%26s: uncertainty = %.3f (%.3f, %.3f)' % (sample.name, sample.y, sample.yl, sample.yh)
 
-per = PerSignal('uncertainty in signal efficiency', y_range=(0.,0.1) if mode != 'vary_sigmadxy' else (0.,0.3))
+per = PerSignal('uncertainty in signal efficiency', y_range=(0.,0.1) if mode != 'vary_sigmadxy' else (0.,1.))
 per.add(multijet, title='#tilde{N} #rightarrow tbs')
 per.add(dijet, title='X #rightarrow d#bar{d}', color=ROOT.kBlue)
 per.draw(canvas=ps.c)
