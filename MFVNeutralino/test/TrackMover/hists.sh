6dd9e4603e67d78aa46d419a9934a526ddc84c32
Jordan Tucker <jordantucker@gmail.com>
2017-08-09 14:08:40 -0500

    inpath var; new list of files; test mode modifies hists.jdl to have the right njobs; snippet at end for hipplusno merge

--- MFVNeutralino/test/TrackMover/hists.sh
+++ MFVNeutralino/test/TrackMover/hists.sh
@@ -2,36 +2,29 @@
 
 job=$1
 
+inpath=root://cmseos.fnal.gov//store/user/tucker/TrackMoverV1
+
 fns=(
- JetHT2015.root 
- JetHT2015C.root 
- JetHT2015D.root 
- JetHT2016.root 
- JetHT2016B3.root 
- JetHT2016BCD.root 
- JetHT2016BthruG.root 
- JetHT2016C.root 
- JetHT2016D.root 
- JetHT2016E.root 
- JetHT2016EF.root 
- JetHT2016F.root 
- JetHT2016G.root 
- JetHT2016H.root 
- JetHT2016H2.root 
- JetHT2016H3.root 
- qcdht0500sum.root 
- qcdht0500sum_2015.root 
- qcdht0700sum.root 
- qcdht0700sum_2015.root 
- qcdht1000sum.root 
- qcdht1000sum_2015.root 
- qcdht1500sum.root 
- qcdht1500sum_2015.root 
- qcdht2000sum.root 
- qcdht2000sum_2015.root 
- ttbar.root 
- ttbar_2015.root
- )
+JetHT2016.root
+JetHT2016B3.root
+JetHT2016BCD.root
+JetHT2016BCDEF.root
+JetHT2016BthruG.root
+JetHT2016C.root
+JetHT2016D.root
+JetHT2016E.root
+JetHT2016EF.root
+JetHT2016F.root
+JetHT2016G.root
+JetHT2016GH.root
+JetHT2016H.root
+JetHT2016H2.root
+JetHT2016H3.root
+qcdht1000.root
+qcdht1000_hip1p0_mit.root
+qcdht1500.root
+qcdht1500_hip1p0_mit.root
+)
 nfns=${#fns[@]}
 
 nls=( 2 3 )
@@ -39,8 +32,14 @@ nbs=( 0 1 2 )
 
 nnls=${#nls[@]}
 nnbs=${#nbs[@]}
-
-echo \#fns $nfns \#nls $nnls \#nbs $nnbs
+njobs=$((nfns * nnls * nnbs))
+echo inpath is $inpath
+echo \#fns $nfns \#nls $nnls \#nbs $nnbs max jobs $njobs
+if [[ $job == "test" ]]; then
+    echo test only, possibly modifying hists.jdl
+    sed -i -e "s/Queue.*/Queue $njobs/" hists.jdl
+    exit 1
+fi
 
 nmax=$((nfns * nnls * nnbs))
 echo job $job, nmax is $nmax
@@ -67,10 +66,18 @@ cd CMSSW_8_0_25/src
 eval $(scram ru -sh)
 cd ../..
 
-./hists.exe root://cmseos.fnal.gov//store/user/tucker/TrackMoverV1/$z/$fn $outfn $nl $nb 2>&1
+./hists.exe $inpath/$z/$fn $outfn $nl $nb 2>&1
 
 # check with:
 #   grep 'return value' hists.log.* | grep -v 'value 0'
 # when done:
 #   tar --remove-files -czf lastlogs.tgz hists.{stdout,stderr,log}.*
 #   for x in 20 21 22 30 31 32 ; do mkdir $x ; for y in ${x}*root; do mv $y $x/${y/${x}_/} ; done ; cd $x ; py $CMSSW_BASE/src/JMTucker/MFVNeutralino/test/utilities.py merge_background ; cd - ; done
+
+# hip and not hip merge:
+# for x in ??; do
+#   cd $x
+#   samples merge qcdht1000and1500_hip1p0_mit.root -22300 qcdht*_hip1p0_mit.root
+#   samples merge qcdht1000and1500.root -16200 qcdht{1000,1500}sum.root
+#   cd -
+# done
