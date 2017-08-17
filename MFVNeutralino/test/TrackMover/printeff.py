5a54c0520c814fca41bb0419454f35cd52c1bd53
Jordan Tucker <jordantucker@gmail.com>
2017-08-17 09:57:56 -0500

    column headers

--- MFVNeutralino/test/TrackMover/printeff.py
+++ MFVNeutralino/test/TrackMover/printeff.py
@@ -2,17 +2,17 @@ import sys, os
 from JMTucker.Tools.ROOTTools import *
 
 fns = [fn for fn in sys.argv[1:] if os.path.isfile(fn) and fn.endswith('.root')]
-ml = max(len(fn) for fn in fns)
+width = max(len(fn) for fn in fns) + 2
 
-for fn in sys.argv[1:]:
-    if os.path.isfile(fn) and fn.endswith('.root'):
-        f = ROOT.TFile(fn)
-        print fn.ljust(ml+2),
-        for x in 'nocuts', 'ntracks', 'all':
-            num = get_integral(f.Get('%s_npv_num'%x))[0]
-            den = get_integral(f.Get('%s_npv_den'%x))[0]
-            r,l,h = wilson_score(num, den)
-            print '   %6.4f +- %6.4f' % (r, (h-l)/2),
-        print
+cutsets = ['nocuts', 'ntracks', 'all']
+print 'fn'.ljust(width), ' '.join('%19s' % x for x in cutsets)
 
-               
+for fn in fns:
+    f = ROOT.TFile(fn)
+    print fn.ljust(width),
+    for cutset in cutsets:
+        num = get_integral(f.Get('%s_npv_num' % cutset))[0]
+        den = get_integral(f.Get('%s_npv_den' % cutset))[0]
+        r,l,h = wilson_score(num, den)
+        print '   %6.4f +- %6.4f' % (r, (h-l)/2),
+    print
