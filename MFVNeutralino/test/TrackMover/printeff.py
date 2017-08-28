96b6e32397ef761bcc6fdf561c8a424a06526d13
Jordan Tucker <jordantucker@gmail.com>
2017-08-28 09:58:41 -0500

    print differences if only 2 fns

--- MFVNeutralino/test/TrackMover/printeff.py
+++ MFVNeutralino/test/TrackMover/printeff.py
@@ -7,12 +7,23 @@ width = max(len(fn) for fn in fns) + 2
 cutsets = ['nocuts', 'ntracks', 'all']
 print 'fn'.ljust(width), ' '.join('%19s' % x for x in cutsets)
 
+z = []
 for fn in fns:
     f = ROOT.TFile(fn)
     print fn.ljust(width),
+    zz = []
     for cutset in cutsets:
         num = get_integral(f.Get('%s_npv_num' % cutset))[0]
         den = get_integral(f.Get('%s_npv_den' % cutset))[0]
         r,l,h = wilson_score(num, den)
-        print '   %6.4f +- %6.4f' % (r, (h-l)/2),
+        e = (h-l)/2
+        print '   %6.4f +- %6.4f' % (r, e),
+        zz.append((r,e))
+    z.append(zz)
     print
+
+if len(z) == 2:
+    print 'difference'.ljust(width),
+    a,b = z
+    for (aa,ea),(bb,eb) in zip(a,b):
+        print '   %6.4f +- %6.4f' % (bb-aa, (ea**2 + eb**2)**0.5),
