9f3ef06d9dc23423458458885c415b27a0adb6e7
Jordan Tucker <jordantucker@gmail.com>
2017-08-28 21:45:21 -0500

    v3: loop over nsigmadxy too (use named_product to make the loop more manageable); tau back to not being easily configurable, but instead we'll reweight in hists; put off btag wp change until v4.

--- MFVNeutralino/test/TrackMover/ntuple.py
+++ MFVNeutralino/test/TrackMover/ntuple.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
 import sys
-from JMTucker.Tools.general import typed_from_argv
+from JMTucker.Tools.general import named_product, typed_from_argv
 from JMTucker.Tools.MiniAOD_cfg import *
 from JMTucker.Tools.CMSSWTools import *
 from JMTucker.MFVNeutralino.Year import year
 
-tau = 1.
-version = 2
+version = 3
+cfgs = named_product(njets = [2,3],
+                     nbjets = [0,1,2],
+                     nsigmadxy = [3.7 + i*0.1 for i in xrange(7)])
 # rest are magic lines for the submitter
 is_mc = True
 H = False
 repro = False
 
-if version >= 3:
+if version >= 4:
     # btag wp changed for rereco https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation80XReReco
     raise ValueError('change the btag working points before running next version')
 
@@ -22,7 +24,7 @@ remove_met_filters(process)
 remove_output_module(process)
 
 tfileservice(process, 'movedtree.root')
-random_dict = {}
+random_dict = {} # create after loop below gets all the seeds
 
 process.patMuons.embedTrack = False
 process.patElectrons.embedTrack = False
@@ -38,54 +40,58 @@ process.load('JMTucker.MFVNeutralino.EventProducer_cfi')
 process.load('JMTucker.MFVNeutralino.WeightProducer_cfi')
 process.mfvWeight.throw_if_no_mcstat = False
 
-for njets in 2,3:
-    for nbjets in 0,1,2:
-        ex = '%i%i' % (njets, nbjets)
-        tracks_name = 'mfvMovedTracks' + ex
-        auxes_name = 'mfvVerticesAux' + ex
-        tree_name = 'mfvMovedTree' + ex
+for icfg, cfg in enumerate(cfgs):
+    nsigmadxy_name = ('nsig%.1f' % cfg.nsigmadxy).replace('.', 'p')
+    ex = '%i%i%s' % (cfg.njets, cfg.nbjets, nsigmadxy_name)
 
-        random_dict[tracks_name] = 13068 + njets*10 + nbjets
+    tracks_name = 'mfvMovedTracks' + ex
+    auxes_name = 'mfvVerticesAux' + ex
+    tree_name = 'mfvMovedTree' + ex
 
-        tracks = cms.EDProducer('MFVTrackMover',
-                                tracks_src = cms.InputTag('generalTracks'),
-                                primary_vertices_src = cms.InputTag('goodOfflinePrimaryVertices'),
-                                jets_src = cms.InputTag('selectedPatJets'),
-                                min_jet_pt = cms.double(50),
-                                min_jet_ntracks = cms.uint32(4),
-                                b_discriminator = cms.string('pfCombinedInclusiveSecondaryVertexV2BJetTags'),
-                                b_discriminator_veto = cms.double(0.46), # change to 0.5426
-                                b_discriminator_tag = cms.double(0.935), #   "     " 0.9535
-                                njets = cms.uint32(njets),
-                                nbjets = cms.uint32(nbjets),
-                                tau = cms.double(tau),
-                                sig_theta = cms.double(0.2),
-                                sig_phi = cms.double(0.2),
-                                )
+    random_dict[tracks_name] = 13068 + icfg
 
-        modifiedVertexSequence(process, ex, track_src = tracks_name)
+    tracks = cms.EDProducer('MFVTrackMover',
+                            tracks_src = cms.InputTag('generalTracks'),
+                            primary_vertices_src = cms.InputTag('goodOfflinePrimaryVertices'),
+                            jets_src = cms.InputTag('selectedPatJets'),
+                            min_jet_pt = cms.double(50),
+                            min_jet_ntracks = cms.uint32(4),
+                            b_discriminator = cms.string('pfCombinedInclusiveSecondaryVertexV2BJetTags'),
+                            b_discriminator_veto = cms.double(0.46), # change to 0.5426
+                            b_discriminator_tag = cms.double(0.935), #   "     " 0.9535
+                            njets = cms.uint32(cfg.njets),
+                            nbjets = cms.uint32(cfg.nbjets),
+                            tau = cms.double(1.),
+                            sig_theta = cms.double(0.2),
+                            sig_phi = cms.double(0.2),
+                            )
 
-        tree = cms.EDAnalyzer('MFVMovedTracksTreer',
-                              event_src = cms.InputTag('mfvEvent'),
-                              weight_src = cms.InputTag('mfvWeight'),
-                              mover_src = cms.string(tracks_name),
-                              vertices_src = cms.InputTag(auxes_name),
-                              max_dist2move = cms.double(0.02),
-                              apply_presel = cms.bool(True),
-                              njets_req = cms.uint32(njets),
-                              nbjets_req = cms.uint32(nbjets),
-                              for_mctruth = cms.bool(False),
-                              )
+    modifiedVertexSequence(process, ex,
+                           track_src = tracks_name,
+                           min_all_track_sigmadxy = cfg.nsigmadxy,
+                           min_seed_track_sigmadxy = cfg.nsigmadxy)
 
-        setattr(process, tracks_name, tracks)
-        setattr(process, tree_name, tree)
-        process.p *= tree
+    tree = cms.EDAnalyzer('MFVMovedTracksTreer',
+                          event_src = cms.InputTag('mfvEvent'),
+                          weight_src = cms.InputTag('mfvWeight'),
+                          mover_src = cms.string(tracks_name),
+                          vertices_src = cms.InputTag(auxes_name),
+                          max_dist2move = cms.double(0.02),
+                          apply_presel = cms.bool(True),
+                          njets_req = cms.uint32(cfg.njets),
+                          nbjets_req = cms.uint32(cfg.nbjets),
+                          for_mctruth = cms.bool(False),
+                          )
+
+    setattr(process, tracks_name, tracks)
+    setattr(process, tree_name, tree)
+    process.p *= tree
 
 random_service(process, random_dict)
 
-#process.options.wantSummary = True
+#want_summary(process)
 process.maxEvents.input = 100
-#file_event_from_argv(process)
+file_event_from_argv(process)
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.MetaSubmitter import *
@@ -102,8 +108,7 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
 
     modify = chain_modifiers(is_mc_modifier, H_modifier, repro_modifier)
 
-    ex = {1.: '', 0.1: '_1mm', 0.03: '_300um'}[tau]
-    batch_name = 'TrackMoverV%i%s' % (version, ex)
+    batch_name = 'TrackMoverV%i' % version
     ms = MetaSubmitter(batch_name)
     ms.common.ex = year
     ms.common.pset_modifier = modify
