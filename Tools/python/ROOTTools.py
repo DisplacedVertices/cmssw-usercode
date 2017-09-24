16e2447e2ce71b10b382d05099561c6c073f4352
Jordan Tucker <jordantucker@gmail.com>
2017-09-24 14:10:03 -0500

    data_mc_comparison: if normalize_to_data and data_integ is 0, scale mc to 1

--- Tools/python/ROOTTools.py
+++ Tools/python/ROOTTools.py
@@ -745,12 +745,14 @@ def data_mc_comparison(name,
             data_sample.hist.Add(ds.hist)
             print 'integ', get_integral(data_sample.hist)[0]
 
-    if normalize_to_data and data_sample: # JMTBAD only equal area for now
-        data_integ = get_integral(data_sample.hist)[0]
-        bkg_integ = sum(get_integral(sample.hist)[0] for sample in background_samples)
-        print 'normalize_to_data scaling to equal area: data %f bkg %f sf %f' % (data_integ, bkg_integ, data_integ / bkg_integ)
-        for sample in background_samples:
-            sample.hist.Scale(data_integ / bkg_integ)
+        if normalize_to_data:
+            data_integ = get_integral(data_sample.hist)[0]
+            if data_integ == 0:
+                data_integ = 1
+                bkg_integ = sum(get_integral(sample.hist)[0] for sample in background_samples)
+                print 'normalize_to_data scaling to equal area: data %f bkg %f sf %f' % (data_integ, bkg_integ, data_integ / bkg_integ)
+                for sample in background_samples:
+                    sample.hist.Scale(data_integ / bkg_integ)
 
     #####################
 
