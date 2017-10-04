97ed89b1480d11c2be012d21e4850a0081d2e147
Jordan Tucker <jordantucker@gmail.com>
2017-10-04 16:03:45 -0500

    oops

--- MFVNeutralino/test/One2Two/smallsigscan.py
+++ MFVNeutralino/test/One2Two/smallsigscan.py
@@ -19,10 +19,6 @@ kinds = 'mfv_neu', 'mfv_ddbar'
 taus = [100, 400, 1000, 10000, 31000]
 masses = [300,400,500,600, 800, 1200, 1800, 3000]
 
-kinds = ['mfv_neu']
-taus = [1000]
-masses = [800]
-
 f = ROOT.TFile('limits_input.root')
 sample_nums = [name2isample(f, name) for name in ['%s_tau%05ium_M%04i' % (k,t,m) for k in kinds for t in taus for m in masses]]
 
