5aaac21f7c30b1863f436b8b4a4d83680386e415
Jennifer Chu <jyc72@cornell.edu>
2017-10-12 15:03:47 -0500

    Add lines to indicate the bin edges.

--- MFVNeutralino/test/One2Two/pretty_closure.py
+++ MFVNeutralino/test/One2Two/pretty_closure.py
@@ -23,20 +23,22 @@ for i in range(4):
     hh.GetYaxis().SetTitleSize(0.04)
     hh.GetYaxis().SetTitleOffset(1.3)
     hh.SetStats(0)
-    hh.SetLineColor(ROOT.kBlue)
-    hh.SetLineWidth(2)
+    hh.SetLineColor(ROOT.kBlack)
+    hh.SetLineWidth(3)
     hh.SetMinimum(0)
+    hh.SetMarkerStyle(20)
+    hh.SetMarkerSize(1.3)
     hh.Draw()
 
     h = ROOT.TFile(fns[i]).Get('h_c1v_dvv')
     h.SetStats(0)
-    h.SetLineColor(ROOT.kRed)
-    h.SetLineWidth(2)
+    h.SetLineColor(ROOT.kBlue)
+    h.SetLineWidth(3)
     h.Scale(hh.Integral()/h.Integral())
     h.Draw('hist sames')
 
     l1 = ROOT.TLegend(0.50, 0.65, 0.85, 0.78)
-    l1.AddEntry(hh, 'Data', 'LE')
+    l1.AddEntry(hh, 'Data', 'LPE')
     l1.AddEntry(h, 'Construction')
     l1.SetBorderSize(0)
     l1.Draw()
@@ -45,5 +47,17 @@ for i in range(4):
     write(61, 0.040, 0.098, 0.91, 'CMS')
     write(52, 0.035, 0.185, 0.91, 'Preliminary')
     write(42, 0.040, 0.625, 0.91, '38.5 fb^{-1} (13 TeV)')
+
+    ymax = [133, 36.1, 3.58, 2.1]
+    lines = [
+        ROOT.TLine(0.04, 0, 0.04, ymax[i]),
+        ROOT.TLine(0.07, 0, 0.07, ymax[i]),
+        ]
+
+    for ll in lines:
+        ll.SetLineColor(ROOT.kRed)
+        ll.SetLineWidth(2)
+        ll.SetLineStyle(2)
+        ll.Draw()
 
     ps.save('closure_%s' % ntk[i])
