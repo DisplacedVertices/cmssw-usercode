1d03724af601c75a59af842b1fd7ea61070ec2fb
Jennifer Chu <jyc72@cornell.edu>
2017-10-31 16:03:06 -0500

    Closure plots: poisson error bars.

--- MFVNeutralino/test/One2Two/pretty_closure.py
+++ MFVNeutralino/test/One2Two/pretty_closure.py
@@ -7,6 +7,7 @@ ps = plot_saver('../plots/EXO-17-018/closure', size=(700,700), log=False, pdf=Tr
 fns = ['2v_from_jets_data_2015p6_3track_default_v15_v5.root', '2v_from_jets_data_2015p6_7track_default_v15_v5.root', '2v_from_jets_data_2015p6_4track_default_v15_v5.root', '2v_from_jets_data_2015p6_5track_default_v15_v5.root']
 ntk = ['3track3track', '4track3track', '4track4track', '5track5track']
 names = ['3-track x 3-track', '4-track x 3-track', '4-track x 4-track', '#geq 5-track x #geq 5-track']
+ymax = [140, 40, 5, 4]
 
 def write(font, size, x, y, text):
     w = ROOT.TLatex()
@@ -18,24 +19,24 @@ def write(font, size, x, y, text):
 
 for i in range(4):
     hh = ROOT.TFile(fns[i]).Get('h_2v_dvv')
-    hh.SetTitle(';d_{VV} (cm);Events/100 #mum')
-    hh.GetXaxis().SetTitleSize(0.04)
-    hh.GetYaxis().SetTitleSize(0.04)
-    hh.GetYaxis().SetTitleOffset(1.3)
-    hh.SetStats(0)
-    hh.SetLineColor(ROOT.kBlack)
-    hh.SetLineWidth(3)
-    hh.SetMinimum(0)
-    hh.SetMarkerStyle(20)
-    hh.SetMarkerSize(1.3)
-    hh.Draw()
-
     h = ROOT.TFile(fns[i]).Get('h_c1v_dvv')
+    h.Scale(hh.Integral()/h.Integral())
+
+    h.SetTitle(';d_{VV} (cm);Events/100 #mum')
+    h.GetXaxis().SetTitleSize(0.04)
+    h.GetYaxis().SetTitleSize(0.04)
+    h.GetYaxis().SetTitleOffset(1.3)
+    h.GetYaxis().SetRangeUser(0,ymax[i])
     h.SetStats(0)
     h.SetLineColor(ROOT.kBlue)
     h.SetLineWidth(3)
-    h.Scale(hh.Integral()/h.Integral())
-    h.Draw('hist sames')
+    h.Draw('hist')
+
+    hh = poisson_intervalize(hh)
+    hh.SetLineWidth(3)
+    hh.SetMarkerStyle(20)
+    hh.SetMarkerSize(1.3)
+    hh.Draw('PE')
 
     l1 = ROOT.TLegend(0.50, 0.65, 0.85, 0.78)
     l1.AddEntry(hh, 'Data', 'LPE')
@@ -48,7 +49,6 @@ for i in range(4):
     write(52, 0.035, 0.185, 0.91, 'Preliminary')
     write(42, 0.040, 0.625, 0.91, '38.5 fb^{-1} (13 TeV)')
 
-    ymax = [133, 36.1, 3.58, 2.1]
     lines = [
         ROOT.TLine(0.04, 0, 0.04, ymax[i]),
         ROOT.TLine(0.07, 0, 0.07, ymax[i]),
