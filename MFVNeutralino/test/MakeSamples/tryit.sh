8ad6472c0b7a3ee14d53d85ecc91fee7414b89e3
Jordan Tucker <jordantucker@gmail.com>
2017-12-07 01:46:28 -0600

    nead to write year.txt and update list of files to copy

--- MFVNeutralino/test/MakeSamples/tryit.sh
+++ MFVNeutralino/test/MakeSamples/tryit.sh
@@ -2,14 +2,14 @@
 # run nstep.sh/cmsRun in the same shell, but rather make a new one as
 # the print out will instruct
 
-echo be sure to change steering.sh to what you want, it was set for the last test
+echo be sure to change year.txt and steering.sh to what you want, it was set for the last test
 owd=$(pwd)
 input_version=$CMSSW_VERSION
-tmpdir=/uscmst1b_scratch/lpc1/3DayLifetime/$USER/$(date +%s)
+tmpdir=/uscmst1b_scratch/lpc1/3DayLifetime/$USER/nstep_tryit_$(date +%s)
 echo $tmpdir
 mkdir $tmpdir 
 edmConfigDump dummy.py > $tmpdir/pset.py
-cp nstep.sh todoify.sh lhe.py gensim.py modify.py dynamicconf.py rawhlt.py minbias.py minbias.txt.gz minbias_premix.txt.gz reco.py fixfjr.py $tmpdir
+cp nstep.sh todoify.sh lhe.py gensim.py modify.py dynamicconf.py scanpack.py rawhlt.py minbias.py minbias.txt.gz minbias_premix.txt.gz private_minbias.txt.gz reco.py fixfjr.py $tmpdir
 for x in ntuple.py minitree.py; do
     cmsDumpPython.py ../$x > $tmpdir/$x
 done
@@ -18,20 +18,18 @@ scram project CMSSW $input_version
 cd $input_version
 cmsMakeTarball.py input.tgz
 tar xf input.tgz
+echo 2016 > $tmpdir/year.txt
 cat > $tmpdir/steering.sh <<EOF
-MAXEVENTS=50
-EXPECTEDEVENTS=3
-SALT=fixedsalt
-USETHISCMSSW=1
-FROMLHE=1
-TRIGFILTER=1
+MAXEVENTS=10
+EXPECTEDEVENTS=10
+USETHISCMSSW=0
+FROMLHE=0
+TRIGFILTER=0
 PREMIX=0
-export DUMMYFORHASH=1494462430446983
-OUTPUTLEVEL=reco
-TODO=todo=qcdht,1000
-TODORAWHLT=SETME
-TODORECO=SETME
-TODONTUPLE=SETME
+export DUMMYFORHASH=1512629531475509
+OUTPUTLEVEL=minitree
+SCANPACK=scanpack=scanpacktest,1
+SALT="scanpacktest_1_None_scanpacktest_1"
 EOF
 echo in new shell, issue e.g.
 echo "cd $tmpdir/$input_version/src ; source /cvmfs/cms.cern.ch/cmsset_default.sh ; eval \$(scramv1 runtime -sh) ; cd ../.. ; ./nstep.sh 1"
