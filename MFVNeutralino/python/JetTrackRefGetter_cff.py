d5e1a72044547d25b9282cd662357b27fbef4adb
Jordan Tucker <jordantucker@gmail.com>
2017-12-08 14:48:35 -0600

    helper class for getting track refs from pat jets whether miniaod or not, for use with MFVUnpackedCandidateTracks

---
+++ MFVNeutralino/python/JetTrackRefGetter_cff.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+
+mfvJetTrackRefGetter = cms.PSet(
+    input_is_miniaod = cms.bool(False),
+    unpacked_tracks_src = cms.InputTag('mfvUnpackedCandidateTracks'),
+    unpacking_map_src = cms.InputTag('mfvUnpackedCandidateTracks'),
+    )
