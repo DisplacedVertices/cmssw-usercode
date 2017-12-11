f1d7b17cc5a77642ab34c5098e397ef77b3e8cb4
Jordan Tucker <jordantucker@gmail.com>
2017-12-11 09:46:52 -0600

    update, this time starting from real miniaod and not my weird hacked version of it

--- MFVNeutralino/test/ntuple2.py
+++ MFVNeutralino/test/ntuple2.py
@@ -5,7 +5,7 @@ from JMTucker.Tools.CMSSWTools import *
 from JMTucker.MFVNeutralino.Year import year
 
 is_mc = True
-version = 'v9'
+version = 'v16maod'
 batch_name = 'Ntuple' + version.upper()
 
 ####
@@ -27,30 +27,48 @@ process.load('CommonTools.ParticleFlow.goodOfflinePrimaryVertices_cfi')
 process.load('PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi')
 process.load('PhysicsTools.PatAlgos.selectionLayer1.muonSelector_cfi')
 process.load('PhysicsTools.PatAlgos.selectionLayer1.electronSelector_cfi')
+process.load('JMTucker.Tools.MCStatProducer_cff')
 process.load('JMTucker.Tools.PATTupleSelection_cfi')
+process.load('JMTucker.MFVNeutralino.UnpackedCandidateTracks_cfi')
 process.load('JMTucker.MFVNeutralino.Vertexer_cff')
+process.load('JMTucker.MFVNeutralino.TriggerFloats_cff')
 process.load('JMTucker.MFVNeutralino.EventProducer_cfi')
 
+process.goodOfflinePrimaryVertices.src = 'offlineSlimmedPrimaryVertices'
+
+process.selectedPatJets.src = 'slimmedJets'
 process.selectedPatJets.cut = process.jtupleParams.jetCut
-process.selectedPatJets.src = 'preselectedPatJets'
+
+process.selectedPatMuons.src = 'slimmedMuons'
 process.selectedPatMuons.cut = process.jtupleParams.muonCut
+
+process.selectedPatElectrons.src = 'slimmedElectrons'
 process.selectedPatElectrons.cut = process.jtupleParams.electronCut
 
-process.mfvVertices.track_src = 'skimmedGeneralTracks'
+process.mfvGenParticles.gen_particles_src = 'prunedGenParticles'
+
+process.mfvVertices.track_src = 'mfvUnpackedCandidateTracks'
+
+#process.mfvVerticesToJets.enable = False # have to figure out how to use daughters of slimmed jets and the map we make in UnpackedCandidateTracks
+process.mfvVerticesToJets.input_is_miniaod = True
+process.mfvVerticesAuxTmp.input_is_miniaod = True
+process.mfvVerticesAuxPresel.input_is_miniaod = True
+
+process.mfvEvent.gen_particles_src = 'prunedGenParticles' # no idea if this lets gen_flavor_code, gen_bquarks, gen_leptons work. I think for the latter you'd want the packed ones that have status 1 particles
+process.mfvEvent.gen_jets_src = 'slimmedGenJets'
+process.mfvEvent.pileup_info_src = 'slimmedAddPileupInfo'
+process.mfvEvent.met_src = 'slimmedMETs'
 
 process.p = cms.Path(process.goodOfflinePrimaryVertices *
                      process.selectedPatJets *
                      process.selectedPatMuons *
                      process.selectedPatElectrons *
-                     process.mfvVertices *
-                     process.mfvEvent *
-                     process.mfvGenParticles *
-                     process.mfvVerticesAuxTmp *
-                     process.mfvSelectedVerticesTmp *
-                     process.mfvVerticesToJets *
-                     process.mfvVerticesAux)
+                     process.mfvTriggerFloats *
+                     process.mfvUnpackedCandidateTracks *
+                     process.mfvVertexSequence *
+                     process.mfvEvent)
 
-#process.options.wantSummary = True
-process.maxEvents.input = 100
-process.source.fileNames = ['file:miniaod.root']
+process.options.wantSummary = True
+process.maxEvents.input = -1
+process.source.fileNames = ['file:/uscmst1b_scratch/lpc1/3DayLifetime/tucker/A00610B3-00B7-E611-8546-A0000420FE80.root']
 file_event_from_argv(process)
