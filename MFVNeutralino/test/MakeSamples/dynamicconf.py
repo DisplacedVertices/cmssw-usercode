238a92ccee65ee9117ccfc90227e48333e014216
Jordan Tucker <jordantucker@gmail.com>
2017-12-13 14:28:21 -0600

    instead of trying to understand usethiscmssw=1 and 2015 versions, just figure out globaltag from cmssw version major+minor

--- MFVNeutralino/test/MakeSamples/dynamicconf.py
+++ MFVNeutralino/test/MakeSamples/dynamicconf.py
@@ -7,17 +7,15 @@ doing_2015 = year == '2015'
 
 cmssw_version = os.environ['CMSSW_VERSION'].replace('patch', '').replace('pre', '-')
 cmssw_version = tuple(int(x) for x in cmssw_version.split('_')[1:])
-if cmssw_version[0] not in (7,8):
+if cmssw_version[:2] not in [(7,1),(7,6),(8,0)]:
     raise ValueError('unsupported cmssw version')
 
 if cmssw_version[0] == 8: 
     assert not doing_2015
-    globaltag_gensim = '80X_mcRun2_asymptotic_2016_TrancheIV_v6' 
-else:
-    globaltag_gensim = 'MCRUN2_71_V1::All'
 
-if doing_2015:
-    globaltag = '76X_mcRun2_asymptotic_v12'
-else:
+if cmssw_version[:2] == (8,0):
     globaltag = '80X_mcRun2_asymptotic_2016_TrancheIV_v6' 
-
+elif cmssw_version[:2] == (7,6):
+    globaltag = '76X_mcRun2_asymptotic_v12'
+elif cmssw_version[:2] == (7,1):
+    globaltag = 'MCRUN2_71_V1::All'
