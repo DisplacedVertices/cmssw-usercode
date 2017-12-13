1378fd5f37c2e260ece075ee4d4c1c642cb3bfda
Jordan Tucker <jordantucker@gmail.com>
2017-12-13 13:59:54 -0600

    this wasn't updated on GenParticles repurposing--this py just for debugging it

--- MFVNeutralino/test/gen_particles.py
+++ MFVNeutralino/test/gen_particles.py
@@ -1,28 +1,16 @@
-import sys, FWCore.ParameterSet.Config as cms
+from JMTucker.Tools.BasicAnalyzer_cfg import *
 
-process = cms.Process('MFVGenParticles')
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5))
-process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring('/store/user/tucker/mfv_neutralino_tau1000um_M0400/mfv_neutralino_tau1000um_M0400/a6ab3419cb64660d6c68351b3cff9fb0/aodpat_1_1_X2h.root'))
-process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
-process.load('FWCore.MessageLogger.MessageLogger_cfi')
-process.MessageLogger.cerr.FwkReport.reportEvery = 100000
+sample_files(process, 'mfv_neu_tau01000um_M0800', 'ntuplev16_wgenv2', 1)
+report_every(process, 1)
 
 process.load('JMTucker.Tools.ParticleListDrawer_cff')
 process.load('JMTucker.MFVNeutralino.GenParticles_cff')
 
-#process.mfvGenParticles.print_info = True
-process.mfvGenVertices.print_info = True
+process.mfvGenParticles.debug = True
+#process.mfvGenParticles.last_flag_check = False
 
-process.ParticleListDrawer2 = process.ParticleListDrawer.clone(src = cms.InputTag('mfvGenParticles', 'All'))
-process.ParticleListDrawer3 = process.ParticleListDrawer.clone(src = cms.InputTag('mfvGenParticles', 'Visible'))
+process.primaries   = process.ParticleListDrawer.clone(src = cms.InputTag('mfvGenParticles', 'primaries'))
+process.secondaries = process.ParticleListDrawer.clone(src = cms.InputTag('mfvGenParticles', 'secondaries'))
+process.visible     = process.ParticleListDrawer.clone(src = cms.InputTag('mfvGenParticles', 'visible'))
 
-process.p = cms.Path(process.ParticleListDrawer * process.mfvGenParticles * process.mfvGenVertices)
-#process.p = cms.Path(process.mfvGenParticles * process.ParticleListDrawer2 * process.ParticleListDrawer3)
-
-if 'out' in sys.argv:
-    process.out = cms.OutputModule('PoolOutputModule',
-                                   fileName = cms.untracked.string('gen_particles.root'),
-                                   outputCommands = cms.untracked.vstring('drop *', 'keep recoGenParticles_*_*_MFVGenParticles'),
-                                   )
-    process.outp = cms.EndPath(process.out)
-
+process.p = cms.Path(process.ParticleListDrawer * process.mfvGenParticles * process.primaries * process.secondaries * process.visible)
