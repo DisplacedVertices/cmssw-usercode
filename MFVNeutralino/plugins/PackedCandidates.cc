70b1fbd845963a0d4d8622e3128ad155e2c4d1fd
Jordan Tucker <jordantucker@gmail.com>
2018-01-04 11:20:18 -0600

    look closer at why matches fail with some PF details; restore closest_cd_dist plot pre-cut; drop not-needed TrackerSpaceExtents

--- MFVNeutralino/plugins/PackedCandidates.cc
+++ MFVNeutralino/plugins/PackedCandidates.cc
@@ -8,23 +8,26 @@
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
+#include "RecoParticleFlow/PFTracking/interface/PFTrackAlgoTools.h"
 #include "JMTucker/MFVNeutralinoFormats/interface/Event.h"
 #include "JMTucker/MFVNeutralinoFormats/interface/VertexAux.h"
-#include "JMTucker/Tools/interface/TrackerSpaceExtent.h"
 
 class MFVPackedCandidates : public edm::EDAnalyzer {
 public:
   explicit MFVPackedCandidates(const edm::ParameterSet&);
   void analyze(const edm::Event&, const edm::EventSetup&);
 private:
+  bool goodPtResolution(const reco::Track&) const;
+
   const edm::EDGetTokenT<reco::BeamSpot> beamspot_token;
   const edm::EDGetTokenT<reco::VertexCollection> primary_vertices_token;
   const edm::EDGetTokenT<reco::TrackCollection> tracks_token;
   const edm::EDGetTokenT<pat::PackedCandidateCollection> packed_candidate_token;
-
-  TrackerSpaceExtents tracker_extents;
+  const double max_closest_cd_dist;
+  const bool prints;
 
   TH2F* h_nmatch_v_nseed;
+  TH2F* h_nmatchpass_v_nseed;
   TH1F* h_matchdist;
   TH1F* h_fracdelta_par[5];
   TH1F* h_fracdelta_cov[5][5]; // only the upper 5*4/2 = 10 are filled
@@ -37,15 +40,106 @@ private:
   TH1F* h_nomatch_pxl;
   TH1F* h_nomatch_stl;
   TH1F* h_nomatch_minr;
+  TH1F* h_nomatch_highpurity;
+  TH1F* h_nomatch_losthits;
+  TH1F* h_nomatch_lostlayers;
+  TH1F* h_nomatch_p;
+  TH1F* h_nomatch_dptopt;
+  TH1F* h_nomatch_goodptrel;
+
+  struct track_ex {
+    const reco::BeamSpot& beamspot;
+    const reco::Track& track;
+    int npxhits;
+    int nsthits;
+    int npxlayers;
+    int nstlayers;
+    double aeta;
+    double dxybs;
+    double sigmadxybs;
+    bool pass;
+
+    track_ex(const reco::BeamSpot& bs, const reco::Track& tk)
+      : beamspot(bs), track(tk)
+    {
+      npxhits = tk.hitPattern().numberOfValidPixelHits();
+      nsthits = tk.hitPattern().numberOfValidStripHits();
+      npxlayers = tk.hitPattern().pixelLayersWithMeasurement();
+      nstlayers = tk.hitPattern().stripLayersWithMeasurement();
+      aeta = fabs(tk.eta());
+      dxybs = tk.dxy(beamspot);
+      sigmadxybs = dxybs / tk.dxyError();
+      pass =
+        tk.pt() > 1 &&
+        tk.hitPattern().hasValidHitInFirstPixelBarrel() &&
+        npxlayers >= 2 &&
+        ((aeta < 2 && nstlayers >= 6) || (aeta >= 2 && nstlayers >= 7)) &&
+        fabs(sigmadxybs) > 4;
+    }
+  };
 };
+
+// from RecoParticleFlow/PFProducer/plugins/importers/GeneralTracksImporter.cc
+bool MFVPackedCandidates::goodPtResolution(const reco::Track& tk) const {
+  //recheck that the track is high purity!
+  if (!tk.quality(reco::TrackBase::highPurity))
+    return false;
+ 
+  const std::vector<double> _DPtovPtCut = {10.0, 10.0, 10.0, 10.0, 10.0, 5.0};
+  const std::vector<unsigned> _NHitCut = { 3, 3, 3, 3, 3, 3 };
+  const bool _useIterTracking = true;
+
+
+  const bool _debug = false;
+
+  const double P = tk.p();
+  const double Pt = tk.pt();
+  const double DPt = tk.ptError();
+  const unsigned int NHit = 
+    tk.hitPattern().trackerLayersWithMeasurement();
+  const unsigned int NLostHit = 
+    tk.hitPattern().trackerLayersWithoutMeasurement(reco::HitPattern::TRACK_HITS);
+  const unsigned int LostHits = tk.numberOfLostHits();
+  const double sigmaHad = sqrt(1.20*1.20/P+0.06*0.06) / (1.+LostHits);
+
+  // Protection against 0 momentum tracks
+  if ( P < 0.05 ) return false;
+ 
+  if (_debug) std::cout << " PFBlockAlgo: PFrecTrack->Track Pt= "
+		   << Pt << " DPt = " << DPt << std::endl;
+
+
+  double dptCut = PFTrackAlgoTools::dPtCut(tk.algo(),_DPtovPtCut,_useIterTracking);
+  unsigned int nhitCut    = PFTrackAlgoTools::nHitCut(tk.algo(),_NHitCut,_useIterTracking);
+
+  if ( ( dptCut > 0. && 
+	 DPt/Pt > dptCut*sigmaHad ) || 
+       NHit < nhitCut ) { 
+    if (_debug) std::cout << " PFBlockAlgo: skip badly measured track"
+		     << ", P = " << P 
+		     << ", Pt = " << Pt 
+		     << " DPt = " << DPt 
+		     << ", N(hits) = " << NHit << " (Lost : " << LostHits << "/" << NLostHit << ")"
+			  << ", Algo = " << tk.algo()
+		     << std::endl;
+    if (_debug) std::cout << " cut is DPt/Pt < " << dptCut * sigmaHad << std::endl;
+    if (_debug) std::cout << " cut is NHit >= " << nhitCut << std::endl;
+    return false;
+  }
+
+  return true;
+}
 
 MFVPackedCandidates::MFVPackedCandidates(const edm::ParameterSet& cfg)
   : beamspot_token(consumes<reco::BeamSpot>(edm::InputTag("offlineBeamSpot"))),
     tracks_token(consumes<reco::TrackCollection>(edm::InputTag("generalTracks"))),
-    packed_candidate_token(consumes<pat::PackedCandidateCollection>(edm::InputTag("packedPFCandidates")))
+    packed_candidate_token(consumes<pat::PackedCandidateCollection>(edm::InputTag("packedPFCandidates"))),
+    max_closest_cd_dist(cfg.getParameter<bool>("max_closest_cd_dist")),
+    prints(cfg.getParameter<bool>("prints"))
 {
   edm::Service<TFileService> fs;
-  h_nmatch_v_nseed = fs->make<TH2F>("h_nmatch_v_nseed", "", 200, 0, 200, 200, 0, 200);
+  h_nmatch_v_nseed = fs->make<TH2F>("h_nmatch_v_nseed", "", 50, 0, 50, 50, 0, 50);
+  h_nmatchpass_v_nseed = fs->make<TH2F>("h_nmatchpass_v_nseed", "", 50, 0, 50, 50, 0, 50);
   h_matchdist = fs->make<TH1F>("h_matchdist", "", 1000, 0, 0.01);
   h_nomatch_par[0] = fs->make<TH1F>("h_nomatch_par_0", "", 100, 0, 1000);
   h_nomatch_par[1] = fs->make<TH1F>("h_nomatch_par_1", "", 100, -3, 3);
@@ -55,7 +149,12 @@ MFVPackedCandidates::MFVPackedCandidates(const edm::ParameterSet& cfg)
   h_nomatch_sigmadxybs = fs->make<TH1F>("h_nomatch_sigmadxybs", "", 10000, -1, 1);
   h_nomatch_pxl = fs->make<TH1F>("h_nomatch_pxl", "", 10, 0, 10);
   h_nomatch_stl = fs->make<TH1F>("h_nomatch_stl", "", 20, 0, 20);
-  h_nomatch_minr = fs->make<TH1F>("h_nomatch_minr", "", 10, 0, 10);
+  h_nomatch_highpurity = fs->make<TH1F>("h_nomatch_highpurity", "", 2, 0, 2);
+  h_nomatch_losthits = fs->make<TH1F>("h_nomatch_losthits", "", 50, 0, 50);
+  h_nomatch_lostlayers = fs->make<TH1F>("h_nomatch_lostlayers", "", 50, 0, 50);
+  h_nomatch_p = fs->make<TH1F>("h_nomatch_p", "", 20, 0, 1);
+  h_nomatch_dptopt = fs->make<TH1F>("h_nomatch_dptopt", "", 100, 0, 5);
+  h_nomatch_goodptrel = fs->make<TH1F>("h_nomatch_goodptrel", "", 2, 0, 2);
   for (int i = 0; i < 5; ++i) {
     h_fracdelta_par[i] = fs->make<TH1F>(TString::Format("h_fracdelta_par_%i", i), "", 10000, -1, 1);
     for (int j = i; j < 5; ++j)
@@ -64,21 +163,12 @@ MFVPackedCandidates::MFVPackedCandidates(const edm::ParameterSet& cfg)
   h_fracdelta_sigmadxybs = fs->make<TH1F>("h_fracdelta_sigmadxybs", "", 10000, -1, 1);
   h_delta_pxl = fs->make<TH1F>("h_delta_pxl", "", 100, -50, 50);
   h_delta_stl = fs->make<TH1F>("h_delta_stl", "", 100, -50, 50);
-  h_delta_minr = fs->make<TH1F>("h_delta_minr", "", 100, -50, 50);
 }
 
 void MFVPackedCandidates::analyze(const edm::Event& event, const edm::EventSetup& setup) {
-  const bool prints = true;
-
   edm::Handle<reco::BeamSpot> beamspot;
   event.getByToken(beamspot_token, beamspot);
-  const double bs_x = beamspot->position().x();
-  const double bs_y = beamspot->position().y();
-  const double bs_z = beamspot->position().z();
   
-  if (!tracker_extents.filled())
-    tracker_extents.fill(setup, GlobalPoint(bs_x, bs_y, bs_z));
-
   //edm::Handle<reco::VertexCollection> primary_vertices;
   //event.getByToken(primary_vertices_token, primary_vertices);
   //const reco::Vertex* primary_vertex = 0;
@@ -93,25 +183,21 @@ void MFVPackedCandidates::analyze(const edm::Event& event, const edm::EventSetup
 
   int npass = 0;
   int nmatch = 0;
+  int nmatchpass = 0;
 
   if (prints) printf("generalTracks that pass our cuts (all # = %lu):\n", tracks->size());
   for (size_t itk = 0, itke = tracks->size(); itk < itke; ++itk) {
     const reco::Track& tk = (*tracks)[itk];
-    const NumExtents ne = tracker_extents.numExtentInRAndZ(tk.hitPattern(), TrackerSpaceExtents::AllowAll);
-    const double dxybs = tk.dxy(*beamspot);
-    const double sigmadxybs = dxybs / tk.dxyError();
-    const bool pass = tk.pt() > 1 && ne.min_r <= 1 && tk.hitPattern().pixelLayersWithMeasurement() >= 2 && tk.hitPattern().stripLayersWithMeasurement() >= 3 && fabs(sigmadxybs) > 4;
+    const track_ex te(*beamspot, tk);
 
-    if (pass) {
+    if (te.pass) {
       ++npass;
 
       if (prints) {
         printf("tk #%4lu: pt %10.4g +- %10.4g eta %10.4g +- %10.4g phi %10.4g +- %10.4g dxy %10.4g +- %10.4g dz %10.4g +- %10.4g\n",
                itk, tk.pt(), tk.ptError(), tk.eta(), tk.etaError(), tk.phi(), tk.phiError(), tk.dxy(), tk.dxyError(), tk.dz(), tk.dzError());
-        printf("  # px hits: %2i layers: %2i st hits: %2i layers: %2i   min_r: %i  dxybs: %10.4g  sigmadxybs: %10.4g\n",
-               tk.hitPattern().numberOfValidPixelHits(), tk.hitPattern().pixelLayersWithMeasurement(),
-               tk.hitPattern().numberOfValidStripHits(), tk.hitPattern().stripLayersWithMeasurement(),
-               ne.min_r, dxybs, sigmadxybs);
+        printf("  # px hits: %2i layers: %2i st hits: %2i layers: %2i   dxybs: %10.4g  sigmadxybs: %10.4g\n",
+               te.npxhits, te.npxlayers, te.nsthits, te.nstlayers, te.dxybs, te.sigmadxybs);
       }
 
       const pat::PackedCandidate* closest_cd = 0;
@@ -121,7 +207,7 @@ void MFVPackedCandidates::analyze(const edm::Event& event, const edm::EventSetup
         if (cd.charge()) {
           const reco::Track& cd_tk = cd.pseudoTrack();
           const double dist = reco::deltaR(tk, cd_tk);
-          if (dist < 0.111e-3 && dist < closest_cd_dist) {
+          if (dist < closest_cd_dist) {
             closest_cd = &cd;
             closest_cd_dist = dist;
           }
@@ -129,14 +215,16 @@ void MFVPackedCandidates::analyze(const edm::Event& event, const edm::EventSetup
       }
 
       h_matchdist->Fill(closest_cd_dist);
+      if (closest_cd_dist > max_closest_cd_dist)
+        closest_cd = 0;
 
       if (closest_cd) {
         ++nmatch;
         const reco::Track& cd_tk = closest_cd->pseudoTrack();
-        const NumExtents cd_ne = tracker_extents.numExtentInRAndZ(cd_tk.hitPattern(), TrackerSpaceExtents::AllowAll);
-        const double cd_dxybs = cd_tk.dxy(*beamspot);
-        const double cd_sigmadxybs = cd_dxybs / cd_tk.dxyError();
-        const bool cd_passes = cd_tk.pt() > 1 && cd_ne.min_r <= 1 && cd_tk.hitPattern().pixelLayersWithMeasurement() >= 2 && cd_tk.hitPattern().stripLayersWithMeasurement() >= 3 && fabs(cd_sigmadxybs) > 4;
+        const track_ex cd_te(*beamspot, cd_tk);
+
+        if (cd_te.pass)
+          ++nmatchpass;
 
         h_fracdelta_par[0]->Fill(cd_tk.pt()  / tk.pt()  - 1);
         h_fracdelta_par[1]->Fill(cd_tk.eta() / tk.eta() - 1);
@@ -147,38 +235,45 @@ void MFVPackedCandidates::analyze(const edm::Event& event, const edm::EventSetup
           for (int j = i; j < 5; ++j)
             if (fabs(tk.covariance(i,j)) > 0)
               h_fracdelta_cov[i][j]->Fill(cd_tk.covariance(i,j) / tk.covariance(i,j) - 1);
-        h_fracdelta_sigmadxybs->Fill(cd_sigmadxybs / sigmadxybs - 1);
-        h_delta_pxl->Fill(cd_tk.hitPattern().pixelLayersWithMeasurement() - tk.hitPattern().pixelLayersWithMeasurement());
-        h_delta_stl->Fill(cd_tk.hitPattern().stripLayersWithMeasurement() - tk.hitPattern().stripLayersWithMeasurement());
-        h_delta_minr->Fill(cd_ne.min_r - ne.min_r);
+        h_fracdelta_sigmadxybs->Fill(cd_te.sigmadxybs / te.sigmadxybs - 1);
+        h_delta_pxl->Fill(cd_te.npxlayers - te.npxlayers);
+        h_delta_stl->Fill(cd_te.nstlayers - te.nstlayers);
         if (prints) {
           printf("  the closest CD with dist %f:\n", closest_cd_dist);
           printf("    pt %10.4g +- %10.4g eta %10.4g +- %10.4g phi %10.4g +- %10.4g dxy %10.4g +- %10.4g dz %10.4g +- %10.4g\n",
                  cd_tk.pt(), cd_tk.ptError(), cd_tk.eta(), cd_tk.etaError(), cd_tk.phi(), cd_tk.phiError(), cd_tk.dxy(), cd_tk.dxyError(), cd_tk.dz(), cd_tk.dzError());
-          printf("    # px hits: %2i layers: %2i st hits: %2i layers: %2i   min_r: %i  dxybs: %10.4g  sigmadxybs: %10.4g\n",
-                 cd_tk.hitPattern().numberOfValidPixelHits(), cd_tk.hitPattern().pixelLayersWithMeasurement(),
-                 cd_tk.hitPattern().numberOfValidStripHits(), cd_tk.hitPattern().stripLayersWithMeasurement(),
-                 cd_ne.min_r, cd_dxybs, cd_sigmadxybs);
-          printf("  also passes? %i\n", cd_passes);
+          printf("    # px hits: %2i layers: %2i st hits: %2i layers: %2i   dxybs: %10.4g  sigmadxybs: %10.4g\n",
+                 cd_te.npxhits, cd_te.npxlayers, cd_te.nsthits, cd_te.nstlayers, cd_te.dxybs, cd_te.sigmadxybs);
+          printf("  also passes? %i\n", cd_te.pass);
         }
       }
       else {
-        if (prints) printf("  NO CD MATCH\n");
         h_nomatch_par[0]->Fill(tk.pt() );
         h_nomatch_par[1]->Fill(tk.eta());
         h_nomatch_par[2]->Fill(tk.phi());
         h_nomatch_par[3]->Fill(tk.dxy());
         h_nomatch_par[4]->Fill(tk.dz() );
-        h_nomatch_sigmadxybs->Fill(sigmadxybs);
-        h_nomatch_pxl->Fill(tk.hitPattern().pixelLayersWithMeasurement());
-        h_nomatch_stl->Fill(tk.hitPattern().stripLayersWithMeasurement());
-        h_nomatch_minr->Fill(ne.min_r);
+        h_nomatch_sigmadxybs->Fill(te.sigmadxybs);
+        h_nomatch_pxl->Fill(te.npxlayers);
+        h_nomatch_stl->Fill(te.nstlayers);
+        const bool highpurity = tk.quality(reco::TrackBase::highPurity);
+        const bool goodptrel = goodPtResolution(tk);
+        h_nomatch_highpurity->Fill(highpurity);
+        if (prints) printf("  NO CD MATCH highpurity %i goodptrel %i\n", highpurity, goodptrel);
+        if (highpurity) {
+          h_nomatch_losthits->Fill(tk.numberOfLostHits());
+          h_nomatch_lostlayers->Fill(tk.hitPattern().trackerLayersWithoutMeasurement(reco::HitPattern::TRACK_HITS));
+          h_nomatch_p->Fill(tk.p());
+          h_nomatch_dptopt->Fill(tk.pt() > 0 ? tk.ptError()/tk.pt() : 999);
+          h_nomatch_goodptrel->Fill(goodptrel);
+        }
       }
     }
   }
 
   h_nmatch_v_nseed->Fill(npass, nmatch);
-  if (prints) printf("# general passing: %i  # with match: %i\n", npass, nmatch);
+  h_nmatchpass_v_nseed->Fill(npass, nmatchpass);
+  if (prints) printf("# general passing: %i  # with match: %i that pass: %i\n", npass, nmatch, nmatchpass);
 }
 
 DEFINE_FWK_MODULE(MFVPackedCandidates);
