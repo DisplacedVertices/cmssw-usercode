a18205c019dbac0c75fe3c222fd6f410364e8a32
Jordan Tucker <jordantucker@gmail.com>
2018-01-08 18:21:25 -0600

    canon and storeonly are useful externally

--- Tools/python/eos.py
+++ Tools/python/eos.py
@@ -15,14 +15,14 @@ def _system(cmd):
     #print cmd
     return subprocess.call(cmd, stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT) == 0
 
-def _storeonly(fn):
+def storeonly(fn):
     if fn.startswith('/store'):
         return fn
     if '/store/' not in fn:
         raise ValueError('fn passed %r does not contain "/store/"' % fn)
     return '/store/' + fn.split('/store/')[1]
     
-def _canon(fn):
+def canon(fn):
     if fn.startswith('root:'):
         return fn
     elif fn.startswith('/store'):
@@ -38,18 +38,18 @@ def quota():
     return "eos quota command didn't work or didn't find user %s" % user
 
 def exists(fn):
-    fn = _storeonly(fn)
+    fn = storeonly(fn)
     return _system('eos %s ls %s' % (url, fn))
 
 def mkdir(fn):
-    fn = _storeonly(fn)
+    fn = storeonly(fn)
     dn = os.path.dirname(fn)
     if exists(dn):
         return True
     return _system('eos %s mkdir -p %s' % (url, dn))
 
 def ls(path):
-    fn = _storeonly(path)
+    fn = storeonly(path)
     fns = _popen(str('eos %s ls %s' % (url, path))).communicate()[0].strip().split()
     return [os.path.join(path, fn) for fn in fns]
 
@@ -58,18 +58,18 @@ def glob(path, pattern):
     return [fn for fn in fns if fnmatch.fnmatch(os.path.basename(fn), pattern)]
 
 def cp(src, dst):
-    src = _canon(src)
-    dst = _canon(dst)
+    src = canon(src)
+    dst = canon(dst)
     if not src.startswith(url) and not dst.startswith(url):
         raise ValueError('refusing to use xrdcp for non eos copy: %r -> %r' % (src, dst))
     return _system('xrdcp -s %s %s' % (src, dst))
 
 def rm(fn):
-    fn = _storeonly(fn)
+    fn = storeonly(fn)
     return _system('eos %s rm %s' % (url, fn))
 
 def md5sum(fn):
-    fn = _storeonly(fn)
+    fn = storeonly(fn)
     cmd = 'xrdcp -s %s%s -' % (url, fn)
     p  = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     p2 = subprocess.Popen(('md5sum',), stdin=p.stdout, stdout=subprocess.PIPE)
