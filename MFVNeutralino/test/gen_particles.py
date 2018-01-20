b5f16f037c363289da36bc8a3523a051fd87d0a6
Jordan Tucker <jordantucker@gmail.com>
2018-01-20 15:31:05 -0600

    use max_events, file_event_from_argv

--- MFVNeutralino/test/gen_particles.py
+++ MFVNeutralino/test/gen_particles.py
@@ -1,7 +1,9 @@
 from JMTucker.Tools.BasicAnalyzer_cfg import *
 
-sample_files(process, 'mfv_neu_tau01000um_M0800', 'ntuplev16_wgenv2', 1)
+sample_files(process, 'mfv_ddbar_tau01000um_M1600', 'main', 1)
 report_every(process, 1)
+max_events(process, 10)
+file_event_from_argv(process)
 
 process.load('JMTucker.Tools.ParticleListDrawer_cff')
 process.load('JMTucker.MFVNeutralino.GenParticles_cff')
