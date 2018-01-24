f51574ecf38f5c8f47b033f1801fc335854ca217
Jordan Tucker <jordantucker@gmail.com>
2018-01-24 12:34:51 -0600

    v2

--- MFVNeutralino/test/PackedCands/packedcands.py
+++ MFVNeutralino/test/PackedCands/packedcands.py
@@ -39,7 +39,7 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         sample.events_per = 3000
 
     from JMTucker.Tools.MetaSubmitter import *
-    ms = MetaSubmitter('PackedCandsV1')
+    ms = MetaSubmitter('PackedCandsV2')
     ms.common.ex = year
     ms.common.dataset = 'miniaod'
     ms.common.pset_modifier = chain_modifiers(is_mc_modifier, H_modifier, repro_modifier, secondary_files_modifier('main'))
