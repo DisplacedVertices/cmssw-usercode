2298ce2cb1e37ccce78798c54b5a92bc2b4fbc5a
Jordan Tucker <jordantucker@gmail.com>
2018-01-24 13:10:55 -0600

    v2; names on statboxes; fewer plots instead of fixing bug that drops the first statbox in subsequent batches

--- MFVNeutralino/test/PackedCands/draw.py
+++ MFVNeutralino/test/PackedCands/draw.py
@@ -7,14 +7,14 @@ ROOT.TH1.AddDirectory(0)
 
 ps = plot_saver(plot_dir('packedcands'), size=(600,600), log=False)
 
-ff = ROOT.TFile('/uscms_data/d2/tucker/crab_dirs/PackedCandsV0/mfv_neu_tau10000um_M1600.root')
+ff = ROOT.TFile('/uscms_data/d2/tucker/crab_dirs/PackedCandsV2/mfv_neu_tau10000um_M1600.root')
 
 def get_em(path):
     f = ff.Get(path)
-    def skip(name, obj):
+    def skip(name, h):
         return False
-    def rebin(name, obj):
-        return obj
+    def rebin(name, h):
+        return h
 
     l = []
     for key in f.GetListOfKeys():
@@ -26,25 +26,27 @@ def get_em(path):
         l.append(obj)
     return l
 
-all_tracks = get_em('mfvPackedCands/all')
-highpurity = get_em('mfvPackedCands/highpurity')
+#all_tracks = get_em('mfvPackedCands/all')
+#highpurity = get_em('mfvPackedCands/highpurity')
 seed = get_em('mfvPackedCands/seed')
-match = get_em('mfvPackedCands/match')
+#match = get_em('mfvPackedCands/match')
 match_pass = get_em('mfvPackedCands/match_pass')
-nomatch = get_em('mfvPackedCands/nomatch')
-nomatch_highpurity = get_em('mfvPackedCands/nomatch_highpurity')
-nomatch_highpurity_goodptres = get_em('mfvPackedCands/nomatch_highpurity')
+#nomatch = get_em('mfvPackedCands/nomatch')
+#nomatch_highpurity = get_em('mfvPackedCands/nomatch_highpurity')
+#nomatch_highpurity_goodptres = get_em('mfvPackedCands/nomatch_highpurity')
 
 zz = [
-    ('all+highpurity+seed', (all_tracks, highpurity, seed)),
-    ('seed+match+match-pass', (seed, match, match_pass)),
-    ('seed+nomatch+nomatch-highpurity+nomatch-highpurity-goodptres', (seed, nomatch, nomatch_highpurity, nomatch_highpurity_goodptres)),
+    ('seed+match-pass', (seed, match_pass)),
+#    ('all+highpurity+seed', (all_tracks, highpurity, seed)),
+#    ('seed+match+match-pass', (seed, match, match_pass)),
+#    ('seed+nomatch+nomatch-highpurity+nomatch-highpurity-goodptres', (seed, nomatch, nomatch_highpurity, nomatch_highpurity_goodptres)),
     ]
 
 colors = [ROOT.kRed, ROOT.kGreen+2, ROOT.kBlue, 9]
 
 for namebase, z in zz:
     assert len(set(len(y) for y in z)) == 1
+    names = namebase.split('+')
     for hists in zip(*z):
         name = None
         for i,h in enumerate(hists):
@@ -52,6 +54,7 @@ for namebase, z in zz:
                 name = h.GetName()
             else:
                 assert name == h.GetName()
+            h.SetName(names[i])
             h.SetLineColor(colors[i])
             h.SetLineWidth(2)
         ratios_plot(namebase + '_' + name, 
