50f2f36c4b4b9f8f1a295e3fcbbcf1229873783c
Jordan Tucker <jordantucker@gmail.com>
2018-02-04 10:23:53 -0600

    improve submit script

--- Tools/test/EventIds/event_ids.py
+++ Tools/test/EventIds/event_ids.py
@@ -14,13 +14,15 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     import JMTucker.Tools.Samples as Samples
     samples = Samples.data_samples + Samples.qcd_samples + Samples.qcd_samples_ext + Samples.ttbar_samples + Samples.mfv_signal_samples
 
-    samples = [s for s in samples if s.has_dataset('ntuplev16m')]
+    samples = Samples.mfv_neuude_samples
+
+    dataset = 'ntuplev16_wgenv2'
+    samples = [s for s in samples if s.has_dataset(dataset)]
 
     for sample in samples:
-        sample.files_per = 20
-        #if not sample.is_mc:
-        #    sample.json = 'jsons/ana_2015p6.json'
+        sample.datasets[dataset].split_by = 'files'
+        sample.datasets[dataset].files_per = 1
 
     from JMTucker.Tools.CondorSubmitter import CondorSubmitter
-    cs = CondorSubmitter('EventIdsNtupleV16m', dataset='ntuplev16m')
+    cs = CondorSubmitter('EventIds', dataset=dataset)
     cs.submit_all(samples)
