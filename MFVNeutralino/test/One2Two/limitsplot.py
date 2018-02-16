35365dd8c03b161ac9b5bb749a9bb7126b9f28c7
Jordan Tucker <jordantucker@gmail.com>
2018-02-16 17:06:15 -0600

    save_1d_plots: tau <= 100 mm

--- MFVNeutralino/test/One2Two/limitsplot.py
+++ MFVNeutralino/test/One2Two/limitsplot.py
@@ -180,15 +180,15 @@ def make_1d_plot(d, name, xkey='mass'):
 
 def save_1d_plots():
     xxx = [
-        ('multijet_M0800',   lambda s: 'neu'   in sample.name and sample.mass == 800 and sample.tau <= 1000., lambda s: s.sample.tau,  ('tau',  800.)),
-        ('multijet_M1600',   lambda s: 'neu'   in sample.name and sample.mass ==1600 and sample.tau <= 1000., lambda s: s.sample.tau,  ('tau', 1600.)),
-        ('multijet_M2400',   lambda s: 'neu'   in sample.name and sample.mass ==2400 and sample.tau <= 1000., lambda s: s.sample.tau,  ('tau', 2400.)),
+        ('multijet_M0800',   lambda s: 'neu'   in sample.name and sample.mass == 800 and sample.tau <= 100., lambda s: s.sample.tau,  ('tau',  800.)),
+        ('multijet_M1600',   lambda s: 'neu'   in sample.name and sample.mass ==1600 and sample.tau <= 100., lambda s: s.sample.tau,  ('tau', 1600.)),
+        ('multijet_M2400',   lambda s: 'neu'   in sample.name and sample.mass ==2400 and sample.tau <= 100., lambda s: s.sample.tau,  ('tau', 2400.)),
         ('multijet_tau300um',lambda s: 'neu'   in sample.name and sample.tau  == 0.3 and sample.mass <= 2600, lambda s: s.sample.mass, 'mass'),
         ('multijet_tau1mm',  lambda s: 'neu'   in sample.name and sample.tau  == 1.  and sample.mass <= 2600, lambda s: s.sample.mass, 'mass'),
         ('multijet_tau10mm', lambda s: 'neu'   in sample.name and sample.tau  == 10. and sample.mass <= 2600, lambda s: s.sample.mass, 'mass'),
-        ('ddbar_M0800',      lambda s: 'ddbar' in sample.name and sample.mass == 800 and sample.tau <= 1000., lambda s: s.sample.tau,  ('tau',  800.)),
-        ('ddbar_M1600',      lambda s: 'ddbar' in sample.name and sample.mass ==1600 and sample.tau <= 1000., lambda s: s.sample.tau,  ('tau', 1600.)),
-        ('ddbar_M2400',      lambda s: 'ddbar' in sample.name and sample.mass ==2400 and sample.tau <= 1000., lambda s: s.sample.tau,  ('tau', 2400.)),
+        ('ddbar_M0800',      lambda s: 'ddbar' in sample.name and sample.mass == 800 and sample.tau <= 100., lambda s: s.sample.tau,  ('tau',  800.)),
+        ('ddbar_M1600',      lambda s: 'ddbar' in sample.name and sample.mass ==1600 and sample.tau <= 100., lambda s: s.sample.tau,  ('tau', 1600.)),
+        ('ddbar_M2400',      lambda s: 'ddbar' in sample.name and sample.mass ==2400 and sample.tau <= 100., lambda s: s.sample.tau,  ('tau', 2400.)),
         ('ddbar_tau300um',   lambda s: 'ddbar' in sample.name and sample.tau  == 0.3 and sample.mass <= 2600, lambda s: s.sample.mass, 'mass'),
         ('ddbar_tau1mm',     lambda s: 'ddbar' in sample.name and sample.tau  == 1.  and sample.mass <= 2600, lambda s: s.sample.mass, 'mass'),
         ('ddbar_tau10mm',    lambda s: 'ddbar' in sample.name and sample.tau  == 10. and sample.mass <= 2600, lambda s: s.sample.mass, 'mass'),
