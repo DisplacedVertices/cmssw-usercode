bdc85cc9ec7fc76d17eca4b473e828c6e9cca6f5
Jordan Tucker <jordantucker@gmail.com>
2018-02-22 08:48:02 -0600

    add h_nmatch_dvvzero, h_samematch, h_samematch_dvvzero, h_dvvzero, h_dvvzero_samematch, h_gendvv_dvvzero

--- MFVNeutralino/plugins/SignalMatch.cc
+++ MFVNeutralino/plugins/SignalMatch.cc
@@ -25,6 +25,9 @@ class MFVSignalMatch : public edm::EDAnalyzer {
   TH1D* h_nmatch_nge2;
   TH1D* h_nmatch_neq2;
   TH1D* h_nmatch_ngt2;
+  TH1D* h_nmatch_dvvzero;
+  TH1D* h_samematch;
+  TH1D* h_samematch_dvvzero;
 
   TH1D* h_gendvv_all;
   TH1D* h_dvv;
@@ -33,6 +36,9 @@ class MFVSignalMatch : public edm::EDAnalyzer {
   TH1D* h_gendvv_nmatch[4];
   TH1D* h_dvv_samematch;
   TH1D* h_gendvv_samematch;
+  TH1D* h_dvvzero;
+  TH1D* h_dvvzero_samematch;
+  TH1D* h_gendvv_dvvzero;
 
   TH1D* h_genrho;
   TH1D* h_genrho_notmatched;
@@ -54,6 +60,9 @@ MFVSignalMatch::MFVSignalMatch(const edm::ParameterSet& cfg)
   h_nmatch_nge2 = fs->make<TH1D>("h_nmatch_nge2", "events with at least two selected vertices;# of selected vertices matching a decay vertex;events", 10, 0, 10);
   h_nmatch_neq2 = fs->make<TH1D>("h_nmatch_neq2", "events with exactly two selected vertices;# of selected vertices matching a decay vertex;events", 10, 0, 10);
   h_nmatch_ngt2 = fs->make<TH1D>("h_nmatch_ngt2", "events with more than two selected vertices;# of selected vertices matching a decay vertex;events", 10, 0, 10);
+  h_nmatch_dvvzero = fs->make<TH1D>("h_nmatch_dvvzero", "d_{VV} ~ 0;# of selected vertices matching a decay vertex;events", 10, 0, 10);
+  h_samematch = fs->make<TH1D>("h_samematch", "# of selected vertices at least 2;same decay vertex matched?;events", 2, 0, 2);
+  h_samematch_dvvzero = fs->make<TH1D>("h_samematch_dvvzero", "# of selected vertices at least 2 and d_{VV} ~ 0;same decay vertex matched?;events", 2, 0, 2);
   h_gendvv_all = fs->make<TH1D>("h_gendvv_all", "all not-skipped events;generated d_{VV} (cm);events/50 #mum", 800, 0, 4);
   h_dvv = fs->make<TH1D>("h_dvv", ";d_{VV} (cm);events/50 #mum", 800, 0, 4);
   h_gendvv = fs->make<TH1D>("h_gendvv", "events with at least two vertices;generated d_{VV} (cm);events/50 #mum", 800, 0, 4);
@@ -63,6 +72,9 @@ MFVSignalMatch::MFVSignalMatch(const edm::ParameterSet& cfg)
   }
   h_dvv_samematch = fs->make<TH1D>("h_dvv_samematch", "both vertices match the same decay vertex;d_{VV} (cm);events/50 #mum", 800, 0, 4);
   h_gendvv_samematch = fs->make<TH1D>("h_gendvv_samematch", "both vertices match the same decay vertex;generated d_{VV} (cm);events/50 #mum", 800, 0, 4);
+  h_dvvzero = fs->make<TH1D>("h_dvvzero", ";d_{VV} ~ 0?;events", 2, 0, 2);
+  h_dvvzero_samematch = fs->make<TH1D>("h_dvvzero_samematch", "both vertices match the same decay vertex;d_{VV} ~ 0?;events", 2, 0, 2);
+  h_gendvv_dvvzero = fs->make<TH1D>("h_gendvv_dvvzero", "d_{VV} ~ 0;generated d_{VV} (cm);events/50 #mum", 800, 0, 4);
   h_genrho = fs->make<TH1D>("h_genrho", ";generated #rho (cm);events/50 #mum", 2000, 0, 10);
   h_genrho_notmatched = fs->make<TH1D>("h_genrho_notmatched", "decay vertex not matched;generated #rho (cm) ;events/50 #mum", 2000, 0, 10);
   h_genrho_notmatched_nge2 = fs->make<TH1D>("h_genrho_notmatched_nge2", "decay vertex not matched and at least two selected vertices;generated #rho (cm) ;events/50 #mum", 2000, 0, 10);
@@ -132,33 +144,48 @@ void MFVSignalMatch::analyze(const edm::Event& event, const edm::EventSetup&) {
   if (nvtx == 2) h_nmatch_neq2->Fill(nmatch);
   if (nvtx >  2) h_nmatch_ngt2->Fill(nmatch);
 
+  const double genrho[2] = {
+    mag(mevent->gen_lsp_decay[0*3+0] - mevent->gen_pv[0], mevent->gen_lsp_decay[0*3+1] - mevent->gen_pv[1]),
+    mag(mevent->gen_lsp_decay[1*3+0] - mevent->gen_pv[0], mevent->gen_lsp_decay[1*3+1] - mevent->gen_pv[1])
+  };
   const double gendvv = mevent->lspdist2d();
+
   h_gendvv_all->Fill(gendvv);
   if (nvtx >= 2) {
     const MFVVertexAux& v0 = (*vertices)[0];
     const MFVVertexAux& v1 = (*vertices)[1];
     const int i0 = match_igen[0];
     const int i1 = match_igen[1];
+    const bool samematch = i0 == i1 && i0 != -1;
     const double dvv = mag(v0.x - v1.x, v0.y - v1.y);
+    const bool dvvzero = dvv < 0.03;
 
     h_dvv->Fill(dvv);
     h_gendvv->Fill(gendvv);
     h_dvv_nmatch[nmatch_max3]->Fill(dvv);
     h_gendvv_nmatch[nmatch_max3]->Fill(gendvv);
-    if (i0 == i1 && i0 != -1) {
+    h_samematch->Fill(samematch);
+    h_dvvzero->Fill(dvvzero);
+
+    if (samematch) {
+      h_dvvzero_samematch->Fill(dvvzero);
       h_dvv_samematch->Fill(dvv);
       h_gendvv_samematch->Fill(gendvv);
+    }
+
+    if (dvvzero) {
+      h_nmatch_dvvzero->Fill(nmatch);
+      h_gendvv_dvvzero->Fill(gendvv);
+      h_samematch_dvvzero->Fill(samematch);
     }
   }
 
   for (int i = 0; i < 2; ++i) {
-    const double genrho = mag(mevent->gen_lsp_decay[i*3+0] - mevent->gen_pv[0],
-                              mevent->gen_lsp_decay[i*3+1] - mevent->gen_pv[1]);
-    h_genrho->Fill(genrho);
+    h_genrho->Fill(genrho[i]);
     if (match_ivtx[i] == -1) {
-      h_genrho_notmatched->Fill(genrho);
+      h_genrho_notmatched->Fill(genrho[i]);
       if (nvtx >= 2)
-        h_genrho_notmatched_nge2->Fill(genrho);
+        h_genrho_notmatched_nge2->Fill(genrho[i]);
     }
   }
 }
