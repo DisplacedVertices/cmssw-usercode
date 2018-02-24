3f0845feef5263ddb9f7b998cecf33dc1dc696dc
Jordan Tucker <jordantucker@gmail.com>
2018-02-23 19:00:40 -0600

    print toys (should unify with vtoy)

---
+++ MFVNeutralino/test/One2Two/combine/printtoys.py
@@ -0,0 +1,21 @@
+import sys
+from JMTucker.Tools.ROOTTools import *
+
+fn = sys.argv[1]
+f = ROOT.TFile(fn)
+
+print fn
+
+limit_t = f.Get('limit')
+limit_by_toy = {}
+for _ in ttree_iterator(limit_t):
+    limit_by_toy[limit_t.iToy] = limit_t.limit
+
+toys = f.Get('toys')
+for ktoy in toys.GetListOfKeys():
+    toy = ktoy.GetName()
+    assert toy.startswith('toy_')
+    itoy = int(toy.replace('toy_',''))
+
+    a,b,c = x = [int(ktoy.ReadObj().get()['n_obs_binb%i'%i].getVal()) for i in 0,1,2]
+    print sum(x), x, limit_by_toy[itoy]
