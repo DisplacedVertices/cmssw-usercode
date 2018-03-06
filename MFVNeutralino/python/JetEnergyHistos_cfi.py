75849a38b63038977f3c7ca8a892bb4ef3e4a2a1
Jordan Tucker <jordantucker@gmail.com>
2018-03-06 14:36:00 -0600

    don't have a cfi when it's simple and only used in one place

--- MFVNeutralino/python/JetEnergyHistos_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-mfvJetEnergyHistos = cms.EDAnalyzer('MFVJetEnergyHistos',
-                                mevent_src = cms.InputTag('mfvEvent'),
-                                weight_src = cms.InputTag('mfvWeight'),
-                                force_bs = cms.vdouble(),
-                                jes = cms.bool(False),
-                                )
