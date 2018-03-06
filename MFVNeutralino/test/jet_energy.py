32cd56aed79734b66d2c308fef0d0fea099562a4
Jordan Tucker <jordantucker@gmail.com>
2018-03-06 14:32:38 -0600

    v16; sample list; fix some syntax errors I apparently introduced

--- MFVNeutralino/test/jet_energy.py
+++ MFVNeutralino/test/jet_energy.py
@@ -3,10 +3,9 @@ from JMTucker.Tools.BasicAnalyzer_cfg import *
 from JMTucker.Tools.CMSSWTools import which_global_tag
 from JMTucker.MFVNeutralino.Year import year
 
-dataset = 'ntuplev15'
+dataset = 'ntuplev16'
 sample_files(process, 'mfv_neu_tau01000um_M0800', dataset, 1)
-#sample_files(process, 'qcdht0700', dataset, 1)
-process.TFileService.fileName = 'jethistos.root'
+process.TFileService.fileName = 'jet_energy.root'
 process.maxEvents.input = -1
 file_event_from_argv(process)
 geometry_etc(process, which_global_tag(True, year, H=False, repro=False))
@@ -33,14 +32,14 @@ def force_bs(process, bs):
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.MFVNeutralino.Year import year
     from JMTucker.Tools import Samples 
-    samples = Samples.mfv_signal_samples + Samples.mfv_ddbar_samples)
+    samples = Samples.mfv_signal_samples + Samples.mfv_stopdbardbar_samples
 
     from JMTucker.Tools.MetaSubmitter import set_splitting
     set_splitting(samples, dataset, 'histos', data_json='jsons/ana_2015p6.json')
 
     from JMTucker.Tools.CondorSubmitter import CondorSubmitter
-    cs = CondorSubmitter('JetEnergyHistosV15',
+    cs = CondorSubmitter('JetEnergyHistosV16',
                          ex = year,
                          dataset = dataset,
                          )
-    cs.submit_all(
+    cs.submit_all(samples)
