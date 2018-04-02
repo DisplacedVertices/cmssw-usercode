9df6642c5bca64b377ed07fe981a04c8291e8f32
Jordan Tucker <jordantucker@gmail.com>
2018-04-01 19:06:31 -0500

    new inputs

--- MFVNeutralino/test/One2Two/limitsinput.py
+++ MFVNeutralino/test/One2Two/limitsinput.py
@@ -19,8 +19,8 @@ bkg_uncert = [(a**2 + b**2)**0.5 for a,b in zip(bkg_uncert, bkg_uncert_stat)] # 
 
 in_fn = '/uscms/home/jchu/public/2v_from_jets_data_2015p6_5track_default_v15_v5.root'
 #in_trees, in_scanpack_list = '/uscms_data/d2/tucker/crab_dirs/MiniTreeV15_v5/mfv*root', None
-in_trees, in_scanpack_list = None, '/uscms/home/tucker/public/mfv/scanpacks/scanpack_merge_1_1p5_2_2p5_2p7.list.gz'
-#in_trees, in_scanpack_list = None, '/uscms/home/tucker/public/mfv/scanpacks/scanpack_merge_hip_1_2_2p6.list.gz'
+in_trees, in_scanpack_list = None, '/uscms/home/tucker/public/mfv/scanpacks/scanpack_merge_1_1p5_2_2p5_2p7_3_3p5_removeddbar.list.gz'
+#in_trees, in_scanpack_list = None, '/uscms/home/tucker/public/mfv/scanpacks/scanpack_merge_hip_1_2_2p6_3_3p6_removeddbar.list.gz'
 
 limitsinput_fn = 'limitsinput.root'
 
