21558162d9edf66e070be7fa0b4eb94e24faa019
Jordan Tucker <jordantucker@gmail.com>
2018-04-11 15:17:55 -0500

    submit script: use all_signal_samples lists

--- MFVNeutralino/test/ntuple.py
+++ MFVNeutralino/test/ntuple.py
@@ -159,12 +159,12 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         samples = \
             Samples.data_samples_2015 + \
             Samples.ttbar_samples_2015 + Samples.qcd_samples_2015 + Samples.qcd_samples_ext_2015 + \
-            Samples.mfv_signal_samples_2015
+            Samples.all_signal_samples_2015
     elif year == 2016:
         samples = \
             Samples.data_samples + \
-            Samples.ttbar_samples + Samples.qcd_samples + Samples.qcd_samples_ext + \
-            Samples.mfv_signal_samples + Samples.mfv_ddbar_samples + Samples.mfv_hip_samples + Samples.qcd_hip_samples
+            Samples.ttbar_samples + Samples.qcd_samples + Samples.qcd_samples_ext + Samples.qcd_hip_samples + \
+            Samples.all_signal_samples
 
     if 'validation' in sys.argv:
         batch_name += '_validation'
