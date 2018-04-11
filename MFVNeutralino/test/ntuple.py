b9409d1de62d19c7e695f61d1ec353f10b0cdbe1
Jordan Tucker <jordantucker@gmail.com>
2018-04-11 15:25:11 -0500

    don't run event filter on signals (toward obviating WGen ntuples)

--- MFVNeutralino/test/ntuple.py
+++ MFVNeutralino/test/ntuple.py
@@ -184,7 +184,14 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         batch_name += '_NTkSeeds'
         samples = [s for s in samples if not s.is_signal]
 
-    modify = chain_modifiers(is_mc_modifier, H_modifier, repro_modifier, event_veto_modifier(skips, 'p'))
+    def signals_no_event_filter_modifier(sample):
+        to_replace = []
+        if sample.is_signal:
+            magic = '\x65vent_filter = not keep_all'
+            to_replace.append((magic, 'event_filter = False', 'tuple template does not contain the magic string "%s"' % magic))
+        return [], to_replace
+
+    modify = chain_modifiers(is_mc_modifier, H_modifier, repro_modifier, event_veto_modifier(skips, 'p'), signals_no_event_filter_modifier)
     ms = MetaSubmitter(batch_name)
     if 'validation' in sys.argv:
         modify.append(max_output_modifier(500))
