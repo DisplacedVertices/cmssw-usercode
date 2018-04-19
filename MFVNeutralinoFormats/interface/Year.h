5645402ee463b5ed4c17fec05a236e46ef39d29b
Jordan Tucker <jordantucker@gmail.com>
2018-04-19 17:15:01 -0500

    2017 is default for v17

--- MFVNeutralinoFormats/interface/Year.h
+++ MFVNeutralinoFormats/interface/Year.h
@@ -2,7 +2,7 @@
 #define JMTucker_MFVNeutralinoFormats_interface_Year_h
 
 //#define MFVNEUTRALINO_2015
-#define MFVNEUTRALINO_2016
-//#define MFVNEUTRALINO_2017
+//#define MFVNEUTRALINO_2016
+#define MFVNEUTRALINO_2017
 
 #endif
