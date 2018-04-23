62b938953c5ef9d6ceb3e0f32849aa628a57d1db
Jordan Tucker <jordantucker@gmail.com>
2018-04-23 17:01:57 -0500

    2017 v17m

--- MFVNeutralino/test/minitree.py
+++ MFVNeutralino/test/minitree.py
@@ -1,12 +1,13 @@
 import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import *
 
-dataset = 'ntuplev16p1'
-sample_files(process, 'qcdht2000', dataset, 1)
+dataset = 'ntuplev17m'
+sample_files(process, 'qcdht2000_2017', dataset, 1)
 process.TFileService.fileName = 'minitree.root'
 file_event_from_argv(process)
 
 process.load('JMTucker.MFVNeutralino.MiniTree_cff')
+
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.Year import year
@@ -19,12 +20,15 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         samples = Samples.data_samples + \
             Samples.ttbar_samples + Samples.qcd_samples + Samples.qcd_samples_ext + Samples.qcd_hip_samples + \
             Samples.all_signal_samples
+    elif year == 2017:
+        samples = \
+            Samples.ttbar_samples_2017 + Samples.qcd_samples_2017
 
     from JMTucker.Tools.MetaSubmitter import set_splitting
     set_splitting(samples, dataset, 'minitree', data_json='jsons/ana_2015p6.json')
 
     from JMTucker.Tools.CondorSubmitter import CondorSubmitter
-    cs = CondorSubmitter('MiniTreeV16p1',
+    cs = CondorSubmitter('MiniTreeV17m',
                          ex = year,
                          dataset = dataset,
                          )
