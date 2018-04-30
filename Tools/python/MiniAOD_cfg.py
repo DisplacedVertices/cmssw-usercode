504f0ab164b6894f58d95eeb81b6084ae4936c31
Jordan Tucker <jordantucker@gmail.com>
2018-04-30 10:45:58 -0500

    drop 2015+6 support and implement 2017 support; use CMSSWSettings object; 2017 needs a lot of crap for the task that I'm not sure why it should be needed

--- Tools/python/MiniAOD_cfg.py
+++ Tools/python/MiniAOD_cfg.py
@@ -1,30 +1,19 @@
 import FWCore.ParameterSet.Config as cms
-from JMTucker.Tools.CMSSWTools import output_file, registration_warnings, report_every, silence_messages, which_global_tag
+from JMTucker.Tools.CMSSWTools import CMSSWSettings, output_file, registration_warnings, report_every, silence_messages, which_global_tag
 
-def pat_tuple_process(customize_before_unscheduled, is_mc, year, H, repro):
-    '''2017 clients need to call associate_paths_to_task'''
+def pat_tuple_process(settings, customize_before_unscheduled=None): #, paths=[]):
+    '''Need to call associate_paths_to_task after defining the paths.'''
 
-    if year not in (2015,2016,2017):
+    if settings.year not in (2017,):
         raise ValueError('what year is it')
 
     from Configuration.StandardSequences.Eras import eras
-    if year == 2015:
-        if is_mc:
-            process = cms.Process('PAT', eras.Run2_25ns)
-        else:
-            process = cms.Process('PAT')
-    elif year == 2016:
-        process = cms.Process('PAT', eras.Run2_25ns if is_mc else eras.Run2_2016)
-    elif year == 2017:
-        assert is_mc
-        process = cms.Process('PAT', eras.Run2_2017)
+    if settings.year == 2017:
+        process = cms.Process('PAT', eras.Run2_2017, eras.run2_miniAOD_94XFall17)
 
     report_every(process, 1000000)
     registration_warnings(process)
-    if year == 2015:
-        silence_messages(process, ['HLTConfigData'])
-    elif year == 2016:
-        silence_messages(process, ['EcalLaserDbService'])
+    #silence_messages(process, ['EcalLaserDbService'])
 
     process.load('Configuration.StandardSequences.Services_cff')
     process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
@@ -36,37 +25,19 @@ def pat_tuple_process(customize_before_unscheduled, is_mc, year, H, repro):
 
     process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
     from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
-    process.GlobalTag = GlobalTag(process.GlobalTag, which_global_tag(is_mc, year, H, repro), '')
+    process.GlobalTag = GlobalTag(process.GlobalTag, which_global_tag(settings), '')
 
-    process.options = cms.untracked.PSet(allowUnscheduled = cms.untracked.bool(True),
-                                         wantSummary = cms.untracked.bool(False),
-                                         )
+    process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
     process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
     process.source = cms.Source('PoolSource', fileNames = cms.untracked.vstring())
     process.source.fileNames = [{
-        (2015, True):  '/store/mc/RunIIFall15DR76/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/AODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/00000/0039E642-58BD-E511-B773-002590DE7230.root',
-        (2015, False): '/store/data/Run2015D/JetHT/AOD/16Dec2015-v1/00000/0A2C6696-AEAF-E511-8551-0026189438EB.root',
-        (2016, True):  'file:/uscmst1b_scratch/lpc1/3DayLifetime/tucker/2A6D6695-3BB2-E611-BA91-24BE05C626B1.root',
-        (2016, False): '/store/data/Run2016G/JetHT/AOD/23Sep2016-v1/100000/0006CE1E-9986-E611-8DFB-6C3BE5B5C0B0.root',
+        (2017, False): 'file:/uscmst1b_scratch/lpc1/3DayLifetime/tucker/itch/3CF97E8C-41DF-E711-9D55-008CFA56D6F4.root',
         (2017, True):  'file:/uscmst1b_scratch/lpc1/3DayLifetime/tucker/itch/B60ECF80-38F1-E711-BE32-02163E01A564.root',
-        }[(year, is_mc)]]
+        }[(settings.year, settings.is_mc)]]
 
     output_file(process, 'pat.root', process.MINIAODSIMEventContent.outputCommands)
-
-    if year == 2015:
-        if not is_mc: # is_mc taken care of by Eras in cms.Process definition?
-            # I think these next two are already set but just in
-            # case. This replaces the use of
-            # Configuration.DataProcessing.RecoTLR.customiseDataRun2Common_25ns,
-            # which seems to be only needed if running raw2digi, L1reco,
-            # reco, or dqm.
-            process.CSCGeometryESModule.useGangedStripsInME1a = False
-            process.idealForDigiCSCGeometry.useGangedStripsInME1a = False
-    #elif year == 2016:
-        # the only customization in the 23sept rereco for runs B-F (not G
-        # or H) was the customizeMinPtForHitRecoveryInGluedDet, but this
-        # doesn't affect re-miniaoding
-        #pass
+    from PhysicsTools.PatAlgos.slimming.MicroEventContent_cff import MiniAODOverrideBranchesSplitLevel
+    process.out.overrideBranchesSplitLevel = MiniAODOverrideBranchesSplitLevel
 
     if customize_before_unscheduled is not None:
         customize_before_unscheduled(process)
@@ -75,30 +46,18 @@ def pat_tuple_process(customize_before_unscheduled, is_mc, year, H, repro):
     process = convertToUnscheduled(process)
     
     from PhysicsTools.PatAlgos.slimming.miniAOD_tools import miniAOD_customizeAllData, miniAOD_customizeAllMC
-    if is_mc:
+    if settings.is_mc:
         process.load('Configuration.StandardSequences.PATMC_cff') # right now, same as PAT_cff
         customize = miniAOD_customizeAllMC
     else:
         process.load('Configuration.StandardSequences.PAT_cff')
         customize = miniAOD_customizeAllData
 
-    if year == 2015:
-        # Don't use the CombinedMVAV2 jet tags, they crash on the data from memory leak (can remove after next (?) patch of 763)
-        process.patJets.discriminatorSources = cms.VInputTag(cms.InputTag("pfJetBProbabilityBJetTags"),
-                                                             cms.InputTag("pfJetProbabilityBJetTags"),
-                                                             cms.InputTag("pfTrackCountingHighPurBJetTags"),
-                                                             cms.InputTag("pfTrackCountingHighEffBJetTags"),
-                                                             cms.InputTag("pfSimpleSecondaryVertexHighEffBJetTags"),
-                                                             cms.InputTag("pfSimpleSecondaryVertexHighPurBJetTags"),
-                                                             cms.InputTag("pfCombinedSecondaryVertexV2BJetTags"),
-                                                             cms.InputTag("pfCombinedInclusiveSecondaryVertexV2BJetTags"))
-
     process = cleanUnscheduled(process)
     process = customize(process)
 
-    if year == 2017:
-        from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
-        process = customiseEarlyDelete(process)
+    from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+    process = customiseEarlyDelete(process)
 
     process.load('JMTucker.Tools.MCStatProducer_cff')
 
@@ -110,9 +69,12 @@ def pat_tuple_process(customize_before_unscheduled, is_mc, year, H, repro):
     if hasattr(process, 'ptau'):
         del process.ptau
 
-    if year == 2017:
-        process.patTask.add(process.caloJetMap)
-        #process.globalTask = cms.Task(*[p for p in process.producers_().itervalues()])
+    if settings.year == 2017:
+        # what the hell?
+        #process.patTask.add(*process.producers_().values())
+        #print 'before hamfisting:\n', process.patTask.moduleNames(), '\n'
+        for x in 'CHSCands NjettinessAK8 NjettinessAK8Puppi NjettinessAK8Subjets QGTagger TrkCands ak4PFCHSL1FastL2L3CorrectorNoHF ak4PFCHSL1FastL2L3ResidualCorrectorNoHF ak4PFCHSL1FastjetCorrectorNoHF ak4PFCHSL2RelativeCorrectorNoHF ak4PFCHSL3AbsoluteCorrectorNoHF ak4PFCHSResidualCorrectorNoHF ak4PFJetTracksAssociatorAtVertex ak4PFJetsCHSNoHF ak4PFJetsLegacyHPSPiZeros ak4PFJetsRecoTauChargedHadrons ak8GenJetsNoNuConstituents ak8GenJetsNoNuSoftDrop ak8PFJetsCHSConstituents ak8PFJetsCHSPruned ak8PFJetsCHSPrunedMass ak8PFJetsCHSSoftDrop ak8PFJetsCHSSoftDropMass ak8PFJetsCHSValueMap ak8PFJetsPuppi ak8PFJetsPuppiConstituents ak8PFJetsPuppiSoftDrop ak8PFJetsPuppiSoftDropMass ak8PFJetsPuppiSoftDropValueMap basicJetsForMet caloJetMap cleanedPatJets combinatoricRecoTaus corrPfMetType1NoHF egmElectronPUPPIIsolation egmElectronPUPPINoLeptonsIsolation egmGsfElectronIDs egmPhotonIDs egmPhotonIsolation egmPhotonPUPPIIsolation electronMVAValueMapProducer goodOfflinePrimaryVerticesNoHF heepIDVarValueMaps hpsPFTauChargedIsoPtSum hpsPFTauChargedIsoPtSumdR03 hpsPFTauDiscriminationByDecayModeFinding hpsPFTauDiscriminationByDecayModeFindingNewDMs hpsPFTauDiscriminationByIsolationMVArun2v1DBdR03oldDMwLTraw hpsPFTauDiscriminationByIsolationMVArun2v1DBnewDMwLTraw hpsPFTauDiscriminationByIsolationMVArun2v1PWdR03oldDMwLTraw hpsPFTauDiscriminationByIsolationMVArun2v1PWnewDMwLTraw hpsPFTauDiscriminationByIsolationMVArun2v1PWoldDMwLTraw hpsPFTauDiscriminationByLooseCombinedIsolationDBSumPtCorr3Hits hpsPFTauDiscriminationByLooseIsolationMVArun2v1DBdR03oldDMwLT hpsPFTauDiscriminationByLooseIsolationMVArun2v1DBnewDMwLT hpsPFTauDiscriminationByLooseIsolationMVArun2v1PWdR03oldDMwLT hpsPFTauDiscriminationByLooseIsolationMVArun2v1PWnewDMwLT hpsPFTauDiscriminationByLooseIsolationMVArun2v1PWoldDMwLT hpsPFTauDiscriminationByLooseMuonRejection3 hpsPFTauDiscriminationByMVA6LooseElectronRejection hpsPFTauDiscriminationByMVA6MediumElectronRejection hpsPFTauDiscriminationByMVA6TightElectronRejection hpsPFTauDiscriminationByMVA6VLooseElectronRejection hpsPFTauDiscriminationByMVA6VTightElectronRejection hpsPFTauDiscriminationByMVA6rawElectronRejection hpsPFTauDiscriminationByMediumCombinedIsolationDBSumPtCorr3Hits hpsPFTauDiscriminationByMediumIsolationMVArun2v1DBdR03oldDMwLT hpsPFTauDiscriminationByMediumIsolationMVArun2v1DBnewDMwLT hpsPFTauDiscriminationByMediumIsolationMVArun2v1PWdR03oldDMwLT hpsPFTauDiscriminationByMediumIsolationMVArun2v1PWnewDMwLT hpsPFTauDiscriminationByMediumIsolationMVArun2v1PWoldDMwLT hpsPFTauDiscriminationByPhotonPtSumOutsideSignalCone hpsPFTauDiscriminationByRawCombinedIsolationDBSumPtCorr3Hits hpsPFTauDiscriminationByTightCombinedIsolationDBSumPtCorr3Hits hpsPFTauDiscriminationByTightIsolationMVArun2v1DBdR03oldDMwLT hpsPFTauDiscriminationByTightIsolationMVArun2v1DBnewDMwLT hpsPFTauDiscriminationByTightIsolationMVArun2v1PWdR03oldDMwLT hpsPFTauDiscriminationByTightIsolationMVArun2v1PWnewDMwLT hpsPFTauDiscriminationByTightIsolationMVArun2v1PWoldDMwLT hpsPFTauDiscriminationByTightMuonRejection3 hpsPFTauDiscriminationByVLooseIsolationMVArun2v1DBdR03oldDMwLT hpsPFTauDiscriminationByVLooseIsolationMVArun2v1DBnewDMwLT hpsPFTauDiscriminationByVLooseIsolationMVArun2v1PWdR03oldDMwLT hpsPFTauDiscriminationByVLooseIsolationMVArun2v1PWnewDMwLT hpsPFTauDiscriminationByVLooseIsolationMVArun2v1PWoldDMwLT hpsPFTauDiscriminationByVTightIsolationMVArun2v1DBdR03oldDMwLT hpsPFTauDiscriminationByVTightIsolationMVArun2v1DBnewDMwLT hpsPFTauDiscriminationByVTightIsolationMVArun2v1PWdR03oldDMwLT hpsPFTauDiscriminationByVTightIsolationMVArun2v1PWnewDMwLT hpsPFTauDiscriminationByVTightIsolationMVArun2v1PWoldDMwLT hpsPFTauDiscriminationByVVTightIsolationMVArun2v1DBdR03oldDMwLT hpsPFTauDiscriminationByVVTightIsolationMVArun2v1DBnewDMwLT hpsPFTauDiscriminationByVVTightIsolationMVArun2v1PWdR03oldDMwLT hpsPFTauDiscriminationByVVTightIsolationMVArun2v1PWnewDMwLT hpsPFTauDiscriminationByVVTightIsolationMVArun2v1PWoldDMwLT hpsPFTauFootprintCorrection hpsPFTauFootprintCorrectiondR03 hpsPFTauNeutralIsoPtSum hpsPFTauNeutralIsoPtSumWeight hpsPFTauNeutralIsoPtSumWeightdR03 hpsPFTauNeutralIsoPtSumdR03 hpsPFTauPUcorrPtSum hpsPFTauPUcorrPtSumdR03 hpsPFTauPhotonPtSumOutsideSignalCone hpsPFTauPhotonPtSumOutsideSignalConedR03 hpsPFTauPrimaryVertexProducer hpsPFTauProducer hpsPFTauProducerSansRefs hpsPFTauSecondaryVertexProducer hpsPFTauTransverseImpactParameters hpsSelectionDiscriminator jetSelectorForMet lostTracksForTkIso muonPUPPIIsolation muonPUPPINoLeptonsIsolation nb1AK8PuppiSoftDrop nb1AK8PuppiSoftDropSubjets nb2AK8PuppiSoftDrop nb2AK8PuppiSoftDropSubjets noHFCands packedCandsForTkIso packedPatJetsAK8 particleFlowClusterOOTECAL particleFlowClusterOOTECALUncorrected particleFlowClusterPS particleFlowDisplacedVertex particleFlowRecHitOOTECAL patCHSMet patCaloMet patJetCorrFactorsAK8 patJetCorrFactorsAK8PFPuppiSoftDrop patJetCorrFactorsAK8PFPuppiSoftDropSubjets patJetCorrFactorsAK8Puppi patJetFlavourAssociationAK8 patJetFlavourAssociationAK8PFPuppiSoftDropSubjets patJetFlavourAssociationAK8Puppi patJetFlavourAssociationLegacyAK8 patJetFlavourAssociationLegacyAK8PFPuppiSoftDropSubjets patJetFlavourAssociationLegacyAK8Puppi patJetGenJetMatchAK8 patJetGenJetMatchAK8PFPuppiSoftDrop patJetGenJetMatchAK8PFPuppiSoftDropSubjets patJetGenJetMatchAK8Puppi patJetPartonAssociationLegacyAK8 patJetPartonAssociationLegacyAK8PFPuppiSoftDropSubjets patJetPartonAssociationLegacyAK8Puppi patJetPartonMatchAK8 patJetPartonMatchAK8PFPuppiSoftDrop patJetPartonMatchAK8PFPuppiSoftDropSubjets patJetPartonMatchAK8Puppi patJetsAK8 patJetsAK8PFPuppiSoftDrop patJetsAK8PFPuppiSoftDropSubjets patJetsAK8Puppi patMETsNoHF patPFMet patPFMetT0Corr patPFMetT0pcT1 patPFMetT0pcT1SmearTxy patPFMetT0pcT1Txy patPFMetT1SmearTxy patPFMetT1T2Corr patPFMetT1T2SmearCorr patPFMetT1Txy patPFMetTxy patPFMetTxyCorr patSmearedJets patTrigger patTrkMet pfBoostedDoubleSVAK8TagInfosAK8Puppi pfBoostedDoubleSecondaryVertexAK8BJetTagsAK8Puppi pfCandidateToVertexAssociation pfCombinedInclusiveSecondaryVertexV2BJetTagsAK8PFPuppiSoftDropSubjets pfCombinedInclusiveSecondaryVertexV2BJetTagsAK8Puppi pfCombinedMVAV2BJetTagsAK8PFPuppiSoftDropSubjets pfCombinedMVAV2BJetTagsAK8Puppi pfCombinedSecondaryVertexV2BJetTagsAK8Puppi pfDeepCSVDiscriminatorsJetTags pfDeepCSVJetTags pfDeepCSVJetTagsAK8PFPuppiSoftDropSubjets pfDeepCSVJetTagsAK8Puppi pfDeepCSVTagInfos pfDeepCSVTagInfosAK8PFPuppiSoftDropSubjets pfDeepCSVTagInfosAK8Puppi pfImpactParameterAK8TagInfosAK8Puppi pfImpactParameterTagInfos pfImpactParameterTagInfosAK8PFPuppiSoftDropSubjets pfImpactParameterTagInfosAK8Puppi pfInclusiveSecondaryVertexFinderAK8TagInfosAK8Puppi pfInclusiveSecondaryVertexFinderTagInfos pfInclusiveSecondaryVertexFinderTagInfosAK8PFPuppiSoftDropSubjets pfInclusiveSecondaryVertexFinderTagInfosAK8Puppi pfMetCHS pfMetNoHF pfMetT1NoHF pfMetTrk pfNoLepPUPPI pfNoPileUpCandidates pfNoPileUpIso pfNoPileUpJMENoHF pfPileUpIso pfPileUpJMENoHF pfRecoTauTagInfoProducer pfSecondaryVertexTagInfosAK8PFPuppiSoftDropSubjets pfSecondaryVertexTagInfosAK8Puppi photonIDValueMapProducer photonMVAValueMapProducer pileupJetId puppi puppiNoLep recoTauAK4PFJets08Region selectedPatJetsAK8PFPuppiSoftDrop selectedPatJetsAK8PFPuppiSoftDropSubjets selectedPatJetsAK8Puppi selectedPatJetsForMetT1T2SmearCorr selectedPrimaryVertexHighestPtTrackSumForPFMEtCorrType0 selectedVerticesForPFMEtCorrType0 slimmedGenJetsAK8SoftDropSubJets slimmedJetsAK8PFPuppiSoftDropPacked slimmedJetsAK8PFPuppiSoftDropSubjets softPFElectronsTagInfosAK8PFPuppiSoftDropSubjets softPFElectronsTagInfosAK8Puppi softPFMuonsTagInfosAK8PFPuppiSoftDropSubjets softPFMuonsTagInfosAK8Puppi tmpPFCandCollPtrNoHF'.split():
+            process.patTask.add(getattr(process,x))
 
     return process
 
@@ -159,4 +121,4 @@ def associate_paths_to_task(process, *paths):
         p.associate(process.patTask)
 
 if __name__ == '__main__':
-    process = pat_tuple_process(None, True, 2016, False, False)
+    process = pat_tuple_process(CMSSWSettings())
