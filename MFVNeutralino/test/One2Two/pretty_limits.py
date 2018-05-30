0deaf36f792462cfc1d8993f83d2954b6f51daed
Jordan Tucker <jordantucker@gmail.com>
2018-05-30 11:17:04 -0500

    flag to draw pm1sigma exclusion lines; always save second copy with "Preliminary"

--- MFVNeutralino/test/One2Two/pretty_limits.py
+++ MFVNeutralino/test/One2Two/pretty_limits.py
@@ -2,7 +2,9 @@ import sys, os
 from array import array
 from JMTucker.Tools.ROOTTools import *
 
-path = plot_dir('pretty_limits_updn_correct', make=True)
+draw_pm1sigma_excl = False
+
+path = plot_dir('pretty_limits_whereami', make=True)
 
 ts = tdr_style()
 ROOT.gStyle.SetPalette(ROOT.kBird)
@@ -107,13 +109,16 @@ for kind in 'mfv_stopdbardbar', 'mfv_neu':
         g_exp   = f2.Get('%s_expect50_fromrinterp_nm_exc_g' % kind)
         g_expup = f2.Get('%s_expect84_fromrinterp_nm_exc_g' % kind)
         g_expdn = f2.Get('%s_expect16_fromrinterp_nm_exc_g' % kind)
-        for g in g_obs, g_exp:
-            g.SetLineWidth(4)
+        if draw_pm1sigma_excl:
+            for g in g_obs, g_exp:
+                g.SetLineWidth(4)
+        else:
+            g_obs.SetLineWidth(3)
         for g in g_obs, g_obsup, g_obsdn:
             g.SetLineColor(ROOT.kBlack)
         for g in g_exp, g_expup, g_expdn:
-            g.SetLineStyle(2)
-            g.SetLineColor(ROOT.kRed)
+            g.SetLineStyle(2 if draw_pm1sigma_excl else 7)
+            g.SetLineColor(ROOT.kRed if draw_pm1sigma_excl else 1)
         for g in (g_obsup, g_obsdn, g_expup, g_expdn):
             g.SetLineWidth(2)
 
@@ -143,7 +148,8 @@ for kind in 'mfv_stopdbardbar', 'mfv_neu':
             g_exp.SetPoint(104,2230.032336,95.94497903);
             g_exp.SetPoint(105,2226.111259,96.99275073);
 
-        for g in g_exp, g_expup, g_expdn, g_obs, g_obsup, g_obsdn:
+        excl_to_draw = [g_exp, g_expup, g_expdn, g_obs, g_obsup, g_obsdn] if draw_pm1sigma_excl else [g_exp, g_obs]
+        for g in excl_to_draw:
             g.Draw('L')
 
         c.Update()
@@ -158,35 +164,40 @@ for kind in 'mfv_stopdbardbar', 'mfv_neu':
         leg.SetFillColor(ROOT.kWhite)
         leg.SetBorderSize(1)
         if kind == 'mfv_neu':
-            model = '#kern[-0.42]{#tilde{g} #rightarrow tbs}'
+            model = '#kern[-0.%i]{#tilde{g} #rightarrow tbs}' % (42 if draw_pm1sigma_excl else 22)
         else:
-            model = '#kern[-0.52]{#tilde{t} #rightarrow #bar{d}#kern[0.1]{#bar{d}}}'
+            model = '#kern[-0.%i]{#tilde{t} #rightarrow #bar{d}#kern[0.1]{#bar{d}}}' % (52 if draw_pm1sigma_excl else 22)
         leg.AddEntry(0, model, '')
-        leg.AddEntry(g_obs, 'Observed #pm 1 #sigma_{th}', 'L')
-        leg.AddEntry(g_exp, 'Expected #pm 1 #sigma_{exp}', 'L')
+        if draw_pm1sigma_excl:
+            leg.AddEntry(g_obs, 'Observed #pm 1 #sigma_{th}', 'L')
+            leg.AddEntry(g_exp, 'Expected #pm 1 #sigma_{exp}', 'L')
+        else:
+            leg.AddEntry(g_obs, '#kern[-0.22]{Observed}', 'L')
+            leg.AddEntry(g_exp, '#kern[-0.22]{Expected}', 'L')
         leg.Draw()
 
-        # these lines make the bands for the lines in the legend, sigh
-        if kind == 'mfv_neu':
-            x00, x01 = 0.239, 0.280
-            x10, x11 = 0.529, 0.570
-        else:
-            x00, x01 = 0.224, 0.265
-            x10, x11 = 0.520, 0.561
-        y0, y1 = 0.894, 0.914
-        ll = [
-            ROOT.TLine(x00, y0, x01, y0),
-            ROOT.TLine(x10, y0, x11, y0),
-            ROOT.TLine(x00, y1, x01, y1),
-            ROOT.TLine(x10, y1, x11, y1),
-            ]
-        for l in ll[1], ll[3]:
-            l.SetLineStyle(2)
-            l.SetLineColor(ROOT.kRed)
-        for l in ll:
-            l.SetLineWidth(2)
-            l.SetNDC(1)
-            l.Draw()
+        if draw_pm1sigma_excl:
+            # these lines make the bands for the lines in the legend, sigh
+            if kind == 'mfv_neu':
+                x00, x01 = 0.239, 0.280
+                x10, x11 = 0.529, 0.570
+            else:
+                x00, x01 = 0.224, 0.265
+                x10, x11 = 0.520, 0.561
+            y0, y1 = 0.894, 0.914
+            ll = [
+                ROOT.TLine(x00, y0, x01, y0),
+                ROOT.TLine(x10, y0, x11, y0),
+                ROOT.TLine(x00, y1, x01, y1),
+                ROOT.TLine(x10, y1, x11, y1),
+                ]
+            for l in ll[1], ll[3]:
+                l.SetLineStyle(2)
+                l.SetLineColor(ROOT.kRed)
+            for l in ll:
+                l.SetLineWidth(2)
+                l.SetNDC(1)
+                l.Draw()
 
         cms = write(61, 0.050, 0.109, 0.950, 'CMS')
         lum = write(42, 0.050, 0.515, 0.950, '38.5 fb^{-1} (13 TeV)')
@@ -194,4 +205,10 @@ for kind in 'mfv_stopdbardbar', 'mfv_neu':
         c.SaveAs(fn + '.pdf')
         c.SaveAs(fn + '.png')
         c.SaveAs(fn + '.root')
+
+        pre = write(52, 0.047, 0.209, 0.950, 'Preliminary')
+        c.SaveAs(fn + '_prelim.pdf')
+        c.SaveAs(fn + '_prelim.png')
+        c.SaveAs(fn + '_prelim.root')
+
         del c
