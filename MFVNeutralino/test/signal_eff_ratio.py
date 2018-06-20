699b68ef128652acb6aef7f30e7575f080f0fe94
Jordan Tucker <jordantucker@gmail.com>
2018-06-20 13:40:14 -0500

    ~more user friendly, autodetect available sample files, do both multijet and dijet

--- MFVNeutralino/test/signal_eff_ratio.py
+++ MFVNeutralino/test/signal_eff_ratio.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
-import os
+from os.path import join, isfile
 from JMTucker.Tools.ROOTTools import *
 from JMTucker.Tools import Samples
 from JMTucker.MFVNeutralino.PerSignal import PerSignal
 
-set_style()
-ps = plot_saver(plot_dir('sigeff_ratio_miniaod'), size=(600,600), log=False)
+titles = 'v18', 'v19'
+paths = ('/uscms_data/d2/tucker/crab_dirs/MiniTreeV18mp1',
+         '/uscms_data/d2/tucker/crab_dirs/MiniTreeV19m')
 
-samples = [s for s in Samples.mfv_signal_samples if s.has_dataset('ntuplev16m')]
+set_style()
+ps = plot_saver(plot_dir('sigeff_ratio_%sV%s' % titles), size=(600,600), log=False)
+
+samples = Samples.all_signal_samples_2017
+def available(samples):
+    return [s for s in samples if isfile(join(paths[0], '%s.root' % s.name)) and isfile(join(paths[1], '%s.root' % s.name))]
+samples = available(samples)
 
 def getit(fn):
     f = ROOT.TFile(fn)
@@ -23,26 +30,29 @@ def getit(fn):
     return e,ee,n,d
 
 for sample in samples:
-    sample.e1 = getit('/uscms_data/d2/tucker/crab_dirs/MiniTreeV16/%s.root'  % sample.name)
-    sample.e2 = getit('/uscms_data/d2/tucker/crab_dirs/MiniTreeV16m/%s.root' % sample.name)
+    sample.e1 = getit(join(paths[0], '%s.root' % sample.name))
+    sample.e2 = getit(join(paths[1], '%s.root' % sample.name))
     print '%26s: efficiency = %.3f +- %.3f -> %.3f +- %.3f' % ((sample.name,) + sample.e1[:2] + sample.e2[:2])
     print '%26s     %r %r' % ('', sample.e1, sample.e2)
 
 per = PerSignal('efficiency', y_range=(0.,1.05))
 for s in samples:
     s.y,s.ye = s.e1[:2]
-per.add(samples, title='aod', color=ROOT.kRed)
+per.add(available(Samples.mfv_signal_samples_2017),       title=titles[0] + ' multijet', color=ROOT.kRed)
+per.add(available(Samples.mfv_stopdbardbar_samples_2017), title=titles[0] + ' dijet',    color=ROOT.kBlue)
 PerSignal.clear_samples(samples)
 for s in samples:
     s.y,s.ye = s.e2[:2]
-per.add(samples, title='miniaod', color=ROOT.kBlue)
+per.add(available(Samples.mfv_signal_samples_2017),       title=titles[1] + ' multijet', color=ROOT.kRed)
+per.add(available(Samples.mfv_stopdbardbar_samples_2017), title=titles[1] + ' dijet',    color=ROOT.kBlue)
 per.draw(canvas=ps.c)
 ps.save('eff')
 
-per = PerSignal('ratio miniaod/aod', y_range=(0.6,1.15))
+per = PerSignal('ratio %s/%s' % titles, y_range=(0.6,1.15))
 PerSignal.clear_samples(samples)
 for s in samples:
     s.y, s.yl, s.yh = clopper_pearson_poisson_means(s.e2[2], s.e1[2])
-per.add(samples, color=ROOT.kMagenta)
+per.add(available(Samples.mfv_signal_samples_2017),       title='multijet', color=ROOT.kPink)
+per.add(available(Samples.mfv_stopdbardbar_samples_2017), title='dijet',    color=ROOT.kCyan)
 per.draw(canvas=ps.c)
 ps.save('eff_ratio')
