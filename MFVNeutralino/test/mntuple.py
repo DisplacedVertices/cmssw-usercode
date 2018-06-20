7cd5f3bfce5c5fa7acf78d55ec198a0229c333a1
Jordan Tucker <jordantucker@gmail.com>
2018-06-20 09:18:03 -0500

    should always run ntuple on lepton samples

--- MFVNeutralino/test/mntuple.py
+++ MFVNeutralino/test/mntuple.py
@@ -164,7 +164,7 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
 
     if year == 2017:
         samples = Samples.ttbar_samples_2017 + Samples.qcd_samples_2017 + Samples.all_signal_samples_2017
-        #samples += Samples.leptonic_samples_2017
+        samples += Samples.leptonic_samples_2017
 
     dataset = 'miniaod'
     #samples = [s for s in samples if s.has_dataset(dataset)]
