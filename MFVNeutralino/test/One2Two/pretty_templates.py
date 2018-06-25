fd2071b2652f65890a38d0f35c2d44e160906951
Jordan Tucker <jordantucker@gmail.com>
2018-06-25 16:52:27 -0500

    break y-axis

--- MFVNeutralino/test/One2Two/pretty_templates.py
+++ MFVNeutralino/test/One2Two/pretty_templates.py
@@ -4,6 +4,10 @@ from signal_efficiency import SignalEfficiencyCombiner
 set_style()
 ps = plot_saver(plot_dir('pretty_templates'), size=(700,700), log=False, pdf=True)
 
+ps.c.SetBottomMargin(0.11)
+ps.c.SetLeftMargin(0.13)
+ps.c.SetRightMargin(0.06)
+
 f = ROOT.TFile('/uscms/home/tucker/public/mfv/limitsinput_100kevent_samples_used_for_Figs_1+3.root')
 combiner = SignalEfficiencyCombiner()
 
@@ -34,7 +38,7 @@ def fmt(z, title, color, style, save=[]):
     h.GetXaxis().SetLabelSize(0.045)
     h.GetYaxis().SetTitleSize(0.05)
     h.GetYaxis().SetLabelSize(0.045)
-    h.GetYaxis().SetTitleOffset(1.1)
+    h.GetYaxis().SetTitleOffset(1.3)
     move_above_into_bin(h, 3.999)
     if title == 'bkg': 
         norm = 1.
@@ -53,14 +57,17 @@ leg.SetBorderSize(0)
 leg.AddEntry(hbkg, 'Background template', 'LF')
 leg.AddEntry(0, '#kern[-0.22]{Multijet signals, M = 800 GeV, #sigma = 1 fb:}', '')
 
+htobreak = None
 for zzz, (name, title, color, style) in enumerate(which):
     h = fmt(name, title, color, style)
+    if name == 'mfv_neu_tau10000um_M0800':
+        htobreak = h
     if zzz == 0:
         h.Draw('hist')
     else:
         h.Draw('hist same')
     h.GetXaxis().SetRangeUser(0,4)
-    h.GetYaxis().SetRangeUser(0,1)
+    h.GetYaxis().SetRangeUser(0,1.05)
     leg.AddEntry(h, title, 'L')
     print name, h.Integral(0,h.GetNbinsX()+2)
 
@@ -76,11 +83,49 @@ def write(font, size, x, y, text):
     w.DrawLatex(x, y, text)
     return w
 
-write(61, 0.050, 0.109, 0.913, 'CMS')
-write(42, 0.050, 0.560, 0.913, '38.5 fb^{-1} (13 TeV)')
+write(61, 0.050, 0.129, 0.913, 'CMS')
+write(42, 0.050, 0.595, 0.913, '38.5 fb^{-1} (13 TeV)')
 
-ps.c.SetBottomMargin(0.11)
-ps.c.SetLeftMargin(0.11)
+# do broken y-axis. replace the "1" label with "18.2" and replace the
+# last bin of that one hist's contents with 1 + contents - 18.2.
+
+yax = htobreak.GetYaxis()
+lastibin = htobreak.FindBin(3.999)
+lastbin = htobreak.GetBinLowEdge(lastibin)
+lastbc = htobreak.GetBinContent(lastibin)
+assert abs(lastbc - 18.202475) < 1e-6
+htobreak.SetBinContent(lastibin, lastbc - 18.2 + 1)
+boxxcenter = 0.
+boxxwidth = 0.06
+boxycenter = 0.9
+boxywidth = 0.0205
+boxx1, boxx2 = boxxcenter - boxxwidth, boxxcenter + boxxwidth
+boxy1, boxy2 = boxycenter - boxywidth, boxycenter + boxywidth
+boxxcenter2 = 4.
+boxx21, boxx22 = boxxcenter2 - boxxwidth, boxxcenter2 + boxxwidth
+boxes = [
+    ROOT.TBox(boxx1, boxy1, boxx2, boxy2), # this does the break in the left x-axis
+    ROOT.TBox(boxx21,boxy1, boxx22,boxy2), # ditto right x-axis
+    ROOT.TBox(lastbin-0.02,boxy1,lastbin+0.02,boxy2), # wipes the part of the curve
+    ROOT.TBox(-0.3, 0.95, -0.01, 1.05) # this wipes the end "1" label
+    ]
+for box in boxes:
+    box.SetLineColor(ROOT.kWhite)
+    box.SetFillColor(ROOT.kWhite)
+    box.Draw()
+
+# draw the new end label
+lab2 = ROOT.TText(-0.402, 0.982, '18.2')
+lab2.SetTextFont(yax.GetLabelFont())
+lab2.SetTextSize(yax.GetLabelSize())
+lab2.Draw()
+
+# draw the break lines
+slantdy = 0.005
+lines = [ROOT.TLine(x1, y - slantdy, x2, y + slantdy) for y in boxy1, boxy2 for x1,x2 in (boxx1, boxx2), (boxx21, boxx22)]
+for line in lines:
+    line.SetLineWidth(1)
+    line.Draw()
 
 ps.save('templates')
 
