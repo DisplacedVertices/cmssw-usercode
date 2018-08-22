b30d3bf57fd135510cc19aa0f72eb1d08aa8f3b9
Jordan Tucker <jordantucker@gmail.com>
2018-08-22 12:18:58 -0500

    (restored) example of looping over minitrees

---
+++ MFVNeutralino/test/MiniTree/looptrees.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+ntk=5
+indir=/uscms_data/d2/tucker/crab_dirs/MiniTreeV20m
+outdir=output
+files=(
+    qcdht0700_2017.root
+    qcdht1000_2017.root
+    qcdht1500_2017.root
+    qcdht2000_2017.root
+    ttbar_2017.root
+#    mfv_neu_tau000100um_M0800_2017.root
+#    mfv_neu_tau000300um_M0800_2017.root
+    mfv_neu_tau001000um_M0800_2017.root
+#    mfv_neu_tau010000um_M0800_2017.root
+#    mfv_neu_tau030000um_M0800_2017.root
+#    mfv_neu_tau100000um_M0800_2017.root
+#    mfv_stopdbardbar_tau000100um_M0800_2017.root
+#    mfv_stopdbardbar_tau000300um_M0800_2017.root
+#    mfv_stopdbardbar_tau001000um_M0800_2017.root
+#    mfv_stopdbardbar_tau010000um_M0800_2017.root
+#    mfv_stopdbardbar_tau030000um_M0800_2017.root
+#    mfv_stopdbardbar_tau100000um_M0800_2017.root
+)
+intlumi=$(python -c 'import JMTucker.MFVNeutralino.AnalysisConstants as ac; print ac.int_lumi_2017 * ac.scale_factor_2017')
+
+########################################################################
+
+if [[ -d $outdir ]]; then
+    echo $outdir already exists
+    exit 1
+fi
+
+mkdir $outdir
+
+make
+if [[ $? != 0 ]]; then
+    exit 1
+fi
+
+outbackgrounds=()
+outsignals=()
+
+for x in ${files[@]}; do
+    fin=$indir/$x
+    fout=$outdir/$x
+    if [[ ! -e $fin ]]; then
+        echo $fin missing
+        exit 1
+    fi
+
+    echo $x
+    ./looptrees.exe $fin $fout $ntk
+    if [[ $? != 0 ]]; then
+        echo problem, exit code was $?
+        exit 1
+    fi
+
+    if [[ $x != mfv* ]]; then
+        outbackgrounds+=($fout)
+    else
+        outsignals+=($fout)
+    fi
+done
+
+samples merge -${intlumi} $outdir/background.root ${outbackgrounds[@]}
+
+echo; echo scaling files, do not rerun merge background or use the h_sums in these files after this
+for x in ${outbackgrounds[@]} ${outsignals[@]}; do
+    y=$outdir/temp.root
+    samples merge -${intlumi} $x $y
+    mv $x ${x/.root/.unscaled.root}
+    mv $y $x
+done
