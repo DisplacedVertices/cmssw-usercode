f12d958073c44ba762cc917e3f807c739a79ac5b
Jordan Tucker <jordantucker@gmail.com>
2018-09-26 12:55:11 -0500

    2017 example and full file paths

--- MFVNeutralino/test/jsons/drop_prescaled.py
+++ MFVNeutralino/test/jsons/drop_prescaled.py
@@ -2,14 +2,16 @@ import gzip
 from FWCore.PythonUtilities.LumiList import LumiList
 from JMTucker.Tools.general import intlumi_from_brilcalc_csv as parse
 
-# 2420  brilcalc lumi -i 2015.json -o 2015.byls.csv --byls --normtag=normtag_2015.json
-# 2424  brilcalc lumi -i 2016.json -o 2016.byls.csv --byls --normtag=normtag_2016.json
-# 2421  brilcalc lumi -i 2015.json -o 2015.byls.HLT_PFHT800.csv --byls --normtag=normtag_2015.json --hltpath=HLT_PFHT800_v\*
-# 2422  brilcalc lumi -i 2016.json -o 2016.byls.HLT_PFHT900.csv --byls --normtag=normtag_2016.json --hltpath=HLT_PFHT900_v\*
+# brilcalc lumi -i 2015.json -o 2015.byls.csv --byls --normtag=normtag_2015.json ; gzip 2015.byls.csv
+# brilcalc lumi -i 2016.json -o 2016.byls.csv --byls --normtag=normtag_2016.json ; gzip 2016.byls.csv
+# brilcalc lumi -i 2015.json -o 2015.byls.HLT_PFHT800.csv --byls --normtag=normtag_2015.json --hltpath=HLT_PFHT800_v\* ; gzip 2015.byls.HLT_PFHT800.csv
+# brilcalc lumi -i 2016.json -o 2016.byls.HLT_PFHT900.csv --byls --normtag=normtag_2016.json --hltpath=HLT_PFHT900_v\* ; gzip 2016.byls.HLT_PFHT900.csv
+# brilcalc lumi -i 2017.json -o 2017.byls.csv --byls --normtag=normtag_2017.json ; gzip 2017.byls.csv
+# brilcalc lumi -i 2017.json -o 2017.byls.HLT_PFHT1050.csv --byls --normtag=normtag_2017.json --hltpath=HLT_PFHT1050_v\* ; gzip 2017.byls.HLT_PFHT1050.csv
 
 ils = [
-    (2017, (parse('2017.byls.csv.gz',          False),
-            parse('2017.byls.PFHT1050.csv.gz', True))),
+    (2017, (parse('/uscms/home/tucker/public/mfv/lumi/2017.byls.csv.gz',              False),
+            parse('/uscms/home/tucker/public/mfv/lumi/2017.byls.HLT_PFHT1050.csv.gz', True))),
     ]
 
 for year, ((wohlt, wohlt_sum), (whlt, whlt_sum)) in ils:
