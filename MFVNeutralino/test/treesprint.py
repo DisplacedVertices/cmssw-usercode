d9d6187018aa6ec6a48344e077ad95ac4a6e5a87
Jordan Tucker <jordantucker@gmail.com>
2018-10-21 06:53:03 -0500

    print b quark fracs

--- MFVNeutralino/test/treesprint.py
+++ MFVNeutralino/test/treesprint.py
@@ -21,7 +21,7 @@ def getit(fn, ntk):
     f = ROOT.TFile.Open(fn)
     t = f.Get('%s/t' % ntk)
     if not t:
-        return (-1,-1,-1), (-1,-1,-1)
+        return (-1,-1,-1), (-1,-1,-1), (-1,-1,-1), (-1,-1,-1)
     hr = draw_hist_register(t, True)
     def c(cut):
         #cut = '(%s) && njets >= 4 && jetht > 700 && jetht < 1000' % cut
@@ -30,13 +30,15 @@ def getit(fn, ntk):
         h,n = hr.draw('weight', cut, binning='1,0,1', get_n=True, goff=True)
         return (n,) + get_integral(h)
     n1v = c('nvtx==1')
+    n1vb = c('nvtx==1 && gen_flavor_code==2')
     if ntk == 'mfvMiniTreeNtk3or4':
         n2v = c('nvtx>=2 && ntk0==4 && ntk1==3')
     else:
         n2v = c('nvtx>=2')
-    return n1v, n2v
+        n2vb = c('nvtx>=2 && gen_flavor_code==2')
+    return n1v, n1vb, n2v, n2vb
 
-fmt = '%40s %9s %9s %9s   %9s +- %9s  %9s +- %9s  %9s +- %9s  %9s +- %9s'
+fmt = '%40s %9s %9s %9s      %7s  %9s +- %9s  %9s +- %9s     %7s  %9s +- %9s  %9s +- %9s'
 
 int_lumi = ac.int_lumi_2017 * ac.scale_factor_2017
 print 'MC scaled to int. lumi. %.3f/fb' % (int_lumi/1000)
@@ -44,13 +46,15 @@ print 'MC scaled to int. lumi. %.3f/fb' % (int_lumi/1000)
 for ntk in 'mfvMiniTreeNtk3', 'mfvMiniTreeNtk4', 'mfvMiniTree':
     print
     print ntk
-    print fmt % ('sample', 'xsec', 'nevents', 'weight', 'rn1v', 'unc', 'wn1v', 'unc', 'rn2v', 'unc', 'wn2v', 'unc')
+    print fmt % ('sample', 'xsec', 'nevents', 'weight', 'f1vb', 'rn1v', 'unc', 'wn1v', 'unc', 'f2vb', 'rn2v', 'unc', 'wn2v', 'unc')
 
     raw_n1v, sum_n1v, var_n1v, raw_n2v, sum_n2v, var_n2v = 0, 0, 0, 0, 0, 0
     seen_bkg, seen_data = False, False
 
     for fn in fns:
-        (r1v, n1v, en1v), (r2v, n2v, en2v) = getit(fn, ntk)
+        (r1v, n1v, en1v), (r1vb, _, _), (r2v, n2v, en2v), (r2vb, _, _) = getit(fn, ntk)
+        f1vb = float(r1vb) / r1v if r1v > 0 else 0.
+        f2vb = float(r2vb) / r2v if r2v > 0 else 0.
 
         sname = os.path.basename(fn).replace('.root', '')
         is_sig = sname.startswith('mfv_')
@@ -73,8 +77,8 @@ for ntk in 'mfvMiniTreeNtk3', 'mfvMiniTreeNtk4', 'mfvMiniTree':
             if not sample.is_mc:
                 w = 1.
                 print fmt % (sample.name, '', '', '',
-                             r1v, '%9.0f' % abs(r1v)**0.5, '', '',
-                             r2v, '%9.0f' % abs(r2v)**0.5, '', '')
+                             '', r1v, '%9.0f' % abs(r1v)**0.5, '', '',
+                             '', r2v, '%9.0f' % abs(r2v)**0.5, '', '')
             else:
                 w = int_lumi * sample.partial_weight(fn)
                 if sample.is_signal:
@@ -87,10 +91,12 @@ for ntk in 'mfvMiniTreeNtk3', 'mfvMiniTreeNtk4', 'mfvMiniTree':
                              xsec,
                              '%.0f' % sample.nevents(fn),
                              '%9.3g' % w,
+                             '%7.2f' % f1vb,
                              x[0][0],
                              '%9.0f' % x[0][1],
                              '%9.2f' % x[0][2],
                              '%9.2f' % x[0][3],
+                             '%7.2f' % f2vb,
                              x[1][0],
                              '%9.0f' % x[1][1],
                              '%9.2f' % x[1][2],
@@ -111,5 +117,5 @@ for ntk in 'mfvMiniTreeNtk3', 'mfvMiniTreeNtk4', 'mfvMiniTree':
         x = (raw_n1v, raw_n1v**0.5, sum_n1v, var_n1v**0.5), (raw_n2v, raw_n2v**0.5, sum_n2v, var_n2v**0.5)
         print
         print fmt % ('total background', '', '', '',
-                     x[0][0], '%9.2f' % x[0][1], '%9.2f' % x[0][2], '%9.2f' % x[0][3],
-                     x[1][0], '%9.2f' % x[1][1], '%9.2f' % x[1][2], '%9.2f' % x[1][3])
+                     '', x[0][0], '%9.2f' % x[0][1], '%9.2f' % x[0][2], '%9.2f' % x[0][3],
+                     '', x[1][0], '%9.2f' % x[1][1], '%9.2f' % x[1][2], '%9.2f' % x[1][3])
