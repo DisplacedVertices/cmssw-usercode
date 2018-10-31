537217468c90a19c46a61ecd46734060bc2cc978
Jordan Tucker <jordantucker@gmail.com>
2018-10-31 15:00:14 -0500

    resurrect most of version that can run on aod

--- Tools/test/pileup_dist.py
+++ Tools/test/pileup_dist.py
@@ -2,15 +2,18 @@ import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import *
 from JMTucker.Tools.CMSSWTools import *
 
+from_miniaod = True
+dataset = 'miniaod' if from_miniaod else 'main'
+
 max_events(process, -1)
-sample_files(process, 'qcdht2000_2017', 'miniaod', 1)
+sample_files(process, 'qcdht2000_2017', dataset, 1)
 tfileservice(process, 'pileup.root')
 
 process.load('JMTucker.Tools.MCStatProducer_cff')
 
 process.PileupDist = cms.EDAnalyzer('PileupDist',
-                                    primary_vertices_src = cms.InputTag('offlineSlimmedPrimaryVertices'),
-                                    pileup_info_src = cms.InputTag('slimmedAddPileupInfo'),
+                                    primary_vertices_src = cms.InputTag('offlineSlimmedPrimaryVertices' if from_miniaod else 'offlinePrimaryVertices'),
+                                    pileup_info_src = cms.InputTag('slimmedAddPileupInfo' if from_miniaod else 'addPileupInfo'),
                                     pileup_weights = cms.vdouble(),
                                     )
 
@@ -22,14 +25,16 @@ process.hltHighLevel.HLTPaths = ['HLT_PFHT1050_v*']
 process.hltHighLevel.andOr = True
 process.hltHighLevel.throw = False
 
-process.load('JMTucker.Tools.PATTupleSelection_cfi')
-process.load('PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi')
-process.selectedPatJets.src = 'slimmedJets'
-process.selectedPatJets.cut = process.jtupleParams.jetCut
+process.p = cms.Path(process.PileupDist * process.hltHighLevel * process.PileupDistHLT)
 
-process.load('JMTucker.Tools.JetFilter_cfi')
+if from_miniaod:
+    process.load('PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi')
+    process.load('JMTucker.Tools.JetFilter_cfi')
+    process.load('JMTucker.Tools.PATTupleSelection_cfi')
+    process.selectedPatJets.src = 'slimmedJets'
+    process.selectedPatJets.cut = process.jtupleParams.jetCut
 
-process.p = cms.Path(process.PileupDist * process.hltHighLevel * process.PileupDistHLT * process.selectedPatJets * process.jmtJetFilter * process.PileupDistPreSel)
+    process.p *= process.selectedPatJets * process.jmtJetFilter * process.PileupDistPreSel
 
 pileup_weights = [] # for changing minbias xsec study
 for xsec, weights in pileup_weights:
@@ -49,9 +54,12 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     if year == 2017:
         samples = Samples.ttbar_samples_2017 + Samples.qcd_samples_2017 + Samples.leptonic_samples_2017 + Samples.all_signal_samples_2017
 
-    set_splitting(samples, 'miniaod', 'default', default_files_per=50)
+    set_splitting(samples, dataset, 'default', default_files_per=50)
 
-    ms = MetaSubmitter('PileupDistV3', dataset='miniaod')
+    batch_name = 'PileupDistV3p1'
+    if not from_miniaod:
+        batch_name += '_aod'
+    ms = MetaSubmitter(batch_name, dataset=dataset)
     ms.common.ex = year
     ms.crab.job_control_from_sample = True
     ms.submit(samples)
