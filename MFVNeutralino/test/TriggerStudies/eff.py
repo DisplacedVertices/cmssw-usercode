9733c80605c0bb14529b6376d90cd38c4b35b2d9
Jordan Tucker <jordantucker@gmail.com>
2018-11-05 14:20:37 -0600

    fixes

--- MFVNeutralino/test/TriggerStudies/eff.py
+++ MFVNeutralino/test/TriggerStudies/eff.py
@@ -18,12 +18,8 @@ global_tag(process, which_global_tag(settings))
 #want_summary(process)
 #report_every(process, 1)
 max_events(process, -1)
-input_files(process, {
-    (2017,True): '/uscmst1b_scratch/lpc1/3DayLifetime/tucker/itch/store/mc/RunIIFall17MiniAOD/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/94X_mc2017_realistic_v10-v1/20000/FA596B3F-C303-E811-B69C-20CF3027A6DC.root',
-    (2017,False):'/uscmst1b_scratch/lpc1/3DayLifetime/tucker/itch/store/data/Run2017F/SingleMuon/MINIAOD/17Nov2017-v1/70001/DC73F8F1-A5EA-E711-A5F3-141877410B4D.root',
-    (2018,True): '/uscmst1b_scratch/lpc1/3DayLifetime/tucker/itch/store/mc/RunIIFall18MiniAOD/WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v12-v1/270000/DD8D39DE-C4B3-D241-99CC-79AF11E2EDE9.root',
-    (2018,False):'/uscmst1b_scratch/lpc1/3DayLifetime/tucker/itch/store/data/Run2018D/SingleMuon/MINIAOD/PromptReco-v2/000/321/457/00000/4402D66D-E0A5-E811-8A35-FA163EBDCF4F.root',
-    }[(year, settings.is_mc)])
+dataset = 'miniaod'
+sample_files(process, 'wjetstolnu_2017', dataset, 1)
 
 process.load('JMTucker.Tools.MCStatProducer_cff')
 process.load('JMTucker.Tools.WeightProducer_cfi')
@@ -87,10 +83,10 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     elif year == 2018:
         samples = Samples.auxiliary_data_samples_2018
     
-    dataset = 'miniaod'
+    samples = [s for s in samples if s.has_dataset(dataset) and (s.is_mc or not cross)]
     set_splitting(samples, dataset, 'default', json_path('ana_2017p8.json'), 50)
 
-    ms = MetaSubmitter('TrigEff%s' % version, dataset=dataset)
+    ms = MetaSubmitter('TrigEff%s%s' % (version, '_' + settings.cross if settings.cross else ''), dataset=dataset)
     ms.common.pset_modifier = chain_modifiers(is_mc_modifier, per_sample_pileup_weights_modifier(cross=settings.cross))
     ms.condor.stageout_files = 'all'
     ms.submit(samples)
