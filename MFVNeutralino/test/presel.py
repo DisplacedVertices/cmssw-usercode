197c1cc23cf2c62201f6993d164cdc8463987820
Jordan Tucker <jordantucker@gmail.com>
2018-11-05 08:19:46 -0600

    json

--- MFVNeutralino/test/presel.py
+++ MFVNeutralino/test/presel.py
@@ -61,7 +61,7 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         samples = Samples.data_samples_2018
 
     #samples = [s for s in samples if s.has_dataset(dataset)]
-    set_splitting(samples, dataset, 'ntuple', data_json=json_path('ana_2017_1pc.json'))
+    set_splitting(samples, dataset, 'ntuple', data_json=json_path('ana_2017p8_1pc.json'))
 
     ms = MetaSubmitter('PreselHistos' + settings.version, dataset=dataset)
     ms.common.pset_modifier = chain_modifiers(is_mc_modifier, per_sample_pileup_weights_modifier())
