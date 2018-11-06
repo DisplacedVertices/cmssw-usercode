3c622633dc9538571476e71d93794a71ae53e29b
Jordan Tucker <jordantucker@gmail.com>
2018-11-06 14:56:56 -0600

    clean up

--- Tools/test/EventIds/event_ids.py
+++ Tools/test/EventIds/event_ids.py
@@ -1,21 +1,18 @@
 import sys
 from JMTucker.Tools.BasicAnalyzer_cfg import *
 
+process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
+
+tfileservice(process, 'evids.root')
 file_event_from_argv(process)
 
-process.TFileService.fileName = cms.string('evids.root')
+add_analyzer(process, 'EventIdRecorder', prints = untracked.bool('prints' in sys.argv))
 
-process.evids = cms.EDAnalyzer('EventIdRecorder')
-process.evids.prints = cms.untracked.bool('prints' in sys.argv)
-
-process.p = cms.Path(process.evids)
-
-process.source.duplicateCheckMode = cms.untracked.string('noDuplicateCheck')
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.MetaSubmitter import *
     from JMTucker.Tools.Year import year
-    import JMTucker.Tools.Samples as Samples
+    from JMTucker.Tools import Samples
 
     if year == 2017:
         samples = Samples.qcd_samples_2017 + Samples.ttbar_samples_2017 + Samples.leptonic_samples_2017 + Samples.all_signal_samples_2017 + Samples.data_samples_2017
@@ -26,7 +23,6 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     samples = [s for s in samples if s.has_dataset(dataset)]
     set_splitting(samples, dataset, 'default', default_files_per=50)
 
-    from JMTucker.Tools.CondorSubmitter import CondorSubmitter
     ms = MetaSubmitter('EventIdsV21m', dataset=dataset)
     ms.condor.stageout_files = 'all'
     ms.submit(samples)
