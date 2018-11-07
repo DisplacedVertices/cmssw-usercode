cc5c77ef0ca47198f340da77a2eadd478411553c
Jordan Tucker <jordantucker@gmail.com>
2018-11-07 08:08:44 -0600

    cleanup; miniaod can be specified from argv

--- Tools/test/particle_list.py
+++ Tools/test/particle_list.py
@@ -1,12 +1,12 @@
-import os, sys
 from JMTucker.Tools.BasicAnalyzer_cfg import *
 
-del process.TFileService
-process.MessageLogger.cerr.FwkReport.reportEvery = 1
+remove_tfileservice(process)
+report_every(process, 1)
 file_event_from_argv(process)
 
 process.load('JMTucker.Tools.ParticleListDrawer_cff')
 process.ParticleListDrawer.maxEventsToPrint = -1
 process.p = cms.Path(process.ParticleListDrawer)
 
-#process.ParticleListDrawer.src = 'prunedGenParticles'
+if 'miniaod' in sys.argv or False:
+    process.ParticleListDrawer.src = 'prunedGenParticles'
