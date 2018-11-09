8ab53164516a8cad0cabd90700b8800361491662
Jordan Tucker <jordantucker@gmail.com>
2018-11-09 11:23:26 -0600

    presel doesn't do half_mc; print header, don't print lepton sel frac; handle SampleFiles missing sample

--- MFVNeutralino/test/Shed/presel_splitting.py
+++ MFVNeutralino/test/Shed/presel_splitting.py
@@ -3,9 +3,10 @@ from math import ceil
 from JMTucker.Tools.ROOTTools import *
 from JMTucker.Tools import SampleFiles as sf
 
-used_half_mc = True
+used_half_mc = False
 target_nevents = 50000 # the number of presel events that make it to the vertexer per job
 
+print '%30s %12s/%12s = %10s -> %10s (%10s)' % ('sample','npresel','nmcstat','frac','targnfns','evsfromtgt')
 for fn in sys.argv[1:]:
     sname = os.path.basename(fn).replace('.root','')
     f = ROOT.TFile(fn)
@@ -13,8 +14,8 @@ for fn in sys.argv[1:]:
     if used_half_mc and not sname.startswith('JetHT'):
         mcstat /= 2
     npresjet = f.Get('mfvEventHistosJetPreSel/h_npv').GetEntries()
-    npreslep = f.Get('mfvEventHistosLeptonPreSel/h_npv').GetEntries()
-    nfns = len(sf.get_fns(sname, 'miniaod'))
+    #npreslep = f.Get('mfvEventHistosLeptonPreSel/h_npv').GetEntries()
+    nfns = len(sf.get_fns(sname, 'miniaod')) if sf.has(sname, 'miniaod') else -1
     tfns = min(int(ceil(target_nevents / npresjet * nfns)), 50)
     evsfromtfns = (2 if used_half_mc else 1) * mcstat * tfns / nfns
-    print '%30s %12.0f/%12.0f = %10.6f -> %3i/%4i (%8i) (lep %12.0f -> %10.6f)' % (sname, npresjet, mcstat, npresjet/mcstat, tfns, nfns, evsfromtfns, npreslep, npreslep/mcstat)
+    print '%30s %12.0f/%12.0f = %10.6f -> %4i/%5i (%10i)' % (sname, npresjet, mcstat, npresjet/mcstat, tfns, nfns, evsfromtfns)    #(lep %12.0f -> %10.6f)' % (, npreslep, npreslep/mcstat)
