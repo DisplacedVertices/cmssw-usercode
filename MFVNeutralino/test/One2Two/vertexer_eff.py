ea64deab9f3dcf37f6d793f9b060a3c0ffbf293f
Jordan Tucker <jordantucker@gmail.com>
2018-11-13 18:16:24 -0600

    do both ntkseeds True and False from one execution of script; keep histograms around instead of saving/reloading from file; maybe introduce a bunch of bugs while refactoring

--- MFVNeutralino/test/One2Two/vertexer_eff.py
+++ MFVNeutralino/test/One2Two/vertexer_eff.py
@@ -2,21 +2,11 @@ import os
 from JMTucker.Tools.ROOTTools import *
 
 is_mc = True
-version = 'v20m'
-path = '/uscms_data/d2/tucker/crab_dirs/VertexerPairEffs' + version.capitalize()
+version = 'v21mp1'
+path = '/uscms_data/d2/tucker/crab_dirs/________________wo_neg_weights/VertexerPairEffs' + version.capitalize()
 year = '2017'
-ntkseeds = False
 
 set_style()
-ps = plot_saver(plot_dir('VertexerPairEffs%s/vertexer_pair_effs%s_%s%s' % (version.capitalize(), '' if is_mc else '_data', year, '_ntkseeds' if ntkseeds else '')), size=(700,700), log=False)
-
-if is_mc:
-    in_fn = os.path.join(path, 'background_%s.root' % year)
-else:
-    in_fn = os.path.join(path, 'JetHT%s.root' % year)
-
-f = ROOT.TFile(in_fn)
-fh = ROOT.TFile('vpeffs%s_%s_%s%s.root' % ('' if is_mc else '_data', year, version, '_ntkseeds' if ntkseeds else ''), 'recreate')
 
 def write(font, size, x, y, text):
     w = ROOT.TLatex()
@@ -26,94 +16,93 @@ def write(font, size, x, y, text):
     w.DrawLatex(x, y, text)
     return w
 
-for itk in [3,4,5]:
-    h_merge = f.Get('mfvVertexerPairEffs%s/h_merge_d2d_mintk0_maxtk%i' % ('%iTkSeed' % itk if ntkseeds else '', itk))
-    h_pairs = f.Get('mfvVertexerPairEffs%s/h_pairs_d2d_mintk0_maxtk%i' % ('%iTkSeed' % itk if ntkseeds else '', itk))
-    h_erase = f.Get('mfvVertexerPairEffs%s/h_erase_d2d_mintk0_maxtk%i' % ('%iTkSeed' % itk if ntkseeds else '', itk))
+in_fn = os.path.join(path, ('background_%s.root' if is_mc else 'JetHT%s.root') % year)
+in_f = ROOT.TFile(in_fn)
 
-    h_merge.Rebin(10)
-    h_pairs.Rebin(10)
-    h_erase.Rebin(10)
+for ntkseeds in True, False:
+    ps = plot_saver(plot_dir('VertexerPairEffs%s/vertexer_pair_effs%s_%s%s' % (version.capitalize(), '' if is_mc else '_data', year, '_ntkseeds' if ntkseeds else '')), size=(700,700), log=False)
 
-    h_add_eff = h_merge.Clone('maxtk%i' % itk)
-    h_add_eff.Add(h_erase)
-    h_add_eff.Divide(h_add_eff, h_pairs, 1,1,'B')
-    for i in range(1, h_add_eff.GetNbinsX()+2):
-        h_add_eff.SetBinContent(i, 1-h_add_eff.GetBinContent(i))
-    h_add_eff.Scale(1./h_add_eff.GetBinContent(h_add_eff.GetNbinsX()))
-    h_add_eff.SetTitle('maxtk%i;d_{VV} (cm);efficiency' % itk)
-    h_add_eff.GetYaxis().SetRangeUser(0,1.05)
-    h_add_eff.SetLineColor(ROOT.kGreen+2)
-    h_add_eff.SetStats(0)
-    h_add_eff.Draw()
+    fh = ROOT.TFile('vpeffs%s_%s_%s%s.root' % ('' if is_mc else '_data', year, version, '_ntkseeds' if ntkseeds else ''), 'recreate')
+    h_merges, h_adds = {}, {}
 
-    h_merge_eff = h_merge.Clone('maxtk%i_merge' % itk)
-    h_merge_eff.Divide(h_merge_eff, h_pairs, 1,1,'B')
-    for i in range(1, h_merge_eff.GetNbinsX()+2):
-        h_merge_eff.SetBinContent(i, 1-h_merge_eff.GetBinContent(i))
-    h_merge_eff.Scale(1./h_merge_eff.GetBinContent(h_merge_eff.GetNbinsX()))
-    h_merge_eff.SetTitle('maxtk%i merge;d_{VV} (cm);efficiency' % itk)
-    h_merge_eff.GetYaxis().SetRangeUser(0,1.05)
-    h_merge_eff.SetLineColor(ROOT.kBlue)
-    h_merge_eff.SetStats(0)
-    h_merge_eff.Draw('sames')
+    for itk in 3,4,5:
+        d = in_f.Get('mfvVertexerPairEffs%iTkSeed' % itk if ntkseeds else 'mfvVertexerPairEffs')
+        h_merge = d.Get('h_merge_d2d_mintk0_maxtk%i' % itk)
+        h_erase = d.Get('h_erase_d2d_mintk0_maxtk%i' % itk)
+        h_pairs = d.Get('h_pairs_d2d_mintk0_maxtk%i' % itk)
 
-    l = ROOT.TLegend(0.50,0.55,0.85,0.70)
-    l.AddEntry(h_merge_eff, '1 - merge / pairs')
-    l.AddEntry(h_add_eff, '1 - (merge + erase) / pairs')
-    l.SetFillColor(0)
-    l.Draw()
+        for h in h_merge, h_erase, h_pairs:
+            h.Rebin(10)
 
-    ps.save('h_eff_maxtk%i' % itk)
-    fh.cd()
-    h_add_eff.Write()
-    h_merge_eff.Write()
+        h_merges[itk] = h_merge.Clone('maxtk%i_merge' % itk)
+        h_adds[itk] = h_merge.Clone('maxtk%i' % itk)
+        h_adds[itk].Add(h_erase)
 
-fh.Close()
+        for ih,h in enumerate((h_adds[itk], h_merges[itk])):
+            is_merge = h == h_merges[itk]
+            h.Divide(h, h_pairs, 1,1,'B')
+            for i in xrange(1, h.GetNbinsX()+2):
+                h.SetBinContent(i, 1-h.GetBinContent(i))
+            h.Scale(1./h.GetBinContent(h.GetNbinsX()))
+            h.SetTitle('maxtk%i%s;d_{VV} (cm);efficiency' % (itk, ' merge' if is_merge else ''))
+            h.GetYaxis().SetRangeUser(0,1.05)
+            h.SetLineColor(ROOT.kBlue if is_merge else ROOT.kGreen+2)
+            h.SetStats(0)
+            if ih == 0: 
+                h.Draw()
+            else:
+                h.Draw('sames')
 
-f = ROOT.TFile('vpeffs%s_%s_%s%s.root' % ('' if is_mc else '_data', year, version, '_ntkseeds' if ntkseeds else ''))
+        l = ROOT.TLegend(0.50,0.55,0.85,0.70)
+        l.AddEntry(h_merges[itk], '1 - merge / pairs')
+        l.AddEntry(h_adds[itk], '1 - (merge + erase) / pairs')
+        l.SetFillColor(0)
+        l.Draw()
 
-colors = [0, 0, 0, ROOT.kRed, ROOT.kBlue, ROOT.kGreen+2]
-l = ROOT.TLegend(0.50,0.15,0.85,0.30)
-for i in [3,4,5]:
-    h = f.Get('maxtk%i'%i)
-    h.SetStats(0)
-    h.SetLineWidth(3)
-    h.SetLineColor(colors[i])
-    if i == 3:
+        ps.save('h_eff_maxtk%i' % itk)
+        fh.cd()
+        h_adds[itk].Write()
+        h_merges[itk].Write()
+
+    l = ROOT.TLegend(0.50,0.15,0.85,0.30)
+    for ih, (itk, color) in enumerate(zip((3,4,5), (ROOT.kRed, ROOT.kBlue, ROOT.kGreen+2))):
+        h = h_adds[itk]
+        h.SetLineColor(color)
+        h.SetLineWidth(3)
         h.SetTitle(';d_{VV} (cm);Efficiency')
         h.GetXaxis().SetRangeUser(0,0.4)
-        h.Draw('hist')
-    else:
-        h.Draw('hist sames')
-    l.AddEntry(h, '%i-track'%i)
-l.SetFillColor(0)
-l.Draw()
-ps.save('efficiency')
-
-if not ntkseeds:
-    h = f.Get('maxtk3')
-    h.Draw('hist')
-    if not is_mc and year == '2015p6':
-        write(61, 0.050, 0.098, 0.913, 'CMS')
-        write(52, 0.035, 0.200, 0.913, 'Preliminary')
-        write(42, 0.050, 0.560, 0.913, '38.5 fb^{-1} (13 TeV)')
-    ps.save('efficiency3')
-
-    ROOT.TH1.AddDirectory(0)
-
-    h2 = ROOT.TFile('vpeffs%s_%s_%s_ntkseeds.root' % ('' if is_mc else '_data', year, version)).Get('maxtk5')
-    if not is_mc and year == '2015p6':
-        h2.Scale(1./h2.GetMaximum())
-    h2.SetLineWidth(3)
-    h.Draw('hist')
-    h2.Draw('hist sames')
-    l = ROOT.TLegend(0.50,0.15,0.85,0.30)
-    l.AddEntry(h, 'default')
-    l.AddEntry(h2, 'variation')
+        if ih == 0:
+            h.Draw('hist')
+        else:
+            h.Draw('hist sames')
+        l.AddEntry(h, '%i-track' % itk)
+    l.SetFillColor(0)
     l.Draw()
-    if not is_mc and year == '2015p6':
-        write(61, 0.050, 0.098, 0.913, 'CMS')
-        write(52, 0.035, 0.200, 0.913, 'Preliminary')
-        write(42, 0.050, 0.560, 0.913, '38.5 fb^{-1} (13 TeV)')
-    ps.save('compare_efficiency')
+    ps.save('efficiency')
+
+    if not ntkseeds:
+        h = h_adds[3]
+        h.Draw('hist')
+        if not is_mc and year == '2015p6':
+            write(61, 0.050, 0.098, 0.913, 'CMS')
+            write(52, 0.035, 0.200, 0.913, 'Preliminary')
+            write(42, 0.050, 0.560, 0.913, '38.5 fb^{-1} (13 TeV)')
+        ps.save('efficiency3')
+
+        ROOT.TH1.AddDirectory(0)
+
+        h2 = ROOT.TFile('vpeffs%s_%s_%s_ntkseeds.root' % ('' if is_mc else '_data', year, version)).Get('maxtk5')
+        if not is_mc and year == '2015p6':
+            h2.Scale(1./h2.GetMaximum())
+        h2.SetLineWidth(3)
+        h.Draw('hist')
+        h2.Draw('hist sames')
+        l = ROOT.TLegend(0.50,0.15,0.85,0.30)
+        l.AddEntry(h, 'default')
+        l.AddEntry(h2, 'variation')
+        l.Draw()
+        if not is_mc and year == '2015p6':
+            write(61, 0.050, 0.098, 0.913, 'CMS')
+            write(52, 0.035, 0.200, 0.913, 'Preliminary')
+            write(42, 0.050, 0.560, 0.913, '38.5 fb^{-1} (13 TeV)')
+        ps.save('compare_efficiency')
