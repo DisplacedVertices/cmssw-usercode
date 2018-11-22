44d5411d2737fd3fb13af468486c67745c6c6d7a
Jordan Tucker <jordantucker@gmail.com>
2018-11-22 07:10:21 -0600

    add debug prints

--- MFVNeutralino/plugins/UnpackedCandidateTracks.cc
+++ MFVNeutralino/plugins/UnpackedCandidateTracks.cc
@@ -12,10 +12,12 @@ public:
   virtual void produce(edm::Event&, const edm::EventSetup&);
 private:
   const edm::EDGetTokenT<pat::PackedCandidateCollection> packed_candidates_token;
+  const bool debug;
 };
 
 MFVUnpackedCandidateTracks::MFVUnpackedCandidateTracks(const edm::ParameterSet& cfg)
-  : packed_candidates_token(consumes<pat::PackedCandidateCollection>(cfg.getParameter<edm::InputTag>("packed_candidates_src")))
+  : packed_candidates_token(consumes<pat::PackedCandidateCollection>(cfg.getParameter<edm::InputTag>("packed_candidates_src"))),
+    debug(cfg.getUntrackedParameter<bool>("debug", false))
 {
   produces<reco::TrackCollection>();
   produces<std::vector<size_t>>(); // map back to which packed candidate
@@ -28,12 +30,17 @@ void MFVUnpackedCandidateTracks::produce(edm::Event& event, const edm::EventSetu
   std::unique_ptr<reco::TrackCollection> tracks(new reco::TrackCollection);
   std::unique_ptr<std::vector<size_t>> which(new std::vector<size_t>);
 
+  int itk = 0;
   for (size_t i = 0, ie = packed_candidates->size(); i < ie; ++i) {
     const pat::PackedCandidate& cand = (*packed_candidates)[i];
+    if (debug) std::cout << "cand #" << i << " id " << cand.pdgId() << " pt " << cand.pt() << " eta " << cand.eta() << " charge " << cand.charge() << " hasTrackDetails? " << cand.hasTrackDetails() << " ";
     if (cand.charge() && cand.hasTrackDetails()) {
+      if (debug) std::cout << "-> track #" << itk << " pt " << cand.pseudoTrack().pt() << " eta " << cand.pseudoTrack().eta() << " min_r? " << cand.pseudoTrack().hitPattern().hasValidHitInPixelLayer(PixelSubdetector::PixelBarrel,1) << " npxlayers " << cand.pseudoTrack().hitPattern().pixelLayersWithMeasurement() << " nstlayers " << cand.pseudoTrack().hitPattern().stripLayersWithMeasurement() << " dxy " << cand.pseudoTrack().dxy() << " +- " << cand.pseudoTrack().dxyError();
       tracks->push_back(cand.pseudoTrack());
       which->push_back(i);
+      ++itk;
     }
+    if (debug) std::cout << "\n";
   }
 
   event.put(std::move(tracks));
--- MFVNeutralino/python/UnpackedCandidateTracks_cfi.py
+++ MFVNeutralino/python/UnpackedCandidateTracks_cfi.py
@@ -2,4 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 mfvUnpackedCandidateTracks = cms.EDProducer('MFVUnpackedCandidateTracks',
                                             packed_candidates_src = cms.InputTag('packedPFCandidates'),
+                                            debug = cms.untracked.bool(False),
                                             )
