1a9e35dba647dba2862edfcc87077f5a377983a4
Jordan Tucker <jordantucker@gmail.com>
2018-11-23 08:12:15 -0600

    2017, V21mV2

--- MFVNeutralino/test/TrackMover/hists.sh
+++ MFVNeutralino/test/TrackMover/hists.sh
@@ -7,27 +7,26 @@ if [[ $job < 0 ]]; then
     test=1
 fi
 
-inpathbase=root://cmseos.fnal.gov//store/user/tucker/TrackMoverV6
+inpathbase=root://cmseos.fnal.gov//store/user/tucker/TrackMoverV21mV2
 
 paths=(
 ${inpathbase}
 )
 
 fns=(
-JetHT2016B3.root
-JetHT2016C.root
-JetHT2016D.root
-JetHT2016E.root
-JetHT2016F.root
-JetHT2016G.root
-JetHT2016H.root
-qcdht1000.root
-qcdht1000_hip1p0_mit.root
-qcdht1500.root
-qcdht1500_hip1p0_mit.root
+JetHT2017B.root
+JetHT2017C.root
+JetHT2017D.root
+JetHT2017E.root
+JetHT2017F.root
+qcdht0700_2017.root
+qcdht1000_2017.root
+qcdht1500_2017.root
+qcdht2000_2017.root
+ttbar_2017.root
 )
 
-nsigs=( 4p0 4p1 )
+nsigs=( 4p0 )
 taus=( 100 300 1000 10000 30000 )
 nls=( 2 3 )
 nbs=( 0 1 2 )
@@ -63,7 +62,7 @@ nb=${nbs[$((ii % nnbs))]}
 ii=$((ii / nnbs))
 
 outfn=$(basename $path)_nsig${nsig}_tau$(printf %05i $tau)um_${nl}${nb}_$(basename $fn .root).root
-treepath=mfvMovedTree${nl}${nb}nsig${nsig}btagoldangle0p2/t
+treepath=mfvMovedTree${nl}${nb}
 
 echo path $path fn $fn nl $nl nb $nb outfn $outfn treepath $treepath
 
@@ -74,10 +73,10 @@ if [[ $test == 1 ]]; then
     exit 1
 fi
 
-export SCRAM_ARCH=slc6_amd64_gcc530
+export SCRAM_ARCH=slc6_amd64_gcc630
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-scram project CMSSW CMSSW_8_0_25 2>&1
-cd CMSSW_8_0_25/src
+scram project CMSSW CMSSW_9_4_6_patch1 2>&1
+cd CMSSW_9_4_6_patch1/src
 eval $(scram ru -sh)
 cd ../..
 
--- MFVNeutralino/test/TrackMover/hists_finish.sh
+++ MFVNeutralino/test/TrackMover/hists_finish.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-njobstot=2310
+njobstot=300
 
-function mergeqcd {
+function mergemc {
     allthere=1
-    for x in qcdht1000.root qcdht1500.root qcdht1000_hip1p0_mit.root qcdht1500_hip1p0_mit.root ; do
+    for x in qcdht0700_2017.root qcdht1000_2017.root qcdht1500_2017.root qcdht2000_2017.root ttbar_2017.root ; do
         if [[ ! -e $x ]]; then
-            echo mergeqcd: $x does not exist
+            echo mergemc: $x does not exist
             allthere=0
         fi
         if [[ allthere -ne 1 ]]; then
             return 1
         fi
     done
-    samples merge qcdht1000and1500_hip1p0_mit.root -22300 qcdht{1000,1500}_hip1p0_mit.root
-    samples merge qcdht1000and1500.root -16200 qcdht{1000,1500}.root
-    hadd.py qcdht1000and1500_hipplusno.root qcdht1000and1500_hip1p0_mit.root qcdht1000and1500.root
+    samples merge qcdht0700_2017.root qcdht1000_2017.root qcdht1500_2017.root qcdht2000_2017.root ttbar_2017.root -41530 background_2017.root
 }
 
 function mergedata {
     allthere=1
-    for x in JetHT2016{B3,C,D,E,F,G,H}.root ; do
+    for x in JetHT2017{B,C,D,E,F}.root ; do
         if [[ ! -e $x ]]; then
             echo mergedata: $x does not exist
             allthere=0
@@ -29,8 +27,7 @@ function mergedata {
             return 1
         fi
     done
-    #python $t16/utilities.py hadd_data
-    hadd.py JetHT2016.root JetHT2016{B3,C,D,E,F,G,H}.root # just need it all in one for now
+    hadd.py JetHT2017.root JetHT2017{B,C,D,E,F}.root # just need it all in one for now
 }
 
 if [[ $1 == "check" ]]; then
@@ -56,29 +53,29 @@ elif [[ $1 == "finish" ]]; then
         if [[ $(basename $d) != bin ]]; then
             echo $d
             cd $d
-            mergeqcd
+            mergemc
             mergedata
             cd - >/dev/null
             echo
         fi
     done
 
-elif [[ $1 == "mergeqcd" ]]; then
-    mergeqcd
+elif [[ $1 == "mergemc" ]]; then
+    mergemc
 
 elif [[ $1 == "mergedata" ]]; then
     mergedata
 
 elif [[ $1 == "allthere" ]]; then
     for d in $(find . -type d -links 2) ; do
-        for x in qcdht1000.root qcdht1500.root qcdht1000_hip1p0_mit.root qcdht1500_hip1p0_mit.root JetHT2016{B3,C,D,E,F,G,H}.root ; do
+        for x in qcdht0700_2017.root qcdht1000_2017.root qcdht1500_2017.root qcdht2000_2017.root ttbar_2017.root JetHT2017{B,C,D,E,F}.root ; do
             if [[ ! -e $d/$x ]]; then
                 echo $d no $x
             fi
         done
-        for x in qcdht1000and1500_hip1p0_mit.root qcdht1000and1500.root qcdht1000and1500_hipplusno.root ; do
+        for x in background_2017.root ; do
             if [[ ! -e $d/$x ]]; then
-                echo $d needs mergeqcd
+                echo $d needs mergemc
             fi
         done
     done
