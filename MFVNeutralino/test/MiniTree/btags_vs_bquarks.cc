d22ccd71dabac06967ce75dc4e763be9c18d828c
Jennifer Chu <jyc72@cornell.edu>
2018-12-12 17:36:42 -0600

    Better description of array indices.

--- MFVNeutralino/test/MiniTree/btags_vs_bquarks.cc
+++ MFVNeutralino/test/MiniTree/btags_vs_bquarks.cc
@@ -8,8 +8,14 @@
 #include "JMTucker/MFVNeutralino/interface/MiniNtuple.h"
 
 const int NVTX = 2;
+const char* vtx_names[NVTX] = {"one-vertex", "two-vertex"};
+
 const int NBTAGS = 2;
+const int nbtag_mins[NBTAGS] = {1, 2};
+
 const int NBDISC = 3;
+const char* bdisc_names[NBDISC] = {"loose", "medium", "tight"};
+const double bdisc_mins[NBDISC] = {0.5803, 0.8838, 0.9693};
 
 TH1F* h_bquark_flavor_code[NVTX] = {0};
 TH1F* h_njets[NVTX] = {0};
@@ -17,8 +23,6 @@ TH1F* h_jet_bdisc[NVTX] = {0};
 TH1F* h_jet_bdisc_bquarks[NVTX] = {0};
 TH1F* h_jet_bdisc_nobquarks[NVTX] = {0};
 
-const char* b_discriminator_wps[NBDISC] = {"loose", "medium", "tight"};
-const double b_discriminator_mins[NBDISC] = {0.5803, 0.8838, 0.9693};
 TH1F* h_nbtags[NVTX][NBDISC] = {{0}};
 TH2F* h_nbtags_vs_bquark_flavor_code[NVTX][NBDISC] = {{0}};
 TH1F* h_btag_flavor_code[NVTX][NBTAGS][NBDISC] = {{{0}}};
@@ -40,22 +44,22 @@ TH1F* h_1v_dbv_nobtag_nobquarks[NBTAGS][NBDISC] = {{0}};
 
 void book_hists(int ntk) {
   for (int k = 0; k < NVTX; ++k) {
-    h_bquark_flavor_code[k] = new TH1F(TString::Format("h_%dv_bquark_flavor_code", k+1), TString::Format("%d-track %d-vertex events;bquark_flavor_code;Events", ntk, k+1), 2, 0, 2);
-    h_njets[k] = new TH1F(TString::Format("h_%dv_njets", k+1), TString::Format("%d-track %d-vertex events;number of jets;Events", ntk, k+1), 40, 0, 40);
-    h_jet_bdisc[k] = new TH1F(TString::Format("h_%dv_jet_bdisc", k+1), TString::Format("%d-track %d-vertex events;jet bdisc;Number of jets", ntk, k+1), 100, 0, 1);
-    h_jet_bdisc_bquarks[k] = new TH1F(TString::Format("h_%dv_jet_bdisc_bquarks", k+1), TString::Format("%d-track %d-vertex events with b quarks;jet bdisc;Number of jets", ntk, k+1), 100, 0, 1);
-    h_jet_bdisc_nobquarks[k] = new TH1F(TString::Format("h_%dv_jet_bdisc_nobquarks", k+1), TString::Format("%d-track %d-vertex events without b quarks;jet bdisc;Number of jets", ntk, k+1), 100, 0, 1);
+    h_bquark_flavor_code[k] = new TH1F(TString::Format("h_%dv_bquark_flavor_code", k+1), TString::Format("%d-track %s events;bquark_flavor_code;Events", ntk, vtx_names[k]), 2, 0, 2);
+    h_njets[k] = new TH1F(TString::Format("h_%dv_njets", k+1), TString::Format("%d-track %s events;number of jets;Events", ntk, vtx_names[k]), 40, 0, 40);
+    h_jet_bdisc[k] = new TH1F(TString::Format("h_%dv_jet_bdisc", k+1), TString::Format("%d-track %s events;jet bdisc;Number of jets", ntk, vtx_names[k]), 100, 0, 1);
+    h_jet_bdisc_bquarks[k] = new TH1F(TString::Format("h_%dv_jet_bdisc_bquarks", k+1), TString::Format("%d-track %s events with b quarks;jet bdisc;Number of jets", ntk, vtx_names[k]), 100, 0, 1);
+    h_jet_bdisc_nobquarks[k] = new TH1F(TString::Format("h_%dv_jet_bdisc_nobquarks", k+1), TString::Format("%d-track %s events without b quarks;jet bdisc;Number of jets", ntk, vtx_names[k]), 100, 0, 1);
 
     for (int i = 0; i < NBDISC; ++i) {
-      h_nbtags[k][i] = new TH1F(TString::Format("h_%dv_nbtags_%s", k+1, b_discriminator_wps[i]), TString::Format("%d-track %d-vertex events;number of %s btags;Events", ntk, k+1, b_discriminator_wps[i]), 20, 0, 20);
-      h_nbtags_vs_bquark_flavor_code[k][i] = new TH2F(TString::Format("h_%dv_nbtags_%s_vs_bquark_flavor_code", k+1, b_discriminator_wps[i]), TString::Format("%d-track %d-vertex events;bquark_flavor_code;number of %s btags", ntk, k+1, b_discriminator_wps[i]), 2, 0, 2, 20, 0, 20);
+      h_nbtags[k][i] = new TH1F(TString::Format("h_%dv_nbtags_%s", k+1, bdisc_names[i]), TString::Format("%d-track %s events;number of %s btags;Events", ntk, vtx_names[k], bdisc_names[i]), 20, 0, 20);
+      h_nbtags_vs_bquark_flavor_code[k][i] = new TH2F(TString::Format("h_%dv_nbtags_%s_vs_bquark_flavor_code", k+1, bdisc_names[i]), TString::Format("%d-track %s events;bquark_flavor_code;number of %s btags", ntk, vtx_names[k], bdisc_names[i]), 2, 0, 2, 20, 0, 20);
       for (int j = 0; j < NBTAGS; ++j) {
-        h_btag_flavor_code[k][j][i] = new TH1F(TString::Format("h_%dv_%d%s_btag_flavor_code", k+1, j+1, b_discriminator_wps[i]), TString::Format("%d-track %d-vertex events;%d%s_btag_flavor_code;Events", ntk, k+1, j+1, b_discriminator_wps[i]), 2, 0, 2);
-        h_btag_flavor_code_vs_bquark_flavor_code[k][j][i] = new TH2F(TString::Format("h_%dv_%d%s_btag_flavor_code_vs_bquark_flavor_code", k+1, j+1, b_discriminator_wps[i]), TString::Format("%d-track %d-vertex events;bquark_flavor_code;%d%s_btag_flavor_code", ntk, k+1, j+1, b_discriminator_wps[i]), 2, 0, 2, 2, 0, 2);
-        h_btag_flavor_code_bquarks[k][j][i] = new TH1F(TString::Format("h_%dv_%d%s_btag_flavor_code_bquarks", k+1, j+1, b_discriminator_wps[i]), TString::Format("%d-track %d-vertex events with b quarks;%d%s_btag_flavor_code;Events", ntk, k+1, j+1, b_discriminator_wps[i]), 2, 0, 2);
-        h_btag_flavor_code_nobquarks[k][j][i] = new TH1F(TString::Format("h_%dv_%d%s_btag_flavor_code_nobquarks", k+1, j+1, b_discriminator_wps[i]), TString::Format("%d-track %d-vertex events without b quarks;%d%s_btag_flavor_code;Events", ntk, k+1, j+1, b_discriminator_wps[i]), 2, 0, 2);
-        h_bquark_flavor_code_btag[k][j][i] = new TH1F(TString::Format("h_%dv_bquark_flavor_code_%d%s_btag", k+1, j+1, b_discriminator_wps[i]), TString::Format("%d-track %d-vertex events with #geq%d %s btag;bquark_flavor_code;Events", ntk, k+1, j+1, b_discriminator_wps[i]), 2, 0, 2);
-        h_bquark_flavor_code_nobtag[k][j][i] = new TH1F(TString::Format("h_%dv_bquark_flavor_code_%d%s_nobtag", k+1, j+1, b_discriminator_wps[i]), TString::Format("%d-track %d-vertex events without #geq%d %s btag;bquark_flavor_code;Events", ntk, k+1, j+1, b_discriminator_wps[i]), 2, 0, 2);
+        h_btag_flavor_code[k][j][i] = new TH1F(TString::Format("h_%dv_%d%s_btag_flavor_code", k+1, nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track %s events;%d%s_btag_flavor_code;Events", ntk, vtx_names[k], nbtag_mins[j], bdisc_names[i]), 2, 0, 2);
+        h_btag_flavor_code_vs_bquark_flavor_code[k][j][i] = new TH2F(TString::Format("h_%dv_%d%s_btag_flavor_code_vs_bquark_flavor_code", k+1, nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track %s events;bquark_flavor_code;%d%s_btag_flavor_code", ntk, vtx_names[k], nbtag_mins[j], bdisc_names[i]), 2, 0, 2, 2, 0, 2);
+        h_btag_flavor_code_bquarks[k][j][i] = new TH1F(TString::Format("h_%dv_%d%s_btag_flavor_code_bquarks", k+1, nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track %s events with b quarks;%d%s_btag_flavor_code;Events", ntk, vtx_names[k], nbtag_mins[j], bdisc_names[i]), 2, 0, 2);
+        h_btag_flavor_code_nobquarks[k][j][i] = new TH1F(TString::Format("h_%dv_%d%s_btag_flavor_code_nobquarks", k+1, nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track %s events without b quarks;%d%s_btag_flavor_code;Events", ntk, vtx_names[k], nbtag_mins[j], bdisc_names[i]), 2, 0, 2);
+        h_bquark_flavor_code_btag[k][j][i] = new TH1F(TString::Format("h_%dv_bquark_flavor_code_%d%s_btag", k+1, nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track %s events with #geq%d %s btag;bquark_flavor_code;Events", ntk, vtx_names[k], nbtag_mins[j], bdisc_names[i]), 2, 0, 2);
+        h_bquark_flavor_code_nobtag[k][j][i] = new TH1F(TString::Format("h_%dv_bquark_flavor_code_%d%s_nobtag", k+1, nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track %s events without #geq%d %s btag;bquark_flavor_code;Events", ntk, vtx_names[k], nbtag_mins[j], bdisc_names[i]), 2, 0, 2);
       }
     }
   }
@@ -65,12 +69,12 @@ void book_hists(int ntk) {
   h_1v_dbv_nobquarks = new TH1F("h_1v_dbv_nobquarks", TString::Format("%d-track one-vertex events without b quarks;d_{BV} (cm);Events", ntk), 40, 0, 0.2);
   for (int i = 0; i < NBDISC; ++i) {
     for (int j = 0; j < NBTAGS; ++j) {
-      h_1v_dbv_btag[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_btag", j+1, b_discriminator_wps[i]), TString::Format("%d-track one-vertex events with #geq%d %s btag;d_{BV} (cm);Events", ntk, j+1, b_discriminator_wps[i]), 40, 0, 0.2);
-      h_1v_dbv_btag_bquarks[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_btag_bquarks", j+1, b_discriminator_wps[i]), TString::Format("%d-track one-vertex events with b quarks and with #geq%d %s btag;d_{BV} (cm);Events", ntk, j+1, b_discriminator_wps[i]), 40, 0, 0.2);
-      h_1v_dbv_btag_nobquarks[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_btag_nobquarks", j+1, b_discriminator_wps[i]), TString::Format("%d-track one-vertex events without b quarks and with #geq%d %s btag;d_{BV} (cm);Events", ntk, j+1, b_discriminator_wps[i]), 40, 0, 0.2);
-      h_1v_dbv_nobtag[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_nobtag", j+1, b_discriminator_wps[i]), TString::Format("%d-track one-vertex events without #geq%d %s btag;d_{BV} (cm);Events", ntk, j+1, b_discriminator_wps[i]), 40, 0, 0.2);
-      h_1v_dbv_nobtag_bquarks[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_nobtag_bquarks", j+1, b_discriminator_wps[i]), TString::Format("%d-track one-vertex events with b quarks and without #geq%d %s btag;d_{BV} (cm);Events", ntk, j+1, b_discriminator_wps[i]), 40, 0, 0.2);
-      h_1v_dbv_nobtag_nobquarks[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_nobtag_nobquarks", j+1, b_discriminator_wps[i]), TString::Format("%d-track one-vertex events without b quarks and without #geq%d %s btag;d_{BV} (cm);Events", ntk, j+1, b_discriminator_wps[i]), 40, 0, 0.2);
+      h_1v_dbv_btag[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_btag", nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track one-vertex events with #geq%d %s btag;d_{BV} (cm);Events", ntk, nbtag_mins[j], bdisc_names[i]), 40, 0, 0.2);
+      h_1v_dbv_btag_bquarks[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_btag_bquarks", nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track one-vertex events with b quarks and with #geq%d %s btag;d_{BV} (cm);Events", ntk, nbtag_mins[j], bdisc_names[i]), 40, 0, 0.2);
+      h_1v_dbv_btag_nobquarks[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_btag_nobquarks", nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track one-vertex events without b quarks and with #geq%d %s btag;d_{BV} (cm);Events", ntk, nbtag_mins[j], bdisc_names[i]), 40, 0, 0.2);
+      h_1v_dbv_nobtag[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_nobtag", nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track one-vertex events without #geq%d %s btag;d_{BV} (cm);Events", ntk, nbtag_mins[j], bdisc_names[i]), 40, 0, 0.2);
+      h_1v_dbv_nobtag_bquarks[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_nobtag_bquarks", nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track one-vertex events with b quarks and without #geq%d %s btag;d_{BV} (cm);Events", ntk, nbtag_mins[j], bdisc_names[i]), 40, 0, 0.2);
+      h_1v_dbv_nobtag_nobquarks[j][i] = new TH1F(TString::Format("h_1v_dbv_%d%s_nobtag_nobquarks", nbtag_mins[j], bdisc_names[i]), TString::Format("%d-track one-vertex events without b quarks and without #geq%d %s btag;d_{BV} (cm);Events", ntk, nbtag_mins[j], bdisc_names[i]), 40, 0, 0.2);
     }
   }
 }
@@ -79,7 +83,7 @@ bool analyze(long long j, long long je, const mfv::MiniNtuple& nt) {
   double w = nt.weight;
 
   if (nt.nvtx >= 1) {
-    int k = nt.nvtx == 1 ? 0 : 1;
+    int k = nt.nvtx > NVTX ? NVTX-1 : nt.nvtx-1;
 
     int bquark_flavor_code = nt.gen_flavor_code == 2 ? 1 : 0;
     h_bquark_flavor_code[k]->Fill(bquark_flavor_code, w);
@@ -99,7 +103,7 @@ bool analyze(long long j, long long je, const mfv::MiniNtuple& nt) {
       if (bquark_flavor_code) h_jet_bdisc_bquarks[k]->Fill(bdisc, w);
       if (!bquark_flavor_code) h_jet_bdisc_nobquarks[k]->Fill(bdisc, w);
       for (int i = 0; i < NBDISC; ++i) {
-        if (bdisc >= b_discriminator_mins[i]) {
+        if (bdisc >= bdisc_mins[i]) {
           ++nbtags[i];
         }
       }
@@ -110,7 +114,7 @@ bool analyze(long long j, long long je, const mfv::MiniNtuple& nt) {
       h_nbtags_vs_bquark_flavor_code[k][i]->Fill(bquark_flavor_code, nbtags[i], w);
 
       for (int j = 0; j < NBTAGS; ++j) {
-        int btag_flavor_code = nbtags[i] >= j+1 ? 1 : 0;
+        int btag_flavor_code = nbtags[i] >= nbtag_mins[j] ? 1 : 0;
         h_btag_flavor_code[k][j][i]->Fill(btag_flavor_code, w);
         h_btag_flavor_code_vs_bquark_flavor_code[k][j][i]->Fill(bquark_flavor_code, btag_flavor_code, w);
         if (bquark_flavor_code) h_btag_flavor_code_bquarks[k][j][i]->Fill(btag_flavor_code, w);
