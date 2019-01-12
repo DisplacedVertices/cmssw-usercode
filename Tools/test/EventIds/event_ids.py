bef285fe5383c4b22773557db198639aeffd6da3
Jordan Tucker <jordantucker@gmail.com>
2019-01-11 22:34:44 -0600

    qcd_samples_2018 (need to go back and check these and ttbarht* for halfmc working)

--- Tools/test/EventIds/event_ids.py
+++ Tools/test/EventIds/event_ids.py
@@ -17,12 +17,12 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     if year == 2017:
         samples = Samples.qcd_samples_2017 + Samples.ttbar_samples_2017 + Samples.leptonic_samples_2017 + Samples.all_signal_samples_2017 + Samples.data_samples_2017
     else:
-        samples = Samples.data_samples_2018
+        samples = Samples.qcd_samples_2018 + Samples.data_samples_2018
 
-    dataset = 'ntuplev22m'
+    dataset = 'miniaod'
     samples = [s for s in samples if s.has_dataset(dataset)]
     set_splitting(samples, dataset, 'default', default_files_per=50)
 
-    ms = MetaSubmitter('EventIdsV22m', dataset=dataset)
+    ms = MetaSubmitter('EventIds_MiniAOD', dataset=dataset)
     ms.condor.stageout_files = 'all'
     ms.submit(samples)
