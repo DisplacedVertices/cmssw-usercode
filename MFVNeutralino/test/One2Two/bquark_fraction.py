c1b588c7d21a204ca31c20cdbb4481186961d2b1
Jennifer Chu <jyc72@cornell.edu>
2019-01-28 16:54:42 -0600

    Calculate the two-vertex b quark fraction using the one-vertex btag fraction.

--- MFVNeutralino/test/One2Two/bquark_fraction.py
+++ MFVNeutralino/test/One2Two/bquark_fraction.py
@@ -30,7 +30,16 @@ def print_f2(ntk,f0,f1,cb,cbbar):
     print 'ntk = %d: f0 = %.3f, f1 = %.3f, cb/cbbar = %.3f/%.3f = %.2f, nb/nbbar = %.2f, effb/effbbar = %.1f, f2 = %.2f' % (ntk, f0, f1, cb, cbbar, c(cb,cbbar), n(f0), e(f0,f1), f2(f0,f1,cb,cbbar))
 
 if __name__ == '__main__':
+    print 'f0,f1,cb,cbbar from sorting events by b quarks'
     print_f2(3, 0.176, 0.461, 0.584, 0.547)
     print_f2(7, 0.176, 0.463, 0.574, 0.532)
     print_f2(4, 0.176, 0.487, 0.563, 0.516)
     print_f2(5, 0.176, 0.549, 0.535, 0.493)
+    print
+
+    print 'f0,f1,cb,cbbar from sorting events by at least 1 medium btag'
+    print_f2(3, 0.199, 0.522, 0.580, 0.547)
+    print_f2(7, 0.199, 0.525, 0.568, 0.535)
+    print_f2(4, 0.199, 0.557, 0.555, 0.521)
+    print_f2(5, 0.199, 0.536, 0.534, 0.494)
+    print
