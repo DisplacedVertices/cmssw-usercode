dc9304676b6d1220ec760fd505ac7ec1abfa7ebd
Jordan Tucker <jordantucker@gmail.com>
2019-01-28 19:56:50 -0600

    batch jobs need the Tools lib; change out fn to end with .txt to be distinct from condor logs

--- MFVNeutralino/test/One2Two/statmodel.sh
+++ MFVNeutralino/test/One2Two/statmodel.sh
@@ -10,6 +10,8 @@ cd /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw-patch/CMSSW_9_4_6_patch1
 eval `scramv1 runtime -sh`
 cd $wd
 
+export LD_LIBRARY_PATH=$(pwd):${LD_LIBRARY_PATH}
+
 samples_index=$((job/9))
 year_index=$(((job%9)/3))
 ntracks=$((3+job%3))
@@ -17,4 +19,4 @@ echo samples_index $samples_index year_index $year_index ntracks $ntracks
 
 out_fn=sm_samples${samples_index}_year${year_index}_ntk${ntracks}
 
-env sm_out_fn=${out_fn} sm_samples_index=${samples_index} sm_year_index=${year_index} sm_ntracks=${ntracks} ./statmodel.exe 2>&1 > ${out_fn}.log
+env sm_out_fn=${out_fn} sm_samples_index=${samples_index} sm_year_index=${year_index} sm_ntracks=${ntracks} ./statmodel.exe 2>&1 > ${out_fn}.txt
