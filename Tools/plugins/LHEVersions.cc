79a7027412a72eb2f7ce37a32e14037b5152adaf
Jordan Tucker <jordantucker@gmail.com>
2019-01-29 14:53:55 -0600

    make it work with 94x (begin->endRun, consumes token); dump header tags and comments too

--- Tools/plugins/LHEVersions.cc
+++ Tools/plugins/LHEVersions.cc
@@ -7,29 +7,39 @@
 class LHEVersions : public edm::EDAnalyzer {
 public:
   explicit LHEVersions(const edm::ParameterSet&);
-
 private:
-  const bool endls;
+  virtual void endRun(const edm::Run&, const edm::EventSetup&) override;
+  virtual void analyze(const edm::Event&, const edm::EventSetup&) override {}
 
-  virtual void beginRun(const edm::Run&, const edm::EventSetup&);
-  virtual void analyze(const edm::Event&, const edm::EventSetup&) {}
+  edm::EDGetTokenT<LHERunInfoProduct> token;
+  const std::string sep;
 };
 
 LHEVersions::LHEVersions(const edm::ParameterSet& cfg)
-  : endls(cfg.getUntrackedParameter<bool>("endls", false))
-{
-}
+  : token(consumes<LHERunInfoProduct, edm::InRun>(edm::InputTag("externalLHEProducer"))),
+    sep("\n================================================================================\n")
+{}
 
-void LHEVersions::beginRun(const edm::Run& run, edm::EventSetup const&) {
+void LHEVersions::endRun(const edm::Run& run, edm::EventSetup const&) {
+  std::cout << "LHEVersions::beginRun run " << run.id().run() << "\n";
+
   edm::Handle<LHERunInfoProduct> product;
-  run.getByLabel("source", product);
+  run.getByToken(token, product);
 
   int count = 0;
   for (LHERunInfoProduct::headers_const_iterator it = product->headers_begin(); it != product->headers_end(); ++it, ++count) {
-    std::cout << "HEADER " << count << std::endl;
-    for (std::vector<std::string>::const_iterator it2 = it->begin(); it2 != it->end(); ++it2)
-      std::cout << *it2;
+    std::cout << sep << "LHE header #" << count << " tag " << it->tag() << " lines:\n";
+    for (auto line : it->lines())
+      std::cout << line;
   }
+
+  if (product->comments_size()) {
+    count = 0;
+    for (LHERunInfoProduct::comments_const_iterator it = product->comments_begin(); it != product->comments_end(); ++it, ++count)
+      std::cout << sep << "LHE comment #" << count << " " << *it;
+  }
+  else
+    std::cout << sep << "LHE comments empty\n";
 }
 
 DEFINE_FWK_MODULE(LHEVersions);
