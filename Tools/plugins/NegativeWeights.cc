154f22aaedbc683da07f5cd2ad38108f224c3b3a
Jordan Tucker <jordantucker@gmail.com>
2019-01-30 10:36:13 -0600

    h_nweights

--- Tools/plugins/NegativeWeights.cc
+++ Tools/plugins/NegativeWeights.cc
@@ -16,6 +16,7 @@ private:
 
   const edm::EDGetTokenT<GenEventInfoProduct> gen_info_token;
 
+  TH1D* h_nweights;
   TH1D* h_weight_eq_product;
   TH1D* h_weight_sign;
 };
@@ -26,6 +27,7 @@ JMTNegativeWeights::JMTNegativeWeights(const edm::ParameterSet& cfg)
   edm::Service<TFileService> fs;
   TH1::SetDefaultSumw2();
 
+  h_nweights = fs->make<TH1D>("h_nweights", "", 100, 0, 100);
   h_weight_eq_product = fs->make<TH1D>("h_weight_eq_product", "", 2, 0, 2);
   h_weight_sign = fs->make<TH1D>("h_weight_sign", "", 2, 0, 2);
 }
@@ -37,6 +39,7 @@ void JMTNegativeWeights::analyze(const edm::Event& event, const edm::EventSetup&
   edm::Handle<GenEventInfoProduct> gen_info;
   event.getByToken(gen_info_token, gen_info);
 
+  h_nweights->Fill(gen_info->weights().size());
   h_weight_eq_product->Fill(fabs(gen_info->weight() - gen_info->weightProduct()) / gen_info->weight() < 1e-3);
   h_weight_sign->Fill(gen_info->weight() > 0);
 }
