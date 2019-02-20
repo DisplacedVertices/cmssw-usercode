fd2c3c5bfa6cf73600d3df9af1b6b5e974654f2f
Jennifer Chu <jyc72@cornell.edu>
2019-02-20 15:38:22 -0600

    s=1: assume the probability of finding two vertices is the one-vertex efficiency squared; s=2: assume all vertices in events with b quarks are reconstructed from b quarks.

--- MFVNeutralino/test/One2Two/bquark_fraction.py
+++ MFVNeutralino/test/One2Two/bquark_fraction.py
@@ -30,16 +30,30 @@ def print_f2(ntk,f0,f1,cb,cbbar,s):
     print 'ntk = %d: f0 = %.3f, f1 = %.3f, cb/cbbar = %.3f/%.3f = %.2f, nb/nbbar = %.2f, effb/effbbar = %.1f, f2 = %.2f' % (ntk, f0, f1, cb, cbbar, c(cb,cbbar), n(f0), e(f0,f1), f2(f0,f1,cb,cbbar,s))
 
 if __name__ == '__main__':
-    print 'f0,f1,cb,cbbar from sorting events by b quarks'
+    print 'f0,f1,cb,cbbar from sorting events by b quarks; assume the probability of finding two vertices is the one-vertex efficiency squared (s=1)'
     print_f2(3, 0.176, 0.461, 0.584, 0.547, 1)
     print_f2(7, 0.176, 0.463, 0.574, 0.532, 1)
     print_f2(4, 0.176, 0.487, 0.563, 0.516, 1)
     print_f2(5, 0.176, 0.549, 0.535, 0.493, 1)
     print
 
-    print 'f0,f1,cb,cbbar from sorting events by at least 1 medium btag'
+    print 'f0,f1,cb,cbbar from sorting events by at least 1 medium btag; assume the probability of finding two vertices is the one-vertex efficiency squared (s=1)'
     print_f2(3, 0.199, 0.522, 0.580, 0.547, 1)
     print_f2(7, 0.199, 0.525, 0.568, 0.535, 1)
     print_f2(4, 0.199, 0.557, 0.555, 0.521, 1)
     print_f2(5, 0.199, 0.536, 0.534, 0.494, 1)
+    print
+
+    print 'f0,f1,cb,cbbar from sorting events by b quarks; assume all vertices in events with b quarks are reconstructed from b quarks (s=2)'
+    print_f2(3, 0.176, 0.461, 0.584, 0.547, 2)
+    print_f2(7, 0.176, 0.463, 0.574, 0.532, 2)
+    print_f2(4, 0.176, 0.487, 0.563, 0.516, 2)
+    print_f2(5, 0.176, 0.549, 0.535, 0.493, 2)
+    print
+
+    print 'f0,f1,cb,cbbar from sorting events by at least 1 medium btag; assume all vertices in events with b quarks are reconstructed from b quarks (s=2)'
+    print_f2(3, 0.199, 0.522, 0.580, 0.547, 2)
+    print_f2(7, 0.199, 0.525, 0.568, 0.535, 2)
+    print_f2(4, 0.199, 0.557, 0.555, 0.521, 2)
+    print_f2(5, 0.199, 0.536, 0.534, 0.494, 2)
     print
