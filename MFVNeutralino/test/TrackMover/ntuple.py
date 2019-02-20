a4bb9b2a499edf6e91a46c7e9120bfe747f93e09
Jordan Tucker <jordantucker@gmail.com>
2019-02-20 17:00:36 -0600

    run over 2018 qcd

--- MFVNeutralino/test/TrackMover/ntuple.py
+++ MFVNeutralino/test/TrackMover/ntuple.py
@@ -108,7 +108,7 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     if year == 2017:
         samples = Samples.data_samples_2017 + Samples.ttbar_samples_2017 + Samples.qcd_samples_2017
     elif year == 2018:
-        samples = Samples.data_samples_2018
+        samples = Samples.data_samples_2018 + Samples.qcd_samples_2018
 
     #samples = [s for s in samples if s.has_dataset(dataset)]
     set_splitting(samples, dataset, 'trackmover', data_json=json_path('ana_2017p8.json'))
