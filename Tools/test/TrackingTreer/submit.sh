ac61c49dcfcdf62f7cf6a9ef3406ce3453e03cba
Jordan Tucker <jordantucker@gmail.com>
2019-04-02 09:43:34 -0500

    work with V23mv3

--- Tools/test/TrackingTreer/submit.sh
+++ Tools/test/TrackingTreer/submit.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-wd=$(pwd)
 batch=$1
 job=$2
 out_dir=$3
 
+wd=$(pwd)
 export SCRAM_ARCH=slc6_amd64_gcc630
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 scram project CMSSW CMSSW_9_4_6_patch1 2>/dev/null >/dev/null
-cd CMSSW_9_4_6_patch1/src
+cd CMSSW_9_4_6_patch1
+tar xf $wd/libs.tgz
 eval $(scram ru -sh)
 cd $wd
 
 in_fn=$(sed -n "$((job+1))p" ${batch}.txt)
 out_fn=${batch}_${job}.root
 
-./hists.exe -i $in_fn -o $out_fn
+cmd="./hists.exe -i $in_fn -o $out_fn"
+echo $cmd
+$cmd
 exit_code=$?
 if [[ $exit_code == 0 ]]; then
     xrdcp -s $out_fn root://cmseos.fnal.gov/${out_dir}
