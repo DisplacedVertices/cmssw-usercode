5bf843c46b6deac5efef3790ae4d386001d33bbf
Jordan Tucker <jordantucker@gmail.com>
2019-04-04 16:46:54 -0500

    flesh out

--- MFVNeutralino/test/K0Tracks/bkgsub.py
+++ MFVNeutralino/test/K0Tracks/bkgsub.py
@@ -1,34 +1,25 @@
 import sys, os
 from JMTucker.Tools.ROOTTools import *
-from JMTucker.Tools.general import bool_from_argv
 set_style()
 
-clobber = bool_from_argv('clobber')
 in_fn = sys.argv[1]
-ex = sys.argv[2] if len(sys.argv) >= 3 else ''
-if ex == 'default':
-    ex = ''
-out_fn = os.path.basename(in_fn)
-if in_fn == out_fn or (os.path.exists(out_fn) and not clobber):
-    raise IOError('refusing to clobber existing file %s' % out_fn)
-
+os.mkdir('bkgsub')
+out_fn = os.path.join('bkgsub', os.path.basename(in_fn))
 sample = out_fn.replace('.root', '')
 
-ps = plot_saver(plot_dir('v0bkgsub/!%s/' % ('default' if not ex else ex)  + sample), size=(600,600))
+ps = plot_saver(plot_dir('v0bkgsub_%s' % sample), size=(600,600))
 
 # fit for s and b using sidebands
 
 in_f = ROOT.TFile(in_fn)
-
-h = in_f.Get('v0eff%s/h_nvtx_pass' % ex)
-h.Draw()
+in_f.Get('massall/h_nvtx').Draw()
 ps.save('nvtx')
 
-h = in_f.Get('v0eff%s/K0_2pi/h_vtx_mass' % ex)
+h = in_f.Get('massall/h_mass')
 
-# must keep these numbers in sync with histos!
-fit_range = 0.42, 0.6 
-fit_exclude = 0.46, 0.54
+# must keep these numbers in sync
+fit_range = 0.420, 0.580
+fit_exclude = 0.490, 0.505
 
 npars = 3 # 2 if sample.startswith('ZeroBias') else 3
 while 1:
@@ -112,7 +103,7 @@ ps.save('mass_fit')
 del insert
 
 # scan mass window for best yield, purity, and significance z = s/sqrt(b + sigb^2)
-# -- but mass window is fixed in histos to 490-505 MeV, so if you want to change it you have to do it there and rerun histos
+# -- but mass window is now fixed in histos to 490-505 MeV, so if you want to change it you have to do it there and rerun hists
 
 xax = h.GetXaxis()
 ibinc = xax.FindBin(0.4976)
@@ -151,37 +142,30 @@ assert abs(xlo-0.490) < 1e-5 and abs(xhi-0.505) < 1e-5 # check that we're in syn
 out_f = ROOT.TFile(out_fn, 'recreate')
 integ = lambda h: h.Integral(0,h.GetNbinsX()+2)
 
-# do the bkg subtraction in whatever variables you want as long as the histos exist
+# do the bkg subtraction in whatever variables you want as long as the hists exist
 # written out to file in folders so the cmp script can do the rest
 
 colors = (2,3,4,6)
 variables = [
-    ('h_vtx_rho', 1, 10, (0,2)),
-#    ('h_track_charge', 2, 1, None),
-    ('h_track_pt', 2, 10, (0,100)),
-    ('h_track_eta', 2, 10, None),
-    ('h_track_phi', 2, 10, None),
-    ('h_track_npxhits', 2, 1, None),
-    ('h_track_nsthits', 2, 1, None),
-    ('h_track_npxlayers', 2, 1, None),
-    ('h_track_nstlayers', 2, 1, None),
-    ('h_track_nstlayersmono', 2, 1, None),
-    ('h_track_nstlayersstereo', 2, 1, None),
-    ('h_track_maxpxlayer', 2, 1, None),
-    ('h_track_minstlayer', 2, 1, None),
-    ('h_track_deltapxlayers', 2, 1, None),
-    ('h_track_deltastlayers', 2, 1, None),
-    ('h_track_dxybs', 2, 10, (-0.5,0.5)),
-    ('h_track_dxypv', 2, 10, (-0.5,0.5)),
-    ('h_track_dzbs', 2, 10, None),
-    ('h_track_sigmadxy', 2, 1, (0,0.03)),
-#    ('h_npv', 1, 1, (0, 60)),
+    ('h_rho', 1, 10, (0,2)),
+#    ('h_tracks_charge', 2, 1, None),
+    ('h_tracks_pt', 2, 10, (0,100)),
+    ('h_tracks_eta', 2, 10, None),
+    ('h_tracks_phi', 2, 10, None),
+    ('h_tracks_dxy', 2, 10, (-0.5,0.5)),
+    ('h_tracks_absdxy', 2, 10, (0.,0.5)),
+    ('h_tracks_dz', 2, 10, None),
+    ('h_tracks_dzpv', 2, 10, None),
+    ('h_tracks_sigmadxy', 2, 1, (0,0.03)),
+    ('h_tracks_npxlayers', 2, 1, None),
+    ('h_tracks_nstlayers', 2, 1, None),
+    ('h_tracks_dxyerr', 2, 10, (0,0.5)),
     ]
 
 for hname, integ_factor, rebin, x_range in variables:
-    hon = in_f.Get('v0effon%s/K0_2pi/' % ex + hname)
-    hbkglo = in_f.Get('v0effbkglo%s/K0_2pi/' % ex + hname)
-    hbkghi = in_f.Get('v0effbkghi%s/K0_2pi/' % ex + hname)
+    hon = in_f.Get('masson/%s' % hname)
+    hbkglo = in_f.Get('masslo/%s' % hname)
+    hbkghi = in_f.Get('masshi/%s' % hname)
 
     d = out_f.mkdir(hname)
     d.cd()
--- MFVNeutralino/test/K0Tracks/hists.cc
+++ MFVNeutralino/test/K0Tracks/hists.cc
@@ -7,14 +7,95 @@ int main(int argc, char** argv) {
   nr.init_options("mfvK0s/t");
   if (!nr.parse_options(argc, argv) || !nr.init()) return 1;
   auto& nt = nr.nt();
+  auto& ntt = nt.tracks();
 
   ////
 
-  const double mpi = 0.13957;
+  const double mpion = 0.13957;
 
-  auto h_premass = new TH1D("h_premass", ";SV mass before refit (GeV);vertices/5 MeV", 400, 0, 2);
-  auto h_mass = new TH1D("h_mass", ";SV mass (GeV);vertices/5 MeV", 400, 0, 2);
-  auto h_costh = new TH1D("h_costh", ";costh;vertices/0.001", 2002, -1.001, 1.001);
+  enum { mass_all, mass_lo, mass_hi, mass_on, max_mass_type };
+  const char* mass_names[max_mass_type] = {"massall", "masslo", "masshi", "masson"};
+
+  enum { tk_all, tk_sel, tk_seed, max_tk_type };
+  const char* tk_names[max_tk_type] = {"all", "sel", "seed"};
+
+  TH1D* h_nvtx[max_mass_type];
+  TH1D* h_premass[max_mass_type];
+  TH1D* h_mass[max_mass_type];
+  TH1D* h_pt[max_mass_type];
+  TH1D* h_eta[max_mass_type];
+  TH1D* h_phi[max_mass_type];
+  TH1D* h_costh[max_mass_type];
+  TH1D* h_ct[max_mass_type];
+  TH1D* h_ctau[max_mass_type];
+  TH1D* h_rho[max_mass_type];
+  
+  TH1D* h_tracks_pt[max_mass_type][max_tk_type];
+  TH1D* h_tracks_eta[max_mass_type][max_tk_type];
+  TH1D* h_tracks_phi[max_mass_type][max_tk_type];
+  TH1D* h_tracks_dxy[max_mass_type][max_tk_type];
+  TH1D* h_tracks_absdxy[max_mass_type][max_tk_type];
+  TH1D* h_tracks_dz[max_mass_type][max_tk_type];
+  TH1D* h_tracks_dzpv[max_mass_type][max_tk_type];
+  TH1D* h_tracks_nhits[max_mass_type][max_tk_type];
+  TH1D* h_tracks_npxhits[max_mass_type][max_tk_type];
+  TH1D* h_tracks_nsthits[max_mass_type][max_tk_type];
+  TH1D* h_tracks_min_r[max_mass_type][max_tk_type];
+  TH1D* h_tracks_npxlayers[max_mass_type][max_tk_type];
+  TH1D* h_tracks_nstlayers[max_mass_type][max_tk_type];
+  TH1D* h_tracks_nsigmadxy[max_mass_type][max_tk_type];
+  TH1D* h_tracks_dxyerr[max_mass_type][max_tk_type];
+  TH1D* h_tracks_dzerr[max_mass_type][max_tk_type];
+  TH1D* h_tracks_dszerr[max_mass_type][max_tk_type];
+  TH1D* h_tracks_lambdaerr[max_mass_type][max_tk_type];
+  TH1D* h_tracks_pterr[max_mass_type][max_tk_type];
+  TH1D* h_tracks_phierr[max_mass_type][max_tk_type];
+  TH1D* h_tracks_etaerr[max_mass_type][max_tk_type];
+
+  for (int i = 0; i < max_mass_type; ++i) {
+    TDirectory* d = nr.f_out().mkdir(mass_names[i]);
+    d->cd();
+
+    h_nvtx[i] = new TH1D("h_nvtx", ";# of K0 candidates;events", 30, 0, 30);
+    h_premass[i] = new TH1D("h_premass", ";K0 candidate pre-fit mass (GeV);cands/5 MeV", 400, 0, 2);
+    h_mass[i] = new TH1D("h_mass", ";K0 candidate mass (GeV);cands/5 MeV", 400, 0, 2);
+    h_pt[i] = new TH1D("h_pt", ";K0 candidate p_{T} (GeV);cands/5 GeV", 100, 0, 500);
+    h_eta[i] = new TH1D("h_eta", ";K0 candidate #eta;cands/0.05", 100, -2.5, 2.5);
+    h_phi[i] = new TH1D("h_phi", ";K0 candidate #phi;cands/0.063", 100, -M_PI, M_PI);
+    h_costh[i] = new TH1D("h_costh", ";K0 candidate cos(angle{flight,momentum});cands/0.01", 202, -1.01, 1.01);
+    h_ct[i] = new TH1D("h_ct", ";K0 candidate ct (cm);cands/0.01", 100, 0, 10);
+    h_ctau[i] = new TH1D("h_ctau", ";K0 candidate c#tau (cm);cands/0.01", 100, 0, 10);
+    h_rho[i] = new TH1D("h_rho", ";K0 candidate #rho (cm);cands/0.01", 100, 0, 10);
+
+    for (int j = 0; j < max_tk_type; ++j) {
+      TDirectory* d2 = d->mkdir(tk_names[j]);
+      d2->cd();
+
+      h_tracks_pt[i][j] = new TH1D("h_tracks_pt", ";tracks pt;arb. units", 2000, 0, 200);
+      h_tracks_eta[i][j] = new TH1D("h_tracks_eta", ";tracks eta;arb. units", 50, -4, 4);
+      h_tracks_phi[i][j] = new TH1D("h_tracks_phi", ";tracks phi;arb. units", 315, -3.15, 3.15);
+      h_tracks_dxy[i][j] = new TH1D("h_tracks_dxy", ";tracks dxy to beamspot;arb. units", 400, -0.2, 0.2);
+      h_tracks_absdxy[i][j] = new TH1D("h_tracks_absdxy", ";tracks |dxy| to beamspot;arb. units", 200, 0, 0.2);
+      h_tracks_dz[i][j] = new TH1D("h_tracks_dz", ";tracks dz to BS;arb. units", 400, -20, 20);
+      h_tracks_dzpv[i][j] = new TH1D("h_tracks_dzpv", ";tracks dz to PV;arb. units", 400, -20, 20);
+      h_tracks_nhits[i][j] = new TH1D("h_tracks_nhits", ";tracks nhits;arb. units", 40, 0, 40);
+      h_tracks_npxhits[i][j] = new TH1D("h_tracks_npxhits", ";tracks npxhits;arb. units", 40, 0, 40);
+      h_tracks_nsthits[i][j] = new TH1D("h_tracks_nsthits", ";tracks nsthits;arb. units", 40, 0, 40);
+      h_tracks_min_r[i][j] = new TH1D("h_tracks_min_r", ";tracks min_r;arb. units", 20, 0, 20);
+      h_tracks_npxlayers[i][j] = new TH1D("h_tracks_npxlayers", ";tracks npxlayers;arb. units", 20, 0, 20);
+      h_tracks_nstlayers[i][j] = new TH1D("h_tracks_nstlayers", ";tracks nstlayers;arb. units", 20, 0, 20);
+      h_tracks_nsigmadxy[i][j] = new TH1D("h_tracks_nsigmadxy", ";tracks nsigmadxy;arb. units", 400, 0, 40);
+      h_tracks_dxyerr[i][j] = new TH1D("h_tracks_dxyerr", ";tracks dxyerr;arb. units", 2000, 0, 0.2);
+      h_tracks_dzerr[i][j] = new TH1D("h_tracks_dzerr", ";tracks dzerr;arb. units", 2000, 0, 0.2);
+      h_tracks_dszerr[i][j] = new TH1D("h_tracks_dszerr", ";tracks dszerr;arb. units", 2000, 0, 0.2);
+      h_tracks_lambdaerr[i][j] = new TH1D("h_tracks_lambdaerr", ";tracks lambdaerr;arb. units", 2000, 0, 0.2);
+      h_tracks_pterr[i][j] = new TH1D("h_tracks_pterr", ";tracks pterr;arb. units", 200, 0, 0.2);
+      h_tracks_phierr[i][j] = new TH1D("h_tracks_phierr", ";tracks phierr;arb. units", 200, 0, 0.2);
+      h_tracks_etaerr[i][j] = new TH1D("h_tracks_etaerr", ";tracks etaerr;arb. units", 200, 0, 0.2);
+    }
+  }
+
+  nr.f_out().cd();
 
   auto fcn = [&]() {
     const double w = nr.weight();
@@ -22,31 +103,93 @@ int main(int argc, char** argv) {
 
     const TVector3 pv = nt.pvs().pos(0);
 
+    int nvtx[max_mass_type] = {0};
+
     for (int isv = 0, isve = nt.svs().n(); isv < isve; ++isv) {
       const TVector3 pos = nt.svs().pos(isv);
       const TVector3 flight = pos - pv;
+      const double rho = flight.Perp();
 
       const int itk = nt.svs().misc(isv) & 0xFFFF;
       const int jtk = nt.svs().misc(isv) >> 16;
       const int irftk = 2*isv;
       const int jrftk = 2*isv+1;
 
-      const TLorentzVector ip4 = nt.tracks().p4(itk, mpi);
-      const TLorentzVector jp4 = nt.tracks().p4(jtk, mpi);
-      const TLorentzVector irfp4 = nt.refit_tks().p4(irftk, mpi);
-      const TLorentzVector jrfp4 = nt.refit_tks().p4(jrftk, mpi);
+      const TLorentzVector ip4 = ntt.p4(itk, mpion);
+      const TLorentzVector jp4 = ntt.p4(jtk, mpion);
+      const TLorentzVector irfp4 = nt.refit_tks().p4(irftk, mpion);
+      const TLorentzVector jrfp4 = nt.refit_tks().p4(jrftk, mpion);
 
       const TLorentzVector prep4 = ip4 + jp4;
       const TLorentzVector p4 = irfp4 + jrfp4;
       const TVector3 momdir = p4.Vect().Unit();
 
-      const double costh = momdir.Dot(flight.Unit());
       const double mass = p4.M();
+      const double costh = momdir.Dot(flight.Unit());
+      const double ct = flight.Mag();
+      const double ctau = ct / p4.Beta() / p4.Gamma();
 
-      fill(h_premass, prep4.M());
-      fill(h_mass, mass);
-      fill(h_costh, costh);
+      if (rho > 2.) // || costh < 0.99)
+        continue;
+
+      int imass2 = mass_on;
+      if      (mass < 0.490) imass2 = mass_lo;
+      else if (mass > 0.505) imass2 = mass_hi;
+
+      for (int ii : {0,1}) {
+        const int imass = ii == 0 ? mass_all : imass2;
+        ++nvtx[imass];
+
+        fill(h_premass[imass], prep4.M());
+        fill(h_mass[imass], mass);
+        fill(h_pt[imass], p4.Pt());
+        fill(h_eta[imass], p4.Eta());
+        fill(h_phi[imass], p4.Phi());
+        fill(h_costh[imass], costh);
+        fill(h_ct[imass], ct);
+        fill(h_ctau[imass], ctau);
+        fill(h_rho[imass], rho);
+
+        for (int j : {itk, jtk}) {
+          const bool sel =
+            ntt.pt(j) > 1 &&
+            ntt.min_r(j) <= 1 &&
+            ntt.npxlayers(j) >= 2 &&
+            ntt.nstlayers(j) >= 6;
+          const bool seed = sel && ntt.nsigmadxybs(j, nt.bs()) > 4;
+          const bool tk_ok[max_tk_type] = { true, sel, seed };
+
+          for (int i = 0; i < max_tk_type; ++i) {
+            if (!tk_ok[i]) continue;
+          
+            fill(h_tracks_pt[imass][i], ntt.pt(j));
+            fill(h_tracks_eta[imass][i], ntt.eta(j));
+            fill(h_tracks_phi[imass][i], ntt.phi(j));
+            fill(h_tracks_dxy[imass][i], ntt.dxybs(j, nt.bs()));
+            fill(h_tracks_absdxy[imass][i], fabs(ntt.dxybs(j, nt.bs())));
+            fill(h_tracks_dz[imass][i], ntt.dz(j));
+            fill(h_tracks_dzpv[imass][i], ntt.dzpv(j, nt.pvs()));
+            fill(h_tracks_nhits[imass][i], ntt.nhits(j));
+            fill(h_tracks_npxhits[imass][i], ntt.npxhits(j));
+            fill(h_tracks_nsthits[imass][i], ntt.nsthits(j));
+            fill(h_tracks_min_r[imass][i], ntt.min_r(j));
+            fill(h_tracks_npxlayers[imass][i], ntt.npxlayers(j));
+            fill(h_tracks_nstlayers[imass][i], ntt.nstlayers(j));
+            fill(h_tracks_nsigmadxy[imass][i], ntt.nsigmadxybs(j, nt.bs()));
+            fill(h_tracks_dxyerr[imass][i], ntt.err_dxy(j));
+            fill(h_tracks_dzerr[imass][i], ntt.err_dz(j));
+            fill(h_tracks_dszerr[imass][i], ntt.err_dsz(j));
+            fill(h_tracks_lambdaerr[imass][i], ntt.err_lambda(j));
+            fill(h_tracks_pterr[imass][i], ntt.err_pt(j));
+            fill(h_tracks_phierr[imass][i], ntt.err_phi(j));
+            fill(h_tracks_etaerr[imass][i], ntt.err_eta(j));
+          }
+        }
+      }
     }
+
+    for (int i = 0; i < max_mass_type; ++i)
+      fill(h_nvtx[i], nvtx[i]);
 
     return std::make_pair(true, w);
   };
