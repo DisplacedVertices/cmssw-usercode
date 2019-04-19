426fa1a27b25bc68c8a8b417ebd1a5fa1ea81d20
Jordan Tucker <jordantucker@gmail.com>
2019-04-19 15:41:14 -0500

    forgot to increment whiches after changing upstream

--- MFVNeutralino/test/SplitPV/hists.cc
+++ MFVNeutralino/test/SplitPV/hists.cc
@@ -23,15 +23,19 @@ int main(int argc, char** argv) {
     const double w = nr.weight();
     auto fill = [&](auto* h, double x) { h->Fill(x, w); };
 
-    int io = -1, ir = -1, ia = -1, ib = -1;
+    int io = 0, ir = -1, __attribute__((unused)) il = -1, ia = -1, ib = -1;
 
-    for (int i = 0, ie = pvs.n(); i < ie; ++i) {
+    for (int i = 1, ie = pvs.n(); i < ie; ++i) {
       unsigned which = pvs.misc(i) & 0xFFFF;
       if      (which == 0) io = i;
       else if (which == 1) ir = i;
-      else if (which == 2) ia = i;
-      else if (which == 3) ib = i;
+      else if (which == 2) il = i;
+      else if (which == 3) ia = i;
+      else if (which == 4) ib = i;
+      //else printf("which is %u\n", which);
     }
+
+    //printf("io %i ir %i il %i ia %i ib %i\n", io, ir, il, ia, ib);
 
     if (io != -1 && ir != -1) {
       fill(h_refit_dx, pvs.x(ir) - pvs.x(io));
