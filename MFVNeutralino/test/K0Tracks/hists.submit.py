617bae27f080e0483780151775ec944e5394a7b2
Jordan Tucker <jordantucker@gmail.com>
2019-04-20 18:43:01 -0500

    default batch name in sync with .cc

--- MFVNeutralino/test/K0Tracks/hists.submit.py
+++ MFVNeutralino/test/K0Tracks/hists.submit.py
@@ -4,4 +4,4 @@ samples = Samples.qcd_samples_2017 + Samples.data_samples_2017 + Samples.qcd_sam
 Samples.qcdht0700_2017.njobs = 1
 Samples.qcdht0700_2018.njobs = 1
 
-CS.NtupleReader_submit('K0HistsV23mv3_nsigdxy0_norhomin', 'root://cmseos.fnal.gov//store/user/tucker/hadded/K0NtupleV23mv3', samples)
+CS.NtupleReader_submit('K0HistsV23mv3_nsigdxy1p5_rhomin0p2', 'root://cmseos.fnal.gov//store/user/tucker/hadded/K0NtupleV23mv3', samples)
