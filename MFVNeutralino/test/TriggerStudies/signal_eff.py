ae7f9ea928e9b51c117c9a46ce940cf6a9f12942
Jordan Tucker <jordantucker@gmail.com>
2019-04-29 13:52:11 -0500

    2017

--- MFVNeutralino/test/TriggerStudies/signal_eff.py
+++ MFVNeutralino/test/TriggerStudies/signal_eff.py
@@ -6,17 +6,17 @@ from JMTucker.Tools import Samples
 from JMTucker.MFVNeutralino.PerSignal import PerSignal
 
 set_style()
-ps = plot_saver(plot_dir('sigeff_trig_stopdbardbar'), size=(600,600), log=False)
+ps = plot_saver(plot_dir('sigeff_trig'), size=(600,600), log=False)
 
-root_file_dir = '/uscms_data/d2/tucker/crab_dirs/TrigFiltCheckV2'
-trigs = ['PFHT800', 'AK8PFJet450', 'Hcombination']
-nice = ['PFHT800', 'AK8PFJet450', 'H combination']
+root_file_dir = '/uscms_data/d2/tucker/crab_dirs/TrigFiltCheckV1'
+trigs = ['Trigger']
+nice = ['PFHT1050']
 colors = [ROOT.kRed, ROOT.kBlue, ROOT.kGreen+2, ROOT.kBlack]
 
 def sample_ok(s):
-    return s.mass not in (500,3000)
-multijet = [s for s in Samples.mfv_signal_samples if sample_ok(s)]
-dijet = [s for s in Samples.mfv_stopdbardbar_samples if sample_ok(s)]
+    return True #s.mass not in (500,3000)
+multijet = [s for s in Samples.mfv_signal_samples_2017 if sample_ok(s)]
+dijet = [s for s in Samples.mfv_stopdbardbar_samples_2017 if sample_ok(s)]
 
 def getit(f, n):
     hnum = f.Get('SimpleTriggerEfficiency/triggers_pass_num')
@@ -39,18 +39,26 @@ for sample in multijet + dijet:
     f = ROOT.TFile(fn)
     sample.ys = {n: getit(f,'p'+n) for n in trigs}
 
-for kind, samples in ('multijet', multijet), ('dijet', dijet):
-    per = PerSignal('efficiency', y_range=(0.,1.15))
-    for itrig, trig in enumerate(trigs):
-        for sample in samples:
-            sample.y, sample.yl, sample.yh = sample.ys[trig]
-        per.add(samples, title=nice[itrig], color=colors[itrig])
-    per.draw(canvas=ps.c)
+if len(trigs) > 1:
+    for kind, samples in ('multijet', multijet), ('dijet', dijet):
+        per = PerSignal('efficiency', y_range=(0.,1.15))
+        for itrig, trig in enumerate(trigs):
+            for sample in samples:
+                sample.y, sample.yl, sample.yh = sample.ys[trig]
+            per.add(samples, title=nice[itrig], color=colors[itrig])
+        per.draw(canvas=ps.c)
 
     mvpave(per.decay_paves[0], 0.703, 1.018, 6.227, 1.098)
     mvpave(per.decay_paves[1], 6.729, 1.021, 14.073, 1.101)
     mvpave(per.decay_paves[2], 14.45, 1.033, 21.794, 1.093) 
 
     ps.save(kind)
-
+else:
+    for sample in multijet + dijet:
+        sample.y, sample.yl, sample.yh = sample.ys[trigs[0]]
+    per = PerSignal('efficiency', y_range=(0.,1.05))
+    per.add(multijet, title='#tilde{N} #rightarrow tbs')
+    per.add(dijet, title='#tilde{t} #rightarrow #bar{d}#bar{d}', color=ROOT.kBlue)
+    per.draw(canvas=ps.c)
+    ps.save('trigeff')
 
