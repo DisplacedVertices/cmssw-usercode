fc9553e00dae521db46ce25bf721d884b842968c
Jordan Tucker <jordantucker@gmail.com>
2019-06-01 08:04:02 -0500

    define for standalone usage

--- Tools/interface/AnalysisEras.h
+++ Tools/interface/AnalysisEras.h
@@ -3,14 +3,16 @@
 
 #include <random>
 #include <vector>
+#ifndef JMT_ANALYSISERAS_STANDALONE
 #include "JMTucker/Tools/interface/Framework.h"
+#endif
 
 namespace jmt {
   class AnalysisEras {
   public:
     enum { e_2017B, e_2017C, e_2017D, e_2017E, e_2017F, e_2018A, e_2018B, e_2018C, e_2018D, e_max };
 
-    static const double int_lumi(int e) { return int_lumi_[e]; }
+    static double int_lumi(int e) { return int_lumi_[e]; }
     static double cumu_int_lumi(int e) { return std::accumulate(std::begin(int_lumi_), std::begin(int_lumi_) + e + 1, 0.); }
     static double total_int_lumi() { return cumu_int_lumi(e_max); }
 
@@ -20,11 +22,13 @@ namespace jmt {
       return d(g);
     }
 
+#ifndef JMT_ANALYSISERAS_STANDALONE
     template <typename T>
     static int pick(const edm::Event& event, const T& consumer) {
       const int which = jmt::getProcessModuleParameter<int>(event, consumer, "jmtAnalysisEras", "which");
       return which == -1 ? pick(event.id().event()) : which;
     }
+#endif
 
   private:
     static const double int_lumi_[e_max];
