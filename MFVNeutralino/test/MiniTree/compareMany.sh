cbcdd7bf6b68c73e38f11730281e427b49259a2f
Joseph Reichert <Joseph.Reichert@cern.ch>
2019-06-04 15:19:29 -0400

    adding / renaming some helper scripts

---
+++ MFVNeutralino/test/MiniTree/compareMany.sh
@@ -0,0 +1,9 @@
+comparehists.py output_btags_vs_bquarks_MiniTreeV25m_ntk3_2017/background.root "" /publicweb/j/joeyr/plots/btags_vs_bquarks_MiniTreeV25m_ntk3_2017 --draw-command '"colz text0" if "flavor_code" in name else ""' --nice "MCbackground_2017"
+comparehists.py output_btags_vs_bquarks_MiniTreeV25m_ntk4_2017/background.root "" /publicweb/j/joeyr/plots/btags_vs_bquarks_MiniTreeV25m_ntk4_2017 --draw-command '"colz text0" if "flavor_code" in name else ""' --nice "MCbackground_2017"
+comparehists.py output_btags_vs_bquarks_MiniTreeV25m_ntk5_2017/background.root "" /publicweb/j/joeyr/plots/btags_vs_bquarks_MiniTreeV25m_ntk5_2017 --draw-command '"colz text0" if "flavor_code" in name else ""' --nice "MCbackground_2017"
+comparehists.py output_btags_vs_bquarks_MiniTreeV25m_ntk7_2017/background.root "" /publicweb/j/joeyr/plots/btags_vs_bquarks_MiniTreeV25m_ntk7_2017 --draw-command '"colz text0" if "flavor_code" in name else ""' --nice "MCbackground_2017"
+
+comparehists.py output_btags_vs_bquarks_MiniTreeV25m_ntk3_2018/background.root "" /publicweb/j/joeyr/plots/btags_vs_bquarks_MiniTreeV25m_ntk3_2018 --draw-command '"colz text0" if "flavor_code" in name else ""' --nice "MCbackground_2018"
+comparehists.py output_btags_vs_bquarks_MiniTreeV25m_ntk4_2018/background.root "" /publicweb/j/joeyr/plots/btags_vs_bquarks_MiniTreeV25m_ntk4_2018 --draw-command '"colz text0" if "flavor_code" in name else ""' --nice "MCbackground_2018"
+comparehists.py output_btags_vs_bquarks_MiniTreeV25m_ntk5_2018/background.root "" /publicweb/j/joeyr/plots/btags_vs_bquarks_MiniTreeV25m_ntk5_2018 --draw-command '"colz text0" if "flavor_code" in name else ""' --nice "MCbackground_2018"
+comparehists.py output_btags_vs_bquarks_MiniTreeV25m_ntk7_2018/background.root "" /publicweb/j/joeyr/plots/btags_vs_bquarks_MiniTreeV25m_ntk7_2018 --draw-command '"colz text0" if "flavor_code" in name else ""' --nice "MCbackground_2018"
