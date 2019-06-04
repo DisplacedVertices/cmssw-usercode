25a956eded4e2147b0050cff72f3b84bd98a1807
Dan Quach <quach.dan@gmail.com>
2019-06-04 11:22:31 -0500

    changes to make plots for 2017+2018

--- MFVNeutralino/test/data_mc_comp.py
+++ MFVNeutralino/test/data_mc_comp.py
@@ -6,16 +6,38 @@ import JMTucker.MFVNeutralino.AnalysisConstants as ac
 from JMTucker.Tools.ROOTTools import *
 from JMTucker.Tools import Samples
 
-root_file_dir = '/uscms_data/d2/tucker/crab_dirs/HistosV18m'
+year = '2017p8'
+version = 'V23m'
+root_file_dir = '/uscms_data/d2/tucker/crab_dirs/Histos%s' % version
 
 set_style()
-ps = plot_saver(plot_dir('data_mc_comp_v18m'))
+ps = plot_saver(plot_dir('data_mc_comp_%s_%s' % (year, version)))
 
 qcd_samples = Samples.qcd_samples_2017[1:]
 ttbar_samples = Samples.ttbar_samples_2017
 signal_sample = Samples.mfv_neu_tau001000um_M0800_2017
 data_samples = [] # Samples.data_samples_2017
 background_samples = ttbar_samples + qcd_samples
+lumi = ac.int_lumi_2017 * ac.scale_factor_2017
+lumi_nice = ac.int_lumi_nice_2017
+
+if year == '2018':
+    qcd_samples = Samples.qcd_samples_2018
+    ttbar_samples = []
+    signal_sample = Samples.mfv_neu_tau001000um_M0800_2017
+    data_samples = [] # Samples.data_samples_2017
+    background_samples = qcd_samples
+    lumi = ac.int_lumi_2018 * ac.scale_factor_2018
+    lumi_nice = ac.int_lumi_nice_2018
+
+if year == '2017p8':
+    qcd_samples = Samples.qcd_samples_2018 + Samples.qcd_samples_2017[1:]
+    ttbar_samples = Samples.ttbar_samples_2017
+    signal_sample = Samples.mfv_neu_tau001000um_M0800_2017
+    data_samples = [] # Samples.data_samples_2017
+    background_samples = qcd_samples + ttbar_samples
+    lumi = ac.int_lumi_2017p8 * ac.scale_factor_2017p8
+    lumi_nice = ac.int_lumi_nice_2017p8
 
 for s in qcd_samples:
     s.join_info = True, 'Multijet events', ROOT.kBlue-9
@@ -32,8 +54,8 @@ C = partial(data_mc_comparison,
             data_samples = [],
             plot_saver = ps,
             file_path = os.path.join(root_file_dir, '%(name)s.root'),
-            int_lumi = ac.int_lumi_2017 * ac.scale_factor_2017,
-            int_lumi_nice = ac.int_lumi_nice_2017,
+            int_lumi = lumi,
+            int_lumi_nice = lumi_nice,
             canvas_top_margin = 0.08,
             overflow_in_last = True,
             poisson_intervals = True,
@@ -46,23 +68,22 @@ C = partial(data_mc_comparison,
             simulation = True,
             )
 
-
-C('ntuple_njets',
-  histogram_path = 'evtHst0VNoHt/h_njets',
-  x_title = 'Number of jets',
-  y_title = 'Events',
-  y_range = (1, 1e8),
-  )
-
-C('ntuple_ht40',
-  histogram_path = 'evtHst0VNoHt/h_jet_ht_40',
-  rebin = 4,
-  x_title = 'Jet H_{T} (GeV)',
-  y_title = 'Events/100 GeV',
-  y_range = (1, 1e8),
-  cut_line = ((1200, 0, 1200, 2.5e8), 2, 5, 1),
-  )
-
+#C('ntuple_njets',
+#  histogram_path = 'evtHst0VNoHt/h_njets',
+#  x_title = 'Number of jets',
+#  y_title = 'Events',
+#  y_range = (1, 1e8),
+#  )
+#
+#C('ntuple_ht40',
+#  histogram_path = 'evtHst0VNoHt/h_jet_ht_40',
+#  rebin = 4,
+#  x_title = 'Jet H_{T} (GeV)',
+#  y_title = 'Events/100 GeV',
+#  y_range = (1, 1e8),
+#  cut_line = ((1200, 0, 1200, 2.5e8), 2, 5, 1),
+#  )
+#
 C('presel_njets',
   histogram_path = 'mfvEventHistosPreSel/h_njets',
   x_title = 'Number of jets',
@@ -78,133 +99,133 @@ C('presel_ht40',
   y_range = (1, 1e8),
   )
 
-C('presel_htall',
-  histogram_path = 'mfvEventHistosPreSel/h_jet_ht',
-  )
-
-C('presel_jetpt1',
-  histogram_path = 'mfvEventHistosPreSel/h_jetpt1',
-  y_range = (1,1e6),
-  )
-
-C('presel_jetpt4',
-  histogram_path = 'mfvEventHistosPreSel/h_jetpt4',
-  y_range = (1,5e6),
-  )
-
-C('presel_jetpt',
-  histogram_path = 'mfvEventHistosPreSel/h_jet_pt',
-  y_range = (1,5e7),
-  )
-
-C('presel_jeteta',
-  histogram_path = 'mfvEventHistosPreSel/h_jet_eta',
-  )
-
-C('presel_jetphi',
-  histogram_path = 'mfvEventHistosPreSel/h_jet_phi',
-  )
-
-C('presel_jetpairdphi',
-  histogram_path = 'mfvEventHistosPreSel/h_jet_pairdphi',
-  )
-
-C('presel_met',
-  histogram_path = 'mfvEventHistosPreSel/h_met',
-  )
-
-C('presel_metphi',
-  histogram_path = 'mfvEventHistosPreSel/h_metphi',
-  )
-
-C('presel_nbtags_tight',
-  histogram_path = 'mfvEventHistosPreSel/h_nbtags_tight',
-  )
-
-C('presel_nmuons_any',
-  histogram_path = 'mfvEventHistosPreSel/h_nmuons_any',
-  )
-
-C('presel_nmuons_selected',
-  histogram_path = 'mfvEventHistosPreSel/h_nmuons_selected',
-  )
-
-C('presel_nelectrons_any',
-  histogram_path = 'mfvEventHistosPreSel/h_nelectrons_any',
-  )
-
-C('presel_nelectrons_selected',
-  histogram_path = 'mfvEventHistosPreSel/h_nelectrons_selected',
-  )
-
-C('presel_npv',
-  histogram_path = 'mfvEventHistosPreSel/h_npv',
-  y_range = (1,1e6),
-  )
-
-C('presel_pvntracks',
-  histogram_path = 'mfvEventHistosPreSel/h_pvntracks',
-  )
-
-C('presel_pvscore',
-  histogram_path = 'mfvEventHistosPreSel/h_pvscore',
-  )
-
-C('presel_pvrho',
-  histogram_path = 'mfvEventHistosPreSel/h_pvrho',
-  )
-
-C('presel_nseedtracks',
-  histogram_path = 'mfvEventHistosPreSel/h_n_vertex_seed_tracks',
-  y_range = (1,1e7)
-  )
-
-C('presel_seedtrack_npxlayers',
-  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_npxlayers',
-  y_range = (1,1e8),
-  )
-
-C('presel_seedtrack_nstlayers',
-  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_nstlayers',
-  y_range = (1,1e8),
-  )
-
-C('presel_seedtrack_chi2dof',
-  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_chi2dof',
-  y_range = (1,1e8),
-  )
-
-C('presel_seedtrack_pt',
-  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_pt',
-  y_range = (1,1e8),
-  )
-
-C('presel_seedtrack_eta',
-  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_eta',
-  y_range = (1,6e6),
-  )
-
-C('presel_seedtrack_phi',
-  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_phi',
-  y_range = (1,6e6),
-  )
-
-C('presel_seedtrack_dxy',
-  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_dxy',
-  y_range = (1,1e6),
-  )
-
-C('presel_seedtrack_dz',
-  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_dz',
-  y_range = (1,1e6),
-  )
+#C('presel_htall',
+#  histogram_path = 'mfvEventHistosPreSel/h_jet_ht',
+#  )
+#
+#C('presel_jetpt1',
+#  histogram_path = 'mfvEventHistosPreSel/h_jetpt1',
+#  y_range = (1,1e6),
+#  )
+#
+#C('presel_jetpt4',
+#  histogram_path = 'mfvEventHistosPreSel/h_jetpt4',
+#  y_range = (1,5e6),
+#  )
+#
+#C('presel_jetpt',
+#  histogram_path = 'mfvEventHistosPreSel/h_jet_pt',
+#  y_range = (1,5e7),
+#  )
+#
+#C('presel_jeteta',
+#  histogram_path = 'mfvEventHistosPreSel/h_jet_eta',
+#  )
+#
+#C('presel_jetphi',
+#  histogram_path = 'mfvEventHistosPreSel/h_jet_phi',
+#  )
+#
+#C('presel_jetpairdphi',
+#  histogram_path = 'mfvEventHistosPreSel/h_jet_pairdphi',
+#  )
+#
+#C('presel_met',
+#  histogram_path = 'mfvEventHistosPreSel/h_met',
+#  )
+#
+#C('presel_metphi',
+#  histogram_path = 'mfvEventHistosPreSel/h_metphi',
+#  )
+#
+#C('presel_nbtags_tight',
+#  histogram_path = 'mfvEventHistosPreSel/h_nbtags_tight',
+#  )
+#
+#C('presel_nmuons_any',
+#  histogram_path = 'mfvEventHistosPreSel/h_nmuons_any',
+#  )
+#
+#C('presel_nmuons_selected',
+#  histogram_path = 'mfvEventHistosPreSel/h_nmuons_selected',
+#  )
+#
+#C('presel_nelectrons_any',
+#  histogram_path = 'mfvEventHistosPreSel/h_nelectrons_any',
+#  )
+#
+#C('presel_nelectrons_selected',
+#  histogram_path = 'mfvEventHistosPreSel/h_nelectrons_selected',
+#  )
+#
+#C('presel_npv',
+#  histogram_path = 'mfvEventHistosPreSel/h_npv',
+#  y_range = (1,1e6),
+#  )
+#
+#C('presel_pvntracks',
+#  histogram_path = 'mfvEventHistosPreSel/h_pvntracks',
+#  )
+#
+#C('presel_pvscore',
+#  histogram_path = 'mfvEventHistosPreSel/h_pvscore',
+#  )
+#
+#C('presel_pvrho',
+#  histogram_path = 'mfvEventHistosPreSel/h_pvrho',
+#  )
+#
+#C('presel_nseedtracks',
+#  histogram_path = 'mfvEventHistosPreSel/h_n_vertex_seed_tracks',
+#  y_range = (1,1e7)
+#  )
+#
+#C('presel_seedtrack_npxlayers',
+#  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_npxlayers',
+#  y_range = (1,1e8),
+#  )
+#
+#C('presel_seedtrack_nstlayers',
+#  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_nstlayers',
+#  y_range = (1,1e8),
+#  )
+#
+#C('presel_seedtrack_chi2dof',
+#  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_chi2dof',
+#  y_range = (1,1e8),
+#  )
+#
+#C('presel_seedtrack_pt',
+#  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_pt',
+#  y_range = (1,1e8),
+#  )
+#
+#C('presel_seedtrack_eta',
+#  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_eta',
+#  y_range = (1,6e6),
+#  )
+#
+#C('presel_seedtrack_phi',
+#  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_phi',
+#  y_range = (1,6e6),
+#  )
+#
+#C('presel_seedtrack_dxy',
+#  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_dxy',
+#  y_range = (1,1e6),
+#  )
+#
+#C('presel_seedtrack_dz',
+#  histogram_path = 'mfvEventHistosPreSel/h_vertex_seed_track_dz',
+#  y_range = (1,1e6),
+#  )
 
 
 C('onevtx_ntracks',
   histogram_path = 'vtxHst1VNoNtracks/h_sv_all_ntracks',
   x_title = 'Number of tracks per vertex',
   y_title = 'Vertices',
-  y_range = (1, 1e6),
+  y_range = (0.1, 1e6),
   cut_line = ((5, 0, 5, 2.1e6), 2, 5, 1),
   )
 
@@ -246,7 +267,7 @@ C('nsv_5track',
   x_title = 'Number of 5-or-more-track vertices',
   y_title = 'Events',
   x_range = (0, 8),
-  y_range = (1, 1e8),
+  y_range = (1e-3, 1e8),
   cut_line = ((2, 0, 2, 2.5e8), 2, 5, 1),
   )
 
--- MFVNeutralino/test/pretty_dbv.py
+++ MFVNeutralino/test/pretty_dbv.py
@@ -4,23 +4,22 @@ import os
 from functools import partial
 import JMTucker.Tools.Samples as Samples
 import JMTucker.MFVNeutralino.AnalysisConstants as ac
-from JMTucker.Tools.ROOTTools import ROOT, data_mc_comparison, set_style, plot_saver
+from JMTucker.Tools.ROOTTools import ROOT, data_mc_comparison, set_style, plot_saver, plot_dir
 
-root_file_dir = '/uscms_data/d2/tucker/crab_dirs/HistosV15_v2'
-plot_dir = 'plots/EXO-17-018/dbv'
+year = '2017p8'
+version = 'V23m'
+root_file_dir = '/uscms_data/d2/tucker/crab_dirs/Histos%s' % version
 
 set_style()
-ps = plot_saver(plot_dir, pdf_log=True)
+ps = plot_saver(plot_dir('dbv_%s_%s' % (year, version)), pdf_log=True)
 
-int_lumi_2015 = ac.int_lumi_2015 * ac.scale_factor_2015
-int_lumi_2016 = ac.int_lumi_2016 * ac.scale_factor_2016
-int_lumi = ac.int_lumi_2015p6 * ac.scale_factor_2015p6
-int_lumi_nice = ac.int_lumi_nice_2015p6
+int_lumi = ac.int_lumi_2017p8 * ac.scale_factor_2017p8
+int_lumi_nice = ac.int_lumi_nice_2017p8
 
-qcd_samples = Samples.qcd_samples_sum_2015 + Samples.qcd_samples_sum
-ttbar_samples = Samples.ttbar_samples_2015 + Samples.ttbar_samples
-signal_sample = Samples.mfv_neu_tau01000um_M0800
-data_samples = Samples.data_samples_2015 + Samples.data_samples
+qcd_samples = Samples.qcd_samples_2017[1:] + Samples.qcd_samples_2018
+ttbar_samples = Samples.ttbar_samples_2017
+signal_sample = Samples.mfv_neu_tau001000um_M0800_2017
+data_samples = Samples.data_samples_2017
 
 background_samples = ttbar_samples + qcd_samples
 for s in qcd_samples:
@@ -38,8 +37,6 @@ C = partial(data_mc_comparison,
             plot_saver = ps,
             file_path = os.path.join(root_file_dir, '%(name)s.root'),
             int_lumi = int_lumi,
-            int_lumi_2015 = int_lumi_2015,
-            int_lumi_2016 = int_lumi_2016,
             int_lumi_nice = int_lumi_nice,
             canvas_top_margin = 0.08,
             overflow_in_last = True,
@@ -52,12 +49,12 @@ C = partial(data_mc_comparison,
             )
 
 C('dbv',
-  data_samples = data_samples,
-  normalize_to_data = True,
-  simulation = False,
-  histogram_path = 'mfvVertexHistosOnlyOneVtx/h_sv_all_bsbs2ddist',
+#  data_samples = data_samples,
+#  normalize_to_data = True,
+#  simulation = False,
+  histogram_path = 'Ntk3mfvVertexHistosOnlyOneVtx/h_sv_all_bsbs2ddist',
   x_title = 'd_{BV} (cm)',
   y_title = 'Events/50 #mum',
   x_range = (0, 0.4),
-  y_range = (1, 1e3),
+  y_range = (1, 1e5),
   )
