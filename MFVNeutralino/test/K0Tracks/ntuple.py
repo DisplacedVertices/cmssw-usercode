32b2b8d6c5f8e08787d9e1396e98d72ac22dd8a3
Jordan Tucker <jordantucker@gmail.com>
2019-06-05 11:17:49 -0500

    v25v1

--- MFVNeutralino/test/K0Tracks/ntuple.py
+++ MFVNeutralino/test/K0Tracks/ntuple.py
@@ -5,7 +5,7 @@ settings.is_mc = True
 settings.is_miniaod = True
 settings.event_filter = 'jets only novtx'
 
-version = settings.version + 'v5'
+version = settings.version + 'v1'
 
 debug = False
 
@@ -38,13 +38,8 @@ ReferencedTagsTaskAdder(process)('p')
 
 if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
     from JMTucker.Tools.MetaSubmitter import *
-    from JMTucker.Tools import Samples
 
-    if year == 2017:
-        samples = Samples.data_samples_2017 + Samples.qcd_samples_2017
-    elif year == 2018:
-        samples = Samples.data_samples_2018 + Samples.qcd_samples_2018
-
+    samples = pick_samples(dataset, ttbar=False, all_signal=False)
     set_splitting(samples, dataset, 'default', json_path('ana_2017p8.json'), 4)
 
     ms = MetaSubmitter('K0Ntuple' + version, dataset=dataset)
