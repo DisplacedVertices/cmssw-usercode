1d93f88fb33291317952bc32467b47ae58dd00e5
Jordan Tucker <jordantucker@gmail.com>
2019-06-06 20:22:36 -0500

    restore batch name; splitting

--- MFVNeutralino/test/K0Tracks/hists.submit.py
+++ MFVNeutralino/test/K0Tracks/hists.submit.py
@@ -1,4 +1,19 @@
 from JMTucker.Tools.MetaSubmitter import *
 
+dataset = 'nr_k0ntuplev25mv1'
 samples = pick_samples(dataset, ttbar=False, all_signal=False)
-NtupleReader_submit('K0HistsV25mv1', 'nr_k0ntuplev25mv1', samples)
+NtupleReader_submit('K0HistsV25mv1_nsigdxy0p0_rhomin0p268_ctaumin0p0268_costh2min0p99975', dataset, samples,
+                    split = {'qcdht1000_2017': 2,
+                             'qcdht1000_2018': 2,
+                             'qcdht1500_2017': 5,
+                             'qcdht1500_2018': 5,
+                             'qcdht2000_2017': 3,
+                             'qcdht2000_2018': 3,
+                             'JetHT2017C': 2,
+                             'JetHT2017E': 2,
+                             'JetHT2017F': 3,
+                             'JetHT2018A': 4,
+                             'JetHT2018B': 2,
+                             'JetHT2018C': 2,
+                             'JetHT2018D': 5
+                             })
