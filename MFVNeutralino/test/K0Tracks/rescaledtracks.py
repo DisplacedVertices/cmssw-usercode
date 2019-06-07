c61dfca9d917218d169ccd0e0c9ca93757b04418
Jordan Tucker <jordantucker@gmail.com>
2019-06-07 14:07:26 -0500

    overlay inclusive plots

--- MFVNeutralino/test/K0Tracks/rescaledtracks.py
+++ MFVNeutralino/test/K0Tracks/rescaledtracks.py
@@ -5,41 +5,49 @@ set_style()
 
 batch = sys.argv[1]
 eta = batch.split('tketa')[1]
+incl = '/uscms_data/d3/dquach/crab3dirs/TrackingTreerHistsV23mv3_eta%s' % eta
 
 ps = plot_saver(plot_dir('v0bkgsub_%s/cfrescaled' % batch), size=(600,600), log=False)
 
 eras = fcns.eras[:]
 de = eras.index('2017DE')
 eras = eras[:de] + ['2017D','2017E'] + eras[de+1:]
+
 for era in eras:
     year = era[:4]
 
     samples = ['background_%s' % year, 'JetHT' + era]
+
     fs = [ROOT.TFile(os.path.join(batch, '%s.root' % s)) for s in samples]
     hs = [f.Get('h_tracks_dxyerr_v_pt/hsig') for f in fs]
-    mc, data = pfs = [h.ProfileX('%s_%s' % (s, h.GetName())) for s,h in zip(samples,hs)]
 
-    mc.SetLineColor(2)
-    data.SetLineColor(1)
-    mc.nice, data.nice = year + 'MC', 'JetHT' + era
+    incl_fs = [ROOT.TFile(os.path.join(incl, '%s.root' % s)) for s in samples]
+    hs += [f.Get('h_sel_tracks_dxyerr_v_pt') for f in incl_fs]
 
-    for pf in pfs:
+    mc, data, incl_mc, incl_data = pfs = [h.ProfileX('%s_%s_%i' % (s, h.GetName(), i)) for i,(s,h) in enumerate(zip(samples*2,hs))]
+
+    mc.nice = year + 'MC'
+    data.nice = 'JetHT' + era
+    incl_mc.nice = 'inclusive ' + year + 'MC'
+    incl_data.nice = 'inclusive JetHT' + era
+
+    for pf,c in zip(pfs, [2,1,6,12]):
         pf.SetLineWidth(2)
+        pf.SetLineColor(c)
         pf.GetYaxis().SetTitle('<dxyerr> (cm)')
 
     fcn = fcns(era, eta)
     fcn.SetLineWidth(2)
-    fcn.SetLineStyle(10)
     fcn.SetLineColor(4)
 
     rp = ratios_plot('dxyerr_v_pt_%s' % era, pfs,
                      plot_saver=ps,
-                     legend=(0.4,0.8,0.8,0.9),
+                     legend=(0.4,0.7,0.9,0.9),
                      x_range=(0.9,25),
                      y_range=(0, 0.011),
                      res_divide_opt={'confint': propagate_ratio, 'force_le_1': False},
                      res_fit=False,
                      res_y_title='data/MC',
-                     res_y_range=(0.8,1.8),
-                     res_fcns = [fcn],
+                     res_y_range=(0.9,1.5),
+                     res_fcns = [(fcn, 'prescribed rescaling')],
                      )
