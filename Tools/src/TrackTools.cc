172b18dd649d9bb2a744890d65c2de32798dd677
Jordan Tucker <jordantucker@gmail.com>
2019-06-19 21:41:50 -0500

    unbug

--- Tools/src/TrackTools.cc
+++ Tools/src/TrackTools.cc
@@ -6,16 +6,15 @@ namespace jmt {
     if (level < 0)
       return true;
 
-    if (tk.pt() < 1 ||
-        tk.hitPattern().pixelLayersWithMeasurement() < 2 ||
-        tk.hitPattern().stripLayersWithMeasurement() < 6 ||
-        (level >= 1 && !tk.hitPattern().hasValidHitInPixelLayer(PixelSubdetector::PixelBarrel,1))
-        )
-      return false;
-
-    if (!bs)
+    if (level >= 2 && !bs)
       throw std::invalid_argument("need beamspot for level 2");
 
-    return fabs(tk.dxy(*bs)) / tk.dxyError() > 4;
+    return
+      tk.pt() >= 1 &&
+      tk.hitPattern().pixelLayersWithMeasurement() >= 2 &&
+      tk.hitPattern().stripLayersWithMeasurement() >= 6 &&
+      (level < 1 || tk.hitPattern().hasValidHitInPixelLayer(PixelSubdetector::PixelBarrel,1)) &&
+      (level < 2 || fabs(tk.dxy(*bs)) / tk.dxyError() > 4)
+      ;
   }
 }
