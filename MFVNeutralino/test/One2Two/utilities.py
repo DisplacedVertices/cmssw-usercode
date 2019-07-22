0f397dac68d0fb58e0bf1c520e604446ab11db3b
Joseph Reichert <Joseph.Reichert@cern.ch>
2019-07-22 17:07:55 -0400

    more automation + might as well add more precision for the f2 values since we don't need to copy by hand anymore

--- MFVNeutralino/test/One2Two/utilities.py
+++ MFVNeutralino/test/One2Two/utilities.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 from JMTucker.MFVNeutralino.UtilitiesBase import *
-
+from f2_vals import dict_of_f2_tuples
 ####
 
 _version = 'V25m'
 
 def cmd_merge_bquarks_nobquarks():
-    for year in '2017', '2018' :
+    for year in ['2017', '2018'] :
         if year == '2017' :
             weights = '0.79,0.21'
         elif year == '2018' :
@@ -22,31 +22,14 @@ def cmd_merge_bquarks_nobquarks():
             os.system(cmd)
 
 def cmd_merge_btags_nobtags():
-    for year in '2017', '2018', '2017p8',:
-        if year == '2017' :
-            tuple_ntracks_weights                    = (3,'0.830,0.170'), (4,'0.853,0.147'), (5,'0.925,0.075'), (7,'0.832,0.168')
-            tuple_ntracks_weights_bcjet_SFs_up       = (3,'0.809,0.191'), (4,'0.832,0.168'), (5,'0.911,0.089'), (7,'0.811,0.189')
-            tuple_ntracks_weights_bcjet_SFs_down     = (3,'0.855,0.145'), (4,'0.876,0.124'), (5,'0.939,0.061'), (7,'0.857,0.143')
-            tuple_ntracks_weights_ljet_SFs_up        = (3,'0.831,0.169'), (4,'0.853,0.147'), (5,'0.924,0.076'), (7,'0.833,0.167')
-            tuple_ntracks_weights_ljet_SFs_down      = (3,'0.832,0.168'), (4,'0.853,0.147'), (5,'0.925,0.075'), (7,'0.834,0.166')
-            tuple_ntracks_weights_data               = (3,'0.830,0.170'), (4,'0.856,0.144')
-        elif year == '2018' :
-            tuple_ntracks_weights                    = (3,'0.860,0.140'), (4,'0.899,0.101'), (5,'0.968,0.032'), (7,'0.861,0.139')
-            tuple_ntracks_weights_bcjet_SFs_up       = (3,'0.844,0.156'), (4,'0.886,0.114'), (5,'0.961,0.039'), (7,'0.847,0.153')
-            tuple_ntracks_weights_bcjet_SFs_down     = (3,'0.877,0.123'), (4,'0.914,0.086'), (5,'0.976,0.024'), (7,'0.879,0.121')
-            tuple_ntracks_weights_ljet_SFs_up        = (3,'0.861,0.139'), (4,'0.901,0.099'), (5,'0.969,0.031'), (7,'0.863,0.137')
-            tuple_ntracks_weights_ljet_SFs_down      = (3,'0.859,0.141'), (4,'0.898,0.102'), (5,'0.969,0.031'), (7,'0.861,0.139')
-            tuple_ntracks_weights_data               = (3,'0.860,0.140'), (4,'0.902,0.098')
-        elif year == '2017p8' :
-            tuple_ntracks_weights                    = (3,'0.851,0.149'), (4,'0.884,0.116'), (5,'0.952,0.048'), (7,'0.854,0.146')
-            tuple_ntracks_weights_bcjet_SFs_up       = (3,'0.833,0.167'), (4,'0.868,0.132'), (5,'0.943,0.057'), (7,'0.837,0.163')
-            tuple_ntracks_weights_bcjet_SFs_down     = (3,'0.870,0.130'), (4,'0.901,0.099'), (5,'0.962,0.038'), (7,'0.874,0.126')
-            tuple_ntracks_weights_ljet_SFs_up        = (3,'0.852,0.148'), (4,'0.885,0.115'), (5,'0.952,0.048'), (7,'0.855,0.145')
-            tuple_ntracks_weights_ljet_SFs_down      = (3,'0.851,0.149'), (4,'0.883,0.117'), (5,'0.952,0.048'), (7,'0.854,0.146')
-            tuple_ntracks_weights_data               = (3,'0.851,0.149'), (4,'0.887,0.113')
-        else :
-            print("Unknown year (%s)! Exiting." % year)
-            sys.exit()
+
+    for year in ['2017', '2018', '2017p8'] :
+        tuple_ntracks_weights                    = dict_of_f2_tuples[year+'_nom']
+        tuple_ntracks_weights_bcjet_SFs_up       = dict_of_f2_tuples[year+'_bcjet_up']
+        tuple_ntracks_weights_bcjet_SFs_down     = dict_of_f2_tuples[year+'_bcjet_down']
+        tuple_ntracks_weights_ljet_SFs_up        = dict_of_f2_tuples[year+'_ljet_up']
+        tuple_ntracks_weights_ljet_SFs_down      = dict_of_f2_tuples[year+'_ljet_down']
+        tuple_ntracks_weights_data               = dict_of_f2_tuples['data_'+year+'_nom'] # FIXME note that we'll have to add systs here for data too
 
         # for the f2 variation
         btag_frac_threetrk = float(tuple_ntracks_weights[0][1].split(",")[0])
@@ -67,18 +50,18 @@ def cmd_merge_btags_nobtags():
             print cmd
             os.system(cmd)
 
-            # f2 variation up
-            weight_btag_up   = min( float(weights.split(",")[0]) * (1.0/(1+frac_variation)*(1+2*frac_variation)), 1)
-            weight_nobtag_up = 1-weight_btag_up
-            weights_up = '%.3f,%.3f' % (weight_btag_up, weight_nobtag_up)
-            cmd = 'mergeTFileServiceHistograms -w %s -i %s -o 2v_from_jets_%s_%dtrack_btag_corrected_vary_3trk_to_5trk_up_%s.root' % (weights_up, ' '.join(files), year, ntracks, _version)
-            print cmd
-            os.system(cmd)
+            # f2 variation up (currently not meaningful; will delete if we settle on the 4-trk variation...)
+            #weight_btag_up   = min( float(weights.split(",")[0]) * (1.0/(1+frac_variation)*(1+2*frac_variation)), 1)
+            #weight_nobtag_up = 1-weight_btag_up
+            #weights_up = '%f,%f' % (weight_btag_up, weight_nobtag_up)
+            #cmd = 'mergeTFileServiceHistograms -w %s -i %s -o 2v_from_jets_%s_%dtrack_btag_corrected_vary_3trk_to_5trk_up_%s.root' % (weights_up, ' '.join(files), year, ntracks, _version)
+            #print cmd
+            #os.system(cmd)
 
             # f2 variation down
             weight_btag_down   = min( float(weights.split(",")[0]) * (1.0/(1+frac_variation)*(1+frac_variation_var)), 1)
             weight_nobtag_down = 1-weight_btag_down
-            weights_down = '%.3f,%.3f' % (weight_btag_down, weight_nobtag_down)
+            weights_down = '%f,%f' % (weight_btag_down, weight_nobtag_down)
             cmd = 'mergeTFileServiceHistograms -w %s -i %s -o 2v_from_jets_%s_%dtrack_btag_corrected_vary_3trk_to_5trk_down_%s.root' % (weights_down, ' '.join(files), year, ntracks, _version)
             print cmd
             os.system(cmd)
@@ -91,6 +74,15 @@ def cmd_merge_btags_nobtags():
                     raise RuntimeError('%s not found' % fn)
 
             cmd = 'mergeTFileServiceHistograms -w %s -i %s -o 2v_from_jets_data_%s_%dtrack_btag_corrected_%s.root' % (weights, ' '.join(files), year, ntracks, _version)
+            print cmd
+            os.system(cmd)
+
+            # FIXME Note that we vary by the MC f2 ratio rather than data here
+            # f2 variation down
+            weight_btag_down   = min( float(weights.split(",")[0]) * (1.0/(1+frac_variation)*(1+frac_variation_var)), 1)
+            weight_nobtag_down = 1-weight_btag_down
+            weights_down = '%f,%f' % (weight_btag_down, weight_nobtag_down)
+            cmd = 'mergeTFileServiceHistograms -w %s -i %s -o 2v_from_jets_data_%s_%dtrack_btag_corrected_vary_3trk_to_5trk_down_%s.root' % (weights_down, ' '.join(files), year, ntracks, _version)
             print cmd
             os.system(cmd)
 
