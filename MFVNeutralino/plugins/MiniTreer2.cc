bb31d9ba8e8e88212ae55eed889a849f29c7f6d6
Jordan Tucker <jordantucker@gmail.com>
2019-08-27 15:57:13 -0500

    add debugging info to exception when vertex track not found

--- MFVNeutralino/plugins/MiniTreer2.cc
+++ MFVNeutralino/plugins/MiniTreer2.cc
@@ -7,6 +7,7 @@
 #include "JMTucker/MFVNeutralino/interface/Ntuple.h"
 #include "JMTucker/MFVNeutralino/interface/NtupleFiller.h"
 #include "JMTucker/MFVNeutralinoFormats/interface/VertexAux.h"
+#include "JMTucker/Tools/interface/ExtValue.h"
 #include "JMTucker/Tools/interface/Utilities.h"
 
 class MFVMiniTreer2 : public edm::EDAnalyzer {
@@ -68,23 +69,25 @@ void MFVMiniTreer2::analyze(const edm::Event& event, const edm::EventSetup&) {
     else if (v.ntracks() >= 5) ++n5;
 
     for (size_t i = 0, ie = v.ntracks(); i < ie; ++i) {
-      double dist2min = 0.1;
-      int which = -1;
-      for (size_t j = 0, je = nt.tracks().n(); j < je; ++j) {
-        const double dist2 = mag2(v.track_qpt(i) - nt.tracks().qpt(j),
-                                  v.track_eta[i] - nt.tracks().eta(j),
-                                  v.track_phi[i] - nt.tracks().phi(j));
-        if (dist2 < dist2min) {
-          dist2min = dist2;
-          which = j;
-        }
+      jmt::MinValue mindist2(0.1);
+      for (size_t j = 0, je = nt.tracks().n(); j < je; ++j)
+        mindist2(j, mag2(v.track_qpt(i) - nt.tracks().qpt(j),
+                         v.track_eta[i] - nt.tracks().eta(j),
+                         v.track_phi[i] - nt.tracks().phi(j)));
+
+      if (mindist2.i() == -1) {
+        cms::Exception ce("BadAssumption");
+        ce << "vertex w " << v.ntracks() << " tracks @ <" << v.x << ", " << v.y << ", " << v.z << ">: track " << i << " <" << v.track_qpt(i) << ", " << v.track_eta[i] << ", " << v.track_phi[i] << "> not found in " << nt.tracks().n() << " tracks in general collection:\n";
+        for (size_t j = 0, je = nt.tracks().n(); j < je; ++j)
+          ce << j << ": <" << nt.tracks().qpt(j) << ", " << nt.tracks().eta(j) << ", " << nt.tracks().phi(j) << ">\n";
+        //std::cout << ce <<"\n";
+        throw ce;
       }
 
-      assert(which != -1);
-      assert(nt.tracks().which_sv(which) == 255);
+      assert(nt.tracks().which_sv(mindist2.i()) == 255);
       const size_t iv = nt.vertices().n() - 1;
       assert(iv < 255);
-      nt.tracks().set_which_sv(which, iv);
+      nt.tracks().set_which_sv(mindist2.i(), iv);
     }
   }
 
