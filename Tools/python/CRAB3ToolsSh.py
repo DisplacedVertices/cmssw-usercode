1b03ac11239a00d5a3c573296298fecf483ee992
Jordan Tucker <jordantucker@gmail.com>
2019-08-28 10:21:43 -0500

    crab/cs_hadd: don't try to chmod eos target

--- Tools/python/CRAB3ToolsSh.py
+++ Tools/python/CRAB3ToolsSh.py
@@ -154,7 +154,7 @@ def crab_hadd(working_dir, new_name=None, new_dir=None, raise_on_empty=False, ch
         print working_dir, ': just one file found, copying'
         cmd = 'xrdcp -s %s %s' % (files[0], new_name)
         result.success = os.system(cmd) == 0
-        if result.success:
+        if result.success and not new_name.startswith('root://'):
             os.chmod(new_name, 0644)
     else:
         result.success = hadd(new_name, files)
