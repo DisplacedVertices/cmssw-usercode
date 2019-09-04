a183013bdbc121780c2c1d162fc3614ef274b59d
Jordan Tucker <jordantucker@gmail.com>
2019-09-04 09:06:16 -0500

    define the year number

--- Tools/interface/Year.h
+++ Tools/interface/Year.h
@@ -3,4 +3,12 @@
 
 #define MFVNEUTRALINO_2017
 
+#ifdef MFVNEUTRALINO_2017
+#define MFVNEUTRALINO_YEAR 2017
+#elif defined(MFVNEUTRALINO_2018)
+#define MFVNEUTRALINO_YEAR 2018
+#else
+#error bad year
+#endif
+
 #endif
--- Tools/python/Year.py
+++ Tools/python/Year.py
@@ -8,7 +8,9 @@ magic = '#define MFVNEUTRALINO_'
 for line in open(os.path.join(os.environ['CMSSW_BASE'], 'src/JMTucker/Tools/interface/Year.h')):
     line = line.strip()
     if line.startswith(magic):
-        assert year is None
+        if year is not None:
+            assert line.startswith(magic + 'YEAR 201')
+            continue
         year = int(line.replace(magic, ''))
 
 assert year in (2015, 2016, 2017, 2018)
