2f9d4e71263565c1a6c7eee7ec4959c8b030ba73
Jordan Tucker <jordantucker@gmail.com>
2019-09-13 09:37:40 -0500

    forget trying to get enum types right

--- MFVNeutralino/interface/Ntuple.h
+++ MFVNeutralino/interface/Ntuple.h
@@ -180,10 +180,10 @@ namespace mfv {
     float edbv(int i) const { return rescale_bs2derr(i); }
     template <typename BS> float dbv(int i, const BS& bs) const { return std::hypot(x(i) - bs.x(z(i)), y(i) - bs.y(z(i))); }
 
-    enum Nm1 { nm1_none, nm1_dbv, nm1_edbv, nm1_beampipe, nm1_all, max_nm1 }; // none = regular cuts applied, all = drop all cuts
+    enum { nm1_none, nm1_dbv, nm1_edbv, nm1_beampipe, nm1_all, max_nm1 }; // none = regular cuts applied, all = drop all cuts
 
     template <typename BS> bool pass(int i, const BS& bs, const int min_ntracks=-1, const int max_ntracks=-1,
-                                     Nm1 nm1=nm1_none, const double min_dbv=0.01, const double max_edbv=0.0025) const {
+                                     int nm1=nm1_none, const double min_dbv=0.01, const double max_edbv=0.0025) const {
       return
         (min_ntracks < 0 || ntracks(i) >= min_ntracks) &&
         (max_ntracks < 0 || ntracks(i) <= max_ntracks) &&
@@ -193,7 +193,7 @@ namespace mfv {
     }
 
     template <typename BS> std::vector<int> pass(const BS& bs, const int min_ntracks=-1, const int max_ntracks=-1,
-                                                 Nm1 nm1=nm1_none, const double min_dbv=0.01, const double max_edbv=0.0025) const {
+                                                 int nm1=nm1_none, const double min_dbv=0.01, const double max_edbv=0.0025) const {
       std::vector<int> r;
       for (int i = 0, ie = n(); i < ie; ++i)
         if (pass(i, bs, min_ntracks, max_ntracks, nm1, min_dbv, max_edbv))
@@ -202,7 +202,7 @@ namespace mfv {
     }
 
     template <typename BS> std::vector<int> npass(const BS& bs, const int min_ntracks=-1, const int max_ntracks=-1,
-                                                  Nm1 nm1=nm1_none, const double min_dbv=0.01, const double max_edbv=0.0025) const {
+                                                  int nm1=nm1_none, const double min_dbv=0.01, const double max_edbv=0.0025) const {
       return pass(bs, min_ntracks, max_ntracks, nm1, min_dbv, max_edbv).size();
     }
 
