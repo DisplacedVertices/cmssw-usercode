6762ace399cf7c7e0bb18712f626a634a3900de4
Jordan Tucker <jordantucker@gmail.com>
2019-09-12 23:25:59 -0500

    get rid of VertexTools: consolidate stuff only used in VertexAuxProducer there, remaining const goes to VertexerParams

---
+++ MFVNeutralino/interface/VertexerParams.h
@@ -0,0 +1,8 @@
+#ifndef JMTucker_MFVNeutralino_VertexerParams_h
+#define JMTucker_MFVNeutralino_VertexerParams_h
+
+namespace mfv {
+  static constexpr double track_vertex_weight_min = 0.5;
+}
+
+#endif
