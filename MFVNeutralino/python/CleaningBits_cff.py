721761071ba162f314459cdfed06ec263539f5a1
Jordan Tucker <jordantucker@gmail.com>
2019-10-03 15:01:24 -0500

    obsolete

--- MFVNeutralino/python/CleaningBits_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-mfvCleaningBits = cms.EDProducer('MFVCleaningBits',
-                                 cleaning_results_src = cms.InputTag('TriggerResults', '', 'PAT')
-                                 )
-
-pmfvCleaningBits = cms.EndPath(mfvCleaningBits)
