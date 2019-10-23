47c4ce34252a2a0dff41cc973c5c76af962f5092
Dan Quach <quach.dan@gmail.com>
2019-10-23 10:17:05 -0500

    ratio plots in vertexer eff

--- MFVNeutralino/test/One2Two/vertexer_eff.py
+++ MFVNeutralino/test/One2Two/vertexer_eff.py
@@ -21,7 +21,6 @@ for is_mc in False, True:
 
         for ntkseeds in True, False:
             ps = plot_saver(plot_dir('VertexerPairEffs%s/vertexer_pair_effs%s_%s%s' % (version.capitalize(), '' if is_mc else '_data', year, '_ntkseeds' if ntkseeds else '')), size=(700,700), log=False)
-
             fh = ROOT.TFile('vpeffs%s_%s_%s%s.root' % ('' if is_mc else '_data', year, version, '_ntkseeds' if ntkseeds else ''), 'recreate')
             h_merges, h_adds = {}, {}
 
@@ -43,7 +42,7 @@ for is_mc in False, True:
                     h.Divide(h, h_pairs, 1,1,'B')
                     for i in xrange(1, h.GetNbinsX()+2):
                         h.SetBinContent(i, 1-h.GetBinContent(i))
-                    h.Scale(1./h.GetBinContent(h.GetNbinsX()))
+#                    h.Scale(1./h.GetBinContent(h.GetNbinsX()))
                     h.SetTitle('maxtk%i%s;d_{VV} (cm);efficiency' % (itk, ' merge' if is_merge else ''))
                     h.GetYaxis().SetRangeUser(0,1.05)
                     h.SetLineColor(ROOT.kBlue if is_merge else ROOT.kGreen+2)
@@ -82,6 +81,7 @@ for is_mc in False, True:
 
             if not ntkseeds:
                 h = h_adds[3]
+                h.GetXaxis().SetRangeUser(0,0.4)
                 h.Draw('hist')
                 if not is_mc and year == '2015p6':
                     write(61, 0.050, 0.098, 0.913, 'CMS')
@@ -91,18 +91,27 @@ for is_mc in False, True:
 
                 ROOT.TH1.AddDirectory(0)
 
-                h2 = ROOT.TFile('vpeffs%s_%s_%s_ntkseeds.root' % ('' if is_mc else '_data', year, version)).Get('maxtk5')
-                if not is_mc and year == '2015p6':
-                    h2.Scale(1./h2.GetMaximum())
-                h2.SetLineWidth(3)
-                h.Draw('hist')
-                h2.Draw('hist sames')
-                l = ROOT.TLegend(0.50,0.15,0.85,0.30)
-                l.AddEntry(h, 'default')
-                l.AddEntry(h2, 'variation')
-                l.Draw()
-                if not is_mc and year == '2015p6':
-                    write(61, 0.050, 0.098, 0.913, 'CMS')
-                    write(52, 0.035, 0.200, 0.913, 'Preliminary')
-                    write(42, 0.050, 0.560, 0.913, '38.5 fb^{-1} (13 TeV)')
-                ps.save('compare_efficiency')
+                for ntk in [3, 4, 5]:
+                    h2 = ROOT.TFile('vpeffs%s_%s_%s_ntkseeds.root' % ('' if is_mc else '_data', year, version)).Get('maxtk%s' % ntk)
+                    if not is_mc and year == '2015p6':
+                        h2.Scale(1./h2.GetMaximum())
+                    h2.SetLineWidth(3)
+                    h.GetXaxis().SetRangeUser(0,0.4)
+                    h.Draw('hist')
+                    h2.Draw('hist sames')
+                    l = ROOT.TLegend(0.50,0.15,0.85,0.30)
+                    l.AddEntry(h, 'default')
+                    l.AddEntry(h2, '%s-track variation' % ntk)
+                    l.Draw()
+                    if not is_mc and year == '2015p6':
+                        write(61, 0.050, 0.098, 0.913, 'CMS')
+                        write(52, 0.035, 0.200, 0.913, 'Preliminary')
+                        write(42, 0.050, 0.560, 0.913, '38.5 fb^{-1} (13 TeV)')
+                    ps.save('compare_efficiency_%s-track' % ntk)
+    
+                    ratio = h2.Clone('ratio')
+                    ratio.Divide(h)
+                    ratio.GetYaxis().SetRangeUser(0.5, 6)
+                    ratio.GetXaxis().SetRangeUser(0,0.4)
+                    ratio.Draw('hist')
+                    ps.save('variation_ratio_%s-track' % ntk)
