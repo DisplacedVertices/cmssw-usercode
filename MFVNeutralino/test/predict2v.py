cfc05949c4803026d549e3cd8c65a2c029e205a0
Jordan Tucker <jordantucker@gmail.com>
2019-11-29 15:02:10 -0600

    year, data from cmd line

--- MFVNeutralino/test/predict2v.py
+++ MFVNeutralino/test/predict2v.py
@@ -1,12 +1,15 @@
 from JMTucker.Tools.ROOTTools import *
+from JMTucker.Tools.general import *
 
 presel_path = '/uscms_data/d2/tucker/crab_dirs/PreselHistosV27m'
-sel_path = '/uscms_data/d2/tucker/crab_dirs/HistosV27m/100pc'
+sel_path = '/uscms_data/d2/tucker/crab_dirs/HistosV27m'
+data = bool_from_argv('data')
+year = '2017' if len(sys.argv) < 2 else sys.argv[1]
 
-if 'data' in sys.argv:
-    fn, presel_scale = 'JetHT2017p8.root', 1.
+if data:
+    fn, presel_scale = 'JetHT%s.root' % year, 1.
 else:
-    fn, presel_scale = 'background_2017.root', 1.
+    fn, presel_scale = 'background_%s.root' % year, 1.
 
 presel_f = ROOT.TFile(os.path.join(presel_path, fn))
 sel_f = ROOT.TFile(os.path.join(sel_path, fn))
@@ -14,11 +17,16 @@ sel_f = ROOT.TFile(os.path.join(sel_path, fn))
 npresel, enpresel = get_integral(presel_f.Get('mfvEventHistosJetPreSel/h_npu'))
 npresel  *= presel_scale
 enpresel *= presel_scale
+print 'year:', year
 print 'presel events predicted: %8.0f +- %4.0f' % (npresel, enpresel)
 print '%16s %24s %19s %19s %15s' % ('n1v', 'e1v', 'pred n2v', 'n2v', 'ratio')
 for ntk in 'Ntk3', 'Ntk4', '':
     n1v, en1v = get_integral(sel_f.Get('%smfvEventHistosOnlyOneVtx/h_npu' % ntk))
-    n2v, en2v = get_integral(sel_f.Get('%smfvEventHistosFullSel/h_npu' % ntk))
+    if data and ntk == '':
+        print '5t2v blind in data'
+        n2v, en2v = 1e-3, 1e-3
+    else:
+        n2v, en2v = get_integral(sel_f.Get('%smfvEventHistosFullSel/h_npu' % ntk))
     e1v, ee1v = effective_wilson_score_vpme(n1v, en1v, npresel, enpresel)
     pred = e1v**2 * npresel
     epred = (2 * e1v * npresel * ee1v**2 + e1v**2 * enpresel)**0.5
