5449a5d0b2b5f16d23fa406446a16c2a55995ec9
Jordan Tucker <jordantucker@gmail.com>
2019-11-29 14:47:36 -0600

    temporary scale factors (never re-ran on 2018 samples?); scaled_int_lumi_* vars

--- MFVNeutralino/interface/AnalysisConstants.h
+++ MFVNeutralino/interface/AnalysisConstants.h
@@ -14,7 +14,10 @@ namespace mfv {
     int_lumi_nice_2017 = "  41.5 fb^{-1} (13 TeV)";
 
     const double
-    scale_factor_2017 = 1.;
+    scale_factor_2017 = 0.989;
+
+    const double
+    scaled_int_lumi_2017 = int_lumi_2017 * scale_factor_2017;
 
     const double
     int_lumi_2018 = 59693.;
@@ -23,16 +26,22 @@ namespace mfv {
     int_lumi_nice_2018 = "  59.7 fb^{-1} (13 TeV)";
 
     const double
-    scale_factor_2018 = 1.;
+    scale_factor_2018 = 0.989;
 
     const double
-    int_lumi_2017p8 = 41521. + 59693.;
+    scaled_int_lumi_2018 = int_lumi_2018 * scale_factor_2018;
+
+    const double
+    int_lumi_2017p8 = int_lumi_2017 + int_lumi_2018;
 
     const char*
     int_lumi_nice_2017p8 = "  101 fb^{-1} (13 TeV)";
 
     const double
-    scale_factor_2017p8 = 1.;
+    scale_factor_2017p8 = 0.989;
+
+    const double
+    scaled_int_lumi_2017p8 = int_lumi_2017p8 * scale_factor_2017p8;
   }
 }
 
