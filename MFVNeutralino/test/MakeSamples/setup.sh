fc131ae30d4934f80e8a632abcd93012e6e6e5c6
Jordan Tucker <jordantucker@gmail.com>
2019-12-11 17:00:20 -0600

    2017+8 minbias files

--- MFVNeutralino/test/MakeSamples/setup.sh
+++ MFVNeutralino/test/MakeSamples/setup.sh
@@ -1,2 +1,3 @@
-#ln -s /uscms/home/tucker/public/mfv/private_minbias.txt.gz
-ln -s /uscms/home/tucker/public/mfv/minbias_premix_2017.txt.gz minbias_premix.txt.gz
+ln -s /uscms/home/tucker/public/mfv/minbias_premix_2017.txt.gz
+ln -s /uscms/home/tucker/public/mfv/minbias_premix_2018.txt.gz
+
