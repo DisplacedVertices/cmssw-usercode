22ef6e0bccb83c674b6592d2397b462856350a0e
Jordan Tucker <jordantucker@gmail.com>
2019-12-20 13:55:34 -0600

    increment minor version

--- MFVNeutralino/test/TrackMover/mctruth.py
+++ MFVNeutralino/test/TrackMover/mctruth.py
@@ -5,7 +5,7 @@ settings.is_mc = True
 settings.is_miniaod = True
 settings.event_filter = 'jets only novtx'
 
-version = settings.version + 'v1'
+version = settings.version + 'v2'
 
 process = ntuple_process(settings)
 remove_output_module(process)
