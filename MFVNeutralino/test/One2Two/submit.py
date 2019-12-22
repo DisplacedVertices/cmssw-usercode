b548dfafebd712ae4982afd71e1649d933f05c01
Jordan Tucker <jordantucker@gmail.com>
2019-12-22 17:00:55 -0600

    switch to new combine version and split toys over multiple jobs

--- MFVNeutralino/test/One2Two/submit.py
+++ MFVNeutralino/test/One2Two/submit.py
@@ -6,9 +6,11 @@ from JMTucker.Tools.CondorSubmitter import CondorSubmitter
 import ROOT; ROOT.gROOT.SetBatch()
 import limitsinput
 
-# the combine tarball is made in a locally checked-out combine environment so the worker nodes don't have to git clone, etc.
-# take JMTucker/Tools/scripts/cmsMakeTarball.py, insert make_tarball in it so it can run standalone, then *in the combine environment* do
-#   ./cmsMakeTarball.py --include-bin combine.tz
+# The combine tarball is made in a locally checked-out combine environment so
+# the worker nodes don't have to git clone, etc. In this environment, run
+#   cmsMakeTarball.py --standalone > /tmp/tarball.py
+# Then, *in the combine environment* do
+#   /tmp/tarball.py --include-bin combine.tgz
 
 script_template = '''#!/bin/bash
 echo combine script starting at $(date) with args $*
@@ -18,13 +20,13 @@ export WHICH=$2
 export WD=$(pwd)
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-export SCRAM_ARCH=slc6_amd64_gcc530
-scram project CMSSW CMSSW_8_1_0 2>&1 >/dev/null
-cd CMSSW_8_1_0/src
+export SCRAM_ARCH=slc7_amd64_gcc700
+scram project CMSSW CMSSW_10_2_13 2>&1 >/dev/null
+cd CMSSW_10_2_13/src
 eval `scram runtime -sh`
 
 cd ..
-xrdcp -s root://cmseos.fnal.gov//store/user/tucker/combine_810.tgz combine.tgz
+xrdcp -s root://cmseos.fnal.gov//store/user/tucker/combine_sl7_801.tgz combine.tgz
 tar xf combine.tgz
 scram b 2>&1 >/dev/null
 hash -r
@@ -37,76 +39,83 @@ cd $WD
     echo datacard:
     python datacard.py $WHICH __DATACARDARGS__ | tee datacard.txt
 
-    cmd="combine -M MarkovChainMC --noDefaultPrior=0 --tries 20 -b 200 --iteration 200000 datacard.txt"
+    cmd="combine -H AsymptoticLimits -M MarkovChainMC --noDefaultPrior=0 --tries 20 -b 200 --iteration 100000 datacard.txt"
 
-    echo "========================================================================="
-    echo Observed limit
-    eval $cmd
-    mv higgsCombine*root observed.root
+    if [[ $JOB == 0 ]]; then
+        echo "========================================================================="
+        echo Observed limit
+        eval $cmd
+        mv higgsCombine*root observed.root
+
+#       echo "========================================================================="
+#       echo Observed limit, no systematics
+#       eval $cmd -S0
+#       mv higgsCombine*root observed_S0.root
+    fi
 
     echo "========================================================================="
     echo Expected limits
-    eval $cmd --toys 1000 --saveToys
-    mv higgsCombine*root expected.root
+    eval $cmd --toys 100 --saveToys -s $((JOB+13068931))
+    mv higgsCombine*root expected_${JOB}.root
 
 #   echo "========================================================================="
-#   echo Observed limit, no systematics
-#   eval $cmd -S0
-#   mv higgsCombine*root observed_S0.root
-#
-#   echo "========================================================================="
 #   echo Expected limits, no systematics
-#   eval $cmd -S0 --toys 500 --saveToys
-#   mv higgsCombine*root expected_S0.root
+#   eval $cmd -S0 --toys 100 --saveToys -s $((JOB+13068931))
+#   mv higgsCombine*root expected_S0_${JOB}.root
+
+########################################################################
 
 #   cmd="combine -M GoodnessOfFit --algo=saturated datacard.txt"
 #
-#   echo "========================================================================="
-#   echo GoodnessOfFit observed
-#   eval $cmd
-#   mv higgsCombine*root gof_observed.root
+#   if [[ $JOB == 0 ]]; then
+#       echo "========================================================================="
+#       echo GoodnessOfFit observed
+#       eval $cmd
+#       mv higgsCombine*root gof_observed.root
+#
+#       echo "========================================================================="
+#       echo GoodnessOfFit observed, no systematics
+#       eval $cmd -S0
+#       combine -S0 -M GoodnessOfFit datacard.txt --algo=saturated
+#       mv higgsCombine*root gof_S0_observed.root
+#   fi
 #
 #   echo "========================================================================="
 #   echo GoodnessOfFit expected
-#   eval $cmd --toys 100
-#   mv higgsCombine*root gof_expected.root
-#
-#   echo "========================================================================="
-#   echo GoodnessOfFit observed, no systematics
-#   eval $cmd -S0
-#   combine -S0 -M GoodnessOfFit datacard.txt --algo=saturated
-#   mv higgsCombine*root gof_S0_observed.root
+#   eval $cmd --toys 100 -s $((JOB+13068931))
+#   mv higgsCombine*root gof_expected_${JOB}.root
 #
 #   echo "========================================================================="
 #   echo GoodnessOfFit expected, no systematics
-#   eval $cmd -S0 --toys 100
-#   mv higgsCombine*root gof_S0_expected.root
+#   eval $cmd -S0 --toys 100 -s $((JOB+13068931))
+#   mv higgsCombine*root gof_S0_expected_${JOB}.root
+
+########################################################################
 
 #   cmd="combine -M Significance datacard.txt"
 #
-#   echo "========================================================================="
-#   echo Observed significance
-#   eval $cmd
-#   mv higgsCombine*root signif_observed.root
+#   if [[ $JOB == 0 ]]; then
+#       echo "========================================================================="
+#       echo Observed significance
+#       eval $cmd
+#       mv higgsCombine*root signif_observed.root
+#       
+#       echo "========================================================================="
+#       echo Observed significance, no systematics
+#       eval $cmd -S0
+#       mv higgsCombine*root signif_observed_S0.root
+#   fi
 #
 #   echo "========================================================================="
 #   echo Expected significance
-#   eval $cmd --toys 100 -saveToys
-#   mv higgsCombine*root signif_expected.root
-#
-#   echo "========================================================================="
-#   echo Observed significance, no systematics
-#   eval $cmd -S0
-#   mv higgsCombine*root signif_observed_S0.root
+#   eval $cmd --toys 100 -saveToys -s $((JOB+13068931))
+#   mv higgsCombine*root signif_expected_${JOB}.root
 #
 #   echo "========================================================================="
 #   echo Expected significances, no systematics
-#   eval $cmd -S0 --toys 100 -saveToys
-#   mv higgsCombine*root signif_expected_S0.root
-} 2>&1 | gzip -c > combine_output.txt.gz
-
-echo run process.py
-python process.py . 2>&1 >results
+#   eval $cmd -S0 --toys 100 -saveToys -s $((JOB+13068931))
+#   mv higgsCombine*root signif_expected_S0_${JOB}.root
+} 2>&1 | gzip -c > combine_output_${JOB}.txt.gz
 '''
 
 if 'save_toys' not in sys.argv:
@@ -137,19 +146,22 @@ notification  = never
 should_transfer_files = YES
 when_to_transfer_output = ON_EXIT
 transfer_input_files = %(input_files)s
-Queue 1
++REQUIRED_OS = "rhel7"
++DesiredOS = REQUIRED_OS
+Queue 50
 '''
 
 batch_root = '/uscms_data/d2/tucker/crab_dirs/combine_output_%i' % time.time()
 if os.path.isdir(batch_root):
     raise IOError('%s exists' % batch_root)
+print batch_root
 os.mkdir(batch_root)
 os.mkdir(os.path.join(batch_root, 'inputs'))
 
 save_git_status(os.path.join(batch_root, 'gitstatus'))
 
 input_files = []
-for x in ['signal_efficiency.py', 'datacard.py', 'process.py', 'limitsinput.root']:
+for x in ['signal_efficiency.py', 'datacard.py', 'limitsinput.root']:
     nx = os.path.abspath(os.path.join(batch_root, 'inputs', os.path.basename(x)))
     shutil.copy2(x, nx)
     input_files.append(nx)
@@ -183,4 +195,4 @@ for sample in samples:
     open(os.path.join(batch_dir, 'cs_dir'), 'wt')
     open(os.path.join(batch_dir, 'cs_submit.jdl'), 'wt').write(jdl_template % locals())
 
-    CondorSubmitter._submit(batch_dir, 1)
+    CondorSubmitter._submit(batch_dir, 50)
