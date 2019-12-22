04b8c6138847cd4989f5ffadcd76a4e01114ca66
Jordan Tucker <jordantucker@gmail.com>
2019-12-22 14:46:56 -0600

    option to dump standalone script

--- Tools/scripts/cmsMakeTarball.py
+++ Tools/scripts/cmsMakeTarball.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys, os, argparse
-from JMTucker.Tools.general import typed_from_argv
 from JMTucker.Tools.CMSSWTools import make_tarball
 
 parser = argparse.ArgumentParser(description = 'cmsMakeTarball.py: make a tarball suitable for distribution to grid sites a la crab')
@@ -10,13 +9,20 @@ parser.add_argument('--verbose', action='store_true', help='Turn on verbosity.')
 parser.add_argument('--include-bin', action='store_true', help='Whether to include the bin dirs.')
 parser.add_argument('--no-include-python', action='store_false', dest='include_python', help='Whether to include the python dirs.')
 parser.add_argument('--no-include-interface', action='store_false', dest='include_interface', help='Whether to include the interface dirs.')
+parser.add_argument('--standalone', action='store_true', help='Instead of running make_tarball, dump this script to stdout in standalone version. (You still have to supply a dummy arg for the tarball name.)')
 options = parser.parse_args()
 
-if os.path.exists(options.tarball_fn):
-    raise IOError('refusing to clobber %s' % options.tarball_fn)
+if options.standalone:
+    import inspect
+    script = inspect.getsource(inspect.getmodule(inspect.currentframe()))
+    fcn = inspect.getsource(make_tarball)
+    print script.replace('from JMTucker.Tools.CMSSWTools import make_tarball\n', fcn)
+else:
+    if os.path.exists(options.tarball_fn):
+        raise IOError('refusing to clobber %s' % options.tarball_fn)
 
-make_tarball(options.tarball_fn,
-             verbose=options.verbose,
-             include_bin=options.include_bin,
-             include_python=options.include_python,
-             include_interface=options.include_interface)
+    make_tarball(options.tarball_fn,
+                 verbose=options.verbose,
+                 include_bin=options.include_bin,
+                 include_python=options.include_python,
+                 include_interface=options.include_interface)
