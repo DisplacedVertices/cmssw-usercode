bca9e382753f1d11e7479802c8ccedefae6ef456
Jordan Tucker <jordantucker@gmail.com>
2020-01-01 14:49:44 -0600

    symlinks

--- MFVNeutralino/test/MakeSamples/setup.sh
+++ MFVNeutralino/test/MakeSamples/setup.sh
@@ -1,2 +1,2 @@
-ln -s /uscms/home/tucker/public/mfv/private_minbias.txt.gz
-ln -s /uscms/home/tucker/public/mfv/minbias_premix.txt.gz
+ln -s /uscms/home/tucker/public/mfv/private_minbias_2016.txt.gz private_minbias.txt.gz
+ln -s /uscms/home/tucker/public/mfv/minbias_premix_2016.txt.gz minbias_premix.txt.gz
