f39393d38be2e5fe0e1ba2acf9ac3dcc77a21fde
Jordan Tucker <jordantucker@gmail.com>
2020-01-19 13:20:47 -0600

    allow arbitrary njobs, ntoysperjob; if results exists, don't remake unless ordered

--- MFVNeutralino/test/One2Two/process.py
+++ MFVNeutralino/test/One2Two/process.py
@@ -2,6 +2,7 @@ import os, sys
 from glob import glob
 from JMTucker.Tools import colors
 from JMTucker.Tools.ROOTTools import ROOT, detree
+from JMTucker.Tools.general import bool_from_argv
 from JMTucker.Tools.hadd import hadd
 
 def stats(fn_or_f, obs, l, header='sigma_sig_limit'):
@@ -28,25 +29,29 @@ def stats(fn_or_f, obs, l, header='sigma_sig_limit'):
     f.close()
     return median, lo68, hi68, lo95, hi95
 
+njobs, ntoysperjob = 50, 100
+#njobs, ntoysperjob = 5, 20
+
 def fromtree(fn):
     bn = os.path.basename(fn)
     f = ROOT.TFile(fn)
     ll = [x for x in detree(f.Get('limit'), 'limit:iToy', xform=(float,int))]
     if bn.startswith('expected'):
-        if len(ll) == 10000: # for some reason, there are two entries for every iToy when --saveToys used
+        ntoysexp = njobs * ntoysperjob
+        if len(ll) == 2*ntoysexp: # for some reason, there are two entries for every iToy when --saveToys used
             ll2 = []
             for mi,(l,i) in enumerate(ll):
-                assert (mi/2 % 100)+1 == i
+                assert (mi/2 % ntoysperjob)+1 == i
                 if mi % 2 == 1:
                     ll2.append(l)
-                elif mi % 200 == 0: # and the limit value saved in the first entry for the first toy in each batch is ~1e-300!
+                elif mi % 2*ntoysperjob == 0: # and the limit value saved in the first entry for the first toy in each batch is ~1e-300!
                     assert l < 1e-300
                 elif mi > 0:
                     assert l == ll2[-1]
-            assert len(ll2) == 5000
+            assert len(ll2) == ntoysexp
             return ll2
         else:
-            assert len(ll) == 5000 and [b for _,b in ll] == range(1,101)*50
+            assert len(ll) == ntoysexp and [b for _,b in ll] == range(1,ntoysperjob+1)*njobs
             return [a for a,_ in ll]
     else:
         assert bn == 'observed.root' and len(ll) == 1 and ll[0][1] == 0
@@ -63,12 +68,14 @@ def doit(path, out_fn):
     exp_fn = os.path.join(path, 'expected.root')
     if not os.path.isfile(exp_fn):
         exp_fns = glob(exp_fn.replace('.root','_*.root'))
-        if len(exp_fns) != 50 or not hadd(exp_fn, exp_fns):
-            raise ValueError('problem hadding %f from %i files' % len(exp_fns))
+        if len(exp_fns) != njobs:
+            raise ValueError('only found %i files, expected %i' % (len(exp_fns), njobs))
+        if not hadd(exp_fn, exp_fns):
+            raise ValueError('problem hadding %s from %s files' % (exp_fn, len(exp_fns)))
 
     exp = fromtree(exp_fn)
-    if len(exp) != 5000:
-        raise ValueError('unexpected number of points in %s' % exp_fn)
+    if len(exp) != njobs*ntoysperjob:
+        raise ValueError('unexpected number of points in %s' % exp_fn) # this can't be given the asserts in fromtree right now
 
     stats(out_fn, obs, exp)
 
@@ -79,6 +86,10 @@ def rrange(path):
 
 
 if __name__ == '__main__':
+    remake = bool_from_argv('remake')
     for path in sys.argv[1:]:
+        out_fn = os.path.join(path, 'results')
+        if not remake and os.path.isfile(out_fn):
+            continue
         print path
-        doit(path, os.path.join(path, 'results'))
+        doit(path, out_fn)
