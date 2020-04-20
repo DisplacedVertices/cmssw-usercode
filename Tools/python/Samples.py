8e5d1208c86488dc1ae4090bb01853baa36a345f
Jordan Tucker <jordantucker@gmail.com>
2020-04-20 12:42:51 -0500

    ntuplev27m_wgen 2017

--- Tools/python/Samples.py
+++ Tools/python/Samples.py
@@ -583,7 +583,7 @@ for x in data_samples_2017 + qcd_samples_2017 + ttbar_samples_2017 + all_signal_
         x.add_dataset("nr_trackmoverv27mv1_norefitdzcut")
 mfv_neu_tau010000um_M0800_2017.add_dataset('ntuplev27m_norefitdzcut')
 
-for x in all_signal_samples_2018:
+for x in all_signal_samples_2017 + all_signal_samples_2018:
     if x not in (mfv_stopdbardbar_tau001000um_M0800_2018, mfv_neu_tau001000um_M1600_2018):
         x.add_dataset("ntuplev27m_wgen")
 
