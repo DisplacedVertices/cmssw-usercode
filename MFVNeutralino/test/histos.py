27d55c471568852b462b47f8516aa29c538f8f72
Joseph Patrick Reichert <joseph.reichert@cern.ch>
2020-08-13 14:14:24 -0400

    MC bkg only for Peace to run over for now

--- MFVNeutralino/test/histos.py
+++ MFVNeutralino/test/histos.py
@@ -125,7 +125,7 @@ if __name__ == '__main__' and hasattr(sys, 'argv') and 'submit' in sys.argv:
         samples = pick_samples(dataset, qcd=True, ttbar=False, all_signal=True, data=False, bjet=True) # no data currently; no sliced ttbar since inclusive is used
         pset_modifier = chain_modifiers(is_mc_modifier, per_sample_pileup_weights_modifier(), half_mc_modifier())
     else :
-        samples = pick_samples(dataset)
+        samples = pick_samples(dataset, span_signal=False, all_signal=False, data=False)
         pset_modifier = chain_modifiers(is_mc_modifier, per_sample_pileup_weights_modifier())
 
     set_splitting(samples, dataset, 'histos', data_json=json_path('ana_2017p8.json'))
