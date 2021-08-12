cabf2b76a760e225823f39a06606394b2999b800
Ang Li <al8fm@virginia.edu>
2021-08-12 15:33:37 -0500

    remove previous IN analyser&config

--- MFVNeutralino/python/INoutputProducer_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-mfvINscore = cms.EDProducer('INoutputProducer',
-                            #graphPath=cms.string("/uscms/home/ali/nobackup/LLP/ML/IN_jets/constantgraph.pb"),
-                            graphPath=cms.string("/uscms/home/ali/nobackup/LLP/ML/IN_jets/savedModel"),
-                            mevent_src = cms.InputTag('mfvEvent'),
-                            njets = cms.int32(10),
-                           )
