87062413aa2dfb8172073b516454773afe01d1c8
Shaun Hogan <sph73@cornell.edu>
2022-02-04 12:07:43 -0600

    Two bug fixes (1 small, 1 big) in FilterHistos

--- MFVNeutralino/plugins/FilterHistos.cc
+++ MFVNeutralino/plugins/FilterHistos.cc
@@ -40,14 +40,14 @@ MFVFilterHistos::MFVFilterHistos(const edm::ParameterSet& cfg)
 
   for (int i = 0; i < MAX_NJETS+1; ++i) {
     TString ijet = i == MAX_NJETS ? TString("all") : TString::Format("%i", i);
-    h_jet_pt[i] = fs->make<TH2F>(TString::Format("h_jet_pt_%s", ijet.Data()), TString::Format(";;p_{T} of jet #%s (GeV)", ijet.Data()), 1+mfv::n_filter_paths, 0, 1+mfv::n_filter_paths, 200, 0, 2000);
+    h_jet_pt[i] = fs->make<TH2F>(TString::Format("h_jet_pt_%s", ijet.Data()), TString::Format(";;p_{T} of jet #%s (GeV)", ijet.Data()), 1+mfv::n_filter_paths, 0, 1+mfv::n_filter_paths, 200, 0, 400);
     h_jet_eta[i] = fs->make<TH2F>(TString::Format("h_jet_eta_%s", ijet.Data()), TString::Format(";;eta of jet #%s", ijet.Data()), 1+mfv::n_filter_paths, 0, 1+mfv::n_filter_paths, 120, -3, 3);
     h_jet_phi[i] = fs->make<TH2F>(TString::Format("h_jet_phi_%s", ijet.Data()), TString::Format(";;phi of jet #%s", ijet.Data()), 1+mfv::n_filter_paths, 0, 1+mfv::n_filter_paths, 100, -3.1416, 3.1416);
   }
 
-  h_jet_energy = fs->make<TH2F>("h_jet_energy", ";;jets energy (GeV)", 1+mfv::n_filter_paths, 0, 1+mfv::n_filter_paths, 200, 0, 2000);
-  h_jet_ht = fs->make<TH2F>("h_jet_ht", ";;H_{T} of jetsevents/25 GeV", 1+mfv::n_filter_paths, 0, 1+mfv::n_filter_paths, 200, 0, 5000);
-  h_jet_ht_40 = fs->make<TH2F>("h_jet_ht_40", ";;H_{T} of jets with p_{T} > 40 GeV", 1+mfv::n_filter_paths, 0, 1+mfv::n_filter_paths, 200, 0, 5000);
+  h_jet_energy = fs->make<TH2F>("h_jet_energy", ";;jets energy (GeV)", 1+mfv::n_filter_paths, 0, 1+mfv::n_filter_paths, 300, 0, 1200);
+  h_jet_ht = fs->make<TH2F>("h_jet_ht", ";;H_{T} of jets", 1+mfv::n_filter_paths, 0, 1+mfv::n_filter_paths, 300, 0, 1200);
+  h_jet_ht_40 = fs->make<TH2F>("h_jet_ht_40", ";;H_{T} of jets with p_{T} > 40 GeV", 1+mfv::n_filter_paths, 0, 1+mfv::n_filter_paths, 300, 0, 1200);
 
   h_jet_energy->GetXaxis()->SetBinLabel(1, "no filter");
   for (int i = 0; i < mfv::n_filter_paths; ++i) {
@@ -66,9 +66,6 @@ MFVFilterHistos::MFVFilterHistos(const edm::ParameterSet& cfg)
 
 }
 
-
-
-
 void MFVFilterHistos::analyze(const edm::Event& event, const edm::EventSetup&) {
   edm::Handle<MFVEvent> mevent;
   event.getByToken(mevent_token, mevent);
@@ -76,14 +73,28 @@ void MFVFilterHistos::analyze(const edm::Event& event, const edm::EventSetup&) {
   edm::Handle<double> weight;
   event.getByToken(weight_token, weight);
   const double w = *weight;
-
+  if (w < 0) return;
 
   for (int i = -1; i < mfv::n_filter_paths; ++i){
     bool passes_seq_filters = true;
-        for (int j = 0; j <= i; ++j){
-          if (not mevent->pass_filter(j))
-            passes_seq_filters = false;
+
+    if (i < 4) {
+      for (int j = 0; j <= i; ++j){
+        if (not mevent->pass_filter(j))
+          passes_seq_filters = false;
+      }
+    }
+
+    // Duplicate of above conditional block. This is a kludge-y way of separating filters
+    // into those that make up the tri- and di-bjet triggers. Di-bjet: [0-3], Tri-bjet: [4-13]
+    // FIXME SHAUN, is there a more compact way of doing this?
+    else {
+      for (int j = 4; j <= i; ++j) {
+        if (not mevent->pass_filter(j)) {
+          passes_seq_filters = false;
         }
+      }
+    }
 
     if (passes_seq_filters) {
       for (int k = 0; k < MAX_NJETS; ++k) {
@@ -92,12 +103,10 @@ void MFVFilterHistos::analyze(const edm::Event& event, const edm::EventSetup&) {
         h_jet_phi[k]->Fill(i+1, mevent->nth_jet_phi(k), w);
       }
 
-      h_jet_ht->Fill(i+1, mevent->jet_ht(mfv::min_jet_pt), w);
+      h_jet_ht->Fill(i+1, mevent->jet_ht(30), w);
       h_jet_ht_40->Fill(i+1, mevent->jet_ht(40), w);
     }
   }
-
-
 
 }
 
