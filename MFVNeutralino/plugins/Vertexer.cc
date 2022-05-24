69706793582fb245f50fc9a0e0663ecb714bfd86
Joseph Reichert <Joseph.Reichert@cern.ch>
2022-05-24 17:24:47 -0400

    add a comment on the track-to-jet-matching

--- MFVNeutralino/plugins/Vertexer.cc
+++ MFVNeutralino/plugins/Vertexer.cc
@@ -1656,6 +1656,7 @@ bool MFVVertexer::match_track_jet(const reco::Track& tk, const pat::Jet& matchje
     std::cout << "  target track pt " << tk.pt() << " eta " << tk.eta() << " phi " << tk.phi() << std::endl;
   }
 
+  // arbitrary threshold, but idea is to minimize [1+delta(pT)] * [1+delta(eta)] * [1+delta(phi)] in order to match the track to the jet
   double match_thres = 1.3;
   int jet_index = 255;
   for (size_t j = 0; j < jets.size(); ++j) {
