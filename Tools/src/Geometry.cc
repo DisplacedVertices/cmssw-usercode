10a248dcc2a7a6b9c5d0044e36478d01d3709602
Shaun Hogan <sph73@cornell.edu>
2022-12-15 11:22:52 -0600

    beampipe differences for 16 vs 17/18

--- Tools/src/Geometry.cc
+++ Tools/src/Geometry.cc
@@ -6,9 +6,10 @@
 namespace jmt {
   namespace Geometry {
     bool inside_beampipe(bool is_mc, double x, double y) {
-      const double r = 2.09;
-      const double c[3][2] = { {0.113, -0.180}, {0.171, -0.175}, {0,0} };
-      const int i = is_mc ? 2 : jmt::Year::get()-2017;
+      int idx = (jmt::Year::get() == 20161 || jmt::Year::get() == 20162) ? 0 : jmt::Year::get() == 2017 ? 1 : 2;
+      const double r = (idx == 0) ? 2.00 : 2.09;
+      const double c[4][2] = { {0.124, 0.07}, {0.113, -0.180}, {0.171, -0.175}, {0,0} };
+      const int i = is_mc ? 3 : idx;
       return std::hypot(x - c[i][0], y - c[i][1]) < r;
     }
 
