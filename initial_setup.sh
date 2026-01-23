7a1c05a51ac4bca63186887c01bdad332a847f0b
Joseph Reichert <Joseph.Reichert@cern.ch>
2026-01-23 15:04:11 -0500

    adding Rochester corrections and EgammaPostRecoTools into setup script

--- initial_setup.sh
+++ initial_setup.sh
@@ -5,6 +5,19 @@ cmsenv
 git cms-init --upstream-only
 #git clone https://github.com/DisplacedVertices/cmssw-usercode.git JMTucker # we can pull via https but not push. Leaving this here in case someone doesn't have ssh keys set up...
 git clone git@github.com:DisplacedVertices/cmssw-usercode.git JMTucker
+
+# dependencies for Rochester corrections
+git clone ssh://git@gitlab.cern.ch:7999/akhukhun/roccor.git RoccoR
+
+# from EgammaPostRecoTools scales/smearings from 
+# https://twiki.cern.ch/twiki/bin/viewauth/CMS/EgammaUL2016To2018#Recipe_for_running_scales_and_sm
+git cms-addpkg RecoEgamma/EgammaTools  ### essentially just checkout the package from CMSSW
+git clone git@github.com:cms-egamma/EgammaPostRecoTools.git
+mv EgammaPostRecoTools/python/EgammaPostRecoTools.py RecoEgamma/EgammaTools/python/.
+git clone -b ULSSfiles_correctScaleSysMC git@github.com:jainshilpi/EgammaAnalysis-ElectronTools.git EgammaAnalysis/ElectronTools/data/
+git cms-addpkg EgammaAnalysis/ElectronTools
+
+
 cd JMTucker
 git checkout UL_Lepton
 scram b -j 4
