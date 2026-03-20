173022d50b3565878c5d4f6647a21f2ed0920949
Gianfranco de Castro <37059445+Gianco99@users.noreply.github.com>
2026-03-20 10:33:31 -0400

    Quick Bugfix
    Was not copying cs_xrootd_url when transferring input files when using local staging

--- Tools/python/CondorSubmitter.py
+++ Tools/python/CondorSubmitter.py
@@ -168,7 +168,7 @@ should_transfer_files = YES
 when_to_transfer_output = ON_EXIT
 # Default memory request is 2100 MB. Request more only when needed, otherwise our priority drops faster and it takes longer to get a slot!
 #RequestMemory = 4000
-transfer_input_files = __TARBALL_FN__,cs_jobmap,cs_njobs,cs_pset.py,cs_filelist.py,cs.json,cs_cmsrun_args,cs_primaryds,cs_samplename,cs_timestamp,cs_split_by_events__INPUT_FNS__
+transfer_input_files = __TARBALL_FN__,cs_jobmap,cs_njobs,cs_pset.py,cs_filelist.py,cs.json,cs_cmsrun_args,cs_primaryds,cs_samplename,cs_timestamp,cs_xrootd_url,cs_split_by_events__INPUT_FNS__
 +SingularityImage = "/cvmfs/unpacked.cern.ch/registry.hub.docker.com/cmssw/el7:x86_64"
 x509userproxy = $ENV(X509_USER_PROXY)
 __EXTRAS__
