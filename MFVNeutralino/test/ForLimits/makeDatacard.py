c0a0f143967d20573eedd9a9ca7ce84d26dd1ef4
Gianfranco de Castro <gianfrancocyclohexane@gmail.com>
2026-06-11 12:32:17 -0500

    Add from __future__ import print_function for Python 2 compatibility

--- MFVNeutralino/test/ForLimits/makeDatacard.py
+++ MFVNeutralino/test/ForLimits/makeDatacard.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import ROOT
 import numpy as np
 
--- MFVNeutralino/test/ForLimits/nuisance_configs_and_functions.py
+++ MFVNeutralino/test/ForLimits/nuisance_configs_and_functions.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy as np
 
 import helper_PyStorage_objects as sth
