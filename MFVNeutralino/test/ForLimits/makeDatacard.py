b1189668c0185457c0d17f6f3515b18f1e15de05
Gianfranco de Castro <gianfrancocyclohexane@gmail.com>
2026-07-24 22:25:58 -0500

    gmN empty-bin alpha back to L x sum(xsec) / sum(N_gen), matching the AN

--- MFVNeutralino/test/ForLimits/makeDatacard.py
+++ MFVNeutralino/test/ForLimits/makeDatacard.py
@@ -318,11 +318,11 @@ def return_special_lines(f, ns_ls, sig_norm_ls, siggrp, sig_id, write_sig=True):
             sig_cts_hname = "h_sig%s_ngen_perbin_%s" % (sig_id, year)
             h_sig_cts = f.Get(sig_cts_hname)
 
-            _h_sumw     = f.Get("h_sig%s_sumw_%s"          % (sig_id, year))
+            _h_ngen     = f.Get("h_sig%s_ngen_total_%s"    % (sig_id, year))
             _h_sigyield = f.Get("h_sig%s_sigyield_total_%s" % (sig_id, year))
-            _sumw_total     = _h_sumw.GetBinContent(1)     if _h_sumw     else 0.0
+            _ngen_total     = _h_ngen.GetBinContent(1)     if _h_ngen     else 0.0
             _sigyield_total = _h_sigyield.GetBinContent(1) if _h_sigyield else 0.0
-            _w_empty = (_sigyield_total / _sumw_total) if _sumw_total > 0 else 0.0
+            _w_empty = (_sigyield_total / _ngen_total) if _ngen_total > 0 else 0.0
 
             for i in range(nbins):
                 strls = []
